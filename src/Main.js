4e07f756baa1d40f46d61d78714a1ef4e297c6da
dev-dino22 <wlsskdud02@naver.com>
2024-10-30 05:32:56 +0900

    feat(Main.js): 로또 구매 개수 계산 및 출력 기능 구현

--- src/Main.js
+++ src/Main.js
@@ -6,18 +6,75 @@ const MESSAGES = {
     WINNING_NUMBERS: '당첨 번호를 입력해주세요.',
     BONUS_NUMBER: '보너스 번호를 입력해주세요.',
   },
+  ERROR: {
+    IS_EMPTY: '[ERROR] 입력값이 비었습니다.',
+    NOT_NUMBER: '[ERROR] 숫자를 입력해주세요.',
+    NOT_NATURAL_NUMBER: '[ERROR] 자연수를 입력해주세요.',
+    NOT_DIVISIBLE_BY_THOUSAND: '[ERROR] 1000의 배수를 입력해주세요.',
+  },
+};
+
+const InputValidator = {
+  isEmpty: (input) => {
+    if (input === null || input.trim().length === 0 || !input) {
+      throw new Error(MESSAGES.ERROR.IS_EMPTY);
+    }
+  },
+  isNumber: (input) => {
+    if (isNaN(Number(input))) {
+      throw new Error(MESSAGES.ERROR.NOT_NUMBER);
+    }
+  },
+  isNaturalNumber: (input) => {
+    if (!Number.isInteger(input) || Number(input) <= 0) {
+      throw new Error(MESSAGES.ERROR.NOT_NATURAL_NUMBER);
+    }
+  },
+  isDivisibleByThousand: (input) => {
+    if (input % 1000 !== 0) {
+      throw new Error(MESSAGES.ERROR.NOT_DIVISIBLE_BY_THOUSAND);
+    }
+  },
+};
+
+const Parser = {
+  parseNumber: (input) => {
+    InputValidator.isEmpty(input);
+    InputValidator.isNumber(input);
+    return Number(input);
+  },
+};
+
+class LottoValidator {
+  static validatePurchaseAmount(input) {
+    const purchaseAmount = Parser.parseNumber(input);
+    InputValidator.isNaturalNumber(purchaseAmount);
+    InputValidator.isDivisibleByThousand(purchaseAmount);
+
+    return purchaseAmount;
+  }
+}
+
+const calculatePurchaseNumber = (purchaseAmount) => {
+  return purchaseAmount / 1000;
 };
 
 const Main = async () => {
   Console.print('프로그램 시작!');
 
-  const purchaseAmount = await Console.readLineAsync(
+  const inputPurchaseAmount = await Console.readLineAsync(
     MESSAGES.INFO.PURCHASE_AMOUNT,
   );
+  const purchaseAmount =
+    LottoValidator.validatePurchaseAmount(inputPurchaseAmount);
+
   const winningNumbers = await Console.readLineAsync(
     MESSAGES.INFO.WINNING_NUMBERS,
   );
   const bonusNumber = await Console.readLineAsync(MESSAGES.INFO.BONUS_NUMBER);
+
+  const purchaseNumber = calculatePurchaseNumber(purchaseAmount);
+  Console.print(`${purchaseNumber}개를 구매했습니다.`);
 };
 
 export default Main;
