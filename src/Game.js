11b1996c0e30df8de96041300cdd04fb6caf8f50
murphybread <nargene@gmail.com>
2024-10-29 17:00:53 +0900

    feat(logic): generate unique 6 numbers based on count

---
+++ src/Game.js
@@ -0,0 +1,18 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
+class Game {
+
+    generateLotto (count) {
+        const lottos = [];
+
+        for (let i = 0; i < count; i++) {
+            const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.push(lotto);
+          }
+
+        return lottos;
+    }
+}
+
+const test = new Game();
+Console.print(test.generateLotto(8));
