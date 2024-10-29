cf4af5d94362b7ad2151147b1493ce7d4a014f0c
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-29 21:59:16 +0900

    feat(app): 구매 금액 입력 받기 로직 구현
    - 안내 메시지 상수로 관리
    - InputProcessor 클래스 추가로 입력 처리 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import InputProcessor from './InputProcessor.js';
+import MESSAGES from './constants/messages.js';
+
 class App {
-  async run() {}
+  #purchasePrice;
+
+  async run() {
+    this.#purchasePrice = await this.#getPurchasePrice();
+  }
+
+  async #getPurchasePrice() {
+    const input = await InputProcessor.get(MESSAGES.PRICE_INPUT);
+    return input;
+  }
 }
 
 export default App;
---
+++ src/constants/messages.js
@@ -0,0 +1,5 @@
+const PROMPT = {
+  PRICE_INPUT: '구매금액을 입력해 주세요.\n'
+};
+
+export default PROMPT;
