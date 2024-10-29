1605338e050dff1d99a60b53e1418ddea969583b
ryanbae94 <ryanbae94@gmail.com>
2024-10-29 20:21:22 +0900

    feat (messages): 에러 메세지 추가
    - 에러 프리픽스 상수화
    - 에러 메세지 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,3 +1,13 @@
-const MESSAGE = Object.freeze({
+export const MESSAGE = Object.freeze({
 	COST_MESSAGE: '구입금액을 입력해 주세요.',
+});
+
+export const ERROR_PREFIX = '[ERROR]';
+
+export const ERROR_MESSAGE = Object.freeze({
+	NOT_A_NUMBER: '숫자를 입력해 주세요.',
+	EMPTY: '아무것도 입력되지 않았습니다.',
+	HAS_WHITE_SPACE: '공백이 포함될 수 없습니다.',
+	IS_NEGATIVE: '음수가 입력될 수 없습니다.',
+	NOT_DIVIDED_BY_THOUSAND: '금액은 1000원 단위로 입력해주세요.',
 });
