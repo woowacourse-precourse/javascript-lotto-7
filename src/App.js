ed82211a7a3135614c72e7651a6989111dc2de3c
Eunjin Cho <whdmswls9207@gmail.com>
2024-10-29 22:57:38 +0900

    feat: 구입금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const cost = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+  }
 }
 
 export default App;
