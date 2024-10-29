5c4cc12ba4721f08ea9eb0762a4092d88eec1b4f
suyeon <moa0823@naver.com>
2024-10-29 17:52:45 +0900

    test : 당첨금 계산 테스트
    - 각 로또의 등수에 따른 당첨금 합계 테스트

---
+++ __tests__/CalculatorTest.js
@@ -0,0 +1,15 @@
+describe("계산기 클래스 테스트", () => {
+  test("당첨금 계산 테스트", () => {
+    const RANKING = [
+      [1, 1, 1],
+      [2, 5],
+      [3, false],
+      [1, 2, 3, 4, 5, false],
+    ];
+    const RESULT = [6000000000, 30005000, 1500000, 2031555000];
+
+    for (let i = 0; i < RANKING.length; i++) {
+      expect(calculator.prizeAmount(RANKING[i])).toBe(RESULT[i]);
+    }
+  });
+});
