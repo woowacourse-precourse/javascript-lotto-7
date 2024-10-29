c58148516967376fe958984e6cbf388a6eba589f
JAESEUNGJAE <valkyrie9923@gmail.com>
2024-10-30 01:26:38 +0900

    feat: 로또 구입 및 당첨 번호 입력 기능 추가
    - getMoney 함수 구현: 사용자의 구입 금액 입력을 받고 1000원 단위인지 확인
    - getLottos 함수 구현: 구입 금액에 따라 로또 번호 생성 및 출력
    - get CorrectNumbers 함수 구현: 사용자가 입력한 당첨 번호를 배열로 반환
    - getBonusNumber 함수 구현: 보너스 번호 입력과 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const money = await this.getMoney();
+    await this.getCorrectNumbers();
+    await this.getLottos(money);
+  }
+
+  async getMoney() {
+    const input = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    if (input % 1000 !== 0) {
+      throw new Error("[ERROR] 구매 금액은 1000원 단위이어야 합니다.");
+    }
+    return input;
+  }
+
+  async getLottos(money) {
+    const count = money / 1000;
+    MissionUtils.Console.print(`${count}개를 구매했습니다.`);
+    const lottoArray = Array.from({length: count}, () => {
+      const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      return randomNumbers.sort((a, b) => a - b);
+    });
+    return lottoArray;
+  }
+
+  async getCorrectNumbers() {
+    const input = await MissionUtils.Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    const numberArray = input.split(",").map(Number);
+    if (numberArray.some(num => num > 45 || num < 1)) {
+      throw new Error("[ERROR] 잘못된 숫자 입력.");
+    }
+    return numberArray;
+  }
+
+  async getBonusNumber() {
+    const input = await MissionUtils.Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    if (input > 45 || input < 0) {
+      throw new Error("[ERROR] 잘못된 숫자 입력.");
+    }
+    return input;
+  }
 }
 
 export default App;
