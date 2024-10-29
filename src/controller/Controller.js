e9f3c60eb8be1b23916c5b9a9848eba4dd3a30a0
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-30 00:22:55 +0900

    feat: 로또 발행 기능 추가
    - 입력받은 구입 금액에 따라 로또 개수 발행
    - 로또 번호는 오름차순으로 정렬
    - 로또 번호 범위, 로또 번호의 개수(6) 상수로 분리

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,7 +1,14 @@
-import { LOTTO_PRICE_UNIT } from '../constants/constants.js';
+import { Random } from '@woowacourse/mission-utils';
+import {
+  LOTTO_NUMBER_COUNT,
+  LOTTO_NUMBER_MAX,
+  LOTTO_NUMBER_MIN,
+  LOTTO_PRICE_UNIT,
+} from '../constants/constants.js';
 import { validatePurchasePrice } from '../validation/validation.js';
 import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
+import Lotto from '../Lotto.js';
 
 class Controller {
   #inputView;
@@ -17,6 +24,8 @@ class Controller {
 
     const lottoCount = purchasePrice / LOTTO_PRICE_UNIT;
     this.#outputView.printLottoCount(lottoCount);
+
+    const lottos = this.getParsedLottos(lottoCount);
   }
 
   async getParsedPurchasePrice() {
@@ -27,6 +36,22 @@ class Controller {
     validatePurchasePrice(purchasePrice);
 
     return Number(purchasePrice);
+  }
+
+  getParsedLottos(lottoCount) {
+    const lottos = [];
+
+    for (let i = 0; i < lottoCount; i++) {
+      const lotto = Random.pickUniqueNumbersInRange(
+        LOTTO_NUMBER_MIN,
+        LOTTO_NUMBER_MAX,
+        LOTTO_NUMBER_COUNT
+      );
+
+      lottos.push(lotto.sort((a, b) => a - b));
+    }
+
+    return lottos.map((numbers) => new Lotto(numbers));
   }
 }
 
