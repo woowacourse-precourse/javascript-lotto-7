937f3738c53a3dc6a2d9b8f00d797b3e8669c808
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-29 18:32:37 +0900

    당첨번호 및 보너스 번호 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import BettedMoney from "./BettedMoney.js";
 
 class App {
   async run() {
-    const bettedMoney = new BettedMoney(String(
-      await Console.readLineAsync("구매할 금액을 입력해주세요.\n")
-    ));
+    const bettedMoney = new BettedMoney(
+      String(await Console.readLineAsync("구매할 금액을 입력해주세요.\n"))
+    );
+    const winningNumbers = new Lotto(
+      String(await Console.readLineAsync("당첨 번호를 입력해 주세요.\n"))
+    );
+    await winningNumbers.inputBonusNumber();
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,62 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+  constructor(input) {
+    input = this.#str2Arr(input);
+    this.#validate(input);
+    this.#numbers = input.map(Number);
   }
 
   #validate(numbers) {
+    numbers.forEach((number) => {
+      this.#validateNumber(number);
+    });
+    this.#validateNumbers(numbers);
+  }
+  #validateNumber(number) {
+    if (number === "") {
+      throw new Error("공백은 입력될 수 없습니다.");
+    }
+    if (isNaN(number)) {
+      throw new Error("숫자를 입력해주세요.");
+    }
+    if (number > 45 || number < 1) {
+      throw new Error("1이상 45이하의 숫자를 입력해주세요.");
+    }
+  }
+  #validateNumbers(numbers) {
+    this.#validateIsSix(numbers);
+    this.#validateIsUnique(numbers);
+  }
+  #validateIsSix(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
+  #validateIsUnique(numbers) {
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error("[ERROR] 중복되는 번호는 입력될 수 없습니다.");
+    }
+  }
+  // ,을 구분자로 배열을 리턴하는 함수
+  #str2Arr(numbers) {
+    return numbers.split(",");
+  }
   // TODO: 추가 기능 구현
+  async inputBonusNumber() {
+    const bonusNumber = String(
+      await Console.readLineAsync("보너스 번호를 입력해 주세요.\n")
+    );
+    this.#validateNumber(bonusNumber);
+    this.#numbers.push(Number(bonusNumber));
+    this.#validateIsUnique(this.#numbers);
+  }
+
+  getLottoNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
