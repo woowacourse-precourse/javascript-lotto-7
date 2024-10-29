e081d4a264a5c249eb7cba685d15343de9b3e5bc
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-29 15:50:24 +0900

    feat : 금액 입력 기능 추가 및 관련 클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+import BettedMoney from "./BettedMoney.js";
+
 class App {
-  async run() {}
+  async run() {
+    const bettedMoney = new BettedMoney(String(
+      await Console.readLineAsync("구매할 금액을 입력해주세요.\n")
+    )).getMoney();
+  }
 }
 
 export default App;
---
+++ src/BettedMoney.js
@@ -0,0 +1,35 @@
+class BettedMoney {
+  #money;
+
+  constructor(money) {
+    this.#validate(money);
+    this.#money = money;
+  }
+
+  #validate(money) {
+    this.#validateBlank(money);
+    this.#validateNumber(money);
+    this.#validateUnit(money);
+  }
+
+  #validateBlank(money) {
+    if (money === "") throw new Error("공백은 입력될 수 없습니다.");
+  }
+
+  #validateNumber(money) {
+    if (isNaN(money)) throw new Error("숫자를 입력해주세요.");
+  }
+
+  #validateUnit(money) {
+    if (money % 1000 !== 0) throw new Error("1000단위의 숫자를 입력해주세요.");
+  }
+  // TODO: 추가 기능 구현
+  getMoney() {
+    return this.#money;
+  }
+  getMoneyToCount() {
+    return this.#money / 10;
+  }
+}
+
+export default BettedMoney;
