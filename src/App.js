81657e87c88f7de52c9ccd5096480be00d324980
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-30 01:53:27 +0900

    feat(input): 사용자로부터 구입금액을 입력받는 getPurchaseAmount 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
+async function getPurchaseAmount() {
+  const input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  return Number(input);
+}
+
 class App {
-  async run() {}
+  async run() {
+    let seedMoney = await getPurchaseAmount();
+  }
 }
 
 export default App;
