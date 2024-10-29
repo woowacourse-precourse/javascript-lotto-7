5164ac0a2472713dea09ac6cf7cd259a1daed5b3
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-29 18:56:02 +0900

    feat : 입력된 횟수 속성 추가 && 랜덤번호 출력기능 추가  && 인스턴스 클래스 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
-import BettedMoney from "./BettedMoney.js";
+import Bet from "./Bet.js";
 
 class App {
   async run() {
-    const bettedMoney = new BettedMoney(
+    const Bet = new Bet(
       String(await Console.readLineAsync("구매할 금액을 입력해주세요.\n"))
     );
-    const winningNumbers = new Lotto(
+    const betList = Bet.getBetResults();
+    const lotto = new Lotto(
       String(await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n"))
     );
-    await winningNumbers.inputBonusNumber();
+    await lotto.inputBonusNumber();
   }
 }
 
--- src/Bet.js
+++ src/Bet.js
@@ -1,9 +1,12 @@
-class BettedMoney {
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+class Bet {
   #money;
+  count;
 
   constructor(money) {
     this.#validate(money);
     this.#money = money;
+    this.count = money / 1000;
   }
 
   #validate(money) {
@@ -24,12 +27,16 @@ class BettedMoney {
     if (money % 1000 !== 0) throw new Error("1000단위의 숫자를 입력해주세요.");
   }
   // TODO: 추가 기능 구현
-  getMoney() {
-    return this.#money;
-  }
-  getMoneyToCount() {
-    return this.#money / 1000;
+  getBetResults() {
+    Console.print(`\n${this.count}개를 구매했습니다.`);
+    const betResults = [];
+    for (let index = 0; index < this.count; index++) {
+      const betResult = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      Console.print(betResult);
+      betResults.push(betResult);
+    }
+    return betResults;
   }
 }
 
-export default BettedMoney;
+export default Bet;
