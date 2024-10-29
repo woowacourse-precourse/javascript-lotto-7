6fdc2a1c661065f7d4e983a160f2ea7d512f6ece
youngsu <everdudtn@gmail.com>
2024-10-30 00:06:47 +0900

    feat(app): 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const PROMPT_MESSAGES = {
+  BUY_LOTTO: "구입금액을 입력해 주세요.",
+};
+
+const ERROR_MESSAGES = {
+  INVAILED_AMOUNT: "[ERROR] 유효하지 않은 금액이 입력되었습니다.",
+};
+
 class App {
-  async run() {}
+  async #getAmount() {
+    MissionUtils.Console.print(PROMPT_MESSAGES.BUY_LOTTO);
+    const amount = parseInt(await MissionUtils.Console.readLineAsync(""), 10);
+    if (!(amount > 0)) {
+      throw new Error(ERROR_MESSAGES.INVAILED_AMOUNT);
+    }
+    return amount;
+  }
+
+  async run() {
+    const amount = await this.#getAmount();
+  }
 }
 
 export default App;
