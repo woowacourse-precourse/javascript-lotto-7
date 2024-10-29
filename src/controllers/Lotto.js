819a6045c7eddb7f50e0b2e07588af27bd99b0c7
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-10-30 07:31:26 +0900

    Refactor: 모듈 구조를 명확하고 표준화된 방식으로 재정의
    - 'check'를 비즈니스 로직 처리를 위한 'controllers'로 변경
    - 'profit'을 실제 데이터 처리 및 이익 계산을 위한 'services'로 변경
    - 'input'을 공통 유틸리티 함수 및 일반적인 데이터 처리를 돕는  'utils'로 변경

--- src/controllers/Lotto.js
+++ src/controllers/Lotto.js
@@ -8,11 +8,12 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  // TDOD: 시간이 남으면 public > index.html 항목 구현
 }
 
 export default Lotto;
