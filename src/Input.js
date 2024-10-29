a0c64c1d3473f0d34ea0468bd14bd2953257e5bb
murphybread <nargene@gmail.com>
2024-10-29 16:44:21 +0900

    refactor(getUserInput): change to public method for using data

--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,13 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class UserInput {
-    async #getUserInput() {
+    async getUserInput() {
         const paidMoney = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요\n");
         MissionUtils.Console.print(paidMoney);
         return paidMoney;
       }
-    
-      async requestUserInput() {
-        return await this.#getUserInput();
-      }
+
 }
 
 const test = new UserInput();
-test.requestUserInput()
+test.getUserInput()
