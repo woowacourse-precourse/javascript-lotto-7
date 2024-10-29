d97ffb6a1335a897d150670bb1e4d3304a917b85
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-30 02:11:44 +0900

    feat(validate): 입력된 구매금액에 대해 체크하는 validatePurchaseAmount 메서드 구현 및 getPurchaseAmount 추가 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
 async function getPurchaseAmount() {
-  const input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  let input = '';
+  while (true) {
+    input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    if (validatePurchaseAmount(input)) break;
+    Console.print('[ERROR] 구입금액은 1,000원 단위여야 합니다!!');
+  }
   return Number(input);
+}
+
+function validatePurchaseAmount(input) {
+  const num = Number(input);
+  if (!isNaN(num) && num % 1000 === 0 && num > 0) return true;
 }
 
 class App {
