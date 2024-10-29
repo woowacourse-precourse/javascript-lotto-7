0d5856ea2aeb40382514792bc18c39a4e76fc185
murphybread <nargene@gmail.com>
2024-10-29 17:24:30 +0900

    refactor(getUserPaidMoney): change method name for clarity

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class UserInput {
-    async getUserInput() {
+    async getUserPaidMoney() {
         const paidMoney = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요\n");
         MissionUtils.Console.print(paidMoney);
         return paidMoney;
