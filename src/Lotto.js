378727b36d7dc6d70bf189c75f8f172ffddb2b22
ockko <ocko119@gmail.com>
2024-10-29 23:54:37 +0900

    feat: 로또 클래스 숫자 범위 예외 상황 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    numbers.map((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
