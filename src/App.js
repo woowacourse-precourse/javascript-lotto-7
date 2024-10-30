268c5b829e2850c8e428bf7bd657ab0f7a0f39fe
Glyph8 <newdy0808@gmail.com>
2024-10-30 15:19:12 +0900

    feat(Console) 필수 입출력 작성
    필요한 API import, 필수 입출력 기능 추가.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import {Random, Console} from '@woowacourse/mission-utils'
+
 class App {
-  async run() {}
+  async run() {
+    const totalMoeny = await Console.readLineAsync("구매할 금액을 입력해주세요 : ");
+
+    const winNumber = await Console.readLineAsync("당첨번호를 입력해주세요 : ");
+
+    const bonusNumber = await Console.readLineAsync("보너스 번호를 입력해주세요 : ");
+
+    Console.print("당첨 결과");
+
+    Console.print("수익률");
+
+  }
 }
 
 export default App;
