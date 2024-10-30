94cfc90767f9479c4a2b8318d90fe4ad6ebfe13c
Kim <kevin2695@naver.com>
2024-10-30 17:08:18 +0900

    feat(app) : 구입 금액을 입력받고 에러 메시지가 표출되는 기능 추가
    - 구입 금액 입력 기능
    - 구입 금액이 숫자가 아닌 경우 에러 발생
    - 구입 금액이 1000으로 나누어 떨어지지 않을 때 에러 발생

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const NUMBER_REGEX = /^[0-9]*$/;
 class App {
-  async run() {}
+  async run() {
+    try {
+      // purchase input
+      const purchaseAmount = await MissionUtils.Console.readLineAsync(
+        "구입금액을 입력해 주세요."
+      );
+
+      // purchase validate
+      // 1. 숫자가 아닌 경우
+      if (!NUMBER_REGEX.test(purchaseAmount)) {
+        throw new Error("숫자가 아닌 문자가 입력되었습니다.");
+      }
+      // 2. 1000 단위로 나누어 떨어지지 않는 경우
+      if (Number(purchaseAmount) % 1000 !== 0) {
+        throw new Error("1000원 단위로 나누어 떨어지지 않습니다.");
+      }
+    } catch (error) {
+      console.log(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
