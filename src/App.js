d64364f0a7217b7d4f125b625b1ca57cb222228b
Minhyuk Kim <labcs@naver.com>
2024-10-30 19:26:41 +0900

    feat: 로또 정렬, 중복 여부 확인 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+// import Lotto from "./Lotto";
+import { MissionUtils } from "@woowacourse/mission-utils"
+
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    MissionUtils.Console.print(purchaseAmount);
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,35 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.#lottoSort(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (!this.#isOnlyUnique(numbers)) {
+      throw new Error("[ERROR] 중복된 숫자가 존재하지 않아야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  #lottoSort(numbers) {
+    numbers.sort((a, b) => a - b);
+  }
+
+  #isOnlyUnique(numbers) {
+    const stack = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+      if (stack.includes(numbers[i])) {
+        return false;
+      }
+      stack.push(numbers[i]);
+    }
+
+    return true;
+  }
+
 }
 
 export default Lotto;
