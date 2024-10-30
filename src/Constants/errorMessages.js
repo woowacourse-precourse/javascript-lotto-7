1b652bee878d1e41037cf30180fa5fcd94a05cb2
Jinny <ggab.challenge@gmail.com>
2024-10-31 01:52:25 +0900

    feat: 당첨번호를 중복된 숫자로 입력한 경우 에러 처리
    - README.md 기능 요구 사항에 추가

--- src/Constants/errorMessages.js
+++ src/Constants/errorMessages.js
@@ -11,8 +11,9 @@ const ERROR_MESSAGE = {
 
   // winningNumbersError
   EMPTY_INPUT_WINNING_NUMBERS: `${ERROR} 빈값입니다. \n ${WINNING_NUMBERS_MESSAGE}`,
-  NUMBERS_LENGTH: `${ERROR} 입력하신 당첨번호의 개수가 6개가 아닙닙다. \n ${WINNING_NUMBERS_MESSAGE}`,
+  NUMBERS_LENGTH: `${ERROR} 입력하신 당첨번호의 개수가 6개가 아닙닙다. \n ${WINNING_NUMBERS_MESSAGE}`, // TODO: 리펙토링-오탈자 고치기
   COMMA_COUNT: `${ERROR} 입력하신 모든 당첨번호의 구분이 쉼표(,) 로 되어있지 않습니다. \n ${WINNING_NUMBERS_MESSAGE}`,
+  UNIQUE_NUMBER: `${ERROR} 입력하신 당첨번호의 숫자가 중복되었습니다. \n ${WINNING_NUMBERS_MESSAGE}`,
 
   // bonusNumberError
   EMPTY_INPUT_BONUS_NUMBER: `${ERROR} 빈값입니다. \n ${BONUS_NUMBER_MESSAGE}`,
--- src/Models/winningNumbersValidator.js
+++ src/Models/winningNumbersValidator.js
@@ -5,13 +5,24 @@ const isWinningNumbersEmptyInput = (winningNumbersInput) => winningNumbersInput 
 const isWinningNumbersSixDigits = (winningNumbersInput) => {
   const arr = [...winningNumbersInput];
   const onlyNumber = arr.filter((number) => !isNaN(number) === true);
+
   return onlyNumber.length === 6;
 };
 
 const isWinningNumbersFiveComma = (winningNumbersInput) => {
   const arr = [...winningNumbersInput];
   const onlyNumber = arr.filter((number) => number === ',');
+
   return onlyNumber.length === 5;
+};
+
+// 중복된 숫자 되면 안됨
+const isWinningNumberDuplicated = (winningNumbers) => {
+  const arr = [...winningNumbers];
+  const onlyNumber = arr.filter((number) => !isNaN(number) === true);
+  const uniqueWinningNumbers = new Set(onlyNumber);
+
+  return onlyNumber.length === uniqueWinningNumbers.size;
 };
 
 const validateWinningNumbers = (winningNumbers) => {
@@ -25,6 +36,10 @@ const validateWinningNumbers = (winningNumbers) => {
 
   if (!isWinningNumbersFiveComma(winningNumbers)) {
     throw new Error(ERROR_MESSAGE.COMMA_COUNT);
+  }
+
+  if (!isWinningNumberDuplicated(winningNumbers)) {
+    throw new Error(ERROR_MESSAGE.UNIQUE_NUMBER);
   }
 };
 
