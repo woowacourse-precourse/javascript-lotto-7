859c33bc38a8716689e153dffe16e44252ceb771
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-10-30 15:23:00 +0900

    feat: Lotto Class 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  // TODO: 추가 기능 구현
+}
+
 class App {
   async run() {
     const count = await this.getLottoCount();
