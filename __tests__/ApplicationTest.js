62dc23f1f0160a671afeb20e1914718237b63877
guesung <gueit214@naver.com>
2024-10-30 11:45:53 +0900

    test: App 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -43,16 +43,9 @@ const runException = async (input) => {
   expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
 };
 
-describe('로또 테스트', () => {
-  beforeEach(() => {
-    jest.restoreAllMocks();
-  });
-
-  test('기능 테스트', async () => {
-    // given
-    const logSpy = getLogSpy();
-
-    mockRandoms([
+const lottoTestCaseArray = [
+  {
+    mockRandomArray: [
       [8, 21, 23, 41, 42, 43],
       [3, 5, 11, 16, 32, 38],
       [7, 11, 16, 35, 36, 44],
@@ -61,15 +54,9 @@ describe('로또 테스트', () => {
       [7, 11, 30, 40, 42, 43],
       [2, 13, 22, 32, 38, 45],
       [1, 3, 5, 14, 22, 45],
-    ]);
-    mockQuestions(['8000', '1,2,3,4,5,6', '7']);
-
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
-    const logs = [
+    ],
+    mockQuestionArray: ['8000', '1,2,3,4,5,6', '7'],
+    logArray: [
       '8개를 구매했습니다.',
       '[8, 21, 23, 41, 42, 43]',
       '[3, 5, 11, 16, 32, 38]',
@@ -85,14 +72,46 @@ describe('로또 테스트', () => {
       '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
       '6개 일치 (2,000,000,000원) - 0개',
       '총 수익률은 62.5%입니다.',
-    ];
+    ],
+  },
+];
 
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+describe('로또 테스트', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
   });
 
-  test('예외 테스트', async () => {
-    await runException('1000j');
+  describe('기능 테스트', () => {
+    test.each(lottoTestCaseArray)(
+      '$title',
+      async ({ logArray, mockQuestionArray, mockRandomArray }) => {
+        const logSpy = getLogSpy();
+
+        mockRandoms(mockRandomArray);
+        mockQuestions(mockQuestionArray);
+
+        const app = new App();
+        await app.run();
+
+        const logs = logArray;
+
+        logs.forEach((log) => {
+          expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+        });
+      },
+    );
+  });
+
+  describe('예외 테스트', () => {
+    test('숫자가 아닌 경우 에러를 출력한다.', async () => {
+      await runException('1000j');
+    });
+    test('1,000원 단위가 아닌 경우 에러를 출력한다.', async () => {
+      await runException('1010');
+    });
+    test('양수가 아닌 경우 에러를 출력한다.', async () => {
+      await runException('0');
+      await runException('-1000');
+    });
   });
 });
