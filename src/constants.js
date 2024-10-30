643e242afa7e281d8565481f6890b290a5cf9f57
xxziiko <xxziiko@gmail.com>
2024-10-30 18:08:48 +0900

    chore(constants): 상수 문자열 정의

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const PROMPT_MESSAGES = Object.freeze({
+  INPUT_MONEY: '구입금액을 입력해 주세요.\n',
+});
+
+export const ERROR_MESSAGES = Object.freeze({
+  INVALID_NUMBER_LENGTH: '로또 번호는 6개여야 합니다.',
+  INVALID_TYPE: '숫자를 입력해주세요.',
+  INVALID_AMOUNT: '금액은 1000원 단위로 입력해주세요.',
+});
+
+export const INFO_MESSAGES = Object.freeze({});
