5dc02db51987bb7059826e6aa542a4def13bd406
yusuShin <tlsdbtn0507@naver.com>
2024-10-31 00:33:10 +0900

    refactor(constant.js) Change way to export constants - Export Prompts obj instead of single export because constant must increase

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
-const PURCHASE_PROMPT = "구입금액을 입력해 주세요.";
+const PROMPTS = {
+  PURCHASE_PROMPT: "구입금액을 입력해 주세요.( '원'과 ,는 생략해주세요) : ",
+  PURCHASE_ERROR_STRING: "로또 구입 금액은 1,000원 단위로 입력해주세요."
+}
 
-export { PURCHASE_PROMPT };
+export default PROMPTS;
