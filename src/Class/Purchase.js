6792415ae0261499d5198f60099a5e6a5db2cd79
thumbthing <ilscc0902@gmail.com>
2024-10-30 15:58:19 +0900

    Feat: Purchase 클래스 구현

--- src/Class/Purchase.js
+++ src/Class/Purchase.js
@@ -1,14 +1,27 @@
+import checkPurchase from "../feature/validate/checkPurchase";
+
 class Purchase {
   /** TODO: 
-   *  1. 입력 값의 유효성을 검사한다
-   *    1. 정수, 1000원으로 나누어 떨어지는 조건 이외는 전부 예외 처리
-   *    2. 예외 처리
-   *      1. 숫자가 아닐 경우 (NaN)
-   *      2. deimal
-   *      3. 음의 정수
-   *      4. 1000원으로 나누어 떨어지지 않을 경우
-   *  2. 유효성을 통과 한 값을 반환
+   *  field: #userInput, purchase
+   *  constructor:
+   *    private 필드에 유저의 입력값을 초기화
+   *    유효성이 확인된 인스턴스를 초기화
    */
+  #userInput;
+  purchase;
+
+  constructor(userInput) {
+    this.#userInput = userInput;
+    this.purchase = this.#validate(this.#userInput);
+  }
+
+  #validate(userInput) {
+    const PURCHASE = Number(userInput);
+    
+    checkPurchase(PURCHASE);
+
+    return PURCHASE;
+  }
 }
 
 export default Purchase;
