99273955e32790a8ea4a2c6c66cb010a852d263d
최영빈 <bin778@naver.com>
2024-10-30 16:26:31 +0900

    feat(LottoController): 에러 발생 시 해당 지점부터 재입력 추가

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,3 +1,4 @@
+import { Console } from "@woowacourse/mission-utils";
 import GameInput from "../view/GameInput.js";
 import Validate from "../validate/Validate.js";
 
@@ -8,16 +9,22 @@ class LottoController {
     this.#gameInput = new GameInput();
   }
 
-  async startGame() {
-    // 사용자에게 구입 금액을 입력받는다
-    const purchase_money = await this.#userInput();
-    console.log(purchase_money);
+  async #purchaseMoneyInput() {
+    while (true) {
+      try {
+        const purchase_money = await this.#gameInput.readPurchaseMoney();
+        Validate.validatePurchaseMoney(purchase_money);
+        return purchase_money;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 
-  async #userInput() {
-    const purchase_money = await this.#gameInput.readPurchaseMoney();
-    Validate.validatePurchaseMoney(purchase_money);
-    return purchase_money;
+  async startGame() {
+    // 사용자에게 구입 금액을 입력받는다
+    const purchase_money = await this.#purchaseMoneyInput();
+    console.log(purchase_money);
   }
 }
 
