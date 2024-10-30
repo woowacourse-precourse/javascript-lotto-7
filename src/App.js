d7271df897531bcde1759e289a8d30e9ff03a13c
hyomin <leehm123456@gmail.com>
2024-10-30 22:23:11 +0900

    feat: 유저 입력 기능 구현
    - 로또 구입 금액 입력
    - 당첨 번호 6개 입력
    - 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from '@woowacourse/mission-utils';
+import { LOTTO } from './constant.js';
+
 class App {
-  async run() {}
+  async run() {
+    const purchasePrice = await Console.readLineAsync(
+      '구입금액을 입력해주세요.\n'
+    );
+
+    const lottoCount = Math.floor(purchasePrice / LOTTO.PRICE);
+
+    Console.print(`${lottoCount}개를 구매했습니다.`);
+
+    let lottoNumbers = await Console.readLineAsync(
+      '당첨 번호를 입력해 주세요.\n'
+    );
+    lottoNumbers = lottoNumbers
+      .split(',')
+      .map((lottoNumber) => parseInt(lottoNumber));
+
+    const bonusNumber = await Console.readLineAsync(
+      '보너스 번호를 입력해 주세요.\n'
+    );
+  }
 }
 
 export default App;
