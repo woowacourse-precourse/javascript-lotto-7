e424d2bb021debc85c84f2035271944a6a68e501
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-30 16:49:28 +0900

    fix: 잘못된 출력 형태 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -22,11 +22,11 @@ class OutputView {
     Console.print('당첨 통계\n---');
 
     Console.print(
-      `3개 일치 (5,000원) ${lottoResult.fifth}개\n` +
-        `4개 일치 (50,000원) ${lottoResult.fourth}개\n` +
-        `5개 일치 (1,500,000원) ${lottoResult.third}개\n` +
-        `5개 일치, 보너스 볼 일치 (30,000,000원) ${lottoResult.second}개\n` +
-        `6개 일치 (2,000,000,000원) ${lottoResult.first}개\n`
+      `3개 일치 (5,000원) - ${lottoResult.fifth}개\n` +
+        `4개 일치 (50,000원) - ${lottoResult.fourth}개\n` +
+        `5개 일치 (1,500,000원) - ${lottoResult.third}개\n` +
+        `5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult.second}개\n` +
+        `6개 일치 (2,000,000,000원) - ${lottoResult.first}개\n`
     );
   }
 
