7ef65b83b4aa31a40f7162c017c6056938c94dd9
COMPOSEDKIM <composedkim@naver.com>
2024-10-30 23:30:20 +0900

    feat: 로또 구입 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { PROMPTS } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    const userMoney = await this.getUserMoney();
+  }
+
+  async getUserMoney() {
+    const userString = await MissionUtils.Console.readLineAsync(
+      PROMPTS.INPUT_USER_MONEY
+    );
+    return userString;
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const PROMPTS = Object.freeze({
+  INPUT_USER_MONEY: '구입금액을 입력해 주세요.\n',
+});
+
+export { PROMPTS };
