2a80eabb6acc5b51319035914f2133b5f944a98d
thumbthing <ilscc0902@gmail.com>
2024-10-30 23:51:55 +0900

    Feat: 당첨 번호 유효성 검사 기능 구현

--- src/feature/validate/checkWinNumber.js
+++ src/feature/validate/checkWinNumber.js
@@ -6,4 +6,15 @@ function checkWinNumber(numbers) {
    * 3. 유효성을 통과하지 못했을 경우 에러 메시지를 생성
    * 4. 에러를 반환
    */
+  numbers.forEach(number => {
+    if (Number.isNaN(number)) {
+      throw new Error('[ERROR] 당첨 번호는 숫자로 이루어져야 합니다.')
+    }
+    if (number <= 0 && number >= 46) {
+      throw new Error('[ERROR] 당첨 번호는 1 ~ 45 사이의 숫자여야 합니다.')
+    }
+    if (Math.floor(number) !== number && Math.ceil(number) !== number) {
+      throw new Error('[ERROR] 소수는 당첨 번호로 지정할 수 없습니다.')
+    }
+  });
 }
