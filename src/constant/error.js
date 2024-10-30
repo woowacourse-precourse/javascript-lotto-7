ba1eab2394fd81007caf00fb64dcebe171951709
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-30 21:18:10 +0900

    [feat]: 당첨 번호 입력 예외 처리 구현

--- src/constant/error.js
+++ src/constant/error.js
@@ -4,7 +4,12 @@ const ERROR_MESSAGES = Object.freeze({
   WRONG_RANGE: {
     PURCHASE_AMOUNT:
       '[ERROR] 구입 금액은 1,000원 이상 100,000원 이하여야 합니다.',
+    LOTTO_NUMBER: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   },
+  DUPLICATE_NUMBER: '[ERROR] 중복된 숫자가 있습니다.',
+  WRONG_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
+  EMPTY_STRING: '[ERROR] 입력할 값은 빈 문자열이 될 수 없습니다.',
+  WRONG_FORMAT: '[ERROR] 쉼표로 올바르게 구분되지 않았습니다.',
 });
 
 export default ERROR_MESSAGES;
--- src/util/validator.js
+++ src/util/validator.js
@@ -2,7 +2,12 @@ import ERROR_MESSAGES from '../constant/error.js';
 
 const MODE = Object.freeze({
   PURCHASE_AMOUNT: 'purchaseAmount',
+  LOTTO_NUMBER: 'lottoNumber',
 });
+
+const EMPTY_STRING = Object.freeze('');
+
+const COMMA = Object.freeze(',');
 
 const isNumber = (value) => {
   if (isNaN(value)) {
@@ -20,6 +25,10 @@ const isInRange = (value, mode) => {
   if (mode === MODE.PURCHASE_AMOUNT && (value < 1000 || value > 100000)) {
     throw new Error(ERROR_MESSAGES.WRONG_RANGE.PURCHASE_AMOUNT);
   }
+
+  if (mode === MODE.LOTTO_NUMBER && (value < 1 || value > 45)) {
+    throw new Error(ERROR_MESSAGES.WRONG_RANGE.LOTTO_NUMBER);
+  }
 };
 
 export const validatePurchaseAmount = (value) => {
@@ -28,4 +37,49 @@ export const validatePurchaseAmount = (value) => {
   isNumber(purchaseAmount);
   isWrongUnit(purchaseAmount);
   isInRange(purchaseAmount, MODE.PURCHASE_AMOUNT);
+};
+
+const isDuplicateInWinningNumbers = (array) => {
+  if (array.length !== new Set(array).size) {
+    throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER);
+  }
+};
+
+const isWrongLength = (array) => {
+  if (array.length !== 6) {
+    throw new Error(ERROR_MESSAGES.WRONG_LENGTH);
+  }
+};
+
+const isEmpty = (value) => {
+  if (value === EMPTY_STRING) {
+    throw new Error(ERROR_MESSAGES.EMPTY_STRING);
+  }
+};
+
+const isUsedComma = (value) => {
+  if (!value.includes(COMMA)) {
+    throw new Error(ERROR_MESSAGES.WRONG_FORMAT);
+  }
+
+  const commaRegex = /,/g;
+  const commaCount = (value.match(commaRegex) || []).length;
+
+  if (commaCount !== 5) {
+    throw new Error(ERROR_MESSAGES.WRONG_FORMAT);
+  }
+};
+
+export const validateWinningNumbers = (value) => {
+  isEmpty(value);
+  isUsedComma(value);
+
+  const winningNumbers = value.split(',').map(Number);
+  isWrongLength(winningNumbers);
+  isDuplicateInWinningNumbers(winningNumbers);
+
+  winningNumbers.forEach((number) => {
+    isNumber(number);
+    isInRange(number, MODE.LOTTO_NUMBER);
+  });
 };
