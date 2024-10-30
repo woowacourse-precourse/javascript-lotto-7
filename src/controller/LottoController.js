277deab419871293bda3a5028ac26a8fcf47169f
최영빈 <bin778@naver.com>
2024-10-30 15:21:31 +0900

    feat(LottoController): 구입금액 Input 및 예외 처리 추가

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,18 +1,23 @@
-import LottoInput from "../view/LottoInput.js";
+import GameInput from "../view/GameInput.js";
+import Validate from "../validate/Validate.js";
 
 class LottoController {
-  #lottoInput;
+  #gameInput;
 
   constructor() {
-    this.#lottoInput = new LottoInput();
+    this.#gameInput = new GameInput();
   }
 
   async startGame() {
-    await this.#userInput();
+    // 사용자에게 구입 금액을 입력받는다
+    const purchase_money = await this.#userInput();
+    console.log(purchase_money);
   }
 
   async #userInput() {
-    const purchase_money = await this.#lottoInput.readPurchaseMoney();
+    const purchase_money = await this.#gameInput.readPurchaseMoney();
+    Validate.validatePurchaseMoney(purchase_money);
+    return purchase_money;
   }
 }
 
