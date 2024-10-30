db0353feb1e95c8da2f959299b3a9a3a6d21fdb3
COMPOSEDKIM <composedkim@naver.com>
2024-10-31 01:22:05 +0900

    feat: 몇개 구매했는지 표시하고 번호들 띄우기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { PROMPTS } from './constants.js';
+import { PROMPTS, INFO } from './constants.js';
+import Lotto from './Lotto.js';
 
 class App {
   async run() {
     const userMoney = await this.getUserMoney();
+    const countLotto = userMoney / 1000;
+    const userLotto = [];
+    MissionUtils.Console.print(`\n${countLotto}${INFO.USER_BUY}`);
+    for (let i = 0; i < countLotto; i++) {
+      userLotto.push(
+        new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6))
+      );
+      MissionUtils.Console.print(userLotto[i].showNumber());
+    }
   }
 
   async getUserMoney() {
-    const userString = await MissionUtils.Console.readLineAsync(
+    const userMoney = await MissionUtils.Console.readLineAsync(
       PROMPTS.INPUT_USER_MONEY
     );
-    return userString;
+    return userMoney;
   }
 }
 
--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,8 @@ const PROMPTS = Object.freeze({
   INPUT_USER_MONEY: '구입금액을 입력해 주세요.\n',
 });
 
-export { PROMPTS };
+const INFO = Object.freeze({
+  USER_BUY: '개를 구매했습니다.',
+});
+
+export { PROMPTS, INFO };
