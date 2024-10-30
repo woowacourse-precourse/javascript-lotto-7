5b304c2ded122cb1972038daff49e87e2519ddae
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-30 21:06:41 +0900

    feat: N개의 사용자 로또를 생성한다

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import Lotto from './Lotto';
 
 class App {
+  constructor() {
+    this.lottos = [];
+  }
+
   async run() {
   }
 
   generateLotto() {
     const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
     return new Lotto(randomNumber);
+  }
+
+  generateLottos(quantity) {
+    const lottos = [];
+
+    for (let i = 0; i < quantity; i++) {
+      lottos.push(this.generateLotto());
+    }
+
+    return lottos;
   }
 }
 
