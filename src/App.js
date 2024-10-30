d509bfb00ecdd0be0185290486dfe657ecb90461
guesung <gueit214@naver.com>
2024-10-30 16:44:11 +0900

    fix: 출력 메시지에 맞게 줄바꿈 추가

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import Input from './Input.js';
 import { OUTPUT_MESSAGE, RANK_OBJECT_ARRAY } from './lib/constants.js';
 import Lotto from './Lotto.js';
+import { calculateRateOfReturn } from './lib/utils.js';
+import Output from './Output.js';
 
 class App {
   #input;
   #lottoArray;
+  #rankMap;
 
   constructor() {
     this.#input = new Input();
     this.#lottoArray = [];
+    this.#rankMap = App.#createRankMap();
   }
 
   async run() {
     await this.#input.getPurchasePrice();
+    const lottoCount = this.#input.purchasePrice / 1_000;
 
-    for (let round = 0; round < this.#input.lottoCount; round += 1) {
+    for (let round = 0; round < lottoCount; round += 1) {
       const randomNumberArray = MissionUtils.Random.pickUniqueNumbersInRange(
         1,
         45,
@@ -26,17 +31,20 @@ class App {
       this.#lottoArray.push(lotto);
     }
 
-    MissionUtils.Console.print(
-      `${this.#input.lottoCount}${OUTPUT_MESSAGE.PURCHASE_COUNT}`,
-    );
+    MissionUtils.Console.print('');
+
+    MissionUtils.Console.print(`${lottoCount}${OUTPUT_MESSAGE.PURCHASE_COUNT}`);
     this.#lottoArray.forEach((lotto) => lotto.printNumbers());
 
+    MissionUtils.Console.print('');
+
     await this.#input.getWinningNumbers();
+
+    MissionUtils.Console.print('');
+
     await this.#input.getBonusNumber();
 
-    const rankMap = new Map(
-      new Array(5).fill().map((_, index) => [index + 1, 0]),
-    );
+    MissionUtils.Console.print('');
 
     let totalWinningPrice = 0;
     this.#lottoArray.forEach((lotto) => {
@@ -46,27 +54,38 @@ class App {
       );
 
       if (rankObject) {
-        rankMap.set(rankMap.get(rankObject.rank) + 1);
+        this.#rankMap.set(this.#rankMap.get(rankObject.rank) + 1);
         totalWinningPrice += rankObject.winningPrice;
       }
     });
 
-    const rateOfReturn = (
-      (totalWinningPrice / this.#input.purchasePrice) *
-      100
-    ).toFixed(2);
+    const rateOfReturn = parseFloat(
+      calculateRateOfReturn(
+        totalWinningPrice,
+        this.#input.purchasePrice,
+      ).toFixed(2),
+    );
 
     MissionUtils.Console.print(OUTPUT_MESSAGE.WINNING_STATICS);
 
     RANK_OBJECT_ARRAY.forEach((rankObject) => {
+      let bonusNumberString = '';
+      if (rankObject.rank === 2) bonusNumberString = ', 보너스 볼 일치';
+
       MissionUtils.Console.print(
-        `${rankObject.winningCount}개 일치 (${rankObject.winningPrice}원) - ${
-          rankMap[rankObject.rank] ?? 0
-        }개`,
+        `${
+          rankObject.winningCount
+        }개 일치${bonusNumberString} (${rankObject.winningPrice.toLocaleString(
+          'ko-KR',
+        )}원) - ${this.#rankMap[rankObject.rank] ?? 0}개`,
       );
     });
 
     MissionUtils.Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
+  }
+
+  static #createRankMap() {
+    return new Map(new Array(5).fill().map((_, index) => [index + 1, 0]));
   }
 }
 
