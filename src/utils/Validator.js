a12412b469c84ef29403057cc4446013b4673be2
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-10-31 04:30:49 +0900

    feat :Add validate function validateBonusNumber

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -25,6 +25,22 @@ class Validator {
       );
     }
   }
+
+  static validateBonusNumber(bonusNumber, winningNumbers) {
+    const parsedBonusNumber = parseInt(bonusNumber, 10);
+
+    if (
+      typeof parsedBonusNumber !== 'number' ||
+      parsedBonusNumber < 1 ||
+      parsedBonusNumber > 45
+    ) {
+      throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+
+    if (winningNumbers.includes(parsedBonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+    }
+  }
 }
 
 export default Validator;
