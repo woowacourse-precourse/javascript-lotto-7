92110ffc00b9e28bd817f8726e9384740cbd9534
siggu <m0_w_0m@naver.com>
2024-10-30 16:37:00 +0900

    feat(App): 사용자로부터 보너스 번호를 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App {
     Console.print('당첨 번호를 입력해 주세요.');
     const pickLottoNumber = (await Console.readLineAsync('')).split(',');
     pickLottoNumber.map((number) => parseInt(number, 10));
+
+    Console.print('');
+    Console.print('보너스 번호를 입력해 주세요.');
+    const pickBonusNumber = parseInt(await Console.readLineAsync(''), 10);
   }
 }
 
