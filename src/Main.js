f371421b6e7111299d74a670575aba120732fb56
dev-dino22 <wlsskdud02@naver.com>
2024-10-31 01:17:11 +0900

    feat(Main.js): 총 수익률 계산 및 출력 기능 구현

--- src/Main.js
+++ src/Main.js
@@ -105,6 +105,18 @@ const calculatePurchaseNumber = (purchaseAmount) => {
   return purchaseAmount / 1000;
 };
 
+const calculateRateOfReturn = (purchaseAmount, winningLottoNumber) => {
+  let isWinningPrize = 0;
+  isWinningPrize += winningLottoNumber[0] * 5000;
+  isWinningPrize += winningLottoNumber[1] * 50000;
+  isWinningPrize += winningLottoNumber[2] * 1500000;
+  isWinningPrize += winningLottoNumber[3] * 30000000;
+  isWinningPrize += winningLottoNumber[4] * 2000000000;
+
+  const rateOfReturn = (isWinningPrize / purchaseAmount) * 100;
+  return rateOfReturn.toFixed(1);
+};
+
 const Main = async () => {
   Console.print('프로그램 시작!');
 
@@ -164,11 +176,17 @@ const Main = async () => {
   });
   Console.print(`3개 일치 (5,000원) - ${winningLottoNumber[0]}개`);
   Console.print(`4개 일치 (50,000원) - ${winningLottoNumber[1]}개`);
-  Console.print(`5개 일치 (1,500,000원원) - ${winningLottoNumber[2]}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${winningLottoNumber[2]}개`);
   Console.print(
     `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningLottoNumber[3]}개`,
   );
   Console.print(`6개 일치 (2,000,000,000원) - ${winningLottoNumber[4]}개`);
+
+  const rateOfReturn = calculateRateOfReturn(
+    purchaseAmount,
+    winningLottoNumber,
+  );
+  Console.print(`총 수익률은 ${rateOfReturn}%입니다. `);
 };
 
 export default Main;
