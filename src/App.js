9acf5186cdea1b3cce0c9ba363f450974561c927
thisisyejin <yejink321@gmail.com>
2024-10-30 19:08:46 +0900

    feat: 구입 금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 구입 금액 입력
+    const payment = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  }
 }
 
 export default App;
