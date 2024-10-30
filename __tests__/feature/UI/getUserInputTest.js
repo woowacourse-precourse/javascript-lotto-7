1635267f71c701f7016580940fd8aa253ef7842e
thumbthing <ilscc0902@gmail.com>
2024-10-30 21:29:17 +0900

    Test: 당첨 번호 입력 기능 테스트 추가

--- __tests__/feature/UI/getUserInputTest.js
+++ __tests__/feature/UI/getUserInputTest.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils"
-import { getPurchase } from "../../../src/feature/UI/getUserInput.js";
+import { getPurchase, getWinNumber } from "../../../src/feature/UI/getUserInput.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -11,7 +11,7 @@ const mockQuestions = (inputs) => {
   });
 };
 
-describe('입력 테스트', () => {
+describe('입력 테스트 : 구매액', () => {
   test.each([
     ['4000', '4000'],
     ['999000', '999000'],
@@ -29,4 +29,25 @@ describe('입력 테스트', () => {
       expect(purchase).toBe(result);
     }
   )
-})
+});
+
+describe('입력 테스트 : 당첨 번호', () => {
+  test.each([
+    ['1,2,3,4,5,6', '1,2,3,4,5,6'],
+    ['45,44,43,42,41,40', '45,44,43,42,41,40'],
+    ['22,24,26,41,43,45', '22,24,26,41,43,45'],
+  ])('입력 값에 대한 문자열 반환 { 입력 : %s, 반환 : %s', 
+    async (input, result) => {
+      // given
+      const userInput = [input];
+
+      mockQuestions(userInput);
+
+      // when
+      const winNumber = await getWinNumber();
+
+      // then
+      expect(winNumber).toBe(result);
+    }
+  );
+});
