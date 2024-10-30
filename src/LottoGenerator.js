120a625d4729d9ed293a60db458d636aa821c3d0
dyoon <dyoon@dyoonui-MacBookAir.local>
2024-10-30 17:38:55 +0900

    LottoGenerator 클래스 수정할 것
    가독성 높이고자 랜덤으로 뽑은 숫자를 변수에 저장 후 Console.print()에 인자 전달함.

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -1,17 +1,12 @@
-import { Console, Random } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils"
 
-class LottoGenerator {
+class LottoGenerator{
     generateLottos(lottoCount) {
-        this.printLottos(lottoCount);
-    }
-
-    printLottos(lottoCount) {
         for (let i = 0; i < lottoCount; i++) {
-            const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
-            Console.print(lottoNumbers);
+            const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a,b)=>a-b)
+            Console.print(lottoNumbers)
         }
     }
 }
-
 
 export default LottoGenerator;
