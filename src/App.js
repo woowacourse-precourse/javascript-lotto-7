fafa56038d1663499b6a013a7fabcde4548ca9b8
jjang-aaa <jadoit473@gmail.com>
2024-10-31 05:54:10 +0900

    feat(App, LottoMachine) : LottoMachine class 생성 후 App 파일에 코드 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { LottoMachine } from './lottoMachine/LottoMachine.js';
+
 class App {
-  async run() {}
+  async run() {
+    const lottoMachine = new LottoMachine();
+    await lottoMachine.run();
+  }
 }
 
 export default App;
