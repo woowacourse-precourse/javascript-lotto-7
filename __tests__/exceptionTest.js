69cd69e548d411e6b406c84f0d7d9cb2ff4f0b46
jicho <jIcho6412@gmail.com>
2024-10-30 22:03:11 +0900

    [예외 처리] 구입 금액이 1000 단위가 아닐 경우 다시 입력받는 함수로 돌아간다.(예외 처리와 테스트 코드 수정)

--- __tests__/exceptionTest.js
+++ __tests__/exceptionTest.js
@@ -1,10 +1,19 @@
 import Exception from "../src/exceptionHandling";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-describe("사용자에게 입력받은 값에 대한 테스트", () => {
-  test("구입 금액이 1000 단위가 아니면 예외가 발생한다.", () => {
-    expect(() => {
-      let checkPrice = new Exception();
-      checkPrice.validatePrice(1234);
-    }).toThrow("[ERROR]");
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("로또 게임에 대한 테스트", () => {
+  test("[입력 테스트]구입 금액이 1000 단위가 아니면 예외가 발생한다.", () => {
+    const logSpy = getLogSpy();
+
+    const exception = new Exception();
+    exception.validatePrice(1234);
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
   });
 });
--- src/exceptionHandling.js
+++ src/exceptionHandling.js
@@ -5,11 +5,12 @@ class Exception {
   constructor(price) {
     this.price = price;
   }
+
   validatePrice(price) {
     if (price % 1000 !== 0) {
-      throw new Error("[ERROR] 구입 금액을 1000 단위로 입력해주세요.");
-      // let replayInputPrice = new Input();
-      // replayInputPrice.inputPrice();
+      Console.print("[ERROR] 구입 금액을 1000 단위로 입력해주세요.");
+      let replayInputPrice = new Input();
+      replayInputPrice.inputPrice();
     }
   }
 }
