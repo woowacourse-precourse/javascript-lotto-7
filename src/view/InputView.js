2c769610cc46acc98e4cd1d9848c2d76d70f4ab2
hanheel <hanhee630630@gmail.com>
2024-10-30 16:11:20 +0900

    feat(InputView) : 로또 구입 금액이 음수인 경우 에러를 발생한다

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,14 +6,21 @@ class InputView {
       "구입 금액을 입력해 주세요.\n"
     );
     this.isPriceValidate(lottoPrice);
+    this.isPriceDivisible(lottoPrice);
     return lottoPrice;
   }
   isPriceValidate(lottoPrice) {
     const isNumber = !Number.isNaN(Number(lottoPrice, 10));
-    const isDivisible = lottoPrice % 1000 == 0;
+    const isPositive = lottoPrice > 0;
     if (!isNumber) {
-      throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
+      throw new Error("[ERROR] 로또 구입 금액은 숫자만 입력할 수 있습니다.");
     }
+    if (!isPositive) {
+      throw new Error("[ERROR] 로또 구입 금액은 양수로 입력해 주세요.");
+    }
+  }
+  isPriceDivisible(lottoPrice) {
+    const isDivisible = lottoPrice % 1000 == 0;
     if (!isDivisible) {
       throw new Error(
         "[ERROR] 로또 구입 금액은 1000원 단위로만 입력 가능합니다."
