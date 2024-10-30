b3f1160ba3848ee808d04e0be5c69cbdd7e82ea3
df2854 <df2854@icloud.com>
2024-10-30 15:55:50 +0900

    feat: LottoGame 클래스(로또 리스트 생성, 로또 수량 반환) 구현

---
+++ src/LottoGame.js
@@ -0,0 +1,21 @@
+import Lotto from "./Lotto.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class LottoGame {
+    constructor(amount) {
+        this.quantity = amount / 1000;
+        this.lottoList = [];
+    }
+
+    createLottoList() {
+        for (let i = 0; i < this.quantity; i++) {
+            const lottoNumber = new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+            this.lottoList.push(lottoNumber);
+        }
+        return this.lottoList;
+    }
+
+    LottoQuantity() {
+        return this.quantity;
+    }
+}
