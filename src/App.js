6803448071d4f9036873a0923588af5970f979fb
yoo_min <vendetta12@naver.com>
2024-10-31 02:58:09 +0900

    feat: 로또 구매 금액 입력기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-
+    try {
+      const lottoBuyMoney = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
+      Console.print(lottoBuyMoney);
+    }
+    catch {
+      throw error;
+    }
   }
 }
 
