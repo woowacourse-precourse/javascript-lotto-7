d239f4221858f52b79afe522dd65ed980c7e341b
aydenote <dslgpgh@gmail.com>
2024-10-30 22:25:24 +0900

    fix(constant):  로또 구매 개수 안내 문구 수정

--- src/constant.js
+++ src/constant.js
@@ -4,5 +4,5 @@ export const LOTTO_MAX_NUMBER = 45;
 export const LOTTO_LENGTH = 6;
 export const PROMPT = {
 	LOTTO_BUY: "구입금액을 입력해 주세요.\n",
-	LOTTO_COUNT: (count) => `${count}개를 구매했습니다.`,
+	LOTTO_COUNT: (count) => `${count}개를 구매했습니다.\n`,
 };
