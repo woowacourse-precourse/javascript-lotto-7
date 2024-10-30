050fd0f63c54e4159f91febf6f89f2cad28940e6
Yeongi Park <yeongi0111@naver.com>
2024-10-30 22:44:25 +0900

    feat(Validaor): Validator 클래스 구현
    유효성 검사를 위한 클래스인 Validator 구현

---
+++ src/Validator.js
@@ -0,0 +1,35 @@
+import {
+  checkDuplicateBonusNumber,
+  checkDuplicateWinningNumbers,
+  checkIntegerBonusNumber,
+  checkIntegerWinningNumbers,
+  checkLengthWinningNumbers,
+  checkMaxPrice,
+  checkMinPrice,
+  checkNumberPrice,
+  checkRangeBonusNumber,
+  checkRangeWinningNumbers,
+  checkUnitPrice,
+} from './util/validationUtil.js';
+
+export default class Validator {
+  static price(price) {
+    checkNumberPrice(price);
+    checkMinPrice(price);
+    checkMaxPrice(price);
+    checkUnitPrice(price);
+  }
+
+  static winningNumbers(winningNumbers) {
+    checkLengthWinningNumbers(winningNumbers);
+    checkDuplicateWinningNumbers(winningNumbers);
+    checkIntegerWinningNumbers(winningNumbers);
+    checkRangeWinningNumbers(winningNumbers);
+  }
+
+  static bonusNumber(winningNumbers, bonusNumber) {
+    checkIntegerBonusNumber(bonusNumber);
+    checkRangeBonusNumber(bonusNumber);
+    checkDuplicateBonusNumber(winningNumbers, bonusNumber);
+  }
+}
