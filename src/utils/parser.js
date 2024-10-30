9ce9f353128e8b5eed40a36a06cd0698d6c215c1
sooyeoniya <tndus1306403@naver.com>
2024-10-30 23:58:06 +0900

    feat(parser): 문자열 숫자 추출 함수 구현

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,20 +1,25 @@
 
 const parseStringToNumber = (price) => {
   return Number(price);
-}
+};
 
 const parseMoneyToLottoCount = (price) => {
   return price / 1_000;
-}
+};
 
 const parseSortArray = (array) => {
   return array.toSorted((a, b) => a - b);
-}
+};
+
+const parseExtractNumbers = (string) => {
+  return string.split(',').map((number) => Number(number.trim()));
+};
 
 const parser = {
   parseStringToNumber,
   parseMoneyToLottoCount,
   parseSortArray,
-}
+  parseExtractNumbers,
+};
 
 export default parser;
