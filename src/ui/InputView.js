e5170369541fb1ca97b183c96d118c0bd7325720
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-30 22:29:04 +0900

    feat: [예외처리] 구입금액을 입력받는 도중에 예외가 발생하면 에러 메시지를 반환한다

--- src/ui/InputView.js
+++ src/ui/InputView.js
@@ -1,12 +1,14 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 const USER_MONEY_PROMPT = '구입금액을 입력해 주세요.';
+const USER_MONEY_INPUT_ERROR_MESSAGE = '[ERROR] 구입금액을 받는 도중에 에러가 발생했습니다.';
 
 export async function getUserMoney() {
   try {
     const userMoney = await MissionUtils.Console.readLineAsync(USER_MONEY_PROMPT);
     return userMoney;
   } catch (error) {
+    throw new Error(USER_MONEY_INPUT_ERROR_MESSAGE);
   }
 }
 
