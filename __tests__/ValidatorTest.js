55bf1d911bf90a369f36f1fdb2a1d4bc040e1b5e
ryanbae94 <ryanbae94@gmail.com>
2024-10-30 20:31:36 +0900

    test (ValidatorTest): 구입 금액 입력 검증 테스트코드
    - isValidWinningNumber(str) 에 대한 테스트 코드 작성

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -40,4 +40,56 @@ describe('유효성 검증 테스트', () => {
 			);
 		});
 	});
+	describe('당첨 번호 입력 검증기 테스트', () => {
+		test('정상 입력 테스트', () => {
+			expect(validator.isValidWinningNumber('1,2,3,4,5,6')).toBe(true);
+			expect(validator.isValidWinningNumber('7,8,9,10,11,12')).toBe(true);
+			expect(validator.isValidWinningNumber('40,41,42,43,44,45')).toBe(true);
+		});
+
+		test('isEmpty 테스트', () => {
+			expect(() => validator.isValidWinningNumber('')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.EMPTY
+			);
+		});
+
+		test('hasWhiteSpace 테스트', () => {
+			expect(() => validator.isValidWinningNumber('1, 2,3,4,5,6')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.HAS_WHITE_SPACE
+			);
+		});
+
+		test('숫자와 쉼표 외의 문자 포함 테스트', () => {
+			expect(() => validator.isValidWinningNumber('1,2,3,a,5,6')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.INVALID_WINNING_FORMAT
+			);
+			expect(() => validator.isValidWinningNumber('1,2,3,4,5,six')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.INVALID_WINNING_FORMAT
+			);
+		});
+
+		test('로또 번호 개수 불일치 테스트', () => {
+			expect(() => validator.isValidWinningNumber('1,2,3,4,5')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.INVALID_LOTTO_COUNT
+			);
+			expect(() => validator.isValidWinningNumber('1,2,3,4,5,6,7')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.INVALID_LOTTO_COUNT
+			);
+		});
+
+		test('중복 숫자 테스트', () => {
+			expect(() => validator.isValidWinningNumber('1,2,3,4,5,5')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.IS_DUPLICATED
+			);
+		});
+
+		test('숫자 범위 초과 테스트', () => {
+			expect(() => validator.isValidWinningNumber('0,1,2,3,4,5')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.IS_OVER_NUMBER_RANGE
+			);
+			expect(() => validator.isValidWinningNumber('1,2,3,4,5,46')).toThrow(
+				ERROR_PREFIX + ' ' + ERROR_MESSAGE.IS_OVER_NUMBER_RANGE
+			);
+		});
+	});
 });
