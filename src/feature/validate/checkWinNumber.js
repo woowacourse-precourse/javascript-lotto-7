839334bf42ecf2da43ee5e0244a3d8800133f256
thumbthing <ilscc0902@gmail.com>
2024-10-30 23:58:20 +0900

    Feat: 단일 번호 유효성 검사 기능으로 분리

--- src/feature/validate/checkWinNumber.js
+++ src/feature/validate/checkWinNumber.js
@@ -1,20 +1,19 @@
-function checkWinNumber(numbers) {
-  /** TODO : 
-   * 1. 매개변수로 숫자로 변환된 배열을 받는다
-   * 2. 유효셩을 체크한다
-   *    - NaN, decimal, 1 ~ 45 사이의 값인지
-   * 3. 유효성을 통과하지 못했을 경우 에러 메시지를 생성
-   * 4. 에러를 반환
-   */
-  numbers.forEach(number => {
-    if (Number.isNaN(number)) {
-      throw new Error('[ERROR] 당첨 번호는 숫자로 이루어져야 합니다.')
-    }
-    if (number <= 0 && number >= 46) {
-      throw new Error('[ERROR] 당첨 번호는 1 ~ 45 사이의 숫자여야 합니다.')
-    }
-    if (Math.floor(number) !== number && Math.ceil(number) !== number) {
-      throw new Error('[ERROR] 소수는 당첨 번호로 지정할 수 없습니다.')
-    }
-  });
+function checkNumber(number) {
+  if (Number.isNaN(number)) {
+    throw new Error('[ERROR] 입력 번호는 숫자여야 합니다.')
+  }
+
+  if (number <= 0 && number >= 46) {
+    throw new Error('[ERROR] 번호는 1 ~ 45 사이의 숫자여야 합니다.')
+  }
+
+  if (Math.floor(number) !== number && Math.ceil(number) !== number) {
+    throw new Error('[ERROR] 번호는 양의 정수여야 합니다.')
+  }
+};
+
+function checkWinNumbers(numbers) {
+  numbers.forEach(number => checkNumber(number));
 }
+
+export {checkNumber, checkWinNumbers};
