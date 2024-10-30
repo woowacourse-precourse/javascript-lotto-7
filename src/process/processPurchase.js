94080d3d14de8a5dab0ca2c69bbdf7327aa510d6
thumbthing <ilscc0902@gmail.com>
2024-10-30 16:24:46 +0900

    Feat: 구입 금액 통합 기능의  구현 목록 작성

---
+++ src/process/processPurchase.js
@@ -0,0 +1,9 @@
+async function processPurchase() {
+  /** TODO: 
+   *  새분화된 함수, class 인스턴스 화를 관리
+   *  1. 유저의 입력값을 받는 함수 호출
+   *  2. 입력값을 활용하여 class를 인스턴스화
+   *  3. 에러가 밸생 했을때의 처리 실행
+   *    - 에러 객체에 callbackFn으로 processPurchase를 담아서 반환
+   */
+}
