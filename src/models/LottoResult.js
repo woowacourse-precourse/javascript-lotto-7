8b28e615f4e607bb571840e661779c6e2c669b8e
subsub-e <ab13696802@gmail.com>
2024-10-30 16:12:00 +0900

    feat(LottoResult) : 로또 결과 모델 LottoResult 클래스 구현

---
+++ src/models/LottoResult.js
@@ -0,0 +1,59 @@
+import LOTTO_CONSTANTS from '../constants/lottoConstants.js';
+
+class LottoResult {
+  #firstPlace;
+  #secondPlace;
+  #thirdPlace;
+  #fourthPlace;
+  #fifthPlace;
+
+  constructor() {
+    this.#firstPlace = 0;
+    this.#secondPlace = 0;
+    this.#thirdPlace = 0;
+    this.#fourthPlace = 0;
+    this.#fifthPlace = 0;
+  }
+
+  winFirstPlace() {
+    this.#firstPlace += 1;
+  }
+
+  winSecondPlace() {
+    this.#secondPlace += 1;
+  }
+
+  winThirdPlace() {
+    this.#thirdPlace += 1;
+  }
+
+  winFourthPlace() {
+    this.#fourthPlace += 1;
+  }
+
+  winFifthPlace() {
+    this.#fifthPlace += 1;
+  }
+
+  getFirstPlaceMoney() {
+    return this.#firstPlace * LOTTO_CONSTANTS.FIRST_PLACE_MONEY;
+  }
+
+  getSecondPlaceMoney() {
+    return this.#secondPlace * LOTTO_CONSTANTS.SECOND_PLACE_MONEY;
+  }
+
+  getThirdPlaceMoney() {
+    return this.#thirdPlace * LOTTO_CONSTANTS.THIRD_PLACE_MONEY;
+  }
+
+  getFourthPlaceMoney() {
+    return this.#fourthPlace * LOTTO_CONSTANTS.FOURTH_PLACE_MONEY;
+  }
+
+  getFifthPlaceMoney() {
+    return this.#fifthPlace * LOTTO_CONSTANTS.FIFTH_PLACE_MONEY;
+  }
+}
+
+export default LottoResult;
