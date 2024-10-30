07b76094846d5edc23cbae3238047123db8f96cd
siggu <m0_w_0m@naver.com>
2024-10-30 16:35:31 +0900

    feat(App): 로또 수량 만큼 로또 번호를 발행하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
     Console.print('구입금액을 입력해 주세요.');
-    const lottoCount = await Console.readLineAsync('');
+    const buyLottoCount = await Console.readLineAsync('');
+    Console.print('');
+
+    const randomLottoNumbers = [];
+
+    for (let index = 0; index < buyLottoCount / 1000; index++) {
+      const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+      randomLottoNumbers.push(lottoNumber.sort((a, b) => a - b));
+    }
+
+    Console.print(`${buyLottoCount / 1000}개를 구매했습니다.`);
+    randomLottoNumbers.forEach((lottoNumber) => Console.print(lottoNumber));
   }
 }
 
