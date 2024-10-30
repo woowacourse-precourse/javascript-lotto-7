ee17d77a4c43d90893519a6e6010fba261943ca4
sooyeoniya <tndus1306403@naver.com>
2024-10-31 00:15:34 +0900

    rename(validations): 로또 구입 금액 유효성 검증 함수 매개변수명 변경

--- src/validations/LottoPurchasePriceValidations.js
+++ src/validations/LottoPurchasePriceValidations.js
@@ -1,32 +1,32 @@
-const validateNumber = (lottoPurchasePrice) => {
-  if (isNaN(lottoPurchasePrice)) {
+const validateNumber = (price) => {
+  if (isNaN(price)) {
     throw new Error('[ERROR] 로또 구입 금액이 숫자가 아닙니다. 다시 입력해주세요.');
   }
 };
 
-const validateInteger = (lottoPurchasePrice) => {
-  if (!Number.isInteger(lottoPurchasePrice)) {
+const validateInteger = (price) => {
+  if (!Number.isInteger(price)) {
     throw new Error('[ERROR] 로또 구입 금액이 정수가 아닙니다. 다시 입력해주세요.');
   }
 };
 
-const validateLottoAmountRange = (lottoPurchasePrice) => {
-  if (lottoPurchasePrice < 1_000 || lottoPurchasePrice > 2_000_000_000) {
+const validateLottoAmountRange = (price) => {
+  if (price < 1_000 || price > 2_000_000_000) {
     throw new Error('[ERROR] 로또 구입 금액은 1,000원 이상 2,000,000,000원 이하로 입력해 주세요.');
   }
 };
 
-const validateIsThousandUnit = (lottoPurchasePrice) => {
-  if (lottoPurchasePrice % 1_000 !== 0) {
+const validateIsThousandUnit = (price) => {
+  if (price % 1_000 !== 0) {
     throw new Error('[ERROR] 로또 구입 금액은 1,000원 단위로 입력해 주세요.');
   }
 };
 
-const LottoPurchasePriceValidations = (lottoPurchasePrice) => {
-  validateNumber(lottoPurchasePrice);
-  validateInteger(lottoPurchasePrice);
-  validateLottoAmountRange(lottoPurchasePrice);
-  validateIsThousandUnit(lottoPurchasePrice);
+const LottoPurchasePriceValidations = (price) => {
+  validateNumber(price);
+  validateInteger(price);
+  validateLottoAmountRange(price);
+  validateIsThousandUnit(price);
 };
 
 export default LottoPurchasePriceValidations;
