3fb05a5f6c858b41f1c21dac678d60986b8c19da
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-10-30 17:27:14 +0900

    test: Lotto 클래스의 메서드 테스트 추가
    - getMatchedCount 메서드의 동작을 검증하는 테스트 케이스 추가
    - hasBonusNumber 메서드의 동작을 검증하는 테스트 케이스 추가
    - 보너스 번호가 포함되지 않은 경우에 대한 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,21 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("getMatchedCount 메서드는 맞춘 번호의 개수를 반환한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 2, 3, 7, 8, 9];
+    expect(lotto.getMatchedCount(winningNumbers)).toBe(3);
+  });
+
+  test("hasBonusNumber 메서드는 보너스 번호가 포함되어 있는지 여부를 반환한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = 6;
+    expect(lotto.hasBonusNumber(bonusNumber)).toBe(true);
+  });
 });
