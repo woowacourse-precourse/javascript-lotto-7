329a142a25b39b8023e06a9a99e745c491afc63b
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-31 00:53:07 +0900

    feat(gameMessages.js): 게임메세지 상수화

---
+++ src/constants/gameMessages.js
@@ -0,0 +1,9 @@
+export const INPUT_MESSAGES = Object.freeze({
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+  BOUNUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+});
+
+export const OUTPUT_MESSAGES = Object.freeze({
+  WINNER_STATIC: '당첨 통계\n---\n',
+});
