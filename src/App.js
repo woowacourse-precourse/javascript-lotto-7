d48fb21fe0d6b8c074005ca9427b8a7105a5f95b
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-10-30 15:14:59 +0900

    feat: 로또 발행 기능 구현
    구매 금액을 입력 및 구매 수 만큼의 로또 번호 발행 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const count = await this.getLottoCount();
+    const lottoNumbers = this.generateLottoNumbers(count);
+    this.printLottoNumbers(lottoNumbers);
+  }
+
+  async getLottoCount() {
+    const purchaseCost = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    if (isNaN(Number(purchaseCost))) {
+      throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
+    }
+    const count = Math.floor(purchaseCost / 1000);
+    return count;
+  }
+
+  generateLottoNumbers(count) {
+    const lottoNumbers = [];
+    for (let i = 0; i < count; i++) {
+      const randomLottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoNumbers.push(randomLottoNumber.sort((a, b) => a - b));
+    }
+    return lottoNumbers;
+  }
+
+  printLottoNumbers(lottoNumbers) {
+    Console.print(`${lottoNumbers.length}개를 구매했습니다.`);
+    lottoNumbers.forEach(numbers => Console.print(`[${numbers.join(', ')}]`));
+  }
 }
 
 export default App;
