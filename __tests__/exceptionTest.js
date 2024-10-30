21a3660dba85580a451e916104fac2ee54679137
jicho <jIcho6412@gmail.com>
2024-10-31 00:22:43 +0900

    이전 커밋에서 발생한 오류 수정 - 예외 처리를 완료한 후 다시 구입 금액을 입력받는 함수를 호출해서 발생한 테스트 파일 오류 수정

--- __tests__/exceptionTest.js
+++ __tests__/exceptionTest.js
@@ -1,29 +1,23 @@
 import Exception from "../src/exceptionHandling";
-import { MissionUtils } from "@woowacourse/mission-utils";
 
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
-};
-
-const validateError = (price) => {
-  const logSpy = getLogSpy();
-  const exception = new Exception();
-  exception.validatePrice(price);
-  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
-};
+const exception = new Exception();
 
 describe("로또 게임에 대한 테스트", () => {
   test("[입력 테스트]구입 금액이 1000 단위가 아니면 예외가 발생한다.", () => {
-    validateError(1234);
+    expect(() => {
+      exception.validatePrice(1234, false);
+    }).toThrow("[ERROR]");
   });
 
   test("[입력 테스트]구입 금액을 공백으로 작성하면 예외가 발생한다.", () => {
-    validateError("");
+    expect(() => {
+      exception.validatePrice("", false);
+    }).toThrow("[ERROR]");
   });
 
   test("[입력 테스트]구입 금액을 음수로 작성하면 예외가 발생한다.", () => {
-    validateError(-1000);
+    expect(() => {
+      exception.validatePrice(-1000, false);
+    }).toThrow("[ERROR]");
   });
 });
--- src/exceptionHandling.js
+++ src/exceptionHandling.js
@@ -6,11 +6,17 @@ class Exception {
     this.price = price;
   }
 
-  validatePrice(price) {
+  validatePrice(price, retry = true) {
     if (price % 1000 !== 0 || price === "" || price < 1) {
       Console.print("[ERROR] 구입 금액을 1000 단위로 입력해주세요.");
-      let replayInputPrice = new Input();
-      replayInputPrice.inputPrice();
+
+      // retry가 true일 때만 inputPrice() 호출
+      if (retry) {
+        let replayInputPrice = new Input();
+        replayInputPrice.inputPrice();
+      } else {
+        throw new Error("[ERROR] 구입 금액을 1000 단위로 입력해주세요.");
+      }
     }
   }
 }
