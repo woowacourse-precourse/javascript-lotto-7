6aacc45d6aa6f0f3ed7d9831f7058e9a3f939695
Kim <kevin2695@naver.com>
2024-10-30 20:35:18 +0900

    feat(app) : 로또 번호를 입력받고 에러 메시지를 표출하는 기능 추가
    - 로또 번호 입력 기능
    - 로또 번호가 6개가 아닌 경우 , 에러 발생
    - 로또 번호에 문자가 입력된 경우 , 에러 발생
    - 로또 번호에 1 - 45 범위 밖의 숫자가 입력된 경우 , 에러 발생

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App {
     try {
       // purchase input
       const purchaseAmount = await MissionUtils.Console.readLineAsync(
-        "구입금액을 입력해 주세요."
+        "구입금액을 입력해 주세요.\n"
       );
 
       // purchase validate
@@ -17,6 +17,36 @@ class App {
       // 2. 1000 단위로 나누어 떨어지지 않는 경우
       if (Number(purchaseAmount) % 1000 !== 0) {
         throw new Error("1000원 단위로 나누어 떨어지지 않습니다.");
+      }
+
+      // lotto number input
+      const lottoNumber = await MissionUtils.Console.readLineAsync(
+        "당첨 번호를 입력해 주세요.\n"
+      );
+
+      const lottoArray = new Set(lottoNumber.split(",").map(Number));
+      // lotto validate
+      // 1. 번호가 6개가 되지 않는 경우
+      if (lottoArray.size != 6) {
+        throw new Error("로또 번호가 6개가 아닙니다.");
+      }
+
+      // 2. 로또 번호에 문자가 있는 경우
+      const hasNaN = [...lottoArray].some(
+        (lottoNumber) => !Number.isInteger(lottoNumber)
+      );
+
+      if (hasNaN) {
+        throw new Error("숫자가 아닌 문자가 입력되었습니다.");
+      }
+
+      // 3. 로또 번호가 1 ~ 45 사이가 아닌 경우
+      const hasOutOfRange = [...lottoArray].some(
+        (lottoNumber) => !(lottoNumber >= 1 && lottoNumber <= 45)
+      );
+
+      if (hasOutOfRange) {
+        throw new Error("1 - 45 범위 밖의 숫자가 입력되었습니다.");
       }
     } catch (error) {
       console.log(`[ERROR] ${error.message}`);
