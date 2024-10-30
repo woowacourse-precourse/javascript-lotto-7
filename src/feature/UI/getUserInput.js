b9bae5ce611419b84d198edf19c6d88bc4980932
thumbthing <ilscc0902@gmail.com>
2024-10-30 21:20:20 +0900

    Feat: 당첨 번호 입력 기능 구현, export 방식 변경

--- src/feature/UI/getUserInput.js
+++ src/feature/UI/getUserInput.js
@@ -15,6 +15,14 @@ async function getWinNumber() {
    * 1. 쉼표(,)로 구분된 6개의 숫자를 입력 받는다.
    * 2. 입력 받은 문자열을 반환 한다. 
    */
+
+  try {
+    const USER_INPUT = await Console.readLineAsync('\n당첨 번호를 입력해 주세요\n');
+    return USER_INPUT;
+  } catch (error) {
+    const ERROR_MESSAGE = '[ERROR] 당첨 번호 입력 중에 예상치 못한 에러가 발생 했습니다.'
+    throw new Error(ERROR_MESSAGE);
+  }
 }
 
-export default getPurchase;
+export { getPurchase, getWinNumber };
