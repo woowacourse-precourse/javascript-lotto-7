476eafcdc3d1fff88ff5466ebd78a90a59edc51b
RohSungKyun <songkyun1108@naver.com>
2024-10-30 16:40:23 +0900

    fix : 유효성 검사 선언 문제 해결

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,19 @@
+import InputValidate from "./utils/InputValidate.js";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.error = new InputValidate();
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.split(',').map(Number);
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.error.lottoNumberValidate(numbers);
   }
-
   // TODO: 추가 기능 구현
+
 }
 
 export default Lotto;
--- src/utils/InputValidate.js
+++ src/utils/InputValidate.js
@@ -1,73 +1,69 @@
 class InputValidate{
-  async inputExist(input) {
+  inputExist(input) {
     if (!input){
       throw new Error("[ERROR] 값을 입력하세요.");
     }
   }
-  async inputType(input) {
+  inputType(input) {
     if (isNaN(Number(input))){
       throw new Error("[ERROR] 숫자를 입력해 주세요.");
     }
   }
-  async inputUnit(input) {
+  inputUnit(input) {
     if (input%1000 !== 0){
       throw new Error("[ERROR] 1000원 단위의 금액으로 입력해세요.");
     }
   }
-  async lottoNumberRange(input) {
-    const lottoNumbers = input.split(',');
-    for (let number of lottoNumbers){
+  lottoNumberRange(input) {
+    for (let number of input){
       if(number > 45 || number < 1){
         throw new Error("[ERROR] 당첨번호는 1~45 사이의 숫자로 입력하세요.");
       }
     }
   }
-  async DuplicateLottoNumber(input) {
-    const lottoNumbers = input.split(',');
-    const noDuplicate = new Set(lottoNumbers);
-    if(lottoNumbers !== noDuplicate){
+  DuplicateLottoNumber(input) {
+    const noDuplicate = new Set(input);
+    if(input.length !== noDuplicate.size){
         throw new Error("[ERROR] 당첨번호는 중복없이 입력해야 합니다.");
     }
   }
-  async lottoNumberLength(input) {
-    const lottoNumbers = input.split(',');
-    if (lottoNumbers.length !== 6){
+  lottoNumberLength(input) {
+    if (input.length !== 6){
         throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  async lottoNumberType(input) {
-    const lottoNumbers = input.split(',');
-    for (let number of lottoNumbers){
+  lottoNumberType(input) {
+    for (let number of input){
       if (isNaN(Number(number))){
         throw new Error("[ERROR] 숫자를 쉼표(,) 구분자로 나누어 입력해야 합니다.");
       }
     }
   }
 
-  async bonusNumberType(input) {
+  bonusNumberType(input) {
     if (isNaN(Number(input))){
       throw new Error("[ERROR] 보너스 번호는 하나의 숫자로 입력하세요.");
     }
   }
-  async bonusNumberRange(input) {
+  bonusNumberRange(input) {
     if (input > 45 || input < 1){
       throw new Error("[ERROR] 보너스 번호는 1~45사이의 숫자로 입력하세요.");
     }
   }
-  async DuplicateBonusNumber(bonusNumber, lottoNumber) {
+  duplicateBonusNumber(bonusNumber, lottoNumber) {
     if (lottoNumber.includes(bonusNumber)) {
       throw new Error("[ERROR] 보너스 번호는 당첨번호와 중복되면 안됩니다."); 
     }
   }
 
-  async priceInputValidate(input) {
+  priceInputValidate(input) {
     this.inputExist(input);
     this.inputType(input);
     this.inputUnit(input);
   }
 
-  async lottoNumberValidate(input) {
+  lottoNumberValidate(input) {
     this.inputExist(input);
     this.lottoNumberRange(input);
     this.lottoNumberLength(input);
@@ -75,9 +71,9 @@ class InputValidate{
     this.lottoNumberType(input);
   }
 
-  async bonusNumberValidate(bonusNumber, lottoNumber) {
+  bonusNumberValidate(bonusNumber, lottoNumber) {
     this.inputExist(bonusNumber);
-    this.DuplicateBonusNumber(bonusNumber, lottoNumber);
+    this.duplicateBonusNumber(bonusNumber, lottoNumber);
     this.bonusNumberType(bonusNumber);
     this.bonusNumberRange(bonusNumber);
   }
