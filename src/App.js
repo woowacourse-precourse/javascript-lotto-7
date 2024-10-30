8d4da87e1df99c6d286de4e71121dcf280733fa7
thisisyejin <yejink321@gmail.com>
2024-10-30 19:16:09 +0900

    feat: 당첨 내역 및 수익률 출력 구현
    무작위로 발행된 로또 번호와 당첨 번호를 비교하여
    당첨 내역과 수익률을 계산하고, 이를 출력하는 기능을
    구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -38,6 +38,50 @@ class App {
     // 보너스 번호 입력
     const bonusString = await MissionUtils.Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n')
     const bonusNumber = Number(bonusString)
+
+    // 당첨 내역 집계
+    const score = {
+      THREE_MATCHES: 0,
+      FOUR_MATCHES: 0,
+      FIVE_MATCHES: 0,
+      FIVE_BONUS_MATCHES: 0,
+      SIX_MATCHES: 0,
+    }
+    const countScore = (score, len, bonus) => {
+      if (len === 6) return score.SIX_MATCHES += 1;
+      if (len === 5 && bonus) return score.FIVE_BONUS_MATCHES += 1;
+      if (len === 5) return score.FIVE_MATCHES += 1;
+      if (len === 4) return score.FOUR_MATCHES += 1;
+      if (len === 3) return score.THREE_MATCHES += 1;
+    }
+
+    for (let i = 0; i < quantity; i++) {
+      const result = myLottoList[i].filter(list => winningNumber.includes(list))
+      const isBonus = myLottoList[i].some((element) => {
+        return element == bonusNumber
+      });
+      countScore(score, result.length, isBonus);
+    }
+
+    // 수익률 계산
+    const profit = Math.round(((5000 * score.THREE_MATCHES) + (50000 * score.FOUR_MATCHES)
+      + (1500000 * score.FIVE_MATCHES) + (30000000 * score.FIVE_BONUS_MATCHES)
+      + (2000000000 * score.SIX_MATCHES)) / payment * 100 * 10) / 10;
+
+    // 당첨 내역 및 수익률 출력
+    const printScore = (score) => {
+      const str = `
+당첨 통계
+---
+3개 일치 (5,000원) - ${score.THREE_MATCHES}개
+4개 일치 (50,000원) - ${score.FOUR_MATCHES}개
+5개 일치 (1,500,000원) - ${score.FIVE_MATCHES}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${score.FIVE_BONUS_MATCHES}개
+6개 일치 (2,000,000,000원) - ${score.SIX_MATCHES}개
+총 수익률은 ${profit}%입니다.`
+      MissionUtils.Console.print(str)
+    }
+    printScore(score)
   }
 }
 
