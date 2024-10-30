cab65382f6ccfd9541bc445781aba2bf75738b92
jicho <jIcho6412@gmail.com>
2024-10-30 22:50:39 +0900

    [테스트] 구입 금액에 대한 테스트 기능 추가 - 공백, 음수로 작성할 경우

--- __tests__/exceptionTest.js
+++ __tests__/exceptionTest.js
@@ -7,13 +7,23 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const validateError = (price) => {
+  const logSpy = getLogSpy();
+  const exception = new Exception();
+  exception.validatePrice(price);
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
+};
+
 describe("로또 게임에 대한 테스트", () => {
   test("[입력 테스트]구입 금액이 1000 단위가 아니면 예외가 발생한다.", () => {
-    const logSpy = getLogSpy();
+    validateError(1234);
+  });
 
-    const exception = new Exception();
-    exception.validatePrice(1234);
+  test("[입력 테스트]구입 금액을 공백으로 작성하면 예외가 발생한다.", () => {
+    validateError("");
+  });
 
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
+  test("[입력 테스트]구입 금액을 공백으로 작성하면 예외가 발생한다.", () => {
+    validateError(-1000);
   });
 });
