2b8b5459fb744520c2a0ed2cbe4e4a4560cc630f
Yurin Wang <king_1994@naver.com>
2024-10-31 00:36:37 +0900

    feat: 로또 구입 금액 범위 검증 로직 추가 - 로또는 1,000원 단위로 구매할 수 있다. - 로또는 최소 한 장(1,000원)부터 구매할 수 있다. - 구매 한도는 100,000원이다.

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,7 @@
 class Validator {
   static validatePurchaseAmount(input) {
     const purchaseAmount = this.#convertToNumber(input);
+    this.#checkAmountRange(purchaseAmount);
   }
 
   static #convertToNumber(input) {
@@ -11,6 +12,20 @@ class Validator {
     }
 
     return number;
+  }
+
+  static #checkAmountRange(amount) {
+    if (amount < 1000) {
+      throw new Error('[ERROR] 구입 금액은 1,000원 이상이어야 합니다.');
+    }
+
+    if (amount > 100000) {
+      throw new Error('[ERROR] 구입 금액은 100,000원을 넘을 수 없습니다.');
+    }
+
+    if (amount % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1,000원 단위여야 합니다.');
+    }
   }
 }
 
