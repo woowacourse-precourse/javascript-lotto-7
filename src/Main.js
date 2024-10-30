595f6f7bbefaa8a975a70c62a3d14f0a46552d62
dev-dino22 <wlsskdud02@naver.com>
2024-10-30 21:45:20 +0900

    feat(Main.js): 로또 번호 생성 및 저장 후 출력 기능 구현

--- src/Main.js
+++ src/Main.js
@@ -11,6 +11,10 @@ const MESSAGES = {
     NOT_NUMBER: '[ERROR] 숫자를 입력해주세요.',
     NOT_NATURAL_NUMBER: '[ERROR] 자연수를 입력해주세요.',
     NOT_DIVISIBLE_BY_THOUSAND: '[ERROR] 1000의 배수를 입력해주세요.',
+    NOT_LOTTO_RANGE:
+      '[ERROR] 로또 당첨 번호는 1부터 45까지의 자연수를 입력해주세요.',
+    NOT_WINNING_LENGTH: '[ERROR] 당첨 번호는 6개를 입력해야합니다.',
+    IS_SAME_NUMBER: '[ERROR] 당첨 번호 입력에 중복된 숫자가 있습니다.',
   },
 };
 
@@ -35,6 +39,22 @@ const InputValidator = {
       throw new Error(MESSAGES.ERROR.NOT_DIVISIBLE_BY_THOUSAND);
     }
   },
+  isLottoRangeNumber: (input) => {
+    if (input < 1 || input > 45) {
+      throw new Error(MESSAGES.ERROR.NOT_LOTTO_RANGE);
+    }
+  },
+  isWinningLength: (inputArray) => {
+    if (inputArray.length !== 6) {
+      throw new Error(MESSAGES.ERROR.NOT_WINNING_LENGTH);
+    }
+  },
+  isSameNumber: (inputArray) => {
+    const distinctNumber = new Set(inputArray);
+    if (distinctNumber.size !== inputArray.length) {
+      throw new Error(MESSAGES.ERROR.IS_SAME_NUMBER);
+    }
+  },
 };
 
 const Parser = {
@@ -50,8 +70,26 @@ class LottoValidator {
     const purchaseAmount = Parser.parseNumber(input);
     InputValidator.isNaturalNumber(purchaseAmount);
     InputValidator.isDivisibleByThousand(purchaseAmount);
-
     return purchaseAmount;
+  }
+  static validateWinningNumbers(inputArray) {
+    const winningNumber = inputArray.map((input) => {
+      const number = Parser.parseNumber(input);
+      InputValidator.isLottoRangeNumber(number);
+      InputValidator.isNaturalNumber(number);
+      InputValidator.isLottoRangeNumber(number);
+      return number;
+    });
+    InputValidator.isWinningLength(winningNumber);
+    InputValidator.isSameNumber(winningNumber);
+    return winningNumber;
+  }
+  static validateBonusNumber(input) {
+    const bonusNumber = Parser.parseNumber(input);
+    InputValidator.isLottoRangeNumber(bonusNumber);
+    InputValidator.isNaturalNumber(bonusNumber);
+    InputValidator.isLottoRangeNumber(bonusNumber);
+    return bonusNumber;
   }
 }
 
@@ -68,13 +106,24 @@ const Main = async () => {
   const purchaseAmount =
     LottoValidator.validatePurchaseAmount(inputPurchaseAmount);
 
-  const winningNumbers = await Console.readLineAsync(
+  const inputWinningNumbers = await Console.readLineAsync(
     MESSAGES.INFO.WINNING_NUMBERS,
   );
-  const bonusNumber = await Console.readLineAsync(MESSAGES.INFO.BONUS_NUMBER);
+
+  const arrayWinningNumbers = inputWinningNumbers.trim().split(',');
+  const winningNumbers =
+    LottoValidator.validateWinningNumbers(arrayWinningNumbers);
+
+  const inputBonusNumber = await Console.readLineAsync(
+    MESSAGES.INFO.BONUS_NUMBER,
+  );
+
+  const bonusNumber = LottoValidator.validateBonusNumber(inputBonusNumber);
+  winningNumbers.push(bonusNumber);
 
   const purchaseNumber = calculatePurchaseNumber(purchaseAmount);
   Console.print(`${purchaseNumber}개를 구매했습니다.`);
+  Console.print(`당첨번호: ${winningNumbers}`);
 };
 
 export default Main;
