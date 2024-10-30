7e43a439c6a00c30d2cb28e8e1540ab36c271c7b
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-30 15:34:12 +0900

    feat: 1에서 45까지 범위에서 중복되지 않는 5개의 숫자를 추출한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+// ### 1개의 사용자 로또 발행하기
+
+// - [x] 1에서 45까지 범위에서 중복되지 않는 5개의 숫자를 추출한다.
+// - [ ] 1개의 로또를 생성한다.
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
 }
 
 export default App;
