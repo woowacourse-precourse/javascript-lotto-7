27fa3a4517bcb12a4e6f2ad665ca69dc8131af4a
guesung <gueit214@naver.com>
2024-10-30 12:07:36 +0900

    feat: 로또를 뽑아 개수와 번호를 출력

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Input from './Input.js';
+import { OUTPUT_MESSAGE } from './lib/constants.js';
 
 class App {
   async run() {
     const input = new Input();
     await input.getPurchasePrice();
 
+    const randomNumberArray = [];
     for (let round = 0; round < input.lottoCount; round += 1) {
       const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
         1,
         45,
         6,
       );
+      randomNumber.sort((a, b) => a - b);
+      randomNumberArray.push(randomNumber);
     }
+
+    MissionUtils.Console.print(
+      `${input.lottoCount}${OUTPUT_MESSAGE.PURCHASE_COUNT}`,
+    );
+    randomNumberArray.forEach((randomNumber) =>
+      MissionUtils.Console.print(randomNumber),
+    );
   }
 }
 
