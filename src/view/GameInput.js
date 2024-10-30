f5eb27ca9e3bd4a393907c6619329ac4ade99c17
최영빈 <bin778@naver.com>
2024-10-30 15:20:05 +0900

    refactor: 네이밍 변경

--- src/view/GameInput.js
+++ src/view/GameInput.js
@@ -1,10 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "../constant/Message.js";
 
-class LottoInput {
+class GameInput {
   async readPurchaseMoney() {
     return Console.readLineAsync(MESSAGE.INPUT.PURCHASE_MONEY);
   }
 }
 
-export default LottoInput;
+export default GameInput;
