f86d3d0d8baaa624b22621cf3bf6aa020fafd12b
Jinny <ggab.challenge@gmail.com>
2024-10-30 14:40:54 +0900

    feat: 로또 구입금액을 빈값으로 입력한 경우 에러 처리
    - README.md 파일 내에 기능 요구사항 체크리스트에 내용 추가

--- src/Models/purchasePriceValidator.js
+++ src/Models/purchasePriceValidator.js
@@ -1,14 +1,20 @@
 import { ERROR_MESSAGE } from './errorMessages.js';
 
-const checkPurchasePriceUnit = (purchasePriceInput) => {
-  const LOTTO_PRICE_UNIT = 1000;
-  const NumberOfPurchasePriceInput = Number(purchasePriceInput);
+const isPurchasePriceEmptyInput = (purchasePriceInput) => purchasePriceInput !== '';
 
-  return NumberOfPurchasePriceInput % LOTTO_PRICE_UNIT === 0;
+const isValidPurchasePriceUnit = (purchasePriceInput) => {
+  const LOTTO_PRICE_UNIT = 1000;
+  const purchasePrice = Number(purchasePriceInput);
+
+  return purchasePrice % LOTTO_PRICE_UNIT === 0;
 };
 
 const validatePurchasePrice = (purchasePriceInput) => {
-  if (!checkPurchasePriceUnit(purchasePriceInput)) {
+  if (!isPurchasePriceEmptyInput(purchasePriceInput)) {
+    throw new Error(ERROR_MESSAGE.EMPTY_INPUT);
+  }
+
+  if (!isValidPurchasePriceUnit(purchasePriceInput)) {
     throw new Error(ERROR_MESSAGE.INCORRECT_UNIT_PRICE);
   }
 };
