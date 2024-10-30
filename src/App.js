d36770b4d797ef62bbd060d07076442c958c514d
Yubeenpark <yoobinpark@gmail.com>
2024-10-30 17:49:56 +0900

    feat(payment): add input validation for purchase amount

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const { Console } = MissionUtils;
+const { Random } = MissionUtils;
+
+function isPaymentValid(input) {
+  if (isNaN(input)) {
+    Console.print("[ERROR] 금액은 숫자여야 합니다.");
+    return false;
+  } else if (input % 1000 != 0) {
+    Console.print("[ERROR] 금액을 1000원 단위로 입력해주세요.");
+    return false;
+  }
+  return true;
+}
+
 class App {
   async run() {}
 }
