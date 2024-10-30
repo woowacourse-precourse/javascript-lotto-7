a035d7fb38e415daaf6351d086b1adcbefe60ad3
Yurin Wang <king_1994@naver.com>
2024-10-31 02:42:10 +0900

    feat: 당첨 번호 입력 기능 추가 - 사용자에게 로또 당첨 번호를 입력받는다.

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,9 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
-export default class InputView {
+class InputView {
   static async getPurchaseAmount() {
     const input = await Console.readLineAsync('구입 금액을 입력해주세요.\n');
 
     return input;
   }
+
+  static async getWinningNumbers() {
+    const input = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+
+    return input;
+  }
 }
+
+export default InputView;
