7215c3f24bebab2c40a62fb39d89495ed2f4e66d
thumbthing <ilscc0902@gmail.com>
2024-10-30 20:04:32 +0900

    Feat: 로또 구매 관련 통합 기능 구현

--- src/process/processLottoList.js
+++ src/process/processLottoList.js
@@ -1,4 +1,7 @@
-function processLottoList() {
+import LottoList from "../Class/LottoList.js";
+import purchaseOutput from "../feature/UI/purchaseOutput.js";
+
+function processLottoList(userPurchase) {
   /** TODO : 
    * 1. 유효한 구매액을 매개변수로 받는다
    * 2. 구매액으로 LottoList를 인스턴스화
@@ -6,4 +9,15 @@ function processLottoList() {
    * 4. 구매내역과 구매 갯수로 사용자게에 내역을 출력
    * 5. 구매내역, 구매 갯수를 반환환
    */
-}
+
+  const LOTTOS = new LottoList(userPurchase);
+
+  const LOTTO_LIST = LOTTOS.lottoList;
+  const PURCHASE_COUNT = LOTTOS.purchase;
+
+  purchaseOutput(LOTTO_LIST, PURCHASE_COUNT);
+
+  return { LOTTO_LIST, PURCHASE_COUNT };
+};
+
+export default processLottoList;
