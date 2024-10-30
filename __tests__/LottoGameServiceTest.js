b59023c39d3595cad8a63b40a807836136782cc5
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-31 00:51:16 +0900

    refactor: 로또 비즈니스 로직 클래스의 테스트를 별도의 테스트 파일로 분리한다

---
+++ __tests__/LottoGameServiceTest.js
@@ -0,0 +1,26 @@
+import LottoGameService from '../src/service/LottoGameService';
+
+describe('로또 비즈니스 로직 클래스 테스트', () => {
+  let lottoGameService;
+  beforeEach(() => {
+    lottoGameService = new LottoGameService([1, 2, 3, 4, 5, 6]);
+  });
+
+  describe('1개의 사용자 로또 발행하기', () => {
+    test('1개의 로또를 생성한다.', () => {
+      expect(lottoGameService.generateLotto()).toBeInstanceOf(Lotto);
+    });
+  });
+
+  describe('N개의 사용자 로또 발행하기', () => {
+    test('N개의 로또는 각각 독립적이다.', () => {
+      expect(lottoGameService.generateLottos(10)).toHaveLength(10);
+    });
+  });
+
+  describe('구입 금액에 따른 로또 개수 계산하기', () => {
+    test('구입 금액에 해당하는 만큼 로또를 발행한다.', () => {
+      expect((lottoGameService.createLottoQuantity(18000))).toBe(18);
+    });
+  });
+});
--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,4 @@
 import Lotto from '../src/Lotto.js';
-import App from '../src/App.js';
 
 describe('로또 클래스 테스트', () => {
   // test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -17,13 +16,6 @@ describe('로또 클래스 테스트', () => {
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 
-  describe('1개의 사용자 로또 발행하기', () => {
-    const app = new App();
-    test('1개의 로또를 생성한다.', () => {
-      expect(app.generateLotto()).toBeInstanceOf(Lotto);
-    });
-  });
-
   describe('당첨 로또와 사용자 로또 비교하기', () => {
     let lotto;
     beforeEach(() => {
@@ -38,20 +30,6 @@ describe('로또 클래스 테스트', () => {
     });
     test('당첨 로또와 사용자 로또의 일치하는 숫자가 5개있으며 보너스 번호가 일치한다.', () => {
       expect(lotto.compareWinningLotto([1, 3, 5, 2, 9, 4], 6)).toEqual([5, true]);
-    });
-  });
-
-  describe('N개의 사용자 로또 발행하기', () => {
-    const app = new App();
-    test('N개의 로또는 각각 독립적이다.', () => {
-      expect(app.generateLottos(10)).toHaveLength(10);
-    });
-  });
-
-  describe('구입 금액에 따른 로또 개수 계산하기', () => {
-    const app = new App();
-    test('구입 금액에 해당하는 만큼 로또를 발행한다.', () => {
-      expect((app.createLottoQuantity(18000))).toBe(18);
     });
   });
 });
