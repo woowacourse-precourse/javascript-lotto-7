e87b98ac94856c0e71d00488ffc4b90b93be261c
ryanbae94 <ryanbae94@gmail.com>
2024-10-30 20:08:53 +0900

    feat (messages): 에러 메세지 추가
    - 당첨 번호 입력 시 에러메세지 추가
    - 에러메세지에 사용되는 상수값에 대한 템플릿 리터럴 처리

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,13 @@
+import {
+	LOTTO_COUNT,
+	MAX_LOTTO_NUMBER,
+	MIN_LOTTO_NUMBER,
+	LOTTO_PRICE,
+} from './numbers.js';
+
 export const MESSAGE = Object.freeze({
 	COST_MESSAGE: '구입금액을 입력해 주세요.',
+	WINNING_NUMBER_MESSAGE: '당첨 번호를 입력해 주세요.',
 });
 
 export const ERROR_PREFIX = '[ERROR]';
@@ -9,5 +17,9 @@ export const ERROR_MESSAGE = Object.freeze({
 	EMPTY: '아무것도 입력되지 않았습니다.',
 	HAS_WHITE_SPACE: '공백이 포함될 수 없습니다.',
 	IS_NEGATIVE: '음수가 입력될 수 없습니다.',
-	NOT_DIVIDED_BY_THOUSAND: '금액은 1000원 단위로 입력해주세요.',
+	NOT_DIVIDED_BY_THOUSAND: `금액은 ${LOTTO_PRICE}원 단위로 입력해주세요.`,
+	INVALID_WINNING_FORMAT: '숫자와 쉼표만 입력할 수 있습니다.',
+	INVALID_LOTTO_COUNT: `당첨 숫자의 갯수는 ${LOTTO_COUNT}개여야 합니다.`,
+	IS_DUPLICATED: '중복된 숫자는 입력될 수 없습니다.',
+	IS_OVER_NUMBER_RANGE: `숫자의 범위는 ${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER} 사이의 값만 가능합니다.`,
 });
