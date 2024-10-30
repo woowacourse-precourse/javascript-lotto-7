1585453d5d3494e0ae0c04bb1b3caede93801e24
siggu <m0_w_0m@naver.com>
2024-10-30 14:23:13 +0900

    feat(App): 로또 금액 입력 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    Console.print('구입금액을 입력해 주세요.');
+    const lottoCount = await Console.readLineAsync('');
+  }
 }
 
 export default App;
