9097c794770a5e1c0db22e6c3adc49cfec80b30f
RohSungKyun <songkyun1108@naver.com>
2024-10-31 00:34:16 +0900

    test : 사용자 금액 입력 테스트 추가

---
+++ __tests__/ValidateTest.js
@@ -0,0 +1,21 @@
+import InputValidate from "../src/utils/InputValidate"
+
+describe("유효성 검사 테스트", () => {
+  let validator = new InputValidate();
+
+  test("금액을 입력하지 않았을 경우", () => {
+    expect(() => validator.priceInputValidate('')).toThrow("[ERROR]");
+  });
+
+  test("금액이 숫자가 아닌 경우", () => {
+    expect(() => validator.priceInputValidate("a")).toThrow("[ERROR]");
+  });
+
+  test("금액이 1000단위로 나누어 떨어지는 경우가 아닐때", () => {
+    expect(() => validator.priceInputValidate("1100").toThrow("[ERROR]"));
+  });
+
+  test("금액이 음수인 경우", () => {
+    expect(() => validator.priceInputValidate("-1000")).toThrow("[ERROR]");
+  });
+});
