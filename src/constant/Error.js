bb3fe16b53b673c8d7afd4baaf5c276e56c3eb6a
최영빈 <bin778@naver.com>
2024-10-30 15:41:29 +0900

    refactor: 에러 메시지 분류

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -1,5 +1,7 @@
 export const ERROR_MESSAGE = Object.freeze({
-  ERROR_NON_NUMBER: "[ERROR] 숫자만 입력이 가능합니다.",
-  ERROR_SMALL_NUMBER: "[ERROR] 1000보다 작은 값은 입력할 수 없습니다.",
-  ERROR_DIVIDE_THOUSAND: "[ERROR] 입력한 값이 1000으로 나누어 떨어져야 합니다.",
+  PURCHASE_MONEY: Object.freeze({
+    ERROR_NON_NUMBER: "[ERROR] 숫자만 입력이 가능합니다.",
+    ERROR_SMALL_NUMBER: "[ERROR] 1000보다 작은 값은 입력할 수 없습니다.",
+    ERROR_DIVIDE_THOUSAND: "[ERROR] 1000으로 나누어 떨어져야 합니다.",
+  }),
 });
--- src/validate/Validate.js
+++ src/validate/Validate.js
@@ -3,12 +3,13 @@ import { ERROR_MESSAGE } from "../constant/Error.js";
 class Validate {
   static validatePurchaseMoney(money) {
     if (!/^[0-9\s]*$/.test(money) || money === "")
-      throw new Error(ERROR_MESSAGE.ERROR_NON_NUMBER);
+      throw new Error(ERROR_MESSAGE.PURCHASE_MONEY.ERROR_NON_NUMBER);
 
-    if (money < 1000) throw new Error(ERROR_MESSAGE.ERROR_SMALL_NUMBER);
+    if (money < 1000)
+      throw new Error(ERROR_MESSAGE.PURCHASE_MONEY.ERROR_SMALL_NUMBER);
 
     if (money % 1000 !== 0)
-      throw new Error(ERROR_MESSAGE.ERROR_DIVIDE_THOUSAND);
+      throw new Error(ERROR_MESSAGE.PURCHASE_MONEY.ERROR_DIVIDE_THOUSAND);
   }
 }
 
