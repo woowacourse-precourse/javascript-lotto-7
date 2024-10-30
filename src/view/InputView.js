c8f2ec47e1d1ca713be4cb978fa46d7583b49299
hanheel <hanhee630630@gmail.com>
2024-10-30 15:03:40 +0900

    feat(InputView) : 로또 가격을 입력 받고 유효성을 검사한다

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -8,8 +8,12 @@ class InputView {
     this.isPriceValidate(lottoPrice);
   }
   isPriceValidate(lottoPrice) {
-    const condition = lottoPrice % 1000 == 0;
-    if (!condition) {
+    const isNumber = !Number.isNaN(parseInt(lottoPrice, 10));
+    const isDivisible = lottoPrice % 1000 == 0;
+    if (!isNumber) {
+      throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
+    }
+    if (!isDivisible) {
       throw new Error(
         "[ERROR] 로또 구입 금액은 1000원 단위로만 입력 가능합니다."
       );
