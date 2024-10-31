2d0cdbac3dad59978e67c78f97c1c771777debed
Yun-Jinwoo <yjw3480@naver.com>
2024-10-31 11:41:52 +0900

    ✨ feat(App.js): 사용자로부터 값 입력받기 - 구입 금액, 당첨 번호, 보너스 번호 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, Random } from "@woowacourse/mission-utils"
 class App {
-  async run() {}
+  async run() {
+    const inputPrice = await Console.readLineAsync("구입금액을 입력해 주세요.");
+
+    const inputNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
+    const winningNumbers = inputNumbers.split(",").filter(item => item !== "");
+
+    const bonusNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
+
+  }
 }
 
 export default App;
