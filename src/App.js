df9b8feb75601f608176b84b6f26a10e42f8b58e
murphybread <nargene@gmail.com>
2024-10-31 18:59:14 +0900

    test(app): show lotto result

--- src/App.js
+++ src/App.js
@@ -11,16 +11,22 @@ class App {
   async run() {
 
     const paidMoney = await this.userInput.getUserPaidMoney();
-    const winngingNumber = await this.userInput.getWinningNumber();
-    const bonusNumber = await this.userInput.getBonusNumber();
-    
     const lottoTickets= this.game.purchaseLotto(paidMoney);
 
     this.displayOutput.displayPaidLotto(lottoTickets);
+
+    const winngingNumber = await this.userInput.getWinningNumber();
+    const splitWinningNumbers = this.game.splitWinningNumbers(winngingNumber);
+
+    const bonusNumber = await this.userInput.getBonusNumber();
     
     const lotto = this.game.generateLotto(lottoTickets);
 
     this.displayOutput.displayLotto(lotto);
+
+    const lottoReult = this.game.checkLottoResult(lotto, splitWinningNumbers, bonusNumber)
+
+    this.displayOutput.displayLottoResult(lottoReult);
 
 
   }
