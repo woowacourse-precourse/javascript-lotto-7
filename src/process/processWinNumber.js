893bd09322097af373d61163cf6c8a838623d667
thumbthing <ilscc0902@gmail.com>
2024-10-31 23:38:06 +0900

    Feat: 당첨 번호 관련 통합 기능 구현

---
+++ src/process/processWinNumber.js
@@ -0,0 +1,28 @@
+import { Console } from "@woowacourse/mission-utils";
+import { getWinNumber } from "../feature/UI/getUserInput.js";
+import { parseToArray, parseToNumberArray } from "../feature/parse/parseArray.js";
+import Lotto from "../Lotto.js";
+import checkDecimal from "../feature/validate/checkDecimal.js";
+import { checkEmptyArguments } from "../feature/validate/checkEmptyInput.js";
+
+// TODO : 함수 줄 길이를 줄여야 함
+
+async function processWinNumber() {
+  try {
+    const USER_INPUT = await getWinNumber();
+    checkDecimal(USER_INPUT);
+
+    const PARSED_TO_ARRAY = parseToArray(USER_INPUT);
+    checkEmptyArguments(PARSED_TO_ARRAY);
+
+    const WIN_NUMBER = parseToNumberArray(PARSED_TO_ARRAY);
+    const LOTTO = new Lotto(WIN_NUMBER).getWinNumber;
+    
+    return LOTTO;
+  } catch (error) {
+    Console.print(error.message);
+    processWinNumber();
+  }
+}
+
+export default processWinNumber;
