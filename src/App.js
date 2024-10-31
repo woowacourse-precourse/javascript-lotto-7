8ce4dd315916f5e03f836f0edbd75f79b65c3cb4
murphybread <nargene@gmail.com>
2024-10-31 17:17:39 +0900

    test: purchase lotto and return ticket

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import UserInput from './Input.js';
-
+import Game from './Game.js';
+import DisplayOutput from './DisplayOutput.js';
 class App {
 
   constructor(){
     this.userInput = new UserInput();
+    this.displayOutput = new DisplayOutput();
+    this.game = new Game();
   }
   async run() {
 
     const paidMoney = await this.userInput.getUserPaidMoney();
     const winngingNumber = await this.userInput.getWinningNumber();
     const bonusNumber = await this.userInput.getBonusNumber();
+
+    this.displayOutput.displayPaidLotto();
+    this.game.purchaseLotto(paidMoney);
+
   }
 }
 
