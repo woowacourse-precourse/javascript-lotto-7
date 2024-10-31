b1966dae1432c51a27e5ba358d8f3d4b586cd3bb
magik99 <hojun0810@naver.com>
2024-11-01 00:23:07 +0900

    feat(Lotto.js): 등수 계산 기능 추가
    로또 번호와 당첨 번호를 비교하여 등수를 return 해주는 checkPrize와 calculatePrize 메서드를 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,45 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  checkPrize(prizeNumbers, bonusNumber) {
+    let matchedBonusNumber = 0;
+
+    const matchingNumbers = prizeNumbers.filter((element) =>
+      this.#numbers.includes(element),
+    );
+
+    if (this.#numbers.includes(bonusNumber)) {
+      matchedBonusNumber = 1;
+    }
+
+    return this.calculatePrize(matchingNumbers, matchedBonusNumber);
+  }
+
+  calculatePrize(matchedNumbers, matchedBonusNumber) {
+    if (matchedNumbers < 3) {
+      return 0;
+    }
+    if (matchedNumbers === 3) {
+      return 5;
+    }
+    if (matchedNumbers === 4) {
+      return 4;
+    }
+    if (matchedNumbers === 5 && matchedBonusNumber === 0) {
+      return 3;
+    }
+    if (matchedNumbers === 5 && matchedBonusNumber === 1) {
+      return 2;
+    }
+    if (matchedNumbers === 6) {
+      return 1;
+    }
+  }
 }
 
 export default Lotto;
