01fea259186318a76605c7e681769868cddc3260
jicho <jIcho6412@gmail.com>
2024-10-31 15:08:36 +0900

    구입 금액만큼 발행할 로또의 갯수를 처리하고 출력하기.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
 import Input from "./inputInfo.js";
 
 class App {
   async run() {
     let price = new Input();
-    price.inputPrice();
+    let purchase = await price.inputPrice();
+    Console.print("");
+    price.NumberOfPurchase(purchase);
   }
 }
 
--- src/inputInfo.js
+++ src/inputInfo.js
@@ -10,6 +10,13 @@ class Input {
     let price = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
     let checkPrice = new Exception();
     checkPrice.validatePrice(price);
+    return price;
+  }
+
+  NumberOfPurchase(price) {
+    let theNumberOfLotto = price / 1000;
+    Console.print(theNumberOfLotto + "개를 구매했습니다.");
+    return theNumberOfLotto;
   }
 }
 export default Input;
