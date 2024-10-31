f7fc8811eab9e7b5bbad57f67db13365760f56c4
magik99 <hojun0810@naver.com>
2024-11-01 01:14:29 +0900

    fix(Input.js): 라이브러리 메서드 사용법 수정
    readLineAsync 앞에 Console. 추가

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,23 @@
-import { readLineAsync } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
 import Validator from './validator';
 
 class Input {
   static async inputAmount() {
     try {
-      const amount = await readLineAsync('구입금액을 입력해 주세요.');
+      const amount = await Console.readLineAsync('구입금액을 입력해 주세요.');
       Validator.checkAmount(amount);
       return amount;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async inputLottoNumber() {
+    try {
+      const lottoNumber = await Console.readLineAsync(
+        '당첨 번호를 입력해 주세요.',
+      );
+      return lottoNumber;
     } catch (error) {
       throw new Error(error.message);
     }
