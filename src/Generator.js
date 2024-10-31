507eafeb73c855280f5db14e8515a478037aa980
magik99 <hojun0810@naver.com>
2024-11-01 00:52:06 +0900

    feat(Generator.js): 랜덤 숫자 생성 메서드 구현
    1~45까지의 6개의 숫자를 생성하는 createLottoNumbers 메서드 구현

--- src/Generator.js
+++ src/Generator.js
@@ -1,8 +1,14 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class Generator {
   createLotto(amount) {
     try {
       const times = this.calculateTimes(amount);
       const lottos = [];
+
+      for (let i = 0; i < times; i++) {
+        const randomNumbers = this.createLottoNumbers();
+      }
     } catch (error) {
       throw new Error(error.message);
     }
@@ -10,6 +16,10 @@ class Generator {
 
   calculateTimes(amount) {
     return amount / 1000;
+  }
+
+  createLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 }
 
