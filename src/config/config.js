eee0ef9abd318754d62047a77cb9f1fc0ecf2ae4
Jaeho Cha <ischar@naver.com>
2024-10-31 15:40:32 +0900

    chore(config): seperate messages and errors, and constant
    - 메시지와 에러 분리, 상수 추가
      - LOTTO_COUNT 추가

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,13 @@
 export const MESSAGES = {
-  ERROR_INPUT_EMPTY: "[ERROR] 입력값이 없습니다.",
+  INPUT_BUY_COST: "구입금액을 입력해 주세요.",
+  BUY_COUNT: "개를 구매했습니다.",
+  INPUT_WIN_NUM: "당첨 번호를 입력해 주세요.",
 };
+
+export const ERROR = {
+  INPUT_EMPTY: "[ERROR] 입력값이 없습니다.",
+  INVALID_NUMBER: "[ERROR] 잘못된 값입니다.",
+  NOT_NUMBER: "[ERROR] 숫자가 아닙니다.",
+};
+
+export const LOTTO_AMOUNT = 1000;
