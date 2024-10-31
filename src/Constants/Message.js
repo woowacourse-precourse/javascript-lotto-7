e4bc57f5fe7cbbbb7ed420b48e215fef3ea84142
ha-kuku <hakukudh@gmail.com>
2024-10-31 11:31:20 +0900

    feat: 출력메시지 상수화

--- src/Constants/Message.js
+++ src/Constants/Message.js
@@ -3,3 +3,14 @@ const INPUT_MESSAGE = {
   winningNumber: '당첨 번호를 입력해 주세요.',
   bonusNumber: '보너스 번호를 입력해 주세요.',
 };
+
+const OUTPUT_MESSAGE = {
+  purchaseQuantity: '개를 구매했습니다.',
+  winningStatistics: '당첨 통계',
+  divisionLine: '---',
+  fifthWinner: '3개 일치 (5,000원) - ',
+  fourthWinner: '4개 일치 (50,000원) - ',
+  thirdWinner: '5개 일치 (1,500,000원) - ',
+  secondWinner: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  firstWinner: '6개 일치 (2,000,000,000원) - ',
+};
