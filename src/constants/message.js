581aea961e5ca8de9e5a4b8f95bd13933148164d
sanghyeonLee <hyeon012366@gmail.com>
2024-11-01 03:32:34 +0900

    feat: 입력 메세지, 출력 메세지 상수 파일에 저장

---
+++ src/constants/message.js
@@ -0,0 +1,10 @@
+export const INPUT = {
+    PURCHASE: "구입금액을 입력해 주세요.",
+    WINNING_NUMBER: "당첨 번호를 입력해 주세요.",
+    BONUS_NUMBER: "보너스 번호를 입력해 주세요."
+}
+export const OUTPUT = {
+    PURCHASE: "개를 구매했습니다.",
+    WINNING_STATISTICS: "",
+    TOTAL_RETURN: "총 수익률은"
+}
