b1806b71273b258e02784053f4c9ff9335d64f0a
ha-kuku <hakukudh@gmail.com>
2024-10-31 19:00:52 +0900

    refactor: 메서드명 변경

--- src/Domain/LottoMachine.js
+++ src/Domain/LottoMachine.js
@@ -1,27 +1,31 @@
-import { INPUT_MESSAGE, OUTPUT_MESSAGE } from '../Constants/Message.js';
+import { OUTPUT_MESSAGE, BASIC_ERROR } from '../Constants/Message.js';
 import { MissionUtils, Console } from '@woowacourse/mission-utils';
 
 class LOTTO_MACHINE {
-  async inputMoney(input) {
-    await Console.readLineAsync(INPUT_MESSAGE.purchaseMoney);
-    return input;
-  }
-
   inputMoneyDevide(number) {
-    if (input % 1000 === 0) {
-      input / 1000;
-      return number;
+    if (number % 1000 === 0) {
+      return number / 1000;
     }
+    throw new Error(BASIC_ERROR.invalidMoney);
   }
 
-  purchaseQuntatityPrint(quantity) {
-    Console.print(`${this.number}${OUTPUT_MESSAGE.purchaseQuantity}`);
+  purchaseQuantityPrint(number) {
+    const quantity = this.inputMoneyDevide(number);
+    Console.print(`${quantity}${OUTPUT_MESSAGE.purchaseQuantity}`);
     return quantity;
   }
 
   drawLottoNumber() {
-    for (var i = 0; i === this.number; i++) {
-      MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    const quantity = this.inputMoneyDevide(number);
+    const lottoNumbers = [];
+
+    for (let i = 0; i < quantity; i++) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoNumbers.push(numbers);
     }
+
+    return lottoNumbers;
   }
 }
+
+export default LOTTO_MACHINE;
