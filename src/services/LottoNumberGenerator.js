d4a7b415794675fe58bb11644672d320c2726f94
subsub-e <ab13696802@gmail.com>
2024-10-31 15:19:09 +0900

    feat(LottoNumberGenerator) : 구매금액 검증 로직 및 구매 금액에 따른 로또 번호를 랜덤으로 생성해주는 LottoNumberGenerator 서비스 로직 구현

---
+++ src/services/LottoNumberGenerator.js
@@ -0,0 +1,35 @@
+import ERROR_MESSAGES from '../constants/errorConstants.js';
+import Lotto from '../Lotto.js';
+import { Random } from '@woowacourse/mission-utils';
+
+class LottoNumberGenerator {
+  #lottoAmount;
+
+  constructor(purchaseAmount) {
+    this.#validate(purchaseAmount);
+    this.#lottoAmount = purchaseAmount / 1000;
+  }
+
+  #validate(purchaseAmount) {
+    if (purchaseAmount === "" || isNaN(Number(purchaseAmount))) {
+      throw new Error(ERROR_MESSAGES.PURCHASE_AMOUNT_IS_NOT_NUMBER);
+    }
+    if (purchaseAmount % 1000 != 0) {
+      throw new Error(ERROR_MESSAGES.PURCHASE_AMOUNT_IS_NOT_DIVIDE_BY_THOUSAND);
+    }
+  }
+
+  #randomGererateNumber() {
+    const randomNumberArray =  Random.pickUniqueNumbersInRange(1, 45, 6).map(Number).sort((a, b) => a - b);
+    return randomNumberArray;
+  }
+
+  generateLotto(repository) {
+    for (let i = 0; i < this.#lottoAmount; i++) {
+      const lotto = new Lotto(this.#randomGererateNumber());
+      repository.addLottos(lotto);
+    }
+  }
+}
+
+export default LottoNumberGenerator;
