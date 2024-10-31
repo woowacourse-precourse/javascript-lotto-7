43cdd69089d91bbcc57343fc5c8d312d6cc6ef37
jeongeunLee <leeju1009@gmail.com>
2024-10-31 21:08:07 +0900

    test: 보너스 번호와 일치하는지 파악하는 기능 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -28,4 +28,17 @@ describe('로또 클래스 테스트', () => {
       expect(matchingCount).toBe(expected);
     },
   );
+
+  test.each([
+    [[1, 3, 5, 7, 9, 11], true],
+    [[1, 2, 3, 4, 5, 6], true],
+    [[1, 7, 8, 9, 10, 11], true],
+    [[7, 8, 11, 14, 18, 21], false],
+  ])('보너스 번호와 일치하는지 파악하는 기능 테스트', (numbers, expected) => {
+    const BONUS_NUMBER = 1;
+    const lotto = new Lotto(numbers);
+    const isBonusNumberMatched = lotto.isBonusNumberMatched(BONUS_NUMBER);
+
+    expect(isBonusNumberMatched).toBe(expected);
+  });
 });
