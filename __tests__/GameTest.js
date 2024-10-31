542ab54f41910751c40c87e1ae883f78670ec93c
murphybread <nargene@gmail.com>
2024-10-31 18:01:52 +0900

    test(game): generateLotto method pass case

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -19,3 +19,26 @@ describe("Game 클래스 - purchaseLotto 메서드", () => {
     }).toThrow("[ERROR]");
   });
 });
+
+
+describe ("Game 클래스 - generateLotto 메서드", () =>{
+    let game;
+
+    beforeEach(() =>{
+        game = new Game();
+    });
+
+    test("성공 케이스: 오름차순으로 정렬된 6개의 숫자 배열을 입력받은 개수만큼 가지는 배열인지 확인",() =>{
+        expect(() => {
+            const lotto = game.generateLotto(8);
+
+            expect(lotto).toHaveLength(8);
+
+            lotto.forEach(eachLotto => {
+                expect(eachLotto).toHaveLength(6); // 각 배열에 6개의 숫자가 있는지 확인
+                expect(eachLotto).toEqual([...eachLotto].sort((a, b) => a - b)); // 오름차순 여부 확인
+            });
+          });
+    });
+
+});
