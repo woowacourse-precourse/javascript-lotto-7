d9d77023762a566217debf0a45907aababb01370
KIMJINYOUNG <k5444862@naver.com>
2024-10-31 20:16:04 +0900

    refactor: RankChecker 클래스 getRank 메서드 책임 분리

--- src/RankChecker.js
+++ src/RankChecker.js
@@ -30,7 +30,18 @@ class RankChecker {
   }
 
   static getRank(matchCounts) {
-    const resultObj = {
+    const resultObj = this.#initializeResultObj();
+
+    matchCounts.forEach(([count, bonusMatch]) => {
+      const rank = this.#determineRank(count, bonusMatch);
+      resultObj[rank] += 1;
+    });
+
+    return resultObj;
+  }
+
+  static #initializeResultObj() {
+    return {
       [RANKS.FIRST]: 0,
       [RANKS.SECOND]: 0,
       [RANKS.THIRD]: 0,
@@ -38,22 +49,15 @@ class RankChecker {
       [RANKS.FIFTH]: 0,
       [RANKS.SIXTH]: 0,
     };
+  }
 
-    const determineRank = (count, bonusMatch) => {
-      if (count === 6) return RANKS.FIRST;
-      if (count === 5 && bonusMatch) return RANKS.SECOND;
-      if (count === 5 && !bonusMatch) return RANKS.THIRD;
-      if (count === 4) return RANKS.FOURTH;
-      if (count === 3) return RANKS.FIFTH;
-      return RANKS.SIXTH;
-    };
-
-    matchCounts.forEach(([count, bonusMatch]) => {
-      const rank = determineRank(count, bonusMatch);
-      resultObj[rank] += 1;
-    });
-
-    return resultObj;
+  static #determineRank(count, bonusMatch) {
+    if (count === 6) return RANKS.FIRST;
+    if (count === 5 && bonusMatch) return RANKS.SECOND;
+    if (count === 5 && !bonusMatch) return RANKS.THIRD;
+    if (count === 4) return RANKS.FOURTH;
+    if (count === 3) return RANKS.FIFTH;
+    return RANKS.SIXTH;
   }
 }
 
