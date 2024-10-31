f7df1b319445ea026786320b97dbddc4a086a498
thumbthing <ilscc0902@gmail.com>
2024-11-01 05:02:13 +0900

    Test: 3개 미만 당첨 제외된 배열 생성 테스트 추가

--- __tests__/feature/match/getMatchCountTest.js
+++ __tests__/feature/match/getMatchCountTest.js
@@ -1,4 +1,4 @@
-import winNumberMatcher from "../../../src/feature/match/getMatchCount.js";
+import { winNumberMatcher, getFilteredMatchList } from "../../../src/feature/match/getMatchCount.js";
 
 describe('구매 내역, 당첨 번호, 보너스 번호 결과 리스트 생성 기능 테스트', 
   () => {
@@ -57,10 +57,51 @@ describe('구매 내역, 당첨 번호, 보너스 번호 결과 리스트 생성
 
         // when
         const result = winNumberMatcher(lottoList, winNumbers, bonusNumber);
-
+        console.log(result.length);
         // then
         result.forEach((lotto, index) => {
           expect(lotto).toEqual(expectedResult[index])
         });
-      })
+      });
+
+      test('구매 리스트에서 3개 이상 일치하는 배열 생성', () => {
+        // given
+        const lottoList = [
+        [2, 3, 13, 14, 15, 44],
+        [1, 2, 9, 20, 25, 40],
+        [14, 17, 21, 36, 38, 45],
+        [2, 7, 11, 12, 23, 24],
+        [1, 2, 4, 5, 6, 43],
+        [4, 8, 17, 27, 32, 42],
+        [2, 8, 11, 12, 18, 26],
+        [5, 7, 24, 29, 30, 36],
+        [2, 8, 19, 21, 22, 29],
+        [1, 2, 3, 4, 5, 6],
+        [1, 2, 3, 4, 6, 7],
+        [2, 3, 4, 5, 6, 7],
+        [1, 2, 30, 31, 32, 33],
+        [2, 3, 34, 35, 36, 37],
+        [4, 5, 7, 38, 39, 40],
+        [1, 2, 3, 41, 42, 43],
+        [3, 4, 5, 6, 44, 45]
+      ];
+
+        const winNumbers = [1, 2, 3, 4, 5, 6];
+        const bonusNumber = 7;
+        const expectedResult = [
+          [5, false],
+          [6, false],
+          [5, true],
+          [5, true],
+          [3, false],
+          [4, false]
+        ];
+
+        // when
+        const result = getFilteredMatchList(lottoList, winNumbers, bonusNumber);
+        // then
+        result.forEach((lotto, index) => {
+          expect(lotto).toEqual(expectedResult[index])
+        });
+      });
 })
