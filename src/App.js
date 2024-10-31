9d783d229dc138d4f45003daf00e38543874cc49
bunju20 <ghkd2271@naver.com>
2024-10-31 18:32:11 +0900

    feat: 구매 입력 받고, 발행된 복권 출력하는 로직 구현 - createLottos으로 받은 금액에 따라 복권을 발행하는 함수 구현 - printLottoPurchase으로 발행한 복권을 순서대로 출력하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-
-async function getCashInHand(){
-const message =
-      '구입금액을 입력해 주세요.\n';
-    const cash = await MissionUtils.Console.readLineAsync(message);
-    return cash;
+async function getCashInHand() {
+  const message = '구입금액을 입력해 주세요.\n';
+  const cash = await MissionUtils.Console.readLineAsync(message);
+  return cash;
 }
 
-
-function cashValidation(string){
+function cashValidation(string) {
   const number = parseFloat(string);
-  if(isNaN(number))return false;
-  const isNotDivided = (number % 1000) !== 0;
-  if(isNotDivided) return false;
+  if (Number.isNaN(number)) return false;
+  const isNotDivided = number % 1000 !== 0;
+  if (isNotDivided) return false;
   return true;
 }
 
-/**
- * 
- * @param {*} lottos
- */
-function printLottoPurchase(lottos){
-  const lottosNumber = lottos.length;
-  MissionUtils.Console.print(`${lottosNumber}개를 구매했습니다.
-`);
+// 구매금액 입력 받고, 복권들 object 반환
+function createLottos(cash) {
+  const numberOfLotto = cash / 1000;
+  const lottos = {};
+  for (let i = 0; i < numberOfLotto; i += 1) {
+    const noSorted = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    lottos[i] = noSorted.sort((a, b) => parseInt(a, 10) - parseInt(b, 10));
+  }
+  return lottos;
+}
 
+function printLottoPurchase(lottos) {
+  const lottosNumber = Object.keys(lottos).length;
+  MissionUtils.Console.print(`${lottosNumber}개를 구매했습니다.`);
+  for (let i = 0; i < lottosNumber; i += 1) {
+    MissionUtils.Console.print(lottos[i]);
+  }
 }
 
 class App {
   async run() {
     const cash = await getCashInHand();
     cashValidation(cash);
-
+    const lottos = createLottos(cash);
+    printLottoPurchase(lottos);
   }
 }
 
