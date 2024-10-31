c8784e9cd72a6e25afb5c81e528030d79a6c9b27
sooyeoniya <tndus1306403@naver.com>
2024-11-01 01:24:15 +0900

    refactor(LottoStatistics): 공통 로직 분리 및 들여쓰기 수정
    - 당첨 번호 또는 보너스 번호와 해당 로또를 비교하는 공통 로직을 isMatch() 함수로 분리
    - indent depth 3에 대한 고민을 하던 중 Airbnb 컨벤션에 따르면
      if 조건문 내부가 단일행인 경우에는 중괄호를 사용하지 않아도 됨
    - 이에 if 루프 들여쓰기 컨벤션 수정하여 indent depth 2로 줄임

--- src/domain/LottoStatistics.js
+++ src/domain/LottoStatistics.js
@@ -18,32 +18,28 @@ class LottoStatistics {
     return this.#matchResults;
   }
 
+  #isMatch(lotto, number) {
+    return lotto.some((lottoNumber) => lottoNumber === number);
+  }
+
   #countLottoMatches(lotto, winningNumbers) {
     let count = 0;
     winningNumbers.forEach((winningNumber) => {
-      const matchNumber = (number) => number === winningNumber;
-      if (lotto.some(matchNumber)) count += 1;
+      if (this.#isMatch(lotto, winningNumber)) count += 1;
     });
     return count;
   }
 
   #checkBonusMatch(lotto, bonusNumber) {
-    const matchNumber = (number) => number === bonusNumber;
-    if (lotto.some(matchNumber)) {
-      return MATCH_CODE.FIVE_WITH_BONUS;
-    }
+    if (this.#isMatch(lotto, bonusNumber)) return MATCH_CODE.FIVE_WITH_BONUS;
     return MATCH_CODE.FIVE;
   }
 
   #setMatchResults(lottos, winningNumbers, bonusNumber) {
     lottos.forEach((lotto) => {
       let count = this.#countLottoMatches(lotto, winningNumbers);
-      if (count === MATCH_CODE.FIVE) {
-        count = this.#checkBonusMatch(lotto, bonusNumber);
-      }
-      if (this.#matchResults.has(count)) {
-        this.#matchResults.set(count, this.#matchResults.get(count) + 1);
-      }
+      if (count === MATCH_CODE.FIVE) count = this.#checkBonusMatch(lotto, bonusNumber);
+      if (this.#matchResults.has(count)) this.#matchResults.set(count, this.#matchResults.get(count) + 1);
     });
   }
 }
