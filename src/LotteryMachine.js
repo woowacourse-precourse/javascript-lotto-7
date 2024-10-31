c71f88e4fc157722e780b46c06e7de15c1f16dfe
sanghyeonLee <hyeon012366@gmail.com>
2024-11-01 03:33:10 +0900

    feat: 입력 메소드 추가 및 호출

--- src/LotteryMachine.js
+++ src/LotteryMachine.js
@@ -1,8 +1,18 @@
+import {Console} from "@woowacourse/mission-utils";
+import {INPUT} from "./constants/message.js";
+
 class LotteryMachine {
 
     async start() {
+        await this.input(INPUT.PURCHASE)
+        await this.input(INPUT.WINNING_NUMBER)
+
 
     }
 
-
+    async input(message) {
+        return await Console.readLineAsync(message)
+    }
 }
+
+export default LotteryMachine
