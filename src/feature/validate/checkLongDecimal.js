1f55e04918e5205a4b81e7cb11f663866224fb17
thumbthing <ilscc0902@gmail.com>
2024-10-31 22:05:11 +0900

    Feat: 유저 입력값 변환전 소수 판별 기능 구현

---
+++ src/feature/validate/checkLongDecimal.js
@@ -0,0 +1,9 @@
+function checkLongDecimal(userInput) {
+  const SPLITED_INPUT = userInput.split('.');
+
+  if(SPLITED_INPUT.length !== 1) {
+    throw new Error('[ERROR] 입력값에 소수가 존재 합니다.');
+  }
+};
+
+export default checkLongDecimal;
