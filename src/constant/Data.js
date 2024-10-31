38ac009e323b8ea286ca0a5f8f02549c497aae1c
최영빈 <bin778@naver.com>
2024-11-01 03:13:33 +0900

    feat(Data): 상금 상수화

--- src/constant/Data.js
+++ src/constant/Data.js
@@ -5,4 +5,28 @@ const LOTTO_DATA = Object.freeze({
   lottoPrice: 1000,
 });
 
-export { LOTTO_DATA };
+const PRIZE_NAME = Object.freeze({
+  first: "prizeFirst",
+  second: "prizeSecond",
+  third: "prizeThird",
+  fourth: "prizeFourth",
+  fifth: "prizeFifth",
+});
+
+const PRIZE_CHECK = Object.freeze({
+  [PRIZE_NAME.first]: 6,
+  [PRIZE_NAME.second]: 5,
+  [PRIZE_NAME.third]: 5,
+  [PRIZE_NAME.fourth]: 4,
+  [PRIZE_NAME.fifth]: 3,
+});
+
+const PRIZE_MONEY = Object.freeze({
+  [PRIZE_NAME.first]: 2000000000,
+  [PRIZE_NAME.second]: 30000000,
+  [PRIZE_NAME.third]: 1500000,
+  [PRIZE_NAME.fourth]: 50000,
+  [PRIZE_NAME.fifth]: 5000,
+});
+
+export { LOTTO_DATA, PRIZE_NAME, PRIZE_CHECK, PRIZE_MONEY };
