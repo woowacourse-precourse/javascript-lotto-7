d6a6bda04f7da9893e820af610e37dd388cc4bdd
oko_jin <dswvgw6412@naver.com>
2024-10-31 20:46:18 +0900

    feat(errorMessage): 에러 메시지 작성
    - 구매 금액이 0이하인 경우
    - 구매 금액이 1000원 단위가 아닌 경우
    - 로또번호 구분자가 , 가 아닌 경우
    - 로또번호 숫자가 1~45 사이의 숫자가 아닌 경우
    - 보너스 번호가 문자로 입력된 경우

---
+++ src/constant/errorMessage.js
@@ -0,0 +1,8 @@
+export const errorMessage = {
+  prefix: '[ERROR] ',
+  negativeAmount: '구매금액은 0보다 커야합니다.',
+  divisibleAmount: '구매금액은 1000원 단위로 입력해주세요.',
+  invalidDelimiter: '구분자는 , 이어야 합니다.',
+  invalidNumber: '로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  invalidBonusNumber: '보너스 번호는 숫자로 입력해주세요.',
+};
