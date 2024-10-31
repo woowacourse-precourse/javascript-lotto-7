4a98a4491ff08449ea48cb0ace0f69b59ae074af
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-31 13:54:41 +0900

    fix(gameMessages.js): \n 추가

--- src/constants/gameMessages.js
+++ src/constants/gameMessages.js
@@ -1,10 +1,10 @@
 export const INPUT_MESSAGES = Object.freeze({
-  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
-  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
-  BOUNUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
-  PURCHASE_COUNT: (count) => `${count}개를 구매했습니다.`,
+  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.\n",
+  WINNING_NUMBERS: "\n당첨 번호를 입력해 주세요.\n",
+  BOUNUS_NUMBER: "\n보너스 번호를 입력해 주세요.\n",
+  PURCHASE_COUNT: (count) => `\n${count}개를 구매했습니다.`,
 });
 
 export const OUTPUT_MESSAGES = Object.freeze({
-  WINNER_STATIC: '당첨 통계\n---\n',
+  WINNER_STATIC: "당첨 통계\n---\n",
 });
