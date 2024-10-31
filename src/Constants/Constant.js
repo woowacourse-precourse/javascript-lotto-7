cc2e5ae2a118568b4a0511c39f85f49f73b25be1
ha-kuku <hakukudh@gmail.com>
2024-11-01 08:40:45 +0900

    refactor: 기본 조건 상수 추출

---
+++ src/Constants/Constant.js
@@ -0,0 +1,58 @@
+const LOTTO_PRICE_UNIT = 1000;
+
+const PURCHASE_MONEY_INITIAL_VALUE = 0;
+
+const LOTTO_NUMBER_STANDARD = {
+  min: 1,
+  max: 45,
+  length: 6,
+  separator: ',',
+};
+
+const firstWinner = {
+  rank: '1',
+  match: '6',
+  reward: 2000000000,
+};
+
+const secondWinner = {
+  rank: '2',
+  match: '5',
+  reward: 30000000,
+};
+
+const thirdWinner = {
+  rank: '3',
+  match: '5',
+  reward: '1500000',
+};
+
+const fourthWinner = {
+  rank: '4',
+  match: '4',
+  reward: 50000,
+};
+
+const fifthWinner = {
+  rank: '5',
+  match: '3',
+  reward: 5000,
+};
+
+const losing_ticket = {
+  rank: '0',
+  match: '0',
+  reward: '0',
+};
+
+export {
+  LOTTO_NUMBER_STANDARD,
+  LOTTO_PRICE_UNIT,
+  PURCHASE_MONEY_INITIAL_VALUE,
+  firstWinner,
+  secondWinner,
+  thirdWinner,
+  fourthWinner,
+  fifthWinner,
+  losing_ticket,
+};
