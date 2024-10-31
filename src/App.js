7004ce329734d1ba8812b5b6207bef96c7a82808
dragonite-lee <ross1222@naver.com>
2024-11-01 05:51:46 +0900

    feat(App, Lotto): 당첨번호 입력과 보너스 번호 입력 구현 - 에러코드 메소드로 빼기 - Lotto 클래스에 로또 번호 검증과 보너스 번호 검증 추가

--- src/App.js
+++ src/App.js
@@ -3,34 +3,65 @@ import Lotto from './Lotto.js';
 
 class App {
   async run() {
-    const buy_cost = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    const buyCost = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    this.validatePurchaseAmount(buyCost);
+
     const STANDARD_COST = 1000;
-    const buy_count = buy_cost / STANDARD_COST;
+    const buyCount = buyCost / STANDARD_COST;
 
-    if (isNaN(buy_cost)) {
-      Console.print('숫자로 입력');
-    }
-    if (buy_cost < STANDARD_COST) {
-      Console.print('1000이상이여야 함');
-    }
-    if (!Number.isInteger(buy_count)) {
-      Console.print('1000원 단위로 떨어져야 함');
+    Console.print(`\n${buyCount}개를 구매했습니다.`);
+    const myLottoNumbers = [];
+
+    for (let i = 0; i < buyCount; i++) {
+      const lottoNumber = this.generateNumber();
+      Console.print(lottoNumber, '\n');
+      myLottoNumbers.push(new Lotto(lottoNumber));
     }
 
-    Console.print(`\n${buy_count}개를 구매했습니다.`);
-    const my_loto_number = [];
-    for (let i = 0; i < buy_count; i++) {
-      const loto_number = this.generateNumber();
-      Console.print(loto_number);
-      my_loto_number.push(loto_number)
-    }
-    
+    const winningNumberInput = await Console.readLineAsync(
+      '\n당첨 번호를 입력해 주세요.\n'
+    );
+    const bonusNumberInput = await Console.readLineAsync(
+      '\n보너스 번호를 입력해 주세요.\n'
+    );
+    const winningNumbers = this.validateWinningNumbers(winningNumberInput);
+    const bonusNumber = this.validateBonusNumber(
+      bonusNumberInput,
+      winningNumbers
+    );
+
+    Console.print('당첨 번호와 보너스 번호가 유효합니다.');
+  }
+
+  validatePurchaseAmount(buyCost) {
+    const STANDARD_COST = 1000;
+    if (isNaN(buyCost)) throw new Error('[ERROR] 숫자로 입력해야 합니다.');
+    if (buyCost < STANDARD_COST)
+      throw new Error('[ERROR] 구입금액은 1000원 이상이어야 합니다.');
+    if (buyCost % STANDARD_COST !== 0)
+      throw new Error('[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.');
   }
 
   generateNumber() {
-    const loto_number = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-    loto_number.sort((a, b) => a - b);
-    return loto_number;
+    const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    lottoNumber.sort((a, b) => a - b);
+    return lottoNumber;
+  }
+
+  validateWinningNumbers(input) {
+    const numbers = input.split(',').map((num) => Number(num.trim()));
+    if (numbers.length !== 6 || numbers.some((num) => isNaN(num))) {
+      throw new Error(
+        '[ERROR] 당첨 번호는 쉼표로 구분된 6개의 숫자여야 합니다.'
+      );
+    }
+    return new Lotto(numbers).numbers;
+  }
+
+  validateBonusNumber(bonusInput, winningNumbers) {
+    const bonusNumber = Number(bonusInput.trim());
+    Lotto.validateBonusNumber(bonusNumber, winningNumbers);
+    return bonusNumber;
   }
 }
 
