f2960dd661afc3ab46d88ca7308ec8aa1e26fbef
Yeongi Park <yeongi0111@naver.com>
2024-10-31 11:43:23 +0900

    feat(constant): input,output 메시지 수정
    출력 형식에 맞게 입력과 출력 메시지에 개행 추가

--- src/constant/ouputMessage.js
+++ src/constant/ouputMessage.js
@@ -1,11 +1,11 @@
 import { MATCH_PRICE } from './system';
 
 export const OUTPUT_MESSAGE = Object.freeze({
-  WINNING_STATISTICS: '당첨 통계\n---\n',
-  LOTTO_NUMBER: '개를 구매했습니다.',
+  WINNING_STATISTICS: '\n당첨 통계\n---',
+  LOTTO_LENGTH: '개를 구매했습니다.',
 });
 
-export const MATCH_OUPUT_MESSAGE = Object.freeze({
+export const MATCH_OUTPUT_MESSAGE = Object.freeze({
   THREE: `3개 일치 (${MATCH_PRICE.THREE.toLocaleString('ko-KR')}원)`,
   FOUR: `4개 일치 (${MATCH_PRICE.FOUR.toLocaleString('ko-KR')}원)`,
   FIVE: `5개 일치 (${MATCH_PRICE.FIVE.toLocaleString('ko-KR')}원)`,
