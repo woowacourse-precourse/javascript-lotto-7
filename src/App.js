e1dd3ebe06f84dd2e91dda5fdd44beed75d22d8b
murphybread <nargene@gmail.com>
2024-10-31 16:09:19 +0900

    test(input): integrate input class to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import UserInput from './Input.js';
+
 class App {
-  async run() {}
+
+  constructor(){
+    this.userInput = new UserInput();
+  }
+  async run() {
+
+    const paidMoney = await this.userInput.getUserPaidMoney();
+    const winngingNumber = await this.userInput.getWinningNumber();
+    const bonusNumber = await this.userInput.getBonusNumber();
+  }
 }
 
 export default App;
