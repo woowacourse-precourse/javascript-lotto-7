49503e3a55b2766900fbe88700bbfe18b3dde1f8
thumbthing <ilscc0902@gmail.com>
2024-10-31 23:57:58 +0900

    Feat: 보너서 번호 입력 기능 구현

--- src/feature/UI/getUserInput.js
+++ src/feature/UI/getUserInput.js
@@ -25,4 +25,14 @@ async function getWinNumber() {
   }
 }
 
-export { getPurchase, getWinNumber };
+async function getBonusNumber() {
+  try {
+    const USER_INPUT = await Console.readLineAsync('\n보너스 번호를 입력해 주세요\n');
+    return USER_INPUT;
+  } catch (error) {
+    const ERROR_MESSAGE = '[ERROR] 보너스 번호 입력 중에 예상치 못한 에러가 발생 했습니다.'
+    throw new Error(ERROR_MESSAGE);
+  }
+}
+
+export { getPurchase, getWinNumber, getBonusNumber };
