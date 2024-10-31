07071527a9fa684049617d713ffbb2c2ee06f842
subsub-e <ab13696802@gmail.com>
2024-10-31 16:58:21 +0900

    fix(errorConstants) : test에 따른 error 상수 추가

--- src/constants/errorConstants.js
+++ src/constants/errorConstants.js
@@ -2,6 +2,7 @@ const ERROR_MESSAGES = {
   PURCHASE_AMOUNT_IS_NOT_NUMBER: "[ERROR] 구입 금액을 숫자로 작성해주세요!",
   PURCHASE_AMOUNT_IS_NOT_DIVIDE_BY_THOUSAND:
     "[ERROR] 구입 금액이 1,000원 단위로 나누어 떨어져야 해요!",
+  PURCHASE_AMOUNT_MORE_THAN_ZERO: "[ERROR] 로또를 1장 이상 구매해주세요!",
   DUPLICATE_NUMBER_IN_WINNING_AND_BONUS:
     "[ERROR] 당첨 숫자와 보너스 숫자 사이에 중복된 값이 있어요!",
   WINNING_NUMBER_IS_NOT_NUMBER:
