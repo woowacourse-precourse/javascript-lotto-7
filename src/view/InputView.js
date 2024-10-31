03a8b87673dfb9206355febed938df6dc204e503
Yeongi Park <yeongi0111@naver.com>
2024-10-31 23:24:55 +0900

    refactor(InputView.js): async 제거
    불필요한 async 키워드 제거

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,15 +2,15 @@ import { Console } from '@woowacourse/mission-utils';
 import { INPUT_MESSAGE } from '../constant/inputMessage.js';
 
 export default class InputView {
-  static async price() {
+  static price() {
     return Console.readLineAsync(INPUT_MESSAGE.PRICE);
   }
 
-  static async winningNumbers() {
+  static winningNumbers() {
     return Console.readLineAsync(INPUT_MESSAGE.WINNING_NUMBERS);
   }
 
-  static async bonusNumber() {
+  static bonusNumber() {
     return Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
   }
 }
