2a9153e516f09478b4db6f01411b66753ad188dd
@u_limit_ <yurim@ajou.ac.kr>
2024-11-01 01:04:56 +0900

    fix: 로또 구입시 1000원으로 나누어 떨어지지 않을 때

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Console } from '@woowacourse/mission-utils'
 import { CONSOLE_MESSAGES } from "./constant.js";
+import Lotto from './Lotto.js';
 
 
 class App {
   async run() {
     const purchasePrice = await Console.readLineAsync(CONSOLE_MESSAGES.buyPrice);
     const divideInto1000 = purchasePrice % 1000;
-    if((divideInto1000).length!==0){
+
+    if ((divideInto1000) !== 0) {
       throw new Error('[ERROR]');
     }
   }
