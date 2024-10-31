1dc54fe116dbf1fffeb54b2c023b3e9198221d56
df2854 <df2854@icloud.com>
2024-10-31 19:46:54 +0900

    fix: 로또 출력 문자열로 변경

--- src/Views/outputView.js
+++ src/Views/outputView.js
@@ -1,7 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export async function printLottoList(quantity, lottoList) {
-    Console.print(`\n${quantity}개를 구매했습니다.`);
-    lottoList.forEach(a => Console.print(a));
-    Console.print('');
+    Console.print(`${quantity}개를 구매했습니다.`);
+    lottoList.forEach(a => Console.print(`[${a.join(', ')}]`));
 }
