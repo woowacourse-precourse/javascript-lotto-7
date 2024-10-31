08114cabf8aefb10d523b7af609a62b40b1c4d1f
thumbthing <ilscc0902@gmail.com>
2024-11-01 03:52:31 +0900

    Feat: 당첨 결과 클래스 기능 구현

---
+++ src/Class/LottoResult.js
@@ -0,0 +1,46 @@
+import winNumberMatcher from "../feature/match/getMatchCount.js";
+
+class LottoResult {
+  #lottoList;
+  #winningStats;
+
+  constructor(lottoList, winNumbers, bonusNumber) {
+    this.#lottoList = winNumberMatcher(lottoList, winNumbers, bonusNumber);
+    this.#winningStats = new Map([
+      ['3개 일치', { count : 0, winningAmount: 5000 }],
+      ['4개 일치', { count : 0, winningAmount: 50000 }],
+      ['5개 일치', { count : 0, winningAmount: 1500000 }],
+      ['5개 일치, 보너스 볼 일치', { count : 0, winningAmount: 30000000 }],
+      ['6개 일치', { count : 0, winningAmount: 2000000000 }],
+    ]);
+    this.#reflectResult(this.#lottoList);
+  };
+
+  #getKey(lotto, isBonus) {
+    let winningStatKey = `${lotto}개 일치`;
+
+    if (isBonus) {
+      winningStatKey += ', 보너스 볼 일치';
+    };
+
+    return winningStatKey;
+  };
+
+  #increaseCount(key) {
+    let currentValue = this.#winningStats.get(key);
+    currentValue++;
+  };
+
+  #reflectResult(lottoList) {
+    lottoList.forEach((lotto, isBonus) => {
+      const KEY = this.#getKey(lotto, isBonus);
+      this.#increaseCount(KEY);
+    });
+  };
+
+  get getResult() {
+    return this.#winningStats;
+  };
+};
+
+export default LottoResult;
