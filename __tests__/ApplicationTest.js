c84bb8e84be3c1567c3c221ff11728a305a40016
guesung <gueit214@naver.com>
2024-10-31 15:28:03 +0900

    test: 애플리케이션 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,46 +1,31 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
 import App from '../src/App.js';
+import { ERROR_MESSAGE } from '../src/lib/constants.js';
+import { getLogSpy, mockQuestions, mockRandoms } from '../src/lib/testUtils.js';
 
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-
-    return Promise.resolve(input);
-  });
-};
-
-const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
-  numbers.reduce(
-    (acc, number) => acc.mockReturnValueOnce(number),
-    MissionUtils.Random.pickUniqueNumbersInRange,
-  );
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
-};
-
-const runException = async (input) => {
-  // given
+const runException = async (inputs, errorMessage) => {
   const logSpy = getLogSpy();
 
+  const inputNumbersToEndStartIndex = inputs.length - 1;
+
   const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-  const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,6', '7'];
+  const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,6', '7'].slice(
+    inputNumbersToEndStartIndex,
+    3,
+  );
 
-  mockRandoms([RANDOM_NUMBERS_TO_END]);
-  mockQuestions([input, ...INPUT_NUMBERS_TO_END]);
+  mockRandoms([
+    RANDOM_NUMBERS_TO_END,
+    RANDOM_NUMBERS_TO_END,
+    RANDOM_NUMBERS_TO_END,
+    RANDOM_NUMBERS_TO_END,
+    RANDOM_NUMBERS_TO_END,
+  ]);
+  mockQuestions([...inputs, ...INPUT_NUMBERS_TO_END]);
 
-  // when
   const app = new App();
   await app.run();
 
-  // then
-  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
 };
 
 const lottoTestCaseArray = [
@@ -66,12 +51,6 @@ const lottoTestCaseArray = [
       '[7, 11, 30, 40, 42, 43]',
       '[2, 13, 22, 32, 38, 45]',
       '[1, 3, 5, 14, 22, 45]',
-      '3개 일치 (5,000원) - 1개',
-      '4개 일치 (50,000원) - 0개',
-      '5개 일치 (1,500,000원) - 0개',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
-      '6개 일치 (2,000,000,000원) - 0개',
-      '총 수익률은 62.5%입니다.',
     ],
   },
 ];
@@ -93,9 +72,7 @@ describe('로또 테스트', () => {
         const app = new App();
         await app.run();
 
-        const logs = logArray;
-
-        logs.forEach((log) => {
+        logArray.forEach((log) => {
           expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
         });
       },
@@ -103,15 +80,57 @@ describe('로또 테스트', () => {
   });
 
   describe('예외 테스트', () => {
-    test('숫자가 아닌 경우 에러를 출력한다.', async () => {
-      await runException('1000j');
+    describe('구입 금액', () => {
+      test('숫자가 아닌 경우.', async () => {
+        await runException(['1000j'], ERROR_MESSAGE.NOT_NUMERIC);
+      });
+      test('1,000원 단위가 아닌 경우.', async () => {
+        await runException(['1010'], ERROR_MESSAGE.NOT_THOUSAND_UNIT);
+      });
+      test('양수가 아닌 경우.', async () => {
+        await runException(['-1000'], ERROR_MESSAGE.NOT_POSITIVE);
+      });
     });
-    test('1,000원 단위가 아닌 경우 에러를 출력한다.', async () => {
-      await runException('1010');
+    describe('당첨 번호', () => {
+      test('6개의 숫자보다 적거나 많게 입력한 경우', async () => {
+        await runException(['5000', '1,2,3'], ERROR_MESSAGE.NOT_SIX);
+        await runException(['5000', '1,2,3,4,5,6,7,8'], ERROR_MESSAGE.NOT_SIX);
+      });
+      test('하나의 숫자라도 1~45 사이의 숫자가 아닌 경우', async () => {
+        await runException(
+          ['5000', '1,2,3,4,5,90'],
+          ERROR_MESSAGE.NOT_BETWEEN_1_AND_45,
+        );
+        await runException(
+          ['5000', '1,2,3,4,5,-10'],
+          ERROR_MESSAGE.NOT_BETWEEN_1_AND_45,
+        );
+      });
+      test('중복된 숫자를 입력했을 경우', async () => {
+        await runException(['5000', '1,2,3,4,5,5'], ERROR_MESSAGE.NOT_UNIQUE);
+      });
     });
-    test('양수가 아닌 경우 에러를 출력한다.', async () => {
-      await runException('0');
-      await runException('-1000');
+    describe('보너스 번호', () => {
+      test('입력한 값이 숫자가 아닌 경우', async () => {
+        await runException(
+          ['5000', '1,2,3,4,5,6', 'a'],
+          ERROR_MESSAGE.NOT_NUMERIC,
+        );
+        await runException(
+          ['5000', '1,2,3,4,5,6', '\\'],
+          ERROR_MESSAGE.NOT_NUMERIC,
+        );
+      });
+      test('1~45 사이의 숫자가 아닌 경우', async () => {
+        await runException(
+          ['5000', '1,2,3,4,5,6', '90'],
+          ERROR_MESSAGE.NOT_BETWEEN_1_AND_45,
+        );
+        await runException(
+          ['5000', '1,2,3,4,5,6', '-10'],
+          ERROR_MESSAGE.NOT_POSITIVE,
+        );
+      });
     });
   });
 });
