42daefdb2039ecd622410803628e11557106b348
sooyeoniya <tndus1306403@naver.com>
2024-10-31 19:34:00 +0900

    feat(LottoStatistics): 로또 당첨 로직 구현

--- src/domain/LottoStatistics.js
+++ src/domain/LottoStatistics.js
@@ -1,27 +1,46 @@
 import { MATCH_CODE } from '../constants/constants.js';
 
 class LottoStatistics {
-  #lottos = [];
-  #winningNumbers;
-  #bonusNumber;
+  #matchResults = {};
 
   constructor(lottos, winningNumbers, bonusNumber) {
-    this.#lottos = lottos;
-    this.#winningNumbers = winningNumbers;
-    this.#bonusNumber = bonusNumber;
-  }
-
-  getMatchResults() {
-    const matchResults = {
-      // { matchCode: winningCount }
-      [MATCH_CODE.THREE]: 1,
+    this.#matchResults = {
+      [MATCH_CODE.THREE]: 0,
       [MATCH_CODE.FOUR]: 0,
       [MATCH_CODE.FIVE]: 0,
       [MATCH_CODE.FIVE_WITH_BONUS]: 0,
       [MATCH_CODE.SIX]: 0,
     };
+    this.#setMatchResults(lottos, winningNumbers, bonusNumber);
+  }
 
-    return matchResults;
+  getMatchResults() {
+    return this.#matchResults;
+  }
+
+  #countLottoMatches(lotto, winningNumbers) {
+    let count = 0;
+    winningNumbers.forEach((winningNumber) => {
+      const matchNumber = (number) => number === winningNumber;
+      if (lotto.some(matchNumber)) count += 1;
+    });
+    return count;
+  }
+
+  #checkBonusMatch(lotto, bonusNumber) {
+    const matchNumber = (number) => number === bonusNumber;
+    if (lotto.some(matchNumber)) {
+      return MATCH_CODE.FIVE_WITH_BONUS;
+    }
+    return MATCH_CODE.FIVE;
+  }
+
+  #setMatchResults(lottos, winningNumbers, bonusNumber) {
+    lottos.forEach((lotto) => {
+      let count = this.#countLottoMatches(lotto, winningNumbers);
+      if (count === MATCH_CODE.FIVE) count = this.#checkBonusMatch(lotto, bonusNumber);
+      if (count >= MATCH_CODE.THREE) this.#matchResults[count] += 1;
+    });
   }
 }
 
