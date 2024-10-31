01c2c7e09a87c8f935afb35299bd6c79cc2689fe
Jang Eunhye <hh961130@naver.com>
2024-10-31 18:53:43 +0900

    feat: (App.js) - 구입금액 입력 및 Lotto_purchase(로또 구매) 클래스 호출

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+import Lotto_purchase from "./Lotto_purchase.js";
 class App {
   async run() {
+    let payment = await Console.readLineAsync("");
+    new Lotto_purchase(payment);
+
     let inputLottoNumber = await Console.readLineAsync("");
     let lottoNumberArray = inputLottoNumber.split(",");
     new Lotto(lottoNumberArray);
