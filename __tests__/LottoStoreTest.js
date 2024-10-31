dcba794a18395c56dfd10081ea2deb312a1bec77
hamo-o <yheel369@gmail.com>
2024-11-01 02:00:19 +0900

    feat(LottoStore,LottoStoreTest): 로또 장수만큼 1~45 사이의 숫자 6개를 뽑아 로또 생성

--- __tests__/LottoStoreTest.js
+++ __tests__/LottoStoreTest.js
@@ -1,10 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "../src/Lotto.js";
 import LottoStore from "../src/LottoStore.js";
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
 
 describe("로또판매점 클래스 테스트", () => {
   describe("buyLotto 메서드", () => {
-    test("로또 구입 금액에 해당하는 만큼 로또를 발행한다.", () => {
+    test("로또 구입 금액에 해당하는 만큼 로또를 구입한다.", () => {
       const lottoStore = new LottoStore(2000);
       expect(lottoStore.buyLotto(2000)).toBe(2);
+    });
+  });
+  describe("createLotto 메서드", () => {
+    test("로또 장수만큼 1~45 사이의 숫자 6개를 뽑아 로또를 생성한다.", () => {
+      const RANDOM_NUMBERS = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]];
+      const lottoStore = new LottoStore(2000);
+      mockRandoms(RANDOM_NUMBERS);
+
+      expect(lottoStore.createLotto())
+        .toEqual([new Lotto(RANDOM_NUMBERS[0]), new Lotto(RANDOM_NUMBERS[1])]);
     });
   });
 });
--- src/LottoStore.js
+++ src/LottoStore.js
@@ -1,14 +1,29 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto";
+import { LOTTO_COUNT } from "./constants/lottoValue";
+
 class LottoStore {
   #lottoCount;
+
+  #lottoList;
 
   LOTTO_PRICE = 1000;
 
   constructor(money) {
     this.#lottoCount = this.buyLotto(money);
+    this.#lottoList = [];
   }
 
   buyLotto(money) {
     return Math.floor(money / this.LOTTO_PRICE);
+  }
+
+  createLotto() {
+    for (let i = 0; i < this.#lottoCount; i += 1) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, LOTTO_COUNT);
+      this.#lottoList.push(new Lotto(numbers));
+    }
+    return this.#lottoList;
   }
 }
 
