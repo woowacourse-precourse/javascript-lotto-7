be029b8db7e963554decb04566b77cf5fe1c2f5e
thumbthing <ilscc0902@gmail.com>
2024-11-01 02:06:15 +0900

    Feat: 당첨 결과 리스트 생성 기능 구현

---
+++ src/feature/match/getMatchCount.js
@@ -0,0 +1,35 @@
+function getWinNumberMatchCount(lotto, winNumbers) {
+  let matchCount = 0;
+
+  lotto.forEach((number) => {
+    const IS_WIN_NUMBER = winNumbers.includes(number);
+    
+    if (IS_WIN_NUMBER) {
+      matchCount = matchCount + 1;
+    }
+  })
+
+  return matchCount;
+};
+
+function getBonusNumberMatch(lotto, bonusNumber) {
+  const IS_CONTAINING = lotto.includes(bonusNumber);
+  return IS_CONTAINING;
+};
+
+function winNumberMatcher(lottoList, winNumbers, bonuseNumber) {
+  const RESULT_LIST = lottoList.map((lotto) => {
+    const MATCH_COUNT = getWinNumberMatchCount(lotto, winNumbers);
+    let bonusNumberContaining = false;
+
+    if (MATCH_COUNT === 5) {
+      bonusNumberContaining = getBonusNumberMatch(lotto, bonuseNumber)
+    }
+
+    return [MATCH_COUNT, bonusNumberContaining];
+  });
+
+  return RESULT_LIST;
+};
+
+export default winNumberMatcher;
