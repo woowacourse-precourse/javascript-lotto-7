773152c5fab4ca691e54893d5a0ec4152ee795a9
ockko <ocko119@gmail.com>
2024-10-31 21:47:31 +0900

    feat(inputView.js): 당첨 번호 입력 받기 기능 구현
    - Console.readLineAsync를 활용하여 당첨 번호를 입력받는 readWinningNumber 메서드 구현

--- src/views/inputView.js
+++ src/views/inputView.js
@@ -6,6 +6,12 @@ const InputView = {
 
     return input;
   },
+
+  async readWinningNumber() {
+    const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+
+    return input;
+  },
 };
 
 export default InputView;
