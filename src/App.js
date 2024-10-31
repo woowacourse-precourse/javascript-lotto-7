604b58eaa8b1bb42707c47bd802ca71649f6c610
silver0108 <kimsoeun0108@gmail.com>
2024-11-01 01:27:50 +0900

    feat: 수량만큼 로또 번호 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import validateAmount from "./validator/validateAmount.js";
 import { InputView } from "./view/InputView.js";
+import LottoService from "./services/LottoService.js";
 
 class App {
   async run() {
     const amountInput = await InputView.getLottoAmount();
     const amount = validateAmount(amountInput);
+
     const lottoCount = amount / 1000;
+    const lottoService = new LottoService();
+    lottoService.generateLottos(lottoCount);
   }
 }
 
--- src/services/LottoService.js
+++ src/services/LottoService.js
@@ -1,5 +1,5 @@
-import Lotto from "../model/Lotto";
-import generateLandomNumber from "../utils/generateLandomNum";
+import Lotto from "../model/Lotto.js";
+import generateLandomNumber from "../utils/generateLandomNum.js";
 
 class LottoService {
   #lottos;
@@ -10,6 +10,13 @@ class LottoService {
 
   getLottos() {
     return this.#lottos;
+  }
+
+  generateLottos(count) {
+    for (let i = 0; i < count; i++) {
+      const lottoNumber = generateLandomNumber(1, 45, 6);
+      this.#lottos.push(new Lotto(lottoNumber));
+    }
   }
 }
 
