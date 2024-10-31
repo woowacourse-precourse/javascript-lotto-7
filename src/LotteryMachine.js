bae807a237cd9aeb5a2c54c45d869b9b8666ea8e
sanghyeonLee <hyeon012366@gmail.com>
2024-11-01 04:32:39 +0900

    refactor: 입력 메소드 App 으로 이동

--- src/LotteryMachine.js
+++ src/LotteryMachine.js
@@ -3,15 +3,11 @@ import {INPUT} from "./constants/message.js";
 
 class LotteryMachine {
 
-    async start() {
-        await this.input(INPUT.PURCHASE)
-        await this.input(INPUT.WINNING_NUMBER)
-
-
+    constructor(purchase) {
+        this.#validate(purchase);
     }
+    #validate(purchase) {
 
-    async input(message) {
-        return await Console.readLineAsync(message)
     }
 }
 
