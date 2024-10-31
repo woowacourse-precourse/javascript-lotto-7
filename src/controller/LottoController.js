a24e2a2fd6be759ad10a3532ab1f69f93cffd237
somm12 <sominan8744@gmail.com>
2024-10-31 19:32:11 +0900

    feat(LottoController): 로또 구입 금액 유효성 검사

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -4,11 +4,22 @@ class LottoController {
   constructor() {
     this.#inputView = new InputView();
   }
+  validateLottoAmount(amount) {
+    if (isNaN(amount)) {
+      throw new Error(`[ERROR] 로또 금액은 숫자로 입력해야합니다.\n`);
+    }
+
+    if (amount < 0 || amount % 1000 !== 0) {
+      throw new Error(`[ERROR] 로또 금액은 1000원 단위의 양수여야 합니다.\n`);
+    }
+  }
   async run() {
     try {
       const lottoAmount = await this.#inputView.readLottoAmount();
+      this.validateLottoAmount(Number(lottoAmount));
     } catch (error) {
-      throw error;
+      console.log(error);
+      this.run();
     }
   }
 }
