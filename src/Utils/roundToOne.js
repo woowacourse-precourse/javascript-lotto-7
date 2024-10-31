5bb5f803b406015a790674add0cc82bf0fc06d91
Minje Sung <minje813@dgu.ac.kr>
2024-11-01 02:55:54 +0900

    fix(Utils): roundToOne()에 정수를 넣으면 생기는 오류 수정
    roundToOne()에 정수를 넣으면 소수점 첫째 자리가 보이도록 변경
    
    __test__
    - roundToOne 함수에 대한 테스트 작성

--- src/Utils/roundToOne.js
+++ src/Utils/roundToOne.js
@@ -1,5 +1,5 @@
 const roundToOne = (number) => {
-  return Math.round((number + Number.EPSILON) * 10) / 10;
+  return (Math.round((number + Number.EPSILON) * 10) / 10).toFixed(1);
 };
 
 export default roundToOne;
