9ff30e52d404dc9edc68061917513352630b1ae4
Jinny <ggab.challenge@gmail.com>
2024-11-01 04:13:33 +0900

    refactor: 당첨 통계 출력 함수 가독성 있게 변경

--- src/Views/outputViews.js
+++ src/Views/outputViews.js
@@ -18,12 +18,15 @@ const printRateOfReturn = (rateOfReturn) => {
 };
 
 const printWinningAmount = () => {
-  Console.print(`\n당첨 통계\n---`);
-  Console.print(`3개 일치 (5,000원) - ${matchCounts.threeMatches}개`);
-  Console.print(`4개 일치 (50,000원) - ${matchCounts.fourMatches}개`);
-  Console.print(`5개 일치 (1,500,000원) - ${matchCounts.fiveMatches}개`);
-  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchCounts.fiveMatchesAndBonus}개`);
-  Console.print(`6개 일치 (2,000,000,000원) - ${matchCounts.sixMatches}개`);
+  Console.print(
+    `\n당첨 통계\n` +
+      `---\n` +
+      `3개 일치 (5,000원) - ${matchCounts.threeMatches}개\n` +
+      `4개 일치 (50,000원) - ${matchCounts.fourMatches}개\n` +
+      `5개 일치 (1,500,000원) - ${matchCounts.fiveMatches}개\n` +
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchCounts.fiveMatchesAndBonus}개\n` +
+      `6개 일치 (2,000,000,000원) - ${matchCounts.sixMatches}개`,
+  );
 };
 
 export { printCountPurchaseAmount, printLottoList, printRateOfReturn, printWinningAmount };
