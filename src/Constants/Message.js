186af719afcc69dd3e42bb047cf31111f0f5eb3c
ha-kuku <hakukudh@gmail.com>
2024-10-31 11:55:45 +0900

    feat: 에러메시지 상수화

--- src/Constants/Message.js
+++ src/Constants/Message.js
@@ -14,3 +14,28 @@ const OUTPUT_MESSAGE = {
   secondWinner: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
   firstWinner: '6개 일치 (2,000,000,000원) - ',
 };
+
+const PREFIX_ERROR = '[ERROR]';
+
+const BASIC_ERROR = {
+  invalidMoney: `${PREFIX_ERROR} 1,000원 단위로 입력해 주십시오.`,
+  invalidInput: `${PREFIX_ERROR} 숫자를 입력해 주십시오.`,
+  invalidNumber: `${PREFIX_ERROR} 1~45 사이의 숫자를 입력해 주십시오`,
+};
+
+const WINNING_NUMBER_ERROR = {
+  invalidSeparator: `${PREFIX_ERROR} ,(쉼표)로 구분해 주십시오.`,
+  invalidlength: `${PREFIX_ERROR} 6개의 숫자를 입력해 주십시오.`,
+};
+
+const BOUNS_NUMBER_ERROR = {
+  invalidlength: `${PREFIX_ERROR} 1개의 숫자를 입력해 주십시오.`,
+};
+
+export {
+  INPUT_MESSAGE,
+  OUTPUT_MESSAGE,
+  BASIC_ERROR,
+  WINNING_NUMBER_ERROR,
+  BOUNS_NUMBER_ERROR,
+};
