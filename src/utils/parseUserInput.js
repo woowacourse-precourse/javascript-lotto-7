4510c277f56b8e6cd7bf618ca4a75de4ee12fbf7
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-10-31 14:14:05 +0900

    feat: 사용자 입력 파싱을 위한 모듈 함수 추가 - getPurchaseAmount를 숫자로 변환하는 toPurchaseAmountNumber 함수 추가 - getWinningNumber와 getBonusNumber를 하나의 배열로 변환하는 parseWinningNumbersWithBonus 함수 추가 - [TODO] 중간 띄어쓰기 예외 처리 추가 예정

--- src/utils/parseUserInput.js
+++ src/utils/parseUserInput.js
@@ -3,3 +3,19 @@ import {
   getWinningNumbers,
   getBonusNumber,
 } from './getUserInput';
+
+function toPurchaseAmountNumber() {
+  return Number(getPurchaseAmount());
+}
+
+function parseWinningNumbersWithBonus() {
+  const winningNumbers = getWinningNumbers()
+    .split(',')
+    .map((e) => Number(e.trim()));
+
+  return [...winningNumbers, Number(getBonusNumber())];
+}
+
+// TODO: [예외사항] 중간 띄어쓰기
+
+export { toPurchaseAmountNumber, parseWinningNumbersWithBonus };
