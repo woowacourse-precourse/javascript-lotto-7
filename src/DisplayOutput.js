21d393b3a585d128e55771cc3f679b9c0bd5c62c
murphybread <nargene@gmail.com>
2024-10-31 18:58:53 +0900

    feat(view): show lottoReulst with prize

--- src/DisplayOutput.js
+++ src/DisplayOutput.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { LOTTO } from "./constants.js";
 import Game from "./Game.js";
 
 class DisplayOutput {
@@ -12,6 +13,30 @@ class DisplayOutput {
         for (const lottoNumbers of lotto) {
             MissionUtils.Console.print(lottoNumbers);
         }
+    }
+
+
+    displayLottoResult (lottoResult){
+        /**
+         * 로또 결과 형식:
+         * lottoResult = [[6등수, 5등수, 4등수, 3등수, 2등수, 1등수], [보너스 당첨자 수]]
+         * @param {Array<Array<number>>} lottoResult - 2차원 배열
+        */
+
+        const prizeKeys = [ 'PRIZE_THREE_NUMBER_MATCHES', 'PRIZE_FOUR_NUMBER_MATCHES', 'PRIZE_FIVE_NUMBER_MATCHES',
+            'PRIZE_FIVE_NUMBER_BONUS_MATCHES', 'PRIZE_SIX_NUMBER_MATCHES'
+        ];
+    
+        MissionUtils.Console.print("당첨 통계\n---");
+    
+        for (let i = 0; i < 3; i++) {
+            MissionUtils.Console.print(`${i + 3}개 일치 (${LOTTO.PRIZE[prizeKeys[i]]}원) - ${lottoResult[0][i + 2]}개`);
+        }
+    
+        // 보너스 당첨자 발표 및 6개 일치
+        MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (${LOTTO.PRIZE.PRIZE_FIVE_NUMBER_BONUS_MATCHES}원) - ${lottoResult[1][0]}개`);
+        MissionUtils.Console.print(`6개 일치 (${LOTTO.PRIZE.PRIZE_SIX_NUMBER_MATCHES}원) - ${lottoResult[0][5]}개`);
+
     }
 
 }
