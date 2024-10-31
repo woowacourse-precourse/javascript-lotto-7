be903889af6e54569e90fe47c25ac37d737ea71e
singyuKang <rkdtlscks123@naver.com>
2024-10-31 17:45:58 +0900

    fix : Lotto Class validate 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+import { ERROR_MESSAGE, LOTTO_COUNT_MAX } from "./constants.js";
+import { hasDuplicatesArr } from "./util/duplicateCheck.js";
+
 class Lotto {
   #numbers;
 
@@ -7,12 +10,22 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.length !== LOTTO_COUNT_MAX) {
+      throw new Error(ERROR_MESSAGE.WINNING_LOTTO_LENGTH_ERROR);
+    }
+    numbers.forEach((value) => {
+      if (isNaN(value) || value === "") {
+        throw new Error(ERROR_MESSAGE.WINNING_LOTTO_NUMBER_ERROR);
+      }
+    });
+    if (hasDuplicatesArr(numbers)) {
+      throw new Error(ERROR_MESSAGE.WINNING_LOTTO_NUMBER_DUPLICATE_ERROR);
     }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
