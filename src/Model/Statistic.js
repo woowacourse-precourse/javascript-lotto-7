7ea3da408f254f1d2792a0caafbd658bbe296ca1
Minje Sung <minje813@dgu.ac.kr>
2024-11-01 02:56:29 +0900

    feat(Model): 통계정보를 담는 Statustic 클래스 추가

---
+++ src/Model/Statistic.js
@@ -0,0 +1,35 @@
+import { PrizeMoney } from '../Constants/constants';
+import roundToOne from '../Utils/roundToOne';
+
+class Statistic {
+  constructor(buyPrice) {
+    this.buyPrice = buyPrice;
+    this.winningResult = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+      miss: 0,
+    };
+  }
+
+  addWinningCount(prizeName) {
+    this.winningResult[prizeName] += 1;
+  }
+
+  getGainPrice() {
+    return (
+      this.winningResult.first * PrizeMoney.FIRST +
+      this.winningResult.second * PrizeMoney.SECOND +
+      this.winningResult.third * PrizeMoney.THIRD +
+      this.winningResult.fourth * PrizeMoney.FOURTH +
+      this.winningResult.fifth * PrizeMoney.FIFTH
+    );
+  }
+
+  getEarningRate() {
+    const rate = this.getGainPrice() / this.buyPrice;
+    return roundToOne(rate);
+  }
+}
