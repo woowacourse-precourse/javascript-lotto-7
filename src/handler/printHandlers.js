dce681a8cff85f2e001601295f9d7b89acb782b8
yusuShin <tlsdbtn0507@naver.com>
2024-10-31 15:46:54 +0900

    refactor(printHandlers.js) Refactor code - Add semicolon - Print ERROR string and message together

--- src/handler/printHandlers.js
+++ src/handler/printHandlers.js
@@ -1,12 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const printError = (msg) => {
-  Console.print(msg);
+  Console.print("[ERROR]" + msg);
   throw new Error("[ERROR]");
 };
 
 const printParam = (msg) => {
-  Console.print(msg)
+  Console.print(msg);
 };
 
 export { printError, printParam };
