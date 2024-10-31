359ea879ee43886454e2b06cb58de9f2a4accccd
yusuShin <tlsdbtn0507@naver.com>
2024-10-31 12:39:37 +0900

    feat(constants.js) Add PURCAHSE_INFO_PROMPT

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const PROMPTS = {
   PURCHASE_PROMPT: "구입금액을 입력해 주세요.( '원'과 ,는 생략해주세요) : ",
-  PURCHASE_ERROR_STRING: "로또 구입 금액은 1,000원 단위로 입력해주세요."
-}
+  PURCAHSE_INFO_PROMPT: "개를 구매했습니다.",
+  PURCHASE_ERROR_STRING: "로또 구입 금액은 1,000원 단위로 입력해주세요.",
+};
 
 export default PROMPTS;
