7148cce2c185ca359150db5d9e9acfcf0aa45ac7
murphybread <nargene@gmail.com>
2024-10-31 18:58:12 +0900

    feat(constants): add price variables

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,19 @@
-    export const LOTTO ={
-        LOTTO_TICKET_PRICE : 1000
+    export const LOTTO = {
+        LOTTO_TICKET_PRICE : 1000,
+        PRIZE : {
+            PRIZE_ONE_NUMBER_MATCHES : 0,
+            PRIZE_TWO_NUMBER_MATCHES : 0,
+            PRIZE_THREE_NUMBER_MATCHES : 5_000,
+            PRIZE_FOUR_NUMBER_MATCHES : 50_000,
+            PRIZE_FIVE_NUMBER_MATCHES : 1_500_000,
+            PRIZE_FIVE_NUMBER_BONUS_MATCHES : 30_000_000,
+            PRIZE_SIX_NUMBER_MATCHES : 2_000_000_000,
+        }
+
+
+    }
+
+    export const DISPLAY_MESSAGE = {
+
+
     }
