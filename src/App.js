7df52ae709ff5aeeae8b9abd629453c1f4033306
@u_limit_ <yurim@ajou.ac.kr>
2024-10-31 17:18:43 +0900

    feat: 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import {Console} from '@woowacourse/mission-utils'
 class App {
-  async run() {}
+  async run() {
+    const purchasePrice = await Console.readLineAsync('구입 금액을 입력해 주세요.\n');
+  }
 }
 
 export default App;
