a4bf809ee41fb02ff5790aab92c9fb04fba0446c
siggu <m0_w_0m@naver.com>
2024-10-31 16:03:27 +0900

    fix(App): 기능 테스트 로그 출력 오류 해결

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App {
   printRandomLottoNumbers(randomLottoNumbers) {
     Console.print('');
     Console.print(`${randomLottoNumbers.length}개를 구매했습니다.`);
-    randomLottoNumbers.forEach((lottoNumber) => Console.print(lottoNumber));
+    randomLottoNumbers.forEach((lottoNumber) => Console.print(`[${lottoNumber.join(', ')}]`));
   }
 
   async getPickLottoNumber() {
@@ -110,7 +110,7 @@ class App {
     Console.print('당첨 통계');
     Console.print('---');
     Console.print(`3개 일치 (5,000원) - ${winningStatistics[0]}개`);
-    Console.print(`4개 일치 (50,000원)  - ${winningStatistics[1]}개`);
+    Console.print(`4개 일치 (50,000원) - ${winningStatistics[1]}개`);
     Console.print(`5개 일치 (1,500,000원) - ${winningStatistics[2]}개`);
     Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningStatistics[3]}개`);
     Console.print(`6개 일치 (2,000,000,000원) - ${winningStatistics[4]}개`);
