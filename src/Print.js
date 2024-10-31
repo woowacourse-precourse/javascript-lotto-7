e6df32d5c34ad506b90023657d93f759b454c6d3
magik99 <hojun0810@naver.com>
2024-11-01 02:12:54 +0900

    feat(Print.js): 로또 번호 출력 기능 구현
    로또 번호 매개변수를 콘솔창에 출력

--- src/Print.js
+++ src/Print.js
@@ -4,6 +4,10 @@ class Print {
   static printTimes(times) {
     Console.print(`${times}개를 구매했습니다.`);
   }
+
+  static printLottosNumber(numbers) {
+    Console.print(numbers);
+  }
 }
 
 export default Print;
