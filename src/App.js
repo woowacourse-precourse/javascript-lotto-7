0a116bf2c5e373909a7fe976faf2702b605e20b5
yubin <yb3143@gmail.com>
2024-10-31 16:18:01 +0900

    feat: 당첨 통계 텍스트 생성

--- src/App.js
+++ src/App.js
@@ -81,7 +81,12 @@ class App {
       }
     });
 
-    return result;
+    // return result;
+    return `3개 일치 (5,000원) - ${result[3]}개 \n
+    4개 일치 (50,000원) - ${result[4]}개 \n 
+    5개 일치 (1,500,000원) - ${result[5]}개 \n
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${result["5(bonus)"]}개 \n
+    6개 일치 (2,000,000,000원) - ${result[6]}개`;
   }
 }
 
