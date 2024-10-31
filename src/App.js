b9f39fae3ffa067cd05ffa695bc0e42e4bd7f775
Yun-Jinwoo <yjw3480@naver.com>
2024-11-01 00:25:36 +0900

    ✨ feat(App.js): 로또 발행 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import { Console, Random } from "@woowacourse/mission-utils"
+import { Console } from "@woowacourse/mission-utils"
+import printAllLotto from "./AllLotto.js";
 class App {
   async run() {
     const inputPrice = await Console.readLineAsync("구입금액을 입력해 주세요.");
+    const lottoNum = inputPrice / 1000;
+    Console.print(`${lottoNum}개를 구매했습니다.`)
+    const lottoArray = printAllLotto(lottoNum);
+
 
     const inputNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
     const winningNumbers = inputNumbers.split(",").filter(item => item !== "");
