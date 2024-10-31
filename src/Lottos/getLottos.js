107e85d38e1c53d31b03c6a5ca02407db4dcc921
minji2219 <200146@jnu.ac.kr>
2024-10-31 15:40:21 +0900

    feat: print 문장 추가
    문제 요구 사항 중에 존재하므로 추가

--- src/Lottos/getLottos.js
+++ src/Lottos/getLottos.js
@@ -1,7 +1,9 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
-export const getLotto = (num) => {
+export const getLottos = (num) => {
   const lottos = [];
+
+  Console.print('\n' + num + '개를 구매했습니다.');
 
   for (let i = 1; i <= num; i++) {
     const lotto = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
