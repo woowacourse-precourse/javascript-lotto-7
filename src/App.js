a1c9d825b36143091a6ac51d17dd8ab7a61b17a7
choeeun-in <cei8645@naver.com>
2024-10-31 21:53:55 +0900

    feat:당첨 결과 계산 및 수익률 출력 기능

--- src/App.js
+++ src/App.js
@@ -32,10 +32,47 @@ class App {
 
     const bonusNumber = Lotto.genBonusNum(lotto.getNum());
 
+    //당첨 결과 계산
+    const winningMoney = {
+      3: 5000,
+      4: 50000,
+      5: 1500000,
+      "5+bonus": 30000000,
+      6: 2000000000,
+    };
+    const result = { 3: 0, 4: 0, 5: 0, "5+bonus": 0, 6: 0 };
+
+    lottoList.forEach((ticket) => {
+      const matchCount = ticket.filter((num) =>
+        lottoNumbers.includes(num)
+      ).length;
+      const isBonusMatched = ticket.includes(bonusNumber);
+
+      if (matchCount === 5 && isBonusMatched) result["5+bonus"]++;
+      else if (matchCount >= 3) result[matchCount]++;
+    });
+
+    //수익률 계산
+    const totalPrize = Object.keys(result).reduce(
+      (acc, key) => acc + result[key] * winningMoney[key],
+      0
+    );
+    const profitRate = Math.round((totalPrize / lottoPrice) * 100 * 10) / 10;
+
     // 로직 실행
     Console.print(`구입 금액: ${lottoPrice}`);
     Console.print(`당첨 번호: ${lotto.getNum()}`);
     Console.print(`보너스 번호: ${bonusNumber}`);
+
+    // 당첨 내역 출력
+    Console.print(`3개 일치 (5,000원) - ${result[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${result[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${result[5]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result["5+bonus"]}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${result[6]}개`);
+    Console.print(`총 수익률은 ${profitRate}%입니다.`);
   }
 }
 
