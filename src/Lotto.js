54593804dce4781cecef56ae74c7a41cc0ea3446
강성준 <comebacktoearth@gangseongjun-ui-MacBookAir.local>
2024-11-01 00:39:33 +0900

    feat(Validator): Add lotto numbers range check to Validator
    각 로또 번호들이 1~45사이의 숫자인지 체크하는 checkNumberRange 메서드를 추가하였습니다. 그리고 vaidator 클래스 속 로또 번호를 검증하는 메서드들을 validateLottoNumbers로 묶어 SRP와 사용성을 높였습니다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,11 +3,10 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    Validator.checkSixNumbers(numbers);
-    Validator.checkDuplicateNumber(numbers);
+    Validator.validateLottoNumbers(numbers);
     this.#numbers = numbers;
   }
-  
+
   // TODO: 추가 기능 구현
 }
 
--- src/Validator.js
+++ src/Validator.js
@@ -1,4 +1,10 @@
 class Validator {
+  static validateLottoNumbers(numbers) {
+    this.checkDuplicateNumber(numbers);
+    this.checkSixNumbers(numbers);
+    this.checkNumberRange(numbers);
+  };
+
   static checkSixNumbers(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
@@ -6,8 +12,15 @@ class Validator {
   }
   static checkDuplicateNumber(numbers) {
     if(numbers.length !== new Set(numbers)) {
-      throw new Error("[ERROR] : 번호들은 중복될 수 없습니다.")
+      throw new Error("[ERROR] : 번호들은 중복될 수 없습니다.");
     }
+  }
+  static checkNumberRange(numbers) {
+    numbers.forEach(number => {
+      if(number < 1 || number > 45) {
+        throw new Error("[ERROR] : 로또 번호는 1부터 45 중 하나이어야 합니다.");
+      }
+    })
   }
 };
 
