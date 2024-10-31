2b505f50d5eac300e8d0c4295af2531a64d088e3
ChaDongmin <cdm1263@gmail.com>
2024-11-01 04:08:07 +0900

    feat(Looto): 로또 클래스 기능 구성
    - validate 메서드 구성
    
    - 로또 등수를 반환하는 getLottoResult 메서드 구성

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,47 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+    if (
+      numbers.some(
+        (num) => isNaN(Number(num)) || !Number.isInteger(Number(num))
+      )
+    ) {
+      throw new Error("[ERROR] 숫자 이외의 문자는 입력할 수 없습니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getLottoResult(winningNumbers, bonusNumber) {
+    const winningNumbersArray = [...winningNumbers.split(","), bonusNumber];
+
+    const matchedCount = this.#numbers.filter((number) =>
+      winningNumbersArray.includes(number.toString())
+    ).length;
+
+    if (matchedCount === 3) {
+      return 5;
+    }
+
+    if (matchedCount === 4) {
+      return 4;
+    }
+
+    if (matchedCount === 5) {
+      return 3;
+    }
+
+    if (matchedCount === 6) {
+      if (this.#numbers.includes(Number(bonusNumber))) {
+        return 2;
+      }
+
+      return 1;
+    }
+
+    return 0;
+  }
 }
 
 export default Lotto;
