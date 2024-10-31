7232cedf0510eeeb2da7b482fe9d024647db7d7b
jeongeunLee <leeju1009@gmail.com>
2024-10-31 22:19:54 +0900

    feat: analyzer 클래스에 winningNumbers 필드 추가

--- src/Analyzer.js
+++ src/Analyzer.js
@@ -1,10 +1,13 @@
 class Analyzer {
   #lottos;
 
+  #winningNumbers;
+
   #winningTable;
 
-  constructor(lottos) {
+  constructor(lottos, winningNumbers) {
     this.#lottos = lottos;
+    this.#winningNumbers = winningNumbers;
     this.#winningTable = new Map();
   }
 }
