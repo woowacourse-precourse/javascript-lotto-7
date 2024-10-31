df94697dbaeb05ca90d63c6a4a13a0e0d846f81b
ockko <ocko119@gmail.com>
2024-10-31 22:03:53 +0900

    feat(inputView.js): 입력된 당첨 번호를 쉼표 기준으로 구분하는 기능 구현
    - 입력받은 문자열을 쉼표로 분리하고 공백 제거 후 그 배열을 반환하는 parseWinningNumber 메서드 구현
    - readWinningNumber에서 parseWinningNumber 메서드를 사용하여 return

--- src/views/inputView.js
+++ src/views/inputView.js
@@ -10,7 +10,11 @@ const InputView = {
   async readWinningNumber() {
     const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
 
-    return input;
+    return this.parseWinningNumber(input);
+  },
+
+  parseWinningNumber(input) {
+    return input.split(",").map((number) => number.trim());
   },
 };
 
