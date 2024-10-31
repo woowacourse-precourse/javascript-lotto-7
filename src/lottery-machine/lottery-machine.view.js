ac6064c714dc365f86b054fdaff28550c655d615
L2HYUNN <donghyuni98@gmail.com>
2024-10-31 23:23:06 +0900

    feat(view): 로또 구입 금액을 입력 받는 기능 구현

---
+++ src/lottery-machine/lottery-machine.view.js
@@ -0,0 +1,19 @@
+// @ts-check
+import { input } from '../lib/view';
+
+class LotteryMachineView {
+  static QUERY = Object.freeze({
+    GET_LOTTERY_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
+  });
+  /**
+   *
+   * @returns {Promise<string>}
+   */
+  async getLotteryPurchaseAmount() {
+    const result = await input(LotteryMachineView.QUERY.GET_LOTTERY_PURCHASE_AMOUNT);
+
+    return result;
+  }
+}
+
+export default LotteryMachineView;
