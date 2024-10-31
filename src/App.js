180192bdd5c79cb4085710e941bc3abf59bcff1c
immaemi <nam14785@soongsil.ac.kr>
2024-10-31 19:56:43 +0900

    feat: Added lotto amount input function
    - Added input for lotto purchase amount
    - Validated that the amount is a positive multiple of 1,000
    - Generate the number of lotto tickets according to the entered amount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await this.getPurchaseAmount();
+    const lottoCount = purchaseAmount / 1000;
+    Console.print(`${lottoCount}개를 구입했습니다.`)
+
+  }
+
+  async getPurchaseAmount() {
+    let amount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    amount = parseInt(amount);
+    validatePurchaseAmount(amount);
+    return amount ;
+  }
+}
+
+function validatePurchaseAmount(amount) {
+  if (amount <= 0 || amount % 1000 !== 0) {
+    throw new Error("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+  }
 }
 
 export default App;
