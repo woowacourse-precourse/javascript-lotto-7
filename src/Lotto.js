c13deba8f4ef6fc3a883c7849b7781f3bb3f5df7
minji2219 <200146@jnu.ac.kr>
2024-10-31 15:43:03 +0900

    feat: 당첨 로또 숫자들의 유효성 검사
    split으로 추출된 당첨 로또 숫자들의 유효성 검사
    1. 1~45의 사이의 숫자인지 검사
    2. 입력된 로또 번호가 6개인지 검사
    3. 중복되지 않은 6개의 숫자인지 검사

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,34 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.validateIsRangeNumber(numbers);
+    this.validateLottoCount(numbers);
+    this.validateDuplicate(numbers);
   }
 
   // TODO: 추가 기능 구현
+  validateIsRangeNumber(numbers) {
+    numbers.forEach((num) => {
+      if (!(num >= 1 && num <= 45)) {
+        throw new Error('[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.');
+      }
+    });
+  }
+
+  validateLottoCount(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
+
+  validateDuplicate(numbers) {
+    const availableNumbers = new Array(46).fill(0);
+    numbers.forEach((num) => {
+      if (availableNumbers[num] > 0)
+        throw new Error('[ERROR] 중복되지 않은 숫자를 입력해주세요.');
+      availableNumbers[num]++;
+    });
+  }
 }
 
 export default Lotto;
