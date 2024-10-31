786819281bafa4d3437feecf7da574840d5b32d0
Yun-Jinwoo <yjw3480@naver.com>
2024-11-01 05:08:49 +0900

    ✨ feat: pickUniqueNumbersInRange함수로 변경

--- src/RandomNumber.js
+++ src/RandomNumber.js
@@ -1,12 +1,7 @@
 import { Random } from "@woowacourse/mission-utils";
 
 const generateRandomNumbers = () => {
-    const numbers = new Set(); // 중복 없는 Set
-    while (numbers.size < 6) {
-        const randomNum = Random.pickNumberInRange(1, 45); // 1~45 숫자 랜덤으로 뽑기
-        numbers.add(randomNum);
-    }
-    return Array.from(numbers);
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
 }
 
 export default generateRandomNumbers;
