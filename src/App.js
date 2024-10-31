e1ad90cc456ff8e9361314693b829f0e72f3c0d6
j0n0m2 <dbsk8223@naver.com>
2024-11-01 03:07:54 +0900

    사용자로부터 로또 당첨 번호와 보너스 번호를 입력 받는다

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App {
   async run(){
 
     let lottoAmount;
+    let lottoWinningNumbers;
+    let lottoBonusNumber;
 
     while (true) {
       try {
@@ -15,7 +17,26 @@ class App {
         Console.print(error.message);
       }
     }
+    const lottoQuantity = lottoAmount / LOTTO_AMOUNT_UNIT;
+    const showRandomNumbers = new LottoNumberPrinter();
+    showRandomNumbers.printLottoNumbers(lottoQuantity);
 
+    while (true) {
+      try {
+        lottoWinningNumbers = await this.promptWinningNumbers();
+        break
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    while (true) {
+      try {
+        lottoBonusNumber = await this.promptBonusNumber(lottoWinningNumbers);
+        break
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 
   async promptLottoAmount() {
@@ -26,6 +47,27 @@ class App {
     return lottoAmount;
   }
 
+  async promptWinningNumbers() {
+    const winningNumberInput = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+    const winningNumbers = this.splitWinningNumbers(winningNumberInput);
+    const validator = new LottoWinningValidator();
+    validator.validateNumberOfWinningBalls(winningNumbers);
+    validator.validateNumbersRange(winningNumbers);
+    validator.validateDuplicateNumbers(winningNumbers);
+    return winningNumbers;
+  }
+
+  async promptBonusNumber(winningNumbers) {
+    const bonusNumber = Number(await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n'));
+    const validator = new LottoBonusNumberValidator();
+    validator.validateDuplicateWithWinningNumbers(bonusNumber,winningNumbers);
+    validator.validateNumbersRange(bonusNumber); // validator 당첨 번호 validator 조건문 중복 : 수정 필요!
+    return bonusNumber;
+  }
+  
+  splitWinningNumbers(numbers) {
+    return numbers.split(',').map(num => Number(num.trim()));
+  }
 }
 
 class LottoAmountValidator {
@@ -39,6 +81,62 @@ class LottoAmountValidator {
       throw new Error('\n[ERROR] 1,000원 단위만 입력할 수 있습니다.');
     }
   }
+}
+
+class LottoWinningValidator {
+  validateNumberOfWinningBalls(numbers) {
+    if ( numbers.length !== NUMBER_OF_BALLS ) {
+      throw new Error('\n[ERROR] 6개의 당첨 번호를 입력해야 합니다.')
+    }
+  }
+  validateNumbersRange(numbers) {
+    numbers.forEach(num => {
+      if ( LOTTO_NUM_RANGE.MIN > num || LOTTO_NUM_RANGE.MAX < num) {
+        throw new Error('\n[ERROR] 당첨 번호는 1부터 45사이의 숫자만 입력할 수 있습니다.')
+      }
+    })
+  }
+  validateDuplicateNumbers(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error('\n[ERROR] 당첨 번호에 중복된 숫자가 포함되어 있습니다.')
+    }
+  }
+}
+
+class LottoBonusNumberValidator {
+  validateNumbersRange(number) {
+      if ( LOTTO_NUM_RANGE.MIN > number || LOTTO_NUM_RANGE.MAX < number) {
+        throw new Error('\n[ERROR] 보너스 번호는 1부터 45사이의 숫자만 입력할 수 있습니다.')
+      }
+  }
+  validateDuplicateWithWinningNumbers(bonusNumber, winningNumbers) {
+    const allNumbers = [...winningNumbers,bonusNumber];
+    const uniqueNumbers = new Set(allNumbers);
+    if (uniqueNumbers.size !== allNumbers.length) {
+      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 다른 숫자를 입력해야 합니다.')
+    }
+  }
+}
+
+class LottoNumberPrinter {
+  printLottoNumbers(quantity) {
+    Console.print(`\n${quantity}개를 구매했습니다.`)
+    const allLottoNumbers = [];
+    for (let i = 0; i < quantity; i++) {
+      const numbers = this.pickLottoRandomNumbers();
+      allLottoNumbers.push(numbers.sort((a,b) => {
+        return a-b
+      }));
+    }
+    allLottoNumbers.forEach(numbers => Console.print(`[${numbers.join(', ')}]`));
+  }
+
+  pickLottoRandomNumbers() {
+    const randomNum = Random.pickUniqueNumbersInRange(LOTTO_NUM_RANGE.MIN,LOTTO_NUM_RANGE.MAX,NUMBER_OF_BALLS)
+    return randomNum;
+  }
+
 }
 
 export default App;
