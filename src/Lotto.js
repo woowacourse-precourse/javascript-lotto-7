5ce19d65f0925857127855955dbbf164e65e296f
hamo-o <yheel369@gmail.com>
2024-11-01 01:06:24 +0900

    feat(Lotto,LottoTest): 로또 번호 중복 유효성 검사

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,10 @@
 class Lotto {
   #numbers;
 
+  /**
+   *
+   * @param {number[]} numbers - 로또 번호 배열.
+   */
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
@@ -10,9 +14,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
 
   // TODO: 추가 기능 구현
+  }
 }
 
 export default Lotto;
