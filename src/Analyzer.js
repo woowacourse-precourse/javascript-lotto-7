8bd95e5f7bdb4fb13a2a35356476e39fc4a9fb79
jeongeunLee <leeju1009@gmail.com>
2024-10-31 22:43:35 +0900

    feat: 각 로또 번호가 당첨 번호와 일치하는 개수를 얻는 함수 추가

--- src/Analyzer.js
+++ src/Analyzer.js
@@ -10,6 +10,16 @@ class Analyzer {
     this.#winningNumbers = winningNumbers;
     this.#winningTable = new Map();
   }
+
+  #getMatchingNumbers() {
+    const lottos = [...this.#lottos];
+    const { numbers } = this.#winningNumbers;
+    const matchedNumbers = lottos.map((lotto) =>
+      lotto.countMatchingNumbers(numbers),
+    );
+
+    return matchedNumbers;
+  }
 }
 
 export default Analyzer;
