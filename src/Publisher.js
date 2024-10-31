92eed8479680997a617abbb22aebc5440f049541
jongbin26 <010jjj@naver.com>
2024-10-31 20:03:23 +0900

    feat(Publisher): 구입 금액에 맞춰 로또 발행 기능 추가
    - 구입 금액에 맞춰 로또를 생성하는 generateLotto 함수를 구현했습니다.
    - 랜덤로또 번호를 뽑는 generateRandomNumbers 함수를 구현했습니다.
    - 기존에 생성자 내부에 있던 setPurchaseQuantity 함수를 메서드로 추출했습니다.
    - 추출을 통해 Publisher 클래스와 로또 구입 수량 간의 의존성을 제거했습니다.

--- src/Publisher.js
+++ src/Publisher.js
@@ -1,10 +1,31 @@
+import { Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import Lottos from './Lottos.js';
 import { calculateQuatity } from './utils/index.js';
 import { Validator } from './Validator.js';
 
 export class Publisher {
-  constructor(purchaseAmount) {
+  constructor() {
     this.validator = new Validator();
-    this.validator.validatePurchaseAmount(purchaseAmount);
-    this.purchaseQuantity = calculateQuatity(purchaseAmount);
   }
+
+  setPurchaseQuantity = (purchaseAmount) => {
+    this.purchaseQuantity = calculateQuatity(purchaseAmount);
+    this.validator.validatePurchaseAmount(purchaseAmount);
+  };
+
+  generateLotto = () => {
+    const lottoNumbers = Array.from({ length: this.purchaseQuantity }).map(
+      () => {
+        const randomNumbers = this.generateRandomNumbers();
+        return new Lotto(randomNumbers);
+      },
+    );
+    return new Lottos(lottoNumbers);
+  };
+
+  generateRandomNumbers = () => {
+    const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return lottoNumbers;
+  };
 }
