0d4cad06a391d1427bdc6bf625735d2c778a104d
최영빈 <bin778@naver.com>
2024-10-31 16:26:13 +0900

    test: 로또 번호 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,31 @@
-import Lotto from "../src/Lotto";
+import LottoValidate from "../src/validate/LottoValidate.js";
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+  test.each([
+    [[1, 2, 3, 4, 5, "A"]],
+    [["최씨", "ABCDE"]],
+    [[null, undefined, "", "\n", "A", "B"]],
+  ])("로또 번호가 숫자가 아닌 경우", async (lotto) => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      LottoValidate.validateIsNumber(lotto);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
+  test.each([[[1, 2, 3, 4, 5]], [[1, 2, 3, 4, 5, 6, 7]]])(
+    "로또 번호의 갯수가 6개가 아닌 경우",
+    async (lotto) => {
+      expect(() => {
+        LottoValidate.validateLottoLength(lotto);
+      }).toThrow("[ERROR]");
+    }
+  );
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test.each([[[1, 2, 3, 4, 5, 5]], [[2, 2, 3, 4, 5, 6]]])(
+    "로또 번호가 중복인 경우",
+    async (lotto) => {
+      expect(() => {
+        LottoValidate.validateLottoDup(lotto);
+      }).toThrow("[ERROR]");
+    }
+  );
 });
