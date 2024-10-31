632b8eba839697a32cd0060e8fcdcd1d8fe932d1
thumbthing <ilscc0902@gmail.com>
2024-10-31 14:45:56 +0900

    Feat: 중복 숫자 검사 기능 추가

--- src/feature/validate/checkWinNumber.js
+++ src/feature/validate/checkWinNumber.js
@@ -7,7 +7,7 @@ function checkNumber(number) {
     throw new Error('[ERROR] 번호는 1 ~ 45 사이의 숫자여야 합니다.')
   }
 
-  if (Math.floor(number) !== number && Math.ceil(number) !== number) {
+  if (Math.floor(number) !== number || Math.ceil(number) !== number) {
     throw new Error('[ERROR] 번호는 양의 정수여야 합니다.')
   }
 };
@@ -16,4 +16,18 @@ function checkWinNumbers(numbers) {
   numbers.forEach(number => checkNumber(number));
 };
 
-export { checkNumber, checkWinNumbers };
+function checkDuplicate(numbers) {
+  const LOTTO_LENGTH = 6;
+  for(let index = 0; index < LOTTO_LENGTH; index++) {
+    const SINGLE_NUMBER = numbers[index];
+    const FILTERED_NUMBERS = numbers.filter((num) => {
+      num === SINGLE_NUMBER;
+    });
+
+    if (FILTERED_NUMBERS.length > 1) {
+      throw new Error('[ERROR] 중복된 숫자가 존재합니다.');
+    }
+  }
+}
+
+export { checkNumber, checkWinNumbers, checkDuplicate };
