b943abb33210df8722ad5bffbfd1fbbb6c28f906
Kim <kevin2695@naver.com>
2024-10-31 21:41:24 +0900

    refactor(error, app) : 에러 메세지들을 상수로 분리 , 에러 메세지에 상수 적용
    - 사용되는 에러 메세지들을 error.js 에 분리
    - 에러 메세지의 상수화

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+import { ERROR_MESSAGE } from "./constants/error.js";
 
 const NUMBER_REGEX = /^[0-9]*$/;
 class App {
   async run() {
     try {
       // purchase input
-      const purchaseAmount = await MissionUtils.Console.readLineAsync(
+      const purchaseAmount = await Console.readLineAsync(
         "구입금액을 입력해 주세요.\n"
       );
 
       // purchase validate
       // 1. 숫자가 아닌 경우
       if (!NUMBER_REGEX.test(purchaseAmount)) {
-        throw new Error("숫자가 아닌 문자가 입력되었습니다.");
-      }
-      // 2. 1000 단위로 나누어 떨어지지 않는 경우
-      if (Number(purchaseAmount) % 1000 !== 0) {
-        throw new Error("1000원 단위로 나누어 떨어지지 않습니다.");
+        throw new Error(ERROR_MESSAGE.PURCHASE_AMOUNT_INPUT.NOT_A_NUMBER);
       }
 
+      // 2. 1000 단위로 나누어 떨어지지 않는 경우
+      if (Number(purchaseAmount) % 1000 !== 0) {
+        throw new Error(
+          ERROR_MESSAGE.PURCHASE_AMOUNT_INPUT.NOT_DIVISION_BY_THOUSAND
+        );
+      }
+
+      const lottoCount = Number(purchaseAmount) / 1000;
+
+      Console.print(`${lottoCount}개를 구매했습니다.`);
+
+      const myLotto = Array.from({ length: lottoCount }, () => {
+        const myLottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+          (a, b) => a - b
+        );
+
+        Console.print(`[${myLottoNumber.join(", ")}]`);
+
+        return new Lotto(myLottoNumber);
+      });
+
       // lotto number input
-      const lottoNumber = await MissionUtils.Console.readLineAsync(
+      const lottoNumber = await Console.readLineAsync(
         "당첨 번호를 입력해 주세요.\n"
       );
 
       const lottoArray = new Set(lottoNumber.split(",").map(Number));
+
       // lotto validate
       // 1. 번호가 6개가 되지 않는 경우
       if (lottoArray.size != 6) {
-        throw new Error("로또 번호가 6개가 아닙니다.");
+        throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_INPUT.NOT_SIX_NUMBERS);
       }
 
       // 2. 로또 번호에 문자가 있는 경우
@@ -37,7 +57,7 @@ class App {
       );
 
       if (hasNaN) {
-        throw new Error("숫자가 아닌 문자가 입력되었습니다.");
+        throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_INPUT.NOT_A_NUMBER);
       }
 
       // 3. 로또 번호가 1 ~ 45 사이가 아닌 경우
@@ -46,17 +66,17 @@ class App {
       );
 
       if (hasOutOfRange) {
-        throw new Error("1 - 45 범위 밖의 숫자가 입력되었습니다.");
+        throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_INPUT.OUT_OF_RANGE_1_to_45);
       }
 
       // bonus number input
-      const bonusNumber = await MissionUtils.Console.readLineAsync(
+      const bonusNumber = await Console.readLineAsync(
         "보너스 번호를 입력해 주세요.\n"
       );
 
       // 1. 숫자가 아닌 문자가 입력된 경우
       if (!NUMBER_REGEX.test(bonusNumber)) {
-        throw new Error("숫자가 아닌 문자가 입력되었습니다.");
+        throw new Error(ERROR_MESSAGE.BONUS_NUMBER_INPUT.NOT_A_NUMBER);
       }
 
       // 2. 1 - 45 범위 밖의 숫자가 입력된 경우
@@ -64,12 +84,12 @@ class App {
         Number(bonusNumber) > 45 || Number(bonusNumber) < 1;
 
       if (bonusNumberOutOfRange) {
-        throw new Error("1 - 45 범위 밖의 숫자가 입력되었습니다.");
+        throw new Error(ERROR_MESSAGE.BONUS_NUMBER_INPUT.OUT_OF_RANGE_1_to_45);
       }
 
       // 3. 기존 로또 번호와 중복되는 경우
       if (lottoArray.has(Number(bonusNumber))) {
-        throw new Error("기존 번호와 중복됩니다.");
+        throw new Error(ERROR_MESSAGE.BONUS_NUMBER_INPUT.DUPLICATED_NUMBER);
       }
     } catch (error) {
       console.log(`[ERROR] ${error.message}`);
---
+++ src/constants/error.js
@@ -0,0 +1,18 @@
+export const ERROR_MESSAGE = {
+  PURCHASE_AMOUNT_INPUT: {
+    NOT_A_NUMBER: "구입 금액 : 숫자가 아닌 문자가 입력되었습니다.",
+    NOT_DIVISION_BY_THOUSAND:
+      "구입 금액 : 1000원 단위로 나누어 떨어지지 않습니다.",
+  },
+  LOTTO_NUMBER_INPUT: {
+    NOT_SIX_NUMBERS: "로또 번호 : 로또 번호가 6개가 아닙니다.",
+    NOT_A_NUMBER: "로또 번호 : 숫자가 아닌 문자가 입력되었습니다.",
+    OUT_OF_RANGE_1_to_45: "로또 번호 : 1 - 45 범위 밖의 숫자가 입력되었습니다.",
+  },
+  BONUS_NUMBER_INPUT: {
+    NOT_A_NUMBER: "보너스 번호 : 숫자가 아닌 문자가 입력되었습니다.",
+    OUT_OF_RANGE_1_to_45:
+      "보너스 번호 : 1 - 45 범위 밖의 숫자가 입력되었습니다.",
+    DUPLICATED_NUMBER: "보너스 번호 : 기존 번호와 중복됩니다.",
+  },
+};
