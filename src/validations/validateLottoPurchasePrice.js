55dc1abe156244f2e959a14cc4cc82e4ae908dbf
sooyeoniya <tndus1306403@naver.com>
2024-11-01 01:48:15 +0900

    refactor(validations): 로또 구입 금액 검증 함수 내부 상수화

--- src/validations/validateLottoPurchasePrice.js
+++ src/validations/validateLottoPurchasePrice.js
@@ -1,4 +1,4 @@
-import { VALIDATE_MESSAGES } from '../constants/constants.js';
+import { VALIDATE_MESSAGES, LOTTO_PRICE_PER_TICKET, MATCH_PRICE, MATCH_CODE } from '../constants/constants.js';
 
 const validateNumber = (price) => {
   if (isNaN(price)) {
@@ -13,13 +13,13 @@ const validateInteger = (price) => {
 };
 
 const validateLottoAmountRange = (price) => {
-  if (price < 1_000 || price > 2_000_000_000) {
+  if (price < LOTTO_PRICE_PER_TICKET || price > MATCH_PRICE[MATCH_CODE.SIX]) {
     throw new Error(VALIDATE_MESSAGES.PURCHASE_PRICE.RANGE);
   }
 };
 
 const validateIsThousandUnit = (price) => {
-  if (price % 1_000 !== 0) {
+  if (price % LOTTO_PRICE_PER_TICKET !== 0) {
     throw new Error(VALIDATE_MESSAGES.PURCHASE_PRICE.THOUSAND);
   }
 };
