a55c0f557a71f5d1a4aa335479fcaeda3ecb6feb
Doyeon <duseh729@naver.com>
2024-10-31 20:51:53 +0900

    feat: Add create lotto number >> 입력 받은 금액만큼 로또 발행

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
     const amount = Number(await Console.readLineAsync("구입금액을 입력해 주세요.\n"))
 
+    const possibleLottoCount = Number(amount/1000);
+
+    const lottoArray = this.createLottoNumber(possibleLottoCount);
+
+    console.log(lottoArray);
+  }
+
+  createLottoNumber(count){
+    const lottoArray = [];
+
+    for(let i=0; i<count; i++){
+      const lotto = Random.pickUniqueNumbersInRange(1, 45, 6);
+      lotto.sort((a, b)=> a-b)
+      lottoArray.push(lotto);
+    }
+
+    return lottoArray;
   }
 }
 
