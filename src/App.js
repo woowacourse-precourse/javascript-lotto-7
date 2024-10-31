b52d5b8a2e74f4021fc566185341e6f752f31be8
yubin <yb3143@gmail.com>
2024-10-31 14:17:15 +0900

    feat: 구입 금액, 구매 개수, 당첨번호, 보너스번호 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let price = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+
+    if (price < 1000) {
+      Console.print("[ERROR] 1000원 이상의 금액을 입력해 주세요.");
+      return;
+    }
+
+    let count = Math.floor(price / 1000);
+
+    Console.print(`${count}개를 구매했습니다.`);
+    let lottoNumbers = this.getLottoNumbers(count);
+    Console.print(lottoNumbers);
+
+    let winningNumbers = await Console.readLineAsync(
+      "당첨 번호를 입력해 주세요.\n"
+    );
+
+    let bonusNumber = await Console.readLineAsync(
+      "보너스 번호를 입력해 주세요.\n"
+    );
+
+    let result = this.compareNumbers(lottoNumbers, winningNumbers, bonusNumber);
+  }
+
+  getLottoNumbers(num) {
+    let lottoNumbers = [];
+    for (let i = 0; i < num; i++) {
+      let numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoNumbers.push(numbers);
+    }
+    return lottoNumbers;
+  }
+
+  compareNumbers(lottoNumbers, winningNumbers, bonusNumber) {
+    let result = [];
+    for (let i = 0; i < lottoNumbers.length; i++) {
+      let count = 0;
+      for (let j = 0; j < lottoNumbers[i].length; j++) {
+        if (winningNumbers.includes(lottoNumbers[i][j])) {
+          count++;
+        }
+      }
+      result.push(count);
+    }
+  }
 }
 
 export default App;
