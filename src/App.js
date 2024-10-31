dc0f6ff9b966940f0620817821df75b6f3a884dd
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-31 21:10:26 +0900

    feat(app): 로또 및 보너스 번호 입력 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
 import Generator from './Generator.js';
 import InputProcessor from './InputProcessor.js';
 import Validator from './Validator.js';
+import LOTTO from './constants/lotto.js';
 import MESSAGES from './constants/messages.js';
+import handleError from './utils/handleError.js';
 
 class App {
-  PRICE_UNIT = 1000;
-
   #purchasePrice;
 
   #lottos;
 
+  #winningNumbers;
+
+  #bonusNumber;
+
   async run() {
-    this.#purchasePrice = await this.#getPurchasePrice();
-    Validator.price(this.#purchasePrice);
-    const quantity = this.#getPurchaseQuantity();
-    const lottoGenerator = new Generator(quantity);
-    const lottos = lottoGenerator.execute();
-    // TODO: 당첨 번호와 보너스 번호
+    try {
+      this.#purchasePrice = await this.#getPurchasePrice();
+      const quantity = this.#getPurchaseQuantity();
+      const lottoGenerator = new Generator(quantity);
+      const lottos = lottoGenerator.execute();
+      this.winningNumbers = await this.#getWinningNumbers();
+      this.#bonusNumber = await this.#getBonusNumber();
+    } catch (error) {
+      handleError(true, error);
+    }
   }
 
   async #getPurchasePrice() {
     const input = await InputProcessor.get(MESSAGES.PRICE_INPUT);
+    Validator.price(input);
     return input;
   }
 
   #getPurchaseQuantity() {
-    const quantity = this.#purchasePrice / this.PRICE_UNIT;
+    const quantity = this.#purchasePrice / LOTTO.PRICE_UNIT;
     return quantity;
+  }
+
+  async #getWinningNumbers() {
+    const input = await InputProcessor.get(MESSAGES.WINNING_NUMBER_INPUT);
+    this.winningNumbers = input.split(',');
+    Validator.winningNumbers(this.winningNumbers);
+    return input;
+  }
+
+  async #getBonusNumber() {
+    const input = await InputProcessor.get(MESSAGES.BONUS_NUMBER_INPUT);
+    Validator.bonusNumber(this.winningNumbers, input);
+    return input;
   }
 }
 
