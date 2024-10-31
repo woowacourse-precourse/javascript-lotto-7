ca20bd2e2868c059c28070e5dd65747a262326ef
minji2219 <200146@jnu.ac.kr>
2024-10-31 23:49:45 +0900

    feat: 전체 로직 run() 함수에서 실행
    작성한 함수를 이용하여 전체 로직을 실행한다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Lotto from './Lotto.js';
+import { getLottoAmount } from './Lottos/getLottoAmount.js';
+import { getLottos } from './Lottos/getLottos.js';
+import { splitLottoNumber } from './Lottos/splitLottoNumber.js';
 import { scan } from './utils/scanner.js';
+import { getWin } from './WinLotto/getWin.js';
+import { printResult } from './WinLotto/getStatistics.js';
 
 class App {
   async run() {
-    const price = scan('구입금액을 입력해 주세요.');
+    const price = await scan('구입금액을 입력해 주세요.');
+    const lottos = getLottos(getLottoAmount(price));
+
+    const lottoString = await scan('\n당첨 번호를 입력해 주세요.');
+    const lotto = splitLottoNumber(lottoString);
+    new Lotto(lotto);
+
+    //TODO: 보너스 번호도 중복되지 않은지, 숫자인지 등 유효성 체크 필요
+    const bonusNumber = await scan('\n보너스 번호를 입력해 주세요.');
+
+    const gradeArr = getWin(lottos, lotto, Number(bonusNumber));
+    printResult(gradeArr, price);
   }
 }
 
