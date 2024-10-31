56fec67da67eb40de42f8cf7bde9cd427bdc34aa
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-31 21:08:01 +0900

    feat(validator): 로또 및 보너스 번호 검증용 정적 메서드 추가
    - 관련 에러 메시지 추가

---
+++ src/constants/lotto.js
@@ -0,0 +1,7 @@
+const LOTTO = {
+  PRICE_UNIT: 1000,
+  LENGTH_WINNING_NUMBER: 6,
+  LENGTH_BONUS_NUMBER: 1
+};
+
+export default LOTTO;
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,7 @@
 const PROMPT = {
-  PRICE_INPUT: '구매금액을 입력해 주세요.\n'
+  PRICE_INPUT: '구매금액을 입력해 주세요.\n',
+  WINNING_NUMBER_INPUT: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER_INPUT: '\n보너스 번호를 입력해 주세요.\n'
 };
 
 export default PROMPT;
