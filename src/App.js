2a61e4f894d5f5a5ecd501e0ddfda66e896649b9
Doyeon <duseh729@naver.com>
2024-10-31 21:14:59 +0900

    feat: Add winning lotto input handler 당첨 번호 입력 받기 및 전처리 구현

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ class App {
     const amount = Number(await Console.readLineAsync("구입금액을 입력해 주세요.\n"))
 
     const possibleLottoCount = Number(amount/1000);
+    Console.print(`\n${possibleLottoCount}개를 구매했습니다.`)
 
     const lottoArray = this.createLotto(possibleLottoCount);
-
     this.displayLotto(lottoArray);
+
+    const inputWinningLotto = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    const winningLotto = this.createWinningLotto(inputWinningLotto);
   }
 
   createLotto(count){
@@ -31,6 +34,10 @@ class App {
     for(let i of lottoArray){
       Console.print(i.getLotto());
     }
+  }
+
+  createWinningLotto(lottoNumbers){
+    return lottoNumbers.split(",").map(lotto => Number(lotto));
   }
 }
 
