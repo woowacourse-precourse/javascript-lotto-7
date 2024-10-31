0575702a60802094f30def792bf762b533041bce
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-10-31 20:14:33 +0900

    feat: 로또 구입 금액 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+const GUIDE_MESSAGE_INPUT_COST = "구입금액을 입력해 주세요.\n";
+const ERROR_MESSAGE_INVALID_COST =
+  "[ERROR] 입력하신 금액이 1,000원 단위가 아닙니다. 다시";
+
 class App {
-  async run() {}
+  async run() {
+    let isValid = false;
+    let lottoCost = 0;
+
+    while (!isValid) {
+      try {
+        lottoCost = Number(
+          await MissionUtils.Console.readLineAsync(GUIDE_MESSAGE_INPUT_COST)
+        );
+        if (lottoCost % 1000 !== 0) {
+          throw new Error(ERROR_MESSAGE_INVALID_COST);
+        }
+
+        isValid = true;
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  }
 }
 
 export default App;
