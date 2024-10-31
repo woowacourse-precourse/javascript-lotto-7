04f4cbe3e4da8fa58e348b44241461775790bdeb
@u_limit_ <yurim@ajou.ac.kr>
2024-10-31 17:41:54 +0900

    feat: 로또 구입시 1000원으로 나눠 떨어지지 않을 때 에러 핸들링

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import {Console} from '@woowacourse/mission-utils'
+import { Console } from '@woowacourse/mission-utils'
+import { CONSOLE_MESSAGES } from "./constant.js";
+
+
 class App {
   async run() {
-    const purchasePrice = await Console.readLineAsync('구입 금액을 입력해 주세요.\n');
+    const purchasePrice = await Console.readLineAsync(CONSOLE_MESSAGES.buyPrice);
+    const divideInto1000 = purchasePrice % 1000;
+    if((divideInto1000).length!==0){
+      throw new Error('[ERROR]');
+    }
   }
 }
 
---
+++ src/constant.js
@@ -0,0 +1,4 @@
+export const CONSOLE_MESSAGES = {
+  buyPrice: '구입금액을 입력해 주세요.\n',
+
+}
