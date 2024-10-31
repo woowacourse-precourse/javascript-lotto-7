2bd149f7d9fbe7d2f1fcaa9fe3f815604ea9c142
Yun-Jinwoo <yjw3480@naver.com>
2024-11-01 00:38:10 +0900

    ✨ feat(MakeLotto.js): 로또 번호 오름차순 정렬 기능 추가

--- src/MakeLotto.js
+++ src/MakeLotto.js
@@ -1,7 +1,7 @@
 import Lotto from "./Lotto.js"
 
 const makeLotto = (lottoNum) => {
-    const newLotto = new Lotto(lottoNum); // lottoNum을 토대로 새 객체 생성
+    const newLotto = new Lotto(lottoNum.sort((a, b) => a - b)); // lottoNum을 토대로 새 객체 생성
     return newLotto;
 }
 
