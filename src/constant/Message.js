919e07b2ed14c66938543683f377eff3da2d1b30
최영빈 <bin778@naver.com>
2024-11-01 03:13:47 +0900

    feat(message): 게임 상금 메시지 추가

--- src/constant/Message.js
+++ src/constant/Message.js
@@ -4,4 +4,17 @@ export const MESSAGE = Object.freeze({
     WINNING_LOTTO: "\n당첨 번호를 입력해 주세요.\n",
     BONUS_NUMBER: "\n보너스 번호를 입력해 주세요.\n",
   }),
+  OUTPUT: Object.freeze({
+    PURCAHSE_LOTTO: "개를 구매했습니다.",
+    RESULT_HEADER: "\n당첨 통계\n---",
+    RESULT: Object.freeze({
+      FIRST: "6개 일치 (2,000,000,000원)",
+      SECOND: "5개 일치, 보너스 볼 일치 (30,000,000원)",
+      THIRD: "5개 일치 (1,500,000원)",
+      FOURTH: "4개 일치 (50,000원)",
+      FIFTH: "3개 일치 (5,000원)",
+    }),
+    RESULT_PROFIT_RATE: "총 수익률은 ",
+    RESULT_FOOTER: "입니다.",
+  }),
 });
