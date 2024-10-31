d538b85d9f903462a7b83eeabdab6dcd1e5dda7a
bunju20 <ghkd2271@naver.com>
2024-10-31 18:10:26 +0900

    feat: 로또 구입 금액 입력받는 로직 구현 - getCashInHand함수로 구입금액 입력받는 로직 구현 - cashValidation으로 입력값 검증하는 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+
+async function getCashInHand(){
+const message =
+      '구입금액을 입력해 주세요.\n';
+    const cash = await MissionUtils.Console.readLineAsync(message);
+    return cash;
+}
+
+
+function cashValidation(string){
+  const number = parseFloat(string);
+  if(isNaN(number))return false;
+  const isNotDivided = (number % 1000) !== 0;
+  if(isNotDivided) return false;
+  return true;
+}
+
+/**
+ * 
+ * @param {*} lottos
+ */
+function printLottoPurchase(lottos){
+  const lottosNumber = lottos.length;
+  MissionUtils.Console.print(`${lottosNumber}개를 구매했습니다.
+`);
+
+}
+
 class App {
-  async run() {}
+  async run() {
+    const cash = await getCashInHand();
+    cashValidation(cash);
+
+  }
 }
 
 export default App;
