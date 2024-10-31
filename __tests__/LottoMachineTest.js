9696c9f9661e909aacd94fc5305a49d20f5d4fc4
jeongeunLee <leeju1009@gmail.com>
2024-10-31 18:15:18 +0900

    test: 로또를 생성하는 기능이 로또 인스턴스 배열을 반환하는지 테스트 추가

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,7 +1,14 @@
+import { Random } from '@woowacourse/mission-utils';
 import LottoMachine from '../src/LottoMachine';
 import Lotto from '../src/Lotto';
 
 describe('LottoMachine 클래스 테스트', () => {
+  const mockingRandomNumbers = (numbers) => {
+    Random.pickUniqueNumbersInRange = jest
+      .fn()
+      .mockImplementation(() => numbers);
+  };
+
   test.each([
     [1000, 1],
     [2000, 2],
@@ -13,5 +20,20 @@ describe('LottoMachine 클래스 테스트', () => {
     const amount = lottoMachine.getAmount();
 
     expect(amount).toBe(expected);
+  });
+
+  test('로또를 발급하는지 테스트한다.', () => {
+    const PAYMENT = 1000;
+    const NUMBERS = [1, 2, 3, 4, 5, 6];
+    const lottoMachine = new LottoMachine(PAYMENT, Lotto);
+
+    mockingRandomNumbers(NUMBERS);
+
+    const lottos = lottoMachine.getLottos();
+
+    lottos.forEach((lotto) => {
+      expect(lotto).toBeInstanceOf(Lotto);
+      expect(lotto.getNumbers()).toEqual(expect.arrayContaining(NUMBERS));
+    });
   });
 });
