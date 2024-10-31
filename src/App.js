4427738fe5002e5c599bebef9c5bb57fe2c30882
j0n0m2 <dbsk8223@naver.com>
2024-11-01 03:06:26 +0900

    사용자로부터 로또 구입 금액을 입력 받는다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { Console } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
+import { LOTTO_AMOUNT_UNIT,LOTTO_NUM_RANGE,NUMBER_OF_BALLS } from './constant/constant.js'
+
 class App {
-  async run() {}
+  async run(){
+
+    let lottoAmount;
+
+    while (true) {
+      try {
+        lottoAmount = await this.promptLottoAmount();
+        break
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+
+  }
+
+  async promptLottoAmount() {
+    const lottoAmount = Number(await Console.readLineAsync('구입금액을 입력해 주세요.\n'));
+    const validator = new LottoAmountValidator();
+    validator.validateMinAmount(lottoAmount);
+    validator.validateUnitAmount(lottoAmount);
+    return lottoAmount;
+  }
+
+}
+
+class LottoAmountValidator {
+  validateMinAmount(amount) {
+    if (amount < LOTTO_AMOUNT_UNIT) {
+      throw new Error('\n[ERROR] 1,000원부터 입력할 수 있습니다.');
+    }
+  }
+  validateUnitAmount(amount) {
+    if (!Number.isInteger(amount / LOTTO_AMOUNT_UNIT)) {
+      throw new Error('\n[ERROR] 1,000원 단위만 입력할 수 있습니다.');
+    }
+  }
 }
 
 export default App;
