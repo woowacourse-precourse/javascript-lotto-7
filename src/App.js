8c7ca1df70781f05dce6e64ce4dc3bd2f9b16e00
hoyyChoi <hoyeon8165@gmail.com>
2024-10-31 19:36:35 +0900

    feat(processLottoCount): 구입금액 바탕 로또 갯수 가공

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    Console.print(this.processLottoCount(purchaseAmount));
+  }
+
+  //로또 갯수 가공
+  processLottoCount(purchaseAmount) {
+    if (purchaseAmount > 0 && purchaseAmount % 1000 !== 0)
+      return "[ERROR] 1000원 단위로 입력해 주세요.";
+
+    return purchaseAmount / 1000;
+  }
 }
 
 export default App;
