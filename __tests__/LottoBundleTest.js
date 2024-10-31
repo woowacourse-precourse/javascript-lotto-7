a4bdfb09e159f07059245192ee2b67276bd1d919
Minje Sung <minje813@dgu.ac.kr>
2024-10-31 15:20:52 +0900

    test(Model): LottoBundle 클래스에 대한 테스트 작성
    로또묶음이 로또 배열을 잘 가지고 있는지 확인하는 테스트 작성

---
+++ __tests__/LottoBundleTest.js
@@ -0,0 +1,19 @@
+import Lotto from '../src/Model/Lotto';
+import LottoBundle from '../src/Model/LottoBundle';
+
+// - 3. 로또 여러장 발행하기
+
+//   - 로또 개수를 입력받으면 그만큼의 로또를 발급
+
+describe('로또묶음 클래스 테스트', () => {
+  const lottoBundle = new LottoBundle(3);
+  test('로또를 발급할 개수를 입력하면 그만큼의 로또를 발급한다.', () => {
+    expect(lottoBundle.getLottoList().length).toBe(3);
+  });
+
+  test('로또묶음엔 로또가 있어야 한다.', () => {
+    expect(
+      lottoBundle.getLottoList().some((lotto) => !(lotto instanceof Lotto))
+    ).toBe(false);
+  });
+});
