ed007074562ae5856213d970ba980918af927da5
magik99 <hojun0810@naver.com>
2024-11-01 05:27:12 +0900

    fix(Print.js): prizedNum.get 키값 수정
    숫자로 되어있던 키값 오타 문자열로 수정

--- src/Print.js
+++ src/Print.js
@@ -13,13 +13,13 @@ class Print {
   static printStats(prizedNum, earningRate) {
     Console.print('당첨 통계');
     Console.print('---');
-    Console.print(`3개 일치 (${PRIZES[5]}원) - ${prizedNum.get(5)}개`);
-    Console.print(`4개 일치 (${PRIZES[4]}원) - ${prizedNum.get(4)}개`);
-    Console.print(`5개 일치 (${PRIZES[3]}원) - ${prizedNum.get(3)}개`);
+    Console.print(`3개 일치 (${PRIZES[5]}원) - ${prizedNum.get('5')}개`);
+    Console.print(`4개 일치 (${PRIZES[4]}원) - ${prizedNum.get('4')}개`);
+    Console.print(`5개 일치 (${PRIZES[3]}원) - ${prizedNum.get('3')}개`);
     Console.print(
-      `5개 일치, 보너스 볼 일치 (${PRIZES[2]}원) - ${prizedNum.get(2)}개`,
+      `5개 일치, 보너스 볼 일치 (${PRIZES[2]}원) - ${prizedNum.get('2')}개`,
     );
-    Console.print(`6개 일치 (${PRIZES[1]}원) - ${prizedNum.get(1)}개`);
+    Console.print(`6개 일치 (${PRIZES[1]}원) - ${prizedNum.get('1')}개`);
     Console.print(`총 수익률은 ${earningRate}%입니다.`);
   }
 }
