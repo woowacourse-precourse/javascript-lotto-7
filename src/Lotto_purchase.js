90abe4287c5573aa35f42176cf36118585088066
Jang Eunhye <hh961130@naver.com>
2024-10-31 17:51:29 +0900

    feat: (로또 구매)- 구입 금액에 따른 로또 개수 계산

--- src/Lotto_purchase.js
+++ src/Lotto_purchase.js
@@ -1,13 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class Lotto_purchase {
   constructor(payment) {
     this.validate(payment);
+    let lottoQuantity = this.countQuantity(payment);
   }
-
   validate(payment) {
     if (payment % 1000 !== 0) {
       throw new Error("[ERROR] 구입 금액은 1000원 단위로 가능합니다.");
     }
   }
+  countQuantity(payment) {
+    const price = 1000;
+    let lottoQuantity = payment / price;
+    return lottoQuantity;
+  }
 }
-
 export default Lotto_purchase;
