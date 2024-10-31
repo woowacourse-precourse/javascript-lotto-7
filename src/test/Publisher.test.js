712b69d5f4cb148beec04cdea3f0549769951a38
jongbin26 <010jjj@naver.com>
2024-10-31 16:07:49 +0900

    test(Publisher.test): Publisher 구입 금액 검증 테스트 코드 작성
    - 구입 금액 성공 테스트 코드 작성
    - 구입 금액 실패 테스트 코드 작성
        1. 구입 금액이 입력되었는지
        2. 구입 금액이 정수인지
        3. 구입 금액이 1,000원 이상인지
        4. 구입 금액이 1,000원 단위인지

---
+++ src/test/Publisher.test.js
@@ -0,0 +1,19 @@
+import { Publisher } from '../Publisher.js';
+
+test.each([
+  ['8000', 8],
+  ['12000', 12],
+])('구입 금액 검증 성공', (purchaseAmount, result) => {
+  const publisher = new Publisher(purchaseAmount);
+  expect(publisher.purchaseQuantity).toBe(result);
+});
+
+test.each([
+  ['8100', '[ERROR] 구입 금액은 1,000원 단위여야 합니다.'],
+  ['0', '[ERROR] 구입 금액은 1,000원 이상이어야 합니다.'],
+  ['1.1', '[ERROR] 구입 금액은 정수이어야 합니다.'],
+  ['-10', '[ERROR] 구입 금액은 1,000원 이상이어야 합니다.'],
+  ['', '[ERROR] 구입 금액이 입력되지 않았습니다.'],
+])('구입 금액 검증 실패', (purchaseAmount) => {
+  expect(() => new Publisher(purchaseAmount)).toThrow('[ERROR]');
+});
