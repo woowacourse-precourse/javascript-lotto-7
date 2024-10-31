7533bb8fd377cd578ac4420aa0c88b652c1826e4
hoyyChoi <hoyeon8165@gmail.com>
2024-10-31 19:46:51 +0900

    feat(printLottoNumbers & generateLottoNumbers): 로또 번호 생성 및 출력

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
+  PurchaseLottoNumbers = [];
+
   async run() {
     const purchaseAmount = await Console.readLineAsync(
       "구입금액을 입력해 주세요.\n"
     );
-    Console.print(this.processLottoCount(purchaseAmount));
+    this.printLottoNumbers(this.processLottoCount(purchaseAmount));
+    Console.print(this.PurchaseLottoNumbers);
   }
 
   //로또 갯수 가공
@@ -14,6 +17,22 @@ class App {
       return "[ERROR] 1000원 단위로 입력해 주세요.";
 
     return purchaseAmount / 1000;
+  }
+
+  //로또 번호 출력
+  printLottoNumbers(lottoCount) {
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+    while (lottoCount > 0) {
+      let lottoNumbers = this.generateLottoNumbers();
+      Console.print(lottoNumbers);
+      this.PurchaseLottoNumbers.push(lottoNumbers);
+      lottoCount--;
+    }
+  }
+
+  // random 로또 번호 생성 + 오름차순 정렬
+  generateLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
   }
 }
 
