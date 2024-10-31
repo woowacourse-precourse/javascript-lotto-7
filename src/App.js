f8d632c05741db39d4ef82ae3596ae0c82658693
songhj10207@naver.com <songhj10207@naver.com>
2024-10-31 17:35:10 +0900

    feat: 로또 번호가 생성되는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoSetting from './LottoSetting.js';
+
 class App {
-  async run() {}
+  async run() {
+    const lottoSetting = new LottoSetting();
+    await lottoSetting.settingLotto();
+  }
 }
 
 export default App;
--- src/LottoSetting.js
+++ src/LottoSetting.js
@@ -1,14 +1,36 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGES, AMOUNT } from './constants.js';
 
 class LottoSetting {
   #numberOfLotto;
 
+  constructor() {
+    this.lottos = [];
+  }
+
   async #getPurchaseAmount() {
-    return await Console.readLineAsync('구입 금액을 입력해주세요.\n');
+    return await Console.readLineAsync(INPUT_MESSAGES.lottoAmountInput + '\n');
+  }
+
+  #countNumberOfLotto(amount) {
+    this.#numberOfLotto = amount / AMOUNT.lottoAmount;
+  }
+
+  #ascendinglottoNumber(lotto) {
+    return lotto.sort((a, b) => a - b);
+  }
+
+  #createRandomLottos() {
+    for (let i = 0; i < this.#numberOfLotto; i++) {
+      const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.lottos.push(this.#ascendinglottoNumber(lotto));
+    }
   }
 
   async settingLotto() {
     const amount = await this.#getPurchaseAmount();
+    this.#countNumberOfLotto(amount);
+    this.#createRandomLottos();
   }
 }
 
