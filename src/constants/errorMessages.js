c046083bf6c1e50f532762a6d5d0811b0b96e8d3
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-31 15:17:34 +0900

    fix(errorMessages.js): 에러메세지 수정및 추가

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -8,10 +8,7 @@ export const ERROR_MESSAGE = Object.freeze({
     `[ERROR] ${min}~${max} 사이의 번호만 입력할 수 있습니다.`,
   INVALID_PURCHASE_AMOUNT: (price) =>
     `[ERROR] 구입 금액은 ${price}원 단위로 입력해 주세요.`,
-  INVALID_WINNING_NUMBERS: (length) =>
-    `[ERROR] 당첨 번호는 ${length}개여야 합니다.`,
-  INVALID_BONUS_NUMBER: (min, max) =>
-    `[ERROR] 보너스 번호는 ${min}부터 ${max} 사이의 숫자여야 합니다.`,
   DUPLICATE_BONUS_NUMBER:
     '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
+  NO_BLANK: '[ERROR] 중간에 숫자가 비어있습니다. 숫자를 모두 입력해주세요.',
 });
