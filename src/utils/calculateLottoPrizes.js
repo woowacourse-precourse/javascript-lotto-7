400f61a7c9daddd90338036f8310a27b9aa6d80d
HS <hasang2691@naver.com>
2024-10-31 18:01:26 +0900

    feat(calculateLottoPrizes.js): 당첨금 계산 기능 구현
    - calculateLottoPrizes.js 파일 생성
      - calculateTotalPrize 메서드 구현

---
+++ src/utils/calculateLottoPrizes.js
@@ -0,0 +1,13 @@
+import { LOTTO_PRIZES } from '../constants/lottoPrize.js';
+
+export const calculateTotalPrize = (statistics) => {
+  let totalPrize = 0;
+
+  totalPrize += statistics[3].count * LOTTO_PRIZES[3].prize;
+  totalPrize += statistics[4].count * LOTTO_PRIZES[4].prize;
+  totalPrize += statistics[5].count * LOTTO_PRIZES[5].prize;
+  totalPrize += statistics['5+bonus'].count * LOTTO_PRIZES['5+bonus'].prize;
+  totalPrize += statistics[6].count * LOTTO_PRIZES[6].prize;
+
+  return totalPrize;
+};
