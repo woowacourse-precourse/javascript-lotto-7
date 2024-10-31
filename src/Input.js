bca69207336817c44e6816420ad186de2a2af0ed
murphybread <nargene@gmail.com>
2024-10-31 18:06:32 +0900

    refactor(inout): remove console print

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,6 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class UserInput {
     async getUserPaidMoney() {
         const paidMoney = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요\n");
-        MissionUtils.Console.print(paidMoney);
         return paidMoney;
     }
 
