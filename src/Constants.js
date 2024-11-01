91b03a9fbdb137d1ab9f852c8dbc5850d142b4a0
zzz664 <zzz664>
2024-11-02 02:19:32 +0900

    feat(VerifyingLotto, Constants): 로또 당첨 내역 출력 기능 구현
    발행 된 로또 리스드의 당첨 내역을 검증하기 위한 VerifyingLotto클래스를 추가함
    로또 리스트 -> 로또 -> 당첨 번호와 매칭, 보너스 번호 매칭 여부의 과정으로 진행함
    Constants.js에는 당첨 금액 및 당첨 내역 출력에 필요한 텍스트를 상수로 추가함
    로또 검증이 끝난 후 내역을 기록하는 saveWinningHistory는 번호 매칭 수에 따라
    분기를 작성하여 winningHistory의 수를 증가하는 방향으로 구현함
    다음 로또의 검증을 할때 보너스 번호 매칭여부를 false로 재설정함

--- src/Constants.js
+++ src/Constants.js
@@ -21,4 +21,21 @@ const ERROR_TEXTS = Object.freeze({
   NOT_A_DUPLICATION_BONUS_NUMBER : '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.\n',
 });
 
-export { INPUT_TEXTS, ERROR_TEXTS };
+const PRIZE_MONEYS = Object.freeze({
+  FIRST : 2000000000,
+  SECOND : 30000000,
+  THIRD : 1500000,
+  FOURTH : 50000,
+  FIFTH : 5000,
+});
+
+const WINNING_HISTORY = Object.freeze({
+  FIRST : '6개 일치 (2,000,000,000원) - ',
+  SECOND : '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  THIRD : '5개 일치 (1,500,000원) - ',
+  FOURTH : '4개 일치 (50,000원) - ',
+  FIFTH : '3개 일치 (5,000원) - ',
+  TITLE : '\n당첨 통계\n---',
+});
+
+export { INPUT_TEXTS, ERROR_TEXTS, PRIZE_MONEYS, WINNING_HISTORY };
