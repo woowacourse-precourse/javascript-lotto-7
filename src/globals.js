0582d97ed711e90bbfca2ffb167941b76bfac3d1
Jiny <moc24@naver.com>
2024-11-01 23:51:36 +0900

    feat(globlas.js) : 당첨 내역 리스트 객체배열 추가

---
+++ src/globals.js
@@ -0,0 +1,27 @@
+export let LottoPrizeList = [
+  {
+    name: '3개 일치',
+    count: 0,
+    value: 5000,
+  },
+  {
+    name: '4개 일치',
+    count: 0,
+    value: 50000,
+  },
+  {
+    name: '5개 일치',
+    count: 0,
+    value: 1500000,
+  },
+  {
+    name: '5개 일치, 보너스 볼 일치',
+    count: 0,
+    value: 30000000,
+  },
+  {
+    name: '6개 일치',
+    count: 0,
+    value: 2000000000,
+  },
+];
