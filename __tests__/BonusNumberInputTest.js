97757e37196de722eb33074dda42f61b19cb3e20
Minje Sung <minje813@dgu.ac.kr>
2024-11-01 18:54:15 +0900

    test(Views): BounsNumberInput 함수 객체에 대한 테스트 작성

--- __tests__/BonusNumberInputTest.js
+++ __tests__/BonusNumberInputTest.js
@@ -1,32 +1,35 @@
-import BuyPriceInput from '../src/Views/BuyPriceInput';
+import BonusNumberInput from '../src/Views/BonusNumberInput';
 
 // - 8. 당첨 번호와 보너스 번호 입력
 
 //   - 보너스 번호를 입력했을 때 당첨 번호와 중복인지 확인 및 예외처리
-//   -
+//   - ➕ 보너스 번호를 입력하지 않을 경우 확인 및 예외처리
+//   - ➕ 보너스 번호로 문자를 입력한 않은 경우 확인 및 예외처리
+//   - ➕ 보너스 번호로 1~45 사이의 숫자를 입력하지 않은 경우 확인 및 예외처리
 
-describe('구입 금액 입력 Validate 테스트', () => {
-  test('금액을 입력하지 않으면 다시 입력을 받아야 합니다.', () => {
-    expect(BuyPriceInput.validate('')).toEqual(false);
+describe('보너스 번호 입력 Validate 테스트', () => {
+  const basicNumbers = [1, 2, 3, 4, 5, 6];
+  test('보너스 번호를 입력하지 않으면 다시 입력을 받아야 합니다.', () => {
+    expect(BonusNumberInput.validate('', basicNumbers)).toEqual(false);
   });
 
-  test('금액으로 숫자를 입력하지 않으면 다시 입력을 받아야 합니다.', () => {
-    expect(BuyPriceInput.validate('MinSungJe')).toEqual(false);
+  test('보너스 번호로 숫자를 입력하지 않으면 다시 입력을 받아야 합니다.', () => {
+    expect(BonusNumberInput.validate('MinSungJe', basicNumbers)).toEqual(false);
   });
 
-  test('금액으로 음수를 입력하면 다시 입력을 받아야 합니다.', () => {
-    expect(BuyPriceInput.validate('-1')).toEqual(false);
+  test('보너스 번호로 1보다 작은 수를 입력하면 다시 입력을 받아야 합니다.', () => {
+    expect(BonusNumberInput.validate('0', basicNumbers)).toEqual(false);
   });
 
-  test('금액으로 너무 큰 수를 입력하면 다시 입력을 받아야 합니다.', () => {
-    expect(BuyPriceInput.validate('10000000000000000')).toEqual(false);
+  test('보너스 번호로 45보다 큰 수를 입력하면 다시 입력을 받아야 합니다.', () => {
+    expect(BonusNumberInput.validate('800', basicNumbers)).toEqual(false);
   });
 
-  test('금액으로 단위에 떨어지지 않는 값을 입력하면 다시 입력을 받아야 합니다.', () => {
-    expect(BuyPriceInput.validate('8130')).toEqual(false);
+  test('보너스 번호가 당첨 번호와 중복되면 다시 입력을 받아야 합니다.', () => {
+    expect(BonusNumberInput.validate('1', basicNumbers)).toEqual(false);
   });
 
-  test('정상 금액을 입력하면, 통과가 됩니다.', () => {
-    expect(BuyPriceInput.validate('7000')).toEqual(true);
+  test('정상적으로 보너스 번호를 입력하면, 통과가 됩니다.', () => {
+    expect(BonusNumberInput.validate('7', basicNumbers)).toEqual(true);
   });
 });
