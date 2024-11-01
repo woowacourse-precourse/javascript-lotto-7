fe3361a8daae7cd28fd749bf92184fa00e7adf35
Kimyerim <yeye092302@gmail.com>
2024-11-02 02:24:56 +0900

    fix: 에러 메시지 추가
    - 입력 시 발생할 수 있는 에러 상황에 대한 메시지를 추가.
    - 입력받은 구입 금액이 1,000원으로 나누어지지 않을 때, 숫자가 아닐 때, 양수가 아닐 때의 에러 메시지 추가

--- src/message.js
+++ src/message.js
@@ -4,3 +4,16 @@ export const COMMON_MESSAGE = {
     INPUT_BONUS_NUM: Object.freeze("보너스 번호를 입력해 주세요."),
     WINNING_STATS: Object.freeze("당첨 통계\n---"),
 }
+
+const DEFAULT_ERROR_MESSAGE = "[ERROR]";
+export const ERROR_MESSAGE = {
+    PURCHASE_AMOUNT_NOT_DIVISIBLE_THOUSAND: Object.freeze(
+        `${DEFAULT_ERROR_MESSAGE} 구입 금액이 1,000원 단위가 아닙니다. 다시 입력해주세요.`
+    ),
+    NOT_NUMBER: Object.freeze(
+        `${DEFAULT_ERROR_MESSAGE} 입력한 값이 숫자가 아닙니다.`
+    ),
+    NOT_POSITIVE: Object.freeze(
+        `${DEFAULT_ERROR_MESSAGE} 입력한 값이 양수가 아닙니다.`
+    ),
+}
