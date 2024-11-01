43e143e4f586ffcf8776fb4924b9dcf3db4bd04e
jeong922 <hwajeong922@naver.com>
2024-11-01 20:59:34 +0900

    feat(App): 로또 구입 금액 받기 기능 구현
    - getPurchaseAmount 메서드로 로또 구입 금액 입력받도록 구현
    - validatePurchaseAmount 메서드로 1,000원 단위로 입력값을 검증 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  constructor() {}
+
+  async getPurchaseAmount() {
+    const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    if (!this.validatePurchaseAmount(Number(purchaseAmount))) {
+      return;
+    }
+
+    return purchaseAmount;
+  }
+
+  validatePurchaseAmount(purchaseAmount) {
+    if (purchaseAmount % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액을 1,000원 단위로 입력해 주세요.');
+    }
+
+    return true;
+  }
+
+  async run() {
+    const purchaseAmount = await this.getPurchaseAmount();
+  }
 }
 
 export default App;
