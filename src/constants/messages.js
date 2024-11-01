4501c1a531d0ed8d5633cfaefad1261e1d963efa
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-11-01 17:44:45 +0900

    feat(constants): 당첨 등수, 금액, 통계 출력 메시지 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,7 +1,15 @@
-const PROMPT = {
+export const PROMPT = {
   PRICE_INPUT: '구매금액을 입력해 주세요.\n',
   WINNING_NUMBER_INPUT: '당첨 번호를 입력해 주세요.\n',
-  BONUS_NUMBER_INPUT: '\n보너스 번호를 입력해 주세요.\n'
+  BONUS_NUMBER_INPUT: '\n보너스 번호를 입력해 주세요.\n',
+  WINNING_OUTPUT: `\n당첨 통계\n---`
 };
 
-export default PROMPT;
+export const OUTPUT = {
+  FIFTH_WINNING: (count) => `3개 일치 (5,000원) - ${count}개`,
+  FOURTH_WINNING: (count) => `4개 일치 (50,000원) - ${count}개`,
+  THIRH_WINNING: (count) => `5개 일치 (1,500,000원) - ${count}개`,
+  SECOND_WINNING: (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+  FIRST_WINNING: (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
+  TOTAL_EARNINGS_RATE: (rate) => `총 수익률은 ${rate}%입니다.`
+};
