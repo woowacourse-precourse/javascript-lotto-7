e398d86cac64df3da15a7527f29501b585cbd3ba
seonae-j <wkdtjsdo135@naver.com>
2024-11-01 18:46:32 +0900

    refactor(TicketController): 로또 구매 장수 및 입력 객체 리팩토링
    - 파일명 변경 LottoController -> TicketController
    - Lotto 전체의 로직이 아닌, 로또 구매와 개수에 대한 로직만 관리하기 때문에 변경

---
+++ src/controller/TicketController.js
@@ -0,0 +1,24 @@
+import { INPUT, OUTPUT } from '../constants/Constants.js';
+import { ticketCount } from '../utils/Calculation.js';
+import { Console } from '@woowacourse/mission-utils';
+import { validateCost } from '../utils/Validation.js';
+
+class TicketController {
+  #tickets;
+
+  constructor() {
+    this.#tickets = 0;
+  }
+
+  async setTicketCount() {
+    const cost = await Console.readLineAsync(INPUT.COST);
+    validateCost(cost);
+    this.#tickets = ticketCount(cost);
+  }
+
+  displayTicketCount() {
+    Console.print(`\n${this.#tickets}${OUTPUT.TICKET}`);
+  }
+}
+
+export default TicketController;
