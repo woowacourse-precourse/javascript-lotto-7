b7dc953da50c8a0483d29f5902eef60393d087de
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-11-01 21:35:53 +0900

    feat: 당첨, 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
+
 class App {
-  async run() {}
+  async run() {
+    const PAY_INPUT = await Console.readLineAsync("구입금액을 입력해 주세요.\n")
+    const WINNING_NUMBERS = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n")
+    const BONUS_NUMBER = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n")
+  }
 }
 
 export default App;
