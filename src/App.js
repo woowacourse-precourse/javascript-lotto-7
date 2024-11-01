4108f5c900dfb0c4a1273217fa9a1a811d65b549
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-11-01 13:57:05 +0900

    feat: 로또구입금액 입력값이 숫자가 아닌 경우 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+const LOTTO_COST_MIN = 1000;
+const LOTTO_NUMBER_MIN = 1;
+const LOTTO_NUMBER_MAX = 45;
+const LOTTO_NUMBER_COUNT = 6;
 const GUIDE_MESSAGE_INPUT_COST = "구입금액을 입력해 주세요.\n";
+const GUIDE_MESSAGE_INPUT_USER_PICKED_NUMBERS =
+  "\n당첨 번호를 입력해 주세요.\n";
 const ERROR_MESSAGE_INVALID_COST =
   "[ERROR] 입력하신 금액이 1,000원 단위가 아닙니다. 다시";
+const ERROR_MESSAGE_NOT_NUMBER = "[ERROR] 입력값이 숫자가 아닙니다. 다시";
 
 class App {
   async run() {
@@ -10,19 +17,28 @@ class App {
     let isValid = false;
 
     while (!isValid) {
-      lottoCost = Number(
-        await MissionUtils.Console.readLineAsync(GUIDE_MESSAGE_INPUT_COST)
-      );
-      if (lottoCost % 1000 !== 0) {
-        throw new Error(ERROR_MESSAGE_INVALID_COST);
+      try {
+        lottoCost = Number(await this.getLottoCost());
+        if (isNaN(lottoCost)) {
+          throw new Error(ERROR_MESSAGE_NOT_NUMBER);
+        } else if (lottoCost % LOTTO_COST_MIN !== 0) {
+          throw new Error(ERROR_MESSAGE_INVALID_COST);
+        }
+        isValid = true;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
       }
-      isValid = true;
     }
+    isValid = false;
 
     const lottoCnt = lottoCost / 1000;
     let lottoNumbers = [];
     for (let i = 0; i < lottoCnt; i++) {
-      lottoNumbers[i] = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoNumbers[i] = MissionUtils.Random.pickUniqueNumbersInRange(
+        LOTTO_NUMBER_MIN,
+        LOTTO_NUMBER_MAX,
+        LOTTO_NUMBER_COUNT
+      );
       lottoNumbers[i].sort((a, b) => {
         return a - b;
       });
@@ -35,9 +51,9 @@ class App {
     });
 
     let userPickedNumbers = [];
-    userPickedNumbers = await MissionUtils.Console.readLineAsync(
-      "\n당첨 번호를 입력해 주세요.\n"
-    );
+  }
+  getLottoCost() {
+    return MissionUtils.Console.readLineAsync(GUIDE_MESSAGE_INPUT_COST);
   }
 }
 
