a04cf951b07b793d3f691bea49428dedc54896b0
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-11-02 01:08:00 +0900

    feat(parse,input): 당첨 번호을 입력 받는 getWinningNumbers 메서드, 입력을 콤마를 기준으로 구분하는 parseWinningNums 메서드, 입력된 당첨 번호가 옳은지 판단하는 validateWinningNumbers 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
+// 입력 모듈
+
 async function getPurchaseAmount() {
   let input = '';
   while (true) {
@@ -10,6 +12,21 @@ async function getPurchaseAmount() {
   return Number(input);
 }
 
+async function getWinningNumbers() {
+  let input = '';
+  while (true) {
+    input = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+    if (validateWinningNumbers(input)) break;
+    Console.print(
+      '[ERROR] 당첨 번호는 6개의 숫자와 쉼표로 이루어져야 하며, 각각의 숫자는 1~45 사이의 숫자여야 합니다.'
+    );
+  }
+  console.log(typeof input);
+  return parseWinningNums(input);
+}
+
+// 생성 모듈
+
 function generateLottos(amount) {
   let array = [];
   for (let i = 0; i < amount; i++) {
@@ -18,6 +35,14 @@ function generateLottos(amount) {
   return array;
 }
 
+// 처리 모듈
+
+function parseWinningNums(input) {
+  return input.split(',').map((e) => Number(e));
+}
+
+// 출력 모듈
+
 function printLottos(lottos) {
   Console.print(`\n${lottos.length}개를 구매했습니다.\n`);
   lottos.forEach((el) => {
@@ -25,9 +50,21 @@ function printLottos(lottos) {
   });
 }
 
+// 예외 처리 모듈
+
 function validatePurchaseAmount(input) {
   const num = Number(input);
   if (!isNaN(num) && num % 1000 === 0 && num > 0) return true;
+}
+
+function validateWinningNumbers(input) {
+  const numbers = parseWinningNums(input);
+  if (
+    !numbers.some((x) => isNaN(x)) &&
+    numbers.length === 6 &&
+    numbers.every((x) => x > 0 && x < 46)
+  )
+    return true;
 }
 
 class App {
@@ -36,6 +73,7 @@ class App {
     let amountOfLottos = seedMoney / 1000;
     let lottos = generateLottos(amountOfLottos);
     printLottos(lottos);
+    let winningNumbers = await getWinningNumbers();
   }
 }
 
