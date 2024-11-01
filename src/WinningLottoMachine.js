c600360267eac98d14067e305eab673c99ad0b5e
moonseonghui <msh9980@naver.com>
2024-11-02 02:36:05 +0900

    feat(checkDuplicateBonusNum): 보너스 번호의 중복 검사 구현

--- src/WinningLottoMachine.js
+++ src/WinningLottoMachine.js
@@ -5,7 +5,7 @@ import Validator from './utils/Validator.js';
 class WinningLottoMachine {
   async createWinningLotto() {
     const winningNumbers = await WinningLottoMachine.#getValidWinningNums();
-    const bonusNumber = await WinningLottoMachine.#getValidBonusNums();
+    const bonusNumber = await WinningLottoMachine.#getValidBonusNums(winningNumbers);
   }
 
   static async #getValidWinningNums() {
@@ -20,11 +20,11 @@ class WinningLottoMachine {
     }
   }
 
-  static async #getValidBonusNums() {
+  static async #getValidBonusNums(winningNumbers) {
     while (true) {
       try {
         const bonusNumber = await InputView.getUserInput('보너스 번호를 입력해 주세요.\n');
-        WinningLottoMachine.#validateBonusNumber(bonusNumber);
+        WinningLottoMachine.#validateBonusNumber(bonusNumber, winningNumbers);
         return bonusNumber;
       } catch (error) {
         OutputView.printError(error);
@@ -47,10 +47,11 @@ class WinningLottoMachine {
     WinningLottoMachine.#checkDuplicateNum(splitNums);
   }
 
-  static #validateBonusNumber(bonusNumber) {
+  static #validateBonusNumber(bonusNumber, winningNumbers) {
     Validator.checkIsNull(bonusNumber);
     Validator.checkRegexPattern(bonusNumber, /^\d+$/, '보너스 번호는 숫자만 입력 가능합니다.');
     Validator.checkValidRange(bonusNumber, 1, 45, '로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    WinningLottoMachine.#checkDuplicateBonusNum(bonusNumber, winningNumbers);
   }
 
   static #splitByComma(inputString) {
@@ -65,6 +66,12 @@ class WinningLottoMachine {
     const uniqueNums = new Set(splitNums);
     if (uniqueNums.size !== splitNums.length) {
       throw new Error('[ERROR] 당첨 번호는 중복된 숫자를 포함할 수 없습니다.');
+    }
+  }
+
+  static #checkDuplicateBonusNum(bonusNumber, winningNumbers) {
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
     }
   }
 }
