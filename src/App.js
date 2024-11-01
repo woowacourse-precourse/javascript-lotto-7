934091956b22dd7aadcc9c9b034d9929a7ec394d
kimywann <Kimywan10@gmail.com>
2024-11-01 23:07:46 +0900

    feat(App): 구매 금액에 따른 로또 계산 출력

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const purchasePrice = await Console.readLineAsync("구입금액을 입력해 주세요.");
+    const purchasePrice = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
 
     if (purchasePrice % 1000 !== 0) {
       throw new Error("[ERROR] 1,000원 단위로 나누어 떨어지지 않습니다.")
     }
+
+    const lottoCount = purchasePrice / 1000;
+    Console.print(`\n${lottoCount}개를 구매했습니다.`)
   }
 }
 
