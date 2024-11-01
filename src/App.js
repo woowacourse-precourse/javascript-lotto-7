44836fb0a49dd6f7047501e28499fd6df10584c8
kimywann <Kimywan10@gmail.com>
2024-11-01 13:27:57 +0900

    feat(App): 구입 금액 입력 및 유효성 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const purchasePrice = await Console.readLineAsync("구입금액을 입력해 주세요.");
+
+    if (purchasePrice % 1000 === 0) {
+      throw new Error("[ERROR] 1,000원 단위로 나누어 떨어지지 않습니다.")
+    }
+  }
 }
 
 export default App;
