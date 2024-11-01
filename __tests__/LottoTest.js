ea8252f9febfef50b4e1afee8926cfb18f0b4eee
sooyeoniya <tndus1306403@naver.com>
2024-11-01 17:23:24 +0900

    test: 유효성 예외 테스트 공통 로직 분리

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,32 +1,20 @@
 import Lotto from '../src/domain/Lotto.js';
+import runExceptionTestPipe from '../src/utils/runExceptionTestPipe.js';
 import { COMMON_ERRORS, VALIDATION_ERRORS } from '../src/constants/constants.js';
 
-const runLottoNumbersException = (lottoNumbers, errorMessage) => {
-  // given: 매개변수
-  // when & then
-  expect(() => new Lotto(lottoNumbers)).toThrow(errorMessage);
-}
-
 describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
-    runLottoNumbersException([1, 2, 3, 4, 5, 6, 7], VALIDATION_ERRORS.LOTTO_NUMBERS.COUNT);
-  });
+  const exceptionTestCases = [
+    ['로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', [1, 2, 3, 4, 5, 6, 7], VALIDATION_ERRORS.LOTTO_NUMBERS.COUNT],
+    ['로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', [1, 2, 3, 4, 5, 5], VALIDATION_ERRORS.LOTTO_NUMBERS.DUPLICATE],
+    ['로또 번호가 숫자가 아닌 경우 예외가 발생한다.', [1, 'e', 3, 4, 5, 'a'], COMMON_ERRORS.NUMBER],
+    ['로또 번호가 정수가 아닌 경우 예외가 발생한다.', [1, 2.4, 3, 4, 5.1, 5], COMMON_ERRORS.INTEGER],
+    ['로또 번호가 1 이상 45 이하가 아닌 경우 예외가 발생한다.', [1, 2, 46, 4, 5, 0], COMMON_ERRORS.RANGE],
+  ];
 
-  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-    runLottoNumbersException([1, 2, 3, 4, 5, 5], VALIDATION_ERRORS.LOTTO_NUMBERS.DUPLICATE);
-  });
-
-  test('로또 번호가 숫자가 아닌 경우 예외가 발생한다.', () => {
-    runLottoNumbersException([1, 'e', 3, 4, 5, 'a'], COMMON_ERRORS.NUMBER);
-  });
-
-  test('로또 번호가 정수가 아닌 경우 예외가 발생한다.', () => {
-    runLottoNumbersException([1, 2.4, 3, 4, 5.1, 5], COMMON_ERRORS.INTEGER);
-  });
-
-  test('로또 번호가 1 이상 45 이하가 아닌 경우 예외가 발생한다.', () => {
-    runLottoNumbersException([1, 2, 46, 4, 5, 0], COMMON_ERRORS.RANGE);
-  });
+  it.each(exceptionTestCases)('%s', (_, input, errorMessage) => {
+      runExceptionTestPipe(Lotto, input, errorMessage);
+    }
+  );
 
   test('로또 번호가 올바른 경우 getLottoNumbers()를 통해 로또 번호를 반환한다.', () => {
     // given
