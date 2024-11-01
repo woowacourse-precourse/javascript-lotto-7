6067a505cb7c69c635034d220f7ff81c464b3f2f
H-sooyeon <wndqhr__@naver.com>
2024-11-01 17:10:34 +0900

    fix: 예외 발생시 에러 메시지 출력 후 재입력 메시지 출력하도록 수정

--- src/utils/io/InputRepeat.js
+++ src/utils/io/InputRepeat.js
@@ -1,4 +1,5 @@
 import Input from './Input.js';
+import Output from './Output.js';
 
 async function inputRecursion(message, fn) {
   const inputs = await Input.get(message);
@@ -16,16 +17,18 @@ async function inputRecursion(message, fn) {
   }
 }
 
-async function handleRecursion(result, fn) {
+async function handleRecursion(result, fn, message) {
   if (result.success) {
     return result.input;
   }
-  const newResult = await inputRecursion(`${result.message}\n`, fn);
-  return handleRecursion(newResult, fn);
+
+  Output.print(`${result.message}\n`);
+  const newResult = await inputRecursion(`${message}`, fn);
+  return handleRecursion(newResult, fn, message);
 }
 
 export default async function InputRepeat(message, fn) {
   const inputs = await inputRecursion(message, fn);
 
-  return handleRecursion(inputs, fn);
+  return handleRecursion(inputs, fn, message);
 }
