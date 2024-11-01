b286356d311fcec4fc564820b126b358aee51793
kim min seok <nas7062@naver.com>
2024-11-01 22:12:14 +0900

    feat(Message) : 메세지 함수
    에러 메세지나 인풋,아웃풋 메세지를
    상수로 저장하는 함수

---
+++ src/utils/Message.js
@@ -0,0 +1,18 @@
+export const INPUT_MEESAGE = {
+  AMOUNT_INPUT: "구입금액을 입력해 주세요.\n",
+  WINNER_INPUT: "당첨 번호를 입력해 주세요.\n",
+  BONUS_INPUT: "보너스 번호를 입력해 주세요.\n",
+};
+
+export const OUPUT_MESSAGE = {
+  RESULT_OUTPUT: "당첨 통계",
+  LINE_OUTPUT: "---",
+  ZERO_OUTPUT: "총 수익률은 0%입니다.",
+};
+export const RESULT_MESSAGE = {
+    THREE_COUNT : "3개 일치 (5,000원) -",
+    FOUR_COUNT : "4개 일치 (50,000원) -",
+    FIVE_COUNT : "5개 일치 (1,500,000원) -",
+    BONUS_COUNT : "5개 일치, 보너스 볼 일치 (30,000,000원) -",
+    SIX_COUNT : "6개 일치 (2,000,000,000원) -"
+}
