4e6e6a03576a137f7673c95ffd1525978f55baec
ha-kuku <hakukudh@gmail.com>
2024-11-01 10:38:34 +0900

    feat: 로또 구입금액 입력기능 추가 - getPurchaseMoney 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+import { INPUT } from './Constants/Message.js';
+import { BasicValidation } from './Validation.js';
+
 class App {
-  async run() {}
+  #basicValidation;
+
+  constructor() {
+    this.#basicValidation = new BasicValidation();
+  }
+
+  async getPurchaseMoney() {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(INPUT.purchaseMoney);
+        this.#basicValidation.PurchaseUnit(input.trim());
+        return Number(input);
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
+  }
+
+  async run() {
+    const purchaseMoney = await this.getPurchaseMoney();
+  }
 }
 
 export default App;
