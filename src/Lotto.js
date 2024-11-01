fe8fccf08583c7fba907b8411192f89be96dc2a1
sanghyeonLee <hyeon012366@gmail.com>
2024-11-02 02:07:28 +0900

    feat: 랜덤 6개 값 배열 생성 메서드 및 횟수만큼 반복된 이차원 배열 생성 메서드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,36 @@
+import {MissionUtils} from "@woowacourse/mission-utils";
+
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    #validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+
+    purchaseNumbers() { //인자로 몇번인지를 넣자
+        let arr = []
+        for (let i = 0; i < 6; i++) {
+            const sixRandomValues = this.getSixRandomValues()
+            arr.push(sixRandomValues)
+        }
+        console.log(arr)
+        return arr
+    }
+
+    getSixRandomValues() {
+        return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+
+    // TODO: 추가 기능 구현
 }
 
 export default Lotto;
