ef3f905ef22940971d07df756b1af2992e40c8be
HS <hasang2691@naver.com>
2024-11-01 15:00:22 +0900

    feat(ValidateBonusNumber.js): 보너스 번호가 1-45 범위를 벗어날 시 예외처리 기능 구현
    - validateBonusNumberRange 메서드 구현

--- src/models/ValidateBonusNumber.js
+++ src/models/ValidateBonusNumber.js
@@ -1,7 +1,13 @@
 class ValidateBonusNumber {
-  #validateIsNumber(input) {
+  validateIsNumber(input) {
     if (Number.isNaN(Number(input))) {
       throw new Error('[ERROR] 보너스 번호는 숫자여야 합니다.');
+    }
+  }
+
+  validateBonusNumberRange(number) {
+    if (number < 1 || number > 45) {
+      throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
     }
   }
 }
