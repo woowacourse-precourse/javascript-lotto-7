f27ee850b7daf738cf8361b6a851af6e1a3cb26e
Sohee <sh11govlday@gmail.com>
2024-11-01 19:54:15 +0900

    feat(App) : 당첨 내역과 수익률 계산 및 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App {
       );
     }
     for (let i = 0; i < lottoCount; i++) {
-      Console.print(lottos[i]);
+      Console.print(`[${lottos[i].join(', ')}]`);
     }
     Console.print('');
 
@@ -83,6 +83,61 @@ class App {
         Console.print(error.message);
       }
     }
+
+    Console.print('당첨 통계');
+    Console.print('---');
+    let matchLotto = {
+      3: {
+        count: 0,
+        reward: 5000,
+      },
+      4: {
+        count: 0,
+        reward: 50000,
+      },
+      5: {
+        count: 0,
+        reward: 1500000,
+      },
+      '5 + bonus': {
+        count: 0,
+        reward: 30000000,
+      },
+      6: {
+        count: 0,
+        reward: 2000000000,
+      },
+    };
+    for (let i = 0; i < lottos.length; i++) {
+      const compareNumbers = winningNumbers.filter((number) =>
+        lottos[i].includes(number)
+      );
+      const matchCount = compareNumbers.length;
+      if (lottos[i].includes(bonusNumber) && matchCount === 5) {
+        ++matchLotto['5 + bonus'].count;
+      } else if (matchCount > 2) {
+        ++matchLotto[matchCount].count;
+      }
+    }
+
+    let totalProfit = 0;
+    for (let [key, value] of Object.entries(matchLotto)) {
+      if (value !== 0) {
+        totalProfit += value.reward * value.count;
+      }
+    }
+
+    let roi = ((totalProfit / price) * 100).toFixed(1);
+
+    Console.print(`3개 일치 (5,000원) - ${matchLotto[3].count}개`);
+    Console.print(`4개 일치 (50,000원) - ${matchLotto[4].count}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${matchLotto[5].count}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchLotto['5 + bonus'].count}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${matchLotto[6].count}개`);
+
+    Console.print(`총 수익률은 ${roi}%입니다.`);
   }
 }
 
