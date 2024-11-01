1ef341b11c084bb60b21ca7472fd2e7b81d15cff
silver0108 <kimsoeun0108@gmail.com>
2024-11-01 23:51:16 +0900

    refactor: LottoController 생성 및 App 클래스 내 로직 이동

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import validateAmount from "./validator/validateAmount.js";
-import { InputView } from "./view/InputView.js";
-import LottoService from "./services/LottoService.js";
-import OutputView from "./view/OutputView.js";
-import validateWinningNumbers from "./validator/validateWinningNumbers.js";
-import validateBonusNumber from "./validator/validateBonusNumber.js";
+import LottoController from "./controller/LottoController.js";
 
 class App {
   async run() {
-    const amountInput = await InputView.getLottoAmount();
-    const amount = validateAmount(amountInput);
-
-    const lottoCount = amount / 1000;
-    const lottoService = new LottoService();
-    lottoService.generateLottos(lottoCount);
-
-    OutputView.printLottoCount(lottoCount);
-    OutputView.printLottoNumbers(lottoService.getLottos());
-
-    const winningNumbersInput = await InputView.getWinningNumbers();
-    const winningNumbers = validateWinningNumbers(winningNumbersInput);
-
-    const bonusNumberInput = await InputView.getBonusNumber();
-    const bonusNumber = validateBonusNumber(bonusNumberInput, winningNumbers);
-
-    const matchCounts = lottoService.compareLottos(winningNumbers, bonusNumber);
-    OutputView.printWinningStatistics(matchCounts);
-
-    const profit = lottoService.calculateProfit(matchCounts, amount);
-    OutputView.printProfit(profit);
+    const controller = new LottoController();
+    await controller.run();
   }
 }
 
---
+++ src/controller/LottoController.js
@@ -0,0 +1,63 @@
+import validateAmount from "../validator/validateAmount.js";
+import { InputView } from "../view/InputView.js";
+import LottoService from "../services/LottoService.js";
+import OutputView from "../view/OutputView.js";
+import validateWinningNumbers from "../validator/validateWinningNumbers.js";
+import validateBonusNumber from "../validator/validateBonusNumber.js";
+
+class LottoController {
+  #lottoService;
+
+  constructor() {
+    this.#lottoService = new LottoService();
+  }
+
+  async run() {
+    const amount = await this.getValidAmount();
+    const lottoCount = amount / 1000;
+
+    this.generateLottos(lottoCount);
+    this.displayLottoResults(lottoCount);
+
+    const winningNumbers = await this.getValidWinningNumbers();
+    const bonusNumber = await this.getValidBonusNumber(winningNumbers);
+
+    this.displayWinningStatistics(winningNumbers, bonusNumber, amount);
+  }
+
+  async getValidAmount() {
+    const amountInput = await InputView.getLottoAmount();
+    return validateAmount(amountInput);
+  }
+
+  async getValidWinningNumbers() {
+    const winningNumbersInput = await InputView.getWinningNumbers();
+    return validateWinningNumbers(winningNumbersInput);
+  }
+
+  async getValidBonusNumber(winningNumbers) {
+    const bonusNumberInput = await InputView.getBonusNumber();
+    return validateBonusNumber(bonusNumberInput, winningNumbers);
+  }
+
+  generateLottos(lottoCount) {
+    this.#lottoService.generateLottos(lottoCount);
+  }
+
+  displayLottoResults(lottoCount) {
+    OutputView.printLottoCount(lottoCount);
+    OutputView.printLottoNumbers(this.#lottoService.getLottos());
+  }
+
+  displayWinningStatistics(winningNumbers, bonusNumber, amount) {
+    const matchCounts = this.#lottoService.compareLottos(
+      winningNumbers,
+      bonusNumber
+    );
+    OutputView.printWinningStatistics(matchCounts);
+    const profit = this.#lottoService.calculateProfit(matchCounts, amount);
+    OutputView.printProfit(profit);
+  }
+}
+
+export default LottoController;
