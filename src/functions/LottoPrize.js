255348bfab93c3268db672742c89e1035bb2b8cc
Jiny <moc24@naver.com>
2024-11-02 02:33:44 +0900

    feat(LottoPrize.js) : 로또 당첨 개수 얻는 함수 생성

--- src/functions/LottoPrize.js
+++ src/functions/LottoPrize.js
@@ -1,3 +1,16 @@
+import Lotto from '../Lotto.js';
+
 export const splitByComma = (stringInput) => {
   return stringInput.split(',');
+};
+
+export const getLottoPrizeCount = (lotto, lottoPrizeNumbers) => {
+  let prizeCount = 0;
+  lottoPrizeNumbers.forEach((lottoPrizeNumber) => {
+    if (lotto.numbers.includes(lottoPrizeNumber)) {
+      prizeCount += 1;
+    }
+  });
+  console.log('prizeCount', prizeCount);
+  return prizeCount;
 };
