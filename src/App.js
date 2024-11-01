e4020f262b3b0ac1bc8441bd7bac55b8f27d5df2
Sohee <sh11govlday@gmail.com>
2024-11-01 12:13:37 +0900

    feat(App) : 로또 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    while (true) {
+      try {
+        const price = await Console.readLineAsync(
+          '구입금액을 입력해 주세요.\n'
+        );
+
+        if (price % 1000 === 0) {
+          break;
+        }
+
+        throw new Error(
+          '[ERROR] 로또 구입 금액은 1000원 단위만 입력 가능합니다.'
+        );
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
 }
 
 export default App;
