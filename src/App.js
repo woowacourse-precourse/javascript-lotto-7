9615dbfab422fedb9a2bfe9e834e50a7f4f835c2
ShinjungOh <tlswjd9260@gmail.com>
2024-11-01 23:13:30 +0900

    feat: 사용자에게 구입금액 입력받는 콘솔 출력

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ class App {
     // 1에서 45 사이의 중복되지 않은 정수 6개 반환
     const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
     console.log(lotto);
+
+    const input = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n')
+    const parseInput = parseInt(input, 10);
   }
 }
 
