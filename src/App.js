aabcc3133bc67d74711cffefcc4c40981ace8c4c
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-11-01 21:19:46 +0900

    ✨feat: 구입 금액 검증 및 로또 구매 수량 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+import PurchaseLotto from './PurchaseLotto.js';
+
 class App {
-  async run() {}
+  async run() {
+    const userInputMoney =
+      await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    const parsedMoney = Number(userInputMoney);
+
+    const purchaseLotto = new PurchaseLotto(parsedMoney);
+    const lottoQuantity = purchaseLotto.getQuantity();
+  }
 }
 
 export default App;
