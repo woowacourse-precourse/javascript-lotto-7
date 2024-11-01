1ff310228b1faca178a9f421c71cf2bfebe4c61f
Lee <dltmdgns0508@gmail.com>
2024-11-02 03:22:42 +0900

    refactor(Rules): 규칙들 export 통합
    Rules.js 제작
    - 각종 RULE들 객체로 통합하여 export

---
+++ src/Validators/Rules.js
@@ -0,0 +1,13 @@
+import DEFAULT_RULES from './defaultValidator.js';
+import PURCHASE_AMOUNT_RULES from './purchaseAmountValidator.js';
+import LOTTO_NUMBER_LIST_RULES from './lottoNumberListValidator.js';
+import LOTTO_NUMBER_RULES from './lottoNumberValidator.js';
+import BONUS_NUMBER_RULES from './bonusNumberValidator.js';
+
+export {
+  DEFAULT_RULES,
+  PURCHASE_AMOUNT_RULES,
+  LOTTO_NUMBER_LIST_RULES,
+  LOTTO_NUMBER_RULES,
+  BONUS_NUMBER_RULES,
+};
