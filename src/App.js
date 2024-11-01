2606f56e483766599f6d5c9e6864545052d19581
ShinjungOh <tlswjd9260@gmail.com>
2024-11-01 23:23:57 +0900

    feat: 구입금액 유효성 검사 및 예외 시 에러 메시지 출력
    1. 숫자를 입력하지 않은 경우
    2. 1,000원 미만의 금액을 입력한 경우
    3. 입력값이 1,000원으로 나누어 떨어지지 않는 경우

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ class App {
   async run() {
     // 1에서 45 사이의 중복되지 않은 정수 6개 반환
     const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-    console.log(lotto);
+    // console.log(lotto);
 
     const input = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n')
     const parseInput = parseInt(input, 10);
+
+    // TODO 유효성 검사 분리
+    // TODO 에러 메시지 상수처리
+    const validate = (input) => {
+      if (typeof input !== 'number') {
+        throw new Error('[ERROR] 입력값이 올바르지 않습니다.')
+      }
+      if (input < 1000) {
+        throw new Error('[ERROR] 최소 1,000원 이상 입력해야 합니다.')
+      }
+      if (input % 1000 !== 0) {
+        throw new Error('[ERROR] 입력값이 1,000원 단위로 나누어 떨어지지 않습니다.')
+      }
+    }
+
+    validate(parseInput);
   }
 }
 
