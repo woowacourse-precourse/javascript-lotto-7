20dd3a09ee2ded82a6b19aa2378d9cfe6999b579
Seio924 <seio9241@gmail.com>
2024-11-01 13:57:41 +0900

    feat(LottoMachine): 구입 금액만큼 로또 발행

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -1,10 +1,28 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
 
 class LottoMachine {
+  #lottos;
+
+  constructor() {
+    this.#lottos = [];
+  }
+
   generateLottoNumbers() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 
+  generateLottoTickets(purchaseAmount) {
+    for (let i = 0; i < purchaseAmount; i++) {
+      const lottoNumbers = this.generateLottoNumbers();
+      const lotto = new Lotto(lottoNumbers);
+      this.#lottos.push(lotto);
+    }
+  }
+
+  getLottos() {
+    return this.#lottos;
+  }
 }
 
 export default LottoMachine;
