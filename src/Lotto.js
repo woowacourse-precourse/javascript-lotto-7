ffa2646c68bc7d0afc9bc8f01c92045931a877cb
Jiny <moc24@naver.com>
2024-11-01 23:37:59 +0900

    feat(Lotto.js) : 로또번호(numbers) getter, setter 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,18 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  get numbers() {
+    return this.#numbers;
+  }
+
+  set numbers(numbers) {
+    this.#numbers = numbers;
+  }
 }
 
 export default Lotto;
