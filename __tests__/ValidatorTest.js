9a3d87ec724f8c049593c6597ff84bdb2dd09b24
Lee <dltmdgns0508@gmail.com>
2024-11-02 03:06:10 +0900

    test(ValidatorTest): 유효성 검사 로직 테스트
    ValidatorTest.js 제작
    - 기본 인풋 validator 테스트코드 작성
    - 구입금액 validator 테스트코드 작성
    - 당첨 로또 번호 validator 테스트 코드 작성
    - 보너스 번호 validator 테스트 코드 작성

---
+++ __tests__/ValidatorTest.js
@@ -0,0 +1,132 @@
+import Validator from '../src/Validators/Validator.js';
+import {
+  DEFAULT_RULES,
+  PURCHASE_AMOUNT_RULES,
+  LOTTO_NUMBER_LIST_RULES,
+  LOTTO_NUMBER_RULES,
+  BONUS_NUMBER_RULES,
+} from '../src/Validators/Rules.js';
+import CustomError from '../src/Utils/CustomError.js';
+
+describe('유효성 검사 테스트', () => {
+  test('입력값이 존재하지 않으면 예외 발생한다.', () => {
+    const validator = new Validator(DEFAULT_RULES);
+    const input = '';
+    expect(() => {
+      validator.validate(input);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each([
+    {
+      input: '3f4n',
+      message: PURCHASE_AMOUNT_RULES.notNumber.errorMessage,
+    },
+    {
+      input: '-10.53',
+      message: PURCHASE_AMOUNT_RULES.notInteger.errorMessage,
+    },
+    {
+      input: '950',
+      message: PURCHASE_AMOUNT_RULES.lessNumber.errorMessage,
+    },
+    {
+      input: '34004',
+      message: PURCHASE_AMOUNT_RULES.disableThousand.errorMessage,
+    },
+  ])(
+    `구입금액 '$input'은 '$message' 에러가 발생한다.`,
+    ({ input, message }) => {
+      const validator = new Validator(PURCHASE_AMOUNT_RULES);
+      expect(() => {
+        validator.validate(input);
+      }).toThrow(new CustomError(message));
+    },
+  );
+
+  test.each([
+    {
+      input: '1, 2, 3, 4, 5, 6, 7',
+      message: LOTTO_NUMBER_LIST_RULES.validLength.errorMessage,
+    },
+    {
+      input: '1, 1, 2, 3, 4, 5',
+      message: LOTTO_NUMBER_LIST_RULES.duplicate.errorMessage,
+    },
+    {
+      input: '1, 2, 3,     , 5, 6',
+      message: LOTTO_NUMBER_RULES.empty.errorMessage,
+    },
+    {
+      input: '1, 2, 2f, 3, 4, 5',
+      message: LOTTO_NUMBER_RULES.notNumber.errorMessage,
+    },
+    {
+      input: '1, 2, 34\\n, 3, 4, 5',
+      message: LOTTO_NUMBER_RULES.notNumber.errorMessage,
+    },
+    {
+      input: '1, 2, 2.23, 3, 4, 5',
+      message: LOTTO_NUMBER_RULES.notInteger.errorMessage,
+    },
+    {
+      input: '1, 2, 3, 4, 5, 56',
+      message: LOTTO_NUMBER_RULES.validRange.errorMessage,
+    },
+    {
+      input: '1, 0, 2, 3, 4, 5',
+      message: LOTTO_NUMBER_RULES.validRange.errorMessage,
+    },
+  ])(
+    `당첨 번호들 '$input'은 '$message' 에러가 발생한다.`,
+    ({ input, message }) => {
+      const numberValidator = new Validator(LOTTO_NUMBER_RULES);
+      const numberListValidator = new Validator(LOTTO_NUMBER_LIST_RULES);
+      const lottoList = input.split(',').map(n => n.trim());
+      expect(() => {
+        numberListValidator.validate(lottoList);
+        lottoList.forEach(number => {
+          numberValidator.validate(number);
+        });
+      }).toThrow(new CustomError(message));
+    },
+  );
+
+  test.each([
+    {
+      input: { bonusNumber: '34%' },
+      message: BONUS_NUMBER_RULES.notNumber.errorMessage,
+    },
+    {
+      input: { bonusNumber: 'f2' },
+      message: BONUS_NUMBER_RULES.notNumber.errorMessage,
+    },
+    {
+      input: { bonusNumber: '3.5325' },
+      message: BONUS_NUMBER_RULES.notInteger.errorMessage,
+    },
+    {
+      input: { bonusNumber: '76' },
+      message: BONUS_NUMBER_RULES.validRange.errorMessage,
+    },
+    {
+      input: { bonusNumber: '0' },
+      message: BONUS_NUMBER_RULES.validRange.errorMessage,
+    },
+    {
+      input: {
+        lottoNumbers: [1, 2, 3, 4, 5, 6],
+        bonusNumber: '5',
+      },
+      message: BONUS_NUMBER_RULES.duplicate.errorMessage,
+    },
+  ])(
+    `보너스 번호 '$input'은 '$message' 에러가 발생한다.`,
+    ({ input, message }) => {
+      const validator = new Validator(BONUS_NUMBER_RULES);
+      expect(() => {
+        validator.validate(input);
+      }).toThrow(new CustomError(message));
+    },
+  );
+});
