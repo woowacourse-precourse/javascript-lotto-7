b32c4899380e22781a8526e36de8c9efeb14ea91
sooinice <sooin9674@naver.com>
2024-11-01 11:40:36 +0900

    feat : 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const lottoPurchaseAmount = await MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+  }
 }
 
 export default App;
