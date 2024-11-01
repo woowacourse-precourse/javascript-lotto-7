a75bcd1f1c16a17cd2e96826b9b32cec06ce8415
guddnboy <hung3018@naver.com>
2024-11-01 20:51:05 +0900

    feat: 로또번호 최소,최대값, 로또 가격 상수 분리

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,11 @@
 export const INPUT_MESSAGE = {
-  INPUT_AMOUT_MESSAGE: '구입금액을 입력해 주세요.\n',
+  INPUT_AMOUNT_MESSAGE: '구입금액을 입력해 주세요.\n',
 };
+
 export const LOTTO_MESSAGE = {
   LOTTO_NUMBER_ERROR_MESSAGE: '[ERROR] 로또 번호는 6개여야 합니다.',
 };
+export const MIN_NUMBER = 1;
+export const MAX_NUMBER = 45;
 export const NUMBER_OF_LOTTO_NUMBERS = 6;
+export const LOTTO_PRICE = 1000;
