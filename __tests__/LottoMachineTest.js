53e94b444499439984184f1ee9136ef9e95d0dc9
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-01 15:18:47 +0900

    [test]: 로또 데이터 가져오는 기능 테스트 구현

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -10,4 +10,19 @@ describe('LottoMachine test', () => {
   test('구매한 수량 가져오기', () => {
     expect(lottoMachine.getLottoQuantity()).toBe(10);
   });
+
+  test('투입한 금액에 맞게 로또가 생성되어야 한다.', () => {
+    const lottos = lottoMachine.getLottos();
+    const quantity = lottoMachine.getLottoQuantity();
+
+    expect(lottos.length).toBe(quantity);
+  });
+
+  test('생성된 로또의 길이는 6이다.', () => {
+    const lottos = lottoMachine.getLottos();
+
+    lottos.forEach((lotto) => {
+      expect(lotto.length).toBe(6);
+    });
+  });
 });
