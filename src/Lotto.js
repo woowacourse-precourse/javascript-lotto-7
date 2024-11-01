d9df123ca493eafb8bc4c38b6b231a9a6ee81fad
oko_jin <dswvgw6412@naver.com>
2024-11-01 17:55:31 +0900

    feat(Lotto): 로또 클래스에서 중복된 번호가 있는지 체크하는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,6 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
 import { errorMessage } from './constant/errorMessage.js';
+import { checkDuplication } from './utils/checkDuplication.js';
+import { checkNumberRange } from './utils/checkNumberRange.js';
 
 class Lotto {
   #numbers;
@@ -7,16 +8,25 @@ class Lotto {
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    console.log(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error(`${errorMessage.prefix} ${errorMessage.invalidLotto}`);
     }
+
+    if (checkDuplication(numbers)) {
+      throw new Error(`${errorMessage.prefix} ${errorMessage.duplicateNumber}`);
+    }
+
+    if (!checkNumberRange(numbers)) {
+      throw new Error(`${errorMessage.prefix} ${errorMessage.invalidNumberRange}`);
+    }
   }
 
   get numbers() {
-    return this.#numbers;
+    return this.#numbers.join(', ');
   }
 }
 
