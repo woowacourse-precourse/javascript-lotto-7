8d05a958e3fa37327d3c27e2ae8340f665b8d74d
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-02 00:47:03 +0900

    ✨ feat: 입력 검증 에러메시지 상수 파일 추가
    잘못된 돈 입력에 대한 에러메시지 상수화

---
+++ src/contents/InputErrorMessages.js
@@ -0,0 +1,9 @@
+export const INPUT_ERROR_MESSAGES = Object.freeze({
+  missingMoney:
+    '금액이 입력되지 않았습니다. 1000 단위의 정수로 입력해주세요.\n',
+  nonNumericInput:
+    '문자는 입력할 수 없습니다. 1000 단위의 정수로 입력해주세요.\n',
+  notUnts1000Won: ' 잘못된 숫자 입력입니다. 1000 단위의 정수로 입력해주세요.\n',
+});
+
+export default INPUT_ERROR_MESSAGES;
