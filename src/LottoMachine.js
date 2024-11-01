69876768ae7964e4e367bb103bff3c81c24a6096
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-01 13:37:30 +0900

    [feat] 구매한 로또 수량을 계산하는 기능 구현

---
+++ src/LottoMachine.js
@@ -0,0 +1,17 @@
+import LOTTO_RULE from './constant/lotto.js';
+
+class LottoMachine {
+  #lottoQuantity;
+  #lottos;
+
+  constructor(purchaseAmount) {
+    this.#lottoQuantity = this.#calculateLottoQuantity(purchaseAmount);
+    this.#lottos = [];
+  }
+
+  #calculateLottoQuantity(purchaseAmount) {
+    return purchaseAmount / LOTTO_RULE.AMOUNT_UNIT;
+  }
+}
+
+export default LottoMachine;
---
+++ src/constant/lotto.js
@@ -0,0 +1,5 @@
+const LOTTO_RULE = Object.freeze({
+  AMOUNT_UNIT: 1000,
+});
+
+export default LOTTO_RULE;
