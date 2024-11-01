8e6510a53198a2459296b6007b67b99c284a8dbf
Jang Eunhye <hh961130@naver.com>
2024-11-02 01:59:24 +0900

    feat: 리턴 값 저장

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 import Lotto_purchase from "./Lotto_purchase.js";
+import Rank_check from "./Rank_check.js";
+
 class App {
   async run() {
     let payment = await Console.readLineAsync("");
-    new Lotto_purchase(payment);
-
+    let myLottoArray = new Lotto_purchase(payment);
+    
     let inputLottoNumber = await Console.readLineAsync("");
     let lottoNumberArray = inputLottoNumber.split(",");
-    new Lotto(lottoNumberArray);
+    let numbers = new Lotto(lottoNumberArray);
   }
 }
 
