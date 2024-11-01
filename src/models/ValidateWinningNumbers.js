943990a4a2ada27b3c9728f9fa3078bd73705b8c
HS <hasang2691@naver.com>
2024-11-01 14:39:02 +0900

    feat(ValidateWinningNumbers.js): 당첨 번호가 1-45 범위를 벗어난 숫자가 입력되었다면 예외처리 기능 구현
    - validateNumberRange 메서드 구현

--- src/models/ValidateWinningNumbers.js
+++ src/models/ValidateWinningNumbers.js
@@ -8,6 +8,12 @@ class ValidateWinningNumbers {
       );
     }
   }
+
+  validateNumberRange(number) {
+    if (number < 1 || number > 45) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  }
 }
 
 export default ValidateWinningNumbers;
