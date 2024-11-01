1f060bd8f56f188f63d6e435e2eb07aab2bcdd3d
yusuShin <tlsdbtn0507@naver.com>
2024-11-01 20:40:37 +0900

    feat(App.js) Add makeLotteries - Make randomized lotteries with Lotto class to follow mission guide line - Sort lotteries to clean and enhance UI - Return the result to run function to start lotto game and comparison

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import {
   handleLotteryNumInput,
   handleBonusLotteryNum,
 } from "./handler/inputHandlers.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./class/Lotto.js";
 
 class App {
   async run() {
     const userPurchaseLotteries = await handlePurchaseInput();
+    const lotteriesToCompare = this.makeLotteries(userPurchaseLotteries);
+
     printParam(userPurchaseLotteries + PROMPTS.PURCAHSE_INFO_PROMPT);
+
     const userLotteryNumbers = await handleLotteryNumInput();
     const bonusLotteryNumber = await handleBonusLotteryNum(userLotteryNumbers);
+  }
+
+  makeLotteries(counts) {
+    const UNIQUE_NUMBERS_RANGE = [1, 45, 6];
+    return Array.from({ length: counts }).map((_) => {
+      const randomNums = MissionUtils
+        .Random
+        .pickUniqueNumbersInRange(...UNIQUE_NUMBERS_RANGE)
+        .sort((a, b) => a - b);
+
+      return new Lotto(randomNums);
+    });
   }
 }
 
