8cb573c59c2b6a714bdf0c825d34ae32b198ed5b
jeongeunLee <leeju1009@gmail.com>
2024-11-01 19:18:25 +0900

    feat: 당첨 상금을 계산하는 함수 추가

--- src/Constants.js
+++ src/Constants.js
@@ -16,3 +16,15 @@ export const ANALYZER = {
   defaultCount: 0,
   unitCount: 1,
 };
+
+export const PRIZE_CALCULATOR = {
+  amount: {
+    3: 5000,
+    4: 50000,
+    5: 1500000,
+    [GLOBAL_CONSTANTS.bonusTag]: 30000000,
+    6: 2000000000,
+  },
+
+  defaultPrize: 0,
+};
--- src/PrizeCalculator.js
+++ src/PrizeCalculator.js
@@ -1,8 +1,21 @@
+import { PRIZE_CALCULATOR } from './Constants.js';
+
 class PrizeCalculator {
   #winningTable;
 
   constructor(winningTable) {
     this.#winningTable = winningTable;
+  }
+
+  calculatePrize() {
+    let winningPrize = PRIZE_CALCULATOR.defaultPrize;
+
+    this.#winningTable.forEach((numbersOfLotto, numbersOfMatched) => {
+      const prizeAmount = PRIZE_CALCULATOR.amount[numbersOfMatched];
+      winningPrize += prizeAmount * numbersOfLotto;
+    });
+
+    return winningPrize;
   }
 }
 
