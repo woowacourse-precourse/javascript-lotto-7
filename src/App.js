336c29b77d1baabc7894bf702b7f555cc208d9d8
ha-kuku <hakukudh@gmail.com>
2024-11-01 11:38:54 +0900

    feat: 로또 수량 및 번호 출력 - 랜덤숫자를 출력하는 drawRandomLottoNumbers 메서드 추가 - 로또 한장을 출력하는 singleLottoTicket 메서드 추가 - 구입한 로또 수량을 계산하는 purchaseLottoTickets 메서드 추가 - print를 이용한 메서드로 해당 기능들 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { Console } from '@woowacourse/mission-utils';
-import { INPUT } from './Constants/Message.js';
+import { Random, Console } from '@woowacourse/mission-utils';
+import { INPUT, OUTPUT } from './Constants/Message.js';
 import { BasicValidation } from './Validation.js';
+import Lotto from './Domain/Lotto.js';
 
 class App {
   #basicValidation;
@@ -21,8 +22,41 @@ class App {
     }
   }
 
+  drawRandomLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  singleLottoTicket() {
+    const numbers = this.drawRandomLottoNumbers();
+    return new Lotto(numbers.sort((a, b) => a - b));
+  }
+
+  purchaseLottoTickets(purchaseMoney) {
+    const purchaseTickets = Array.from({ length: purchaseMoney / 1000 }).map(
+      () => {
+        return this.singleLottoTicket();
+      }
+    );
+
+    return purchaseTickets;
+  }
+
+  printPurchaseLottoCount(purchaseTickets) {
+    Console.print(`${purchaseTickets.length}${OUTPUT.ticketNumber}`);
+  }
+
+  printPurchaseLottoNumbers(purchaseTickets) {
+    purchaseTickets.forEach((purchaseTickets) =>
+      purchaseTickets.printLottoNumbers()
+    );
+  }
+
   async run() {
     const purchaseMoney = await this.getPurchaseMoney();
+    const purchaseTickets = this.purchaseLottoTickets(purchaseMoney);
+
+    this.printPurchaseLottoCount(purchaseTickets);
+    this.printPurchaseLottoNumbers(purchaseTickets);
   }
 }
 
