f9fc03fae449a34c06e50f375cd4046a78200cd6
Jee Hwan <jeehwan98@naver.com>
2024-11-01 13:44:33 +0900

    feat: constant 추가

---
+++ src/util/constant.js
@@ -0,0 +1,39 @@
+const INPUT = Object.freeze({
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+});
+
+const OUTPUT = Object.freeze({
+  PURCHASED_TICKETS: (input) => {
+    `${input}개를 구매했습니다.`
+  }
+})
+
+const LOTTO = Object.freeze({
+  PRIZE: 1000,
+  COUNT: 6,
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+});
+
+const PRIZE = Object.freeze({
+  FIFTH_PLACE: 5000,
+  FOURTH_PLACE: 50000,
+  THIRD_PLACE: 1500000,
+  SECOND_PLACE: 30000000,
+  FIRST_PLACE: 2000000000,
+});
+
+const ERROR = Object.freeze({
+  INVALID_AMOUNT: '[ERROR] 구입 금액은 1,000원 단위여야 합니다.',
+  EMPTY_QUERY: '[ERROR] 값을 입력해야 합니다.',
+})
+
+export {
+  INPUT,
+  OUTPUT,
+  LOTTO,
+  PRIZE,
+  ERROR
+};
