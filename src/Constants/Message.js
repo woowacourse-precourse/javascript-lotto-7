2134f05bc04267ac8468bd543899236a1b5614c0
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-01 22:44:02 +0900

    chore: 파일 이름 변경 및 결과 출력 메시지 상수 파일 이동

--- src/Constants/Message.js
+++ src/Constants/Message.js
@@ -5,3 +5,11 @@ export const PROGRESS_MESSAGE = Object.freeze({
   PURCHASE_RESULT: '개를 구매했습니다.',
   WINNING_RESULT: '당첨 통계\n---',
 });
+
+export const LOTTO_RESULT_MESSAGES = Object.freeze({
+  FIRST: '6개 일치 (2,000,000,000원) - ',
+  SECOND: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  THIRD: '5개 일치 (1,500,000원) - ',
+  FOURTH: '4개 일치 (50,000원) - ',
+  FIFTH: '3개 일치 (5,000원) - ',
+});
