eb2b3335709a0c62e99c8166d7055cffde8dd6dd
oko_jin <dswvgw6412@naver.com>
2024-11-01 18:21:40 +0900

    refactor(testCase): 로또 클래스 테스트 케이스 상수 배열로 관리

---
+++ src/constant/LottoTestCase.js
@@ -0,0 +1,24 @@
+import { errorMessage } from './errorMessage.js';
+
+export const LottoTestCase = [
+  {
+    numbers: [1, 2, 3, 4, 5, 6, 7],
+    description: '로또 번호의 개수가 6개가 넘어가면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
+  },
+  {
+    numbers: [1, 2, 3, 4, 5, 5],
+    description: '로또 번호에 중복된 숫자가 있으면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.duplicatedNumber}`,
+  },
+  {
+    numbers: [1, 2, 3, 4, 5],
+    description: '로또 번호의 개수가 6개보다 적으면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
+  },
+  {
+    numbers: [1, 2, 3, 4, 5, 55],
+    description: '로또 번호에 1~45 범위를 벗어나는 숫자가 있으면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidNumber}`,
+  },
+];
