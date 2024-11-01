492a06c9d5ccf22867071b25c6f95bfc73d10e30
hamo-o <yheel369@gmail.com>
2024-11-01 21:13:03 +0900

    feat(InputView,App): 당첨 번호 입력받기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import InputView from "./views/InputView.js";
 import LottoStore from "./LottoStore.js";
+import LottoPrize from "./LottoPrize.js";
 
 class App {
+  lottoPrize;
+
+  constructor() {
+    this.lottoPrize = new LottoPrize();
+  }
+
   async run() {
     const money = await InputView.getLottoMoney();
     const lottoStore = new LottoStore(money);
+    const numbers = await InputView.getWinningNumbers();
+    this.lottoPrize.createWinningNumbers(numbers);
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -5,6 +5,10 @@ const InputView = {
     const data = MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
     return data;
   },
+  async getWinningNumbers() {
+    const data = MissionUtils.Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    return data;
+  },
 };
 
 export default InputView;
