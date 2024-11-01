83ebc31f03a3480424e58c7e7fe238dfd4956a64
silver <ekdldkaa@naver.com>
2024-11-01 16:24:57 +0900

    ✨feat(Lotto) : 생성자 오름차순 정렬, 필드값 리턴
    로또 인스턴스 생성 시, numbers를 오름차순 정렬하고 private field인 numbers를 얻는 함수 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,26 @@
+import { MissionUtils, Random } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 로또 번호 중복이 없어야 합니다');
     }
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
