6a6c906f977e22ff5fba9177338f5350c5868f95
Parksangwoo <psu8430@gmail.com>
2024-11-02 04:05:29 +0900

    feat(validate): 특정 값이 0인지 검증하는 로직 추가

--- src/utils/Valldator.js
+++ src/utils/Valldator.js
@@ -4,6 +4,12 @@ class Validator {
   static isNumber(value) {
     if (isNaN(value)) throw new Error(`${ERROR_PREFIX} 숫자만 입력해주세요.`);
   }
+
+  static isZero(value) {
+    if (value == 0) {
+      throw new Error('[ERROR] 0은 유효하지 않은 값입니다.');
+    }
+  }
 }
 
 export default Validator;
