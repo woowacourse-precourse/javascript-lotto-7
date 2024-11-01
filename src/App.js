628fc60734b5dde9c1762566fd0bb5321c1f19f7
Parksangwoo <psu8430@gmail.com>
2024-11-01 19:05:36 +0900

    feat(input) : 로또 구입금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  #cash;
+
+  async run() {
+    // 1. 구입 금액을 입력받는다.
+    this.#cash = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  }
 }
 
 export default App;
