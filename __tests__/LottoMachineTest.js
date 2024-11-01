2b2a81ea8430165c74f0d053c8a6d9b818e5510a
H-sooyeon <wndqhr__@naver.com>
2024-11-01 17:01:17 +0900

    refactor: 로또 가격으로 머신 작동

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -2,7 +2,7 @@ import LottoMachine from '../src/components/LottoMachine.js';
 
 describe('LottoMachine 클래스 테스트', () => {
   test('구입한 로또의 개수만큼 로또를 발행한다.', () => {
-    const lottoMachine = new LottoMachine(3);
+    const lottoMachine = new LottoMachine(3000);
     expect(lottoMachine.getLottoList().length).toBe(3);
   });
 });
