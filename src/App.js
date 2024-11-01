b9f9d706cb40573312d9b0aed2539e7a55e10d81
Ray <rlawlsdn9583@gmail.com>
2024-11-02 02:29:02 +0900

    feat: 구입 금액 입력 후 로또 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js"
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseMoney = await Console.readLineAsync(
+      "구입금액을 입력해 주세요."
+    );
+    const tickets = Array.from({ length: purchaseMoney / 1000 }, () =>
+      MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+    );
+    const lottos = tickets.map((ticket) => new Lotto(ticket));
+
+    Console.print(`${tickets.length}개를 구매했습니다.`);
+    
+    // const winningNumber = await Console.readLineAsync(
+    //   "당첨 번호를 입력해 주세요."
+    // );
+
+    // const bonusNumber = await Console.readLineAsync(
+    //   "보너스 번호를 입력해 주세요."
+    // );
+  }
 }
 
 export default App;
