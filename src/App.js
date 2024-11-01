96c17e40b80f84c1fa503db6b26b0dd2809f65d0
ho8ae <xogh2242@gmail.com>
2024-11-01 12:42:19 +0900

    feat: 보너스 번호 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App {
     Console.print(`\n${lottoCount}개를 구매했습니다.`);
     this.#generateLottoNumbers(lottoCount);
     const winningNumbers = await this.#readWinningNumbers();
+    const bonusNumber = await this.#readBonusNumber();
   }
 
-  async #readWinningNumbers() {
-    const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
-    return input.split(",").map((number) => Number(number.trim()));
+  async #readAmount() {
+    const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    return Number(input);
   }
 
   #generateLottoNumbers(count) {
@@ -22,8 +23,13 @@ class App {
     }
   }
 
-  async #readAmount() {
-    const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+  async #readWinningNumbers() {
+    const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
+    return input.split(",").map(number => Number(number.trim()));
+  }
+
+  async #readBonusNumber() {
+    const input = await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
     return Number(input);
   }
 }
