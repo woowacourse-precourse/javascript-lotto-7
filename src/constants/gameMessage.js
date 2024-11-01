dec98702f41b48db0ff0b7965356fab3dc41b06a
장정안 <wjddks96@naver.com>
2024-11-01 14:05:25 +0900

    feat: 정적인 게임 메세지 추가

--- src/constants/gameMessage.js
+++ src/constants/gameMessage.js
@@ -1,5 +1,10 @@
 const GAME_MESSAGE = Object.freeze({
   PURCHASE: '구입금액을 입력해 주세요.\n',
+  BOUGHT: `개를 구매했습니다.`,
+  WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  WINNING_STATISTICS: '당첨 통계',
+  DIVIDING_LINE: '---',
 });
 
 export { GAME_MESSAGE };
