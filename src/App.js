398e8e1067fb28f21e823b8d348d26a259a99cbb
Jiny <moc24@naver.com>
2024-11-02 00:30:18 +0900

    test(ApplicationTest.js) : 로또 구입 개수 변수 선언

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Console, MissionUtils } from '@woowacourse/mission-utils';
-
+import { getLottoBuyCount } from './functions/LottoMake';
 class App {
   async run() {
     try {
-      // * 변수 선언 ============== //
-
-      // * ========================== //
-      // * 입력 =================== //
+      // * 로또 구입
       const lottoBuyMoneyInput = await Console.readLineAsync(
         '구입금액을 입력해 주세요.\n',
       );
-      // * ======================= //
+      const lottoBuyCount = getLottoBuyCount(lottoBuyMoneyInput);
+      const lottoArray = [];
+
+      // * ==== //
     } catch (error) {}
   }
 }
