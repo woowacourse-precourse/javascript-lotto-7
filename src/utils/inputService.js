b68ceef92e63fbf1fc80b6900e9ba11c302d84a9
oko_jin <dswvgw6412@naver.com>
2024-11-01 20:59:25 +0900

    feat(input): 보너스 번호 입력 함수 구현

--- src/utils/inputService.js
+++ src/utils/inputService.js
@@ -1,5 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
-import { validateAmount, validateNumbers } from '../Validator.js';
+import { validateAmount, validateBonusNumber, validateNumbers } from '../Validator.js';
 import { inputMessage } from '../constant/ioMessage.js';
 import Lotto from '../Lotto.js';
 
@@ -15,4 +15,10 @@ export async function inputWinningLotto() {
 
   const winningNumbers = new Lotto(inputNumbers.split(',').map(Number));
   return winningNumbers;
+}
+
+export async function inputBonusNumber() {
+  const bonusNumber = await Console.readLineAsync(`${inputMessage.bonus}`);
+  validateBonusNumber(bonusNumber);
+  return parseInt(bonusNumber, 10);
 }
