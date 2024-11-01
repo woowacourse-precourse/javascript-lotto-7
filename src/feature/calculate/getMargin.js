e1f78c150ae27dabda8fd70af1a1f658823f3206
thumbthing <ilscc0902@gmail.com>
2024-11-01 16:44:08 +0900

    Fix: 불필요한 매개변수 삭제, 반환값 누락 수정

--- src/feature/calculate/getMargin.js
+++ src/feature/calculate/getMargin.js
@@ -1,8 +1,9 @@
-function getMarginSum(winningStats, purchase) {
+function getMarginSum(winningStats) {
   const VALUES = [...winningStats.values()];
   const MARGIN_SUM = VALUES.reduce((acc, currentValue) => {
     const SINGLE_MARGIN = currentValue.count * currentValue.winningAmount;
     acc += SINGLE_MARGIN;
+    return acc;
   }, 0);
   return MARGIN_SUM;
 };
