c144c34f96a6d71eb331cb36bb514d30969714fc
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-01 22:48:09 +0900

    feat: 로또 상금 상수로 정의

--- src/Constants/LottoConstants.js
+++ src/Constants/LottoConstants.js
@@ -6,3 +6,11 @@ export const BOUND = Object.freeze({
   LOWER: 1,
   UPPER: 45,
 });
+
+export const LOTTO_REWARD = Object.freeze({
+  FIRST: 2000000000,
+  SECOND: 30000000,
+  THIRD: 1500000,
+  FOURTH: 50000,
+  FIFTH: 5000,
+});
