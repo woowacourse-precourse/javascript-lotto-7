348d1f6a7fb32b5aa49a4fe0e5c372e9da41e33b
thisisyejin <yejink321@gmail.com>
2024-11-02 00:51:19 +0900

    feat: Lotto 생성자 유효성 검증
    로또 클래스 테스트를 통과하기 위해, Lotto의 생성자가
    유효하지 않은 숫자에 대해 Error를 던지도록
    구현하였습니다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,11 +10,24 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    numbers.forEach(element => {
+      // 로또 범위 내 숫자인지 검증
+      Lotto.isValidNumber(element);
+      
+      // 중복된 숫자인지 검증
+      if (numbers.indexOf(element) !== numbers.lastIndexOf(element)) {
+        throw new Error("[ERROR] 로또 번호는 중복 입력할 수 없습니다.");
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
   get numbers() {
     return this.#numbers;
+  }
+
+  static isValidNumber(number) {
+    if (number < 1 || number > 45) throw Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
   }
 }
 
