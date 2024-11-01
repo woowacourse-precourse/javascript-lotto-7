0babe721c3a39801f0be2ca8f5ec7677768436bd
ha-kuku <hakukudh@gmail.com>
2024-11-01 18:09:38 +0900

    refactor: 로또 내부 동작 클래스로 추출 - 당첨 번호 관련 setWinningNumbers 메서드 생성 - 보너스 번호 관련 setBonusNumber 메서드 생성

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import { Random } from '@woowacourse/mission-utils';
-import {
-  BasicValidation,
-  WinningNumberValidation,
-  BonusNumberValidation,
-} from './Validation.js';
-import Lotto from './Domain/Lotto.js';
 import Input from './view/Input.js';
 import Output from './view/Output.js';
 import User from './Domain/user.js';
+import LOTTO_MACHINE from './Domain/LottoMachine.js';
 
 class App {
   #input;
   #output;
+  #lottomachine;
 
   constructor() {
     this.#input = new Input();
     this.#output = new Output();
+    this.#lottomachine = new LOTTO_MACHINE();
   }
 
   async getPurchaseMoney(user) {
@@ -30,124 +26,50 @@ class App {
     }
   }
 
-  validateWinningNumbers(input) {
-    BasicValidation.InputBlank(input);
-    WinningNumberValidation.InputSeparator(input);
-
-    const numbers = input.split(',').map((number) => {
-      return Number(number);
-    });
-
-    WinningNumberValidation.InputOverlap(numbers);
-    BasicValidation.InputLength(numbers, 6);
-
-    numbers.forEach((number) => {
-      BasicValidation.InputNumberType(number);
-      WinningNumberValidation.InputLottoRange(number);
-    });
-  }
-
   async getWinningNumbers() {
     while (true) {
       try {
         const winningNumbers = await this.#input.winningNumbers();
-
-        this.validateWinningNumbers(winningNumbers);
-
-        const winningNumbersArray = winningNumbers
-          .split(',')
-          .map((winningNumber) => {
-            return Number(winningNumber);
-          });
-
-        return winningNumbersArray;
+        this.#lottomachine.setWinningNumbers(winningNumbers);
+        break;
       } catch (err) {
         this.#output.error(err.message);
       }
     }
   }
 
-  validateBonusNumber(winningNumbers, input) {
-    BasicValidation.InputBlank(input);
-
-    const number = Number(input);
-
-    BasicValidation.InputNumberType(number);
-    BasicValidation.InputLength(number, 1);
-    BonusNumberValidation.InputOverlap(number, winningNumbers);
-  }
-
-  async getBounsNumber(winningNumbers) {
+  async getBounsNumber() {
     while (true) {
       try {
         const bonusNumber = await this.#input.bonusNumbers();
-
-        this.validateBonusNumber(winningNumbers, bonusNumber);
-
-        return bonusNumber;
+        this.#lottomachine.setBonusNumber(bonusNumber);
+        break;
       } catch (err) {
         this.#output.error(err.message);
       }
     }
-  }
-
-  drawRandomLottoNumbers() {
-    return Random.pickUniqueNumbersInRange(1, 45, 6);
-  }
-
-  singleLottoTicket() {
-    const numbers = this.drawRandomLottoNumbers();
-    return new Lotto(numbers.sort((a, b) => a - b));
-  }
-
-  purchaseLottoTickets(user) {
-    const tickets = Array.from({ length: user.getMoney() / 1000 }).map(() => {
-      return this.singleLottoTicket();
-    });
-
-    user.setTickets(tickets);
-  }
-
-  calculateWinningResult(tickets, winningNumbers, bonusNumber) {
-    const results = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
-
-    tickets.forEach((ticket) => {
-      const rank = ticket.calculateWinningLotto(winningNumbers, bonusNumber);
-      results[rank] += 1;
-    });
-
-    return results;
-  }
-
-  calculateTotalReturn(results, purchaseMoney) {
-    const total =
-      results['1'] * 2000000000 +
-      results['2'] * 30000000 +
-      results['3'] * 1500000 +
-      results['4'] * 50000 +
-      results['5'] * 5000;
-    return (total / purchaseMoney) * 100;
   }
 
   async run() {
     const user = new User();
 
     await this.getPurchaseMoney(user);
-    this.purchaseLottoTickets(user);
+    this.#lottomachine.purchaseLottoTickets(user);
 
     this.#output.lottoTicketCount(user.getTickets);
     this.#output.lottoTicketNumbers(user.getTickets);
 
-    const winningNumbers = await this.getWinningNumbers();
-    const bonusNumber = await this.getBounsNumber(winningNumbers);
+    await this.getWinningNumbers();
+    await this.getBounsNumber();
 
-    const results = this.calculateWinningResult(
-      user.getTickets(),
-      winningNumbers,
-      bonusNumber
+    const results = this.#lottomachine.calculateWinningResult(
+      user.getTickets()
+    );
+    const totalReturn = this.#lottomachine.calculateTotalReturn(
+      results,
+      user.getMoney()
     );
 
-    const totalReturn = this.calculateTotalReturn(results, user.getMoney);
     this.#output.winningResult(results);
     this.#output.totalReturnResult(totalReturn);
   }
---
+++ src/Domain/LottoMachine.js
@@ -0,0 +1,95 @@
+import { Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import {
+  BasicValidation,
+  WinningNumberValidation,
+  BonusNumberValidation,
+} from '../Validation.js';
+
+class LOTTO_MACHINE {
+  #winningNumbers;
+  #bonusNumber;
+
+  constructor() {
+    this.#winningNumbers = [];
+    this.#bonusNumber = 0;
+  }
+
+  #parseWinningNumbers(winningNumbers) {
+    return winningNumbers.split(',').map((number) => {
+      return Number(number);
+    });
+  }
+
+  #validateWinningNumbers(winningNumbers) {
+    WinningNumberValidation.InputSeparator(winningNumbers);
+
+    const winningNumbersArray = this.#parseWinningNumbers(winningNumbers);
+
+    WinningNumberValidation.InputOverlap(winningNumbersArray);
+    BasicValidation.inputLength(winningNumbersArray, 6);
+
+    winningNumbersArray.forEach((number) => {
+      WinningNumberValidation.InputNumberType(number);
+      WinningNumberValidation.InputLottoRange(number);
+    });
+  }
+
+  #validateBonusNumber(bonusNumber) {
+    BasicValidation.InputNumberType(bonusNumber);
+    BasicValidation.InputLength(bonusNumber, 1);
+    BonusNumberValidation.InputOverlap(bonusNumbernumber, this.#winningNumbers);
+  }
+
+  setWinningNumbers(winningNumbers) {
+    this.#validateWinningNumbers(winningNumbers);
+    this.#winningNumbers = this.#parseWinningNumbers(winningNumbers);
+  }
+
+  setBonusNumber(bonusNumber) {
+    this.#validateBonusNumber(bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  drawRandomLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  singleLottoTicket() {
+    const numbers = this.drawRandomLottoNumbers();
+    return new Lotto(numbers.sort((a, b) => a - b));
+  }
+
+  purchaseLottoTickets(user) {
+    const tickets = Array.from({ length: user.getMoney() / 1000 }).map(() => {
+      return this.singleLottoTicket();
+    });
+
+    user.setTickets(tickets);
+  }
+
+  calculateWinningResult(tickets) {
+    const results = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+
+    tickets.forEach((ticket) => {
+      const rank = ticket.calculateWinningLotto(
+        this.#winningNumbers,
+        this.#bonusNumber
+      );
+      results[rank] += 1;
+    });
+
+    return results;
+  }
+
+  calculateTotalReturn(results, money) {
+    const total =
+      results['1'] * 2000000000 +
+      results['2'] * 30000000 +
+      results['3'] * 1500000 +
+      results['4'] * 50000 +
+      results['5'] * 5000;
+    return (total / money) * 100;
+  }
+}
+export default LOTTO_MACHINE;
