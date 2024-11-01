9d3a45f4321d1d7431077f57de096d88f3a538d1
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-01 22:21:57 +0900

    feat: '당첨 통계'라는 문구와 함께 등수에 해당하는 값을 보여준다

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -10,3 +10,17 @@ export function printLottos(lottos) {
   });
   MissionUtils.Console.print('\n');
 }
+
+export function printPrizes(prizes) {
+  MissionUtils.Console.print('당첨 통계\n---\n');
+
+  for (const [_, prize] of Object.entries(prizes)) {
+    let BONUS_MESSAGE = ', 보너스 볼 일치';
+
+    if (prize.bonus === false) {
+      BONUS_MESSAGE = '';
+    }
+
+    MissionUtils.Console.print(`${prize.matchCount}개 일치${BONUS_MESSAGE} (${prize.money.toLocaleString()}원) - ${prize.count}개}`);
+  }
+}
