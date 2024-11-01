ffcc77788cd8923b3c0fb557c1ad8e711e98f5f1
oko_jin <dswvgw6412@naver.com>
2024-11-01 18:20:50 +0900

    test(Lotto): 로또 클래스 테스트 케이스 상수화

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,12 +1,8 @@
+import { LottoTestCase } from '../src/constant/LottoTestCase.js';
 import Lotto from '../src/Lotto';
 
 describe('로또 클래스 테스트', () => {
-  test.each([
-    { numbers: [1, 2, 3, 4, 5, 6, 7], description: '로또 번호의 개수가 6개가 넘어가면' },
-    { numbers: [1, 2, 3, 4, 5, 5], description: '로또 번호에 중복된 숫자가 있으면' },
-    { numbers: [1, 2, 3, 4, 5], description: '로또 번호의 개수가 6개보다 적으면' },
-    { numbers: [1, 2, 3, 4, 5, 55], description: '로또 번호에 1~45 범위를 벗어나는 숫자가 있으면' },
-  ])('$description 예외가 발생한다.', ({ numbers }) => {
+  test.each(LottoTestCase)('$description 예외가 발생한다.', ({ numbers }) => {
     expect(() => {
       new Lotto(numbers);
     }).toThrow('[ERROR]');
