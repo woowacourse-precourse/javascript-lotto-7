68c4faf4fb86e698818a6c7f474ecbc0b4a4ab52
jeonhyeonseon <good_pretty@naver.com>
2024-11-01 15:08:10 +0900

    chore: ESLint 및 Prettier 설정 파일 추가

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,       // 따옴표를 홑따옴표로 설정
+  semi: true,              // 문장 끝에 세미콜론 추가
+  tabWidth: 2,             // 들여쓰기 너비를 2칸으로 설정
+  trailingComma: "all",    // 가능한 경우 모든 항목에 후행 쉼표 추가
+  printWidth: 80           // 한 줄에 80자를 넘지 않도록 설정
+};
