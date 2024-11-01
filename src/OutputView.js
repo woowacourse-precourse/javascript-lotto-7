532b09b3323a0ef32495c12276d47332367bacd1
dudcns02 <duddl2460@naver.com>
2024-11-02 01:14:24 +0900

    feat: Add winning statistics output function

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,3 +2,6 @@ import { Console } from "@woowacourse/mission-utils";
 
 const printLottoCont = Console.print(numberOfOrder(a).NUBER_OF_ORDER);
 // ${a}개를 구매했습니다.
+const printWinningStatistics = Console.print(LOG_MESSAGE.WINNING_STATISTICS);
+///당첨통계
+///---
