baad6e91269af5f38ed5bf0805961f502a17c667
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-02 00:34:29 +0900

    ✨ feat: 입력 메시지 상수 파일 추가

---
+++ src/contents/InputPrintMessages.js
@@ -0,0 +1,7 @@
+export const INPUT_PRINT_MESSAGES = Object.freeze({
+  money: '구입 금액을 입력해 주세요.\n',
+  winning_number: '당첨 번호를 입력해 주세요.\n',
+  bonus_number: '보너스 번호를 입력해 주세요.\n',
+});
+
+export default INPUT_PRINT_MESSAGES;
