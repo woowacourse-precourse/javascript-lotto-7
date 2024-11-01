a12d42c67438a1ee60686ab2c11d1d65f4235b64
beomjo <dongjun1315@naver.com>
2024-11-01 15:26:56 +0900

    feat(#inputView) : 기본적인 입력 값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { inputCash, inputWinningNumbers, inputBonusNumbers } from "./utils/inputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    const cash = await inputCash();
+    const winningNumbers = await inputWinningNumbers();
+    const BonusNumbers = await inputBonusNumbers();
+  }
 }
 
 export default App;
