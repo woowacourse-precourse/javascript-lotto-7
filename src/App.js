c731b1075942339302919d9fecefdbeac7848322
Kimyerim <yeye092302@gmail.com>
2024-11-02 03:31:11 +0900

    feat(App): 당첨 번호 입력받는 로직 구현
    - 당첨 번호 입력을 위한 Input 인스턴스 생성
    - 당첨 번호 유효성 검사 적용
    - 입력된 당첨 번호 저장 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import  Input  from './Input.js';
-import { validatePurchaseAmount } from './validate.js';
+import { validatePurchaseAmount, validateWinnigNum } from './validate.js';
 import { COMMON_MESSAGE } from './message.js';
 
 class App {
@@ -7,6 +7,10 @@ class App {
     const inputPurchaseAmount = new Input(validatePurchaseAmount.validation);
     await inputPurchaseAmount.inputValue(COMMON_MESSAGE.INPUT_PURCHASE);
     const purchaseAmount = inputPurchaseAmount.getValue();
+
+    const inputWinningNum = new Input(validateWinnigNum.validation);
+    await inputWinningNum.inputValue(COMMON_MESSAGE.INPUT_WINNING_NUM);
+    const winnigNum = inputWinningNum.getValue();
   }
 }
 
