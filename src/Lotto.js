4f8172b2fecc524bc3d6773c3f7eaa37f9bbbe28
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-02 02:48:09 +0900

    [feat]: 로또의 당첨 결과를 가져오는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import LOTTO_RULE from './constant/lotto.js';
+
 class Lotto {
   #numbers;
 
@@ -17,6 +19,56 @@ class Lotto {
 
   getNumbers() {
     return this.#numbers;
+  }
+
+  getWinningResult(winningNumbers, bonusNumber) {
+    const matchCount = this.#numbers.filter((number) =>
+      winningNumbers.includes(number),
+    ).length;
+    const isBonusMatch = this.#numbers.includes(bonusNumber);
+    const rank = this.#getRank(matchCount, isBonusMatch);
+    const reward = this.#getReward(rank);
+
+    return { rank, reward };
+  }
+
+  #getRank(matchCount, isBonusMatch) {
+    switch (matchCount) {
+      case LOTTO_RULE.MATCH_COUNT.FIRST:
+        return LOTTO_RULE.RANK.FIRST;
+      case LOTTO_RULE.MATCH_COUNT.SECOND_OR_THIRD:
+        return this.#getSecondOrThird(isBonusMatch);
+      case LOTTO_RULE.MATCH_COUNT.FOURTH:
+        return LOTTO_RULE.RANK.FOURTH;
+      case LOTTO_RULE.MATCH_COUNT.FIFTH:
+        return LOTTO_RULE.RANK.FIFTH;
+      default:
+        return LOTTO_RULE.RANK.NONE;
+    }
+  }
+
+  #getSecondOrThird(isBonusMatch) {
+    if (isBonusMatch) {
+      return LOTTO_RULE.RANK.SECOND;
+    }
+    return LOTTO_RULE.RANK.THIRD;
+  }
+
+  #getReward(rank) {
+    switch (rank) {
+      case LOTTO_RULE.RANK.FIRST:
+        return LOTTO_RULE.REWARD.FIRST;
+      case LOTTO_RULE.RANK.SECOND:
+        return LOTTO_RULE.REWARD.SECOND;
+      case LOTTO_RULE.RANK.THIRD:
+        return LOTTO_RULE.REWARD.THIRD;
+      case LOTTO_RULE.RANK.FOURTH:
+        return LOTTO_RULE.REWARD.FOURTH;
+      case LOTTO_RULE.RANK.FIFTH:
+        return LOTTO_RULE.REWARD.FIFTH;
+      default:
+        return LOTTO_RULE.REWARD.NONE;
+    }
   }
 }
 
--- src/constant/lotto.js
+++ src/constant/lotto.js
@@ -5,6 +5,28 @@ const LOTTO_RULE = Object.freeze({
     MAX: 45,
   },
   LOTTO_NUMBER_LENGTH: 6,
+  MATCH_COUNT: {
+    FIRST: 6,
+    SECOND_OR_THIRD: 5,
+    FOURTH: 4,
+    FIFTH: 3,
+  },
+  RANK: {
+    FIRST: 1,
+    SECOND: 2,
+    THIRD: 3,
+    FOURTH: 4,
+    FIFTH: 5,
+    NONE: 0,
+  },
+  REWARD: {
+    FIRST: 2000000000,
+    SECOND: 30000000,
+    THIRD: 1500000,
+    FOURTH: 50000,
+    FIFTH: 5000,
+    NONE: 0,
+  },
 });
 
 export default LOTTO_RULE;
