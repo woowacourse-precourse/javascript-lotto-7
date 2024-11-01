257512bba2ebdf444de464da7ee36ba8b948f07a
gyur1kim <qri1114@gmail.com>
2024-11-02 05:28:50 +0900

    test(error): 구입 금액 테스트케이스 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -91,7 +91,7 @@ describe("로또 테스트", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    await runException("1000j");
+  test.each([["1000j"], ["12345"]])("예외 테스트", async (input) => {
+    await runException(input);
   });
 });
