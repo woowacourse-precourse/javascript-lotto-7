63ae6dfb866c12892bf8fdc136b0a5559a87d3e7
seonae-j <wkdtjsdo135@naver.com>
2024-11-01 17:37:05 +0900

    feat(LottoController): 로또 구매 개수 입출력 메서드 추가

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,7 +1,19 @@
+import { INPUT, OUTPUT } from '../constants/Constants.js';
+import { ticketCount } from '../utils/Calculation.js';
+import { Console } from '@woowacourse/mission-utils';
+
 class LottoController {
   #tickets;
 
   constructor() {
     this.#tickets;
   }
+
+  async getTicketCount() {
+    const cost = await Console.readLineAsync(INPUT.COST);
+    this.#tickets = ticketCount(cost);
+    Console.print(`${this.#tickets}${OUTPUT.TICKET}`);
+  }
 }
+
+export default LottoController;
