8c45c9b0c3a64088b3a3ea8d1779a39be087ce51
potalzz <tprhkd1607@naver.com>
2024-11-01 15:42:55 +0900

    chore: 로또 번호 개수 테스트 메시지 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,9 +1,14 @@
 import Lotto from '../src/controller/Lotto';
+import BonusNumber from '../src/model/BonusNumber.js';
 
 describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+  test('로또 번호의 개수가 6개가 아닌 경우 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow('[ERROR]');
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
     }).toThrow('[ERROR]');
   });
 
