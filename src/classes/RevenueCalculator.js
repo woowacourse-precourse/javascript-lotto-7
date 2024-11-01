4f77c12bd571e02ed432648b71427ef940074ee4
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 01:29:10 +0900

    feat: 수익률 계산 기능 구현
    - RevenueCalculator 클래스 추가
    - 구매 비용과 당첨 결과 배열을 입력받아 수익률을 계산

---
+++ src/classes/RevenueCalculator.js
@@ -0,0 +1,22 @@
+import LottoChecker from './LottoChecker.js';
+
+class RevenueCalculator {
+  constructor(totalSpent, winningResults) {
+    this.totalSpent = totalSpent;
+    this.totalPrize = 0;
+    this.winningResults = winningResults;
+  }
+
+  calculateYield() {
+    return (this.totalPrize / this.totalSpent) * 100;
+  }
+
+  calculateTotalPrize() {
+    this.totalPrize = this.winningResults.reduce((total, count, index) => {
+      const prize = LottoChecker.PRIZE_TIERS[index].prize;
+      return total + count * prize;
+    }, 0);
+  }
+}
+
+export default RevenueCalculator;
