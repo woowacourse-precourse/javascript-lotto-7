ca3186ae3c64dca3195bae1898e14cded9068cb7
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-01 14:03:40 +0900

    [feat]: 구매한 수량만큼 새로운 로또를 생성하는 기능 구현

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -1,3 +1,4 @@
+import { Random } from '@woowacourse/mission-utils';
 import LOTTO_RULE from './constant/lotto.js';
 
 class LottoMachine {
@@ -6,11 +7,26 @@ class LottoMachine {
 
   constructor(purchaseAmount) {
     this.#lottoQuantity = this.#calculateLottoQuantity(purchaseAmount);
-    this.#lottos = [];
+    this.#lottos = this.#generateLottos();
   }
 
   #calculateLottoQuantity(purchaseAmount) {
     return purchaseAmount / LOTTO_RULE.AMOUNT_UNIT;
+  }
+
+  #generateLottos() {
+    const lottoList = [];
+
+    for (let i = 0; i < this.#lottoQuantity; i++) {
+      const lotto = Random.pickUniqueNumbersInRange(
+        LOTTO_RULE.LOTTO_NUMBER_RANGE.MIN,
+        LOTTO_RULE.LOTTO_NUMBER_RANGE.MAX,
+        LOTTO_RULE.LOTTO_NUMBER_LENGTH,
+      );
+      lottoList.push(lotto.sort((a, b) => a - b));
+    }
+
+    return lottoList;
   }
 
   getLottoQuantity() {
--- src/constant/lotto.js
+++ src/constant/lotto.js
@@ -1,5 +1,10 @@
 const LOTTO_RULE = Object.freeze({
   AMOUNT_UNIT: 1000,
+  LOTTO_NUMBER_RANGE: {
+    MIN: 1,
+    MAX: 45,
+  },
+  LOTTO_NUMBER_LENGTH: 6,
 });
 
 export default LOTTO_RULE;
