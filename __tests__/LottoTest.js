8f9b563ca57e84fadb044be48148063ecf0878b6
oko_jin <dswvgw6412@naver.com>
2024-11-01 17:54:16 +0900

    test(lotto): 로또 관련 테스트 구현
    - 번호 7개 입력
    - 중복된 숫자 입력
    - 번호 6개 미만 입력
    - 1~45 범위를 벗어나는 숫자 입력

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,17 +1,15 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test.each([
+    { numbers: [1, 2, 3, 4, 5, 6, 7], description: '로또 번호의 개수가 6개가 넘어가면' },
+    { numbers: [1, 2, 3, 4, 5, 5], description: '로또 번호에 중복된 숫자가 있으면' },
+    { numbers: [1, 2, 3, 4, 5], description: '로또 번호의 개수가 6개보다 적으면' },
+    { numbers: [1, 2, 3, 4, 5, 55], description: '로또 번호에 1~45 범위를 벗어나는 숫자가 있으면' },
+  ])('$description 예외가 발생한다.', ({ numbers }) => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
-  });
-
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+      new Lotto(numbers);
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
