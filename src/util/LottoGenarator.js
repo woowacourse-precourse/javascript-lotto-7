bbe6b691616145d05c63063070f2c1c59a5ab499
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-02 01:13:25 +0900

    feat: 무작위 번호를 생성하는 `LottoGenerator` 추가

---
+++ src/util/LottoGenarator.js
@@ -0,0 +1,18 @@
+import { Random } from '@woowacourse/mission-utils';
+import { CONFIG } from '../constants/constants.js';
+
+const generateLotto = () => {
+  const lottoNumbers = new Set();
+
+  while (lottoNumbers.size < CONFIG.LOTTO_COUNT) {
+    const number = Random.pickNumberInRange(
+      CONFIG.LOTTO_MIN_NUMBER,
+      CONFIG.LOTTO_MAX_NUMBER
+    );
+    lottoNumbers.add(number);
+  }
+
+  return [...lottoNumbers];
+};
+
+export default generateLotto;
