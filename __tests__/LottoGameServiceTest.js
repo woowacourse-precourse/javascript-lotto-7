e3e40b5de166bc615dc42e801cafe1ac23656ec7
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-01 12:00:16 +0900

    test: 당첨 결과에 해당하는 등수를 1 증가시킨다

--- __tests__/LottoGameServiceTest.js
+++ __tests__/LottoGameServiceTest.js
@@ -28,6 +28,7 @@ describe('로또 비즈니스 로직 클래스 테스트', () => {
       const prizes = lottoGameService.getPrizes();
       expect((prizes[1].count)).toBe(0);
     });
+
     describe('당첨 로또와 사용자 로또의 일치하는 개수를 가지고 등수를 계산한다.', () => {
       test('일치하는 개수가 5개이고, 보너스 볼이 일치하면 2를 반환한다.', () => {
         expect(lottoGameService.calculateRank(5, true)).toBe(2);
@@ -41,6 +42,12 @@ describe('로또 비즈니스 로직 클래스 테스트', () => {
       test('일치하는 개수가 1개이면, -1을 반환한다.', () => {
         expect(lottoGameService.calculateRank(1, true)).toBe(-1);
       });
+    });
+
+    test('당첨 결과에 해당하는 등수를 1 증가시킨다.', () => {
+      expect(lottoGameService.getPrizes()[1].count).toBe(0);
+      lottoGameService.increaseRankCount(1);
+      expect(lottoGameService.getPrizes()[1].count).toBe(1);
     });
   });
 });
