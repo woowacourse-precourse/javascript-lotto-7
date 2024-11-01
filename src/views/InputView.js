e627112507ec3915d072eddd250fb5781693cbc2
silver <ekdldkaa@naver.com>
2024-11-01 16:27:09 +0900

    ✨feat(InputView) : 입력값 검증 후 에러 시 반복 입력받기

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -3,25 +3,44 @@ import { Console } from '@woowacourse/mission-utils';
 
 const INPUT_MESSAGE = {
   COST: '구입금액을 입력해 주세요.\n',
-  SUCCESS_NUM: '당첨 번호를 입력해 주세요.\n',
-  BONUS_NUM: '보너스 번호를 입력해 주세요.\n',
+  SUCCESS_NUM: '\n당첨 번호를 입력해 주세요.\n',
+  BONUS_NUM: '\n보너스 번호를 입력해 주세요.\n',
 };
 
 const inputView = {
   async getInputCost() {
-    const input = await Console.readLineAsync(INPUT_MESSAGE.COST);
-    validator.validateInputCost(input);
-    return input;
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(INPUT_MESSAGE.COST);
+        validator.validateInputCost(input);
+        return input;
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
   },
   async getSuccessNum() {
-    const input = await Console.readLineAsync(INPUT_MESSAGE.SUCCESS_NUM);
-    validator.validateSuccessNum(input);
-    return input;
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(INPUT_MESSAGE.SUCCESS_NUM);
+        const inputList = input.split(',').map(Number);
+        validator.validateSuccessNum(inputList);
+        return inputList;
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
   },
   async getBonusNum() {
-    const input = await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUM);
-    validator.validateBonusNum(input);
-    return input;
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUM);
+        validator.validateBonusNum(input);
+        return input;
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
   },
 };
 
