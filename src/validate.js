f68d3625c77b7f0586e7b336f5385e5a0dd30349
Kimyerim <yeye092302@gmail.com>
2024-11-02 03:27:52 +0900

    fix(validate): 당첨 번호를 입력받았을 때 유효성 검사 추가
    - 입력 번호가 숫자가 아닌 경우
    - 입력 번호의 범위가 1~45 사이가 아닌 경우
    - 입력 번호가 6개가 아닌 경우

--- src/validate.js
+++ src/validate.js
@@ -35,3 +35,38 @@ export const validatePurchaseAmount = {
     }
 
 };
+
+export const validateWinnigNum = {
+    validation: (inputNum) => {
+        const numbers = inputNum.split(',').map(Number);
+        return(
+            validateWinnigNum.checkIsNumber(numbers) &&
+            validateWinnigNum.checkRange(numbers) &&
+            validateWinnigNum.checkIsSixElements(numbers)
+        );
+    },
+
+    checkIsNumber(value){
+        if(value.some(isNaN)){
+            Console.print(ERROR_MESSAGE.NOT_NUMBER);
+            return false;
+        }
+        return true;
+    },
+    checkRange(value){
+        const isVaild = value.every(num => num >= 1 && num <= 45);
+
+        if(!isVaild){
+            Console.print(ERROR_MESSAGE.NOT_RANGE);
+            return false;
+        }
+        return true;
+    },
+    checkIsSixElements(value){
+        if(value.length !== 6){
+            Console.print(ERROR_MESSAGE.NOT_SIX_ELEMENTS);
+            return false;
+        }
+        return true;
+    }
+}
