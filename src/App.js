9de8f20f8672ff15a01e5f2b1ac3ac1a43f48fd5
Jiny <moc24@naver.com>
2024-11-02 00:04:23 +0900

    feat(App.js) : 로또 구입 금액 입력받기 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // * 변수 선언 ============== //
+
+      // * ========================== //
+      // * 입력 =================== //
+      const lottoBuyMoneyInput = await Console.readLineAsync(
+        '구입금액을 입력해 주세요.\n',
+      );
+
+      // * ======================= //
+    } catch (error) {}
+  }
 }
 
 export default App;
