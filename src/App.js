f61819ae90c24968387e1dc035f8dd1d23e29bc9
ho8ae <xogh2242@gmail.com>
2024-11-01 12:46:54 +0900

    feat: 당첨 결과 출력 기능 구현
    - 당첨 통계 출력 형식 구현
    - 가독성을 위한 코드 흐름 수정

--- src/App.js
+++ src/App.js
@@ -15,30 +15,6 @@ class App {
     this.#matchLottoNumbers();
   }
 
-  #calculateResults(matches) {
-    const results = { 3: 0, 4: 0, 5: 0, "5+": 0, 6: 0 };
-    
-    matches.forEach(({ matchCount, hasBonus }) => {
-      if (matchCount === 6) results[6]++;
-      else if (matchCount === 5 && hasBonus) results["5+"]++;
-      else if (matchCount === 5) results[5]++;
-      else if (matchCount === 4) results[4]++;
-      else if (matchCount === 3) results[3]++;
-    });
-    
-    return results;
-  }
-
-
-  #matchLottoNumbers() {
-    return this.#lottos.map(lotto => {
-      const matchCount = lotto.filter(number => 
-        this.#winningNumbers.includes(number)
-      ).length;
-      const hasBonus = matchCount === 5 && lotto.includes(this.#bonusNumber);
-      return { matchCount, hasBonus };
-    });
-  }
 
   async #readAmount() {
     const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
@@ -63,6 +39,40 @@ class App {
     const input = await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
     return Number(input);
   }
+
+  #printResults(results) {
+    Console.print("\n당첨 통계\n---");
+    Console.print(`3개 일치 (5,000원) - ${results[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${results[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${results[5]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${results["5+"]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${results[6]}개`);
+  }
+
+  #matchLottoNumbers() {
+    return this.#lottos.map(lotto => {
+      const matchCount = lotto.filter(number => 
+        this.#winningNumbers.includes(number)
+      ).length;
+      const hasBonus = matchCount === 5 && lotto.includes(this.#bonusNumber);
+      return { matchCount, hasBonus };
+    });
+  }
+
+  #calculateResults(matches) {
+    const results = { 3: 0, 4: 0, 5: 0, "5+": 0, 6: 0 };
+    
+    matches.forEach(({ matchCount, hasBonus }) => {
+      if (matchCount === 6) results[6]++;
+      else if (matchCount === 5 && hasBonus) results["5+"]++;
+      else if (matchCount === 5) results[5]++;
+      else if (matchCount === 4) results[4]++;
+      else if (matchCount === 3) results[3]++;
+    });
+    
+    return results;
+  }
+
 }
 
 export default App;
