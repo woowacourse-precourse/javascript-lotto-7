b36a073ca8214916418e458846b58d32c80fd299
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-01 23:03:23 +0900

    feat: 보너스 번호와 당첨 번호의 중복을 검사하는 validateBonusNumberUniqueness 함수 추가

--- src/validation/validateLottoNumbers.js
+++ src/validation/validateLottoNumbers.js
@@ -67,4 +67,10 @@ function validateInRange(bonusNumber) {
   }
 }
 
+function validateBonusNumberUniqueness(winningNumbersArray, bonusNumber) {
+  if (winningNumbersArray.includes(Number(bonusNumber))) {
+    throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+  }
+}
+
 export { validateWinningNumbers, validateBonusNumber };
