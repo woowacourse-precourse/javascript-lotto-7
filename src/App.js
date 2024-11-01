434019a012bf1780e3f162ed72fd82db6ca60dde
sanghyeonLee <hyeon012366@gmail.com>
2024-11-02 02:51:17 +0900

    feat: 입력 순서 필드 추가 및 run 반복문 안에서 1 씩 증가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import LotteryMachine from "./LotteryMachine.js";
 import {INPUT} from "./constants/message.js";
 import {Console} from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
+    sequence = 1
+
     async run() {
         for (const prompt of INPUT) {
             const input = await this.input(prompt)
+            const lotto = new Lotto(input.split(","))
+            lotto.purchaseNumbers()
+            this.sequence++
         }
+
     }
 
 
     async input(message) {
-        return await Console.readLineAsync(message)
+        const input = await Console.readLineAsync(message)
+        return this.#validate(input) && input
     }
+
+    #validate(input) { //추후 구현
+        return input
+    }
+
 }
 
 export default App;
