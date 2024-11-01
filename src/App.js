5c1cdea9c6c00a78cd511e7bd24efa971aaafc8d
KIMJINYOUNG <k5444862@naver.com>
2024-11-01 14:26:59 +0900

    refactor: App 클래스에서 불필요한 import 문, 주석 제거

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import LottoMachine from "./LottoMachine.js";
 import Output from "./Output.js";
-import RankChecker from "./RankChecker.js";
 
 class App {
   async run() {
@@ -9,9 +8,6 @@ class App {
     const totalLottoCount = LottoMachine.getTotalLottoCount(userPurchaseAmount);
     const purchasedLottos = await LottoMachine.getLottoNumbers(totalLottoCount);
 
-    // LottoMachine 클래스를 통해 얻은
-    // 구매 금액, 구매한 로또들, 당첨 번호, 구매한 로또 수를
-    // 인자로 받는 Output 클래스
     const output = new Output(
       userPurchaseAmount,
       winningAndBonusNumber,
