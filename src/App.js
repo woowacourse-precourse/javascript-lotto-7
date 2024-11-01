e17f5020d82592dca985300b130ac430052b29ef
ho8ae <xogh2242@gmail.com>
2024-11-01 12:30:27 +0900

    feat: 로또 구매 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const amount = await this.#readAmount();
+  }
+
+  async #readAmount() {
+    const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    return Number(input);
+  }
 }
 
 export default App;
