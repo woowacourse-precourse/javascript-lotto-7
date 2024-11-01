5a7a3ba3430963f4bb5478c0e41db3ef4e686e87
Jang Eunhye <hh961130@naver.com>
2024-11-02 02:17:14 +0900

    feat: (Rank_check)- 당첨 개수 확인

--- src/Rank_check.js
+++ src/Rank_check.js
@@ -14,6 +14,15 @@ class Rank_check {
     ).length;
     return numberMatch;
   }
+
+  countRank(numberMatchArray){
+    let match_three = numberMatchArray.filter(numberMatch => 3 === numberMatch).length;
+    let match_four = numberMatchArray.filter(numberMatch => 4 === numberMatch).length;
+    let match_five = numberMatchArray.filter(numberMatch => 5 === numberMatch).length;
+    let match_five_bonus = 0;
+    let match_six = numberMatchArray.filter(numberMatch => 6 === numberMatch).length;
+  }
+
 }
 
 export default Rank_check;
