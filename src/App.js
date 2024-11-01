6991937b86d814613321615c8858e31eb1ef94f6
guddnboy <hung3018@naver.com>
2024-11-01 20:52:12 +0900

    feat: 금액 입력, 구매 개수 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import { INPUT_MESSAGE, LOTTO_PRICE } from './constants/constants.js';
+
+function purchaseLotto(price) {
+  const amountOfLotto = price / LOTTO_PRICE;
+  return amountOfLotto;
+}
+
 class App {
-  async run() {}
+  async run() {
+    const amount = await Console.readLineAsync(
+      INPUT_MESSAGE.INPUT_AMOUNT_MESSAGE
+    );
+    const amountOfLotto = purchaseLotto(amount);
+    Console.print(`${amountOfLotto}개를 구매했습니다.`);
+
+    const lotto = new Lotto();
+    Console.print(lotto.getNumbers());
+  }
 }
 
 export default App;
