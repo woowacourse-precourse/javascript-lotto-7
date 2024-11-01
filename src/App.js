416d284665d970897283a0aa1f023470dc26b32f
Sohee <sh11govlday@gmail.com>
2024-11-01 13:05:00 +0900

    feat(App) : 당첨 번호 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ class App {
           await Console.readLineAsync('구입금액을 입력해 주세요.\n')
         );
 
-        if (price % 1000 === 0) {
-          break;
+        if (price % 1000 !== 0) {
+          throw new Error(
+            '[ERROR] 로또 구입 금액은 1000원 단위만 입력 가능합니다.'
+          );
         }
 
-        throw new Error(
-          '[ERROR] 로또 구입 금액은 1000원 단위만 입력 가능합니다.'
-        );
+        Console.print('');
+        break;
       } catch (error) {
         Console.print(error.message);
       }
     }
 
     const lottoCount = price / 1000;
-    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+    Console.print(`${lottoCount}개를 구매했습니다.`);
 
     let lottos = [];
     for (let i = 0; i < lottoCount; i++) {
@@ -33,7 +34,29 @@ class App {
       );
     }
     for (let i = 0; i < lottoCount; i++) {
-      Console.print(`[${lottos[i].join(', ')}]`);
+      Console.print(lottos[i]);
+    }
+    Console.print('');
+
+    while (true) {
+      try {
+        const winnerNumbers = (
+          await Console.readLineAsync('당첨 번호를 입력해 주세요.\n')
+        )
+          .split(',')
+          .map(Number);
+
+        if (winnerNumbers.some((number) => number < 1 || number > 45)) {
+          throw new Error(
+            '[ERROR] 로또 번호는 1-45 사이의 숫자로 이루어져야 합니다.'
+          );
+        }
+
+        Console.print('');
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
   }
 }
