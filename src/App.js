6768911a4a7f46a7d6d5c07a006c8aaf1741005a
sooinice <sooin9674@naver.com>
2024-11-01 11:48:48 +0900

    feat : 로또 수량 계산

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App {
     if (lottoPurchaseAmount % 1000 !== 0) {
       throw new Error("[ERROR] : 구입금액이 1000원 단위로 나누어지지 않음.");
     }
+
+    const lottoQuantity = lottoPurchaseAmount / 1000;
+    MissionUtils.Console.print(`${lottoQuantity}개를 구매했습니다.`);
   }
 }
 
