9ba3265fcefc9ca5291ce67f224d0f50c3170861
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-11-01 21:14:24 +0900

    feat: 로또 구입 금액만큼 로또 발행

---
+++ src/LottoArr.js
@@ -0,0 +1,27 @@
+import Lotto from "./Lotto.js"
+import { MissionUtils } from "@woowacourse/mission-utils"
+
+class LottoArr {
+    #lottoArr = []
+    #TIMES
+    #PAY_INPUT
+
+    constructor(PAY_INPUT) {
+        this.#PAY_INPUT = PAY_INPUT
+        this.TIMES = PAY_INPUT / 1000
+    }
+
+    create() {
+        for (let i = 0; i < this.TIMES; i++) {
+            const LOTTO_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+            this.#lottoArr[i] = new Lotto(LOTTO_NUMBERS)
+            MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+        }
+        return this.#lottoArr
+    }
+}
+
+// 테스트 코드
+const lottoArr = new LottoArr(5000);
+const result = lottoArr.create();
+console.log(result);
