df92735f660b337abb782488f01efcb6b0f8960a
ahhyun <wndkgus0225@naver.com>
2024-11-02 01:04:04 +0900

    feat(App) : 구입 금액에 대한 클래스 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+import { getCost } from '../src/IO.js';
+
 class App {
-  async run() {}
+  // 1. 필드 : 이 클래스에서 필요한 속성 정의
+  cost;
+  // 2. 생성자 : 필드를 초기화
+  constructor(){
+    this.cost = 0;
+  }
+  // 3. 메서드 : 클래스의 동작 정의
+  async run() {
+    try{
+      this.cost = await getCost();
+      Console.print(this.cost);
+    } catch(error){
+      Console.print(`[ERROR] 오류가 발생했습니다: ${error.message}`)
+    }
+  }
 }
 
 export default App;
