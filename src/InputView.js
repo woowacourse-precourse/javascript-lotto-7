b729fb383bee75b9cb1002a9e4a165c97aaee119
dudcns02 <duddl2460@naver.com>
2024-11-02 00:22:06 +0900

    feat: Get a winning number as input

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 const price = await Console.readLineAsync(LOG_MESSAGE.START_MESSAGE);
 // price = '8000'
+const winningNumber = await Console.readLineAsync(
+  LOG_MESSAGE.WINNING_NUMBER_MESSAGE
+);
+// winningNuber = '1,2,3,4,5,6'
