5b40048fe14ae758a53f98efca5f66e959741b3a
Yurin Wang <king_1994@naver.com>
2024-11-01 17:03:49 +0900

    refactor: 입출력 중복 로직 제거 및 메시지 상수화 적용 - 중복된 Console.print() 호출을 반복문을 통해 중복을 제거한다. - 입출력 메시지를 상수화 한다.

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,20 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGES } from '../constants/IOMessages.js';
 
 class InputView {
   static async getPurchaseAmount() {
-    const input = await Console.readLineAsync('구입 금액을 입력해주세요.\n');
+    const input = await Console.readLineAsync(INPUT_MESSAGES.purchase_amount);
 
     return input;
   }
 
   static async getWinningNumbers() {
-    const input = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    const input = await Console.readLineAsync(INPUT_MESSAGES.winning_numbers);
 
     return input;
   }
 
   static async getBonusNumber() {
-    const input = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    const input = await Console.readLineAsync(INPUT_MESSAGES.bonus_number);
 
     return input;
   }
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,24 +1,24 @@
 import { Console } from '@woowacourse/mission-utils';
+import { OUTPUT_MESSAGES } from '../constants/IOMessages.js';
 
 class OutputView {
   static printPurchasedLottos(lottos) {
-    Console.print(`\n${lottos.length}개를 구매했습니다.`);
+    Console.print(OUTPUT_MESSAGES.purchased_lottos(lottos.length));
     lottos.forEach((lotto) => {
-      Console.print(`[${lotto.join(', ')}]`);
+      Console.print(OUTPUT_MESSAGES.lotto_numbers(lotto));
     });
   }
 
   static printResult(prizeCounts) {
-    Console.print('\n당첨 통계\n---');
-    Console.print(`3개 일치 (5,000원) - ${prizeCounts[3]}개`);
-    Console.print(`4개 일치 (50,000원) - ${prizeCounts[4]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${prizeCounts[5]}개`);
-    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${prizeCounts['5Bonus']}개`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${prizeCounts[6]}개`);
+    Console.print(OUTPUT_MESSAGES.result_header);
+
+    OUTPUT_MESSAGES.prize_counts.forEach(({ key, message }) => {
+      Console.print(`${message} - ${prizeCounts[key]}개`);
+    });
   }
 
   static printROI(ROI) {
-    Console.print(`총 수익률은 ${ROI}%입니다.`);
+    Console.print(OUTPUT_MESSAGES.roi(ROI));
   }
 
   static printError(message) {
