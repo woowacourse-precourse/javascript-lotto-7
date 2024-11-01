138560107db0acc3f5b0daa85a22ab5340cc8e04
kimywann <Kimywan10@gmail.com>
2024-11-01 13:31:22 +0900

    fix(App): 구입 금액 유효성 검사 조건 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   async run() {
     const purchasePrice = await Console.readLineAsync("구입금액을 입력해 주세요.");
 
-    if (purchasePrice % 1000 === 0) {
+    if (purchasePrice % 1000 !== 0) {
       throw new Error("[ERROR] 1,000원 단위로 나누어 떨어지지 않습니다.")
     }
   }
