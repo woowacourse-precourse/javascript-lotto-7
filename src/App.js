0e5c509d4b8d2cac29d24ad11cd89373aae40db3
ho8ae <xogh2242@gmail.com>
2024-11-01 12:35:08 +0900

    feat: 로또 번호 정렬 기능 구현

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App {
   #generateLottoNumbers(count) {
     for (let i = 0; i < count; i++) {
       const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const sortedNumbers = numbers.sort((a, b) => a - b);
     }
   }
 
