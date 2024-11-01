0c20dddb81dc77bf4b75e25e2a405c17cefb1ad8
Jang Eunhye <hh961130@naver.com>
2024-11-02 02:26:52 +0900

    feat: (Rank_check)- 당첨 개수 출력

--- src/Rank_check.js
+++ src/Rank_check.js
@@ -6,6 +6,7 @@ class Rank_check {
     myLottoArray.forEach((myLotto) =>
       numberMatchArray.push(this.countNumbersMatch(myLotto, numbers))
     );
+    this.countRank(numberMatchArray);
   }
 
   countNumbersMatch(myLotto, numbers) {
@@ -21,8 +22,18 @@ class Rank_check {
     let match_five = numberMatchArray.filter(numberMatch => 5 === numberMatch).length;
     let match_five_bonus = 0;
     let match_six = numberMatchArray.filter(numberMatch => 6 === numberMatch).length;
+    this.printRank(match_three, match_four, match_five, match_five_bonus, match_six);
   }
-
+  
+  printRank(match_three, match_four, match_five, match_five_bonus, match_six) {
+    Console.print("\n당첨 통계");
+    Console.print("---");
+    Console.print("3개 일치 (5000원) - " + match_three + "개")
+    Console.print("4개 일치 (50,000원) - " + match_four + "개")
+    Console.print("5개 일치 (1,500,000원) - " + match_five + "개")
+    Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + match_five_bonus + "개")
+    Console.print("6개 일치 (2,000,000,000원) - " + match_six + "개")
+  }
 }
 
 export default Rank_check;
