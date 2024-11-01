6c593b3dc40367037883e6385e6b73e88624b7b8
mlnyx <nsm110@naver.com>
2024-11-01 20:07:07 +0900

    feat(App): Implement user input for lotto purchase amount
    - Add getPurchaseAmount() method to prompt the user for input.
    - Implement validateAmount() to ensure the purchase amount is in multiples of 1,000.
    - Include error handling for invalid amounts, printing appropriate error messages to the console.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-class App {
-  async run() {}
-}
+import Lotto from "./Lotto.js";
+import { Console } from "@woowacourse/mission-utils"; // 외부 라이브러리 나중
 
+class App {
+  async run() {
+    const purchaseAmount = await this.getPurchaseAmount();
+    const ticketCount = purchaseAmount / 1000;
+  }
+
+  async getPurchaseAmount() {
+    Console.print("구입금액을 입력해 주세요.");
+    const inputPurchase = await Console.readLineAsync("");
+
+    const amount = Number(inputPurchase);
+    this.validateAmount(amount);
+    return amount;
+  }
+
+  validateAmount(amount) {
+    if (amount % 1000 !== 0) {
+      Console.print("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
+    }
+    if (amount <= 0) {
+      Console.print("[ERROR] 정상적인 금액을 입력해주세요.");
+    }
+  }
+}
 export default App;
+
+// - **로또 구입 금액 입력**: 로또 구입 금액을 입력받고,
+// 입력 값이 1,000원 단위로 받을 수 있는 기능 구현
