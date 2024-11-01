5af7f9d34d32444fe5613e94c88a075b5e9ab36b
potalzz <tprhkd1607@naver.com>
2024-11-01 16:12:49 +0900

    feat: 보너스 번호 1개 이상인 경우 검증 기능 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -8,6 +8,7 @@ const ERROR_MESSAGE = {
     '[ERROR] 로또 번호는 중복되지 않은 숫자로 이루어져야 합니다.',
   NUMBER_RANGE: '[ERROR] 로또 번호는 1이상 45이하의 정수로 이루어져야 합니다.',
   BONUS_NUMBER_COUNT: '[ERROR] 보너스 로또 번호는 1개여야 합니다.',
+  BONUS_NUMBER_EXCEEDS_LIMIT: '[ERROR] 보너스 번호는 1개를 초과할 수 없습니다.',
 };
 
 export { INPUT_MESSAGE, ERROR_MESSAGE };
--- src/model/BonusNumber.js
+++ src/model/BonusNumber.js
@@ -3,12 +3,20 @@ import { ERROR_MESSAGE } from '../constants/messages.js';
 class BonusNumber {
   constructor(number) {
     this.#checkEmptyInput(number);
+    this.#checkNumberExceedsLimit(number);
     this.bonusNumber = number;
   }
 
   #checkEmptyInput(number) {
     if (!number || number.trim() === '') {
       throw new Error(ERROR_MESSAGE.BONUS_NUMBER_COUNT);
+    }
+  }
+
+  #checkNumberExceedsLimit(number) {
+    const bonusNumber = number.split(',').map((num) => num.trim());
+    if (bonusNumber.length > 1) {
+      throw new Error(ERROR_MESSAGE.BONUS_NUMBER_EXCEEDS_LIMIT);
     }
   }
 }
