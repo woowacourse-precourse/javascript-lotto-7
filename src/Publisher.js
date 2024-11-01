02ae44ade788ac1f94376a5efa22a76dbc644716
jongbin26 <010jjj@naver.com>
2024-11-01 18:59:13 +0900

    refactor(Publisher): Publisher에서 검증 관련 로직 제거
    - Publisher에서 관리하던 검증 로직을 추출했습니다.
    - 추출을 통해 Publisher 클래스가 로또 발행과 관련된 로직만을 수행합니다.
    - 클래스가 단순하고 명료해지는 장점이 있습니다.

--- src/Publisher.js
+++ src/Publisher.js
@@ -1,42 +1,24 @@
 import Lotto from './Lotto.js';
 import Lottos from './Lottos.js';
-import { Validator } from './Validator.js';
-import { SEPERATOR } from './constants/index.js';
-import {
-  calculateQuatity,
-  generateRandomNumbers,
-  splitBySeperator,
-} from './utils/index.js';
+import { generateRandomNumbers } from './utils/index.js';
 
 export class Publisher {
-  constructor() {
-    this.validator = new Validator();
+
+  #winningNumbers;
+
+  #bonusNumber;
+
+  setLottoNumber = (winningNumbers, bonusNumber) => {
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumber = bonusNumber;
   }
 
-  setPurchaseQuantity = (purchaseAmount) => {
-    this.purchaseQuantity = calculateQuatity(purchaseAmount);
-    this.validator.validatePurchaseAmount(purchaseAmount);
-  };
-
-  setWinningNumbers = (winningNumbersString) => {
-    this.validator.validateWinningNumberString(winningNumbersString);
-
-    this.winningNumbers = splitBySeperator(SEPERATOR, winningNumbersString);
-    this.validator.validateWinningNumbers(this.winningNumbers);
-  };
-
-  setBonusNumbers = (bonusNubmer) => {
-    this.bonusNumber = bonusNubmer;
-    this.validator.validateBonusNumber(this.bonusNumber);
-  };
-
-  generateLotto = () => {
-    const lottoNumbers = Array.from({ length: this.purchaseQuantity }).map(
-      () => {
-        const randomNumbers = generateRandomNumbers();
-        return new Lotto(randomNumbers);
-      },
-    );
+  generateLotto = (purchaseQuantity) => {
+    const lottoNumbers = [];
+    for (let i = 0; i < purchaseQuantity; i++) {
+      const randomNumbers = generateRandomNumbers();
+      lottoNumbers.push(new Lotto(randomNumbers));
+    }
     return new Lottos(lottoNumbers);
   };
 }
