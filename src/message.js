7e0faee8b9fe1921e57db4d0ca240e63b919a1e3
Kimyerim <yeye092302@gmail.com>
2024-11-02 01:30:31 +0900

    feat(message): 메시지 상수 파일 추가
    - 구입금액 입력 메시지, 당첨 번호 입력 메시지, 보너스 번호 입력 메시지, 당첨 통계 메시지 추가

---
+++ src/message.js
@@ -0,0 +1,6 @@
+export const COMMON_MESSAGE = {
+    INPUT_PURCHASE: Object.freeze("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUM: Object.freeze("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUM: Object.freeze("보너스 번호를 입력해 주세요."),
+    WINNING_STATS: Object.freeze("당첨 통계\n---"),
+}
