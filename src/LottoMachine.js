e7515890f47cdb3f5990fd22067ce253336090c0
Seio924 <seio9241@gmail.com>
2024-11-01 16:12:46 +0900

    feat(LottoMachine): 당첨 내역 계산

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -23,6 +23,81 @@ class LottoMachine {
   getLottos() {
     return this.#lottos;
   }
+
+  calculateWinningResults(winningNumbers, bonusNumber, lottos) {
+    const totalWinningRank = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
+
+    lottos.forEach(lotto => {
+      const winningRank = this.calculateWinningResult(
+        winningNumbers,
+        bonusNumber,
+        lotto.getNumbers()
+      );
+
+      switch (winningRank) {
+        case 1:
+          totalWinningRank.first += 1;
+          break;
+        case 2:
+          totalWinningRank.second += 1;
+          break;
+        case 3:
+          totalWinningRank.third += 1;
+          break;
+        case 4:
+          totalWinningRank.fourth += 1;
+          break;
+        case 5:
+          totalWinningRank.fifth += 1;
+          break;
+        default:
+          break;
+      }
+    });
+
+    return totalWinningRank;
+  }
+
+  calculateWinningResult(winningNumbers, bonusNumber, lottoNumbers) {
+    let winningCount = 0;
+    let winningRank;
+
+    lottoNumbers.forEach(lottoNumber => {
+      if (winningNumbers.includes(lottoNumber)) {
+        winningCount += 1;
+      }
+    });
+
+    switch (winningCount) {
+      case 3:
+        winningRank = 5;
+        break;
+      case 4:
+        winningRank = 4;
+        break;
+      case 5:
+        if (lottoNumbers.includes(bonusNumber)) {
+          winningRank = 2;
+          break;
+        }
+        winningRank = 3;
+        break;
+      case 6:
+        winningRank = 1;
+        break;
+      default:
+        winningRank = -1;
+        break;
+    }
+
+    return winningRank;
+  }
 }
 
 export default LottoMachine;
