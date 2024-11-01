9ba3263b313f6c6c4b16932974cbeee073fd08ef
ahhyun <wndkgus0225@naver.com>
2024-11-02 01:03:29 +0900

    feat(message) : 구입 금액에 관한 메시지, 에러메시지 작성

---
+++ src/message.js
@@ -0,0 +1,12 @@
+export const MESSAGE = {
+  ASK_COST: "구입금액을 입력해 주세요."
+}
+
+export const ERROR_MESSAGE = {
+  NOT_NUMBER: "[ERROR] 숫자 이외의 입력은 사용할 수 없습니다.",
+  NOT_INTEGER: "[ERROR] 정수 이외의 입력은 사용할 수 없습니다.",
+  NUM_IS_ZERO : "[ERROR] 0은 입력할 수 없습니다.",
+  NEGATIVE_NUMBER: "[ERROR] 음수는 입력할 수 없습니다.",
+  VALIDATE_DIVERSITY_BY_THOUSAND: "[ERROR] 금액은 1000원 단위이어야 합니다."
+}
+
