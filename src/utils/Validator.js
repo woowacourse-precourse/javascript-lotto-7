7868224bbd48155f87587beb81bb9476e7e270dd
moonseonghui <msh9980@naver.com>
2024-11-01 15:58:09 +0900

    feat(Validator): 정규식 패턴과 일치하는지 확인하는 checkRegexRattern 함수 구현
    - 숫자만 들어오는 경우와 숫자와 컴마가 들어오는 경우를 모두 확인하기 위해서 regex와 에러 메세지를 인자로 받는 형식으로 구현

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -3,6 +3,10 @@ class Validator {
     if (!userInput || !userInput.trim())
       throw new Error('[ERROR] 입력값이 비어 있습니다. 값을 입력해 주세요.');
   }
+
+  static checkRegexPattern(userInput, regex, errorMessage) {
+    if (!regex.test(userInput)) throw new Error(`[ERROR] ${errorMessage}`);
+  }
 }
 
 export default Validator;
