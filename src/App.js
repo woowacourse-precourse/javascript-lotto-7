931fdabe2bdd4aed81e3a491ab163690f5536737
Kimyerim <yeye092302@gmail.com>
2024-11-02 04:15:46 +0900

    feat(App): 보너스 번호 입력 로직 구현
    - 보너스 번호 입력을 위한 Input 인스턴스 생성
    - 보너스 번호 유효성 검사 적용
    - 보너스 번호 숫자로 변환
    - 입력된 보너스 번호 값 저장

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import  Input  from './Input.js';
-import { validatePurchaseAmount, validateWinnigNum } from './validate.js';
+import { validateBonusNum, validatePurchaseAmount, validateWinnigNum } from './validate.js';
 import { COMMON_MESSAGE } from './message.js';
 
 class App {
   async run() {
     const inputPurchaseAmount = new Input(validatePurchaseAmount.validation);
     await inputPurchaseAmount.inputValue(COMMON_MESSAGE.INPUT_PURCHASE);
+    inputPurchaseAmount.changeNum();
     const purchaseAmount = inputPurchaseAmount.getValue();
 
     const inputWinningNum = new Input(validateWinnigNum.validation);
     await inputWinningNum.inputValue(COMMON_MESSAGE.INPUT_WINNING_NUM);
+    inputWinningNum.changeArray();
     const winnigNum = inputWinningNum.getValue();
+
+    const inputBonusNum = new Input((bonusNum) => validateBonusNum.validation(bonusNum, winnigNum));
+    await inputBonusNum.inputValue(COMMON_MESSAGE.INPUT_BONUS_NUM);
+    inputBonusNum.changeNum();
+    const bonusNum = inputBonusNum.getValue();
   }
 }
 
