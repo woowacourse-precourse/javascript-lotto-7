4a1a6b5c1e56ac6f09376c6d3a901246d2b4e938
Kimyerim <yeye092302@gmail.com>
2024-11-02 02:59:31 +0900

    feat(App): 구입 금액 입력받는 로직 구현
    - 구입 금액 입력을 위한 Input 인스턴스 생성
    - 구입 금액 유효성 검사 적용
    - 입력된 구입 금액 값 저장 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import  Input  from './Input.js';
+import { validatePurchaseAmount } from './validate.js';
+import { COMMON_MESSAGE } from './message.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputPurchaseAmount = new Input(validatePurchaseAmount.validation);
+    await inputPurchaseAmount.inputValue(COMMON_MESSAGE.INPUT_PURCHASE);
+    const purchaseAmount = inputPurchaseAmount.getValue();
+  }
 }
 
 export default App;
