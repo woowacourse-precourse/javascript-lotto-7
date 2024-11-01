b9cbcedc83259a5d0144a49a74064e1f2b7bc4be
oko_jin <dswvgw6412@naver.com>
2024-11-01 12:25:40 +0900

    feat(input): 구매 금액 입력 함수 반환 값 설정

--- src/utils/inputService.js
+++ src/utils/inputService.js
@@ -1,7 +1,9 @@
 import { Console } from '@woowacourse/mission-utils';
-import { validateAmount } from '../Validator';
+import { validateAmount } from '../Validator.js';
+import { inputMessage } from '../constant/inputMessage.js';
 
 export async function inputAmount() {
-  const amount = await Console.readLineAsync('구매금액을 입력해주세요.\n');
+  const amount = await Console.readLineAsync(`${inputMessage.amount}`);
   validateAmount(amount);
+  return amount;
 }
