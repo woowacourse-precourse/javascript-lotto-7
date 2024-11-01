95750b7a118f2a8d149ec2da80a3f02af3f2ec92
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-02 08:58:43 +0900

    test: 로또 번호에서 보너스 번호 존재 여부 판단하는 기능 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -18,7 +18,12 @@ describe('로또 클래스 테스트', () => {
   test('로또 번호에서 당첨 번호를 포함하는 개수를 반환한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const WINNING_NUMBER = [1, 2, 3, 7, 8, 9];
-
     expect(lotto.countMatchedWinningNumbers(WINNING_NUMBER)).toEqual(3);
+  });
+
+  test('로또 번호에서 보너스 번호를 포함하면 true를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const BONUS_NUMBER = 4;
+    expect(lotto.hasBunusNumber(BONUS_NUMBER)).toBeTruthy();
   });
 });
