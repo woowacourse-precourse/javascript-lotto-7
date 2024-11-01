5a6c421d09df08eb01812e5ab55e3894a617f29a
thumbthing <ilscc0902@gmail.com>
2024-11-01 15:32:05 +0900

    Feat: 로또 결과 문자열 생성 기능 구현

--- src/feature/parse/parseMap.js
+++ src/feature/parse/parseMap.js
@@ -6,7 +6,23 @@ function addComma(winningAmount) {
   return RESULT_STRING;
 }
 
-export { addComma };
+function getWinningResultText(winningStats) {
+  const KEYS = [...winningStats.keys()];
+  const VALUES = [...winningStats.values()];
+
+  let resultText = `\n당첨 통계\n---`;
+
+  VALUES.forEach((args, index) => { 
+    const MATCH_COUNT = KEYS[index];
+    const PARSED_AMOUNT = addComma(args.winningAmount);
+    const SINGLE_RESULT_STRING = `\n${MATCH_COUNT} ${PARSED_AMOUNT} - ${args.count}개`;
+    resultText += SINGLE_RESULT_STRING;
+  });
+
+  return resultText;
+}
+
+export { addComma, getWinningResultText };
 
 
 
