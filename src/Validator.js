7de18a3ed4637df090b273edc8076868d672539e
jongbin26 <010jjj@naver.com>
2024-11-01 17:04:26 +0900

    feat(Validator): 당첨 번호, 보너스 번호 검증 추가
    - 당첨 번호 문자열과 숫자들에 대한 검증을 각각 나눠서 진행했습니다.
    - 보너스 번호에 대한 검증을 추가했습니다.

--- src/Validator.js
+++ src/Validator.js
@@ -1,4 +1,4 @@
-import { UNIT_LOTTO_PRICE } from "./constants/index.js";
+import { UNIT_LOTTO_PRICE } from './constants/index.js';
 
 export class Validator {
   validatePurchaseAmount = (purchaseAmount) => {
@@ -13,6 +13,32 @@ export class Validator {
     }
     if (parseInt(purchaseAmount, 10) % UNIT_LOTTO_PRICE !== 0) {
       throw new Error('[ERROR] 구입 금액은 1,000원 단위여야 합니다.');
+    }
+  };
+
+  validateWinningNumberString = (winningNumbersString) => {
+    const regex = /^[0-9,]+$/;
+    if (!regex.test(winningNumbersString)) {
+      throw new Error(
+        '[ERROR] 숫자와 구분자를 제외한 문자가 포함되어있습니다.',
+      );
+    }
+  };
+
+  validateWinningNumbers = (winningNumbers) => {
+    winningNumbers.forEach((winningNumber) => {
+      if (parseInt(winningNumber, 10) < 1 || parseInt(winningNumber, 10) > 45) {
+        throw new Error('[ERROR] 로또 번호가 1과 45 사이에 존재하지 않습니다.');
+      }
+    });
+  };
+
+  validateBonusNumber = (bonusNubmer) => {
+    if (bonusNubmer === '') {
+      throw new Error('[ERROR] 보너스  입력되지 않았습니다.');
+    }
+    if (!Number.isInteger(parseFloat(bonusNubmer))) {
+      throw new Error('[ERROR] 구입 금액은 정수이어야 합니다.');
     }
   };
 }
