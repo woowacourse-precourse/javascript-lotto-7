8c303ccf7d2ca8b30251bdb7034e7767219584a3
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-01 22:20:53 +0900

    feat: 사용자 로또를 한 줄에 하나씩 보여준다

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -3,3 +3,10 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 export function printLottoQuantity(lottos) {
   MissionUtils.Console.print(`${lottos.length}개를 구매했습니다.\n`);
 }
+
+export function printLottos(lottos) {
+  lottos.forEach((lotto) => {
+    MissionUtils.Console.print(lotto.printNumbers());
+  });
+  MissionUtils.Console.print('\n');
+}
