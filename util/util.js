3a79a70c25d844f7b79950e2980edf8a8639e37e
KimYoungWoo <rladuddn3924@gmail.com>
2024-11-01 21:47:01 +0900

    feat: util 함수 업데이트
    matchedBonusNumber: lottoNumbers에서 bonusNumber가 포함된지 여부 확인
    getPrize: prize와 count를 곱한 값을 반환
    getRate: 수익률을 반환(prizePrice, purchasePrice 중 둘 중 하나라도 0이면 0을 반환)
    getReduceValue: 입력된 리스트의 값을 모두 더한 값을 반환
    generateLottoNumbers: 입력된 숫자 만큼 랜덤 로또 번호를 생성 후 반환

--- util/util.js
+++ util/util.js
@@ -1,6 +1,29 @@
+import { Random } from "@woowacourse/mission-utils";
 import { CONSTANT_LOTTO } from "./const";
 
 export const isOutOfRange = (number) =>
   number < CONSTANT_LOTTO.MIN_NUMBER || number > CONSTANT_LOTTO.MAX_NUMBER;
 
 export const isNotANumber = (number) => Number.isNaN(+number);
+
+export const matchedBonusNumber = (lottoNumbers, bonusNumber) =>
+  lottoNumbers.includes(bonusNumber);
+
+export const getPrize = (prize, count) => prize * count;
+
+export const getRate = (prizePrice, purchasePrice) => {
+  if (purchasePrice === 0) {
+    return 0;
+  }
+  return (prizePrice / purchasePrice) * 100;
+};
+
+export const getReduceValue = (prizeList) =>
+  prizeList.reduce((prev, cur) => prev + cur, 0);
+
+export const generateLottoNumbers = (count) => {
+  const lottoNumberDoubleArray = [];
+  for (let i = 0; i < count; i++) {
+    lottoNumberDoubleArray.push(Random.pickUniqueNumbersInRange(1, 45, 6));
+  }
+};
