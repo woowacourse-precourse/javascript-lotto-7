3ad40e8d218690fa4a0e2f62afd7f79aee5325da
mlnyx <nsm110@naver.com>
2024-11-02 04:14:49 +0900

    feat(App): Improve error handling
    Added validateWinningNumbers method to include checks for:
      Duplicate numbers among the winning numbers.
      Ensuring all input values are numbers.
      Verifying that all numbers are within the range of 1 to 45.
    
    Throw appropriate error messages for each validation failure to improve user feedback.
    
    Corrected the list of error handling functions

--- src/App.js
+++ src/App.js
@@ -24,16 +24,13 @@ class App {
 
   validateAmount(amount) {
     if (isNaN(amount)) {
-      Console.print("[ERROR] 금액은 숫자여야 합니다.");
-      return;
+      throw new Error("[ERROR] 금액은 숫자여야 합니다.");
     }
     if (amount % 1000 !== 0) {
-      Console.print("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
-      return;
+      throw new Error("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
     }
     if (amount <= 0) {
-      Console.print("[ERROR] 정상적인 금액을 입력해주세요.");
-      return;
+      throw new Error("[ERROR] 정상적인 금액을 입력해주세요.");
     }
   }
 
@@ -48,6 +45,8 @@ class App {
     Console.print("당첨 번호를 입력해 주세요.");
     const winningNumbersInput = await Console.readLineAsync("");
     const winningNumbersArr = winningNumbersInput.split(",").map(Number);
+    this.validateWinningNumbers(winningNumbersArr);
+
     Console.print("");
     return winningNumbersArr;
   }
@@ -57,6 +56,22 @@ class App {
     const bonusNumberInput = await Console.readLineAsync("");
     Console.print("");
     return Number(bonusNumberInput);
+  }
+
+  validateWinningNumbers(winningNumbers) {
+    const uniqueNumbers = new Set(winningNumbers);
+    if (isNaN(amount)) {
+      throw new Error("[ERROR] 금액은 숫자여야 합니다.");
+    }
+    if (uniqueNumbers.size !== 6) {
+      throw new Error("[ERROR] 중복된 숫자가 포함되어 있습니다.");
+    }
+
+    winningNumbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 숫자는 1에서 45 사이여야 합니다.");
+      }
+    });
   }
 }
 export default App;
