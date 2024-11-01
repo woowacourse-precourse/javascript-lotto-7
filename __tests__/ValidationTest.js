8b9c3e7313ca031405e593ece4fde0b6e5e4eaa0
seonae-j <wkdtjsdo135@naver.com>
2024-11-01 18:47:12 +0900

    test(ValidationTest): 구매 금액 입력에 대한 예외 테스트 추가

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,22 @@
+import { validateCost } from '../src/utils/Validation.js';
+import { ERROR } from '../src/constants/Constants.js';
+
+describe('로또 컨트롤 클래스 테스트', () => {
+  test('사용자가 입력한 로또 구매 금액이 숫자가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      validateCost('10a');
+    }).toThrow(ERROR.COST_TYPE);
+  });
+
+  test('사용자가 입력한 로또 구매 금액이 1,000원 단위가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      validateCost(12345);
+    }).toThrow(ERROR.COST_UNIT);
+  });
+
+  test('사용자가 입력한 로또 구매 금액이 정수가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      validateCost(12.35);
+    }).toThrow(ERROR.COST_UNIT);
+  });
+});
