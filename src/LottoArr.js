78723a115fcf1a8de9d72b277e0d95dd2c8029bb
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-11-01 23:32:14 +0900

    feat: 발행 횟수, 발행된 로또 출력
    Lotto 클래스의 일부 Error throw도 구현했다.

--- src/LottoArr.js
+++ src/LottoArr.js
@@ -9,22 +9,32 @@ class LottoArr {
     constructor(PAY_INPUT) {
         this.#PAY_INPUT = PAY_INPUT
         this.#TIMES = this.#calculateTIMES()
+        this.#getLottoArr()
     }
 
     #calculateTIMES() {
         return this.#PAY_INPUT / 1000
     }
 
-    create() {
+    #getLottoArr() {
         for (let i = 0; i < this.#TIMES; i++) {
-            const LOTTO_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+            const LOTTO_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b)
             this.#lottoArr[i] = new Lotto(LOTTO_NUMBERS)
         }
-        return this.#lottoArr
     }
+
+    print() {
+        let result = this.#TIMES + "개를 구매했습니다.\n"
+
+        for (let i = 0; i < this.#lottoArr.length; i++) {
+            result += this.#lottoArr[i] + "\n"
+        }
+        return result
+    }
+
 }
 
 // 테스트 코드
 const lottoArr = new LottoArr(5000);
-const result = lottoArr.create();
-console.log(result);
+const result = lottoArr.print();
+
