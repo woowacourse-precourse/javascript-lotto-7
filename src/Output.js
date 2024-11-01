2cbd392599d740a35e8482e785a4bae6a42f5d93
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-02 03:35:25 +0900

    [feat]: 당첨 통계 출력하는 기능 구현

--- src/Output.js
+++ src/Output.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 
+import LOTTO_RULE from './constant/lotto.js';
 import OUTPUT_MESSAGES from './constant/output.js';
 
 class Output {
@@ -9,7 +10,7 @@ class Output {
     }`;
   }
 
-  static printProfitRate(profitRate) {
+  static #printProfitRate(profitRate) {
     const profitRateMessage = this.#getProfitRateMessage(profitRate);
     Console.print(profitRateMessage);
   }
@@ -24,6 +25,22 @@ class Output {
 
   static printErrorMessage(errorMessage) {
     Console.print(errorMessage);
+  }
+
+  static printWinningResult(rankCounts, profitRate) {
+    Console.print(OUTPUT_MESSAGES.WINNING_STATS.HEAD_MESSAGE);
+    this.#printRankStats(rankCounts);
+    this.#printProfitRate(profitRate);
+  }
+
+  static #printRankStats(rankCounts) {
+    OUTPUT_MESSAGES.PRINT_ORDER.forEach((rankKey) => {
+      const rank = LOTTO_RULE.RANK[rankKey];
+      const count = rankCounts[rank];
+      Console.print(
+        `${OUTPUT_MESSAGES.WINNING_STATS[rankKey]}${count}${OUTPUT_MESSAGES.WINNING_STATS.COUNT}`,
+      );
+    });
   }
 }
 
