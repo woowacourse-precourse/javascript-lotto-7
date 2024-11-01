8ed80b028c441b4c68cac822d5eb2283f8b07fc9
KimYoungWoo <rladuddn3924@gmail.com>
2024-11-01 22:15:54 +0900

    feat: Price 클래스 추가

---
+++ src/Price.js
@@ -0,0 +1,24 @@
+import Validate from "./Validate.js";
+
+class Price {
+  #price;
+  constructor(price) {
+    this.#validate(price);
+    this.#price = price;
+  }
+
+  #validate(price) {
+    const validate = new Validate();
+    validate.validatePrice(price);
+  }
+
+  get value() {
+    return this.#price;
+  }
+
+  get lottoCount() {
+    return this.#price / 1000;
+  }
+}
+
+export default Price;
