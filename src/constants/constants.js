a5766ba5868aab23c713b7e46ec62b1939fcf695
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-01 22:26:24 +0900

    feat: 상수 설정
    - 로또 미션에 필요한 상수 설정

---
+++ src/constants/constants.js
@@ -0,0 +1,17 @@
+const CONFIG = Object.freeze({
+  PURCHASE_AMOUNT_UNIT: 1000,
+  LOTTO_COUNT: 6,
+  LOTTO_MIN_NUMBER: 1,
+  LOTTO_MAX_NUMBER: 45,
+});
+
+const PRIZE = Object.freeze({
+  1: 2_000_000_000,
+  2: 30_000_000,
+  3: 1_500_000,
+  4: 50_000,
+  5: 5_000,
+  6: 0,
+});
+
+export { CONFIG, PRIZE };
