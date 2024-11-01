ed132c6637a303a658a3bd41e4f26e400a9972b6
hamo-o <yheel369@gmail.com>
2024-11-01 21:09:58 +0900

    feat(InputView,App): 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import InputView from "./views/InputView.js";
+import LottoStore from "./LottoStore.js";
+
 class App {
-  async run() {}
+  async run() {
+    const money = await InputView.getLottoMoney();
+    const lottoStore = new LottoStore(money);
+  }
 }
 
 export default App;
---
+++ src/views/InputView.js
@@ -0,0 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const InputView = {
+  async getLottoMoney() {
+    const data = MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    return data;
+  },
+};
+
+export default InputView;
