c0d46671f20d94d12a88ff8c7a229d839a40943b
oko_jin <dswvgw6412@naver.com>
2024-11-01 12:42:49 +0900

    refactor(input): 구매 금액 입력 후 1줄 개행을 App에서 수행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+import { inputAmount } from './utils/inputService.js';
+import { outputPayment } from './utils/outputService.js';
+
 class App {
-  async run() {}
+  async run() {
+    const amount = await inputAmount();
+    Console.print('');
+    outputPayment(amount);
+  }
 }
 
 export default App;
--- src/utils/inputService.js
+++ src/utils/inputService.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import { validateAmount } from '../Validator.js';
-import { inputMessage } from '../constant/inputMessage.js';
+import { inputMessage } from '../constant/ioMessage.js';
 
 export async function inputAmount() {
   const amount = await Console.readLineAsync(`${inputMessage.amount}`);
