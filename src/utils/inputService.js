9c9262dfc3cd81e0e303ee7eef6fd41404593aff
oko_jin <dswvgw6412@naver.com>
2024-11-01 17:53:27 +0900

    feat(input): 로또 번호 입력 함수 구현

--- src/utils/inputService.js
+++ src/utils/inputService.js
@@ -1,9 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
-import { validateAmount } from '../Validator.js';
+import { validateAmount, validateNumbers } from '../Validator.js';
 import { inputMessage } from '../constant/ioMessage.js';
+import Lotto from '../Lotto.js';
 
 export async function inputAmount() {
   const amount = await Console.readLineAsync(`${inputMessage.amount}`);
   validateAmount(amount);
   return amount;
+}
+
+export async function inputWinningLotto() {
+  const inputNumbers = await Console.readLineAsync(`${inputMessage.lotto}`);
+  validateNumbers(inputNumbers);
+
+  const winningNumbers = new Lotto(inputNumbers.split(',').map(Number));
+  return winningNumbers;
 }
