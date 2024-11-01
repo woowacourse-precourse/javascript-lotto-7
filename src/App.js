a551d78e20b801056cc8344bc788f6951149b06c
jeong922 <hwajeong922@naver.com>
2024-11-01 21:39:34 +0900

    feat(App): 로또 발행하기 기능 구현
    - 구매 금액 / 1000 만큼 발행할 로또 수 계산 로직 추가
    - 로또 수 만큼 1 ~ 45 사이 숫자 중 중복되지 않은 6개의 숫자 뽑아 오름차순으로 정렬 구현
    - 발행된 로또를 배열 형태 문자열로 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+
 class App {
-  constructor() {}
+  constructor() {
+    this.numbers = [];
+  }
 
   async getPurchaseAmount() {
     const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
@@ -20,8 +24,22 @@ class App {
     return true;
   }
 
+  createLottoNumbers(purchaseAmount) {
+    // 발행할 로또 수 계산
+    const amount = purchaseAmount / 1000;
+
+    // 로또 수 만큼 랜덤 숫자 만들기
+    for (let i = 0; i < amount; i++) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      this.numbers = [...this.numbers, numbers];
+    }
+
+    Console.print(`${amount}개를 구매했습니다.\n${this.numbers.map((number) => `[${number.join(', ')}]`).join('\n')}`);
+  }
+
   async run() {
     const purchaseAmount = await this.getPurchaseAmount();
+    const amount = this.createLottoNumbers(purchaseAmount);
   }
 }
 
