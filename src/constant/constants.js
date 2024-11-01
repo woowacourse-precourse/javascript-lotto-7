d51818affe5cb6d8c21695404463936da3375d25
kimyou1102 <yoosk5485@naver.com>
2024-11-02 05:45:39 +0900

    fix: WINNING_NUMBER 상수 구조 수정

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -7,12 +7,12 @@ export const ERROR_MESSAGES = {
   },
   WINNING_NUMBER: {
     INVALID_COMMA: '당첨 번호를 구분하는 쉼표(,)를 포함해 입력해야 합니다.',
+    INVALID_COUNT: '당첨 번호는 6개를 입력해야 합니다.',
+    DUPLICATION_NUMBER: '당첨 번호는 중복 없이 입력해야 합니다.',
+    SPECIAL_SYMBOL: '쉼표(,) 외의 특수 문자는 사용할 수 없습니다.',
+    OUT_OF_RANGE: '당첨 번호는 1부터 45 사이의 숫자여야 합니다.',
+    NOT_A_NUMBER: '당첨 번호는 숫자로 입력해야 합니다.',
   },
-  INVALID_COUNT: '당첨 번호는 6개를 입력해야 합니다.',
-  DUPLICATION_NUMBER: '당첨 번호는 중복 없이 입력해야 합니다.',
-  SPECIAL_SYMBOL: '쉼표(,) 외의 특수 문자는 사용할 수 없습니다.',
-  OUT_OF_RANGE: '당첨 번호는 1부터 45 사이의 숫자여야 합니다.',
-  NOT_A_NUMBER: '당첨 번호는 숫자로 입력해야 합니다.',
 };
 
 export const LOTTO = {
