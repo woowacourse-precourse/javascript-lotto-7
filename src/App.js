a44d15a2cb3b4cb79315b498ca4cc5a472bff6dd
ShinjungOh <tlswjd9260@gmail.com>
2024-11-01 22:45:54 +0900

    feat: pickUniqueNumbersInRange 메소드로 로또 랜덤값 추출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 1에서 45 사이의 중복되지 않은 정수 6개 반환
+    const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    console.log(lotto);
+  }
 }
 
 export default App;
