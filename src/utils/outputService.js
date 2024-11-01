01e0aee5bbca13b7acb43bcb069affa0c63edb10
oko_jin <dswvgw6412@naver.com>
2024-11-01 13:18:07 +0900

    feat(output): 구매한 로또를 출력하는 함수 구현

--- src/utils/outputService.js
+++ src/utils/outputService.js
@@ -3,5 +3,13 @@ import { Console } from '@woowacourse/mission-utils';
 export function outputPayment(amount) {
   if (amount % 1000 !== 0) return;
 
-  Console.print(`${amount / 1000}개를 구입했습니다.`);
+  Console.print(`${amount / 1000}개를 구매했습니다.`);
+}
+
+export function printLottos(lottos) {
+  lottos.forEach((lotto) => {
+    Console.print(`[${lotto.numbers}]`);
+  });
+
+  Console.print('');
 }
