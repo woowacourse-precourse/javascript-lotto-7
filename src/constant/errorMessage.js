b668c6f751dd9c9794200dc405d46409d74a4659
oko_jin <dswvgw6412@naver.com>
2024-11-01 20:57:58 +0900

    refactor(errorMessage): invalidNumberRange로 이름 변경

--- src/constant/errorMessage.js
+++ src/constant/errorMessage.js
@@ -4,7 +4,7 @@ export const errorMessage = Object.freeze({
   negativeAmount: '구매금액은 0보다 커야합니다.',
   divisibleAmount: '구매금액은 1000원 단위로 입력해주세요.',
   invalidDelimiter: '구분자는 , 이어야 합니다.',
-  invalidNumber: '로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  invalidNumberRange: '로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   invalidLotto: '로또 번호는 6개여야 합니다.',
   duplicatedNumber: '로또 번호에 중복된 숫자가 있습니다.',
   invalidBonusNumber: '보너스 번호는 숫자로 입력해주세요.',
