4fd8f9241e5ed5191d4914d85eb1de674a3ae224
moonseonghui <msh9980@naver.com>
2024-11-02 05:48:44 +0900

    feat(calculateTotalPrize): 총 당첨 금액 구하는 메서드 구현

--- src/LottoStatistics.js
+++ src/LottoStatistics.js
@@ -1,8 +1,14 @@
 class LottoStatistics {
+  #purchasedLottos;
+
+  #winningLotto;
+
+  #winningStatistics;
+
   constructor(purchasedLottos, winningLotto) {
-    this.purchasedLottos = purchasedLottos;
-    this.winningLotto = winningLotto;
-    this.winningStatistics = {
+    this.#purchasedLottos = purchasedLottos;
+    this.#winningLotto = winningLotto;
+    this.#winningStatistics = {
       3: { count: 0, prize: 5000 },
       4: { count: 0, prize: 50000 },
       5: { count: 0, prize: 1500000 },
@@ -12,18 +18,22 @@ class LottoStatistics {
   }
 
   calculateStatistics() {
-    const winningNumbers = this.winningLotto.getNumbers();
-    const bonusNumber = this.winningLotto.getBonusNumber();
+    const winningNumbers = this.#winningLotto.getNumbers();
+    const bonusNumber = this.#winningLotto.getBonusNumber();
 
-    this.purchasedLottos.forEach((lotto) => {
-      const matchedNumCount = LottoStatistics.#getMatchedNumCount(lotto, winningNumbers);
+    this.#purchasedLottos.forEach((lotto) => {
+      const matchedNumCount = LottoStatistics.#getMatchedNumCount(
+        lotto,
+        winningNumbers,
+      );
       const hasBonus = LottoStatistics.#hasBonusNumber(lotto, bonusNumber);
       this.#recordMatchedResult(matchedNumCount, hasBonus);
     });
   }
 
   static #getMatchedNumCount(lotto, winningNumbers) {
-    return lotto.getNumbers().filter((num) => winningNumbers.includes(num)).length;
+    return lotto.getNumbers().filter((num) => winningNumbers.includes(num))
+      .length;
   }
 
   static #hasBonusNumber(lotto, bonusNumber) {
@@ -32,12 +42,19 @@ class LottoStatistics {
 
   #recordMatchedResult(matchedNumCount, hasBonus) {
     if (matchedNumCount === 5 && hasBonus) {
-      this.winningStatistics['5bonus'].count += 1;
+      this.#winningStatistics['5bonus'].count += 1;
       return;
     }
     if (matchedNumCount >= 3) {
-      this.winningStatistics[matchedNumCount].count += 1;
+      this.#winningStatistics[matchedNumCount].count += 1;
     }
+  }
+
+  calculateTotalPrize() {
+    return Object.values(this.#winningStatistics).reduce(
+      (total, { count, prize }) => total + count * prize,
+      0,
+    );
   }
 }
 
