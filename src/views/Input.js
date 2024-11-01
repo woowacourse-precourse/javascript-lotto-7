d5d4f39e75655b2d2bc8369a2e2b450259d90563
hwanheejung <hwanheejung117@gmail.com>
2024-11-01 11:39:50 +0900

    feat: implement Input class

---
+++ src/views/Input.js
@@ -0,0 +1,25 @@
+import { Console } from '@woowacourse/mission-utils';
+import { PROMPT_MSG } from '../constants/messages.js';
+
+class Input {
+  static async getPurchaseAmount() {
+    const input = await Console.readLineAsync(
+      `${PROMPT_MSG.PURCHASE_AMOUNT}\n`,
+    );
+    return input;
+  }
+
+  static async getWinningNumbers() {
+    const input = await Console.readLineAsync(
+      `${PROMPT_MSG.WINNING_NUMBERS}\n`,
+    );
+    return input;
+  }
+
+  static async getBonusNumber() {
+    const input = await Console.readLineAsync(`${PROMPT_MSG.BONUS_NUMBER}\n`);
+    return input;
+  }
+}
+
+export default Input;
