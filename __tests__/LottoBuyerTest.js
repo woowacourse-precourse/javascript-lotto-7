e874e5f2e89cba0a2fa44b48dd684144edf3f50b
guesung <gueit214@naver.com>
2024-11-01 13:31:19 +0900

    refactor: 정상적인 입력과 출력을 상수로 분리

--- __tests__/LottoBuyerTest.js
+++ __tests__/LottoBuyerTest.js
@@ -1,7 +1,8 @@
+import { NORMAL_CASE_OUTPUT } from '../src/lib/test/normalCases.js';
 import { getLogSpy, mockQuestions, mockRandoms } from '../src/lib/testUtils.js';
 import LottoBuyer from '../src/LottoBuyer.js';
-import LottoCompany from '../src/LottoCompany';
-import LottoShop from '../src/LottoShop';
+import LottoCompany from '../src/LottoCompany.js';
+import LottoShop from '../src/LottoShop.js';
 
 describe('LottoBuyer 테스트', () => {
   const lottoBuyer = new LottoBuyer();
@@ -25,17 +26,7 @@ describe('LottoBuyer 테스트', () => {
 
     await lottoBuyer.purchaseLottos(lottoShop);
 
-    const logArray = [
-      '8개를 구매했습니다.',
-      '[8, 21, 23, 41, 42, 43]',
-      '[3, 5, 11, 16, 32, 38]',
-      '[7, 11, 16, 35, 36, 44]',
-      '[1, 8, 11, 31, 41, 42]',
-      '[13, 14, 16, 38, 42, 45]',
-      '[7, 11, 30, 40, 42, 43]',
-      '[2, 13, 22, 32, 38, 45]',
-      '[1, 3, 5, 14, 22, 45]',
-    ];
+    const logArray = NORMAL_CASE_OUTPUT.PURCHASE;
 
     logArray.forEach((log) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
@@ -56,15 +47,7 @@ describe('LottoBuyer 테스트', () => {
       ]);
     });
 
-    const logArray = [
-      '당첨 통계',
-      '---',
-      '3개 일치 (5,000원) - 1개',
-      '4개 일치 (50,000원) - 0개',
-      '5개 일치 (1,500,000원) - 0개',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
-      '6개 일치 (2,000,000,000원) - 0개',
-    ];
+    const logArray = NORMAL_CASE_OUTPUT.WINNING_STATIC;
 
     lottoBuyer.checkWinningLotto(lottoCompany);
 
