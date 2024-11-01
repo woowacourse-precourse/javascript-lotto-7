c2454813896cfa20c4f009d0e3de8db1c30b907d
sooyeoniya <tndus1306403@naver.com>
2024-11-01 14:55:39 +0900

    refactor(controller): 컨트롤러 두 개로 분리

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,25 +1,20 @@
-import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
-import parser from '../utils/parser.js';
-import inputPipe from '../utils/inputPipe.js';
-import LottoCount from '../domain/LottoCount.js';
-import Lotto from '../domain/Lotto.js';
-import LottoBonus from '../domain/LottoBonus.js';
 import LottoIssuance from '../domain/LottoIssuance.js';
 import LottoStatistics from '../domain/LottoStatistics.js';
 import LottoRevenue from '../domain/LottoRevenue.js';
+import InputController from './InputController.js';
 
 class LottoController {
   async start() {
-    const lottoCount = await this.#inputLottoPurchasePrice();
+    const lottoCount = await InputController.getLottoPurchasePrice();
     OutputView.printLottoPurchaseCount(lottoCount);
 
     const lottoIssuance = new LottoIssuance(lottoCount);
     const lottos = lottoIssuance.getIssuedLottos();
     lottos.forEach((lotto) => OutputView.printLottoIssueDetails(lotto));
 
-    const winningNumbers = await this.#inputWinningNumbers();
-    const bonusNumber = await this.#inputBonusNumber(winningNumbers);
+    const winningNumbers = await InputController.getWinningNumbers();
+    const bonusNumber = await InputController.getBonusNumber(winningNumbers);
 
     const lottoStatistics = new LottoStatistics(lottos, winningNumbers, bonusNumber);
     const matchResults = lottoStatistics.getMatchResults();
@@ -28,34 +23,6 @@ class LottoController {
     const lottoRevenue = new LottoRevenue(lottoCount, matchResults);
     const revenue = lottoRevenue.getRevenue();
     OutputView.printRevenue(revenue);
-  }
-
-  async #inputLottoPurchasePrice() {
-    return await inputPipe(
-      InputView.readLottoPurchasePriceAsync,
-      parser.parseStringToNumber,
-      LottoCount,
-      'getLottoCount',
-    );
-  }
-
-  async #inputWinningNumbers() {
-    return await inputPipe(
-      InputView.readWinningNumbersAsync,
-      parser.parseExtractNumbers,
-      Lotto,
-      'getLottoNumbers',
-    );
-  }
-
-  async #inputBonusNumber(winningNumbers) {
-    return await inputPipe(
-      InputView.readBonusNumberAsnyc,
-      parser.parseStringToNumber,
-      LottoBonus,
-      'getBonusNumber',
-      [winningNumbers],
-    );
   }
 }
 
