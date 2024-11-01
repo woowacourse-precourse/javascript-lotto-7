d246cea7a745011b6204fa5f8bb3ff57843562e1
ha-kuku <hakukudh@gmail.com>
2024-11-01 09:56:29 +0900

    feat: 유효성 검증 추가

---
+++ src/Validation.js
@@ -0,0 +1,69 @@
+import {
+  BASIC_ERROR,
+  WINNING_NUMBER_ERROR,
+  BOUNS_NUMBER_ERROR,
+} from './Constants/Message.js';
+import {
+  PURCHASE_MONEY_INITIAL_VALUE,
+  LOTTO_PRICE_UNIT,
+  LOTTO_NUMBER_STANDARD,
+} from './Constants/Constant.js';
+
+class BasicValidation {
+  InputBlank(input) {
+    if (input === '') {
+      throw new Error(BASIC_ERROR.null);
+    }
+  }
+
+  InputNumberType(input) {
+    if (Number.isNaN(input)) {
+      throw new Error(BASIC_ERROR.invalidType);
+    }
+  }
+
+  PurchaseUnit(input) {
+    if (input % LOTTO_PRICE_UNIT !== 0) {
+      throw new Error(BASIC_ERROR.invalidMoney);
+    }
+  }
+
+  InputLength(input, count) {
+    if (input.length !== count) {
+      throw new Error(BASIC_ERROR.invalidLength);
+    }
+  }
+}
+
+class WinningNumberValidation {
+  InputSeparator(input) {
+    if (!input.includes(LOTTO_NUMBER_STANDARD.separator)) {
+      throw new Error(WINNING_NUMBER_ERROR.invalidSeparator);
+    }
+  }
+
+  InputLottoRange(input) {
+    if (
+      input < LOTTO_NUMBER_STANDARD.min ||
+      input > LOTTO_NUMBER_STANDARD.max
+    ) {
+      throw new Error(BASIC_ERROR.invalidNumber);
+    }
+  }
+
+  InputOverlap(input) {
+    if (new Set(input).size !== input.length) {
+      throw new Error(WINNING_NUMBER_ERROR.overlap);
+    }
+  }
+}
+
+class BonusNumberValidation {
+  InputOverlap(input, winningNumbers) {
+    if (winningNumbers.includes(input)) {
+      throw new Error(BOUNS_NUMBER_ERROR.overlapBonus);
+    }
+  }
+}
+
+export { BasicValidation, WinningNumberValidation, BonusNumberValidation };
