42bd7275c01e6e6f1c60cd4e427f5165b710772e
ejinn1 <ejinn1@naver.com>
2024-11-01 15:21:41 +0900

    refactor: refactor lotto numbers validation

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,14 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if (numbers.some((number) => isNaN(number))) {
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다');
+    }
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.some((number) => number <= 0 || number > 45)) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 힙니다.');
     }
   }
 
