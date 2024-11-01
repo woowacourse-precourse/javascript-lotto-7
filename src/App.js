68e9c6ae4fb2ab022e3b121020cb9472d649fa71
Parksangwoo <psu8430@gmail.com>
2024-11-01 19:47:02 +0900

    feat(ouput) : 구매한 로또 매수와 6개의 랜덤 숫자로 구성된 배열을 매수 만큼 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   #cash;
 
   #numberOfLotto;
+
+  #lottos;
+
+  constructor() {
+    this.#lottos = [];
+  }
 
   // eslint-disable-next-line max-lines-per-function
   async run() {
@@ -13,6 +19,15 @@ class App {
     // 2. 구매된 로또 매수를 출력한다.
     this.#numberOfLotto = this.#cash / 1000;
     Console.print(`\n${this.#numberOfLotto}개를 구매했습니다.`);
+
+    // 3. 매수 만큼 6개의 랜덤한 숫자로 이루어진 정수 배열을 출력한다.
+    for (let i = 0; i < this.#numberOfLotto; i += 1) {
+      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const sortedNumbers = randomNumbers.sort((a, b) => a - b);
+      this.#lottos.push(sortedNumbers);
+
+      Console.print(sortedNumbers);
+    }
   }
 }
 
