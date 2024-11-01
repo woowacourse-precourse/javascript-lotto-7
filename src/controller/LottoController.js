446c8ef8f2993bdf7bf5a2fb66fb6706999a9deb
gunyongbok <qhrrjsdyd123@gmail.com>
2024-11-02 00:12:37 +0900

    feat: Add lotto number matching and ranking count logic in RankingModel
    Compare purchased lotto tickets with winning numbers to update matchingCount
    Distinguish cases with 5 matches by including bonus number matches when 5 numbers in the winning numbers and purchased numbers match

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,5 +1,6 @@
 import Lotto from "../Lotto.js";
 import PurchasedLottoModel from "../model/purchasedLottoModel.js";
+import RankingModel from "../model/RankingModel.js";
 import LottoView from "../view/LottoView.js";
 
 export default class LottoController {
@@ -25,9 +26,18 @@ export default class LottoController {
     const winningLottoInput = await this.view.getWinningLottoNumbers();
     const winningLottoNumbers = winningLottoInput.split(",").map(Number);
     const winningLotto = new Lotto(winningLottoNumbers);
-    console.log(winningLotto);
 
     // 보너스 번호 입력 받기
     const winningBonusInput = await this.view.getWinningLottoBonusNumbers();
+
+    // 당첨 내역
+    const rankingModel = new RankingModel(
+      purchasedLottos,
+      winningLottoNumbers,
+      winningBonusInput
+    );
+
+    // 당첨 내역 결과
+    const rankingCount = rankingModel.countMatch();
   }
 }
