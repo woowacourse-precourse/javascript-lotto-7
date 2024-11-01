0cb666427854c02ced14aa6093201f07de08e216
BaeJia <qowldk1030@gmail.com>
2024-11-01 15:28:34 +0900

    refactor: 에러 메시지 네이밍 수정

--- src/Model/Error.js
+++ src/Model/Error.js
@@ -7,12 +7,12 @@ const ERROR_MESSAGES = Object.freeze({
     NOT_ENTERED: "구입 금액이 입력되지 않았습니다.",
     MUST_BE_NUMERIC: "구입 금액은 무조건 숫자여야 합니다.",
   },
-  winning_number: {
+  winningNumber: {
     OUT_OF_RANGE: "로또 번호의 숫자 범위는 1~45입니다.",
     DUPLICATE_NUMBER:
       "당첨 번호 추첨 시 중복되지 않는 숫자 6개를 뽑아야 합니다.",
   },
-  bonus_number: {
+  bonusNumber: {
     OUT_OF_RANGE: "보너스 번호의 숫자 범위는 1~45입니다.",
     DUPLICATE_NUMBER:
       "보너스 번호 추첨 시 중복되지 않는 숫자 1개를 뽑아야 합니다.",
