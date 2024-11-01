b6aa4e66910af3b86f40f34641c85512847eb2fd
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-01 17:26:44 +0900

    feat: 총 수익률 결과 확인 기능 추가

--- src/OutputHandler.js
+++ src/OutputHandler.js
@@ -17,6 +17,9 @@ class OutputHandler {
     Console.print(`${LOTTO_PRIZES.SECOND}${WinningStatus.secondPrizeCount}개`);
     Console.print(`${LOTTO_PRIZES.FRIST}${WinningStatus.firstPrizeCount}개`);
   }
+  static showProfitRate(profitRate) {
+    Console.print(`총 수익률은 ${profitRate}% 입니다.`);
+  }
 }
 
 export default OutputHandler;
