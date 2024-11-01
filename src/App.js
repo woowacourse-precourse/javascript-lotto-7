b58855d046f019974214dbe94ef2b1340e7a5ac9
sooinice <sooin9674@naver.com>
2024-11-01 11:43:28 +0900

    feat : 구입금액 유효성 검사

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ class App {
     const lottoPurchaseAmount = await MissionUtils.Console.readLineAsync(
       "구입금액을 입력해 주세요.\n"
     );
+    if (lottoPurchaseAmount % 1000 !== 0) {
+      throw new Error("[ERROR] : 구입금액이 1000원 단위로 나누어지지 않음.");
+    }
   }
 }
 
