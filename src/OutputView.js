ef8feeef5384a3e11dea17c0ded352ff968b6c96
dudcns02 <duddl2460@naver.com>
2024-11-02 01:16:38 +0900

    feat: Add rate of return Output Capability

--- src/OutputView.js
+++ src/OutputView.js
@@ -5,3 +5,5 @@ const printLottoCont = Console.print(numberOfOrder(a).NUBER_OF_ORDER);
 const printWinningStatistics = Console.print(LOG_MESSAGE.WINNING_STATISTICS);
 ///당첨통계
 ///---
+const printRateOfReturn = Console.print(rateOfReturn(c).RATE_OF_RETURN);
+// 수익률은 ${c}%입니다.
