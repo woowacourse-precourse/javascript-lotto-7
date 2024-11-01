d9bc3c5cec89e3d3b1f74ebf3d7a61bfd0e78dc0
Parksangwoo <psu8430@gmail.com>
2024-11-01 19:08:27 +0900

    feat(output) : 구매한 로또 매수 출력
    - 프로그래밍 요구사항인 한 함수에 15줄 이내의 코드가 있어야한다는 사항은 이후 리팩토링에서 반영할 예정

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   #cash;
 
+  #numberOfLotto;
+
+  // eslint-disable-next-line max-lines-per-function
   async run() {
     // 1. 구입 금액을 입력받는다.
     this.#cash = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    // 2. 구매된 로또 매수를 출력한다.
+    this.#numberOfLotto = this.#cash / 1000;
+    Console.print(`\n${this.#numberOfLotto}개를 구매했습니다.`);
   }
 }
 
