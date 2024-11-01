558769e4f5872032922a6ed9f34cc3368d594c34
mlnyx <nsm110@naver.com>
2024-11-02 02:20:16 +0900

    feat(Lotto): Implement Lotto number generation
    - Add a getter method for #numbers to retrieve the generated lotto numbers.
    - Implement static method createLotto to generate unique lotto numbers based on the purchase amount.
    - Calculate the number of lotto tickets based on a purchase amount in increments of 1,000 KRW.
    - Generate unique lotto numbers for each ticket using Random.pickUniqueNumbersInRange, ensuring numbers are sorted before being added to the lotto array.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
@@ -12,7 +14,25 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  get numbers() {
+    return this.#numbers;
+  }
+
+  static createLotto(purchaseAmount) {
+    const lottoCount = purchaseAmount / 1000; // 1,000원 단위로 티켓 수 계산
+    const lottoArr = [];
+
+    for (let i = 0; i < lottoCount; i++) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(
+        1,
+        45,
+        6
+      ).sort((a, b) => a - b);
+      lottoArr.push(new Lotto(numbers)); // 각 티켓에 대해 Lotto 객체 생성
+    }
+
+    return lottoArr;
+  }
 }
 
 export default Lotto;
