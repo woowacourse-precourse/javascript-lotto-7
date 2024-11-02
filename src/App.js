8723ca4cc60ec14292554d62c88bde806d9177c0
jeong922 <hwajeong922@naver.com>
2024-11-02 21:55:35 +0900

    feat(App): 당첨 통계 출력 기능 구현
    - 당첨 번호와 보너스 번호를 비교하여 당첨 로그 저장 추가
    - 당첨 결과를 출력하는 로직 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Lotto from './Lotto.js';
 class App {
   constructor() {
     this.numbers = [];
+    this.bonusNumber = null;
   }
 
   async getPurchaseAmount() {
@@ -45,7 +46,52 @@ class App {
     const numbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
     const lottoNumbers = numbers.split(',').map((number) => parseInt(number));
     const lotto = new Lotto(lottoNumbers);
-    const bonusNumber = await this.getBonusNumber(); // 보너스 번호 입력 받기
+    this.bonusNumber = await this.getBonusNumber(); // 보너스 번호 입력 받기
+
+    return this.numbers.map((number) => lotto.checkWinningNumbers(number));
+  }
+
+  getMatchResult(matchNumbers) {
+    const logs = [
+      { matchCount: 3, count: 0, bonusNumber: false, prizeMoney: 5000 },
+      { matchCount: 4, count: 0, bonusNumber: false, prizeMoney: 50000 },
+      { matchCount: 5, count: 0, bonusNumber: false, prizeMoney: 1500000 },
+      { matchCount: 5, count: 0, bonusNumber: true, prizeMoney: 30000000 },
+      { matchCount: 6, count: 0, bonusNumber: false, prizeMoney: 2000000000 },
+    ];
+
+    matchNumbers
+      .filter((numbers) => numbers.length >= 3)
+      .forEach((item) => {
+        const findIndex = logs.findIndex((log) => log.matchCount === item.length);
+
+        if (logs[findIndex].matchCount === 5 && logs[findIndex].bonusNumber) {
+          logs[findIndex].count += 1;
+        } else {
+          logs[findIndex].count += 1;
+        }
+      });
+
+    return logs;
+  }
+
+  printLog(logs) {
+    const money = {
+      3: '5,000',
+      4: '50,000',
+      5: '1,500,000',
+      6: '2,000,000,000',
+    };
+
+    const result = logs.map((log) => {
+      let amount = ` (${money[log.matchCount]}원)`;
+      if (log.matchCount === 5 && log.bonusNumber) {
+        amount = `, 보너스 볼 일치 (30,000,000원)`;
+      }
+      return `${log.matchCount}개 일치${amount} - ${log.count}개`;
+    });
+
+    Console.print('당첨 통계\n' + '---\n' + result.join('\n'));
   }
 
   async getBonusNumber() {
@@ -67,6 +113,8 @@ class App {
     const purchaseAmount = await this.getPurchaseAmount();
     const amount = this.createLottoNumbers(purchaseAmount);
     const winningNumbers = await this.getWinningNumbers();
+    const matchResult = this.getMatchResult(winningNumbers);
+    const log = this.printLog(matchResult);
   }
 }
 
