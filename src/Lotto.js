9bed65240a538bdcf6917e7a23b1ac7ed453365d
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-02 16:07:42 +0900

    feat: 로또 번호는 중복될 수 없다

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    const setNumbers = new Set(numbers);
+    if (setNumbers.size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다');
+    }
   }
 
   getFormattedNumbers() {
