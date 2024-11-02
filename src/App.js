92fdbcebb0f08ecac8cd066ae743e4d9c7157f58
salmonco <xkqjsslsek80@naver.com>
2024-11-02 20:19:01 +0900

    style(app): 주석 삭제

--- src/App.js
+++ src/App.js
@@ -2,54 +2,28 @@ import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    // - 로또 구입 금액을 입력받는다.
     const priceString = await this.readPriceString();
+    this.validatePriceString(priceString);
     const price = Number(priceString);
 
-    // - 구입 금액이 숫자가 아닐 경우 예외 처리한다.
-    this.validatePriceString(priceString);
-
-    // - 구입 금액이 0보다 작거나 같을 경우 예외 처리한다.
-    // - 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.
-    // - 입력한 당첨 번호에서 중복된 것이 있는 경우 예외 처리한다.
-    // - 입력한 당첨 번호가 1~45 사이의 숫자가 아닌 경우 예외 처리한다.
-
-    // - 구입 금액에 따라 발행할 로또 개수가 몇 개인지 구한다.
     const lottoCount = this.getLottoCount(price);
-
-    // - 로또를 발행한다.
     const lottosArray = this.getLottosArray(lottoCount);
-
-    // - 로또 번호는 오름차순으로 정렬하여 보여준다.
     const sortedLottosArray = this.getSortedLottosArray(lottosArray);
 
-    // - 발행한 로또 수량을 출력한다.
     this.print(`\n${lottoCount}개를 구매했습니다.`);
-
-    // - 발행한 로또 번호를 출력한다.
     this.printLottos(sortedLottosArray);
 
-    // - 당첨 번호 6개를 입력받는다.
     const winningNumbersString = await this.readWinningNumbersString();
-
-    // - 당첨 번호를 쉼표(,)를 기준으로 구분한다.
     const winnigNumbersArray = this.getWinningNumberArray(winningNumbersString);
 
-    // - 보너스 번호 1개를 입력받는다.
     const bonusNumberString = await this.readBonusNumberString();
     const bounusNumber = Number(bonusNumberString);
 
-    // - 각 로또에 대해 일치하는 개수를 구한다.
     const matchedCountArray = this.getMatchedCountInLottos(sortedLottosArray, winnigNumbersArray, bounusNumber);
-
-    // - 당첨 기준에 일치하는 로또의 개수를 구한다.
     const matchedCountPerMatchOption = this.getMatchedCountPerMatchOption(matchedCountArray);
 
-    // - 당첨 내역을 출력한다.
     this.print('\n당첨 통계\n---');
     this.printWinningResult(matchedCountPerMatchOption);
-
-    // - 수익률을 출력한다.
     this.printRateOfReturn(matchedCountPerMatchOption, price);
   }
 
