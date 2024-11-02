f56b4697eb7905b3d777e998d777ab87a1d11413
jeongeunLee <leeju1009@gmail.com>
2024-11-02 19:33:00 +0900

    test: 숫자로 파싱하는 유틸 함수 테스트 추가

---
+++ __tests__/UtilsTest.js
@@ -0,0 +1,20 @@
+import Utils from '../src/utils/Utils';
+
+describe('Utils 함수 테스트', () => {
+  test.each([
+    ['2000', 2000],
+    ['0', 0],
+    ['0.5', 0.5],
+    ['-21', -21],
+    ['숫자', NaN],
+    ['2000원', NaN],
+    [null, null],
+    [undefined, NaN],
+    ['$', NaN],
+    ['$2000$', NaN],
+  ])('input값을 숫자로 형 변환하는 함수 테스트', (inputs, expected) => {
+    const parsedInput = Utils.parsingNumber(inputs);
+
+    expect(parsedInput).toBe(expected);
+  });
+});
