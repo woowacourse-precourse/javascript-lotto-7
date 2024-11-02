07880e705ab59597307f6a5042fda660bac8e255
youdame <112458620+youdame@users.noreply.github.com>
2024-11-02 21:39:21 +0900

    feat : 구입 금액 입력 및 구매 개수 출력 - 제공해주는 유틸 함수로 입출력 구현 - 예외 처리 내역 주석으로 명시

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console, Random } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
-}
+  async run() {
+    /* TODO: 
+    1) 1000원으로 안 떨어지는 경우 예외처리,
+    2) 숫자가 아닌 금액을 입력했을 경우 예외처리 
+    3) 1000원보다 적은 금액을 입력했을 경우 예외처리 
+    4) 아무 것도 입력하지 않은 경우 예외 처리 
+    */
+    const money = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
 
+    const count = money / 1000;
+    Console.print(`${count}개를 구매했습니다.`);
+  }
+}
 export default App;
