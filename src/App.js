c2b9648b49bd54354c2919b5a4ef4b2c48c0f52f
jin123457 <jin_ox@naver.com>
2024-11-02 16:42:32 +0900

    feat : 로또 구입 금액 입력
    수정된 파일명 : App.js
    App.js : Console.readLineAsync() 함수를 이용하여 로또 구입 금액을 입력 받은 후 readUserMoney 변수에 할당

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const readUserMoney =
+      await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  }
 }
 
 export default App;
