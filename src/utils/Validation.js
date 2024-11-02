e9e9568535ea840fbd44c99ca14fa925287d1c83
seonae-j <wkdtjsdo135@naver.com>
2024-11-02 21:31:44 +0900

    refactor(Validation): 유효성 검사 함수 분리
    - 로또 구매 금액 예외 사항과 보너스 숫자 예외 사항 처리 함수가 겹쳐서 재사용하기 위해 함수 분리

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,18 +1,26 @@
 import { UNIT, ERROR } from '../constants/Constants.js';
 
-export function validateCost(cost) {
-  const numberCost = Number(cost);
-
-  if (isNaN(numberCost)) {
+export function validateNumber(number) {
+  if (isNaN(Number(number))) {
     throw new Error(ERROR.COST_TYPE);
   }
+}
 
-  if (!Number.isInteger(numberCost)) {
+export function validateInteger(number) {
+  if (!Number.isInteger(number)) {
     throw new Error(ERROR.COST_UNIT);
   }
+}
 
-  if (numberCost % UNIT.TICKET_PRICE !== 0) {
+export function validateCost(cost) {
+  if (Number(cost) % UNIT.TICKET_PRICE !== 0) {
     throw new Error(ERROR.COST_UNIT);
+  }
+}
+
+export function validateRange(number) {
+  if ((Number(number) < 1) | (Number(number) > 45)) {
+    throw new Error(ERROR.LOTTO_RANGE);
   }
 }
 
