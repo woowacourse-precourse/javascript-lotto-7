008dc2b2308aa2152a73c2083d1d4cd933769d30
laire7 <syoungkwak@gmail.com>
2024-11-02 09:48:50 +0900

    feat(): Check user input are valid lotto numbers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async run() {}
+  async run() {
+    //Get user input
+    async function getUserInput() {
+      const INPUT = await MissionUtils.Console.readLineAsync(
+        "당첨 번호를 입력해 주세요.\n"
+      );
+      return INPUT;
+    }
+
+    const LOTTO_DRAW = await getUserInput();
+
+    //Create lotto instance
+    const lotto = new Lotto(LOTTO_DRAW.split(","));
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -2,8 +2,13 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+    if (
+      this.#validate(numbers) &
+      this.#areNumbers(numbers) &
+      this.#between1And46(numbers) &
+      this.#noRepeats(numbers)
+    )
+      this.#numbers = numbers;
   }
 
   #validate(numbers) {
@@ -12,7 +17,37 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  // 입력한 것들이 숫자인지 확인한다
+  #areNumbers(numbers) {
+    for (let number of numbers) {
+      if (!!isNaN(number)) {
+        throw new Error("[ERROR] 로또 번호는 숫자이어야 합니다.");
+      }
+    }
+    return true;
+  }
+
+  // 입력한 숫자들이 1에서 45까지인지 확인한다
+  #between1And46(numbers) {
+    // 입력한 것들이 숫자인지 확인한다
+    for (let number of numbers) {
+      if ((number < 1) | (number > 45)) {
+        throw new Error("[ERROR] 로또 번호는 1에서 45까지 숫자이어야 합니다.");
+      }
+    }
+    return true;
+  }
+
+  //입력한 숫자들이 중복된 것이 있는지 확인한다
+  #noRepeats(numbers) {
+    var CHECK_REPEAT = new Set();
+    for (let number of numbers) CHECK_REPEAT.add(number);
+    if (CHECK_REPEAT.size != numbers.length) {
+      console;
+      throw new Error("[ERROR] 로또 번호는 중복이 없습니다.");
+    }
+    return true;
+  }
 }
 
 export default Lotto;
