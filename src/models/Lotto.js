776060d1052bd8a636cc6a2a7a7d56679ae57fdf
zelkovaria <hyein396@gmail.com>
2024-11-03 02:39:20 +0900

    feat: 로또 번호 중복 여부 validate 메서드 구현

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -8,11 +8,19 @@ class Lotto {
 
   #validate(numbers) {
     this.#validateLottoLength(numbers);
+    this.#validateDuplicateNum(numbers);
   }
 
   #validateLottoLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  #validateDuplicateNum(numbers) {
+    const setNumbers = new Set(numbers);
+    if (numbers.length !== setNumbers.size) {
+      throw new Error("[ERROR] 로또 번호는 중복없이 입력되어야 합니다.");
     }
   }
 }
