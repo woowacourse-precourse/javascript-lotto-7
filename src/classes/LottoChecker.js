e5fe4a939c47d48b50a9711c24c92e0753fbade7
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 22:01:39 +0900

    refactor(LottoChecker): 클래스 캡슐화
    - static과 private을 적절히 사용해 클래스 캡슐화

--- src/classes/LottoChecker.js
+++ src/classes/LottoChecker.js
@@ -1,46 +1,41 @@
-
 class LottoChecker {
   static PRIZE_TIERS = [
     { tier: 0, matchCount: 6, bonus: false, prize: 2000000000 },
     { tier: 1, matchCount: 5, bonus: true, prize: 30000000 },
     { tier: 2, matchCount: 5, bonus: false, prize: 1500000 },
     { tier: 3, matchCount: 4, bonus: false, prize: 50000 },
-    { tier: 4, matchCount: 3, bonus :  false, prize: 5000 },
+    { tier: 4, matchCount: 3, bonus: false, prize: 5000 },
   ];
 
-  constructor(winningNumbers, bonusNumber) {
-    this.lottoCheckResults = new Array(5).fill(0);
-    this.winningNumbers = winningNumbers;
-    this.bonusNumber = bonusNumber;
+  static checkWinningLottos(lottos, winningNumbers, bonusNumber) {
+    const lottoCheckResults = new Array(5).fill(0);
+
+    for (const lotto of lottos) {
+      const tier = this.#checkSingleLotto(lotto, winningNumbers, bonusNumber);
+      this.#countCheckResults(lottoCheckResults, tier);
+    }
+    return lottoCheckResults;
   }
 
-  checkSingleLotto(lotto) {
+  static #checkSingleLotto(lotto, winningNumbers, bonusNumber) {
     const numbers = lotto.getNumbers();
     const matchCount = numbers.filter((num) =>
-      this.winningNumbers.includes(num)
+      winningNumbers.includes(num)
     ).length;
-    const hasBonus = numbers.includes(this.bonusNumber);
+    const hasBonus = numbers.includes(bonusNumber);
 
-    return this.findTier(matchCount, hasBonus);
+    return this.#findTier(matchCount, hasBonus);
   }
 
-  checkWinningLottos(lottos) {
-    for (const lotto of lottos) {
-      const tier = this.checkSingleLotto(lotto);
-      this.countCheckResults(tier);
-    }
-    return this.lottoCheckResults;
-  }
-
-  countCheckResults(tier) {
+  static #countCheckResults(lottoCheckResults, tier) {
     if (tier !== undefined) {
-      this.lottoCheckResults[tier]++;
+      lottoCheckResults[tier]++;
     }
   }
 
-  findTier(matchCount, hasBonus) {
+  static #findTier(matchCount, hasBonus) {
     const tier = LottoChecker.PRIZE_TIERS.find(
-      (e) => e.matchCount === matchCount && e.bonus && hasBonus
+      (e) => e.matchCount === matchCount && (!e.bonus || (e.bonus && hasBonus))
     );
     return tier ? tier.tier : undefined;
   }
