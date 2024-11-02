86547072c91c4ea1d3263bdbac7825eff287a5a2
yena <rladpsk1117@gmail.com>
2024-11-03 01:34:36 +0900

    feat(BonusNumber): 보너스 번호 유효성 체크 및 예외 처리 구현

--- src/model/BonusNumber.js
+++ src/model/BonusNumber.js
@@ -1,8 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
 class BonusNumber {
   #bonusNumber;
 
   constructor(bonusNumber) {
+    this.#validate(bonusNumber);
     this.#bonusNumber = bonusNumber;
+  }
+
+  #validate(bonusNumber) {
+    if ((bonusNumber < 1) | (bonusNumber > 45)) {
+      throw new Error('[ERROR] 보너스 번호 숫자 범위는 1~45 입니다.');
+    }
+    if (bonusNumber % 1 !== 0) {
+      throw new Error('[ERROR] 보너스 번호는 정수만 입력 가능합니다.');
+    }
+    if (isNaN(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 하나의 숫자만 입력 가능합니다.');
+    }
   }
 }
 
