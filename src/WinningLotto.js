01846e7ec3c002dd11f210ee13aa3405aac3dc55
hyunyeee <5004ohy@naver.com>
2024-11-03 02:36:00 +0900

    feat(validate): 보너스 번호가 로또 번호 범위를 벗어난 경우 에러를 던진다

--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -1,10 +1,23 @@
 import Lotto from "./Lotto.js";
+import { LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER } from "./constant/constants.js";
+import { inValidMessages } from "./constant/message.js";
 
 export class WinningLotto extends Lotto {
   #bonusNumber;
 
   constructor(numbers, bonusNumber) {
     super(numbers);
+    this.#validate(bonusNumber);
     this.#bonusNumber = bonusNumber;
+  }
+
+  #validate(bonusNumber) {
+    this.#validateRange(bonusNumber);
+  }
+
+  #validateRange(bonusNumber) {
+    if (bonusNumber < LOTTO_MIN_NUMBER || bonusNumber > LOTTO_MAX_NUMBER) {
+      throw new Error(inValidMessages.range);
+    }
   }
 }
--- src/constant/message.js
+++ src/constant/message.js
@@ -1,4 +1,4 @@
-import { LOTTO_NUMBERS_COUNT, LOTTO_PRICE, SPLIT_CHAR } from "./constants.js";
+import { LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER, LOTTO_NUMBERS_COUNT, LOTTO_PRICE, SPLIT_CHAR } from "./constants.js";
 
 export const viewMessages = {
   price: '구입금액을 입력해주세요.\n',
@@ -14,5 +14,6 @@ export const inValidMessages = {
   priceUnit: `구입 금액은 ${LOTTO_PRICE}원 단위로 입력해주세요.\n`,
   separator: `당첨 번호를 쉼표(${SPLIT_CHAR})로 구분해서 입력해주세요.`,
   winningLottoCount: `로또 번호는 ${LOTTO_NUMBERS_COUNT}개여야 합니다.`,
+  range: `당첨 번호는 ${LOTTO_MIN_NUMBER}~${LOTTO_MAX_NUMBER}사이의 숫자입니다.`,
   duplicate: '로또 번호는 중복을 허용하지 않습니다.',
 }
