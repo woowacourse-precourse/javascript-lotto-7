58bcebb33b05423963d9bdb4831d912c6e8cdc35
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 23:12:55 +0900

    feat: App.js 실행 로직 구현
    - 리팩토링한 각 클래스들에 맞추어 실행 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import LottoChecker from './classes/LottoChecker.js';
 import LottoInputReader from './classes/LottoInputReader.js';
 import LottoIssuer from './classes/LottoIssuer.js';
+import LottoOutputWriter from './classes/LottoOutputWriter.js';
+import RevenueCalculator from './classes/RevenueCalculator.js';
 
 class App {
   async run() {
     const lottoPurchaseAmount =
       await LottoInputReader.readLottoPurchaseAmount();
-    const winningNumber = await LottoInputReader.readWinningNumbers();
-    const bonusNumber = await LottoInputReader.readBonusNumber();
-    const checker = new LottoChecker(winningNumber, bonusNumber);
-
     const lottoCount = LottoIssuer.calculateLottoCount(lottoPurchaseAmount);
     const lottos = LottoIssuer.generateLottos(lottoCount);
 
-    const winningResult = checker.checkWinningLottos(lottos);
+    LottoOutputWriter.printLottos(lottos);
+
+    const winningNumbers = await LottoInputReader.readWinningNumbers();
+    const bonusNumber = await LottoInputReader.readBonusNumber();
+
+    const winningResult = LottoChecker.checkWinningLottos(
+      lottos,
+      winningNumbers,
+      bonusNumber
+    );
+    LottoOutputWriter.printWinningResults(winningResult);
+
+    const totalYield = RevenueCalculator.calculateYield(
+      lottoPurchaseAmount,
+      winningResult
+    );
+    LottoOutputWriter.printYield(totalYield);
   }
 }
 
