19712706565eaf1d8b072d996eb0201046de4fdb
jllee000 <jllee000@naver.com>
2024-11-02 16:29:04 +0900

    feat : 유저 인풋창 구현 (구입금액, 보너스 번호)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async run() {}
+  async run() {
+    const LOTTO_WIN_NUM = [1, 2, 3, 4, 5, 6];
+    const lottoResult = new Lotto(LOTTO_WIN_NUM); // 변수 이름을 lottoResult로 변경
+    // 비동기 함수 호출 후 결과를 출력
+    const lottoCount = await lottoResult.InputCost();
+    Console.print(`구입 가능한 로또 개수: ${lottoCount}`);
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,7 @@
+import { Console, Random } from "@woowacourse/mission-utils";
 class Lotto {
-  #numbers;
-
+  #numbers; // 프라이빗 필드를 뜻함
+  // 로또 클래스 생성자 함수
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
@@ -13,6 +14,20 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  // 인풋 구현
+  async InputCost() {
+    const USER_COST = await Console.readLineAsync("구입 금액을 입력해주세요: ");
+    const LOTTO_CNT = Math.floor(Number(USER_COST) / 1000);
+    return LOTTO_CNT;
+  }
+
+  async InputBonus() {
+    const BONUS_NUM = await Console.readLineAsync(
+      "보너스 번호를 입력해 주세요: "
+    );
+    return Number(BONUS_NUM);
+  }
+  GenerateLottoResult() {}
 }
 
 export default Lotto;
