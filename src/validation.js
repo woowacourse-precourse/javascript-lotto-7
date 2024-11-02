021475446fde37d20b3bd6860f38fd3c0094dec2
jiyun1006 <jiyun4251@gmail.com>
2024-11-02 18:44:24 +0900

    ✨ feat : 당첨 번호에 대한 오류 검증 1
    당첨 번호에 띄어쓰기가 되었는지 확인
    - hasSpcae

--- src/validation.js
+++ src/validation.js
@@ -2,17 +2,40 @@ import { ERRORMESSAGE, NUM } from './constants/index.js';
 
 const validation = Object.freeze({
   purchaseAmount: {
+    /**
+     *
+     * @param {string} number
+     */
     isNotNumber: function (number) {
       if (isNaN(number)) throw new Error(ERRORMESSAGE.ISNOTNUMBER);
     },
+    /**
+     *
+     * @param {string} number
+     */
     isNotPositiveNumber: function (number) {
       if (Number(number) < NUM.MINIMUM_POSITIVE_NUMBER)
         throw new Error(ERRORMESSAGE.ISNOTPOSITIVENUMBER);
     },
-
+    /**
+     *
+     * @param {string} number
+     */
     isNotGoodNumber: function (number) {
       if (Number(number) % NUM.LOTTO_PRICE !== 0)
         throw new Error(ERRORMESSAGE.ISNOTGOODNUMBER);
+    },
+  },
+
+  winningNumber: {
+    /**
+     *
+     * @param {Array<string>} numbers
+     */
+    hasSpace: function (numbers) {
+      const numberArr = numbers.split(',');
+      if (numberArr.some((number) => /\s/g.test(number)))
+        throw new Error(ERRORMESSAGE.HASSPACE);
     },
   },
 });
