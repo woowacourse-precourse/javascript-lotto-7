ec2de10367df3edf4bcdbdc927f43a5507c23664
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-03 02:43:06 +0900

    test: LottoTest의 메서드 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,23 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  describe('메서드 테스트', () => {
+    test('getNumbers() 메서드는 로또 번호 배열을 반환한다.', () => {
+      const numbers = [1, 2, 3, 4, 5, 6];
+      const lotto = new Lotto(numbers);
+      expect(lotto.getNumbers()).toEqual(numbers);
+    });
+
+    test('toString() 메서드는 로또 번호를 "[1, 2, 3, 4, 5, 6]" 형식의 문자열로 반환한다.', () => {
+      const numbers = [1, 2, 3, 4, 5, 6];
+      const lotto = new Lotto(numbers);
+      expect(lotto.toString()).toBe('[1, 2, 3, 4, 5, 6]');
+    });
+  });
 });
