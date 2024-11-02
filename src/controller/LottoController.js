1beacdc9572ab0966c1b8ec6408f6d857f3aa02f
yena <rladpsk1117@gmail.com>
2024-11-03 01:38:25 +0900

    feat(LottoController): LottoController 클래스 생성

---
+++ src/controller/LottoController.js
@@ -0,0 +1,9 @@
+class LottoController {
+  #purchaseAmount; // 구입금액
+  #lotteryNumbers; // 구입한 로또 번호
+  #winnigLottoNumbers; // 당첨 번호
+  #bonusNumber; // 보너스 번호
+
+  async start() {}
+}
+export default LottoController;
