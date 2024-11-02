a266d3387b6bde816a3696cc7018e266a7803e29
jeongeunLee <leeju1009@gmail.com>
2024-11-02 16:30:22 +0900

    feat: lottoMachine 인스턴스를 생성하는 함수를 app 클래스에 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+import inputView from './userInterface/InputView.js';
+import Lotto from './Lotto.js';
+import LottoMachine from './LottoMachine.js';
+
 class App {
-  async run() {}
+  async run() {
+    const lottoMachine = await this.getLottoMachine();
+  }
+
+  async getLottoMachine() {
+    try {
+      const payment = await inputView.askPayment();
+      const lottoMachine = new LottoMachine(payment, Lotto);
+      return lottoMachine;
+    } catch (error) {
+      Console.print(error.message);
+
+      return this.getLottoMachine();
+    }
+  }
 }
 
 export default App;
