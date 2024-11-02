39fb8b6dd9024bc90b1bc54f5aded002a0204648
mlnwns <mlnwns@naver.com>
2024-11-02 20:36:06 +0900

    feat: 구입 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+import { PROMPTS } from "./constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await this.getPurchaseAmount();
+  }
+
+  async getPurchaseAmount() {
+    const purchaseAmount = await Console.readLineAsync(PROMPTS.PURCHASE_AMOUNT);
+    return purchaseAmount;
+  }
 }
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -16,7 +16,7 @@ export const ERROR_MESSAGES = {
 };
 
 export const PROMPTS = {
-  PURCHASE_AMOUNT: "구입 금액을 입력해 주세요.",
+  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.\n",
   WINNING_NUMBERS: "당첨 번호를 입력해 주세요.",
   BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
 };
