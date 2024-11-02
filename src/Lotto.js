600e9ebb59ba0915498fba7cfd927a1221a75dd0
Jinny <ggab.challenge@gmail.com>
2024-11-02 19:04:31 +0900

    feat: 로또 번호가 쉼표로 구분되어 있지 않는 경우 에러 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,16 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    // 쉼표 5개로 이루어져있는지 체크
+    const arr = [...numbers];
+    const onlyNumber = arr.filter((number) => number === ',');
+    if (onlyNumber.length !== 5) {
+      throw new Error('[ERROR] 로또 번호는 쉼표(,) 로 구분되어야 합니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
