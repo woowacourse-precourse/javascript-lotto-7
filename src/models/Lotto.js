72b46ad140d601769dc8ddd6ead99e1a283a80a7
zelkovaria <hyein396@gmail.com>
2024-11-03 02:34:48 +0900

    feat: 로또 번호 개수 validate 메서드 구현

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -7,12 +7,14 @@ class Lotto {
   }
 
   #validate(numbers) {
+    this.#validateLottoLength(numbers);
+  }
+
+  #validateLottoLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
