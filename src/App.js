508be21494fb1d3b341a58cd2bcb4f8e052b9ed2
yoo_min <vendetta12@naver.com>
2024-11-02 21:34:26 +0900

    refactor: 결과 출력 기능 LottoController로부터 분리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 import InputHandler from "./handler/InputHandler.js"
 import OutputHandler from "./handler/OutputHandler.js";
+import ResultController from "./controller/ResultController.js";
 import LottoController from "./controller/LottoController.js";
 
 class App {
   constructor() {
     this.inputHandler = new InputHandler();
     this.outputHandler = new OutputHandler();
+    this.resultController = new ResultController();
     this.lottoController = new LottoController();
   }
   async run() {
@@ -19,7 +21,7 @@ class App {
 
       const bonusNumber = await this.inputHandler.getBonusNumber(winningNumber);
 
-      this.lottoController.execute(lottos, winningNumber, bonusNumber, lottoMoney);
+      this.resultController.execute(lottos, winningNumber, bonusNumber, lottoMoney);
     }
     catch (error) {
       Console.print(error.message);
