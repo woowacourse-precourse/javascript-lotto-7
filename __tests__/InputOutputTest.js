5c6ab61c239406cee3d520813c75ad1e2c0776b6
jiyun1006 <jiyun4251@gmail.com>
2024-11-02 16:41:47 +0900

    ♻️ refactor : validation 객체 구조 수정
    validation 객체에 purchaseAmount 로 구조 세분화

--- __tests__/InputOutputTest.js
+++ __tests__/InputOutputTest.js
@@ -1,22 +1,25 @@
-import { inputMethod } from '../src/utils/index.js';
 import validation from '../src/validation.js';
 
 describe('입력문에 대한 테스트', () => {
   test.each(['100+100', 's20f', '@GJe22'])(
     '구매금액은 숫자만 입력되어야 한다.',
     (numberStr) => {
-      expect(() => validation.isNotNumber(numberStr)).toThrow();
+      expect(() => validation.purchaseAmount.isNotNumber(numberStr)).toThrow();
     },
   );
 
   test.each(['-100', '0'])('구매금액은 양수만 입력해야 한다.', (numberStr) => {
-    expect(() => validation.isNotPositiveNumber(numberStr)).toThrow();
+    expect(() =>
+      validation.purchaseAmount.isNotPositiveNumber(numberStr),
+    ).toThrow();
   });
 
   test.each(['1001', '100.5', '1234.543'])(
     '구매금액은 1000으로 나누어 떨어져야 한다.',
     (numberStr) => {
-      expect(() => validation.isNotGoodNumber(numberStr)).toThrow();
+      expect(() =>
+        validation.purchaseAmount.isNotGoodNumber(numberStr),
+      ).toThrow();
     },
   );
 });
--- src/validation.js
+++ src/validation.js
@@ -1,21 +1,19 @@
 import { ERRORMESSAGE, NUM } from './constants/index.js';
 
 const validation = Object.freeze({
-  /**
-   *
-   * @param {str} number
-   */
-  isNotNumber: function (number) {
-    if (isNaN(number)) throw new Error(ERRORMESSAGE.ISNOTNUMBER);
-  },
-  isNotPositiveNumber: function (number) {
-    if (Number(number) < NUM.MINIMUM_POSITIVE_NUMBER)
-      throw new Error(ERRORMESSAGE.ISNOTPOSITIVENUMBER);
-  },
+  purchaseAmount: {
+    isNotNumber: function (number) {
+      if (isNaN(number)) throw new Error(ERRORMESSAGE.ISNOTNUMBER);
+    },
+    isNotPositiveNumber: function (number) {
+      if (Number(number) < NUM.MINIMUM_POSITIVE_NUMBER)
+        throw new Error(ERRORMESSAGE.ISNOTPOSITIVENUMBER);
+    },
 
-  isNotGoodNumber: function (number) {
-    if (Number(number) % NUM.LOTTO_PRICE !== 0)
-      throw new Error(ERRORMESSAGE.ISNOTGOODNUMBER);
+    isNotGoodNumber: function (number) {
+      if (Number(number) % NUM.LOTTO_PRICE !== 0)
+        throw new Error(ERRORMESSAGE.ISNOTGOODNUMBER);
+    },
   },
 });
 
