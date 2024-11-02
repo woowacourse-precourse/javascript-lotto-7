5f749123c33aed2cf08117f766770f1c6c4004a2
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-02 23:08:32 +0900

    chore : 에러 문구 수정

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,13 +1,13 @@
 export const ERROR_MESSAGES = Object.freeze({
-  purchaseAmount: '구입 금액은 양의 정수여야 합니다.',
+  purchaseAmount: '[ERROR] 구입 금액은 양의 정수여야 합니다.',
   lottoNumbers: {
-    invalidArray: '로또 번호는 6개의 숫자로 구성된 배열이어야 합니다.',
+    invalidArray: '[ERROR] 로또 번호는 6개의 숫자로 구성된 배열이어야 합니다.',
     duplicateNumbers:
-      '로또 번호는 중복되지 않는 6개의 숫자로 구성되어야 합니다.',
-    outOfRange: '로또 번호는 1에서 45 사이의 정수여야 합니다.',
+      '[ERROR] 로또 번호는 중복되지 않는 6개의 숫자로 구성되어야 합니다.',
+    outOfRange: '[ERROR] 로또 번호는 1에서 45 사이의 정수여야 합니다.',
   },
   bonusNumber: {
-    outOfRange: '보너스 번호는 1에서 45 사이의 정수여야 합니다.',
-    duplicateWithLotto: '보너스 번호는 로또 번호와 중복될 수 없습니다.',
+    outOfRange: '[ERROR] 보너스 번호는 1에서 45 사이의 정수여야 합니다.',
+    duplicateWithLotto: '[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.',
   },
 });
