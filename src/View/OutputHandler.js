89f02f76105b59e956e6dc3a12f52ecf0178ab0b
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-02 20:24:42 +0900

    refactor: 메서드 및 변수명 수정

--- src/View/OutputHandler.js
+++ src/View/OutputHandler.js
@@ -1,6 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
-import { PROGRESS_MESSAGE } from '../constants/Message.js';
-import LOTTO_PRIZES from '../Constants/LottoPrizes.js';
+import {
+  LOTTO_RESULT_MESSAGES,
+  PROGRESS_MESSAGE,
+} from '../constants/Message.js';
 
 class OutputHandler {
   static showPurchsedLotto(lottoTickets) {
@@ -11,11 +13,11 @@ class OutputHandler {
   }
   static showWinningStatus(WinningStatus) {
     Console.print(PROGRESS_MESSAGE.WINNING_RESULT);
-    Console.print(`${LOTTO_PRIZES.FIFTH}${WinningStatus.fifthPrizeCount}개`);
-    Console.print(`${LOTTO_PRIZES.FOURTH}${WinningStatus.fourthPrizeCount}개`);
-    Console.print(`${LOTTO_PRIZES.THIRD}${WinningStatus.thirdPrizeCount}개`);
-    Console.print(`${LOTTO_PRIZES.SECOND}${WinningStatus.secondPrizeCount}개`);
-    Console.print(`${LOTTO_PRIZES.FRIST}${WinningStatus.firstPrizeCount}개`);
+    Console.print(`${LOTTO_RESULT_MESSAGES.FIFTH}${WinningStatus[5]}개`);
+    Console.print(`${LOTTO_RESULT_MESSAGES.FOURTH}${WinningStatus[4]}개`);
+    Console.print(`${LOTTO_RESULT_MESSAGES.THIRD}${WinningStatus[3]}개`);
+    Console.print(`${LOTTO_RESULT_MESSAGES.SECOND}${WinningStatus[2]}개`);
+    Console.print(`${LOTTO_RESULT_MESSAGES.FIRST}${WinningStatus[1]}개`);
   }
   static showProfitRate(profitRate) {
     Console.print(`총 수익률은 ${profitRate}% 입니다.`);
