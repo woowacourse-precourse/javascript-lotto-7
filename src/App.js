08823c18e445749356d39cad5478fc66d73b60ee
Juyudang <ssuw223@gmail.com>
2024-11-02 14:15:43 +0900

    Docs: Edit README.md
    Write down what to make & how to make.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import lotto from "./Lotto.js";
 class App {
-  async run() {}
+  async run() {
+    let arr3 = [1100, 2, 30]
+    console.log(arr3.sort((a, b) => a - b))
+
+  }
 }
 
 export default App;
