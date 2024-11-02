028f9b939c474907a87d7a459ac83b803d524efe
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-03 06:04:40 +0900

    feat: 로또 번호와 당첨 번호 비교 로직 추가

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -1,12 +1,38 @@
+import { RANK } from '../constants/constants.js';
+import { validateLottoNumbers } from '../util/validators.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    validateLottoNumbers(numbers);
     this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   getNumbers() {
     return [...this.#numbers];
+  }
+
+  #getMatchCount(winningNumbers) {
+    return this.#numbers.filter((number) => winningNumbers.includes(number))
+      .length;
+  }
+
+  #hasBonusMatch(bonusNumber) {
+    return this.#numbers.includes(bonusNumber);
+  }
+
+  getMatchCount({ winningNumbers, bonusNumber }) {
+    const matchCount = this.#getMatchCount(winningNumbers);
+
+    if (
+      matchCount === RANK.THIRD.matchCount &&
+      this.#hasBonusMatch(bonusNumber)
+    ) {
+      return RANK.SECOND.matchCount;
+    }
+
+    return matchCount;
   }
 }
 
