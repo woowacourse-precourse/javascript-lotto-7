2ec67378836a3abed6d2f32438f77188c987c295
jiyun1006 <jiyun4251@gmail.com>
2024-11-02 11:27:00 +0900

    ✨ feat : 입출력문에 대한 상수화
    입력문, 출력문에 쓰이는 문구들을 ioMessage.js에 객체로 저장

---
+++ src/constants/ioMessage.js
@@ -0,0 +1,9 @@
+const IOMESSAGE = Object.freeze({
+  INPUT_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
+  OUTPUT_PURCHASE_AMOUN: '개를 구매했습니다.',
+  INPUT_LOTTO_NUMBER: '당첨 번호를 입력해 주세요.',
+  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  WINNING_RESULT: '당첨 통계',
+  DIVIDER: '---',
+  WINNING_COUNT_MESSAGE: '개 일치',
+});
