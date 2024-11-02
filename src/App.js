df071133031b10482f8fef68026944567c120bc4
salmonco <xkqjsslsek80@naver.com>
2024-11-02 17:46:08 +0900

    feat(app): 당첨 번호를 입력받기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App {
     const price = await this.readPrice();
 
     // - 당첨 번호 6개를 입력받는다.
+    const winningNumbersString = await this.readWinningNumbers();
+
     // - 당첨 번호를 쉼표(,)를 기준으로 구분한다.
     // - 보너스 번호 1개를 입력받는다.
     // - 1~45 사이의 중복되지 않는 6개 숫자를 랜덤으로 뽑는다.
@@ -22,6 +24,10 @@ class App {
 
   readPrice() {
     return Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  }
+
+  readWinningNumbers() {
+    return Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
   }
 }
 
