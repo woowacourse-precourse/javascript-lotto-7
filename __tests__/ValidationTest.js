5cd0e77925541cd1a482e2039e2419c77d289492
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-02 23:26:31 +0900

    chore: expect 함수에 중괄호 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -2,28 +2,36 @@ import Validation from '../src/Contoller/Validation.js';
 
 describe('Validation 테스트', () => {
   test('구매 금액이 1000원 단위가 아니면 예외가 발생한다', () => {
-    expect(() => Validation.validateThousandUnit(1500)).toThrow(
-      '[ERROR] 구매 금액은 1000원 단위여야 합니다.'
-    );
-    expect(() => Validation.validateThousandUnit(1000)).not.toThrow();
+    expect(() => {
+      Validation.validateThousandUnit(1500);
+    }).toThrow('[ERROR] 구매 금액은 1000원 단위여야 합니다.');
+
+    expect(() => {
+      Validation.validateThousandUnit(1000);
+    }).not.toThrow();
   });
 
   test('입력값을 입력하지 않은 경우 예외가 발생한다.', () => {
-    expect(() => Validation.validateNotEmpty('')).toThrow(
-      '[ERROR] 입력값이 없습니다.'
-    );
-    expect(() => Validation.validateNotEmpty(1000)).not.toThrow();
+    expect(() => {
+      Validation.validateNotEmpty('');
+    }).toThrow('[ERROR] 입력값이 없습니다.');
+
+    expect(() => {
+      Validation.validateNotEmpty(1000);
+    }).not.toThrow();
   });
 
   test('로또 번호가 6개가 아닌 경우 예외가 발생한다.', () => {
     expect(() => {
       Validation.validateSixNumbers([1, 2, 3, 4, 5, 6]);
     }).not.toThrow();
-    expect(() => Validation.validateSixNumbers([1, 2, 3, 4, 5])).toThrow(
-      '[ERROR] 로또 번호는 6개여야 합니다.'
-    );
-    expect(() => Validation.validateSixNumbers([1, 2, 3, 4, 5, 6, 7])).toThrow(
-      '[ERROR] 로또 번호는 6개여야 합니다.'
-    );
+
+    expect(() => {
+      Validation.validateSixNumbers([1, 2, 3, 4, 5]);
+    }).toThrow('[ERROR] 로또 번호는 6개여야 합니다.');
+
+    expect(() => {
+      Validation.validateSixNumbers([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow('[ERROR] 로또 번호는 6개여야 합니다.');
   });
 });
