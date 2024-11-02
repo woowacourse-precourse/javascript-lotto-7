a783e9e3b9b3321f8d594b6d8e4e0c6ab87ba5cd
seonae-j <wkdtjsdo135@naver.com>
2024-11-02 19:03:55 +0900

    refactor(Random): Array.from으로 랜덤 배열 생성 및 데이터 숫자 변환 리팩토링

--- src/utils/Random.js
+++ src/utils/Random.js
@@ -1,17 +1,13 @@
 import { Random } from '@woowacourse/mission-utils';
 
 export function randomTicket() {
-  const randomArray = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
-    (a, b) => a - b
-  );
+  const randomArray = Random.pickUniqueNumbersInRange(1, 45, 6)
+    .sort((a, b) => a - b)
+    .map(Number);
   return randomArray;
 }
 
 export function ticketArray(tickets) {
-  const ticketArray = [];
-  for (let i = 0; i < tickets; i++) {
-    ticketArray.push(randomTicket());
-  }
-
+  const ticketArray = Array.from({ length: tickets }, () => randomTicket());
   return ticketArray;
 }
