58a2413b230e9835d71d90e0738bd693f46a8544
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-02 23:00:14 +0900

    refactor : 검증 메시지 분리

---
+++ src/utils/validator.js
@@ -0,0 +1,40 @@
+import { ERROR_MESSAGES } from '../constants/errorMessage';
+
+class Validator {
+  static validatePurchaseAmount(amount) {
+    if (!Number.isInteger(amount) || amount <= 0) {
+      throw new Error(ERROR_MESSAGES.purchaseAmount);
+    }
+    return true;
+  }
+
+  static validateLottoNumbers(numbers) {
+    if (!Array.isArray(numbers) || numbers.length !== 6) {
+      throw new Error(ERROR_MESSAGES.lottoNumbers.invalidArray);
+    }
+    const uniqueNumbers = new Set(numbers);
+
+    if (uniqueNumbers.size !== 6) {
+      throw new Error(ERROR_MESSAGES.lottoNumbers.duplicateNumbers);
+    }
+
+    for (let number of numbers) {
+      if (!Number.isInteger(number) || number < 1 || number > 45) {
+        throw new Error(ERROR_MESSAGES.lottoNumbers.outOfRange);
+      }
+    }
+    return true;
+  }
+
+  static validateBonusNumber(bonusNumber, lottoNumbers) {
+    if (!Number.isInteger(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error(ERROR_MESSAGES.bonusNumber.outOfRange);
+    }
+    if (lottoNumbers.includes(bonusNumber)) {
+      throw new Error(ERROR_MESSAGES.bonusNumber.duplicateWithLotto);
+    }
+    return true;
+  }
+}
+
+export default Validator;
