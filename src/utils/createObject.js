3db6faba8e61ba1fa666dafd6415b2703a7b6473
jiyun1006 <jiyun4251@gmail.com>
2024-11-02 18:16:42 +0900

    ✨ feat : 로또 번호를 만드는 기능 작성
    createMyLotto를 작성하여 랜덤한 6가지 번호를 가지는 로또 번호 생성

--- src/utils/createObject.js
+++ src/utils/createObject.js
@@ -1,11 +1,26 @@
 import validation from '../validation.js';
 import retry from './retry.js';
-import { inputMethod } from './ioMethod.js';
+import { inputMethod, outputMethod } from './ioMethod.js';
+import { Random } from '@woowacourse/mission-utils';
+import { NUM } from '../constants/index.js';
 
 const createObject = {
   createPurchaseAmount: async function () {
     const validationCondition = Object.values(validation.purchaseAmount);
     return await retry(inputMethod.inputPurchaseAmount, validationCondition);
+  },
+  createMyLotto: function (lottoCount) {
+    const myLotto = Array.from({ length: lottoCount }).map((numbers) => {
+      numbers = Random.pickUniqueNumbersInRange(
+        NUM.STARTINCLUSIVE,
+        NUM.ENDINCLUSIVE,
+        NUM.LOTTO_NUMBER_COUNT,
+      );
+      outputMethod(`[${numbers.join(', ')}]`);
+      return numbers;
+    });
+    outputMethod('\n');
+    return myLotto;
   },
 };
 
