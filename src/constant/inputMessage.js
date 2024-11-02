9e952f5ee43801bc242dcc071b1e2a615b4a6a72
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-02 21:16:36 +0900

    feat : input message 모음 생성

---
+++ src/constant/inputMessage.js
@@ -0,0 +1,7 @@
+export const INPUT_MESSAGE = {
+  purchaseAmountPrompt: '구입금액을 입력해 주세요.',
+  purchaseMessage: '개를 구매했습니다.',
+  winningNumberPrompt: '당첨 번호를 입력해 주세요.',
+  bounusNumberPrompt: '보너스 번호를 입력해 주세요',
+  winningTotalMessage: '당첨 통계\n---',
+};
