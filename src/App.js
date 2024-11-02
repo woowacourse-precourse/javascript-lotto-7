990b2679aff24bc15e62ccf1cab8b5650633c9fa
devjaeyoon <devjaeyoon@gmail.com>
2024-11-02 11:49:10 +0900

    refactor(App): 에러 처리 및 LottoController 통합
    - LottoController 인스턴스화 및 실행 로직 추가
    - try-catch 블록을 사용한 전역 에러 처리 구현
    - 발생한 에러 메시지를 OutputView의 printMessage 메소드를 통해 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import LottoController from './controllers/LottoController.js';
+import OutputView from './views/OutputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const lottoController = new LottoController();
+      await lottoController.run();
+    } catch (error) {
+      OutputView.printMessage(error.message);
+    }
+  }
 }
 
 export default App;
