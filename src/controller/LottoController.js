4a819c4da215371a92993e6a34eda9c436d02458
yena <rladpsk1117@gmail.com>
2024-11-03 02:22:59 +0900

    feat(LottoController): 당첨 번호 입력 기능 구현
    InputView를 통해 입력된 값을 Lotto 클래스로 전달
    유효하지 않은 입력일 경우 에러 메시지 출력 후 재입력 받음

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,9 +1,27 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputView from '../view/InputView.js';
+import Lotto from '../model/Lotto.js';
+
 class LottoController {
   #purchaseAmount; // 구입금액
   #lotteryNumbers; // 구입한 로또 번호
   #winnigLottoNumbers; // 당첨 번호
   #bonusNumber; // 보너스 번호
 
-  async start() {}
+  async start() {
+    this.#winnigLottoNumbers = await this.#getWinningLottoNumbers();
+  }
+
+  async #getWinningLottoNumbers() {
+    try {
+      const input = await InputView.inputWinningNumbers();
+      const splittedInput = input.split(',');
+      const convertInput = splittedInput.map((num) => Number(num));
+      this.#winnigLottoNumbers = new Lotto(convertInput);
+    } catch (error) {
+      Console.print(error);
+      await this.#getWinningLottoNumbers();
+    }
+  }
 }
 export default LottoController;
