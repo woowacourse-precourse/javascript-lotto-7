42e83572cb9dbedaf86e25a671ee515e40d1936c
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-02 14:52:09 +0900

    style: 들여쓰기를 수정한다

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -2,9 +2,9 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 import { getUserMoney, getWinningNumbers, getBonusNumber } from '../src/views/InputView.js';
 
-import { validateUserMoney } from '../src/validators/InputValidator.js';
+import { validateNumber, validateUserMoney } from '../src/validators/InputValidator.js';
 
-import { INPUT_ERROR_MESSAGE, INVALID_USER_MONEY_ERROR_MESSAGE } from '../src/constants/message.js';
+import { INPUT_ERROR_MESSAGE, INVALID_USER_MONEY_ERROR_MESSAGE, INVALID_NUMBER_ERROR_MESSAGE } from '../src/constants/message.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -30,40 +30,8 @@ describe('사용자 입력 테스트하기', () => {
 
       await expect(getUserMoney()).rejects.toThrow(INPUT_ERROR_MESSAGE);
     });
-    describe('당첨 번호 입력받기', () => {
-      test('당첨 번호를 정수 배열로 입력받는다', async () => {
-        const inputs = ['1, 2, 3, 4, 5, 6'];
-        mockQuestions(inputs);
-
-        const winningNumber = await getWinningNumbers();
-        expect(winningNumber).toEqual([1, 2, 3, 4, 5, 6]);
-      });
-
-      test('당첨 번호를 입력받는 도중에 예외가 발생하면 에러 메시지를 반환한다.', async () => {
-        mockQuestions(new Error('error'));
-
-        await expect(getWinningNumbers()).rejects.toThrow(INPUT_ERROR_MESSAGE);
-      });
-    });
-
-    describe('보너스 번호 입력받기', () => {
-      test('보너스 번호를 정수로 입력받는다', async () => {
-        const inputs = ['11'];
-        mockQuestions(inputs);
-
-        const bonusNumber = await getBonusNumber();
-        expect(bonusNumber).toBe(11);
-      });
-    });
-
-    test('보너스 번호를 입력받는 도중에 예외가 발생하면 에러 메시지를 반환한다.', async () => {
-      mockQuestions(new Error('error'));
-
-      await expect(getBonusNumber()).rejects.toThrow(INPUT_ERROR_MESSAGE);
-    });
   });
-
-  describe('유효성 검사하기', () => {
+  describe('구입금액 유효성 검사하기', () => {
     test('사용자의 구입금액이 1,000원으로 나누어 떨어지지 않는다면 예외 처리한다.', () => {
       expect(() => validateUserMoney(100)).toThrow(INVALID_USER_MONEY_ERROR_MESSAGE);
       expect(() => validateUserMoney(10005)).toThrow(INVALID_USER_MONEY_ERROR_MESSAGE);
@@ -72,5 +40,48 @@ describe('사용자 입력 테스트하기', () => {
     test('사용자의 구입금액이 1,000원으로 나누어 떨어진다면 예외 처리하지 않는다.', () => {
       expect(() => validateUserMoney(1000)).not.toThrow();
     });
+  });
+
+  describe('당첨 번호 입력받기', () => {
+    test('당첨 번호를 정수 배열로 입력받는다', async () => {
+      const inputs = ['1, 2, 3, 4, 5, 6'];
+      mockQuestions(inputs);
+
+      const winningNumber = await getWinningNumbers();
+      expect(winningNumber).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    test('당첨 번호를 입력받는 도중에 예외가 발생하면 에러 메시지를 반환한다.', async () => {
+      mockQuestions(new Error('error'));
+
+      await expect(getWinningNumbers()).rejects.toThrow(INPUT_ERROR_MESSAGE);
+    });
+  });
+
+  describe('보너스 번호 입력받기', () => {
+    test('보너스 번호를 정수로 입력받는다', async () => {
+      const inputs = ['11'];
+      mockQuestions(inputs);
+
+      const bonusNumber = await getBonusNumber();
+      expect(bonusNumber).toBe(11);
+    });
+
+    test('보너스 번호를 입력받는 도중에 예외가 발생하면 에러 메시지를 반환한다.', async () => {
+      mockQuestions(new Error('error'));
+
+      await expect(getBonusNumber()).rejects.toThrow(INPUT_ERROR_MESSAGE);
+    });
+  });
+});
+
+describe('공통적인 예외 처리', () => {
+  test('입력 값이 정수가 아닌 값이라면 예외 처리한다.', () => {
+    expect(() => validateNumber(undefined)).toThrow(INVALID_NUMBER_ERROR_MESSAGE);
+    expect(() => validateNumber('Hi')).toThrow(INVALID_NUMBER_ERROR_MESSAGE);
+  });
+  test('입력 값이 정수 값이라면 예외 처리하지 않는다.', () => {
+    expect(() => validateNumber(1000)).not.toThrow();
+    expect(() => validateNumber('5000')).not.toThrow();
   });
 });
