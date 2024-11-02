6855aa3c2178a2486e5859e3b3ae9b1929e64957
Seio924 <seio9241@gmail.com>
2024-11-02 18:58:33 +0900

    feat(Lotto): lotto 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,7 +8,12 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error('[ERROR] 로또 번호는 반드시 6개여야 합니다.');
+    }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error('[ERROR] 중복된 당첨 번호가 있습니다.');
     }
   }
 
