38acda4fa10b8ab0a1543c29b0df90dd4868ae1b
yena <rladpsk1117@gmail.com>
2024-11-02 23:33:25 +0900

    feat(Lotto): 당첨 번호 유효성 체크 및 예외 처리 구현

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -10,6 +10,24 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] 로또 번호는 중복하여 입력할 수 없습니다.');
+    }
+
+    numbers.forEach((num) => {
+      if ((num < 1) | (num > 45)) {
+        throw new Error('[ERROR] 로또 번호 숫자 범위는 1~45입니다.');
+      }
+
+      if (isNaN(num)) {
+        throw new Error('[ERROR] 로또 번호는 숫자만 입력 가능합니다.');
+      }
+
+      if (num % 1 !== 0) {
+        throw new Error('[ERROR] 로또 번호는 정수만 입력 가능합니다.');
+      }
+    });
   }
 }
 
