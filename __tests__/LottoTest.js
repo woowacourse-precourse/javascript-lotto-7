8b0ce0da3b8da40efe82762625a941edb29dce7a
haesa <xdhaesa@gmail.com>
2024-11-03 07:05:41 +0900

    test(Lotto): 당첨 번호와 일치하는 번호 개수 및 보너스 번호 일치 여부를 확인하는 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,20 @@
 import Lotto from '../src/Lotto';
 
 describe('로또 클래스 테스트', () => {
+  test('당첨 번호와 일치하는 번호 개수 및 보너스 번호 일치 여부 확인 기능 테스트', () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonus = 7;
+    const expected = {
+      matchingCount: 5,
+      hasBonus: true,
+    };
+
+    const lotto = new Lotto([1, 2, 3, 4, 5, 7]);
+    const matchResult = lotto.match(winningNumbers, bonus);
+
+    expect(matchResult).toEqual(expected);
+  });
+
   test('로또 번호의 개수가 6개가 아니면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
