d56aad3dd77c6e1673cfcde1311b56bd7d561d20
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-11-03 05:18:50 +0900

    test :refactor test each case

--- src/test/statistics.test.js
+++ src/test/statistics.test.js
@@ -2,10 +2,12 @@ import Statistics from '../utils/Statistics.js';
 
 describe('금액별 개수 나누기', () => {
   test('정상 케이스', () => {
-    const input = ['5,000원', '5,000원', '50,000원'];
+    const input = [0, 2, 2, 3, 4, 4];
     const expectedOutput = [
-      { money: '5,000원', count: 2 },
-      { money: '50,000원', count: 1 },
+      { money: '0', count: 1 },
+      { money: '2', count: 2 },
+      { money: '3', count: 1 },
+      { money: '4', count: 2 },
     ];
 
     const result = Statistics.countOccurrences(input);
@@ -22,18 +24,9 @@ describe('금액별 개수 나누기', () => {
     expect(result).toEqual(expectedOutput);
   });
 
-  test('한 개 있을 때 테스트', () => {
-    const input = ['5,000원'];
-    const expectedOutput = [{ money: '5,000원', count: 1 }];
-
-    const result = Statistics.countOccurrences(input);
-
-    expect(result).toEqual(expectedOutput);
-  });
-
   test('모두 동일한 요소 테스트', () => {
-    const input = ['50,000원', '50,000원', '50,000원'];
-    const expectedOutput = [{ money: '50,000원', count: 3 }];
+    const input = [2, 2, 2, 2];
+    const expectedOutput = [{ money: '2', count: 4 }];
 
     const result = Statistics.countOccurrences(input);
 
