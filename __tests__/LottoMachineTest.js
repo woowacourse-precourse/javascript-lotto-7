552fe76237b3be04e593f5c6c3b0d87117cac8de
Seio924 <seio9241@gmail.com>
2024-11-02 20:15:17 +0900

    test(LottoNumberGenerateServiceTest): 로또 번호 생성 테스트 분리

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -2,22 +2,6 @@ import LottoMachineService from '../src/Service/LottoMachineService';
 import { mockRandoms } from './ApplicationTest';
 
 describe('LottoMachineService 테스트', () => {
-  test('1~45 사이의 중복되지 않는 6개의 숫자를 뽑는다.', () => {
-    const mockValues = [[8, 21, 23, 41, 42, 43]];
-
-    mockRandoms(mockValues);
-
-    const lottoMachine = new LottoMachineService();
-    const lottoNumbers = lottoMachine.generateLottoNumbers();
-
-    expect(lottoNumbers).toHaveLength(6);
-
-    lottoNumbers.forEach(number => {
-      expect(number).toBeGreaterThanOrEqual(1);
-      expect(number).toBeLessThanOrEqual(45);
-    });
-  });
-
   test('구입 금액에 해당하는 만큼 로또를 발행한다.', () => {
     const purchaseAmount = 3000;
     const mockValues = [
