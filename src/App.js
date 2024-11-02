83e1dcd7eee0bf8b4059018335686bf1202a2724
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-03 02:10:14 +0900

    [feat]: 로또 구매 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import Input from './Input.js';
+import Output from './Output.js';
+import LottoMachine from './LottoMachine.js';
+import Lotto from './Lotto.js';
+import { validatePurchaseAmount } from './util/validator.js';
+
 class App {
-  async run() {}
+  async run() {
+    const { lottos, purchaseAmount } = await this.#buyLottos();
+
+    Output.printLottos(lottos.map((lotto) => lotto.getNumbers()));
+  }
+
+  async #tryInput(inputFunction) {
+    try {
+      const input = await inputFunction();
+      return input;
+    } catch (error) {
+      Output.printErrorMessage(error.message);
+      return await this.#tryInput(inputFunction);
+    }
+  }
+
+  async #tryPurchaseAmount() {
+    const purchaseAmount = await Input.getPurchaseAmountInput();
+    validatePurchaseAmount(purchaseAmount);
+    return purchaseAmount;
+  }
+
+  async #buyLottos() {
+    const purchaseAmount = await this.#tryInput(
+      this.#tryPurchaseAmount.bind(this),
+    );
+    const lottoMachine = new LottoMachine(Number(purchaseAmount));
+    const lottos = lottoMachine.getLottos().map((lotto) => new Lotto(lotto));
+
+    return { lottos, purchaseAmount };
+  }
 }
 
 export default App;
