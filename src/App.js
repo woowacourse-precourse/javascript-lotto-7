c9cd80e1197181365b78c1a88444b54c62e8a44b
Juyudang <ssuw223@gmail.com>
2024-11-02 19:44:13 +0900

    Feat: Add Purchase input check
    Make "Lotto.checkPurchase(input)" for checking the input is fit for Purchase requirements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import lotto from "./Lotto.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
   async run() {
-    let arr3 = [1100, 2, 30]
-    console.log(arr3.sort((a, b) => a - b))
+    try {
+      let input = await MissionUtils.Console.readLineAsync('구입 금액을 입력해 주세요.\n');
+      const PURCHASE = Lotto.checkPurchase(input);
+      MissionUtils.Console.print(PURCHASE);
+      // buyLottos()
+      // const lotto = new Lotto();
+      // const WINNUMS = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+      // MissionUtils.Console.print("");
+      // setWINNUMS()
+      // const BONUSNUM = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+      // setBONUSNUM()
 
+      // showResult()
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 }
+
+// function checkPruchase(num) {
+//   if (Number(num)) {
+//     if (num%1000) {
+//       throw new Error("[ERROR] 구입 금액은 1,000의 배수여야 합니다.");
+//     }
+
+//   }
+// }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
@@ -10,6 +12,17 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  static checkPurchase(num) {
+    const NUM = Number(num);
+    if (NUM) {
+      if (NUM % 1000) {
+        throw new Error("[ERROR] 구입 금액은 1,000의 배수여야 합니다.");
+      }
+      return NUM
+    }
+    throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
   }
 
   // TODO: 추가 기능 구현
