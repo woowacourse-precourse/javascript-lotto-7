e72aa864e07fe937735aa2ef07a79f1cf9e80657
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-02 14:15:02 +0900

    refactor: 구입금액을 정수로 입력받도록 테스트를 변경한다

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -17,12 +17,12 @@ const mockQuestions = (inputs) => {
 
 describe('사용자 입력 테스트하기', () => {
   describe('구입금액 입력받기', () => {
-    test('정상적으로 구입금액을 입력받는다', async () => {
+    test('구입금액을 정수로 입력받는다', async () => {
       const inputs = ['1000'];
       mockQuestions(inputs);
 
       const userMoney = await getUserMoney();
-      expect(userMoney).toBe('1000');
+      expect(userMoney).toBe(1000);
     });
 
     test('구입금액을 입력받는 도중에 예외가 발생하면 에러 메시지를 반환한다.', async () => {
