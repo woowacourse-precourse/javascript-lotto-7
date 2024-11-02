93a340b592fbadf7fcb7cdf1c148496b99cb006d
Yusi <yusi4781@naver.com>
2024-11-02 19:02:30 +0900

    feat(app): 갯수에 따라 오름차순으로 정렬된 로또 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import ConsoleView from "./views/ConsoleView.js";
+import LottoController from "./controllers/LottoController.js";
 
 class App {
   async run() {
     const consoleView = new ConsoleView();
     const purchaseAmount = await consoleView.getPurchaseAmount();
-    consoleView.printLottoCount(purchaseAmount);
+    const lottoController = new LottoController();
   }
 }
-
+    const lottos = lottoController.generateLottos(purchaseAmount);
 export default App;
---
+++ src/controllers/LottoController.js
@@ -0,0 +1,20 @@
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "../models/Lotto.js";
+
+class LottoController {
+
+  generateLottos(purchaseAmount) {
+    const lottos = [];
+    const count = purchaseAmount / 1000;
+
+    for (let i = 0; i < count; i++) {
+      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      const lotto = new Lotto(randomNumbers);
+      lottos.push(lotto);
+    }
+
+    return lottos;
+  }
+}
+
+export default LottoController;
