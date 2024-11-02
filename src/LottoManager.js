54372c25dd49ea22e476ea4a6f6a346570d33072
erica-jeong <wjddbwjd1205@gmail.com>
2024-11-03 01:36:04 +0900

    feat: 로또 번호 생성 기능 추가
    - 구매한 로또 개수만큼 중복되지 않는 6개의 번호 생성
    - 번호 범위는 1~45로 설정
    - Lotto 클래스 활용

--- src/LottoManager.js
+++ src/LottoManager.js
@@ -1,6 +1,9 @@
+import { Random } from '@woowacourse/mission-utils';
 import InputHandler from "./InputHandler.js";
-import { LOTTO_PRICE } from "./lottoConstants.js";
+import { LOTTO_PRICE, LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX,
+  LOTTO_NUMBER_COUNT } from "./lottoConstants.js";
 import OutputHandler from "./OutputHandler.js";
+import Lotto from './Lotto.js';
 
 class LottoManager {
   #inputHandler;
@@ -16,6 +19,7 @@ class LottoManager {
       const amount = await this.#buyLotto();
       const lottoCount = this.#calculateLottoCount(amount);
       this.#outputHandler.printLottoCount(lottoCount);
+      const lottoTickets = this.#generateLottoTickets(lottoCount);
     } catch (error) {
       throw error;
     }
@@ -34,6 +38,22 @@ class LottoManager {
 
   #calculateLottoCount(amount) {
     return amount / LOTTO_PRICE;
+  }
+
+  #generateLottoTickets(lottoCount) {
+    let LottoTickets = [];
+    for(let i = 0; i < lottoCount; i++) {
+      const lottoNumbers = Random.pickUniqueNumbersInRange(
+        LOTTO_NUMBER_MIN,
+        LOTTO_NUMBER_MAX,
+        LOTTO_NUMBER_COUNT
+      );
+
+      lottoNumbers.sort((a, b) => a - b);
+      const lottoTicket = new Lotto(lottoNumbers);
+      LottoTickets.push(lottoTicket);
+    }
+    return LottoTickets;
   }
 }
 
