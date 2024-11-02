877d930f2d90eea2e2b2d87d6309e9cee35b41b0
장정안 <wjddks96@naver.com>
2024-11-02 19:34:35 +0900

    refactor: Game클래스는 controller에 맞는 책임만 가지게 리팩토링

--- src/Controller/Game.js
+++ src/Controller/Game.js
@@ -1,48 +1,50 @@
 //@ts-check
 
-import { MissionUtils } from '@woowacourse/mission-utils';
 import { GAME_MESSAGE } from '../constants/gameMessage.js';
-import { LOTTO } from '../constants/lotto.js';
+import { LOTTO_CONFIG } from '../constants/lotto.js';
 import User from '../User/User.js';
-import { outputView } from '../views/outputView.js';
+import LottoService from '../Service/LottoService.js';
+import LottoResultView from '../views/LottoResultView.js';
 
 class Game {
   constructor() {
     this.user = new User();
+    this.lottoService = new LottoService();
+    this.resultView = new LottoResultView();
   }
 
   async process() {
     const purchaseAmount = await this.user.readUserInput(GAME_MESSAGE.PURCHASE);
-    this.lotto(Number(purchaseAmount));
+    const count = purchaseAmount / LOTTO_CONFIG.PRICE_PER;
+
+    const lottos = this.lottoService.createLottos(count);
+    this.resultView.printPurchaseResult(count, lottos);
+
+    const winningNumbers = await this.getWinningNumbers();
+    const bonusNumber = await this.getBonusNumber();
+
+    const results = this.lottoService.calculateResults(
+      lottos,
+      winningNumbers,
+      bonusNumber
+    );
+    this.resultView.printResults(results);
+
+    const earningRate = this.lottoService.calculateEarningRate(
+      purchaseAmount,
+      results
+    );
+    this.resultView.printEarningRate(earningRate);
   }
 
-  /**@param {number} purchaseAmount  */
-  lotto(purchaseAmount) {
-    const lottoCount = purchaseAmount / LOTTO.PRICE_PER;
-    outputView.printMessage(`${lottoCount}${GAME_MESSAGE.BOUGHT}`);
-    this.printLottos(lottoCount);
+  async getWinningNumbers() {
+    const input = await this.user.readUserInput(GAME_MESSAGE.WINNING_NUMBER);
+    return input.split(',').map((num) => Number(num.trim()));
   }
 
-  /**@param {number} count  */
-  createLotto(count) {
-    const lottos = [];
-    for (let i = 0; i < count; i++) {
-      const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
-        LOTTO.MIN_NUMBER,
-        LOTTO.MAX_NUMBER,
-        LOTTO.NUMBERS_PER
-      );
-      lottos.push(randomNumbers.sort((a, b) => a - b));
-    }
-    return lottos;
-  }
-
-  /**@param {number} count  */
-  printLottos(count) {
-    const lottos = this.createLotto(count);
-    lottos.forEach((lotto) => {
-      outputView.printMessage(`[${lotto.join(', ')}]`);
-    });
+  async getBonusNumber() {
+    const input = await this.user.readUserInput(GAME_MESSAGE.BONUS_NUMBER);
+    return Number(input);
   }
 }
 
