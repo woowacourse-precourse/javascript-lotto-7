e8fa1c9efb34810512efa97e88dd74b7ff02a7ba
youngju6143 <youngju6143@naver.com>
2024-11-02 18:39:35 +0900

    feat(input): 입력 기능들 구현(구매 금액, 당첨 번호, 보너스 번호)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
 class App {
-  async run() {}
+  async run() {
+    try {
+      let amount = await Console.readLineAsync(
+        '구입금액을 입력해 주세요.\n'
+      );
+      let answerNum = await Console.readLineAsync(
+        '당첨 번호를 입력해 주세요.\n'
+      );
+      answerNum = answerNum.split(',').map(num => num.trim());
+
+      let bonusNum = await Console.readLineAsync(
+        '보너스 번호를 입력해 주세요.\n'
+      );
+
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
 
 export default App;
