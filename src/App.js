db012fdd535693bb53b1d41e96af4d5b536a08c3
Yusi <yusi4781@naver.com>
2024-11-02 16:42:03 +0900

    feat(app): 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await Console.readLineAsync();
+  }
 }
 
 export default App;
