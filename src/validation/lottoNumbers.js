fc5cc2bc4242464fe56570b9351973b47caa74b3
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-02 19:37:50 +0900

    refactor: lottoNumbers.js 상수화 및 중복 코드 리팩토링

--- src/validation/lottoNumbers.js
+++ src/validation/lottoNumbers.js
@@ -1,5 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 import { getWinningNumbers, getBonusNumber } from '../utils/getUserInput.js';
+import { LOTTO, ERROR_MESSAGES } from '../utils/constants.js';
+
+function isInRange(number, range = LOTTO.NUMBER_RANGE) {
+  return number >= range.MIN && number <= range.MAX;
+}
 
 async function validateWinningNumbers(winningNumbers) {
   try {
@@ -27,10 +32,10 @@ async function validateBonusNumber(winningNumberArray, bonusNumber) {
 
 function validateSixNumbers(winningNumbers) {
   const validateWinningNumbers = winningNumbers
-    .split(',')
+    .split(LOTTO.SEPARATOR)
     .map((num) => num.trim());
-  if (validateWinningNumbers.length !== 6) {
-    throw new Error('[ERROR] 입력할 로또 번호는 6개이며 ,(쉼표)로 구분합니다.');
+  if (validateWinningNumbers.length !== LOTTO.WINNING_NUMBERS_COUNT) {
+    throw new Error(ERROR_MESSAGES.INVALID_WINNING_NUMBERS);
   }
 
   return validateWinningNumbers;
@@ -39,34 +44,34 @@ function validateSixNumbers(winningNumbers) {
 function validateAllNumeric(winningNumbers) {
   winningNumbers.forEach((number) => {
     if (isNaN(number)) {
-      throw new Error('[ERROR] 입력값은 숫자여야 합니다.');
+      throw new Error(ERROR_MESSAGES.NON_NUMERIC_VALUE);
     }
   });
 }
 
 function validateAllInRange(winningNumbers) {
   winningNumbers.forEach((number) => {
-    if (number < 1 || number > 45) {
-      throw new Error('[ERROR] 로또 번호는 1부터 45까지의 숫자입니다.');
+    if (!isInRange) {
+      throw new Error(ERROR_MESSAGES.LOTTO_RANGE);
     }
   });
 }
 
 function validateIsNumeric(bonusNumber) {
   if (Number.isNaN(Number(bonusNumber))) {
-    throw new Error('[ERROR] 입력은 숫자와 구분자 ,만 허용됩니다.');
+    throw new Error(ERROR_MESSAGES.NON_NUMERIC_BONUS);
   }
 }
 
 function validateInRange(bonusNumber) {
-  if (bonusNumber < 1 || bonusNumber > 45) {
-    throw new Error('[ERROR] 로또 번호는 1부터 45까지의 숫자입니다.');
+  if (!isInRange) {
+    throw new Error(ERROR_MESSAGES.LOTTO_RANGE);
   }
 }
 
 function validateBonusNumberUniqueness(winningNumbersArray, bonusNumber) {
   if (winningNumbersArray.includes(Number(bonusNumber))) {
-    throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+    throw new Error(ERROR_MESSAGES.DUPLICATE_BONUS_NUMBER);
   }
 }
 
