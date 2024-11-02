1ac8b46fe48df543dc4592bd85365ff7187f585c
Kimyerim <yeye092302@gmail.com>
2024-11-03 04:31:57 +0900

    feat(App): 중복 없는 로또 번호 생성을 위한 generateLottoNumbers 메서드 추가
    - Random.pickUniqueNumbersInRange를 사용하여 1~45 범위 내 중복 없는 6개 숫자 생성
    - 생성된 번호는 오름차순으로 정렬하여 반환

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import  Input  from './Input.js';
+import Lotto from './Lotto.js';
 import { validateBonusNum, validatePurchaseAmount, validateWinnigNum } from './validate.js';
 import { COMMON_MESSAGE } from './message.js';
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
     const inputPurchaseAmount = new Input(validatePurchaseAmount.validation);
     await inputPurchaseAmount.inputValue(COMMON_MESSAGE.INPUT_PURCHASE);
-    inputPurchaseAmount.changeNum();
-    const purchaseAmount = inputPurchaseAmount.getValue();
+    const lottoCount = inputPurchaseAmount.getLottoCount();
+
+    Console.print(`${lottoCount}개를 구매했습니다.`);
+
+    const lottos = [];
+    for(let i = 0; i < lottoCount; i++){
+      const numbers = this.generateLottoNumbers();
+    }
 
     const inputWinningNum = new Input(validateWinnigNum.validation);
     await inputWinningNum.inputValue(COMMON_MESSAGE.INPUT_WINNING_NUM);
@@ -18,6 +26,10 @@ class App {
     await inputBonusNum.inputValue(COMMON_MESSAGE.INPUT_BONUS_NUM);
     inputBonusNum.changeNum();
     const bonusNum = inputBonusNum.getValue();
+  }
+
+  generateLottoNumbers(){
+    return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a- b);
   }
 }
 
