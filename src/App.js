8a6e4559b7546fdf35737c6b351838b342b20710
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-11-02 19:08:17 +0900

    feat: 구입한 로또 번호 출력

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
-import Lotto from "./Lotto.js"
+import Lotto from "./Lotto.js";
 
 class App {
   async run() {
     const purchaseMoney = await Console.readLineAsync(
       "구입금액을 입력해 주세요."
     );
-    const tickets = Array.from({ length: purchaseMoney / 1000 }, () =>
+    let lottos = Array.from({ length: purchaseMoney / 1000 }, () =>
       MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
     );
-    const lottos = tickets.map((ticket) => new Lotto(ticket));
 
-    Console.print(`${tickets.length}개를 구매했습니다.`);
-    
+    Console.print(`${lottos.length}개를 구매했습니다.`);
+    lottos = lottos.map((lotto) => {
+      Console.print(lotto);
+      return new Lotto(lotto);
+    });
+
     // const winningNumber = await Console.readLineAsync(
     //   "당첨 번호를 입력해 주세요."
     // );
