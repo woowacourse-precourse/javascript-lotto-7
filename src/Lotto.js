62189dad9adbee802485112efb29500940f5d44a
kimyou1102 <yoosk5485@naver.com>
2024-11-02 19:55:56 +0900

    refactor: Lotto클래스 예외추가 및 getNumbers 메서드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,20 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    const uniqueValues = new Set(numbers);
+    const uniqueNumbers = [...uniqueValues];
+    if (numbers.length !== uniqueNumbers.length) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자 존재할수없음');
     }
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
