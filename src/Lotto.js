984a5672334246e533ff4653825cdb8b1fbb3be2
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-11-02 16:16:10 +0900

    debug(Lotto): 중복검사 메서드에서 숫자가 1~6 까지의 숫자만 검증하는 문제 수정
    가능한 숫자의 범위는 1~45 까지지만, 1~6 까지의 숫자만 검사하는 문제 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,8 +2,8 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#checkDuplicate(numbers);
+    Lotto.#validate(numbers);
+    Lotto.#checkDuplicate(numbers);
     this.#numbers = numbers;
   }
 
@@ -11,16 +11,16 @@ class Lotto {
     return this.#numbers;
   }
 
-  #validate(numbers) {
+  static #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  #checkDuplicate(numbers) {
-    const LOTTO_LENGTH = 6;
+  static #checkDuplicate(numbers) {
+    const LOTTO_LENGTH = 45;
     const checked = Array(LOTTO_LENGTH + 1).fill(false);
-    for (let i = 0; i < LOTTO_LENGTH; i++) {
+    for (let i = 0; i < LOTTO_LENGTH; i += 1) {
       if (checked[numbers[i]]) {
         throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
       }
