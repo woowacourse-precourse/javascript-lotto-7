8b3059fdbbb2192086d78142f5a98d5321fab700
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-02 23:06:49 +0900

    test: 입력값이 없는 경우 예외가 발생하는 기능 테스트 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -3,8 +3,15 @@ import Validation from '../src/Contoller/Validation.js';
 describe('Validation 테스트', () => {
   test('구매 금액이 1000원 단위가 아니면 예외가 발생한다', () => {
     expect(() => Validation.validateThousandUnit(1500)).toThrow(
-      '[ERROR] 구매 금액은 1000원 단위 여야 합니다.'
+      '[ERROR] 구매 금액은 1000원 단위여야 합니다.'
     );
     expect(() => Validation.validateThousandUnit(1000)).not.toThrow();
+  });
+
+  test('입력값을 입력하지 않은 경우 예외가 발생한다.', () => {
+    expect(() => Validation.validateNotEmpty('')).toThrow(
+      '[ERROR] 입력값이 없습니다.'
+    );
+    expect(() => Validation.validateNotEmpty(1000)).not.toThrow();
   });
 });
