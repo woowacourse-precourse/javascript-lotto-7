c76731c88ac0cd777463c386520b84bed888032b
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-11-02 13:54:24 +0900

    feat: 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = Console.readLineAsync("구입금액을 입력해 주세요");
+  }
 }
 
 export default App;
