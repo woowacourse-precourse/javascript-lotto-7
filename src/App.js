4e27a136450def4962cc7f573a34fce46c22ec7c
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-11-02 13:59:34 +0900

    feat: 구입 금앨으로 0을 입력하는 경우 예외처리

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const purchaseAmount = Console.readLineAsync("구입금액을 입력해 주세요");
+    const purchaseAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요"
+    );
+    if (purchaseAmount === 0) {
+      throw new Error("[ERROR]: 구입 금액은 0을 입력할 수 없습니다.");
+    }
   }
 }
 
