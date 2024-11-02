c2378382f4174dcdae42ac233dd52abdb56584a8
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 23:01:17 +0900

    refactor(LottoOutputWriter): 클래스 캡슐화

--- src/classes/LottoOutputWriter.js
+++ src/classes/LottoOutputWriter.js
@@ -2,48 +2,43 @@ import { Console } from '@woowacourse/mission-utils';
 import LottoChecker from './LottoChecker.js';
 
 class LottoOutputWriter {
-  printLottos(lottos) {
+  static printLottos(lottos) {
     this.#printLottoCount(lottos.length);
     this.#printLottoNumbers(lottos);
   }
 
-  printWinningResult(winningResult) {
+  static printWinningResults(winningResults) {
     Console.print('당첨 통계\n---\n');
 
-    const output = [];
-    for (let i = winningResult.length - 1; i >= 0; i--) {
+    for (let i = winningResults.length - 1; i >= 0; i--) {
       const { matchCount, prize, bonus } = LottoChecker.PRIZE_TIERS[i];
-      output.push(
+      Console.print(
         this.#generateWinningResultMessage(
           matchCount,
           prize,
           bonus,
-          winningResult[i]
+          winningResults[i]
         )
       );
     }
-
-    Console.print(output.join('\n'));
   }
 
-  printYield(totalYield) {
-    Console.print(`총 수익률은 ${totalYield}%입니다.`);
+  static printYield(yieldRate) {
+    Console.print(`총 수익률은 ${yieldRate}%입니다.`);
   }
 
-  #printLottoCount(lottoCount) {
+  static #printLottoCount(lottoCount) {
     Console.print(`${lottoCount}개를 구매했습니다.\n`);
   }
 
-  #printLottoNumbers(lottos) {
+  static #printLottoNumbers(lottos) {
     const output = lottos.map((lotto) => `[${lotto.getNumbers().join(', ')}]`);
     Console.print(`${output.join('\n')}\n\n`);
   }
 
-  #generateWinningResultMessage(matchCount, prize, bonus, count) {
-    if (matchCount === 5 && bonus) {
-      return `${matchCount}개 일치, 보너스 볼 일치 (${prize}원) - ${count}개`;
-    }
-    return `${matchCount}개 일치 (${prize}원) - ${count}개`;
+  static #generateWinningResultMessage(matchCount, prize, bonus, count) {
+    const bonusMessage = bonus && matchCount === 5 ? ', 보너스 볼 일치' : '';
+    return `${matchCount}개 일치${bonusMessage} (${prize}원) - ${count}개`;
   }
 }
 
