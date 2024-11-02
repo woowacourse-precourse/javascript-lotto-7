965eb65958e91cfbd9ef00b63ff31e9c58fcc05e
BoRamSon <onbrson1@gmail.com>
2024-11-03 03:18:12 +0900

    Fix(LottoController): 이전 구매금액을 입력받고, 구매금액에 따라 로또번호를 생성하고 출력하도록 연동 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,13 +1,22 @@
 import { InputPriceModel } from "../model/inputPriceModel.js";
+import { OutputLottoNumbers } from "../model/OutputLottoModel.js";
 
 export class LottoController {
-  constructor () {
-    this.inputPriceModel = new InputPriceModel()
+  constructor() {
+    this.inputPriceModel = new InputPriceModel();
+    this.outputLottoNumbers = new OutputLottoNumbers();
   }
 
-  async startLottoGame () {
-    await this.inputPriceModel.getPrice();
-
+  async startLottoGame() {
+    const price = await this.getPriceResult();
+    this.getLottoNumberResult(price);
   }
 
+  async getPriceResult() {
+    return await this.inputPriceModel.getPrice();
+  }
+
+  getLottoNumberResult(price) {
+    return this.outputLottoNumbers.getRandomNumbers(price);
+  }
 }
