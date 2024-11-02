7630e8d1e1f728ec2fbe35e22ac4b9340c85c66d
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 23:11:08 +0900

    refactor(RevenueCalculator): 클래스 캡슐화

--- src/classes/RevenueCalculator.js
+++ src/classes/RevenueCalculator.js
@@ -1,14 +1,14 @@
 import LottoChecker from './LottoChecker.js';
 
 class RevenueCalculator {
-  calculateYield(totalSpent, winningResults) {
+  static calculateYield(totalSpent, winningResults) {
     const totalPrize = this.#calculateTotalPrize(winningResults);
 
     const totalYield = (totalPrize / totalSpent) * 100;
     return parseFloat(totalYield.toFixed(2));
   }
 
-  #calculateTotalPrize(winningResults) {
+  static #calculateTotalPrize(winningResults) {
     const totalPrize = winningResults.reduce((total, count, index) => {
       const prize = LottoChecker.PRIZE_TIERS[index].prize;
 
