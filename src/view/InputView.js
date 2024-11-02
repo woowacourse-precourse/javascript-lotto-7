c4182d6731dc773090264894010a69f5cff3bae5
BoRamSon <onbrson1@gmail.com>
2024-11-02 21:27:56 +0900

    feat(InputView): 최초 로또 구매금액을 입력할 수 있도록 console에 띄우기

---
+++ src/view/InputView.js
@@ -0,0 +1,28 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGES } from "../constants/inputMessages.js";
+
+export class InputView {
+  async inputBoughtPrice() {
+    const inputPriceString = await MissionUtils.Console.readLineAsync(
+      INPUT_MESSAGES.BUY_PRICE
+    );
+    const inputPriceNumber = Number(inputPriceString.trim());
+    return inputPriceNumber;
+  }
+
+  async inputPrizeNumbers() {
+    const inputPrizeString = await MissionUtils.Console.readLineAsync(
+      INPUT_MESSAGES.WINNING_NUMBER
+    );
+    const inputPrizeNumber = Number(inputPrizeString.trim());
+    return inputPrizeNumber;
+  }
+
+  async inputBonusNumber() {
+    const inputBonusString = await MissionUtils.Console.readLineAsync(
+      INPUT_MESSAGES.BONUS_NUMBER
+    );
+    const inputBonusNumber = Number(inputBonusString.trim());
+    return inputBonusNumber;
+  }
+}
