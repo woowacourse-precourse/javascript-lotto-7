6e81d4e03667219cfc7ce3cc27e40b4774a15d0a
youdame <112458620+youdame@users.noreply.github.com>
2024-11-02 21:49:53 +0900

    feat : 로또 번호 생성 후 오름차순으로 정렬하여 출력

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 class App {
   async run() {
-    /* TODO: 
-    1) 1000원으로 안 떨어지는 경우 예외처리,
-    2) 숫자가 아닌 금액을 입력했을 경우 예외처리 
-    3) 1000원보다 적은 금액을 입력했을 경우 예외처리 
-    4) 아무 것도 입력하지 않은 경우 예외 처리 
-    */
     const money = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
 
     const count = money / 1000;
     Console.print(`${count}개를 구매했습니다.`);
+
+    for (let i = 1; i <= count; i++) {
+      const lottoNumber = await Random.pickUniqueNumbersInRange(1, 45, 6);
+      const sortedNumberList = lottoNumber.sort((a, b) => a - b);
+      Console.print(sortedNumberList);
+    }
   }
 }
 export default App;
