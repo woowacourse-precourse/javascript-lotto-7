fd838ed4bd0065ac5f17af0c73756704b194076b
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-03 03:56:54 +0900

    feat : 당첨번호 입력 기능 구현

--- src/controllers/lottoController.js
+++ src/controllers/lottoController.js
@@ -1,8 +1,34 @@
+import Lotto from '../model/Lotto.js';
+import LottoStore from '../model/LottoStore.js';
 import LottoView from '../views/LottoView.js';
 
 class LottoController {
   static async start() {
-    const purchageAmonut = await LottoView.InputPurchaseAmount();
+    const amount = await this.convertPurchaseAmount();
+    const lottoStore = new LottoStore(amount);
+    LottoView.PrintLottos(lottoStore.getCount(), lottoStore.getLottos());
+    const winningNumbers = await this.convertWinningNumber();
+    const lottot = new Lotto(winningNumbers);
+    console.log(lottot);
+  }
+
+  static async convertPurchaseAmount() {
+    const purchaseAmount = await LottoView.InputPurchaseAmount();
+    return Number(purchaseAmount);
+  }
+
+  static async convertWinningNumber() {
+    const winningNumber = await LottoView.InputwinningNumbers();
+    return winningNumber
+      .trim()
+      .split(',')
+      .map((num) => Number(num.trim()));
+  }
+
+  static async convertBounusNumber() {
+    const bounusNumber = await LottoView.InputbounusNumber();
+    return Number(bounusNumber);
   }
 }
+
 export default LottoController;
