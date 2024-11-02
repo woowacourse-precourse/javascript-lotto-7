b8828ad37df03c0d25440d54f6b04292aeb554be
jongbin26 <010jjj@naver.com>
2024-11-02 17:46:15 +0900

    feat(App): 구현된 기능 App.js에 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Simulator } from './Simulator.js';
+import {Validator} from './Validator.js';
+import {Publisher} from './Publisher.js';
+
 class App {
-  async run() {}
+  async run() {
+    const validator = new Validator();
+    const publisher = new Publisher();
+    const simulator = new Simulator(validator, publisher);
+    await simulator.simulate();
+  }
 }
 
 export default App;
