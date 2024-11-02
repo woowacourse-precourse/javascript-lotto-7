21a3031470e69d7f99a78b81ebc42fd5b914174f
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-03 04:28:54 +0900

    test: ProfitCalculator 클래스 단위 테스트 추가
    - calculateProfitRate 메서드의 로직을 검증하는 단위 테스트 구현
    - 수익률 계산이 근사치로 일치하는지 확인하기 위해 toBeCloseTo 매처 사용

---
+++ __tests__/controllers/ProfitCalculator.test.js
@@ -0,0 +1,47 @@
+import ProfitCalculator from '../../src/controllers/ProfitCalculator.js';
+import { GAME_SETTINGS, LOTTO_REWARD } from '../../src/utils/constants.js';
+
+describe('ProfitCalculator 클래스 테스트', () => {
+  test('수익률이 올바르게 계산된다.', () => {
+    const rankCounts = {
+      threeMatch: 3,
+      fourMatch: 1,
+      fiveMatch: 0,
+      fiveWithBonusMatch: 1,
+      sixMatch: 0,
+    };
+    const purchaseAmount = 100000;
+
+    const expectedProfit =
+      ((LOTTO_REWARD.threeMatch.prize * 3 +
+        LOTTO_REWARD.fourMatch.prize * 1 +
+        LOTTO_REWARD.fiveWithBonusMatch.prize * 1) /
+        purchaseAmount) *
+      GAME_SETTINGS.PERCENTAGE_MULTIPLIER;
+
+    const profitRate = ProfitCalculator.calculateProfitRate(
+      rankCounts,
+      purchaseAmount
+    );
+
+    expect(Number(profitRate)).toBeCloseTo(expectedProfit, 1);
+  });
+
+  test('당첨금이 없을 때 수익률은 0%로 계산된다.', () => {
+    const rankCounts = {
+      threeMatch: 0,
+      fourMatch: 0,
+      fiveMatch: 0,
+      fiveWithBonusMatch: 0,
+      sixMatch: 0,
+    };
+    const purchaseAmount = 50000;
+
+    const profitRate = ProfitCalculator.calculateProfitRate(
+      rankCounts,
+      purchaseAmount
+    );
+
+    expect(profitRate).toBe('0.0');
+  });
+});
