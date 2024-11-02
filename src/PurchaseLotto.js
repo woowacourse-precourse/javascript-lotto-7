2a80b9a8fdd2917aaf85c7814dae20d3cd7e6719
yeabbbbb <b08009@naver.com>
2024-11-02 21:33:02 +0900

    refactor: store generated numbers in a seperate variable
    - Updated the Lotto instantiation to store the generated random numbers
    in a seperate variable for improved readability and maintainability.

--- src/PurchaseLotto.js
+++ src/PurchaseLotto.js
@@ -1,10 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Lotto from './Lotto.js'
 
-export function generateLotto(purchaseCount) {
+export function purchaseLotto(purchaseCount) {
   const lottoList = []
   for (let i = 0; i < purchaseCount; i++) {
-    const lotto = new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    const lotto = new Lotto(numbers);
     lottoList.push(lotto);
   }
   return lottoList;
