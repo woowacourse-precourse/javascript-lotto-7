5596a3c1b0cefc39b89e9b817e395c1ad66d4b5b
yeabbbbb <b08009@naver.com>
2024-11-02 20:21:23 +0900

    feat: add Lotto class with number validation and print formatting
    - Implement Lotto class to manage lottery numbers with validation
    - Validate Lotto numbers for 6 numbers, numerical format, range 1~45, and uniqueness
    - Add Method to print numbers in formatted way

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,40 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if(!numbers) {
+      throw new Error("[ERROR] 입력이 없습니다.");
+    }
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+
+    const regex = /^[0-9]*$/;
+    numbers.forEach((number) => {
+      if (!regex.test(number)) {
+        throw new Error('[ERROR] 숫자 이외의 문자가 입력되었습니다.');
+      }
+
+      if (Number(number) < 1 || Number(number) > 45) {
+        throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+      }
+    });
+
+    const numbersSet = new Set(numbers);
+    if (numbers.length != numbersSet.size) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  get getNumbers() {
+    return this.#numbers;
+  }
+
+  printNumbers() {
+    const formattedNumbers = JSON.stringify(this.#numbers).replace(/,/g, ', ');
+    Console.print(formattedNumbers);
+  }
 }
 
 export default Lotto;
