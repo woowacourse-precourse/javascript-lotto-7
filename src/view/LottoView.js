56fb06896a4b2f1bc32ccbfd90b1208df29e0c4f
taeyomi <terry11ty@naver.com>
2024-11-03 02:52:54 +0900

    feat: 로또 구입 금액 입력받는 메서드 구현

--- src/view/LottoView.js
+++ src/view/LottoView.js
@@ -1,6 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 import { INPUT_PURCHASE_AMOUNT } from "../constants/input.js";
 
-class LottoView {}
+class LottoView {
+  async inputLottoAmount() {
+    const userInputAmounts = await Console.readLineAsync(INPUT_PURCHASE_AMOUNT);
+    return userInputAmounts;
+  }
+}
 
 export default LottoView;
