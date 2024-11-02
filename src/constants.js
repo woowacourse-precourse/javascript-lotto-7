0a5ecd138c51c0f29f58076a53c7cb7f6370d11e
Murphybread <nargene@gmail.com>
2024-11-03 02:57:15 +0900

    refactor(constants): change name from LOTTO to LOTTO_INFO

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,14 @@
+export const LOTTO_INFO = {
+    LOTTO_TICKET_PRICE : 1000,
+    PRIZE : {
+        PRIZE_ONE_NUMBER_MATCHES : 0,
+        PRIZE_TWO_NUMBER_MATCHES : 0,
+        PRIZE_THREE_NUMBER_MATCHES : 5_000,
+        PRIZE_FOUR_NUMBER_MATCHES : 50_000,
+        PRIZE_FIVE_NUMBER_MATCHES : 1_500_000,
+        PRIZE_FIVE_NUMBER_BONUS_MATCHES : 30_000_000,
+        PRIZE_SIX_NUMBER_MATCHES : 2_000_000_000,
+    }
 
 
 }
@@ -7,7 +18,4 @@ export const DISPLAY_MESSAGE = {
     ERROR_PAID_MONEY_MESSAGE : `[ERROR] 로또 1장당 금액은 ${LOTTO_INFO.LOTTO_TICKET_PRICE}원입니다. 거스름돈 없이 해주세요`,
     REQUST_WINNING_NUMBER_MESSAGE : "지난 주 당첨 번호를 입력해 주세요.\n",
     REQEUST_BUONUS_NUMBER_MESSAGE : "\n보너스 번호를 입력해 주세요.\n"
-
-
-
 }
