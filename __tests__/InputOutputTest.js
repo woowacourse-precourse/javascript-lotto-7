2b4ce4df8ded94d9a78783d27c716481e7fe7bb0
jiyun1006 <jiyun4251@gmail.com>
2024-11-02 16:06:01 +0900

    ✅ test : 구매금액에 대한 테스트 3
    구매금액이 1000으로 나누어떨어지는지에 대한 오류검증의 테스트
    - isNotGoodNumber에 대한 테스트 작성

--- __tests__/InputOutputTest.js
+++ __tests__/InputOutputTest.js
@@ -3,13 +3,20 @@ import validation from '../src/validation.js';
 
 describe('입력문에 대한 테스트', () => {
   test.each(['100+100', 's20f', '@GJe22'])(
-    '숫자만 입력되어야 한다.',
+    '구매금액은 숫자만 입력되어야 한다.',
     (numberStr) => {
       expect(() => validation.isNotNumber(numberStr)).toThrow();
     },
   );
 
-  test.each(['-100', '0'])('양수만 입력해야 한다.', (numberStr) => {
+  test.each(['-100', '0'])('구매금액은 양수만 입력해야 한다.', (numberStr) => {
     expect(() => validation.isNotPositiveNumber(numberStr)).toThrow();
   });
+
+  test.each(['1001', '100.5', '1234.543'])(
+    '구매금액은 1000으로 나누어 떨어져야 한다.',
+    (numberStr) => {
+      expect(() => validation.isNotGoodNumber(numberStr)).toThrow();
+    },
+  );
 });
