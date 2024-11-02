e49e5815ee3f861978d9e8890c67a979f6042de3
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-03 01:10:52 +0900

    feat: 입력 및 출력 메시지 상수화

---
+++ src/constants/InputMessage.js
@@ -0,0 +1,6 @@
+const INPUT_MESSAGES = Object.freeze({
+  PURCHASE_MONEY: "구입금액을 입력해 주세요.",
+  WINNING_NUMBERS: "당첨 번호를 입력해 주세요.",
+  BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+});
+export { INPUT_MESSAGES };
