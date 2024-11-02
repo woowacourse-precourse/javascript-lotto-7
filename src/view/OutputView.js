5a74419f197ac57aaacf123cb85bd66bff0ed8a3
yena <rladpsk1117@gmail.com>
2024-11-02 20:34:41 +0900

    feat(OutputView): 로또 구입 수량 출력

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,9 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const OutputView = {};
+const OutputView = {
+  printLottoTickets(tickets) {
+    Console.print(`${tickets}개를 구매했습니다.`);
+  },
+};
 
 export default OutputView;
