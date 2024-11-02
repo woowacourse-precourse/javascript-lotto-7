687ade28dbe3b98c7292e479fe665aa8e8e32cfa
rwaeng <rwaeng0421@gmail.com>
2024-11-02 15:13:34 +0900

    feat(App): 로또 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async getPayment() {
+    const payment = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    this.validatePayment(payment);
+  }
+
+  validatePayment(payment) {
+    if (isNaN(payment)) throw new Error("[ERROR] 숫자를 입력해야 합니다.");
+    if (payment <= 0)
+      throw new Error("[ERROR] 0이상의 금액을 입력해야 합니다.");
+    if (payment % 1000 !== 0)
+      throw new Error("[ERROR] 1000원 단위로 금액을 입력해야 합니다.");
+  }
+
+  async run() {
+    await this.getPayment();
+  }
 }
 
 export default App;
