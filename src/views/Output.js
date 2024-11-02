932fa2b3c03a32445d1a1708a4b34e531c1b9af4
최하은 <haeun4345@gmail.com>
2024-11-02 13:06:15 +0900

    style: 로또 발행 출력 양식 수정

--- src/views/Output.js
+++ src/views/Output.js
@@ -2,13 +2,16 @@ import { Console } from '@woowacourse/mission-utils';
 
 class Output {
   printLottoCount(count) {
-    Console.print(`${count}개를 구매했습니다.`);
+    Console.print(`\n${count}개를 구매했습니다.`);
   }
 
   printLottos(lottos) {
-    lottos.forEach((lotto) => {
-      Console.print(lotto.getNumbers());
-    });
+    lottos.forEach((lotto) => this.#printLotto(lotto));
+    Console.print('');
+  }
+
+  #printLotto(lotto) {
+    Console.print('[' + lotto.getNumbers().join(', ') + ']');
   }
 }
 
