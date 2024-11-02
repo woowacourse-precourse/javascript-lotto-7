14ec59eb6572b0be47f9526b748d66a541483a9e
mlnwns <mlnwns@naver.com>
2024-11-03 04:39:17 +0900

    feat: 보너스 번호 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Lotto from "./Lotto.js";
 import LottoGenerator from "./utils/LottoGenerator.js";
 import Output from "./utils/Output.js";
 import Validator from "./utils/Validator.js";
+import { ERROR_MESSAGES } from "./constants/constants.js";
 
 class App {
   async run() {
@@ -15,6 +16,7 @@ class App {
 
     const winningNumbers = await this.getWinningNumbers();
     const winningLotto = new Lotto(winningNumbers);
+    const bonusNumber = await this.getBonusNumber(winningNumbers);
   }
 
   async getPurchaseAmount() {
@@ -46,6 +48,23 @@ class App {
       }
     }
     return winningNumbers;
+  }
+
+  async getBonusNumber(winningNumbers) {
+    let bonusNumber;
+    while (true) {
+      try {
+        bonusNumber = await Input.bonusNumber();
+        Validator.isSingleNumber(bonusNumber);
+        Validator.isNumber(bonusNumber, ERROR_MESSAGES.BONUS_NUMBER_NAN);
+        Validator.isWithinRange(bonusNumber, 1, 45);
+        Validator.isUniqueBonusNumber(bonusNumber, winningNumbers);
+        break;
+      } catch (error) {
+        Output.printError(error.message);
+      }
+    }
+    return Number(bonusNumber);
   }
 }
 
--- src/utils/Input.js
+++ src/utils/Input.js
@@ -11,6 +11,11 @@ const Input = {
     const input = await Console.readLineAsync(PROMPTS.WINNING_NUMBERS);
     return input.trim();
   },
+
+  async bonusNumber() {
+    const input = await Console.readLineAsync(PROMPTS.BONUS_NUMBER);
+    return input.trim();
+  },
 };
 
 export default Input;
