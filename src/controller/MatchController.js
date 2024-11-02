d4e454b8bccfee00325aefb19c3df2f82606eb20
seonae-j <wkdtjsdo135@naver.com>
2024-11-02 14:20:45 +0900

    refactor(MatchController): 사용자에게 입력받는 메서드 위치 변경
    - Lotto.js : 로또 당첨 번호를 입력받고 예외처리 담당

--- src/controller/MatchController.js
+++ src/controller/MatchController.js
@@ -3,12 +3,6 @@ import { INPUT } from '../constants/Constants.js';
 class MatchController {
   constructor() {}
 
-  async setJackpot() {
-    const jackpotNumber = await Console.readLineAsync(INPUT.JACKPOT);
-    const jackpotArray = jackpotNumber.split(',');
-    return jackpotArray;
-  }
-
   async setBonusJackpot() {
     const bonusNumber = await Console.readLineAsync(INPUT.BONUS);
     return bonusNumber;
