d67f0c95d5cb5ff49c30d63b0af5e35d2d02331e
silver0108 <kimsoeun0108@gmail.com>
2024-11-03 01:12:29 +0900

    refactor: 에러 처리 함수 분리

--- src/validator/AmountValidator.js
+++ src/validator/AmountValidator.js
@@ -1,9 +1,10 @@
 import Validator from "./Validator.js";
 import { _pipe } from "../utils/util.js";
+import throwError from "../utils/throwError.js";
 
 class AmountValidator extends Validator {
   static isMultipleOfThousand(input) {
-    if (input % 1000 !== 0) throw new Error("1,000원 단위 아님");
+    if (input % 1000 !== 0) throwError("1,000원 단위 아님");
     return input;
   }
 
--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,29 +1,30 @@
+import throwError from "../utils/throwError";
+
 class Validator {
   static isEmpty(input) {
-    if (!input) throw new Error("빈 값");
+    if (!input) throwError("빈 값");
     return input;
   }
 
   static isSeparatedFormat(input, separator = ",") {
     const values = input.split(separator).map((value) => value.trim());
-    if (values.length < 1) throw new Error("입력 형식이 잘못됐습니다.");
+    if (values.length < 1) throwError("입력 형식이 잘못됐습니다.");
     return values;
   }
 
   static isNumber(input) {
-    if (isNaN(input)) throw new Error("숫자 타입 아님");
+    if (isNaN(input)) throwError("숫자 타입 아님");
     return Number(input);
   }
 
   static isNumberInRange(input, min = 1, max = 45) {
     if (input < min || input > max)
-      throw new Error(`값은 ${min}부터 ${max} 사이여야 합니다.`);
+      throwError(`값은 ${min}부터 ${max} 사이여야 합니다.`);
     return input;
   }
 
   static isLengthSix(input) {
-    if (input.length !== 6)
-      throw new Error("로또 번호는 6개의 숫자여야 합니다.");
+    if (input.length !== 6) throwError("로또 번호는 6개의 숫자여야 합니다.");
     return input;
   }
 
