df9ab4c51fef79b989ff61ec88384f1c038dd720
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-02 16:08:46 +0900

    feat: 로또 번호는 1이상 45이하 숫자여야 한다

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,6 +15,12 @@ class Lotto {
     if (setNumbers.size !== 6) {
       throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다');
     }
+
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error('[ERROR] 로또 번호는 1이상 45이하 숫자여야 합니다.');
+      }
+    });
   }
 
   getFormattedNumbers() {
