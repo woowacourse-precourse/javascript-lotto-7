5cba898a30bbe2d19c4bfdac67209517d19aba25
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-02 23:07:44 +0900

    feat: 입력값이 없는 경우 예외를 발생시키는 기능 추가

--- src/Contoller/Validation.js
+++ src/Contoller/Validation.js
@@ -1,7 +1,13 @@
 class Validation {
   static validateThousandUnit(amount) {
     if (amount % 1000 !== 0) {
-      throw new Error('[ERROR] 구매 금액은 1000원 단위 여야 합니다.');
+      throw new Error('[ERROR] 구매 금액은 1000원 단위여야 합니다.');
+    }
+  }
+
+  static validateNotEmpty(input) {
+    if (input === '') {
+      throw new Error('[ERROR] 입력값이 없습니다.');
     }
   }
 }
