4b56eb1fa0bb51eb821ad79a76d8335a18a33316
yena <rladpsk1117@gmail.com>
2024-11-02 20:35:13 +0900

    feat(OutputView): 발행 로또 번호 출력

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,6 +4,10 @@ const OutputView = {
   printLottoTickets(tickets) {
     Console.print(`${tickets}개를 구매했습니다.`);
   },
+
+  printLottoNumbers(numbers) {
+    Console.print(`[${numbers}]\n`);
+  },
 };
 
 export default OutputView;
