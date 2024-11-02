3dc76a66480046ed454d9699320ec1324e820d08
DodoJBNU <dh723@jbnu.ac.kr>
2024-11-02 22:57:15 +0900

    feat: add validation for purchase amount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import LottoController from './components/LottoController.js';
+import purchaseAmountValidator from './validation/purchaseAmountValidator.js';
 
 class App {
   async run() {
     const lottoController = new LottoController();
 
     await lottoController.promptPurchaseAmount();
+    purchaseAmountValidator(lottoController.getPurchaseAmount());
   }
 }
 
--- src/resources/Constants.js
+++ src/resources/Constants.js
@@ -1,3 +1,12 @@
+const ERROR_PREFIX = '[ERROR]';
+
+export const PURCHASE_UNIT = 1000;
+
 export const InputPrompts = {
   purchaseAmount: '구입 금액을 입력해주세요.\n',
+};
+
+export const ErrorMessage = {
+  PRICE_AMOUNT_IS_NEGATIVE: `${ERROR_PREFIX} 구입 금액은 양수여야 합니다.`,
+  INVALID_PURCHASE_UNIT: `${ERROR_PREFIX} 구입 금액 단위는 ${PURCHASE_UNIT}원 이어야 합니다.`,
 };
