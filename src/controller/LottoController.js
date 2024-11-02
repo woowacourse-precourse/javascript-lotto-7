0ec12d458d8d4bff53a974e669f1bbc2a494946b
yena <rladpsk1117@gmail.com>
2024-11-03 03:32:54 +0900

    fix(LottoController): 필드에 값이 저장되도록 수정
    winnigLottoNumbers -> winningLottoNumbers 오타 수정
    숫자로 변환된 입력 값이 필드에 저장되도록 수정

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -6,20 +6,20 @@ import BonusNumber from '../model/BonusNumber.js';
 class LottoController {
   #purchaseAmount; // 구입금액
   #lotteryNumbers; // 구입한 로또 번호
-  #winnigLottoNumbers; // 당첨 번호
+  #winningLottoNumbers; // 당첨 번호
   #bonusNumber; // 보너스 번호
 
   async start() {
-    this.#winnigLottoNumbers = await this.#getWinningLottoNumbers();
-    this.#bonusNumber = await this.#getBonusNumber();
+    await this.#getWinningLottoNumbers();
+    await this.#getBonusNumber();
   }
 
   async #getWinningLottoNumbers() {
     try {
       const input = await InputView.inputWinningNumbers();
       const splittedInput = input.split(',');
-      const convertInput = splittedInput.map((num) => Number(num));
-      this.#winnigLottoNumbers = new Lotto(convertInput);
+      this.#winningLottoNumbers = splittedInput.map((num) => Number(num));
+      return new Lotto(this.#winningLottoNumbers);
     } catch (error) {
       Console.print(error);
       await this.#getWinningLottoNumbers();
@@ -29,12 +29,13 @@ class LottoController {
   async #getBonusNumber() {
     try {
       const input = await InputView.inputBonusNumber();
-      const convertInput = Number(input);
-      this.#bonusNumber = new BonusNumber(convertInput);
+      this.#bonusNumber = Number(input);
+      return new BonusNumber(this.#bonusNumber);
     } catch (error) {
       Console.print(error);
       await this.#getBonusNumber();
     }
   }
 }
+
 export default LottoController;
