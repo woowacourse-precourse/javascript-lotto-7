5fc06069c37250ad2ee0fe8193fd6b7696734ec2
hyunyeee <5004ohy@naver.com>
2024-11-02 23:57:51 +0900

    feat(lotto): 구입 금액에 따라 Lotto 를 발행하는 클래스 구현

---
+++ src/LottoMaker.js
@@ -0,0 +1,24 @@
+import { LOTTO_PRICE } from "./constant/constants.js";
+import { inValidMessages } from "./constant/message.js";
+import { RandomNumberMaker } from "./RandomNumberMaker.js";
+import Lotto from "./Lotto.js";
+
+export class LottoMaker {
+  makeLotto(price) {
+    this.#validateDivisible(price);
+    return this.#createLotto(price);
+  }
+
+  #validateDivisible(price) {
+    if (price % LOTTO_PRICE !== 0) throw new Error(inValidMessages.priceUnit);
+  }
+
+  #createLotto(price) {
+    let myLottoArr = [];
+    for (let i = 0; i < (price / LOTTO_PRICE); i++) {
+      const randomNumbers = new RandomNumberMaker().makeRandomNumbers();
+      myLottoArr.push(new Lotto(randomNumbers));
+    }
+    return myLottoArr;
+  }
+}
