8800cc231b8438e1b784d523504a7ed730d88b41
Daeuning <happyjde3@gmail.com>
2024-11-02 21:53:49 +0900

    feat(PurchaseAmountValidator): 입력된 금액의 타입을 검사하는 유효성 검사 기능 구현

--- src/constants/message.js
+++ src/constants/message.js
@@ -4,6 +4,9 @@ const LOG_MESSAGE = Object.freeze({
 
 const ERROR_MESSAGE = Object.freeze({
   EMPTY_INPUT: "입력값이 비어 있습니다. 값을 입력해 주세요!",
+  INVALID_AMOUNT_FORMAT: "올바른 금액을 입력해 주세요. 숫자만 입력해야 하며, 1000원 단위로 작성해 주세요.",
+  AMOUNT_CONTAINS_NON_NUMERIC: "금액에는 숫자만 입력해 주세요. 1000원 단위로 입력해야 합니다.",
+  INVALID_AMOUNT_UNIT: "이 게임에서는 1000원 단위로만 금액을 입력할 수 있습니다. 다시 입력해 주세요.",
 });
 
 const TAGS = Object.freeze({
--- src/validators/PurchaseAmountValidator.js
+++ src/validators/PurchaseAmountValidator.js
@@ -1,4 +1,5 @@
 import { ERROR_MESSAGE } from '../constants/message.js';
+import { GAME_RULES } from '../constants/gameRule.js';
 import { throwError } from '../utils/console.js';
 
 const validate = {
@@ -7,10 +8,25 @@ const validate = {
       throwError(ERROR_MESSAGE.EMPTY_INPUT);
     }
   },
+
+  validateType(price) {
+    if (!/[0-9]/.test(price)) {
+      throwError(ERROR_MESSAGE.INVALID_AMOUNT_FORMAT); 
+    }
+
+    if (!/^\d+$/.test(price)) {
+      throwError(ERROR_MESSAGE.AMOUNT_CONTAINS_NON_NUMERIC);
+    }
+
+    if (price % GAME_RULES.CURRENCY_UNIT !== 0) {
+      throwError(ERROR_MESSAGE.INVALID_AMOUNT_UNIT);
+    }
+  },
 };
 
 function validatePurchaseAmount(price) {
   validate.validateEmpty(price);
+  validate.validateType(price);
 }
 
 export default validatePurchaseAmount;
