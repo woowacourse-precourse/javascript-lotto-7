9d354e2b66806b079de40537b862343791a8b17c
yena <rladpsk1117@gmail.com>
2024-11-02 20:08:43 +0900

    feat(InputView): 구입 금액 입력 받기

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const InputView = {};
+const InputView = {
+  async inputAmount() {
+    const purchaseAmount = await Console.readLineAsync(
+      `구입금액을 입력해 주세요.\n`
+    );
+    return purchaseAmount;
+  },
+
+  as,
+};
 
 export default InputView;
