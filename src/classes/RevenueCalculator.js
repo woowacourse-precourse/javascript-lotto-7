62a3214a9eb6e7abdfcfdca5dfc6c6cfb65a4a57
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 21:36:55 +0900

    fix: 수익률 계산에서 소수 둘째자리 반올림 설정

--- src/classes/RevenueCalculator.js
+++ src/classes/RevenueCalculator.js
@@ -1,16 +1,11 @@
 import LottoChecker from './LottoChecker.js';
 
 class RevenueCalculator {
-  constructor(totalSpent, winningResults) {
-    this.totalSpent = totalSpent;
-    this.totalPrize = 0;
-    this.winningResults = winningResults;
-  }
-
   calculateYield(totalSpent, winningResults) {
     const totalPrize = this.#calculateTotalPrize(winningResults);
 
-    return (totalPrize / totalSpent) * 100;
+    const yield = (totalPrize / totalSpent) * 100;
+    return parseFloat(yield.toFixed(2));
   }
 
   #calculateTotalPrize(winningResults) {
