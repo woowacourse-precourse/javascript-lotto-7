6fc6b38a745e831c85997f08b46aad1297bc8ee3
juhui88 <sand8594@naver.com>
2024-11-02 19:25:13 +0900

    feat: LottoServive 구현
    - 당첨 번호와 구매한 로또 번호 대조하여 당첨 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import PurchasedLotto from "./PurchasedLotto.js";
+import LottoService from "./LottoService.js";
 class App {
   async run() {
     try {
@@ -7,13 +8,29 @@ class App {
         "구입금액을 입력해 주세요.\n"
       );
       const lottoTickes = new PurchasedLotto(purchaseAmount);
-      console.log(lottoTickes.getTickets());
-      // const lottoNumbers = await MissionUtils.Console.readLineAsync(
-      //   "당첨 번호를 입력해 주세요.\n"
-      // );
-      // const lottoBonusNumber = await MissionUtils.Console.readLineAsync(
-      //   "보너스 번호를 입력해 주세요.\n"
-      // );
+
+      MissionUtils.Console.print(
+        `${lottoTickes.getNumberOfTickets()}개를 구입했습니다.`
+      );
+
+      lottoTickes
+        .getTickets()
+        .forEach((ticket) => MissionUtils.Console.print(ticket));
+
+      const lottoNumbers = await MissionUtils.Console.readLineAsync(
+        "당첨 번호를 입력해 주세요.\n"
+      );
+      const lottoBonusNumber = await MissionUtils.Console.readLineAsync(
+        "보너스 번호를 입력해 주세요.\n"
+      );
+
+      const lottoService = new LottoService(
+        lottoNumbers,
+        lottoBonusNumber,
+        lottoTickes
+      );
+
+      lottoService.start();
     } catch (error) {
       MissionUtils.Console.print(error.message);
       throw error;
---
+++ src/LottoService.js
@@ -0,0 +1,88 @@
+import errorMessages from "./constants/errorMessages.js";
+import Lotto from "./Lotto.js";
+import PurchasedLotto from "./PurchasedLotto.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class LottoService {
+  constructor(winningNumbers, bonusNum, purchasedLotto) {
+    const nums = this.#splitNumsByComma(winningNumbers);
+    this.winningNumbers = new Lotto(this.#convertStringToNumber(nums));
+    this.bonusNum = bonusNum;
+    this.purchasedLotto = purchasedLotto;
+  }
+
+  #splitNumsByComma(winningNumbers) {
+    return winningNumbers.split(",");
+  }
+
+  #convertStringToNumber(winningNumbers) {
+    return winningNumbers.map((num) => Number(num));
+  }
+
+  start() {
+    const purchasedLotto = this.purchasedLotto.getTickets();
+
+    const counts = [];
+    purchasedLotto.forEach((pLotto) => {
+      counts.push(this.#calculateMatchCount(pLotto));
+    });
+
+    const awards = counts.reduce((acc, value) => {
+      acc[value] = (acc[value] || 0) + 1;
+      return acc;
+    }, {});
+
+    this.printResult(awards);
+  }
+
+  #calculateMatchCount(numbers) {
+    let count = 0;
+    const winningNumbers = this.winningNumbers.getNumbers();
+    numbers.forEach((num) => {
+      if (num in winningNumbers) count += 1;
+    });
+    if (count === 5 && this.#checkBonusBallMatch(numbers)) count = "5+bonus";
+    return count;
+  }
+
+  #checkBonusBallMatch(numbers) {
+    if (this.bonusNum in numbers) return true;
+    return false;
+  }
+
+  printResult(counts) {
+    const winningResult = [
+      {
+        count: 3,
+        price: "5,000",
+      },
+      {
+        count: 4,
+        price: "50,000원",
+      },
+      {
+        count: 5,
+        price: "1,500,000",
+      },
+      {
+        count: 5,
+        bonus: true,
+        price: "30,000,000",
+      },
+      {
+        count: 6,
+        print: "2,000,000,000",
+      },
+    ];
+
+    winningResult.forEach((win) => {
+      MissionUtils.Console.print(
+        `${win.count}개 일치${win.bonus === true && ", 보너스 볼 일치"} (${
+          win.price
+        }원) - ${counts[String(win.count) || 0]}개`
+      );
+    });
+  }
+}
+
+export default LottoService;
