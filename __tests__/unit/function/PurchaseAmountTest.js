5ee31dff02093e9928ce6c4bfa121d9cc48e2dee
DodoJBNU <dh723@jbnu.ac.kr>
2024-11-02 21:18:23 +0900

    test: update PurchaseAmountTest

--- __tests__/unit/function/PurchaseAmountTest.js
+++ __tests__/unit/function/PurchaseAmountTest.js
@@ -1,22 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
-import test from 'node:test';
+import LottoController from '../../../src/components/LottoController.js';
 
 describe('구입 금액 입력 기능 테스트', () => {
-  let lottoController;
-
   beforeEach(() => {
-    lottoController = new LottoController();
     jest.restoreAllMocks();
   });
 
   test('구입 금액 5000원을 입력받아 저장한다.', async () => {
-    const mockPurchaseAmount = '5000';
+    const mockPurchaseAmount = 5000;
     Console.readLineAsync = jest.fn().mockResolvedValue(mockPurchaseAmount);
-    const logspy = jest.spyOn(Console, 'print');
+    const logspy = jest.spyOn(Console, 'readLineAsync');
 
-    await lottoController.promptPurchaseAmount;
+    const lottoController = new LottoController();
+    await lottoController.promptPurchaseAmount();
 
     expect(logspy).toHaveBeenCalledWith('구입 금액을 입력해주세요.\n');
-    expect(lottoController.purchaseAmount).toBe(5000);
+    expect(lottoController.getPurchaseAmount()).toBe(5000);
   });
 });
