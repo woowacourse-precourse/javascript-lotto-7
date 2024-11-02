d298062161e541e6e0252d55a547ffa5fdf2eda2
jin123457 <jin_ox@naver.com>
2024-11-02 17:21:08 +0900

    feat : 로또 구입 갯수 출력 및 저장
    수정된 파일명 : App.js
    App.js : readUserMoney 변수에 할당된 값을 숫자로 형 변환 후 1000으로 나누어 로또 구입 갯수를 계산 및 출력

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ class App {
   async run() {
     const readUserMoney =
       await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    const buyCount = parseInt(readUserMoney, 10) / 1000;
+    Console.print(`\n${buyCount}개를 구매했습니다.`);
   }
 }
 
