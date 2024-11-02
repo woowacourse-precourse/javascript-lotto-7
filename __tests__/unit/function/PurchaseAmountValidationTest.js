989b26beeedbe1bedebbaba5a68ed1458963386c
DodoJBNU <dh723@jbnu.ac.kr>
2024-11-02 22:19:09 +0900

    test: add test case

--- __tests__/unit/function/PurchaseAmountValidationTest.js
+++ __tests__/unit/function/PurchaseAmountValidationTest.js
@@ -12,20 +12,30 @@ describe('구입 금액 검증 테스트, 정상 테스트', () => {
 });
 
 describe('구입 금액 검증 테스트, 예외 테스트', () => {
-  test('음수 검증, 구입 금액 : -1000원', () => {
+  test('양수 검증 구입 금액 : abc', () => {
+    const purchaseAmount = 'abc';
+    expect(() => purchaseAmountValidator(purchaseAmount)).toThrow(ERROR);
+  });
+
+  test('양수 검증 구입 금액 : ', () => {
+    const purchaseAmount = '';
+    expect(() => purchaseAmountValidator(purchaseAmount)).toThrow(ERROR);
+  });
+
+  test('양수 검증, 구입 금액 : -1000원', () => {
     const purchaseAmount = -1000;
     expect(() => purchaseAmountValidator(purchaseAmount)).toThrow(ERROR);
   });
 
-  test('음수 검증, 구입 금액 : -6000원', () => {
+  test('양수 검증, 구입 금액 : -6000원', () => {
     const purchaseAmount = -6000;
     expect(() => purchaseAmountValidator(purchaseAmount)).toThrow(ERROR);
   });
-  test('음수 검증, 구입 금액 : -3235원', () => {
+  test('양수 검증, 구입 금액 : -3235원', () => {
     const purchaseAmount = -3235;
     expect(() => purchaseAmountValidator(purchaseAmount)).toThrow(ERROR);
   });
-  test('단위 검증, 구입 금액 : 1333원', () => {
+  test('양수 검증, 구입 금액 : 1333원', () => {
     const purchaseAmount = 1333;
     expect(() => purchaseAmountValidator(purchaseAmount)).toThrow(ERROR);
   });
