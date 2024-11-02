8c0537e52a74e91debcace5e16e95eeb3678960e
bbjbc <jobc4550@gmail.com>
2024-11-02 22:48:48 +0900

    refactor: 로또 번호 검증 로직 수정

--- src/validator/LottoNumberValidator.js
+++ src/validator/LottoNumberValidator.js
@@ -2,29 +2,43 @@ import { GAME_CONSTANTS } from "../utils/GameConstants.js";
 import { ERROR_MESSAGES } from "../utils/ErrorMessageConstants.js";
 
 export class LottoNumberValidator {
-  static validate(numbers) {
+  // 로또 번호 검증
+  static validateLottoNumbers(numbers) {
+    this.validateNumberArray(numbers);
     this.validateLength(numbers);
     this.validateRange(numbers);
     this.validateDuplicated(numbers);
   }
 
-  // 로또 번호 6개인지 검증
+  // 로또 번호의 배열이 숫자인지 검증
+  static validateNumberArray(numbers) {
+    if (
+      !Array.isArray(numbers) ||
+      numbers.some((number) => typeof number !== "number" || isNaN(number))
+    ) {
+      throw new Error(ERROR_MESSAGES.INVALID_NUMBER_FORMAT);
+    }
+    numbers.forEach(this.validatePositiveNumber);
+  }
+
+  // 로또 개수가 6개인지 검증
   static validateLength(numbers) {
     if (numbers.length !== GAME_CONSTANTS.NUMBER_COUNT) {
       throw new Error(ERROR_MESSAGES.INVALID_LENGTH);
     }
   }
 
-  // 로또 번호 범위 1~45인지 검증
+  // 로또 번호 범위가 1~45인지 검증
   static validateRange(numbers) {
-    numbers.forEach((number) => {
-      if (
-        number < GAME_CONSTANTS.MIN_NUMBER ||
-        number > GAME_CONSTANTS.MAX_NUMBER
-      ) {
-        throw new Error(ERROR_MESSAGES.INVALID_RANGE);
-      }
-    });
+    if (
+      numbers.some(
+        (number) =>
+          number < GAME_CONSTANTS.MIN_NUMBER ||
+          number > GAME_CONSTANTS.MAX_NUMBER
+      )
+    ) {
+      throw new Error(ERROR_MESSAGES.INVALID_RANGE);
+    }
   }
 
   // 로또 번호 중복 검증
