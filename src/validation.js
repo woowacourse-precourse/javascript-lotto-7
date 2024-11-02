85ad946e7825cd0ddda83c854c8028433833b95c
jiyun1006 <jiyun4251@gmail.com>
2024-11-02 16:03:19 +0900

    ✨ feat : 구매금액에 대한 오류검증 3
    구매금액이 1000으로 나누어 떨어지는지에 대한 검증
    - isNotGoodNumber 메서드를 이용해 검증

--- src/validation.js
+++ src/validation.js
@@ -3,7 +3,7 @@ import { ERRORMESSAGE, NUM } from './constants/index.js';
 const validation = Object.freeze({
   /**
    *
-   * @param {str} numberStr
+   * @param {str} number
    */
   isNotNumber: function (number) {
     if (isNaN(number)) throw new Error(ERRORMESSAGE.ISNOTNUMBER);
@@ -11,6 +11,11 @@ const validation = Object.freeze({
   isNotPositiveNumber: function (number) {
     if (Number(number) < NUM.MINIMUM_POSITIVE_NUMBER)
       throw new Error(ERRORMESSAGE.ISNOTPOSITIVENUMBER);
+  },
+
+  isNotGoodNumber: function (number) {
+    if (Number(number) % NUM.LOTTO_PRICE !== 0)
+      throw new Error(ERRORMESSAGE.ISNOTGOODNUMBER);
   },
 });
 
