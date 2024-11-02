0f3eee91f662dab11ace5cabb6b2bff6ceac57bb
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-03 00:33:59 +0900

    feat: LottoControllers 클래스를 구현해 로또 생성, 발행, 매칭, 수익 계산 기능 통합
    - LottoValidator: 로또 번호와 보너스 번호의 유효성 검사
    - LottoIssuer: 입력된 금액만큼 로또 티켓 발행
    - LottoMatcher: 로또 티켓과 당첨 번호 매칭 후 등수 확인
    - ProfitCalculator: 수익률 계산

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -1,18 +1,30 @@
-// 전체 로또 프로그램 흐름 제어
-// issueTickets : 입력한 구입 금액에 따라 로또 티켓 발급
-// checkResults : 당첨 결과 계산 및 수익률 반환
-
-// LottoControllers
-// Lotto : getUserInput, validation, parseUserInput
-// LottoIssuer: getUserInput, validation, parseUserInput, lottoDisplay
-// LottoMatcher: winningNumbers, winningRank
-// ProfitCalculator: profitCalculator
 import Lotto from './Lotto.js';
 import LottoValidator from './LottoValidator.js';
+import LottoIssuer from './LottoIssuer.js';
+import LottoMatcher from './LottoMatcher.js';
+import ProfitCalculator from './ProfitCalculator.js';
 
 class LottoControllers {
   createLotto(numbers, bonusNumber) {
+    LottoValidator.validateWinningNumber(numbers);
+    LottoValidator.validateBonusNumber(numbers, bonusNumber);
     return new Lotto(numbers);
+  }
+
+  issueTickets(purchaseAmount) {
+    return LottoIssuer.issueLottoTickets(purchaseAmount);
+  }
+
+  matchLottoTickets(lottoTickets, winningNumbers, bonusNumber) {
+    return LottoMatcher.assignLottoRank(
+      lottoTickets,
+      winningNumbers,
+      bonusNumber
+    );
+  }
+
+  calculateProfit(rankCounts, purchaseAmount) {
+    return ProfitCalculator.calculateProfitRate(rankCounts, purchaseAmount);
   }
 }
 
