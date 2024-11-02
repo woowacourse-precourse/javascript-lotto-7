4b4d73b200c547d76c38e38cc690bb8ec01ec4d4
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-02 23:58:42 +0900

    feat: LottoMatcher 클래스를 생성해 당첨 매칭 기능을 구현
    - 로또 번호와 당첨 번호를 비교하고 순위를 매칭하는 기능

--- src/controllers/LottoMatcher.js
+++ src/controllers/LottoMatcher.js
@@ -1,3 +1,61 @@
-class LottoMatcher {}
+class LottoMatcher {
+  static checkMatchingNumbers(lottoTickets, winningNumbers) {
+    return lottoTickets.map((lottoTicket) => {
+      const ticketSet = new Set(lottoTicket);
+      const commonNumbers = winningNumbers.filter((number) =>
+        ticketSet.has(number)
+      );
+
+      return commonNumbers.length;
+    });
+  }
+
+  static checkBonusMatch(lottoTicket, bonusNumber) {
+    return lottoTicket.some((ticket) => ticket === bonusNumber);
+  }
+
+  static assignLottoRank(lottoTickets, winningNumbers, bonusNumber) {
+    const rankCounts = {
+      [RANK_KEYS.THREE_MATCH]: GAME_SETTINGS.ZERO,
+      [RANK_KEYS.FOUR_MATCH]: GAME_SETTINGS.ZERO,
+      [RANK_KEYS.FIVE_MATCH]: GAME_SETTINGS.ZERO,
+      [RANK_KEYS.FIVE_WITH_BONUS_MATCH]: GAME_SETTINGS.ZERO,
+      [RANK_KEYS.SIX_MATCH]: GAME_SETTINGS.ZERO,
+    };
+
+    const matchNumbersArray = checkMatchingNumbers(
+      lottoTickets,
+      winningNumbers
+    );
+
+    matchNumbersArray.forEach((matchCount, index) => {
+      let rewardKey = null;
+
+      if (matchCount === MATCH_COUNT.SIX) {
+        rewardKey = RANK_KEYS.SIX_MATCH;
+      }
+      if (matchCount === MATCH_COUNT.FIVE) {
+        const bonusMatch = checkBonusMatch(lottoTickets[index], bonusNumber);
+        if (bonusMatch) {
+          rewardKey = RANK_KEYS.FIVE_WITH_BONUS_MATCH;
+        } else {
+          rewardKey = RANK_KEYS.FIVE_MATCH;
+        }
+      }
+      if (matchCount === MATCH_COUNT.FOUR) {
+        rewardKey = RANK_KEYS.FOUR_MATCH;
+      }
+      if (matchCount === MATCH_COUNT.THREE) {
+        rewardKey = RANK_KEYS.THREE_MATCH;
+      }
+
+      if (rewardKey) {
+        rankCounts[rewardKey]++;
+      }
+    });
+
+    return rankCounts;
+  }
+}
 
 export default LottoMatcher;
