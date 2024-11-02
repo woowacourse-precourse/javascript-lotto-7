977e22090aff52990ab05dbacb9971532c1ae0e3
jeonhyeonseon <good_pretty@naver.com>
2024-11-03 00:54:17 +0900

    feat(lotto): 로또 번호 중복되는 경우 예외처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,6 +15,12 @@ class Lotto {
     if (!numbers.every(num => num >= 1 && num <= 45)) {
       throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
     }
+
+    // 중복되는 번호가 있는 경우
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
   }
 }
 
