5cac67fe42ac4b1f7351af1350cb6ccf204700be
SanghyunLee <lgw9736@naver.com>
2024-11-02 22:32:45 +0900

    feat: 로또 검증 추가
    - 로또 생성시 배열에 중복된 숫자가 들어오면 에러 처리 추가

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,16 +1,17 @@
-import { lottoConfig } from '../models/lottoConfig';
+import { lottoConfig } from '../models/lottoConfig.js';
 
 export const ERROR_MESSAGE = Object.freeze({
   LOTTO: {
     INVALID_LOTTO_NUMBER_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
+    NUMBER_DUPLICATION : '[ERROR] 로또 번호에 중복된 숫자가 있습니다.'
   },
   LOTTO_GENERATOR: {
-    INVALID_LOTTO_PRICE_UNIT: `[ERROR] 구매금액은 ${lottoConfig.LOTTO_PRICE}원 단위여야합니다.\n`,
-    INVALID_POSITIVE_NUMBER: '[ERROR] 구매금액은 0보다 커야합니다.\n',
+    INVALID_LOTTO_PRICE_UNIT: `[ERROR] 구매금액은 ${lottoConfig.LOTTO_PRICE}원 단위여야합니다.`,
+    INVALID_POSITIVE_NUMBER: '[ERROR] 구매금액은 0보다 커야합니다.',
   },
   WINNING_LOTTO: {
-    INVALID_NUMBER_RANGE: `[ERROR] 번호의 범위는 ${lottoConfig.MIN_NUMBER}~${lottoConfig.MAX_NUMBER}여야합니다.\n`,
-    MAIN_NUMBER_DUPLICATION: '[ERROR] 당첨 번호에 중복된 숫자가 있습니다.\n',
-    BONUS_NUMBER_DUPLICATION: '[ERROR] 보너스 번호가 당첨번호와 중복됩니다.\n',
+    INVALID_NUMBER_RANGE: `[ERROR] 번호의 범위는 ${lottoConfig.MIN_NUMBER}~${lottoConfig.MAX_NUMBER}여야합니다.`,
+    MAIN_NUMBER_DUPLICATION: '[ERROR] 당첨 번호에 중복된 숫자가 있습니다.',
+    BONUS_NUMBER_DUPLICATION: '[ERROR] 보너스 번호가 당첨번호와 중복됩니다.',
   },
 });
--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -1,4 +1,4 @@
-import { ERROR_MESSAGE } from "../constants/errorMessage";
+import { ERROR_MESSAGE } from "../constants/errorMessage.js";
 
 export default class Lotto {
   #numbers;
@@ -8,13 +8,29 @@ export default class Lotto {
     this.#numbers = numbers;
   }
 
+  getNumbers() {
+    return this.#numbers;
+  }
+
   #validate(numbers) {
+    this.#checkLottoCount(numbers);
+    this.#checkNumberDuplication(numbers);
+  }
+
+  #checkLottoCount(numbers) {
     if (numbers.length !== 6) {
       throw new Error(ERROR_MESSAGE.LOTTO.INVALID_LOTTO_NUMBER_COUNT);
     }
   }
 
-  getNumbers() {
-    return this.#numbers;
-  }
+  #checkNumberDuplication(numbers) {
+    const lottoSet = new Set();
+    
+    numbers.forEach((number) => {
+      if (lottoSet.has(number)) {
+        throw new Error(ERROR_MESSAGE.LOTTO.NUMBER_DUPLICATION);
+      }
+      lottoSet.add(number);
+    });
+  }  
 }
