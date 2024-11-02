cf849a9a12fcb64914a233b9e7daf09bb85f78b3
yena <rladpsk1117@gmail.com>
2024-11-03 05:56:03 +0900

    feat(LottoController): 구입 금액 입력 기능 구현
    InputView를 통해 입력된 구입 금액 값을 숫자로 변환하여 Purchase 클래스로 전달
    OutputView로 구입 수량 출력
    잘못된 값 입력 시 에러 메시지 출력 후 재입력 받음

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,17 +1,35 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
 import Lotto from '../model/Lotto.js';
 import BonusNumber from '../model/BonusNumber.js';
+import Purchase from '../model/Purchase.js';
 
 class LottoController {
+  #purchase; // purchase 인스턴스
   #purchaseAmount; // 구입금액
+  #tickets; // 구입 수량
   #lotteryNumbers; // 구입한 로또 번호
   #winningLottoNumbers; // 당첨 번호
   #bonusNumber; // 보너스 번호
 
   async start() {
+    await this.#getPurchase();
     await this.#getWinningLottoNumbers();
     await this.#getBonusNumber();
+  }
+
+  async #getPurchase() {
+    try {
+      const input = await InputView.inputAmount();
+      this.#purchaseAmount = Number(input);
+      this.#purchase = new Purchase(this.#purchaseAmount);
+      this.#tickets = this.#purchase.purchaseTickets();
+      await OutputView.printLottoTickets(this.#tickets);
+    } catch (error) {
+      Console.print(error);
+      await this.#getPurchase();
+    }
   }
 
   async #getWinningLottoNumbers() {
