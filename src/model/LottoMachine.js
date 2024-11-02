f7c8779c375f4a565c4b63f8bdcf2c855e39c61b
Daeuning <happyjde3@gmail.com>
2024-11-03 00:53:23 +0900

    feat(LottoMachine): 입력된 금액만큼 로또 개수를 받을 수 있는 기능 구현

--- src/model/LottoMachine.js
+++ src/model/LottoMachine.js
@@ -3,12 +3,21 @@ import validatePurchaseAmount from '../validators/PurchaseAmountValidator.js';
 
 class LottoMachine {
   #purchaseAmount;
+  #lottoCount;
 
   constructor(purchaseAmount) {
     validatePurchaseAmount(purchaseAmount);
-    this.#purchaseAmount = purchaseAmount;
+    this.#purchaseAmount = Number(purchaseAmount);
+    this.#lottoCount = this.#calculateLottoCount();
   }
 
+  #calculateLottoCount() {
+    return this.#purchaseAmount / GAME_RULES.CURRENCY_UNIT;
+  }
+
+  getLottoCount() {
+    return this.#lottoCount;
+  }
 }
 
 export default LottoMachine;
