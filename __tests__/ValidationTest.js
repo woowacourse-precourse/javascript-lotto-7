888c0b529f3a2b433c2488f6b2b0fbb908e40815
seonae-j <wkdtjsdo135@naver.com>
2024-11-02 21:36:05 +0900

    test(Validation): 보너스 숫자 예외 테스트 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,10 +1,15 @@
-import { validateCost } from '../src/utils/Validation.js';
+import {
+  validateCost,
+  validateInteger,
+  validateNumber,
+  validateRange,
+} from '../src/utils/Validation.js';
 import { ERROR } from '../src/constants/Constants.js';
 
-describe('로또 구매 금액 함수 예외 테스트', () => {
-  test('사용자가 입력한 로또 구매 금액이 숫자가 아니면 예외가 발생한다.', () => {
+describe('사용자 입력값 예외 테스트', () => {
+  test('사용자가 입력한 값이 숫자가 아니면 예외가 발생한다.', () => {
     expect(() => {
-      validateCost('10a');
+      validateNumber('10a');
     }).toThrow(ERROR.COST_TYPE);
   });
 
@@ -14,9 +19,15 @@ describe('로또 구매 금액 함수 예외 테스트', () => {
     }).toThrow(ERROR.COST_UNIT);
   });
 
-  test('사용자가 입력한 로또 구매 금액이 정수가 아니면 예외가 발생한다.', () => {
+  test('사용자가 입력한 값이 정수가 아니면 예외가 발생한다.', () => {
     expect(() => {
-      validateCost(12.35);
+      validateInteger(12.35);
     }).toThrow(ERROR.COST_UNIT);
+  });
+
+  test('사용자가 입력한 값이 1-45 범위 밖의 숫자이면 예외가 발생한다.', () => {
+    expect(() => {
+      validateRange(46);
+    }).toThrow(ERROR.LOTTO_RANGE);
   });
 });
