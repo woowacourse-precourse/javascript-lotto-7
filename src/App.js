a95a0dea0d14e73e34a41cc3dab4f653e7a24e35
Ong_Du <pas4976@gmail.com>
2024-11-02 22:29:50 +0900

    feat : 유저 입력 세트 추가
    - 유저의 구입 금액 입력
    - 유저의 당첨 번호 입력
    - 유저의 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import {Console} from '@woowacourse/mission-utils'
 class App {
-  async run() {}
+  async run() {
+    const userPurchaseAmount = this.getUserPurchaseAmount();
+    const userWinningNumber = this.getUserWinningNumber();
+    const userWinningBonusNumber = this.getUserWinningBonusNumber();
+  }
+
+  getUserPurchaseAmount(){
+    return Console.readLineAsync('구입금액을 입력해 주세요.');
+  }
+
+  getUserWinningNumber(){
+    return Console.readLineAsync('당첨 번호를 입력해 주세요.');
+  }
+
+  getUserWinningBonusNumber(){
+    return Console.readLineAsync('보너스 번호를 입력해 주세요.');
+  }
 }
 
 export default App;
