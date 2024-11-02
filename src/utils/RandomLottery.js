51124a979754a23a976a86deb155fe5b2c422766
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-11-03 06:54:28 +0900

    :sparkles: feat: 로또 번호 오름차순 정렬

--- src/utils/RandomLottery.js
+++ src/utils/RandomLottery.js
@@ -1,7 +1,8 @@
 import { Random } from '@woowacourse/mission-utils';
 
 const drawLottery = () => {
-  return Random.pickUniqueNumbersInRange(1, 45, 6);
+  const draw = Random.pickUniqueNumbersInRange(1, 45, 6);
+  return draw.sort((a, b) => a - b);
 };
 
 export default drawLottery;
