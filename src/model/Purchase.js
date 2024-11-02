f2da1b05e04686fe627f18eee8353a37cf254acd
yena <rladpsk1117@gmail.com>
2024-11-03 04:18:49 +0900

    feat(Purchase): 구입 금액 유효성 체크 및 예외 처리 구현

--- src/model/Purchase.js
+++ src/model/Purchase.js
@@ -3,7 +3,17 @@ class Purchase {
   #lotteryNumbers;
 
   constructor(amount) {
+    this.#validate(amount);
     this.#amount = amount;
+  }
+
+  #validate(amount) {
+    if (amount % 1000 !== 0) {
+      throw new Error('[ERROR] 잘못된 구입 금액입니다.');
+    }
+    if (amount < 1000) {
+      throw new Error('[ERROR] 구입 금액은 1000원 이상이어야 합니다.');
+    }
   }
 }
 
