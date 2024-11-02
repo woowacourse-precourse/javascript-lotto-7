68f9bb6c9e65374ce318daa1c7468e4e98cefb97
Lee <dltmdgns0508@gmail.com>
2024-11-02 15:50:47 +0900

    refactor(Validator): class => function 변경
    Validator.js 수정
    - Validator를 사용하는 방식이 class로 선언하는것 보단 함수로 관리하는게 효율적이라 판단되어 변경

--- src/Validators/Validator.js
+++ src/Validators/Validator.js
@@ -1,18 +1,11 @@
 import CustomError from '../Utils/CustomError.js';
 
-class Validator {
-  #rules;
-  constructor(rules) {
-    this.#rules = rules;
-  }
-
-  validate(target) {
-    Object.values(this.#rules).forEach(rule => {
-      if (!rule.isValid(target)) {
-        throw new CustomError(rule.errorMessage);
-      }
-    });
-  }
+function validator(target, rules) {
+  Object.values(rules).forEach((rule) => {
+    if (!rule.isValid(target)) {
+      throw new CustomError(rule.errorMessage);
+    }
+  });
 }
 
-export default Validator;
+export default validator;
