7055c4e82ccfaf1039209b39380947f121b0b117
최영빈 <bin778@naver.com>
2024-11-02 14:17:46 +0900

    refactor: 구입한 로또 생성 시간복잡도 성능 개선

--- src/view/GameOutput.js
+++ src/view/GameOutput.js
@@ -4,21 +4,20 @@ import { MESSAGE } from "../constant/Message.js";
 class GameOutput {
   printNewLotto(new_lotto) {
     Console.print(`\n${new_lotto.length}${MESSAGE.OUTPUT.PURCAHSE_LOTTO}`);
-    new_lotto.forEach((lotto) => {
-      Console.print(`[${lotto.join(", ")}]`);
-    });
+    const format_lotto = new_lotto.map((lotto) => `[${lotto.join(", ")}]`).join("\n");
+    Console.print(format_lotto);
   }
 
   printGameResult(result) {
     const REGEX = /\B(?=(\d{3})+(?!\d))/g;
-    const formattedBenefitRate = result.benefitRate.toString().replace(REGEX, ",");
+    const format_benefit_rate = result.benefitRate.toString().replace(REGEX, ",");
     Console.print(MESSAGE.OUTPUT.RESULT_HEADER);
     Console.print(`${MESSAGE.OUTPUT.RESULT.FIFTH} - ${result.prizeStat.prizeFifth}개`);
     Console.print(`${MESSAGE.OUTPUT.RESULT.FOURTH} - ${result.prizeStat.prizeFourth}개`);
     Console.print(`${MESSAGE.OUTPUT.RESULT.THIRD} - ${result.prizeStat.prizeThird}개`);
     Console.print(`${MESSAGE.OUTPUT.RESULT.SECOND} - ${result.prizeStat.prizeSecond}개`);
     Console.print(`${MESSAGE.OUTPUT.RESULT.FIRST} - ${result.prizeStat.prizeFirst}개`);
-    Console.print(`${MESSAGE.OUTPUT.RESULT_PROFIT_RATE}${formattedBenefitRate}${MESSAGE.OUTPUT.RESULT_FOOTER}`);
+    Console.print(`${MESSAGE.OUTPUT.RESULT_PROFIT_RATE}${format_benefit_rate}${MESSAGE.OUTPUT.RESULT_FOOTER}`);
   }
 
   printErrorMesssage(error) {
