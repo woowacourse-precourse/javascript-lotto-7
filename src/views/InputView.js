fa055f214bc144d881cf4addd089836b324310a7
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-11-02 15:22:43 +0900

    fix: 이중 try catch 구문으로 인해 에러 메시지가 정상적으로 출력되지 않았다

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,11 +1,12 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
+import Lotto from '../Lotto.js';
+
 import { validateUserMoney, validateNumber } from '../validators/InputValidator.js';
 
 import parseWinningNumbers from '../utils/parsedWinningNumbers.js';
 
 import { USER_MONEY_PROMPT, WINNING_NUMBER_PROMPT, BONUS_NUMBER_PROMPT } from '../constants/prompts.js';
-import { INPUT_ERROR_MESSAGE } from '../constants/message.js';
 
 export async function getUserMoney() {
   const userMoney = await MissionUtils.Console.readLineAsync(USER_MONEY_PROMPT);
@@ -15,20 +16,13 @@ export async function getUserMoney() {
 }
 
 export async function getWinningNumbers() {
-  try {
-    const winningNumbers = await MissionUtils.Console.readLineAsync(WINNING_NUMBER_PROMPT);
-    return parseWinningNumbers(winningNumbers);
-  } catch (error) {
-    throw new Error(INPUT_ERROR_MESSAGE);
-  }
+  const winningNumbers = await MissionUtils.Console.readLineAsync(WINNING_NUMBER_PROMPT);
+  new Lotto(winningNumbers);
+  return parseWinningNumbers(winningNumbers);
 }
 
 export async function getBonusNumber() {
-  try {
-    const bonusNumber = await MissionUtils.Console.readLineAsync(BONUS_NUMBER_PROMPT);
-    validateNumber(bonusNumber);
-    return parseInt(bonusNumber, 10);
-  } catch (error) {
-    throw new Error(INPUT_ERROR_MESSAGE);
-  }
+  const bonusNumber = await MissionUtils.Console.readLineAsync(BONUS_NUMBER_PROMPT);
+  validateNumber(bonusNumber);
+  return parseInt(bonusNumber, 10);
 }
