422eb3e89daf6a197506ee462d663a74b6345203
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-11-02 21:38:03 +0900

    feat: 출력 기능 구현
    - LottoOutputWriter 클래스 추가
    - Console.print()가 필요한 모든 경우를 각각의 메소드로 구현

---
+++ src/classes/LottoOutputWriter.js
@@ -0,0 +1,50 @@
+import { Console } from '@woowacourse/mission-utils';
+import LottoChecker from './LottoChecker.js';
+
+class LottoOutputWriter {
+  printLottos(lottos) {
+    this.#printLottoCount(lottos.length);
+    this.#printLottoNumbers(lottos);
+  }
+
+  printWinningResult(winningResult) {
+    Console.print('당첨 통계\n---\n');
+
+    const output = [];
+    for (let i = winningResult.length - 1; i >= 0; i--) {
+      const { matchCount, prize, bonus } = LottoChecker.PRIZE_TIERS[i];
+      output.push(
+        this.#generateWinningResultMessage(
+          matchCount,
+          prize,
+          bonus,
+          winningResult[i]
+        )
+      );
+    }
+
+    Console.print(output.join('\n'));
+  }
+
+  printYield(yieldRate) {
+    Console.print(`총 수익률은 ${yieldRate}%입니다.`);
+  }
+
+  #printLottoCount(lottoCount) {
+    Console.print(`${lottoCount}개를 구매했습니다.\n`);
+  }
+
+  #printLottoNumbers(lottos) {
+    const output = lottos.map((lotto) => `[${lotto.getNumbers().join(', ')}]`);
+    Console.print(`${output.join('\n')}\n\n`);
+  }
+
+  #generateWinningResultMessage(matchCount, prize, bonus, count) {
+    if (matchCount === 5 && bonus) {
+      return `${matchCount}개 일치, 보너스 볼 일치 (${prize}원) - ${count}개`;
+    }
+    return `${matchCount}개 일치 (${prize}원) - ${count}개`;
+  }
+}
+
+export default LottoOutputWriter;
