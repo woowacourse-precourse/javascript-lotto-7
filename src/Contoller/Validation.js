d6b975f354edc294b2b1de72a4c082acb273c6f4
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-02 23:20:58 +0900

    feat: 로또 번호가 6자리가 아닌 경우 예외가 발생하는 기능 추가

--- src/Contoller/Validation.js
+++ src/Contoller/Validation.js
@@ -10,6 +10,12 @@ class Validation {
       throw new Error('[ERROR] 입력값이 없습니다.');
     }
   }
+
+  static validateSixNumbers(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
 }
 
 export default Validation;
