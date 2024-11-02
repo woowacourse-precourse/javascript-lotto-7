9b83a2487140dc3627d13baaefda76879446d72e
Jestin-FE <jestinfe@naver.com>
2024-11-03 05:18:00 +0900

    feat(README, App): 기능 목록 작성 및 첫 번째 기능 구현
    README에 전체적인 기능 목록을 작성했다. App에 로또 구입 금액을 입력 받고, 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외 처리하고 금액을 재입력하도록 하고, 로또의 개수를 계산하는 기능을 구현했다. 기능들이 UI 로직이므로 테스트를 작성하지 않았다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let purchaseMoney = 0;
+    const inputMoney = async () => {
+      const money = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.');
+      return Number(money);
+    }
+    
+    const TICKET_PRICE = 1000;
+    let ticketCount = 0;
+    const checkRestZero = async () => {
+      purchaseMoney = await inputMoney();
+      if (purchaseMoney % TICKET_PRICE === 0) {
+        ticketCount = purchaseMoney / TICKET_PRICE;
+      } else {
+        throw new Error('[ERROR] 1,000원 단위로 입력하세요.');
+      }
+    }
+
+    try {
+      await checkRestZero();
+    } catch(error) {
+      MissionUtils.Console.print(error.message);
+      await checkRestZero();
+    }
+  }
 }
 
 export default App;
