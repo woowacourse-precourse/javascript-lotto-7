f322ef7980e96bda92532d4c58a32ffe91811b92
Yusi <yusi4781@naver.com>
2024-11-02 16:55:10 +0900

    feat(app): 로또 구입 금액 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
+import Validator from "./utils/Validator.js";
 
 class App {
   async run() {
-    const purchaseAmount = await Console.readLineAsync();
+    const validator = new Validator();
+    const purchaseAmount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    validator.validatePurchaseAmount(purchaseAmount);
   }
 }
 
---
+++ src/utils/Validator.js
@@ -0,0 +1,15 @@
+class Validator {
+  validatePurchaseAmount(purchaseAmount) {
+    if (isNaN(purchaseAmount)) {
+      throw new Error("[ERROR] 로또 구매 금액은 숫자여야 합니다.");
+    }
+    if (purchaseAmount <= 0) {
+      throw new Error("[ERROR] 로또 구매 금액은 0보다 커야 합니다.");
+    }
+    if (purchaseAmount % 1000 !== 0) {
+      throw new Error("[ERROR] 로또 구매 금액은 1000원 단위로 이루어져야 합니다.");
+    }
+  }
+}
+
+export default Validator;
