888846524ebd4a4f77cdcbd2d5a03669eab8e0b3
장정안 <wjddks96@naver.com>
2024-11-02 19:33:10 +0900

    feat: Lotto 클래스 내에서 데이터 관리
    - 유효성 검증은 추후 구체화하여 분리할 예정입니다

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,45 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   #validate(numbers) {
+    this.#validateLength(numbers);
+    this.#validateDuplicate(numbers);
+    this.#validateRange(numbers);
+  }
+
+  #validateLength(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateDuplicate(numbers) {
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
+    }
+  }
+
+  #validateRange(numbers) {
+    if (numbers.some((num) => num < 1 || num > 45)) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  }
+
+  getNumbers() {
+    return [...this.#numbers];
+  }
+
+  match(winningNumbers) {
+    return this.#numbers.filter((number) => winningNumbers.includes(number))
+      .length;
+  }
+
+  includes(number) {
+    return this.#numbers.includes(number);
+  }
 }
 
 export default Lotto;
