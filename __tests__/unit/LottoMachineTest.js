576930e76f4bb84fd51b5820fc4edf6fcd5fbbd0
Daeuning <happyjde3@gmail.com>
2024-11-03 03:21:36 +0900

    test(LottoMachineTest): 구매 금액에 따른 로또 번호를 얻는 getLottoNumbers()테스트 코드 작성

--- __tests__/unit/LottoMachineTest.js
+++ __tests__/unit/LottoMachineTest.js
@@ -1,6 +1,18 @@
 import LottoMachine from '../../src/model/LottoMachine.js';
 
-describe("LottoMachine 클래스의 로또 개수 계산 테스트", () => {
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+
+  numbers.reduce((mockFn, number) => {
+    return mockFn.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+beforeEach(() => {
+  jest.restoreAllMocks(); 
+});
+
+describe("getLottoCount() 테스트", () => {
   test.each([
     { purchaseAmount: "1000", expectedCount: 1 },         
     { purchaseAmount: "50000", expectedCount: 50 },      
@@ -12,5 +24,24 @@ describe("LottoMachine 클래스의 로또 개수 계산 테스트", () => {
     const lottoCount = lottoMachine.getLottoCount();
 
     expect(lottoCount).toBe(expectedCount);
+  });
+});
+
+describe("getLottoNumbers() 테스트", () => {
+  test("구입 금액에 따른 로또 번호 배열을 생성해야 한다", () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44]
+    ]);
+    const lottoMachine = new LottoMachine("3000"); 
+
+    const lottoNumbers = lottoMachine.getLottoNumbers();
+
+    expect(lottoNumbers).toEqual([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44]
+    ]);
   });
 });
