4069ff64d162959e24ba18129b92c7bc67b7930d
hamo-o <yheel369@gmail.com>
2024-11-02 20:34:49 +0900

    fix(ValidationError): 확장자 수정, 메시지 개행문자 추가

--- src/errors/ValidationError.js
+++ src/errors/ValidationError.js
@@ -1,8 +1,8 @@
-import { ERROR_HEADER } from "../constants/errorMessage";
+import { ERROR_HEADER } from "../constants/errorMessage.js";
 
 class ValidationError extends Error {
   constructor(message) {
-    super(`${ERROR_HEADER} ${message}`);
+    super(`${ERROR_HEADER} ${message}\n`);
     this.name = this.constructor.name;
   }
 }
