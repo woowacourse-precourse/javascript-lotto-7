fa771d7400ddeaf7b9af3a05cf716fdae36c9f5f
yeabbbbb <b08009@naver.com>
2024-11-02 22:01:33 +0900

    refactor: change error message on non-numerical input
    - [ERROR] 숫자 이외의 문자가 입력되었습니다. -> [ERROR] 숫자만 입력해 주세요.

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,10 +9,6 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if(!numbers) {
-      throw new Error("[ERROR] 입력이 없습니다.");
-    }
-
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
@@ -20,7 +16,7 @@ class Lotto {
     const regex = /^[0-9]*$/;
     numbers.forEach((number) => {
       if (!regex.test(number)) {
-        throw new Error('[ERROR] 숫자 이외의 문자가 입력되었습니다.');
+        throw new Error('[ERROR] 숫자만 입력해 주세요.');
       }
 
       if (Number(number) < 1 || Number(number) > 45) {
@@ -34,7 +30,6 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
   get getNumbers() {
     return this.#numbers;
   }
