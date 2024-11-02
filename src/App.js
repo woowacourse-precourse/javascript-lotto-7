c295d514f33aa93dd72b316736562ca0c81b977b
jin123457 <jin_ox@naver.com>
2024-11-02 18:25:14 +0900

    feat : 복권 객체를 순회하며 등 수 확인 및 결과 저장
    수정된 파일명 : App.js, Lotto.js
    App.js : 로또 당첨된 결과를 담는 lottoResult 2차원 배열을 생성하고 당첨 복권의 등수를 담는 RANK_TABLE 객체를 생성한다.
    lottoList 배열 안에 있는 로또 객체를 하나씩 꺼내오면서 Lotto 클래스 안에 있는 matchRank 함수를 호출하고 그 결과 값을 rank 변수에 할당한다.
    rank 변수의 값이 있을 경우 lottoResults에 해당하는 등 수를 찾아 + 1을 해준다.
    
    Lotto.js : matchRank 함수를 추가
    match 함수의 기능은 복권의 숫자와 당첨 번호를 set에 넣어 중복되는 횟수를 계산 후 matchCount 변수에 할당하고
    복권의 숫자 중 보너스 번호와 일치하는 것이 있을 경우 winningbouns 변수에 할당한다.
    RANK_TABLE에서 matchCount 숫자를 검색하여 등 수를 확인하고 일치하는 숫자가 5개인 경우에만 보너스 번호가 유효하기에 보너스 번호 일치 여부에 따른 조건을 추가

--- src/App.js
+++ src/App.js
@@ -10,17 +10,41 @@ class App {
     Console.print(`\n${buyCount}개를 구매했습니다.`);
 
     const lottoList = Array.from({ length: buyCount }, () => {
-      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+        (a, b) => a - b,
+      );
       return new Lotto(randomNumbers);
     });
 
     const readUserWinningNumbers = await Console.readLineAsync(
       '\n당첨 번호를 입력해 주세요.\n',
     );
+    const winNumbers = readUserWinningNumbers
+      .split(',')
+      .map((number) => parseInt(number, 10));
 
-    const readUserBonusNumber = await Console.readLineAsync(
-      '\n보너스 번호를 입력해 주세요.\n',
+    const readUserBonusNumber = parseInt(
+      await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n'),
+      10,
     );
+
+    const lottoResults = [
+      ['6개 일치', 2000000000, 0],
+      ['5개 일치, 보너스 볼 일치', 30000000, 0],
+      ['5개 일치', 1500000, 0],
+      ['4개 일치', 50000, 0],
+      ['3개 일치', 5000, 0],
+    ];
+    const RANK_TABLE = {
+      6: 1,
+      5: [2, 3],
+      4: 4,
+      3: 5,
+    };
+    lottoList.forEach((lotto) => {
+      const rank = lotto.matchRank(RANK_TABLE, winNumbers, readUserBonusNumber);
+      if (rank) lottoResults[rank - 1][2] += 1;
+    });
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -18,6 +18,22 @@ class Lotto {
   #printNumbers() {
     return Console.print(`[${this.#numbers.join(', ')}]`);
   }
+
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  matchRank(RANK_TABLE, winNumbers, isWinningBouns) {
+    const matchCount = 12 - new Set([...this.#numbers, ...winNumbers]).size;
+    const winningbouns = this.#numbers.some((number) => number === isWinningBouns);
+    const rank = RANK_TABLE[matchCount];
+    
+    if (matchCount === 5 && winningbouns) return rank[0];
+
+    if (matchCount === 5 && !winningbouns) return rank[1];
+
+    return rank;
+  }
 }
 
 export default Lotto;
