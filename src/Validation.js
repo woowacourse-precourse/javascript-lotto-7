820d881a5caeeb23c3611346b011f5f65c8c8130
yeabbbbb <b08009@naver.com>
2024-11-02 21:17:26 +0900

    refactor: modulize input validation functions
    - Extracted validation logic into seperate functions: isNotEmpty, isNumerical, endsWithThreeZeros, isInRange.
    - Improve code readability by implementing modulized validation function

--- src/Validation.js
+++ src/Validation.js
@@ -1,55 +1,36 @@
 export function validatePurchaseAmount(purchaseAmountInput) {
-  if (!purchaseAmountInput) {
-    throw new Error('[ERROR] 입력이 없습니다.');
-  }
+  isNotEmpty(purchaseAmountInput);
+  isNumerical(purchaseAmountInput);
+  endsWithThreeZeros(purchaseAmountInput);
+}
 
+export function validateBonusNumber(bonusNumberInput) {
+  isNotEmpty(bonusNumberInput);
+  isNumerical(bonusNumberInput);
+  isInRange(bonusNumberInput);
+}
+
+export function isNotEmpty(input) {
+  if (!input) {
+    throw new Error('[ERROR] 입력값이 없습니다.');
+  }
+};
+
+export function isNumerical(input) {
   const regex = /^[0-9]*$/;
-  if (!regex.test(purchaseAmountInput)) {
-    throw new Error('[ERROR] 숫자 이외의 문자가 입력되었습니다.');
+  if (!regex.test(input)) {
+    throw new Error('[ERROR] 숫자만 입력해 주세요.');
   }
+};
 
-  if (!purchaseAmountInput.endsWith('000')) {
+export function endsWithThreeZeros(input) {
+  if (!input.endsWith('000')) {
     throw new Error('[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.');
   }
 };
 
-export function validateWinningNumber(parsedWinningNumbers) {
-  if (!parsedWinningNumbers) {
-    throw new Error('[ERROR] 입력이 없습니다.');
-  }
-
-  if (parsedWinningNumbers.length != 6) {
-    throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-  }
-
-  const regex = /^[0-9]*$/;
-  parsedWinningNumbers.forEach((num) => {
-    if (!regex.test(num)) {
-      throw new Error('[ERROR] 숫자 이외의 문자가 입력되었습니다.');
-    }
-
-    if (Number(num) < 1 || Number(num) > 45) {
-      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
-    }
-  });
-
-  const winningNumbersSet = new Set(parsedWinningNumbers);
-  if (parsedWinningNumbers.length != winningNumbersSet.size) {
-    throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
-  }
-};
-
-export function validateBonusNumber(bonusNumberInput) {
-  if (!bonusNumberInput) {
-    throw new Error('[ERROR] 입력이 없습니다.');
-  }
-
-  const regex = /^[0-9]*$/;
-  if (!regex.test(bonusNumberInput)) {
-    throw new Error('[ERROR] 숫자 이외의 문자가 입력되었습니다.');
-  }
-
-  if (Number(bonusNumberInput) < 1 || Number(bonusNumberInput) > 45) {
+export function isInRange(input) {
+  if (Number(input) < 1 || Number(input) > 45) {
     throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
   }
 };
