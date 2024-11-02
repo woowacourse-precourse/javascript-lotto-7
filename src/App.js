ad716c0cf2cdc35d8be4dad8bac6f381b4f968ff
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-11-02 16:33:11 +0900

    feat: 구매 금액 input 받기 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+import { purchaseAmountValidation } from './inputHandler/inputValidation';
+
 class App {
-  async run() {}
+  async readPurchaseAmount() {
+    const purchaseAmount = await Console.readLineAsync(
+      '구입금액을 입력해 주세요.\n',
+    );
+    purchaseAmountValidation(purchaseAmount);
+  }
+
+  async run() {
+    const purchaseAmount = await this.readPurchaseAmount();
+  }
 }
 
 export default App;
