c0261b7c2745f17f473b978508880143fee165fa
kch6396 <cjfghks6396@gmail.com>
2024-11-02 22:54:21 +0900

    test(Draw): Add unit tests for Draw model
    Implement unit tests for the Draw model to verify correct handling
    of winning and bonus numbers.

--- src/model/Draw.js
+++ src/model/Draw.js
@@ -1,4 +1,8 @@
-import { WINNING_NUMBER_MESSAGE } from "../constant/constant.js";
+import {
+  BONUS_NUMBER_MESSAGE,
+  START_ERROR,
+  WINNING_NUMBER_MESSAGE,
+} from "../constant/constant.js";
 
 class Draw {
   #winningNUmbers;
@@ -18,30 +22,54 @@ class Draw {
           !winningNumber || isNaN(winningNumber) || winningNumber <= 0
       )
     ) {
-      throw new Error(WINNING_NUMBER_MESSAGE.INVALID_WINNING_NUMBER);
+      throw new Error(
+        START_ERROR.START_ERROR_MESSAGE +
+          " " +
+          WINNING_NUMBER_MESSAGE.INVALID_WINNING_NUMBER
+      );
     }
     if (winningNumbers.length !== 6) {
-      throw new Error(WINNING_NUMBER_MESSAGE.NOT_SIX_NUMBER);
+      throw new Error(
+        START_ERROR.START_ERROR_MESSAGE +
+          " " +
+          WINNING_NUMBER_MESSAGE.NOT_SIX_NUMBER
+      );
     }
     if (new Set(winningNumbers).size !== winningNumbers.length) {
-      throw new Error(WINNING_NUMBER_MESSAGE.NOT_SAME_NUMBER);
+      throw new Error(
+        START_ERROR.START_ERROR_MESSAGE +
+          " " +
+          WINNING_NUMBER_MESSAGE.NOT_SAME_NUMBER
+      );
     }
     if (
       winningNumbers.some(
         (winningNumber) => winningNumber < 1 || winningNumber > 45
       )
     ) {
-      throw new Error(WINNING_NUMBER_MESSAGE.NOT_RANGE_NUMBER);
+      throw new Error(
+        START_ERROR.START_ERROR_MESSAGE +
+          " " +
+          WINNING_NUMBER_MESSAGE.NOT_RANGE_NUMBER
+      );
     }
   }
 
   #validateBonusNumber(bonusNumber) {
     if (!bonusNumber || isNaN(bonusNumber) || bonusNumber <= 0) {
-      throw new Error(BONUS_NUMBER_MESSAGE.INVALID_WINNING_NUMBER);
+      throw new Error(
+        START_ERROR.START_ERROR_MESSAGE +
+          " " +
+          BONUS_NUMBER_MESSAGE.INVALID_WINNING_NUMBER
+      );
     }
 
     if (bonusNumber < 1 || bonusNumber > 45) {
-      throw new Error(BONUS_NUMBER_MESSAGE.NOT_RANGE_NUMBER);
+      throw new Error(
+        START_ERROR.START_ERROR_MESSAGE +
+          " " +
+          BONUS_NUMBER_MESSAGE.NOT_RANGE_NUMBER
+      );
     }
   }
 
