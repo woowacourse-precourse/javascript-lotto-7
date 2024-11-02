03b15ed97d29bbe6d21444df553fed5e2c657fbe
Jinny <ggab.challenge@gmail.com>
2024-11-02 18:36:56 +0900

    refactor: 1개로 통합한 입력 받는 클래스 가독성있게 코드 줄이기

--- src/Views/inputViews.js
+++ src/Views/inputViews.js
@@ -5,63 +5,61 @@ import { BonusNumberValidator } from '../Controllers/Validator/bonusNumberValida
 
 class Inputs {
   constructor() {
-    this.purchaseAmount = 0;
-    this.winningNumbers = 0;
-    this.bonusNumber = 0;
     this.purchaseAmountValidator = new PurchaseAmountValidator();
     this.winningNumbersValidator = new WinningNumbersValidator();
     this.BonusNumberValidator = new BonusNumberValidator();
+
+    this.purchaseAmountMessage = '구입금액을 입력해 주세요.\n';
+    this.winningNumbersMessage = '당첨 번호를 입력해 주세요.\n';
+    this.bonusNumberMessage = '\n보너스 번호를 입력해 주세요.\n';
   }
 
   async getPurchaseAmountInput() {
     while (true) {
       try {
-        this.purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-        return this.validatePurchaseAmount();
+        const purchaseAmount = await Console.readLineAsync(this.purchaseAmountMessage);
+        this.validatePurchaseAmount(purchaseAmount);
+        return purchaseAmount;
       } catch (e) {
         Console.print(e.message);
       }
-    }
-  }
-
-  validatePurchaseAmount() {
-    if (this.purchaseAmountValidator.validatePurchaseAmount(this.purchaseAmount)) {
-      return this.purchaseAmount;
     }
   }
 
   async getWinningNumbersInput() {
     while (true) {
       try {
-        this.winningNumbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
-        return validateWinningNumbers();
+        const winningNumbers = await Console.readLineAsync(this.winningNumbersMessage);
+        this.validateWinningNumbers(winningNumbers);
+        return winningNumbers;
       } catch (e) {
         Console.print(e.message);
       }
-    }
-  }
-
-  validateWinningNumbers() {
-    if (this.winningNumbersValidator.validateWinningNumbers(this.winningNumbers)) {
-      return this.winningNumbers;
     }
   }
 
   async getBonusNumberInput() {
     while (true) {
       try {
-        this.bonusNumber = await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
-        validateBonusNumber();
+        const bonusNumber = await Console.readLineAsync(this.bonusNumberMessage);
+        this.validateBonusNumber(bonusNumber);
+        return bonusNumber;
       } catch (e) {
         Console.print(e.message);
       }
     }
   }
 
-  validateBonusNumber() {
-    if (this.BonusNumberValidator.validateBonusNumber(this.bonusNumber)) {
-      return this.bonusNumber;
-    }
+  validatePurchaseAmount(purchaseAmount) {
+    this.purchaseAmountValidator.validatePurchaseAmount(purchaseAmount);
+  }
+
+  validateWinningNumbers(winningNumbers) {
+    this.winningNumbersValidator.validateWinningNumbers(winningNumbers);
+  }
+
+  validateBonusNumber(bonusNumber) {
+    this.BonusNumberValidator.validateBonusNumber(bonusNumber);
   }
 }
 
