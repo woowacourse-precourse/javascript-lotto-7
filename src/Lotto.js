d0ce6b33d247b1e55d4cd6a4951fed9190219c47
Murphybread <nargene@gmail.com>
2024-11-02 17:34:34 +0900

    feat(lotto): add get lotto numbers

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,10 +14,13 @@ class Lotto {
     if (new Set(numbers).size !== numbers.length) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
     }
-
+  
   }
 
   // TODO: 추가 기능 구현
+  getLottoNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
