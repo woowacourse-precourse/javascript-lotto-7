19c5b67babe5591e55bb0fbb4c47ead0ea60b213
Ong_Du <pas4976@gmail.com>
2024-11-02 22:31:59 +0900

    fix : 유저 입력 세트 async 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import {Console} from '@woowacourse/mission-utils'
 class App {
   async run() {
-    const userPurchaseAmount = this.getUserPurchaseAmount();
-    const userWinningNumber = this.getUserWinningNumber();
-    const userWinningBonusNumber = this.getUserWinningBonusNumber();
+    const userPurchaseAmount = await this.getUserPurchaseAmount();
+    const userWinningNumber = await this.getUserWinningNumber();
+    const userWinningBonusNumber = await this.getUserWinningBonusNumber();
   }
 
-  getUserPurchaseAmount(){
+  async getUserPurchaseAmount(){
     return Console.readLineAsync('구입금액을 입력해 주세요.');
   }
 
-  getUserWinningNumber(){
+  async getUserWinningNumber(){
     return Console.readLineAsync('당첨 번호를 입력해 주세요.');
   }
 
-  getUserWinningBonusNumber(){
+  async getUserWinningBonusNumber(){
     return Console.readLineAsync('보너스 번호를 입력해 주세요.');
   }
 }
