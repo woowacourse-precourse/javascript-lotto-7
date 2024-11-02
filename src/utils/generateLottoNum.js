147bc1776e537d9c96b89a11192c1d050aed72eb
youngju6143 <youngju6143@naver.com>
2024-11-02 21:36:30 +0900

    chore(const): 하드 코딩한 값 -> const로 변경

--- src/utils/generateLottoNum.js
+++ src/utils/generateLottoNum.js
@@ -2,9 +2,13 @@ import { Random } from "@woowacourse/mission-utils";
 import Lotto from "../Lotto.js";
 
 export function generateLottoNum(count) {
+  const MIN_NUMBER = 1;
+  const MAX_NUMBER = 45;
+  const NUMBER_OF_LOTTO = 6;
+  
   let lottoArray = [];
   for (let i = 0; i < count; i++) {
-    let lotto = new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6));
+    let lotto = new Lotto(Random.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_OF_LOTTO));
     lotto.sort();
     lottoArray.push(lotto);
   }
