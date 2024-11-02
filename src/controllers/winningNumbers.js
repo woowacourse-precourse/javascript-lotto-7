6e02b11f3ad6e2ef336fb98245fef134b2dcb71c
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-02 11:58:05 +0900

    feat: 보너스 번호 일치 여부를 확인하는 checkBonusMatch 함수 추가
    - some 메서드를 이용해 티켓에 보너스 번호가 포함되어 있는지 검증

--- src/controllers/winningNumbers.js
+++ src/controllers/winningNumbers.js
@@ -1,25 +1,16 @@
-import {
-  toPurchaseAmountNumber,
-  parseWinningNumbers,
-  parseBonusNumber,
-} from './utils/parseUserInput.js';
-
-import issueLottoTickets from './controllers/lottoDisplay.js';
-
-// [[],[],[]] lottoTickets
-// 당첨 번호와 로또 비교 winningNumbers
-// 보너스 번호 추가 비교
-// Set
-
-// const set = new Set(arr1);
-// const commonElements = arr2.filter(x => set.has(x));
-
 function checkMatchingNumbers(lottoTickets, winningNumbers) {
-  return lottoTickets.map((ticket) => {
-    const ticket = new Set(ticket);
-    const commonNumbers = winningNumbers.filter((number) => ticket.hax(number));
+  return lottoTickets.map((lottoTicket) => {
+    const lottoTicket = new Set(lottoTicket);
+    const commonNumbers = winningNumbers.filter((number) =>
+      lottoTicket.hax(number)
+    );
+
     return commonNumbers.length;
   });
 }
 
-function checkBonusMatch(lottoTickets, bonusNumber) {}
+function checkBonusMatch(lottoTicket, bonusNumber) {
+  return lottoTicket.some((ticket) => ticket === bonusNumber);
+}
+
+export { checkMatchingNumbers, checkBonusMatch };
