d18a8e8b6230b02e7496dfbc3a0a38a679ab9d91
hyunyeee <5004ohy@naver.com>
2024-11-02 23:56:53 +0900

    refactor: 구입 금액 단위에 대한 유효성 검사를 PriceInputHandler 에서 하지 않도록 변경

--- src/handler/PriceInputHandler.js
+++ src/handler/PriceInputHandler.js
@@ -1,6 +1,5 @@
 import { InputHandler } from "./InputHandler.js";
-import { inValidMessages, viewMessages } from "../constant/message.js";
-import { LOTTO_PRICE } from "../constant/constants.js";
+import { viewMessages } from "../constant/message.js";
 
 export class PriceInputHandler {
   #inputHandler;
@@ -10,16 +9,6 @@ export class PriceInputHandler {
   }
 
   async readPrice() {
-    const price = await this.#inputHandler.readNumber(viewMessages.price);
-    this.#validate(price);
-    return price;
-  }
-
-  #validate(price) {
-    this.#validateDivisible(price);
-  }
-
-  #validateDivisible(price) {
-    if (price % LOTTO_PRICE !== 0) throw new Error(inValidMessages.priceUnit);
+    return await this.#inputHandler.readNumber(viewMessages.price);
   }
 }
