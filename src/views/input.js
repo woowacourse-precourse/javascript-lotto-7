ccbd727f875590df39a0dd27ab70a44ef75bc827
abyss-s <xyz@inha.edu>
2024-11-03 03:10:27 +0900

    refactor(input.js): 화살표 함수로 변경 및 검증 로직 분리

--- src/views/input.js
+++ src/views/input.js
@@ -1,57 +1,24 @@
-// src/views/input.js (1-53)
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { ERROR_MESSAGES } from '../datas/error.js';
 
 export class Input {
   async inputPurchaseAmount() {
-    const price = await MissionUtils.Console.readLineAsync(
-      '구입금액을 입력해 주세요.\n'
-    );
-
-    // 입력값이 숫자로만 구성되어 있는지 확인
-    if (!/^\d+$/.test(price)) {
-      throw new Error(ERROR_MESSAGES.INVALID_PURCHASE_AMOUNT);
-    }
-
-    const amount = Number(price);
-
-    if (amount <= 0 || amount % 1000 !== 0) {
-      throw new Error(ERROR_MESSAGES.INVALID_PURCHASE_AMOUNT);
-    }
-
-    return amount;
+    const price = await this.readLineAsync('구입금액을 입력해 주세요.\n');
+    this.validatePurchaseAmount(price);
+    return Number(price);
   }
 
   async inputWinningNumbers() {
-    const numbers = await MissionUtils.Console.readLineAsync(
-      '당첨 번호를 입력해 주세요.\n'
-    );
-    const winningNumbers = numbers.split(',').map((num) => Number(num.trim()));
-    if (
-      new Set(winningNumbers).size !== 6 ||
-      winningNumbers.some((num) => num < 1 || num > 45)
-    ) {
-      throw new Error(ERROR_MESSAGES.INVALID_WINNING_NUMBERS);
-    }
+    const numbers = await this.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    const winningNumbers = this.parseWinningNumbers(numbers);
+    this.validateWinningNumbers(winningNumbers);
     return winningNumbers;
   }
 
   async inputBonusNumber(winningNumbers = []) {
-    const number = await MissionUtils.Console.readLineAsync(
-      '\n보너스 번호를 입력해 주세요.\n'
-    );
+    const number = await this.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
     const bonusNumber = Number(number);
-    if (
-      bonusNumber < 1 ||
-      bonusNumber > 45 ||
-      winningNumbers.includes(bonusNumber)
-    ) {
-      throw new Error(
-        bonusNumber < 1 || bonusNumber > 45
-          ? ERROR_MESSAGES.INVALID_BONUS_NUMBER
-          : ERROR_MESSAGES.BONUS_DUPLICATE
-      );
-    }
+    this.validateBonusNumber(bonusNumber, winningNumbers);
     return bonusNumber;
   }
 
@@ -65,4 +32,47 @@ export class Input {
       return await this.inputLottoAndBonus();
     }
   }
+
+  readLineAsync = async (message) => {
+    return await MissionUtils.Console.readLineAsync(message);
+  };
+
+  validatePurchaseAmount = (price) => {
+    if (!/^\d+$/.test(price)) {
+      throw new Error(ERROR_MESSAGES.INVALID_PURCHASE_AMOUNT);
+    }
+
+    const amount = Number(price);
+
+    if (amount <= 0 || amount % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGES.INVALID_PURCHASE_AMOUNT);
+    }
+  };
+
+  parseWinningNumbers = (numbers) => {
+    return numbers.split(',').map((num) => Number(num.trim()));
+  };
+
+  validateWinningNumbers = (winningNumbers) => {
+    if (
+      new Set(winningNumbers).size !== 6 ||
+      winningNumbers.some((num) => num < 1 || num > 45)
+    ) {
+      throw new Error(ERROR_MESSAGES.INVALID_WINNING_NUMBERS);
+    }
+  };
+
+  validateBonusNumber = (bonusNumber, winningNumbers) => {
+    if (
+      bonusNumber < 1 ||
+      bonusNumber > 45 ||
+      winningNumbers.includes(bonusNumber)
+    ) {
+      throw new Error(
+        bonusNumber < 1 || bonusNumber > 45
+          ? ERROR_MESSAGES.INVALID_BONUS_NUMBER
+          : ERROR_MESSAGES.BONUS_DUPLICATE
+      );
+    }
+  };
 }
