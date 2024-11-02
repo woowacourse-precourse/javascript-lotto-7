5c0e72bdf243f63a780bdc950608984a7e32ede5
Jinny <ggab.challenge@gmail.com>
2024-11-02 23:46:22 +0900

    fix: 로또 번호 쉼표로 구분하는 경우 삭제

--- src/Lotto.js
+++ src/Lotto.js
@@ -17,11 +17,7 @@ class Lotto {
       throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
     }
 
-    const arr = [...numbers];
-    const onlyNumber = arr.filter((number) => number === ',');
-    if (onlyNumber.length !== 5) {
-      throw new Error('[ERROR] 로또 번호는 쉼표(,) 로 구분되어야 합니다.');
-    }
+
   }
 }
 
