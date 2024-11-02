b2d45a47073a89f37ddb24977c7a6efa564044f9
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-11-03 05:09:17 +0900

    test :refactor test text

--- src/test/statistics.test.js
+++ src/test/statistics.test.js
@@ -1,12 +1,11 @@
 import Statistics from '../utils/Statistics.js';
 
-describe('개수 나누기 ', () => {
+describe('금액별 개수 나누기', () => {
   test('정상 케이스', () => {
-    const input = [123, 1234, 123, 1231, 1234];
+    const input = ['5,000원', '5,000원', '50,000원'];
     const expectedOutput = [
-      { money: 123, count: 2 },
-      { money: 1234, count: 2 },
-      { money: 1231, count: 1 },
+      { money: '5,000원', count: 2 },
+      { money: '50,000원', count: 1 },
     ];
 
     const result = Statistics.countOccurrences(input);
@@ -24,8 +23,8 @@ describe('개수 나누기 ', () => {
   });
 
   test('한 개 있을 때 테스트', () => {
-    const input = [123];
-    const expectedOutput = [{ money: 123, count: 1 }];
+    const input = ['5,000원'];
+    const expectedOutput = [{ money: '5,000원', count: 1 }];
 
     const result = Statistics.countOccurrences(input);
 
@@ -33,8 +32,8 @@ describe('개수 나누기 ', () => {
   });
 
   test('모두 동일한 요소 테스트', () => {
-    const input = [123, 123, 123];
-    const expectedOutput = [{ money: 123, count: 3 }];
+    const input = ['50,000원', '50,000원', '50,000원'];
+    const expectedOutput = [{ money: '50,000원', count: 3 }];
 
     const result = Statistics.countOccurrences(input);
 
