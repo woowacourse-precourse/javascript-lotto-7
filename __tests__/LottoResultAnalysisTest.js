3113f9d82b384cd277d6286332fc1c5ae77c3118
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-02 20:15:32 +0900

    test: 등수에 맞게 현재 당첨 상태를 업데이트 하는 기능 테스트 추가
    - 등수별 당첨 개수 업데이트
    - 총 상금 업데이트

--- __tests__/LottoResultAnalysisTest.js
+++ __tests__/LottoResultAnalysisTest.js
@@ -1,3 +1,4 @@
+import { LOTTO_REWARD } from '../src/Constants/lottoConstants.js';
 import LottoResultAnalysis from '../src/Contoller/LottoResultAnalysis.js';
 import Lotto from '../src/Model/Lotto.js';
 
@@ -16,11 +17,24 @@ describe('LottoResultAnaysis 테스트', () => {
     const FOURTH = new Lotto([1, 2, 3, 4, 9, 10]);
     const FIFTH = new Lotto([1, 2, 3, 9, 10, 11]);
     const NONE = new Lotto([11, 12, 13, 14, 15, 16]);
+
     expect(lottoResultAnalysis.checkRank(FIRST)).toEqual(1);
     expect(lottoResultAnalysis.checkRank(SECOND)).toEqual(2);
     expect(lottoResultAnalysis.checkRank(THIRD)).toEqual(3);
     expect(lottoResultAnalysis.checkRank(FOURTH)).toEqual(4);
     expect(lottoResultAnalysis.checkRank(FIFTH)).toEqual(5);
     expect(lottoResultAnalysis.checkRank(NONE)).toEqual(-1);
+  });
+
+  test('등수에 맞게 당첨 상태와 총 상금의 합을 업데이트 한다.', () => {
+    lottoResultAnalysis.updateWinningStatus(2);
+    lottoResultAnalysis.updateWinningStatus(3);
+
+    expect(lottoResultAnalysis.winningStatus[2]).toBe(1);
+    expect(lottoResultAnalysis.winningStatus[3]).toBe(1);
+
+    const expectedProfitRate = LOTTO_REWARD.SECOND + LOTTO_REWARD.THIRD;
+
+    expect(lottoResultAnalysis.rewardSum).toEqual(expectedProfitRate);
   });
 });
