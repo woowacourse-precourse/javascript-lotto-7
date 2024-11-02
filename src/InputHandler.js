85b653cd75720d2957225c371fe1a1519f54eadc
ZUITOPIA <korhwngml123@naver.com>
2024-11-03 00:17:30 +0900

    docs(InputHandler): 불필요한 주석과 console.log 제거

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -1,18 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class InputHandler {
-    // input 관련 클래스
-
     //  1. 로또 구입 금액 입력받기
-    //  1,000원 단위이므로 1000으로 나눈 몫 구하기 (구한 몫 만큼 로또 게임을 진행하게 됨)
     async getLottoTryCount() {
         const money = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
-        console.log(`입력한 금액: ${money}원`);
-
         this.validateMoney(money);
 
         const lottoCount = Math.floor(money / 1000);
-        console.log(`구매할 로또 개수: ${lottoCount}개`);
         return lottoCount;
     }
 
@@ -25,16 +19,11 @@ class InputHandler {
     }
 
     //  2. 당첨 번호 입력 받기
-    //  입력 시 쉼표를 기준으로 값을 분리하고, 공백도 제거
-    //  중복 제거
-    //  숫자 범위가 1부터 45 사이인지 확인 (0이하 or 46이상이면 ERROR)
     async getJackpotNumbers() {
         const input = await Console.readLineAsync("당첨 번호를 쉼표(,)를 기준으로 구분하여 입력해 주세요.\n");
         const inputjackpot = this.parseNumbers(input);
         const inputjackpotNumbers = this.validateDuplicateNumbers(inputjackpot);
         const jackpotNumbers = this.validateNumberRange(inputjackpotNumbers);
-
-        console.log(`당첨 번호 배열: ${jackpotNumbers}`);
     }
 
     parseNumbers(inputNumbers) {
@@ -63,8 +52,6 @@ class InputHandler {
     }
 
     //  3. 보너스 번호 입력받기
-    //  숫자 범위가 1부터 45 사이인지 확인 (0이하 or 46이상이면 ERROR)
-
     async getBonusNumber() {
         const inputNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
         this.validateNumber(inputNumber);
