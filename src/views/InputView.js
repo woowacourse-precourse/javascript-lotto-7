61ab575c351dd6bffeb24bd67279330ef40879d0
bbjbc <jobc4550@gmail.com>
2024-11-03 00:22:51 +0900

    feat: 입력받는 view와 view에 사용할 상수 추가

---
+++ src/views/InputView.js
@@ -0,0 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_CONSTANTS } from "../utils/ViewConstants";
+
+class InputView {
+  static async inputPurchaseAmount() {
+    const input = await Console.readLineAsync(INPUT_CONSTANTS.PURCHASE_AMOUNT);
+    return input;
+  }
+
+  static async inputWinningNumbers() {
+    const input = await Console.readLineAsync(INPUT_CONSTANTS.WINNING_NUMBERS);
+    return input;
+  }
+
+  static async inputBonusNumber() {
+    const input = await Console.readLineAsync(INPUT_CONSTANTS.BONUS_NUMBER);
+    return input;
+  }
+}
+
+export default InputView;
