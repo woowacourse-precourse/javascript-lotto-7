1cea2deb9024ba9d42cd90e93da83cb3f0ae031d
sanghyeonLee <hyeon012366@gmail.com>
2024-11-03 02:46:59 +0900

    feat: 2차원 배열의 순회를 통한 당첨 통계 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,7 +11,7 @@ class Lotto {
 
     constructor(numbers) {
         this.#validate(numbers);
-        this.#numbers = numbers;
+        this.#numbers = numbers.map((number) => Number(number));
     }
 
     #validate(numbers) {
@@ -20,27 +20,61 @@ class Lotto {
         }
     }
 
-    setStatistics(lottoNumbers, bonusNumber) {
+    setStatistics(lottoes, bonusNumber) { //2 차원 배열
         let matchCnt = 0
         let isBonusNumberMatch = false
-        for (const lottoNumber of lottoNumbers) {
-            if (this.#numbers.includes(lottoNumber)) {
-                matchCnt++
-            }
-            if (this.#numbers.includes(bonusNumber)) {
-                isBonusNumberMatch = true
-            }
+        const statMatch = {
+            three: 0,
+            four: 0,
+            five: 0,
+            bonus: 0,
+            six: 0,
         }
-        return this.resultOutput({matchCnt, isBonusNumberMatch})
+        console.log(this.#numbers)
+        for (const lotto of lottoes) {
+            for (const lottoNum of lotto) {
+                if (this.#numbers.includes(lottoNum)) {
+                    matchCnt++
+                }
+                if (this.#numbers.includes(bonusNumber)) {
+                    isBonusNumberMatch = true
+                }
+            }
+            switch (matchCnt) {
+                case 3:
+                    statMatch.three += matchCnt;
+                    break;
+                case 4:
+                    statMatch.four += matchCnt;
+                    break;
+                case 5:
+                    statMatch.five += matchCnt;
+                    break;
+                case 6:
+                    statMatch.six += matchCnt;
+                    break;
+            }
+            if (matchCnt === 5 && isBonusNumberMatch) {
+                statMatch.bonus += matchCnt
+            }
+            matchCnt = 0
+        }
+        return this.resultOutput(statMatch)
     }
 
-    resultOutput({matchCnt, isBonusNumberMatch}) {
+    resultOutput(statMatch) {
         Console.print(`당첨통계\n---`)
-
+        Object.values(statMatch).map((elem, idx) => {
+            Console.print(obj[idx].content + elem + "개")
+        })
     }
-
-
-    // TODO: 추가 기능 구현
 }
 
+const obj = [
+    {match: "three", content: "3개 일치 (5,000원) - "},
+    {match: "four", content: "4개 일치 (50,000원) -"},
+    {match: "five", content: "5개 일치 (1,500,000원) - "},
+    {match: "bonus", content: "5개 일치, 보너스 볼 일치 (30,000,000원) - "},
+    {match: "six", content: "6개 일치 (2,000,000,000원) - "}
+]
 export default Lotto;
