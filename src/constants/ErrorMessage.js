3d171bf0700da3798fb5c7c3273a35cb6b101ade
H-sooyeon <wndqhr__@naver.com>
2024-11-02 19:35:09 +0900

    refactor: 사용하지 않는 메시지 상수 제거

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -7,12 +7,6 @@ export default {
   INVALID_LOTTO_NUMBER: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
   INVALID_LOTTO_NUMBER_RANGE:
     '[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다',
-  LOTTO_NUMBERS: '[ERROR] 당첨 번호를 입력해 주세요.',
-  INVALID_WINNING_LOTTO_NUMBERS:
-    '[ERROR] 당첨 번호는 1부터 45까지의 숫자로 입력해주세요.',
-  INVALID_WINNING_LOTTO_NUMBER_LENGTH:
-    '[ERROR] 당첨 번호는 6개를 입력해주세요.',
-  INVALID_WINNING_LOTTO__DELIMITER: '[ERROR] 쉼표(,)로 구분해주세요.',
   BONUS_NUMBER: '[ERROR] 보너스 번호를 입력해 주세요.',
   INVALID_WINNING_BONUS_NUMBER:
     '[ERROR] 보너스 번호는 1부터 45까지의 숫자로 입력해주세요.',
@@ -20,5 +14,4 @@ export default {
     '[ERROR] 보너스 번호는 1개를 입력해주세요.',
   DUPLICATE_WINNING_NUMBERS:
     '[ERROR] 당첨 번호와 보너스 번호는 모두 중복되지 않게 입력해주세요.',
-  INVALID_INPUT: '[ERROR] 5번 이상 잘못된 입력을 하셨습니다.',
 };
