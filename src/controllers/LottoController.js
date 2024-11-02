eecf54c1bcca1249b28277f30ddc8f1ec34689a7
devjaeyoon <devjaeyoon@gmail.com>
2024-11-02 19:52:42 +0900

    feat(LottoController): 사용자 보너스 번호 입력 메소드 추가

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -1,3 +1,4 @@
+import BounsNumber from '../models/BonusNumber.js';
 import Lotto from '../models/Lotto.js';
 import Money from '../models/Money.js';
 import InputView from '../views/InputView.js';
@@ -7,10 +8,13 @@ class LottoController {
   #lottoCount;
   #lottos;
   #winningNumbers;
+  #bonusNumber;
 
   async run() {
     await this.getLottoCount();
     await this.getLotto();
+    await this.getWinningNumbers();
+    await this.getBonusNumber();
   }
 
   async getLottoCount() {
@@ -29,6 +33,12 @@ class LottoController {
   async getWinningNumbers() {
     const winningNumbersInput = await InputView.readLineAsync('당첨 번호를 입력해 주세요.\n');
     this.#winningNumbers = Lotto.convertInputToNumbers(winningNumbersInput);
+    OutputView.printNewLine();
+  }
+
+  async getBonusNumber() {
+    const bonusNumberInput = await InputView.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    this.#bonusNumber = new BounsNumber(this.#winningNumbers, bonusNumberInput).bonusNumber;
     OutputView.printNewLine();
   }
 }
