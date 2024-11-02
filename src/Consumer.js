392c2c46c1e475cf27f6724e04690669812ec4f7
dudcns02 <duddl2460@naver.com>
2024-11-02 15:36:37 +0900

    chore: Correct typo

--- src/Consumer.js
+++ src/Consumer.js
@@ -8,34 +8,34 @@ class Consumer {
   isNumber() {
     // 보너스 번호 검사도 trim()사용해서 여기서 함
     if (!/^[0-9]+$/.test(price)) {
-      throw new Error(ERROR_MESSAGE.IS_NUMBER);
+      throw new Error(NUMBER_INPUT_ERROR_MESSAGE.IS_NUMBER);
     }
   }
 
   isUnitOfPrice() {
     if (!(price % 1000 === 0)) {
-      throw new Error(ERROR_MESSAGE.PRICE_INCORRECT);
+      throw new Error(PRICE_ERROR_MESSAGE.PRICE_INCORRECT);
     }
   }
 
   minPrice() {
     if (price < 1000) {
-      throw new Error(ERROR_MESSAGE.MIN_PRICE);
+      throw new Error(PRICE_ERROR_MESSAGE.MIN_PRICE);
     }
   }
 
   maxPrice() {
     if (100000 < price) {
-      throw new Error(ERROR_MESSAGE.MAX_PRICE);
+      throw new Error(PRICE_ERROR_MESSAGE.MAX_PRICE);
     }
   }
 
-  lottoCount() {
-    lottoNumber = price / 1000;
+  getLottoCount() {
+    lottoCount = price / 1000;
   }
 
   getOrderLotto() {
-    for (i = 0; i < a; i++) {
+    for (i = 0; i < lottoCount; i++) {
       randomNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
       lotto = new Lotto(randomNumber);
       this.#lottoTickets.push(lotto);
@@ -44,14 +44,14 @@ class Consumer {
   ///당첨번호 유효성
   CheckWinningNumberInput() {
     if (!/^[0-9]+(,\s*[0-9]+)*$/.test(winningNumber)) {
-      throw new Error(WINNING_NUMBER_ERROR_MESSAGE.ONLY_NUMBER_COMMA);
+      throw new Error(NUMBER_INPUT_ERROR_MESSAGE.ONLY_NUMBER_COMMA);
     }
   }
 
   isWinningNumberLength() {
     const maxNum = 6;
     if (winningNumber.length !== maxNum) {
-      throw new Error(WINNING_NUMBER_ERROR_MESSAGE.INCORRECT_COUNT_NUMBER);
+      throw new Error(NUMBER_INPUT_ERROR_MESSAGE.INCORRECT_NUMBER_COUNT);
     }
   }
 
@@ -65,14 +65,7 @@ class Consumer {
   isduplicateNumber() {
     checkduplicate = new set(winningNumber);
     if (checkduplicate.size !== winningNumber.length) {
-      throw new Error(WINNING_NUMBER_ERROR_MESSAGE.DUPLICATE_NUMBER);
-    }
-  }
-
-  // 보너스 번호 유효성 검사
-  checkBonusNumber() {
-    if (!/^[0-9\s]+$/.test(bonusNumber)) {
-      throw new Error();
+      throw new Error(NUMBER_INPUT_ERROR_MESSAGE.DUPLICATE_NUMBER);
     }
   }
 }
--- src/Contents.js
+++ src/Contents.js
@@ -17,15 +17,15 @@ const rateOfReturn = (c) => ({
 });
 
 const PRICE_ERROR_MESSAGE = Object.freeze({
-  IS_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
   PRICE_INCORRECT: "[ERROR] 1,000원 단위만 구매 가능합니다.",
   MIN_PRICE: "[ERROR] 최소 단위는 1,000원입니다.",
   MAX_PRICE: "[ERROR] 최대 단위는 100,000원입니다.",
 });
 
 const NUMBER_INPUT_ERROR_MESSAGE = Object.freeze({
-  INCORRECT_COUNT_NUMBER: "[ERROR] 쉼표로 구분한 숫자 6개를 입력해 주세요.",
+  IS_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
+  INCORRECT_NUMBER_COUNT: "[ERROR] 쉼표로 구분한 숫자 6개를 입력해 주세요.",
   ONLY_NUMBER_COMMA: "[ERROR] 숫자와 쉼표만 입력 가능합니다.",
-  NUMBER_RANGE: "[ERROR] 1 ~ 45 사이의 숫자만 입력해 주세요..",
+  NUMBER_RANGE: "[ERROR] 1 ~ 45 사이의 숫자만 입력해 주세요.",
   DUPLICATE_NUMBER: "[ERROR] 중복되지 않는 숫자만 입력해 주세요.",
 });
