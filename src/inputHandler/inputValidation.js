e62dd49c35835d0f395d4e70b15514f1c799dd2b
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-11-02 17:03:31 +0900

    feat: 로또 당첨번호 input 예외처리

--- src/inputHandler/inputValidation.js
+++ src/inputHandler/inputValidation.js
@@ -1,18 +1,30 @@
 export function purchaseAmountValidation(purchaseAmount) {
   if (!purchaseAmount.trim()) throw '구매금액은 1000단위의 숫자만 입력해주세요';
-  if (!isNaN(purchaseAmount)) throw '구매금액은 1000단위의 숫자만 입력해주세요';
+  if (isNaN(purchaseAmount)) throw '구매금액은 1000단위의 숫자만 입력해주세요';
   if (Number(purchaseAmount) % 1000)
     throw '구매금액을 1000 단위로 입력해주세요';
 
   return Number(purchaseAmount) / 1000;
 }
 
-export function winningLottoValidation(winningLottoArray) {
-  if (
-    winningLottoArray.filter((lottoNum) => !lottoNumberValidation(lottoNum))
-      .length > 0
-  )
-    throw '로또 번호는 1 ~ 45까지의 숫자만 가능합니다.';
+export function winningLottoValidation(winningLottoInput) {
+  if (!winningLottoInput.trim())
+    throw '당첨 번호 6자리 숫자를 쉽표로 구분하여 입력해 주세요';
+  const winningLottoArray = winningLottoInput.split(',');
+  winningLottoArray.map((lottoNum) => {
+    console.log(lottoNum);
+    if (isNaN(lottoNum)) throw '당첨번호는 숫자만 입력해주세요';
+    if (!lottoNumberValidation(lottoNum))
+      throw '당첨번호는 1 ~ 45까지의 숫자만 가능합니다.';
+  });
   if (winningLottoArray.length !== 6) throw '당첨번호는 6자리로 입력해주세요';
-  return testArr;
+  const winningLottoDuplicateValid = new Set(winningLottoArray);
+  if (winningLottoDuplicateValid.size !== 6) throw '번호는 중복될 수 없습니다.';
+  return winningLottoArray;
+}
+
+function lottoNumberValidation(lottoNumber) {
+  if (Number(lottoNumber) < 1 || Number(lottoNumber) > 45) return false;
+  if (Number(lottoNumber) % 1) return false;
+  return true;
 }
