1d6cf1492ba536336cf1c25951524692f21a37a0
salmonco <xkqjsslsek80@naver.com>
2024-11-02 17:49:03 +0900

    feat(app): 당첨 번호를 쉼표(,)를 기준으로 구분

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ class App {
     const price = await this.readPrice();
 
     // - 당첨 번호 6개를 입력받는다.
-    const winningNumbersString = await this.readWinningNumbers();
+    const winningNumbersString = await this.readWinningNumbersString();
 
     // - 당첨 번호를 쉼표(,)를 기준으로 구분한다.
+    const winnigNumbersArray = this.getWinningNumberArray(winningNumbersString);
+
     // - 보너스 번호 1개를 입력받는다.
     // - 1~45 사이의 중복되지 않는 6개 숫자를 랜덤으로 뽑는다.
     // - 구입 금액에 따라 발행할 로또 개수가 몇 개인지 구한다.
@@ -26,8 +28,12 @@ class App {
     return Console.readLineAsync('구입금액을 입력해 주세요.\n');
   }
 
-  readWinningNumbers() {
+  readWinningNumbersString() {
     return Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+  }
+
+  getWinningNumberArray(numbersString) {
+    return numbersString.split(',').map(Number);
   }
 }
 
