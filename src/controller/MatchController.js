55efdf9ed98f5fe07c539211820f679f14826905
seonae-j <wkdtjsdo135@naver.com>
2024-11-02 15:52:04 +0900

    feat(MatchController): 사용자에게 당첨 번호를 입력받고 Lotto 클래스로 유효성 검사 기능 추가

--- src/controller/MatchController.js
+++ src/controller/MatchController.js
@@ -1,11 +1,25 @@
 import { INPUT } from '../constants/Constants.js';
+import { Console } from '@woowacourse/mission-utils';
+import Lotto from '../Lotto.js';
 
 class MatchController {
-  constructor() {}
+  constructor() {
+    this.jackpot = [];
+  }
+
+  async setJackpot() {
+    this.jackpot = await Console.readLineAsync(INPUT.JACKPOT);
+    this.jackpot = this.jackpot.split(',').map(Number);
+    this.validateJackpot(this.jackpot);
+  }
 
   async setBonusJackpot() {
     const bonusNumber = await Console.readLineAsync(INPUT.BONUS);
     return bonusNumber;
+  }
+
+  validateJackpot(numbers) {
+    return new Lotto(numbers);
   }
 }
 
