e2483a6fc07a132d1c089812415ad58dc8732615
moonseonghui <msh9980@naver.com>
2024-11-03 05:14:54 +0900

    test(WinningLottoTest): 유효하지 않은 로또 번호로 WinningLotto 생성 테스트 구현

--- __tests__/WinningLottoTest.js
+++ __tests__/WinningLottoTest.js
@@ -17,6 +17,7 @@ const expectInvalidBonusNumber = (winningLotto, bonusNumber, error) => {
   }).toThrow(error);
 };
 
+// 유효한 로또 번호로 WinningLotto 생성 및 보너스 번호 유효성 테스트
 describe.each([
   { numbers: [1, 2, 3, 4, 5, 6], validBonus: 7, duplicateNumber: 6 },
   { numbers: [15, 26, 33, 35, 40, 44], validBonus: 1, duplicateNumber: 15 },
@@ -38,5 +39,21 @@ describe.each([
     ])('[유효하지 않은 보너스 번호 설정]', ({ bonusNumber, error }) => {
       expectInvalidBonusNumber(winningLotto, bonusNumber, error);
     });
+  },
+);
+
+// 유효하지 않은 로또 번호로 WinningLotto 생성 테스트
+test.each([
+  { numbers: [1, 2, 3, 4, 5], error: ERROR_MESSAGES.INVALID_LOTTO_NUMBER_COUNT },
+  { numbers: [1, 2, 3, 4, 5, 6, 7], error: ERROR_MESSAGES.INVALID_LOTTO_NUMBER_COUNT },
+  { numbers: [1, 2, 3, 4, 5, 5], error: ERROR_MESSAGES.DUPLICATE_WINNING_NUMBER },
+  { numbers: [0, 2, 3, 4, 5, 6], error: ERROR_MESSAGES.INVALID_LOTTO_RANGE },
+  { numbers: [1, 2, 3, 4, 5, 46], error: ERROR_MESSAGES.INVALID_LOTTO_RANGE },
+])(
+  '[유효하지 않은 로또 번호] WinningLotto 생성 시 예외가 발생한다 : $error',
+  ({ numbers, error }) => {
+    expect(() => {
+      new WinningLotto(numbers);
+    }).toThrow(error);
   },
 );
