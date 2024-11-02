aa4c93343310c32f03693ddf445c35934b1269d2
KimYoungWoo <rladuddn3924@gmail.com>
2024-11-02 15:58:59 +0900

    fix: execute-message 수정
    필요 없는 상수 값 제거
    PRIZE.STATISTIC의 파라미터 변경

--- src/util/messages/execute-message.js
+++ src/util/messages/execute-message.js
@@ -1,14 +1,11 @@
 const EXECUTE_MESSAGE = {
   LOTTO: {
     RECEIPT: (count) => Object.freeze(`${count}개를 구매했습니다.`),
-    NUMBERS: (numbers) => Object.freeze(numbers),
   },
   PRIZE: {
-    STATISTIC: ({ matchingNumber, price, count, isBonus }) =>
+    STATISTIC: ({ matchingNumber, price, count, bonusWord }) =>
       Object.freeze(
-        `${matchingNumber}개 일치${
-          isBonus ?? ", 보너스 볼 일치"
-        } (${price}원) - ${count}개`
+        `${matchingNumber}개 일치${bonusWord} (${price}원) - ${count}개`
       ),
     TOTAL_RATE: (rate) => Object.freeze(`총 수익률은 ${rate}%입니다.`),
   },
