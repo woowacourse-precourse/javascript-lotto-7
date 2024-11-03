5c3db9dda9648308acb032be7ec954ec75c7894a
keemsebeen <sebin0580@gmail.com>
2024-11-03 23:07:23 +0900

    feat(constants) : LOTTO_VALUES, PURCHASE OUTPUT 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -4,6 +4,16 @@ export const INPUT = {
   BONUS: "보너스 번호를 입력해 주세요.\n",
 };
 
+export const OUTPUT = {
+  PURCHASE: "개를 구매했습니다.",
+};
+
+export const LOTTO_VALUES = {
+  MIN: 1,
+  MAX: 45,
+  COUNT: 6,
+};
+
 export const PRICE_NUMBER = 1000;
 export const RATE = "당첨 통계";
 export const SECTION = "---";
