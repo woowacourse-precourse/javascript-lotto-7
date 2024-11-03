5fe998d28b7ac9a0bc8521eb980900781954aeda
df2854 <df2854@icloud.com>
2024-11-03 20:11:43 +0900

    refactor: 에러 메시지 상수화

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const ERROR = {
+    AMOUNT_INVALID : '[ERROR] 1000원 단위로 입력하세요.',
+    WIN_NUMBER_COUNT : '[ERROR] 로또 번호는 6개여야 합니다.',
+    WIN_NUMBER_RANGE : '[ERROR] 로또 번호는 1~45여야 합니다.',
+    WIN_NUMBER_DUPLICATE : '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
+    BONUS_NUMBER_NOT_NUMBER: '[ERROR] 숫자를 입력하세요.',
+    BONUS_NUMBER_DUPLICATE: '[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.',
+    BONUS_NUMBER_RANGE: '[ERROR] 보너스 번호는 1~45여야 합니다.'
+};
--- src/validation.js
+++ src/validation.js
@@ -1,29 +1,31 @@
+import { ERROR } from "./constants.js";
+
 export function checkAmount(input) {
     if (isNaN(input) || input % 1000 !== 0) {
-        throw new Error('[ERROR] 1000원 단위로 입력하세요.');
+        throw new Error(ERROR.AMOUNT_INVALID);
     }
 }
 
 export function checkWinNumbers(input) {
     if (input.length !== 6) {
-        throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+        throw new Error(ERROR.WIN_NUMBER_COUNT);
     }
     if (!input.every(a => a >= 1 && a <= 45)) {
-        throw new Error('[ERROR] 로또 번호는 1~45여야 합니다.');
+        throw new Error(ERROR.WIN_NUMBER_RANGE);
     }
     if ([...new Set(input)].length !== 6) {
-        throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+        throw new Error(ERROR.WIN_NUMBER_DUPLICATE);
     }
 }
 
 export function checkBonusNumber(input, winNumbers) {
     if (isNaN(input)) {
-        throw new Error('[ERROR] 숫자를 입력하세요.');
+        throw new Error(ERROR.BONUS_NUMBER_NOT_NUMBER);
     }
     if (winNumbers.includes(input)) {
-        throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.');
+        throw new Error(ERROR.BONUS_NUMBER_DUPLICATE);
     }
     if (!(input >= 1 && input <= 45)) {
-        throw new Error('[ERROR] 보너스 번호는 1~45여야 합니다.');
+        throw new Error(ERROR.BONUS_NUMBER_RANGE);
     }
 }
