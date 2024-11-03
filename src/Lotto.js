21ca328ae7b95369146ad577e7bb372f6c58bd5e
jeongwoo903 <www8565@naver.com>
2024-11-04 02:04:01 +0900

    refactor(Lotto): 로또 검증 매서드 리팩토링

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import LottoValidator from './domain/LottoValidator.js';
+
 class Lotto {
   #numbers;
 
@@ -7,9 +9,8 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
+    const lottoValidator = new LottoValidator();
+    lottoValidator.validateLottoNumber(numbers);
   }
 
   getLottoNumbers() {
