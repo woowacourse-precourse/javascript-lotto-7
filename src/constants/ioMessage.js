dc7a359f67829a9e550acc3ce035764bd955991b
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-04 02:57:22 +0900

    chore: 입력 메시지 상수화

---
+++ src/constants/ioMessage.js
@@ -0,0 +1,7 @@
+export const INPUT_MESSAGE = Object.freeze({
+  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.",
+  WINNING_NUMBER: "당첨 번호를 입력해 주세요.",
+  BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+});
+
+export const OUTPUT_MESSAGE = Object.freeze({});
