219e7a570a674eeca9ed38a12aa6d3a16e5bcd89
taeyomi <terry11ty@naver.com>
2024-11-03 20:32:12 +0900

    feat(LottoController): 로또 발행 이후 출력 메서드 연결

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,17 +1,24 @@
 import LottoView from "../view/LottoView.js";
 import { validatePurchasingAmount } from "../validation.js";
+import LottoGenerator from "../model/LottoGenerator.js";
 class LottoController {
   constructor() {
     this.view = new LottoView();
+    this.generator = new LottoGenerator();
   }
 
   async init() {
-    await this.getLottoAmounts();
+    await this.setLottoAmounts();
   }
 
   async setLottoAmounts() {
     try {
-      await this.getLottoAmounts();
+      const purchaseAmount = await this.getLottoAmounts();
+      const lottoCount = this.calculateLotto(purchaseAmount);
+      this.generator.generateLottos(lottoCount);
+      const lottos = this.generator.getLottos();
+      this.view.printGetLottos(lottoCount);
+      this.view.printLottos(lottos);
     } catch (error) {
       throw error;
     }
@@ -20,10 +27,16 @@ class LottoController {
   async getLottoAmounts() {
     const userInputAmounts = await this.view.inputLottoAmount();
     this.checkLottoAmounts(userInputAmounts);
+    return Number(userInputAmounts);
   }
-
+  //입력숫자 검증
   checkLottoAmounts(userInputAmounts) {
     validatePurchasingAmount(userInputAmounts);
+  }
+
+  //로또 개수 계산
+  calculateLotto(count) {
+    return Math.floor(count / 1000);
   }
 }
 
