1aa8a23f573d6a5e2787038108adbb13d926ba81
jeheecheon <jeheecheon@gmail.com>
2024-11-04 03:08:32 +0900

    chore: 사용자에게 쿼리 메시지 출력 시, 개행되도록 수정
    쿼리 메시지 출력 시 개행되도록 메시지 끝에 개행 문자 추가

--- src/constants/userMessages.js
+++ src/constants/userMessages.js
@@ -1,7 +1,7 @@
 const QUERIES = Object.freeze({
-  PAYMENT_AMOUNT: '구입금액을 입력해 주세요.',
-  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
-  BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  PAYMENT_AMOUNT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
 });
 
 const ANSWERS = Object.freeze({
