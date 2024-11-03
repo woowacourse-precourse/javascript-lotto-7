bb286a544f8e4a9a45be2b52b4d5e36cc9b9f5bb
dlsxjzld <sinjuk1@naver.com>
2024-11-03 21:44:15 +0900

    feat(App.js): 로또 구입 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const lottoBuyPrice =
+      await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  }
 }
 
 export default App;
