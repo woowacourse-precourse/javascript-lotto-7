fa5165f3a631a9992a95a017e0392df2470aa6de
immaemi <nam14785@soongsil.ac.kr>
2024-11-04 03:51:05 +0900

    test: Add Lotto class tests - Add matchNumbers unit test

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,14 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("일치하는 로또 번호의 개수를 반환한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.matchNumbers([1, 2, 3])).toBe(3);
+  });
 });
