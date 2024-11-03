2c47d9e214231f53f89f81b5917f8fa531215d57
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-04 01:57:28 +0900

    🐛 fix: Lotto import 위치 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,4 @@
-import Lotto from '../src/controller/Lotto';
+import Lotto from '../src/Lotto';
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -13,7 +13,6 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
   test('로또 번호는 1~45 사이의 숫자여야 한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 50, 5]);
