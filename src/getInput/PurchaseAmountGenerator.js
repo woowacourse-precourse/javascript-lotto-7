a0d34f2829b1a37981fc5727bd9760171664f2c7
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-11-03 23:50:03 +0900

    feat: 구입금액 입력 및 유효성 검사 추가
    - 구입 금액 입력
    - NumverValidator 클래스의 메서드로 구입금액 유효성 검사
    - 에러 시 재입력

---
+++ src/getInput/PurchaseAmountGenerator.js
@@ -0,0 +1,37 @@
+import { Console } from '@woowacourse/mission-utils';
+import NumberValidator from '../utils/NumberValidator.js';
+
+class PurchaseAmountGenerator {
+  #purchaseAmount;
+
+  constructor(number) {
+    this.#validatePurchaseAmount(number);
+    this.#purchaseAmount = number;
+  }
+
+  static async getPurchaseAmount() {
+    try {
+      const purchaseAmount =
+        await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+      Console.print('');
+      const validator = new PurchaseAmountGenerator(purchaseAmount);
+      return validator.#purchaseAmount;
+    } catch (error) {
+      Console.print(error.message);
+      return PurchaseAmountGenerator.getPurchaseAmount();
+    }
+  }
+
+  #validatePurchaseAmount(purchaseAmount) {
+    NumberValidator.validateIsEmpty(purchaseAmount);
+    NumberValidator.validateIsNumber(purchaseAmount);
+    NumberValidator.validateNoDecimal(purchaseAmount);
+
+    const parsedAmount = parseFloat(purchaseAmount);
+    NumberValidator.validateIsInteger(parsedAmount);
+    NumberValidator.validateIsPositive(parsedAmount);
+    NumberValidator.validateDivisibleBy1000(parsedAmount);
+  }
+}
+
+export default PurchaseAmountGenerator;
