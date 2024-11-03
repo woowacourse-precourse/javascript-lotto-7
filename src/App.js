a90d8796219d1fb008b67bda6a97f2ed2fe59de5
jeong922 <hwajeong922@naver.com>
2024-11-03 22:25:49 +0900

    feat(App): private 필드 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
-import Validator from './Validator.js';
 import ReturnRateCalculator from './ReturnRateCalculator.js';
 import LottoManager from './lottoManager.js';
 
 class App {
+  #lottoManager;
+  #returnRateCalculator;
+
   constructor() {
-    this.validator = new Validator();
-    this.lottoManager = new LottoManager();
-    this.returnRateCalculator = new ReturnRateCalculator();
+    this.#lottoManager = new LottoManager();
+    this.#returnRateCalculator = new ReturnRateCalculator();
   }
 
   async run() {
     try {
-      const purchaseAmount = await this.lottoManager.getPurchaseAmount();
-      this.lottoManager.createLottoNumbers(purchaseAmount);
-      const winningNumbers = await this.lottoManager.getWinningNumbers();
-      const matchResult = this.lottoManager.getMatchResult(winningNumbers);
-      this.lottoManager.printLog(matchResult);
+      const purchaseAmount = await this.#lottoManager.getPurchaseAmount();
+      this.#lottoManager.createLottoNumbers(purchaseAmount);
+      const winningNumbers = await this.#lottoManager.getWinningNumbers();
+      const matchResult = this.#lottoManager.getMatchResult(winningNumbers);
+      this.#lottoManager.printLog(matchResult);
 
-      const returnRate = this.returnRateCalculator.calculateReturnRate(matchResult, purchaseAmount);
+      const returnRate = this.#returnRateCalculator.calculateReturnRate(matchResult, purchaseAmount);
 
       Console.print(`총 수익률은 ${returnRate}%입니다.`);
     } catch (error) {
