9503467afc692a76112657f6f921fa5515923790
Ko Daeun <drechsu@gmail.com>
2024-11-03 19:47:06 +0900

    feat(getInput): 보너스 번호 입력받는 함수 추가

---
+++ src/getInput/getBonusNumber.js
@@ -0,0 +1,7 @@
+import { Console } from "@woowacourse/mission-utils";
+export default async function getBonusNumber() {
+  const bonusNumber = await Console.readLineAsync(
+    "보너스 번호를 입력해 주세요.\n"
+  );
+  return bonusNumber;
+}
