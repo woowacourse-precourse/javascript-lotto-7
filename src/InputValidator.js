e8485c99735048ed4a728d32ad4ab009e5a2c09f
kochungwon <yud01063@gmail.com>
2024-11-03 19:32:50 +0900

    fix(InputValidator): InputValidator 유효성 검사 개선
    - 구입 금액 입력 시 null 또는 빈 문자열 체크 추가
    - 구입 금액을 숫자로 변환 후 유효성 검사
    - 당첨 번호 입력 시 배열 형식과 비어있지 않음 확인 추가
    - 보너스 번호 입력 시 null 또는 빈 문자열 체크 추가
    - 보너스 번호를 숫자로 변환 후 유효성 검사

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,17 +1,34 @@
 class InputValidator {
-    //구입금액 유효성 검사
+    // 구입금액 유효성 검사
     validateAmount(purchaseAmount) {
-        if (purchaseAmount%1000 !== 0) {
+        if (purchaseAmount == null || purchaseAmount === "") {
+            throw new Error("[ERROR] 구입 금액은 입력해야 합니다.");
+        }
+        console.log(purchaseAmount);
+        
+        purchaseAmount = Number(purchaseAmount);
+        console.log(purchaseAmount);
+        console.log(isNaN(purchaseAmount));
+        
+        if (isNaN(purchaseAmount)) {
+            throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+        
+        if (purchaseAmount % 1000 !== 0) {
             throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
-        if (purchaseAmount<= 1000) {
+        if (purchaseAmount < 1000) {
             throw new Error("[ERROR] 구입 금액은 1,000원 이상이여야 합니다.");
         }
+        
         return true; 
     }
     
-    //당첨번호 유효성 검사
+    // 당첨번호 유효성 검사
     validateWinningNumbers(winningNumbers) {
+        if (!winningNumbers || !Array.isArray(winningNumbers) || winningNumbers.length === 0) {
+            throw new Error("[ERROR] 당첨 번호는 입력해야 하며, 배열 형식이어야 합니다.");
+        }
         if (winningNumbers.length !== 6) {
             throw new Error("[ERROR] 당첨번호는 6개를 입력하셔야 합니다.");
         }
@@ -25,10 +42,22 @@ class InputValidator {
         if (hasDuplicates) {
             throw new Error("[ERROR] 당첨번호에는 중복된 숫자가 없어야 합니다.");
         }
+
+        return true; 
     }
 
     // 보너스 번호 유효성 검사
     validateBonusNumber(bonusNumber, winningNumbers) {
+        if (bonusNumber == null || bonusNumber === "") {
+            throw new Error("[ERROR] 보너스 번호는 입력해야 합니다.");
+        }
+
+        bonusNumber = Number(bonusNumber); 
+
+        if (isNaN(bonusNumber)) {
+            throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
+
         if (bonusNumber < 1 || bonusNumber > 45) {
             throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
@@ -36,7 +65,9 @@ class InputValidator {
         if (winningNumbers.includes(bonusNumber)) {
             throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
+
+        return true; 
     }
 }
 
-export default InputValidator
+export default InputValidator;
