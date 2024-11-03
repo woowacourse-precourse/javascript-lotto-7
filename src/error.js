9609e955faf1c81280a0678608e96f7a1740ebad
jennaaaaaaaaa <ggm4954@gmail.com>
2024-11-03 11:17:07 +0900

    fix: 에러 메세지 수정

--- src/error.js
+++ src/error.js
@@ -1,7 +1,7 @@
 export const ERR_MSG = {
    ERR_PURCHASE_AMOUNT: '[ERROR] 숫자를 입력해주세요.',
    ERR_AMOUNT_UNITS: '[ERROR] 1000단위의 금액을 입력해주세요.',
-   ERR_NUMBERS_COUNT: '[ERROR] 6개의 번호를 입력해주세요.(번호는 쉼표(,)를 기준으로 구분)',
+   ERR_NUMBERS_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
    ERR_NUMBERS_RANGE: '[ERROR] 1 ~ 45 범위의 숫자만 입력해주세요.',
    ERR_ONLYONE_NUMBER: '[ERROR] 숫자 하나만 입력해주세요',
 };
