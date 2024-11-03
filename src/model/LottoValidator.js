75fbdf8112473d2a93939f5b2380e00ac9f7e1a8
ChanLee_KR <cl700120@gmail.com>
2024-11-03 22:50:59 +0900

    feat: 로또 번호 타입 검증 함수 구현

--- src/model/LottoValidator.js
+++ src/model/LottoValidator.js
@@ -5,6 +5,16 @@ class LottoValidator {
   static isCorrectLength({ length }) {
     return length === LOTTO_NUMBER_LENGTH;
   }
+
+  /** @param {number[]} numbers */
+  static isSafeNumberAll(numbers) {
+    return numbers.every(LottoValidator.isSafeNumber);
+  }
+
+  /** @param {number} n */
+  static isSafeNumber(n) {
+    return !Number.isNaN(n) && Number.isSafeInteger(n);
+  }
 }
 
 export default LottoValidator;
