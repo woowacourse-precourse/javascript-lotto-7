51ca8bbf3633b69743235321694c4083d466c229
전성모 <jeonsm22@daum.net>
2024-11-03 18:55:12 +0900

    feat: 구매 금액이 숫자로 이루어져 있는지 확인하는 함수 추가 및 1000으로 isDivided1000 함수 수정

--- src/buyPrice.js
+++ src/buyPrice.js
@@ -6,12 +6,17 @@ function inputPrice(){
     const PRICE = Console.readLineAsync();
 }
 
+function isNumber(PRICE){
+    return /^[0-9]+$/.test(PRICE);
+}
+
 function isDivided1000(PRICE){
     const DividedPrice = PRICE / 1000;
     if(DividedPrice === Math.floor(DividedPrice)){
         return true;
     }
-    return false;
+    
+    throw new Error("[ERROR] : 1000으로 나누어 떨어지지 않습니다.");
 }
 
 export {inputPrice};
