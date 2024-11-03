5e43dcd7f1d65ed728d0c396e947059479f5ad5b
ChanLee_KR <cl700120@gmail.com>
2024-11-03 23:38:26 +0900

    feat: 당첨 번호 범위 검증 기능 구현

--- src/model/LottoGame.js
+++ src/model/LottoGame.js
@@ -1,5 +1,5 @@
 import { Exception } from '../Utils.js';
-import { LOTTO_NUMBER_LENGTH } from '../Constants.js';
+import { LOTTO_NUMBER_LENGTH, LOTTO_NUMBER_RANGE } from '../Constants.js';
 import LottoValidator from './LottoValidator.js';
 
 class LottoGame {
@@ -16,6 +16,7 @@ class LottoGame {
   static validateWinningNumbers(numbers) {
     LottoGame.#validateWinningNumbersType(numbers);
     LottoGame.#validateWinningNumbersLength(numbers);
+    LottoGame.#validateWinningNumbersRange(numbers);
   }
 
   /** @param {number[]} numbers */
@@ -29,6 +30,15 @@ class LottoGame {
   static #validateWinningNumbersLength(numbers) {
     if (numbers.length !== LOTTO_NUMBER_LENGTH) {
       throw new Exception(`당첨 번호는 ${LOTTO_NUMBER_LENGTH}개여야 합니다.`);
+    }
+  }
+
+  /** @param {number[]} numbers */
+  static #validateWinningNumbersRange(numbers) {
+    const [MIN, MAX] = LOTTO_NUMBER_RANGE;
+
+    if (!LottoValidator.isInRangeAll(numbers)) {
+      throw new Exception(`당첨 번호는 ${MIN}에서 ${MAX}사이여야 합니다.`);
     }
   }
 }
