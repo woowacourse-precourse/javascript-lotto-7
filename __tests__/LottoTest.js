cef6234a50478fe020784f2b048a2bc2efe058d7
young24n <won8560@gmail.com>
2024-11-04 00:08:07 +0900

    test: 로또 번호 출력 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,16 @@
 import Lotto from '../src/Lotto';
 
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe('로또 클래스 테스트', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   test('로또 번호의 개수가 6개가 아니면 예외 발생.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -11,13 +21,21 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR] 로또 번호가 6자리보다 적게 생성됨!');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR] 로또 번호에 중복된 값이 있음!');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('로또 번호를 출력', () => {
+    expect(() => {
+    const logSpy = getLogSpy();
+
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.printNumbers();
+
+    expect(logSpy).toHaveBeenCalledWith("[1, 2, 3, 4, 5, 6]")
+    })
+  })
 
 });
