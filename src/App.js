755116fc45051b96931f6258592b660b0c0a175e
@u_limit_ <yurim@ajou.ac.kr>
2024-11-04 05:13:03 +0900

    feat: 수익률 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import { Console } from '@woowacourse/mission-utils'
 import LottoGame from "./LottoGame.js";
 import InputHandler from "./InputHandler.js";
 
-const countWonLotto = (wonRecord, target) => {
-  return wonRecord.filter((record) => record === target).length;
-}
-
-const printSingleResult = (match, reward, bonus, count) => {
-  if(bonus) return Console.print(`${match}개 일치, 보너스 볼 일치 (${reward}) - ${count}개`);
-  return Console.print(`${match}개 일치 (${reward}) - ${count}개`);
-}
-
 const printWinningResults = (lottoGame, userLotto, winningResults) => {
   const resultMessages = [
     `3개 일치 (5,000원) - ${winningResults[3]}개`,
@@ -23,10 +14,10 @@ const printWinningResults = (lottoGame, userLotto, winningResults) => {
   Console.print("당첨 통계");
   Console.print("---");
 
-  results.forEach(result => {
-    const count = countWonLotto(wonRecord, result.match);
-    printSingleResult(result.match, result.reward, result.bonus, count);
-  });
+  const profitRate = lottoGame.calculateProfit(userLotto);
+  resultMessages.push(`총 수익률은 ${profitRate}%입니다.`)
+
+  return resultMessages.forEach(Console.print);
 };
 
 
@@ -48,7 +39,8 @@ class App {
     const bonusNumber = await InputHandler.getBonusNumber();
     lottoGame.setBonusNumber(bonusNumber);
 
-    printWinningResults(wonRecord);
+    const wonRecord = lottoGame.checkWinning(userLotto);
+    printWinningResults(lottoGame, userLotto, wonRecord);
   }
 }
 
