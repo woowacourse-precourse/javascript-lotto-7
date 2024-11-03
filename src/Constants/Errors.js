e8b966c4e31f20f43e22fd714e6ea03adbe41344
Minje Sung <minje813@dgu.ac.kr>
2024-11-03 21:24:35 +0900

    style(Constants): Errors 내용 변경

--- src/Constants/Errors.js
+++ src/Constants/Errors.js
@@ -4,34 +4,31 @@ import { InputComment } from './display.js';
 const Errors = Object.freeze({
   PREFIX: '[ERROR]',
   BuyPrice: {
-    NO_INPUT: `구입 금액을 입력받지 못했어요.\n구입 금액은 ${BuyPriceConfig.Unit.NAME} 단위로 떨어지는 숫자를 입력해주세요.`,
-    NOT_NUMBER_INPUT: '구입 금액은 숫자로 입력해야 합니다.\n다시 입력해주세요.',
-    LESS_THAN_MIN: `구입 금액은 ${BuyPriceConfig.Min.NAME} 이상이어야 합니다.\n다시 입력해주세요.`,
-    MORE_THAN_MAX: `구입 금액은 최대 ${BuyPriceConfig.Max.NAME}까지 입력할 수 있습니다.\n다시 입력해주세요.`,
-    NOT_UNIT_NUMBER: `구입 금액은 ${BuyPriceConfig.Unit.NAME} 단위로 떨어져야 합니다.\n다시 입력해주세요.`,
+    NO_INPUT: `구입 금액은 ${BuyPriceConfig.Unit.NAME} 단위로 떨어지는 숫자를 입력해주세요.`,
+    NOT_NUMBER_INPUT: '구입 금액은 숫자로 입력해야 합니다.',
+    LESS_THAN_MIN: `구입 금액은 ${BuyPriceConfig.Min.NAME} 이상이어야 합니다.`,
+    MORE_THAN_MAX: `구입 금액은 최대 ${BuyPriceConfig.Max.NAME}까지 입력할 수 있습니다.`,
+    NOT_UNIT_NUMBER: `구입 금액은 ${BuyPriceConfig.Unit.NAME} 단위로 떨어져야 합니다.`,
   },
 
   BasicNumbers: {
-    NO_INPUT: `당첨 번호를 입력받지 못했어요.\n6개의 숫자를 ${InputComment.Seperator.NAME}(${InputComment.Seperator.VALUE})로 구분하여 입력해주세요.`,
-    IS_WRONG_LENGTH: `숫자의 갯수가 맞지 않습니다.\n6개의 숫자를 ${InputComment.Seperator.NAME}(${InputComment.Seperator.VALUE})로 구분하여 입력해주세요.`,
-    IS_DUPLICATED_VALUE_IN: `번호들 중 중복되는 값이 있어요.\n중복되지 않는 숫자를 ${InputComment.Seperator.NAME}(${InputComment.Seperator.VALUE})로 구분하여 입력해주세요.`,
+    NO_INPUT: `당첨 번호를 입력받지 못했어요.`,
+    IS_WRONG_LENGTH: `숫자의 갯수가 맞지 않습니다.`,
+    IS_DUPLICATED_VALUE_IN: `번호들 중 중복되는 값이 있어요.`,
   },
 
   BasicEachNumber: {
-    NO_INPUT: `입력하지 않은 숫자가 있습니다.\n6개의 숫자를 ${InputComment.Seperator.NAME}(${InputComment.Seperator.VALUE})로 구분하여 입력해주세요.`,
-    NOT_NUMBER_INPUT: `숫자들 중 문자가 있습니다.\n문자가 아닌 숫자를 ${InputComment.Seperator.NAME}(${InputComment.Seperator.VALUE})로 구분하여 입력해주세요.`,
-    NOT_RANGED_INPUT: `숫자들 중 범위에 맞지 않은 숫자가 있습니다.\n1부터 45사이의 숫자를 ${InputComment.Seperator.NAME}(${InputComment.Seperator.VALUE})로 구분하여 입력해주세요.`,
+    NO_INPUT: `입력하지 않은 숫자가 있습니다.`,
+    NOT_NUMBER_INPUT: `숫자들 중 문자가 있습니다.`,
+    NOT_RANGED_INPUT: `숫자들 중 범위에 맞지 않은 숫자가 있습니다.`,
   },
 
   BonusNumber: {
-    NO_INPUT:
-      '보너스 번호를 입력받지 못했어요.\n1개의 숫자를 당첨 번호와 중복되지 않게 입력해주세요.',
-    NOT_NUMBER_INPUT:
-      '보너스 번호는 숫자로 입력해야 합니다.\n다시 입력해주세요.',
-    NOT_RANGED_INPUT:
-      '보너스 번호는 1부터 45 사이의 숫자이어야 합니다.\n다시 입력해주세요.',
+    NO_INPUT: '보너스 번호를 입력받지 못했어요.',
+    NOT_NUMBER_INPUT: '보너스 번호는 숫자로 입력해야 합니다.',
+    NOT_RANGED_INPUT: '보너스 번호는 1부터 45 사이의 숫자이어야 합니다.',
     IS_DUPLICATED_WITH_BASIC_NUMBERS:
-      '보너스 번호는 당첨 번호와 중복되면 안됩니다.\n다시 입력해주세요.',
+      '보너스 번호는 당첨 번호와 중복되면 안됩니다.',
   },
 
   Lotto: {
