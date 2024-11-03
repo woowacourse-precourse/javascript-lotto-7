d5e7f99e2b9e50b11bf94caf18127a619f4e9aa7
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-11-04 07:55:00 +0900

    ✨ feat(lotto): 1~45 중 중복되지 않은 6개의 숫자 오름차순 추출
    - `Random.pickUniqueNumbersInRange` 함수를 사용하여 지정된 범위 내에서 중복되지 않은 숫자들을 추출하였습니다.
    - 추출한 숫자들을 오름차순으로 정렬하여 반환하는 유틸리티 함수 `getRandomSortedNumbers`를 구현했습니다.
    - Lotto 클래스의 생성자에서 `getRandomSortedNumbers` 함수를 호출하여 `#numbers` 필드를 초기화합니다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,15 +1,14 @@
+import { LOTTO } from './constant';
+import getRandomSortedNumbers from './util/getRandomSortedNumbers';
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  constructor() {
+    this.#numbers = getRandomSortedNumbers(
+      LOTTO.START_NUMBER,
+      LOTTO.END_NUMBER,
+      LOTTO.COUNT,
+    );
   }
 
   // TODO: 추가 기능 구현
