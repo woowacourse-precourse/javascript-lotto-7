ee8e359d61c95b2a7987704bbfa6eb640424eecd
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-04 01:38:05 +0900

    ✨ feat: 당첨번호 구분자로 구분 기능 구현

--- src/view/InputHandler.js
+++ src/view/InputHandler.js
@@ -5,7 +5,11 @@ class InputHandler {
     return await MissionUtils.Console.readLineAsync(`\n${prompt}`); // readLineAsync 사용
   }
 
-
+  async getWinningNumbersInput(prompt) {
+    const input = await this.getInput(prompt);
+    const numbers = input.split(',').map((num) => Number(num.trim()));
+    return numbers;
+  }
 }
 
 export default InputHandler;
