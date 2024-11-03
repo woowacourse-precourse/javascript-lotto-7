2e53a20e75f89713345c2d42887f083fee32074b
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-03 22:32:51 +0900

    feat: 로또 티켓 리스트 출력 기능 추가

--- src/OutputValue.js
+++ src/OutputValue.js
@@ -5,6 +5,12 @@ const OutputValue = {
   printLottoTicketCount(lottoQuantity) {
     Console.print(`\n${lottoQuantity}${OUTPUT_MESSAGE.PURCHASE_NUMBER}`);
   },
+
+  printLottoTicketList(ticketList) {
+    ticketList.forEach((ticket) => {
+      Console.print(`[${ticket.join(", ")}]`);
+    });
+  },
 };
 
 export default OutputValue;
