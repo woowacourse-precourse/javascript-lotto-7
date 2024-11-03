fba00f1b1d6106e5d9de2384b6afc98b502ea7c1
BoRamSon <onbrson1@gmail.com>
2024-11-03 19:20:05 +0900

    feat(InputPrizeNumModel): 당첨번호 입력과 validate 진행하는 model

---
+++ src/model/InputPrizeNumModel.js
@@ -0,0 +1,22 @@
+import { InputView } from "../view/InputView.js";
+import { validInputPrizeNum } from "../validator/validInputPrizeNum.js";
+
+export class InputPrizeNumModel {
+  constructor() {
+    this.inputView = new InputView();
+  }
+
+  async getPrizeNumbers() {
+    let inputPrizeNumbersResult;
+    for (let i = 1; i < 10; i++) {
+      const inputPrizeNumbers = await this.inputView.inputPrizeNumbers();
+      const isNoProblem = validInputPrizeNum(inputPrizeNumbers);
+      if (isNoProblem) return (inputPrizeNumbersResult = inputPrizeNumbers);
+      if (i === 3)
+        throw new Error(
+          `[ERROR] 3회 이상 잘 못 입력하여 초기화되었습니다. 다시 실행해 주세요.`
+        );
+    }
+    return inputPrizeNumbersResult;
+  }
+}
