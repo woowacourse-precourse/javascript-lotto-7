7dc1a95c48cfda63a3ee0851a1654eba243bc217
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-11-03 20:27:56 +0900

    feat: 구입한 로또 수량 출력

--- src/Controller/GameController.js
+++ src/Controller/GameController.js
@@ -7,20 +7,22 @@ import { createErrorMessage } from '../util/error.js';
 
 class GameController {
   async init() {
-    const purchaseAmount = await Input.getPurchaseAmount()(
-      this.#validatePurchaseAmount,
+    const purchaseCount = await Input.getPurchaseAmount()(
+      this.#getValidatedPurchaseCount,
     );
+
+    Console.print(`\n${purchaseCount}개를 구매했습니다.`);
+
     const winningNumbers = await Input.getWinningNumbers()(
       this.#validateWinningNumbers,
     );
     const bonusNumber = await this.#getValidatedBonusNumber();
 
-    Console.print(purchaseAmount);
     Console.print(winningNumbers);
     Console.print(bonusNumber);
   }
 
-  #validatePurchaseAmount(input) {
+  #getValidatedPurchaseCount(input) {
     const amount = +input;
     if (!isNumber(amount)) {
       throw new Error(createErrorMessage(ERROR_MESSAGE.invalidNumberType));
@@ -32,7 +34,7 @@ class GameController {
       );
     }
 
-    return amount;
+    return amount / 1000;
   }
 
   #validateWinningNumbers(input) {
