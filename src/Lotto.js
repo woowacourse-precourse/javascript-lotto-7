fd6bdd99a5ff9ab0da6fef3280998016ac70f946
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-11-04 00:00:00 +0900

    feat: 당첨 번호 생성 및 유효성 검사기능 추가
    - 로또 번호가 6개인지 검사하는 validateLottoLength 메서드 추가
    - 로또 번호 중복 여부를 검사하는 validateDuplicatedNumber 메서드 추가
    -  NumberValidator 클래스로 로또 번호 하나당 유효성 재검사

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,58 @@
+// Lotto.js
+import { Console } from '@woowacourse/mission-utils';
+import NumberValidator from './utils/NumberValidator.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    Lotto.validateLottoNumbers(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  static async createLottoWithInput() {
+    try {
+      const lottoNumbers =
+        await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+      Console.print('');
+
+      Lotto.validateLottoNumbers(lottoNumbers);
+
+      return lottoNumbers.map(Number);
+    } catch (error) {
+      Console.print(error.message);
+      return this.createLottoWithInput();
     }
   }
 
-  // TODO: 추가 기능 구현
+  static validateLottoNumbers(numbers) {
+    NumberValidator.validateIsEmpty(numbers);
+    this.validateLottoLength(numbers);
+    this.validateDuplicatedNumber(numbers);
+
+    numbers.forEach((number) => {
+      NumberValidator.validateIsEmpty(number);
+      NumberValidator.validateIsNumber(number);
+      NumberValidator.validateNoDecimal(number);
+
+      const parsedNumber = parseFloat(number);
+      NumberValidator.validateIsInteger(parsedNumber);
+      NumberValidator.validateLottoNumberRange(parsedNumber);
+    });
+  }
+
+  static validateLottoLength(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
+
+  static validateDuplicatedNumber(numbers) {
+    const hasDuplicatedNumber = new Set(numbers).size !== numbers.length;
+    if (hasDuplicatedNumber) {
+      throw new Error('[ERROR] 로또 번호가 중복됩니다.');
+    }
+  }
 }
 
 export default Lotto;
