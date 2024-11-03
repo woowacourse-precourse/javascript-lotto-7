d31a08e18360989b1b6eac52f41567fb4d3bce8c
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 05:45:49 +0900

    refactor: 코드 형식에 따라 유틸함수 디렉토리로 기존 코드 이동

--- src/utils/methods.js
+++ src/utils/methods.js
@@ -1,3 +1,14 @@
 export const sortAsc = (unsortedValue) => {
     return unsortedValue.sort((a, b) => a - b)
 }
+export const stringToNumberArray = (input) => {
+    return input.split(",").map((elem) => Number(elem))
+}
+
+export const amountToNumber = (input) => {
+    return Number(input) / 1000
+}
+
+export const stringToNumber = (input) => {
+    return Number(input)
+}
