b76397f2c70694a8b67891240f739a2b9890e452
Heesu01 <rulru0413@naver.com>
2024-11-03 18:58:29 +0900

    feat(LottoController): 로또 컨트롤러 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoController from "./controllers/LottoController.js";
+
 class App {
-  async run() {}
+  async run() {
+    const lottoController = new LottoController();
+    await lottoController.run();
+  }
 }
 
 export default App;
