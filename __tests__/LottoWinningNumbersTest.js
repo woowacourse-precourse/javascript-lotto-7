471a5c92c3a5a20bcdedc92b19fd96881d472389
yheseul <kangyheseul@naver.com>
2024-11-04 00:47:28 +0900

    test: 당첨 번호 검증 관련 테스트 케이스 추가
    - 숫자 범위 1 ~ 45를 벗어나는 경우에 대한 테스트 케이스 추가
    - 당첨 번호의 갯수가 6개가 아닌 경우에 대한 테스트 케이스 추가

--- __tests__/LottoWinningNumbersTest.js
+++ __tests__/LottoWinningNumbersTest.js
@@ -5,6 +5,9 @@ describe("로또 당첨 번호 테스트", () => {
     ["1/2/3", WINNING_NUMBERS.INVALID_SEPARATOR],
     ["q,w,e", WINNING_NUMBERS.NOT_A_NUMBER],
     ["1,2,3,4,5,1", WINNING_NUMBERS.DUPLICATE_NUMBERS],
+    ["1,2,3,4,5,90", WINNING_NUMBERS.OUT_OF_RANGE],
+    ["1,2,3", WINNING_NUMBERS.INVALID_COUNT],
+    ["1,2,3,4,5,6,7", WINNING_NUMBERS.INVALID_COUNT],
   ])("validateWinningNumbersPipe(%s) throw %s", (input, expected) => {
     expect(() => validateWinningNumbersPipe(input).toThrow(expected));
   });
