07239dc26b621803dc6b9727ad818327c759067c
jungwho <jungwho0328@gmail.com>
2024-11-04 01:37:49 +0900

    feat(Validation) - Add Validation class

---
+++ src/GameSetting/Validation.js
@@ -0,0 +1,33 @@
+class Validation {
+  static inputNaturalNumber(number) {
+    let validatedNumber = Number(number);
+    if (
+      isNaN(validatedNumber) ||
+      !Number.isInteger(validatedNumber) ||
+      validatedNumber < 1
+    ) {
+      throw new Error("[ERROR] 숫자는 자연수로 입력 되어야 합니다.");
+    }
+    return validatedNumber;
+  }
+
+  static numbersLength(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  static numberRange(number) {
+    if (number < 1 || number > 45) {
+      throw new Error("[ERROR] 로또 번호는 1-45여야 합니다.");
+    }
+  }
+
+  static numbersDuplicate(numbers) {
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 중복되는 숫자가 없어야 합니다.");
+    }
+  }
+}
+
+export default Validation;
