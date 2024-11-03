eebf2f317d9ad0442c94a7fbccdee0b60ee5d7ac
jennaaaaaaaaa <ggm4954@gmail.com>
2024-11-03 18:53:05 +0900

    feat: 로또 결과 출력 기능 추가 코드 흐름 개선

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import { Console } from '@woowacourse/mission-utils';
 import { Input } from './inputHandler.js';
 import LotteryTickets from './lotteryTicket.js';
 import Lotto from './Lotto.js';
+import LotteryResult from './lotteryResult.js';
+
 // import { Validator } from './validate.js';
 
 class App {
    async run() {
-      const purchaseAmount = await Input.getPurchaseAmount(); //로또 장 수 lotteryTickets를 위한 구매 금액
-      const lotteryTickets = new LotteryTickets(purchaseAmount); //로또 티켓 생성, 클래스의 인스턴스
-      const tickets = lotteryTickets.getTickets(); //인스턴스를 통해 메서드 호출
+      try {
+         const purchaseAmount = await Input.getPurchaseAmount(); // 로또 장 수 lotteryTickets를 위한 구매 금액
+         const lotteryTickets = new LotteryTickets(purchaseAmount); // 로또 티켓 생성, 클래스의 인스턴스
+         const tickets = lotteryTickets.getTickets(); // 인스턴스를 통해 메서드 호출
 
-      // Lotto 객체로 변환, lotto 클래스의 인스턴스 배열
-      const lottoTickets = tickets.map((ticket) => new Lotto(ticket)); //
+         Console.print(`${tickets.length}개를 구매했습니다.`);
+         tickets.forEach((ticket) => Console.print(`[${ticket.join(', ')}]`));
 
-      Console.print(`${tickets.length}개를 구매했습니다.`);
-      tickets.forEach((ticket) => Console.print(ticket));
-
-      const winningNumbers = await Input.getWinningNumbers();
-      const bonusNumber = await Input.getBonusNumber();
-
-      console.log('app.js에서의 winningNumbers ', winningNumbers);
-      console.log('app.js에서의 bonusNumber ', bonusNumber);
-      console.log('app.js에서의 purchaseAmount', purchaseAmount);
-      console.log('app.js에서의 tickets', tickets);
+         const winningNumbers = await Input.getWinningNumbers();
+         const bonusNumber = await Input.getBonusNumber();
+         LotteryResult.printResults(tickets, winningNumbers, bonusNumber, purchaseAmount);
+      } catch (error) {
+         Console.print(error.message); // 에러 메시지만 출력
+      }
    }
 }
 
