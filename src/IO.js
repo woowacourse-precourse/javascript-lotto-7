23acd890126ba324b14209b87623a02b2fe0e745
ahhyun <wndkgus0225@naver.com>
2024-11-03 16:14:44 +0900

    feat(IO) : 발행한 로또 수량 및 번호 출력 추가

--- src/IO.js
+++ src/IO.js
@@ -1,9 +1,22 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from '../src/message.js';
 import { validateCost } from '../src/error.js';
+import { generateLottos } from '../src/Lotto.js';
 
 export async function getCost() {
   const cost = await Console.readLineAsync(MESSAGE.ASK_COST+"\n");
   validateCost(Number(cost));
-  return cost;
+  const count = cost/1000;
+  return count;
+}
+
+export async function displayLottosCount(count){
+  Console.print("\n"+count+MESSAGE.COUNT_INFO);
+}
+
+export async function displayLottosNumber(count){
+  const lottos = generateLottos(count);
+  lottos.forEach(lotto => {
+    Console.print(`[${lotto.getNumbers().join(', ')}]`);
+  });
 }
