fcda1c15e8e1cfda82712a91884cb816a031ab54
Jaeho Cha <ischar@naver.com>
2024-11-03 14:51:43 +0900

    chore(validation): add export to checkDuplicates

--- src/features/checkDuplicates.js
+++ src/features/checkDuplicates.js
@@ -1,6 +1,6 @@
 import { ERROR } from "../config/config.js";
 
-const checkDuplicates = (winningNumbers, bonusNumber) => {
+export const checkDuplicates = (winningNumbers, bonusNumber) => {
   const winningSet = new Set(winningNumbers);
   if (winningSet.has(bonusNumber)) {
     throw new Error(ERROR.DUPLICATE_NUMBER);
