d91bffb55fb9c6114fc51480f69546846166064c
GSIII <kalseo901@gmail.com>
2024-11-03 18:43:55 +0900

    feat(view) : 로또 수량/번호 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import { isValidPrice, isZeroPrice } from "./errors/LottoInputErrors.js";
+import { getLottoCount, getLottoNums } from "./services/LottoGenerator.js";
 
 class App {
   async run() {
@@ -8,6 +9,14 @@ class App {
     );
     isZeroPrice(lottoPrice);
     isValidPrice(lottoPrice);
+
+    const lottoCount = getLottoCount(lottoPrice);
+    const lottoNumbers = getLottoNums(lottoCount);
+
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+    lottoNumbers.forEach((numbers) => {
+      Console.print(numbers.sort((a, b) => a - b));
+    });
   }
 }
 
---
+++ src/services/LottoGenerator.js
@@ -0,0 +1,15 @@
+import { Random } from "@woowacourse/mission-utils";
+
+export const getLottoCount = (lottoPrice) => {
+  const lottoNums = lottoPrice / 1000;
+  return lottoNums;
+};
+
+export const getLottoNums = (lottoNums) => {
+  const lottoArrs = [];
+  for (let i = 0; i < lottoNums; i++) {
+    const lottoNum = Random.pickUniqueNumbersInRange(1, 45, 6);
+    lottoArrs.push(lottoNum);
+  }
+  return lottoArrs;
+};
