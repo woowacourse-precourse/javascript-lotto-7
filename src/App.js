d4377187f443f1dc04d0df65a27265f977655c2d
xxxjinn <tlsguswls319@naver.com>
2024-11-03 19:45:09 +0900

    feat(App.js): 로또 구입 금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "./constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    const amount = await Console.readLineAsync(INPUT_MESSAGE.PURCHASE_AMOUNT_MESSAGE);
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const INPUT_MESSAGE={
+    PURCHASE_AMOUNT_MESSAGE:'구입금액을 입력해 주세요.\n',
+}
+
+export {INPUT_MESSAGE}
