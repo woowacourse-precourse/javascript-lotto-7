045e457febe4f0800943e9e2b7c5e5d12468b5ce
Rigu1 <dlsrbtla@gmail.com>
2024-11-04 02:56:00 +0900

    feat(Lotto): Add rank calculation and validation for lotto numbers
    - 로또 번호가 6개를 초과하거나 중복된 숫자가 포함된 경우 예외를 발생하도록 유효성 검사 기능 추가
    - calculateRank 메서드를 구현하여 매칭된 숫자 개수와 보너스 여부에 따라 등수를 반환
    - #getMatchingCount 메서드로 당첨 번호와 일치하는 숫자의 개수를 계산하도록 구현
    - LottoTest.js에서 예외 처리와 기능 테스트 수행

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+import Constants from "./Constants";
 class Lotto {
   #numbers;
 
@@ -8,11 +9,34 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(Constants.ERROR_MESSAGES.LOTTO_NUMBER_COUNT_ERROR);
+    }
+
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error(Constants.ERROR_MESSAGES.DUPLICATE_NUMBER_ERROR);
     }
   }
 
-  // TODO: 추가 기능 구현
+  #getMatchingCount(winningNumbers) {
+    return this.#numbers.filter((number) => winningNumbers.includes(number))
+      .length;
+  }
+
+  calculateRank(winningNumbers, bonusNumber) {
+    const matchingCount = this.#getMatchingCount(winningNumbers);
+    const isBonusMatch = this.#numbers.includes(bonusNumber);
+
+    for (let rank in Constants.RANKINGS) {
+      const { matches, BonusMatch } = Constants.RANKINGS[rank];
+      if (
+        matchingCount === matches &&
+        (BonusMatch === undefined || BonusMatch === isBonusMatch)
+      ) {
+        return parseInt(rank);
+      }
+    }
+    return 0;
+  }
 }
 
 export default Lotto;
