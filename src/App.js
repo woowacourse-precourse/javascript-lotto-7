5a289d6b9ed638f09e5be41e273a37d75cab8410
Ray <rlawlsdn9583@gmail.com>
2024-11-03 22:12:20 +0900

    feat: 실행 결과 예시에 맞게 입출력 수정

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import Lotto from "./Lotto.js";
 class App {
   async run() {
     const purchaseMoney = await Console.readLineAsync(
-      "구입금액을 입력해 주세요."
+      "구입금액을 입력해 주세요.\n"
     );
     let lottos = Array.from({ length: purchaseMoney / 1000 }, () =>
       MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
     );
 
-    Console.print(`${lottos.length}개를 구매했습니다.`);
+    Console.print(`\n${lottos.length}개를 구매했습니다.`);
     lottos = lottos.map((lotto) => {
       Console.print(lotto);
       return new Lotto(lotto);
     });
 
-    // const winningNumber = await Console.readLineAsync(
-    //   "당첨 번호를 입력해 주세요."
-    // );
+    const winningNumber = await Console.readLineAsync(
+      "\n당첨 번호를 입력해 주세요.\n"
+    );
 
-    // const bonusNumber = await Console.readLineAsync(
-    //   "보너스 번호를 입력해 주세요."
-    // );
+    const bonusNumber = await Console.readLineAsync(
+      "\n보너스 번호를 입력해 주세요.\n"
+    );
   }
 }
 
