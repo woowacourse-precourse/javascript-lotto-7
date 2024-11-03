ddee4b87402447bb5185c4afca3fa8ee78662607
YujeongLEE <dldbwjd24@gmail.com>
2024-11-03 14:06:34 +0900

    test(Lotto) : 로또 클래스 테스트 추가
    test(Lotto) : 로또 클래스 테스트 추가
    
    - 로또 번호가 6개인지 확인
    - 로또 번호에 중복이 없는지 확인
    - 로또 번호를 오름차순에 맞게 출력하는지 확인

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,54 @@
 import Lotto from "../src/Lotto";
+import {MissionUtils} from "@woowacourse/mission-utils";
+import {lottoUtils} from "../src/utils/lotto.utils.js";
+import {ERROR_CODE, LOTTO} from "../src/constants/constants.js";
+
+const getLogSpy = () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+    return logSpy;
+};
+
+const mockRandoms = (numbers) => {
+    MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+    numbers.reduce((acc, number) => {
+        return acc.mockReturnValueOnce(number);
+    }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
-  });
+    test("예외 테스트 : 로또 번호의 개수가 6개가 넘어가는 경우", () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5, 6, 7]);
+        }).toThrow(ERROR_CODE.LOTTO_SIZE_OUT_OF_RANGE(LOTTO.SIZE));
+    });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
+    test("예외 테스트 : 로또 번호의 개수가 6개 이하인 경우", () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5]);
+        }).toThrow(ERROR_CODE.LOTTO_SIZE_OUT_OF_RANGE(LOTTO.SIZE));
+    });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    test("예외 테스트 : 로또 번호 중복이 있는 경우", () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5, 5]);
+        }).toThrow(ERROR_CODE.LOTTO_NUMBER_DUPLICATE);
+    });
+
+    test("정상 케이스 : 로또 오름차순 정렬 후 출력", () => {
+        const logSpy = getLogSpy();
+
+        const RANDOM_NUMBERS_TO_END = [7, 1, 43, 24, 35, 6];
+        mockRandoms([RANDOM_NUMBERS_TO_END]);
+
+        const log = [1, 6, 7, 24, 35, 43]
+
+        const lottos = lottoUtils.generateNLottos(1)
+        lottos.forEach(lotto => {
+            lotto.print()
+            expect(logSpy).toHaveBeenCalledWith(log);
+        })
+
+    })
+
 });
