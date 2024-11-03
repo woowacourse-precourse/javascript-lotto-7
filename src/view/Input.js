e8da04a3090041e5aab6a9958d578b941af11148
ChanLee_KR <cl700120@gmail.com>
2024-11-03 23:18:44 +0900

    feat: 인풋 공통 리더 함수 구현

--- src/view/Input.js
+++ src/view/Input.js
@@ -1,9 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+import { Exception } from '../Utils.js';
+
 class Input {
   static async readPurchaseAmount() {}
 
   static async readWinningNumbers() {}
 
   static async readBonusNumber() {}
+
+  /** @param {string} message */
+  static async #readLine(message) {
+    /** @type {string} */
+    const input = await Console.readLineAsync('\n' + message + '\n');
+
+    if (!input.trim()) {
+      throw new Exception(message);
+    }
+
+    return input;
+  }
 }
 
 export default Input;
