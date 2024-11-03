7869bb54e2ec2ae62e1cc3e90c665bc46c250f9c
keemsebeen <sebin0580@gmail.com>
2024-11-03 21:16:20 +0900

    feat(App) : 구매 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { INPUT } from "./constants/lotto";
+import { getInput } from "./io/Input";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const purchasePrice = await getInput(INPUT.PURCHASE_PRICE);
+    } catch (error) {}
+  }
 }
 
 export default App;
