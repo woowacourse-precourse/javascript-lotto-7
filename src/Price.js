51b95eeacb2aabf7a54f2644a1c9f8495e4c93d8
wowalswjd <alswjd5491@naver.com>
2024-11-03 20:00:02 +0900

    refactor(Price): 함수가 한 가지 일만 하도록 분리

--- src/Price.js
+++ src/Price.js
@@ -10,18 +10,31 @@ class Price {
   }
 
   #validate(priceNum) {
+    this.#checkIfNaN(priceNum);
+    this.#checkIfInteger(priceNum);
+    this.#checkIfPositive(priceNum);
+    this.#checkIfThousand(priceNum);
+  }
+
+  #checkIfNaN(priceNum) {
     if (Number.isNaN(priceNum)) {
       throw new Error(PRICE_ERROR.IS_NOT_NUMBER);
     }
+  }
 
+  #checkIfInteger(priceNum) {
     if (!Number.isInteger(priceNum)) {
       throw new Error(PRICE_ERROR.IS_NOT_INTEGER);
     }
+  }
 
+  #checkIfPositive(priceNum) {
     if (priceNum <= 0) {
       throw new Error(PRICE_ERROR.IS_NOT_POSITIVE);
     }
+  }
 
+  #checkIfThousand(priceNum) {
     if (priceNum % 1000) {
       throw new Error(PRICE_ERROR.IS_NOT_THOUSAND);
     }
@@ -30,7 +43,6 @@ class Price {
   getPrice() {
     return this.#price;
   }
-
 }
 
 export default Price;
