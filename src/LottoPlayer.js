aed3b70fd3c286347b6fb6bf9eb999d7a15ae5a4
hienjoy <wisdom16no@naver.com>
2024-11-04 04:36:25 +0900

    feat: 로또 발행하기 추가

--- src/LottoPlayer.js
+++ src/LottoPlayer.js
@@ -1,13 +1,40 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import inputHandler from "./inputHandler.js";
+import Lotto from "./Lotto.js";
 
 class LottoPlayer {
+  static LOTTO_PRICE = 1000;
+
+  constructor() {
+    this.numberOfLottos = 0;
+    this.winningNumbers = [];
+    this.bonusNumber = null;
+    this.lottos = [];
+  }
+
   async play() {
     try {
       const purchaseAmount = await inputHandler.requestPurchaseAmount();
+      this.setNumberOfLottos(purchaseAmount);
+      this.createLottos();
     } catch (error) {
       MissionUtils.Console.print(error.message);
       await this.play();
+    }
+  }
+
+  setNumberOfLottos(value) {
+    this.numberOfLottos = value / LottoPlayer.LOTTO_PRICE;
+  }
+
+  createLottos() {
+    for (let i = 0; i < this.numberOfLottos; i++) {
+      const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
+        1,
+        45,
+        6
+      );
+      this.lottos.push(new Lotto(randomNumber));
     }
   }
 }
