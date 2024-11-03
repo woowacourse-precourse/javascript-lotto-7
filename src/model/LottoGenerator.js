61da6ad116442abd22eb80961c6b420cb60d2df1
taeyomi <terry11ty@naver.com>
2024-11-03 20:34:10 +0900

    fix(LottoGenerator): 로또 생성에서 발생하는 에러 수정

--- src/model/LottoGenerator.js
+++ src/model/LottoGenerator.js
@@ -1,21 +1,22 @@
 import { Random } from "@woowacourse/mission-utils";
-import Lotto from "./Lotto";
+import Lotto from "./Lotto.js";
 
 class LottoGenerator {
   constructor() {
     this.lottos = [];
   }
 
-  setGeneratedLottos(count) {
+  generateLottos(count) {
     for (let i = 0; i < count; i++) {
       const numbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
         (a, b) => a - b
       );
-      lottos.push(new Lotto(numbers));
+      this.lottos.push(numbers);
+      console.log(this.lottos);
     }
   }
 
-  getGeneratedLottos() {
+  getLottos() {
     return this.lottos;
   }
 }
