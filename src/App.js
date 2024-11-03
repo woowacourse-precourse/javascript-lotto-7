5843de30b7040a775612e4c79ca8ac5a7a79945b
yheseul <kangyheseul@naver.com>
2024-11-03 17:00:59 +0900

    fix: RUN_MESSAGE 파일 생성 및 재적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
-import { RUN_MESSAGE } from "./constants/constants.js";
+import { RUN_MESSAGE } from "./constants/runMessages.js";
 import { validationPipe } from "./validation/validationPipe.js";
 
 class App {
---
+++ src/constants/runMessages.js
@@ -0,0 +1,3 @@
+export const RUN_MESSAGE = Object.freeze({
+  PURCHASE_AMOUNT: "로또 구입 금액을 입력해 주세요.\n",
+});
