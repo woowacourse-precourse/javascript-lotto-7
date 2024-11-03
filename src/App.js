5060ff09d4fc08e953694a0dd46a4a956c6176e9
Rigu1 <dlsrbtla@gmail.com>
2024-11-03 17:49:21 +0900

    refactor(InputFormatter): Update input handling and validation
    - InputHandler를 InputFormatter로 대체하여 구매 금액, 당첨 번호,
      보너스 번호의 입력 처리, 검증, 포맷팅을 한곳에서 관리하도록 개선
    - 각 입력 유형에 대한 상세한 에러 처리 추가: 로또 번호의 개수 확인,
      유효 범위 (1-45), 1000 단위 검증 등
    - App 클래스에서 새로운 InputFormatter를 활용해 입력 처리를 간소화
    - 다양한 입력 시나리오에 대한 포맷팅 및 에러 처리가 올바르게 동작하는지
      검증하는 InputFormatter 부분 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import InputFormatter from "./InputFormatter";
+
 class App {
-  async run() {}
+  async #getInput() {
+    const purchaseAmount = await MissionUtils.Console.readLineAsync();
+    const winningNumbers = await MissionUtils.Console.readLineAsync();
+    const bonusNumber = await MissionUtils.Console.readLineAsync();
+    
+    return [purchaseAmount, winningNumbers, bonusNumber];
+  }
+  async run() {
+    const { purchaseAmount, winningNumbers, bonusNumber } = await this.#getInput();
+    const inputFormatter = new InputFormatter(
+      purchaseAmount,
+      winningNumbers,
+      bonusNumber
+    );
+  }
 }
 
 export default App;
