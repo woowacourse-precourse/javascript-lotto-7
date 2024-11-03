b4dced00532b308e4333ddf03d55ea836865ecaa
osw6858 <osw7875@naver.com>
2024-11-03 21:03:29 +0900

    feat: 보너스 번호의 형식을 검증하는 메서드 추가

--- src/Bonus.js
+++ src/Bonus.js
@@ -10,12 +10,19 @@ export class Bonus {
 
   #validate(bonus, lottoList) {
     this.#validBonusRange(bonus);
+    this.#validBonusFormat(bonus);
   }
 
   #validBonusRange(bonus) {
     const number = Number(bonus.trim());
     if (number < 1 || number > 45) {
       throw new Error('보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  }
+
+  #validBonusFormat(bonus) {
+    if (Number.isNaN(Number(bonus.trim()))) {
+      throw new Error('보너스 번호는 숫자여야 합니다.');
     }
   }
 }
