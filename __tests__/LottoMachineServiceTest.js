fa289f58bb024c0c1c85eb293b71ca3005bccb9b
Seio924 <seio9241@gmail.com>
2024-11-03 15:55:57 +0900

    refactor(ReturnRateCalculatorServiceTest): 수익률 계산 테스트 분리

--- __tests__/LottoMachineServiceTest.js
+++ __tests__/LottoMachineServiceTest.js
@@ -1,18 +1,1 @@
-import Lotto from '../src/Lotto';
-import LottoMachineService from '../src/Service/LottoMachineService';
-
-describe('LottoMachineService 테스트', () => {
-  test('총 수익률을 계산한다.', () => {
-    const purchaseAmount = 8000;
-    const totalWinningRank = [0, 0, 0, 0, 1];
-    const output = 62.5;
-
-    const lottoMachine = new LottoMachineService();
-    const totalReturnRate = lottoMachine.calculateTotalReturnRate(
-      purchaseAmount,
-      totalWinningRank
-    );
-
-    expect(totalReturnRate).toBe(output);
-  });
-});
+describe('LottoMachineService 테스트', () => {});
