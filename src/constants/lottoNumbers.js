a47eae3c36589b6dcdef754fc4b3d3071e48c613
SNXWXH <snxwxh@gmail.com>
2024-11-04 01:15:54 +0900

    refactor(LottoGenerator): 매직넘버 상수화

---
+++ src/constants/lottoNumbers.js
@@ -0,0 +1,7 @@
+const LOTTO_NUMBER_RANGE = Object.freeze({
+  minimum: 1,
+  maximum: 45,
+  count: 6,
+});
+
+export default LOTTO_NUMBER_RANGE;
--- src/utils/LottoGenerator.js
+++ src/utils/LottoGenerator.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Lotto from '../models/Lotto.js';
+import LOTTO_NUMBER_RANGE from '../constants/lottoNumbers.js';
 
 class LottoGenerator {
   constructor(purchaseCount) {
@@ -9,7 +10,11 @@ class LottoGenerator {
 
   generateLottoNumbers() {
     for (let i = 0; i < this.purchaseCount; i++) {
-      const NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(
+        LOTTO_NUMBER_RANGE.minimum,
+        LOTTO_NUMBER_RANGE.maximum,
+        LOTTO_NUMBER_RANGE.count,
+      );
       const LOTTO = new Lotto(NUMBERS);
       this.lottoNumbers.push(LOTTO.getNumbers().sort((a, b) => a - b));
     }
