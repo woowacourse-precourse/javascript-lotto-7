8a1a34972ab7a9978e098edd97ca58e31bd6d038
seongmin <seongm22n@gmail.com>
2024-11-03 17:35:39 +0900

    feat: 기능2. 로또 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import { LottoInputHandler } from './LottoInputHandler.js';
+
 class App {
-  async run() {}
+	async run() {
+		const inputHandler = new LottoInputHandler();
+		const inputAmount = await Console.readLineAsync(
+			'구입금액을 입력해 주세요.\n'
+		);
+		inputHandler.getPurchaseAmount(inputAmount);
+	}
 }
 
 export default App;
