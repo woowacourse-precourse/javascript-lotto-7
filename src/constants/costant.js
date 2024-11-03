a26da2a4707e31e7446e4b626ddbeabcf81324ee
mynameSumin <msm4167@naver.com>
2024-11-03 15:48:37 +0900

    feat(constant): 필요한 문자열 및 숫자 상수 처리

---
+++ src/constants/costant.js
@@ -0,0 +1,13 @@
+const PURCHASE_UNIT = Object.freeze({
+  UNIT_OF_LOTTO: 1000,
+});
+
+const LOTTO_CANSTANT = Object.freeze({
+    MIN_LOTTO_NUMBER: 1,
+    MAX_LOTTO_NUMBER: 45,
+    LOTTO_LENGTH: 6
+});
+
+const CONSTANT = Object.freeze({PURCHASE_UNIT, LOTTO_CANSTANT});
+
+export default CONSTANT;
