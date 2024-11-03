965179611d8599d4d476f6b3abaa0c3fc1a791db
Jaeho Cha <ischar@naver.com>
2024-11-03 22:01:24 +0900

    test(lotto): add unit tests for checkLotto
    - checkLotto 유닛 테스트 추가
      - 정상적으로 값을 반환하는 지 검증

---
+++ __tests__/checkLotto.test.js
@@ -0,0 +1,19 @@
+import { checkLotto } from "../src/features/checkLotto.js";
+import LottoRank from "../src/LottoRank.js";
+
+describe("checkLotto 테스트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("정상적으로 값을 반환하는 지 확인", () => {
+    const LOTTO_NUMBERS = [1, 2, 3, 4, 5, 6];
+    const WINNING_NUMBERS = [1, 2, 3, 7, 8, 9];
+    const BONUS_NUMBER = 10;
+    const EXPECTED = new LottoRank(3, false);
+
+    expect(checkLotto(LOTTO_NUMBERS, WINNING_NUMBERS, BONUS_NUMBER)).toEqual(
+      EXPECTED
+    );
+  });
+});
