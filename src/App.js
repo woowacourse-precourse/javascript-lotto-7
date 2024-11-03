f003441e2a7cf587a013f74c27a4e2d430ed86ab
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-11-03 16:42:57 +0900

    feat(constant.js): PRIZE

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+import { PRIZE } from "./constants/constant.js";
 
-const PRIZE = {
-  three: 5000,
-  four: 50000,
-  five: 1500000,
-  fiveBonus: 30000000,
-  six: 2000000000,
-};
 class App {
   async run() {
     const purchaseAmount = await Console.readLineAsync(
---
+++ src/constants/constant.js
@@ -0,0 +1,9 @@
+const PRIZE = Object.freeze({
+  three: 5000,
+  four: 50000,
+  five: 1500000,
+  fiveBonus: 30000000,
+  six: 2000000000,
+});
+
+export { PRIZE };
