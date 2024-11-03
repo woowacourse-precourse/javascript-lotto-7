77b58cd9f354ea2f49866ea7c5e5b383d1bbebee
osw6858 <osw7875@naver.com>
2024-11-03 22:17:27 +0900

    feat: 결과 객체를 초기화 하는 메서드 추가

--- src/Calculate.js
+++ src/Calculate.js
@@ -8,4 +8,19 @@ export class Calculate {
     this.#winningLotto = winningLotto;
     this.#bonusNumber = bonusNumber;
   }
+
+  #initializeResults() {
+    return {
+      first: { count: 0, prize: 2000000000, match: 6, message: '6개 일치' },
+      second: {
+        count: 0,
+        prize: 30000000,
+        match: 5,
+        message: '5개 일치, 보너스 볼 일치',
+      },
+      third: { count: 0, prize: 1500000, match: 5, message: '5개 일치' },
+      fourth: { count: 0, prize: 50000, match: 4, message: '4개 일치' },
+      fifth: { count: 0, prize: 5000, match: 3, message: '3개 일치' },
+    };
+  }
 }
