ef268d41017ed150af396602bcd0050b439df247
백지윤 <jy.baek079@gmail.com>
2024-11-04 02:33:00 +0900

    refactor: AppController 메서드 분리

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -10,39 +10,63 @@ class AppController {
 
   async run() {
     try {
-      // 1. 구입 금액 입력 및 검증
-      const purchaseAmount = await InputView.askPurchaseAmount();
-      Validator.validatePurchaseAmount(purchaseAmount);
-
-      // 2. 로또 티켓 생성 및 출력
-      this.lottoController = new LottoController(purchaseAmount);
-      this.lottoController.generateLottos();
-      const tickets = this.lottoController.getLottos();
-
-      OutputView.displayLottoCount(purchaseAmount);
-      OutputView.displayLottoNumbers(tickets);
-
-      // 3. 당첨 번호 및 보너스 번호 입력 및 검증
-      const winningNumbers = await InputView.askWinningNumbers();
-      Validator.validateWinningNumbers(winningNumbers);
-
-      const bonusNumber = await InputView.askBonusNumber();
-      Validator.validateBonusNumber(bonusNumber, winningNumbers);
-
-      // 4. 당첨 결과 계산
-      const resultCounts = this.lottoController.calculateResult(
-        winningNumbers,
-        bonusNumber,
-      );
-      const profitRate =
-        this.lottoController.calculateProfitRate(purchaseAmount);
-
-      // 5. 당첨 결과 및 수익률 출력
-      OutputView.displayResults(resultCounts);
-      OutputView.displayProfitRate(profitRate);
+      const purchaseAmount = await this.handlePurchaseAmount();
+      this.generateAndDisplayLottos(purchaseAmount);
+      const { winningNumbers, bonusNumber } = await this.handleWinningNumbers();
+      const resultCounts = this.calculateResults(winningNumbers, bonusNumber);
+      const profitRate = this.calculateAndDisplayProfitRate(purchaseAmount);
+      this.displayResults(resultCounts, profitRate);
     } catch (error) {
-      OutputView.displayError(error.message);
+      this.handleError(error);
     }
+  }
+
+  //구매 금액을 입력받고 검증하는 메서드
+  async handlePurchaseAmount() {
+    const amount = await InputView.askPurchaseAmount();
+    Validator.validatePurchaseAmount(amount);
+    return amount;
+  }
+
+  //로또 티켓을 생성하고 출력하는 메서드
+  generateAndDisplayLottos(purchaseAmount) {
+    this.lottoController = new LottoController(purchaseAmount);
+    this.lottoController.generateLottos();
+    const tickets = this.lottoController.getLottos();
+    OutputView.displayLottoCount(purchaseAmount);
+    OutputView.displayLottoNumbers(tickets);
+  }
+
+  //당첨 번호와 보너스 번호를 입력받고 검증하는 메서드
+  async handleWinningNumbers() {
+    const winningNumbers = await InputView.askWinningNumbers();
+    Validator.validateWinningNumbers(winningNumbers);
+
+    const bonusNumber = await InputView.askBonusNumber();
+    Validator.validateBonusNumber(bonusNumber, winningNumbers);
+
+    return { winningNumbers, bonusNumber };
+  }
+
+  //당첨 결과를 계산하는 메서드
+  calculateResults(winningNumbers, bonusNumber) {
+    return this.lottoController.calculateResult(winningNumbers, bonusNumber);
+  }
+
+  //수익률을 계산하고 출력하는 메서드
+  calculateAndDisplayProfitRate(purchaseAmount) {
+    const profitRate = this.lottoController.calculateProfitRate(purchaseAmount);
+    return profitRate;
+  }
+
+  //당첨 결과와 수익률을 출력하는 메서드
+  displayResults(resultCounts, profitRate) {
+    OutputView.displayResults(resultCounts);
+    OutputView.displayProfitRate(profitRate);
+  }
+
+  handleError(error) {
+    OutputView.displayError(error.message);
   }
 }
 
