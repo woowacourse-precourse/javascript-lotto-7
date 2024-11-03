99729f0167789f19022066070a6f7c32d71fde5d
yoojin <everywavyday@gmail.com>
2024-11-03 21:42:17 +0900

    feat: 로또 추첨을 위한 입력 시 예외 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+import { validateWinnerNumbers } from "./errors.js";
 class Lotto {
   #numbers;
 
@@ -7,9 +8,7 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    validateWinnerNumbers(numbers);
   }
 
   getNumbers() {
--- src/errors.js
+++ src/errors.js
@@ -11,3 +11,31 @@ export function validateIsNumber(userInput) {
         throw new Error(ERROR_MESSAGES.INVALID_NUMBER);
     }
 }
+
+export function validateWinnerNumbers(numbers) {
+    if (numbers.length !== 6) {
+        throw new Error(ERROR_MESSAGES.INVALID_NUMBER_COUNT);
+    }
+    const numberSet = new Set(numbers);
+    if (numberSet.size !== numbers.length) {
+        throw new Error(ERROR_MESSAGES.DUPLICATE_BONUS_NUMBER);
+    }
+    numbers.forEach(number => {
+        validateIsNumber(number);
+        validateNumberInRange(number);
+    });
+}
+
+export function validateNumberInRange(number) {
+    if (number < 1 || number > 45) {
+        throw new Error(ERROR_MESSAGES.INVALID_NUMBER_RANGE);
+    }
+}
+
+export function validateBonusNumber(bonusNumber, winnerNumbers) {
+    validateIsNumber(bonusNumber);
+    validateNumberInRange(bonusNumber);
+    if (winnerNumbers.includes(bonusNumber)) {
+        throw new Error(ERROR_MESSAGES.DUPLICATE_BONUS_NUMBER);
+    }
+}
