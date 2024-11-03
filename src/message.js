3049a3b7ef947a4d9770df30b2882cf7e5469cac
ahhyun <wndkgus0225@naver.com>
2024-11-04 00:51:39 +0900

    feat(message) : 당첨 번호 및 보너스 번호에 대한 메시지 추가

--- src/message.js
+++ src/message.js
@@ -1,5 +1,13 @@
 export const MESSAGE = {
-  ASK_COST: "구입금액을 입력해 주세요."
+  ASK_COST: "구입금액을 입력해 주세요.",
+  COUNT_INFO: "개를 구매했습니다.",
+  ASK_USER_PICK_NUMBER: "당첨 번호를 입력해 주세요.",
+  ASK_USER_PICK_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+  CORRECT_THREE: "3개 일치 (5,000원) - ",
+  CORRECT_FOUR: "4개 일치 (50,000원) - ",
+  CORRECT_FIVE: "5개 일치 (1,500,000원) - ",
+  CORRECT_FIVE_WITH_BONUS: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  CORRECT_SIX: "6개 일치 (2,000,000,000원) - "
 }
 
 export const ERROR_MESSAGE = {
@@ -7,6 +15,9 @@ export const ERROR_MESSAGE = {
   NOT_INTEGER: "[ERROR] 정수 이외의 입력은 사용할 수 없습니다.",
   NUM_IS_ZERO : "[ERROR] 0은 입력할 수 없습니다.",
   NEGATIVE_NUMBER: "[ERROR] 음수는 입력할 수 없습니다.",
-  VALIDATE_DIVERSITY_BY_THOUSAND: "[ERROR] 금액은 1000원 단위이어야 합니다."
+  VALIDATE_DIVERSITY_BY_THOUSAND: "[ERROR] 금액은 1000원 단위이어야 합니다.",
+  NOT_SIX_NUMBERS: "[ERROR] 당첨 번호는 6개이어야 합니다.",
+  DUPLICATE_NUMBER: "[ERROR] 당첨 번호는 중복될 수 없습니다,",
+  INVALID_SEPARATOR: "[ERROR] 번호는 쉼표(,)로 구분해야 합니다.",
+  OUT_OF_RANGE: "[ERROR] 번호는 1부터 45까지만 입력 가능합니다."
 }
-
