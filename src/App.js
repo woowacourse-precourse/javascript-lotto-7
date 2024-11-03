16ca08d21a66930b4ff1b85cde818d4c58d200e0
Murphybread <nargene@gmail.com>
2024-11-03 22:35:40 +0900

    style: remove too  much comments

--- src/App.js
+++ src/App.js
@@ -14,13 +14,10 @@ class App {
     const paidMoney = await this.userInput.getUserPaidMoney();
     const lottoTickets= this.game.purchaseLotto(paidMoney);
 
-    // 구매한 로또 개수 출력
     this.displayOutput.displayPaidLotto(lottoTickets);
 
-    // 구매한 로또 개수 만큼 로또 번호 생성
     const lottoPackage = this.game.generateLotto(lottoTickets);
 
-    // 구매한 로또 번호 출력
     this.displayOutput.displayLotto(lottoPackage);
 
     // 당청번호 및 보너스 번호 입력
@@ -28,13 +25,11 @@ class App {
     const splitWinningNumbers = this.game.splitWinningNumbers(winngingNumber);
     const bonusNumber = await this.userInput.getBonusNumber();
 
-    // 로또 결과 계산
     const lottoReult = this.game.checkLottoResult(lottoPackage, splitWinningNumbers, bonusNumber)
 
-    // 로또 결과 출력
+
     this.displayOutput.displayLottoResult(lottoReult);
 
-    // 수익률 계산 후 출력
     const profit = this.game.calculateProfit(lottoReult, paidMoney)
     this.displayOutput.displayProfit(profit);
   }
