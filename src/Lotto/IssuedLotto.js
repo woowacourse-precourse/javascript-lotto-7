38d8b8eacb452b874ffa7d8e9ed06bf6c5e15e58
Kim_Yjoo <pas4976@gmail.com>
2024-11-04 03:50:46 +0900

    feat : 당첨 번호와 발행된 번호를 비교하는 함수 추가

---
+++ src/Lotto/IssuedLotto.js
@@ -0,0 +1,34 @@
+class IssuedLotto extends Lotto {
+    #lottoTable;
+
+    constructor(numbers) {
+        super(numbers);
+        this.#lottoTable = this.applyLottoToTable(numbers);
+    }
+
+    static makeIssuedLotto() {
+      return new IssuedLotto(random.makeUniqueNumbers(1, 45, 6));
+    }
+    // makeLottoTable(numbers) {
+    //     let lottoTables = []
+    //     numbers.forEach((oneLotto) => {
+    //         tables.push(this.applyLottoToTable(oneLotto));
+    //     });
+    //     return lottoTables;
+    // }
+    applyLottoToTable(numbers) {
+        let lottoTable = Array.from({ length: 45 }, () => 0);
+        numbers.forEach((element) => {
+            lottoTable[element - 1] += 1;
+        });
+        return lottoTable;
+    }
+
+    getNumberOfMatches(answer) {
+        let matchNumber = 0;
+        answer.forEach((element) => {
+            matchNumber += this.#lottoTable[element];
+        });
+        return matchNumber;
+    }
+}
