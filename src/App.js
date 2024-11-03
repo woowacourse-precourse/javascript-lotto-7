47118bf865c89e72fd4a78f2a0ca517fa02e0b05
ahhyun <wndkgus0225@naver.com>
2024-11-04 00:52:08 +0900

    feat(App) : 당첨 번호 및 보너스 번호 함수 입력받아 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
-import { getCost, displayLottosCount, displayLottosNumber } from '../src/IO.js';
+import { getCost, displayLottosCount, displayLottosNumber, userPickedNumbers, userPickedBonusNumber, printResults } from '../src/IO.js';
+import { generateLottos } from '../src/Lotto.js';
+import { evaluateResults } from '../src/result.js';
 
 class App {
   // 1. 필드 : 이 클래스에서 필요한 속성 정의
@@ -14,6 +16,17 @@ class App {
       const count = await getCost();
       displayLottosCount(count);
       displayLottosNumber(count);
+      
+      const pickedNumbers = await userPickedNumbers();
+      const pickedBounsNumber = await userPickedBonusNumber(pickedNumbers);
+
+      // displayLottosCount(count);
+      // displayLottosNumber(lottos);
+
+      // const winningNumbers = await getWinningNumbers();
+      // const bonusNumber = await getBonusNumber();
+
+      // evaluateResults(lottos, winningNumbers, bonusNumber);
       
     } catch(error){
       Console.print(`[ERROR] 오류가 발생했습니다: ${error.message}`)
