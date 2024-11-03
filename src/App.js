b85f1057b4a54bdf619ffa63e5ae60734349eea9
seonae-j <wkdtjsdo135@naver.com>
2024-11-04 01:39:40 +0900

    fix(App): try-catch 구문으로 값 반환

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Match from './domains/Match.js';
+import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const match = new Match();
-    await match.getLottos();
-    await match.getJackpot();
-    match.resetRanks(); // 매 실행 시 rank를 초기화
-    match.matchLottos();
-    match.displayResult(); // 결과를 출력
+    try {
+      const match = new Match();
+      await match.getLottos();
+      await match.getJackpot();
+      match.resetRanks(); // 매 실행 시 rank를 초기화
+      match.matchLottos();
+      match.displayResult(); // 결과를 출력
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 }
 
