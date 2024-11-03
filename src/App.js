401afddc268907a2ff8a90f372a099c1364c8aaa
wowalswjd <alswjd5491@naver.com>
2024-11-03 20:02:06 +0900

    feat(App): 1~2. 로또 구입 금액 입력 및 유효성 검증 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+import Price from "./Price.js";
+
 class App {
-  async run() {}
+  async run() {
+    const priceInput = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    const price = new Price(priceInput);
+    Console.print("price : "+ price.getPrice());
+  }
 }
 
 export default App;
