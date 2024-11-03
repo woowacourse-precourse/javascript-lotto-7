0a7383c2355bc9c41a672a648eec1a9d4a82aa20
hyunyeee <5004ohy@naver.com>
2024-11-03 22:51:08 +0900

    test: 구입 금액이 로또 가격으로 나누어 떨어지지 않으면 에러를 던진다

--- __tests__/LottoMakerTest.js
+++ __tests__/LottoMakerTest.js
@@ -1,5 +1,6 @@
 import { LottoMaker } from "../src/lotto/LottoMaker.js";
 import { LOTTO_PRICE } from "../src/constant/constants.js";
+import { inValidMessages } from "../src/constant/message.js";
 
 describe('LottoMaker Class 테스트', () => {
   const priceArr = [1000, 3000, 5000];
@@ -7,5 +8,11 @@ describe('LottoMaker Class 테스트', () => {
     const generatedLotto = new LottoMaker().makeLotto(price);
 
     expect(generatedLotto).toHaveLength(price / LOTTO_PRICE);
+  })
+
+  const inDivisiblePriceArr = [200, 1100, 1500];
+  test.each(inDivisiblePriceArr)('구입 금액이 로또 가격으로 나누어 떨어지지 않으면 에러를 던진다.', (price) => {
+    const lottoMaker = new LottoMaker();
+    expect(() => lottoMaker.makeLotto(price)).toThrow(inValidMessages.priceUnit);
   })
 });
