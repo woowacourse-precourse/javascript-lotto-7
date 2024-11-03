2edd50f3b953fdb60c6d358b51eefc805910a6bc
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-04 01:05:24 +0900

    test:기능- 당첨 통계 출력

---
+++ __tests__/WinLotto.test.js
@@ -0,0 +1,51 @@
+import WinLotto from "../src/WinLotto";
+import Lotto from "../src/Lotto";
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("WinLotto 클래스 테스트", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("당첨 통계 출력 테스트", () => {
+    const purchasedLotto = [
+      [1, 2, 3, 4, 5, 6], // 6개 일치
+      [1, 2, 3, 4, 5, 7], // 5개 일치 + 보너스
+      [1, 2, 3, 4, 5, 8], // 5개 일치
+      [1, 2, 3, 4, 9, 10], // 4개 일치
+      [1, 2, 3, 11, 12, 13], // 3개 일치
+    ];
+    const winningLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = 7;
+
+    const logSpy = getLogSpy();
+    new WinLotto(purchasedLotto, winningLotto, bonusNumber);
+
+    const expectedLogs = [
+      "당첨 통계",
+      "---",
+      "3개 일치 (5,000원) - 1개",
+      "4개 일치 (50,000원) - 1개",
+      "5개 일치 (1,500,000원) - 1개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+      "6개 일치 (2,000,000,000원) - 1개",
+    ];
+
+    expectedLogs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
