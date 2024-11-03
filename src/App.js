3c6c1dd1c1392b608e9878daf03b375a2b0915f4
sanghee01 <sang6299@naver.com>
2024-11-04 01:46:22 +0900

    feat: 보너스 번호 입력시 에러 추가
    당첨 번호와 중복되는 경우 추가

--- src/App.js
+++ src/App.js
@@ -67,6 +67,9 @@ class App {
     if (!(bonusNumber >= 1 && bonusNumber <= 45)) {
       throw new Error('[ERROR] 보너스 번호는 1 ~ 45 사이의 숫자여야 합니다.');
     }
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+    }
   }
 }
 
