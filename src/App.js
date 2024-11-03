e02956b1750f9a841e9d29074090d30a0a355ccd
조혜린 <hyer1205@gmail.com>
2024-11-03 22:57:46 +0900

    Fix(app): ApplicationTest.js 테스트 코드 에러 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
 import LottoGame from './models/LottoGame.js';
 
 class App {
   async run() {
-    const lottoGame = new LottoGame();
-    await lottoGame.play();
+    try {
+      const lottoGame = new LottoGame();
+      await lottoGame.play();
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 }
 
