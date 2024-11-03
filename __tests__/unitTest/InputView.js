715b4d4050320fe998a4026d086811362a9ba479
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-11-04 02:19:29 +0900

    test : InputView에 대한 단위테스트 1. 구입금액 (에러, 통과) 2. 당첨번호 (에러, 통과) 3. 보너스번호 (에러, 통과)

---
+++ __tests__/unitTest/InputView.js
@@ -0,0 +1,71 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import InputView from '../../src/InputView.js';
+import App from '../../src/App.js';
+
+const mockQuestions = (input) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+  MissionUtils.Console.readLineAsync.mockImplementation(() =>
+    Promise.resolve(input)
+  );
+};
+
+describe('로또 구입 금액 입력 테스트', () => {
+  const ERROR_CASES = ['1001', '', 'd'];
+  test.each(ERROR_CASES)('구입 금액 실패', async (input) => {
+    mockQuestions(input);
+
+    await expect(InputView.readMoney()).rejects.toThrow('[ERROR]');
+  });
+
+  const SUCCESS_CASES = ['1000', '8000', ' 15000 '];
+  test.each(SUCCESS_CASES)('구입 금액 성공', async (input) => {
+    mockQuestions(input);
+
+    expect(await InputView.readMoney()).toEqual(input.trim());
+  });
+});
+
+describe('당첨 번호 입력 테스트', () => {
+  const ERROR_CASES = [
+    '1',
+    '1,2,3,4, ,6',
+    '1,2,3,4,5,6,7',
+    '1,2,3,4,5,d',
+    '1,2,3,4,5,78',
+    '1,1,2,3,4,5',
+  ];
+  test.each(ERROR_CASES)('당첨 번호 입력 실패', async (input) => {
+    mockQuestions(input);
+
+    await expect(InputView.readWinningNumber()).rejects.toThrow('[ERROR]');
+  });
+
+  const SUCCESS_CASES = ['1,2,3,4,5,6', '1,7,8,23,45,2'];
+  test.each(SUCCESS_CASES)('당첨 번호 입력 성공', async (input) => {
+    mockQuestions(input);
+
+    const winningNumbers = input.split(',').map((number) => number.trim());
+
+    expect(await InputView.readWinningNumber()).toEqual(winningNumbers);
+  });
+});
+
+describe('보너스 번호 입력 테스트', () => {
+  const app = new App();
+  app.winNumbers = ['1', '2', '3', '4', '5', '6'];
+
+  const ERROR_CASES = ['1', '', '76', 'd'];
+  test.each(ERROR_CASES)('보너스 번호 실패', async (input) => {
+    mockQuestions(input);
+
+    await expect(InputView.readBonusNumber(app.winNumbers)).rejects.toThrow(
+      '[ERROR]'
+    );
+  });
+  const SUCCESS_CASES = ['7', '21', '10'];
+  test.each(SUCCESS_CASES)('보너스 번호 성공', async (input) => {
+    mockQuestions(input);
+
+    expect(await InputView.readBonusNumber(app.winNumbers)).toEqual(input);
+  });
+});
