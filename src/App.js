332ac1a91e5582f640bc7c3c48451780256beba3
jsy <suyeonb1db@gmail.com>
2024-11-03 22:29:08 +0900

    feat: 당첨내역 및 수익률 출력하기

--- src/App.js
+++ src/App.js
@@ -18,6 +18,43 @@ class App {
 
     const jackpotNumber = await Input.getJackpotNumber();
     const bonusNumber = await Input.getBonusNumber(jackpotNumber);
+
+    const results = this.calculateResults(lottoList, jackpotNumber, bonusNumber);
+    Output.printStatics(results);
+
+    const totalPrize = this.calculateTotalPrize(results);
+    const profitRate = this.calculateRate(totalPrize, money);
+    Output.printProfitRate(profitRate);
+  }
+  
+  calculateResults(lottoList, jackpotNumber, bonusNumber) {
+    const lottoResult = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+
+    lottoList.forEach((lotto) => {
+      const matchCount = lotto.getNumbers().filter(num => jackpotNumber.includes(num)).length;
+      const hasBonus = lotto.getNumbers().includes(bonusNumber);
+
+      if (matchCount === 6) lottoResult[1] += 1;
+      else if (matchCount === 5 && hasBonus) lottoResult[2] += 1;
+      else if (matchCount === 5) lottoResult[3] += 1;
+      else if (matchCount === 4) lottoResult[4] += 1;
+      else if (matchCount === 3) lottoResult[5] += 1;
+    });
+    return lottoResult;
+  }
+
+  calculateTotalPrize(lottoResult) {
+    const prizeMoney = { 1: 2000000000, 2: 30000000, 3: 1500000, 4: 50000, 5: 5000 };
+    let totalPrize = 0;
+  
+    for (const rank in lottoResult) {
+      totalPrize += prizeMoney[rank] * lottoResult[rank];
+    }
+    return totalPrize;
+  }
+
+  calculateRate(totalPrize, purchaseAmount) {
+    return ((totalPrize / purchaseAmount) * 100).toFixed(1);
   }
 }
 
