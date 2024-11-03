8495a800c0e3a39a4be1d42bdf51506e6c14d651
aydenote <dslgpgh@gmail.com>
2024-11-04 00:28:08 +0900

    test(AppUnitTest): 로또 구매 개수 출력 단위 테스트 추가

--- __tests__/AppUnitTest.js
+++ __tests__/AppUnitTest.js
@@ -1,4 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import App from "../src/App.js";
+import Output from "../src/Output.js";
+
+const getLogSpy = () => {
+	const logSpy = jest.spyOn(MissionUtils.Console, "print");
+	logSpy.mockClear();
+	return logSpy;
+};
 
 describe("App 단위 테스트", () => {
 	// given
@@ -11,9 +19,25 @@ describe("App 단위 테스트", () => {
 	test.each(COUNT_CASES)("로또 구매 개수 계산", async (money, count) => {
 		//when
 		const app = new App();
-		const result = await app.calculateLottoCount(money);
+		const RESULT = await app.calculateLottoCount(money);
 
 		// then
-		expect(result).toBe(count);
+		expect(RESULT).toBe(count);
+	});
+
+	test("로또 구매 개수 출력", () => {
+		// given
+		const INPUT = 6;
+		const RESULT = ["6개를 구매했습니다."];
+
+		const logSpy = getLogSpy();
+
+		// when
+		const output = new Output();
+		output.printLottoCount(INPUT);
+
+		RESULT.forEach((output) => {
+			expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+		});
 	});
 });
