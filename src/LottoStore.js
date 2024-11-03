7f5b4ef1f9ddb5183ad19ea118c11ec03c578f2b
silver93hq <gkagjsrb@gmail.com>
2024-11-03 19:36:39 +0900

    feat: 로또 생성 기능 구현

---
+++ src/LottoStore.js
@@ -0,0 +1,38 @@
+import { Random } from '@woowacourse/mission-utils';
+import VALUES from './constants/Values.js';
+import Lotto from './Lotto.js';
+
+class LottoStore {
+  #lottos;
+
+  constructor(amount) {
+    this.#lottos = [];
+    this.#purchaseLottos(Number(amount));
+  }
+
+  #generateLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(
+      VALUES.lottoMinNumber,
+      VALUES.lottoMaxNumber,
+      VALUES.lottoLength,
+    ).sort((a, b) => a - b);
+  }
+
+  #purchaseOneLotto() {
+    const lottoNumbers = this.#generateLottoNumbers();
+    this.#lottos.push(new Lotto(lottoNumbers));
+  }
+
+  #purchaseLottos(amount) {
+    const quantity = amount / VALUES.amountUnit;
+    for (let i = 0; i < quantity; i += 1) {
+      this.#purchaseOneLotto();
+    }
+  }
+
+  getLottoNumbers() {
+    return this.#lottos.map((lotto) => lotto.getNumber());
+  }
+}
+
+export default LottoStore;
