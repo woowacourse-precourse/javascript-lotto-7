ca96cc58b4b35288499cc8f090fdcb24fae96a4b
ChangOKIM <kk104202@naver.com>
2024-11-04 01:24:22 +0900

    feat: Lotto 클래스에 번호 중복 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,8 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.length !== 6 || new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 6개여야 합니다.");
     }
   }
 
