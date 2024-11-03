1e3ce487c82406416f68cb2fe49c1befe41f9989
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-03 22:25:21 +0900

    ✅ test: LottoResult 클래스 기능 테스트 작성
    로또 배열 반환, 결과 계산, 수익률 계산

---
+++ __tests__/LottoResultTest.js
@@ -0,0 +1,57 @@
+import LottoResult from '../src/model/LottoResult.js';
+import LottoComparer from '../src/model/LottoComparer.js';
+import { PRIZE, INITIAL_RANK } from '../src/contents/PrizeContents.js';
+
+describe('LottoResult 클래스 테스트', () => {
+  let lottoResult;
+  let mockWinningNumbers;
+  let mockBonusNumber;
+  let mockLottos;
+  const ticketCount = 8;
+
+  beforeEach(() => {
+    // 초기 값 설정
+    mockWinningNumbers = [1, 2, 3, 4, 5, 6];
+    mockBonusNumber = 7;
+    mockLottos = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+
+    // LottoResult 객체 생성
+    lottoResult = new LottoResult(
+      mockWinningNumbers,
+      mockBonusNumber,
+      ticketCount,
+      mockLottos,
+    );
+  });
+
+  test('getLottos() 메서드가 로또 번호 배열을 반환해야 함', () => {
+    expect(lottoResult.getLottos()).toEqual(mockLottos);
+  });
+
+  test('calculateResults() 메서드가 당첨 결과를 정확하게 계산해야 함', () => {
+    const expectedRank = {
+      ...INITIAL_RANK,
+      fifth: 1, // 3개 일치 - 1개
+    };
+
+    const results = lottoResult.calculateResults();
+    expect(results).toEqual(expectedRank);
+  });
+
+  test('calculateProfitRate() 메서드가 올바른 수익률을 계산해야 함', () => {
+    const expectedProfitRate = (
+      ((PRIZE.fifth * 1) / (ticketCount * 1000)) *
+      100
+    ).toFixed(1); // 수익률 계산
+    expect(lottoResult.calculateProfitRate()).toBe(expectedProfitRate);
+  });
+});
