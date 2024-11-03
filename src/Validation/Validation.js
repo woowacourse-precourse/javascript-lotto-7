2b93fc4c50a00452fa6bd742473c1ee721171b45
BaeJia <qowldk1030@gmail.com>
2024-11-03 21:15:00 +0900

    feat: 로또 금액 입력받고 예외 처리하기

---
+++ src/Validation/Validation.js
@@ -0,0 +1,19 @@
+import { ERROR_MESSAGES, throwError } from "../Model/Error.js";
+
+function validatePrice(price) {
+  if (price === undefined || price === null || price === "") {
+    throwError(ERROR_MESSAGES.price.NOT_ENTERED);
+  }
+  if (isNaN(price)) {
+    throwError(ERROR_MESSAGES.price.MUST_BE_NUMERIC);
+  }
+
+  // 숫자가 1,000원 단위로 나누어 떨어지지 않는 경우 예외 처리
+  if (price % 1000 !== 0) {
+    throwError(ERROR_MESSAGES.price.MUST_BE_DIVISIBLE_BY_1000);
+  }
+
+  return price;
+}
+
+export { validatePrice };
