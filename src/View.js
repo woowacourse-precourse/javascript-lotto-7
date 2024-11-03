a5a377366815b230c58f67d187b3102a18125998
Chaesunbak <coanstjd0422@gmail.com>
2024-11-04 05:33:50 +0900

    refacotor : validatePurchaseMoney private 메서드로 변경

--- src/View.js
+++ src/View.js
@@ -11,12 +11,12 @@ class View {
     const PURCHASE_MONEY =
       await Console.readLineAsync("구입금액을 입력해 주세요.\n");
 
-    View.validatePurchaseMoney(PURCHASE_MONEY);
+    this.#validatePurchaseMoney(PURCHASE_MONEY);
 
     return PURCHASE_MONEY;
   };
 
-  static validatePurchaseMoney = (PURCHASE_MONEY) => {
+  #validatePurchaseMoney = (PURCHASE_MONEY) => {
     if (PURCHASE_MONEY.trim() === "") {
       Console.print("[ERROR] 구입금액을 입력해 주세요.");
       throw new Error("[ERROR] 구입금액을 입력해 주세요.");
@@ -34,12 +34,12 @@ class View {
     const PURCHASED_LOTTO_COUNT = LOTTOS.length;
     Console.print(`${PURCHASED_LOTTO_COUNT}개를 구매했습니다.`);
     for (const LOTTO of LOTTOS) {
-      this.printLotto(LOTTO);
+      this.#printLotto(LOTTO);
     }
     Console.print("");
   };
 
-  printLotto(LOTTO) {
+  #printLotto(LOTTO) {
     const numbersString = `[${LOTTO.getNumbers().join(", ")}]`;
     Console.print(numbersString);
   }
