098cc1da533b0086fb7602f5e7f8a5b457697520
Chaesunbak <coanstjd0422@gmail.com>
2024-11-04 03:16:24 +0900

    feat 당첨번호를 받아 등수를 반환하는 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,6 +15,33 @@ class Lotto {
   getNumbers() {
     return this.#numbers;
   }
+
+  getRank(WINNING_NUMBER, BONUS_NUMBER) {
+    const MATCHED_COUNT = this.#getMatchedCount(WINNING_NUMBER);
+    const IS_BONUS_MATCHED = this.#isBonusMatched(BONUS_NUMBER);
+
+    switch (MATCHED_COUNT) {
+      case 6:
+        return 1;
+      case 5:
+        return IS_BONUS_MATCHED ? 2 : 3;
+      case 4:
+        return 4;
+      case 3:
+        return 5;
+      default:
+        return 0;
+    }
+  }
+
+  #getMatchedCount(WINNING_NUMBER) {
+    return this.#numbers.filter((number) => WINNING_NUMBER.includes(number))
+      .length;
+  }
+
+  #isBonusMatched(BONUS_NUMBER) {
+    return this.#numbers.includes(BONUS_NUMBER);
+  }
 }
 
 export default Lotto;
