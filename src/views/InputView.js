79b698b2c2233c9f3d48cc0698f19e10ee8542d0
jeongwoo903 <www8565@naver.com>
2024-11-03 23:17:21 +0900

    feat(InputView): 당첨 번호 및 보너스 번호 입력 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -5,6 +5,14 @@ class InputView {
   async getPurchaseAmount() {
     return Console.readLineAsync(INPUT_MESSAGES.purchase_amount);
   }
+
+  async getWinningNumbers() {
+    return Console.readLineAsync(INPUT_MESSAGES.winning_numbers);
+  }
+
+  async getBonusNumber() {
+    return Console.readLineAsync(INPUT_MESSAGES.bonus_number);
+  }
 }
 
 export default InputView;
