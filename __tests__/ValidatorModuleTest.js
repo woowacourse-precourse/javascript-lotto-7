dc4cbf570ecb0916a0450cc7e37babc51e0a70b4
Parksangwoo <psu8430@gmail.com>
2024-11-03 16:43:36 +0900

    test(validateModule) : 구매 금액 검증 모듈 테스트 코드 추가

--- __tests__/ValidatorModuleTest.js
+++ __tests__/ValidatorModuleTest.js
@@ -22,4 +22,26 @@ describe('Validator Module Test', () => {
       expect(validate).not.toThrow();
     });
   });
+
+  describe('checkPurchaseCash : 구매 금액 입력값을 검증한다.', () => {
+    test.each([
+      ['빈 값이 입력되었을 경우', ''],
+      ['양의 정수가 들어오지 않은 경우', -1000],
+      ['단위 금액으로 나누어 떨어지지 않는 경우', 10020],
+    ])('%s, 에러를 발생시킨다.', (_, value) => {
+      const validate = () => {
+        ValidatorModule.checkPurchaseCash(value);
+      };
+
+      expect(validate).toThrow(ERROR_PREFIX);
+    });
+
+    test.each([1000, 12000])('유효한 구매 금액이 입력되었을 때 정상적으로 동작한다.', (value) => {
+      const validate = () => {
+        ValidatorModule.checkPurchaseCash(value);
+      };
+
+      expect(validate).not.toThrow(ERROR_PREFIX);
+    });
+  });
 });
