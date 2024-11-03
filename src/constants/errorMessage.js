bce136dd5ca32a41d8263bb89277fa24849b866d
장정안 <wjddks96@naver.com>
2024-11-03 22:21:30 +0900

    feat: 로또 구입 관련 에러 메세지 추가

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,3 +1,11 @@
 const ERROR_PREFIX = Object.freeze('[ERROR]');
 
-export { ERROR_PREFIX };
+const ERROR_MESSAGE = Object.freeze({
+  NO_INPUT: '구매 금액을 입력해주세요.',
+  NOT_THOUSAND_UNIT: '구매 금액은 1,000원 단위로 입력해야 합니다.',
+  NOT_NUMBER: '구매 금액은 숫자로 입력해야 합니다.',
+  LESS_THAN_ZERO: '구매 금액은 0원 이하일 수 없습니다.',
+  ZERO_AMOUNT: '구매 금액은 0원입니다.',
+});
+
+export { ERROR_PREFIX, ERROR_MESSAGE };
