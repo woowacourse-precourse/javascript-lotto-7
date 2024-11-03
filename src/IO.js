5a97344db985fb91eeb63ca3e18769911b4481d8
ahhyun <wndkgus0225@naver.com>
2024-11-04 00:48:43 +0900

    feat(IO) : 당첨 번호 및 보너스 번호 입력받는 함수 추가

--- src/IO.js
+++ src/IO.js
@@ -1,7 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from '../src/message.js';
-import { validateCost } from '../src/error.js';
+import { validateCost, validatePickedNumbers, validatePickedBonusNumbers } from '../src/error.js';
 import { generateLottos } from '../src/Lotto.js';
+import { evaluateResults } from '../src/result.js';
 
 export async function getCost() {
   const cost = await Console.readLineAsync(MESSAGE.ASK_COST+"\n");
@@ -19,4 +20,25 @@ export async function displayLottosNumber(count){
   lottos.forEach(lotto => {
     Console.print(`[${lotto.getNumbers().join(', ')}]`);
   });
+}
+
+export async function userPickedNumbers() {
+  const pickedNumbers = await Console.readLineAsync("\n"+MESSAGE.ASK_USER_PICK_NUMBER+"\n");
+  validatePickedNumbers(pickedNumbers);
+  return pickedNumbers.split(',').map(number => Number(number.trim()));
+} 
+
+export async function userPickedBonusNumber(pickedNumbers) {
+  const pickedBounsNumber = await Console.readLineAsync("\n"+MESSAGE.ASK_USER_PICK_BONUS_NUMBER+"\n");
+  validatePickedBonusNumbers(pickedBounsNumber, pickedNumbers);
+  return Number(pickedBounsNumber.trim()); 
+}
+
+
+export function printResults(rankCount) {
+  Console.print(MESSAGE.CORRECT_THREE+`${rankCount[3].count}개`);
+  Console.print(MESSAGE.CORRECT_FOUR+`${rankCount[4].count}개`);
+  Console.print(MESSAGE.CORRECT_FIVE+`${rankCount[5].count}개`);
+  Console.print(MESSAGE.CORRECT_FIVE_WITH_BONUS+`${rankCount['5+bonus'].count}개`);
+  Console.print(MESSAGE.CORRECT_SIX+`${rankCount[6].count}개`);
 }
