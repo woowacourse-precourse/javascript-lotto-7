1fc69f79835d4cd022ebc49e70c48f61a9d0b5b9
leelsj2007 <leelsj2007@gmail.com>
2024-11-04 03:27:23 +0900

    feat : App에서 controller 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Controller from "./controller/Controller.js";
+
 class App {
-  async run() {}
+  #controller;
+
+  constructor() {
+    this.#controller = new Controller();
+  }
+
+  async run() {
+    await this.#controller.run();
+  }
 }
 
 export default App;
