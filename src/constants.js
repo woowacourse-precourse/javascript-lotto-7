768c2594fcaa32e53752d1e366d409954ff9e5c1
mywhzy <cmj2393a@naver.com>
2024-11-04 04:06:50 +0900

    feat: 인풋 프롬프트, 구매 금액 에러 메세지 상수 정의

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const INPUT_PROMPT = {
+  purchaseAmount: '구매 금액을 입력해 주세요.',
+  winningNumbers: '당첨 번호를 입력해 주세요.',
+  bonusNumber: '보너스 번호를 입력해 주세요.',
+};
+
+export const ERROR_MESSAGE = {
+  prefix: '[ERROR]',
+  purchaseAmountError: '로또 구입은 최소 1,000원부터 시작해 1,000원 단위로만 구매 가능합니다.',
+};
