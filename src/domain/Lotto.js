b7473a4fc26dc0f12fa78c8927d768026d96026b
keemsebeen <sebin0580@gmail.com>
2024-11-04 00:45:35 +0900

    chore(Lotto) : Lotto 폴더 이동

---
+++ src/domain/Lotto.js
@@ -0,0 +1,12 @@
+import { ERROR_MESSAGE, LOTTO_ERROR } from "../constants/error.js";
+import { DELIMITER } from "../constants/message.js";
+import { DELIMITER_REGEX } from "../utils/regex.js";
+import LottoValidate from "../validation/LottoValidate.js";
+
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {}
+}
+
+export default Lotto;
