ea414fd34cd11cb5c94c6addb2c44accd6e0f521
jeong922 <hwajeong922@naver.com>
2024-11-03 20:54:58 +0900

    refactor(App): Validator 클래스 추가 및 유효성 검사 로직 분리
    - Validator 클래스를 새로 추가하여 구입 금액, 로또 번호, 보너스 번호에 대한 유효성 검사 로직을 분리

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import { PRIZE_MONEY } from './constants.js';
 import Lotto from './Lotto.js';
+import Validator from './validator.js';
 
 class App {
   constructor() {
     this.numbers = [];
     this.bonusNumber = null;
     this.lottoNumbers = null;
+    this.validator = new Validator();
   }
 
   async getPurchaseAmount() {
     const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
 
-    if (!this.validatePurchaseAmount(purchaseAmount)) {
+    if (!this.validator.validatePurchaseAmount(purchaseAmount)) {
       return;
     }
 
     return parseInt(purchaseAmount);
-  }
-
-  validatePurchaseAmount(purchaseAmount) {
-    if (!purchaseAmount || purchaseAmount.trim() === '') {
-      throw new Error('[ERROR] 구입 금액은 숫자로 입력해 주세요.');
-    }
-
-    if (!/^\d+$/.test(purchaseAmount)) {
-      throw new Error('[ERROR] 구입 금액은 숫자로 입력해 주세요.');
-    }
-
-    if (parseInt(purchaseAmount) % 1000 !== 0) {
-      throw new Error('[ERROR] 구입 금액을 1,000원 단위로 입력해 주세요.');
-    }
-
-    return true;
   }
 
   createLottoNumbers(purchaseAmount) {
@@ -50,11 +36,12 @@ class App {
 
   async getWinningNumbers() {
     const numbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
-    const lottoNumbers = numbers.split(',').map((number) => parseInt(number));
 
-    if (lottoNumbers.some((num) => isNaN(num) || num < 1 || num > 45)) {
-      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    if (!this.validator.validateLottoNumber(numbers)) {
+      return;
     }
+
+    const lottoNumbers = numbers.split(',').map((number) => parseInt(number));
 
     this.lottoNumbers = lottoNumbers;
 
@@ -102,17 +89,8 @@ class App {
   async getBonusNumber() {
     const bonusNumber = await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
 
-    if (
-      isNaN(parseInt(bonusNumber)) ||
-      bonusNumber.trim() === '' ||
-      parseInt(bonusNumber) < 1 ||
-      parseInt(bonusNumber) > 45
-    ) {
-      throw new Error('[ERROR] 1 ~ 45 사이의 숫자로 입력해 주세요.');
-    }
-
-    if (this.lottoNumbers.includes(parseInt(bonusNumber))) {
-      throw new Error('[ERROR] 중복된 번호 입니다.');
+    if (!this.validator.validateBonusNumber(this.lottoNumbers, bonusNumber)) {
+      return;
     }
 
     return parseInt(bonusNumber);
