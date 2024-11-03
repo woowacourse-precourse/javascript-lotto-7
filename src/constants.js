653bd503a1dcad87b167f3c73d2570ba5ecd840a
LeeJinyoung218 <amy021899@gmail.com>
2024-11-03 22:17:41 +0900

    fix: 당첨 통계 메세지 수정

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const MSG_PURCHASE_FEE = "구입금액을 입력해 주세요.";
 export const MSG_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 export const MSG_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-export const MSG_WINNING_STAT = "당첨 통계\n---"
+export const MSG_WINNING_STAT = "당첨 통계\n---\n"
 export const ERR_MSG_PURCHASE_FEE = "[ERROR] 1000원 단위를 입력해주세요.";
 export const ERR_MSG_WINNING_NUMBERS_INVALID_STRING = "[ERROR] 숫자와 구분자(,)로만 입력 가능합니다.";
 export const ERR_MSG_WINNING_NUMBERS_INVALID_LENGTH = "[ERROR] 로또 번호는 6개여야 합니다."
