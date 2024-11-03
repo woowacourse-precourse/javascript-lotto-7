893b61e11e55ad8000dcde086233476990715947
hyeongdong <khd941018@gmail.com>
2024-11-04 03:13:19 +0900

    refactor(LottoMatchCheckerTest): 클래스 변경사항에 맞게 테스트 수정

--- __tests__/LottoMatchCheckerTest.js
+++ __tests__/LottoMatchCheckerTest.js
@@ -4,22 +4,22 @@ import Lotto from "../src/Lotto";
 describe("LottoMatchChecker 클래스 테스트", () => {
   test("당첨번호와 일치하는 로또 번호의 개수를 구할 수 있다.", () => {
     const lotto = new Lotto([1, 3, 5, 7, 9, 10]);
-    const lottoMatchChecker = new LottoMatchChecker(4, [1, 3, 5, 7, 9, 10]);
+    const lottoMatchChecker = new LottoMatchChecker();
 
-    expect(lottoMatchChecker.countMatchesIn(lotto)).toBe(6);
+    expect(lottoMatchChecker.countMatches([1, 3, 5, 7, 9, 10], lotto)).toBe(6);
   });
 
   test("보너스 번호와 일치하는 로또 번호가 존재하면 true를 리턴할 수 있다.", () => {
     const lotto = new Lotto([1, 3, 5, 7, 9, 4]);
-    const lottoMatchChecker = new LottoMatchChecker(4, [1, 3, 5, 7, 9, 10]);
+    const lottoMatchChecker = new LottoMatchChecker();
 
-    expect(lottoMatchChecker.isBonusNumberIn(lotto)).toBeTruthy();
+    expect(lottoMatchChecker.hasBonusNumber(4, lotto)).toBeTruthy();
   });
 
   test("보너스 번호와 일치하는 로또 번호가 없으면 false를 리턴할 수 있다.", () => {
     const lotto = new Lotto([1, 3, 5, 7, 9, 44]);
     const lottoMatchChecker = new LottoMatchChecker(4, [1, 3, 5, 7, 9, 10]);
 
-    expect(lottoMatchChecker.isBonusNumberIn(lotto)).toBeFalsy();
+    expect(lottoMatchChecker.hasBonusNumber(4, lotto)).toBeFalsy();
   });
 });
