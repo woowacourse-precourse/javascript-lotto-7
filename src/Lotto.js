12260707923572ae06344104a8b181d01f2eeac9
ChangOKIM <kk104202@naver.com>
2024-11-04 01:24:54 +0900

    feat: Lotto 클래스에 번호 범위 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,9 @@ class Lotto {
     if (numbers.length !== 6 || new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않는 6개여야 합니다.");
     }
+    if (numbers.some(num => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
