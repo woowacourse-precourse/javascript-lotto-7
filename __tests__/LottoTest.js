30842ec265b58c7847ffb74b192865d1dd68858d
silver93hq <gkagjsrb@gmail.com>
2024-11-03 19:32:18 +0900

    test(Lotto): 로또 추첨 기능 테스트코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,23 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
+  describe.each([[[1, 2, 3, 4, 5, 6], 7]])(
+    '로또 추첨 결과 테스트 ( 당첨번호 : %s 보너스번호 : %s ',
+    (winningNumber, bonusNumber) => {
+      test.each([
+        [[1, 2, 3, 4, 5, 6], 6, false],
+        [[1, 2, 3, 4, 5, 7], 5, true],
+        [[1, 2, 3, 4, 5, 8], 5, false],
+        [[1, 2, 3, 4, 8, 9], 4, false],
+        [[1, 2, 3, 8, 9, 10], 3, false],
+      ])(' 입력 : %s 결과 : %s )', (lottoNumbers, expectedMatchCount, expectedMatchBonus) => {
+        const { matchCount, matchBonus } = new Lotto(lottoNumbers).draw(winningNumber, bonusNumber);
+
+        expect(matchCount).toBe(expectedMatchCount);
+        expect(matchBonus).toBe(expectedMatchBonus);
+      });
+    },
+  );
+
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 });
