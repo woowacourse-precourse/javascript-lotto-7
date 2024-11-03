a5f4e00e6a6c36c66960fcaf701a675684e13af2
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-11-04 06:53:14 +0900

    ✨ feat(error): `ERROR_MESSAGE` 상수 추가
    - ERROR_MESSAGE 상수를 추가하여 다양한 오류 메시지를 관리할 수 있도록 개선하였습니다.
    - 기존에 export 해서 외부에서 사용했던 `ERROR_PREFIX`를 `ERROR_MESSAGE` 객체 내에서 사용하도록 변경해 코드의 일관성과 가독성을 향상시켰습니다.

--- src/Validator.js
+++ src/Validator.js
@@ -1,9 +1,9 @@
-class Validator {
-  #ERROR_PREFIX = '[ERROR] ';
+import { ERROR_MESSAGE } from './constant/error';
 
+class Validator {
   #validateNaturalNumber(value) {
     if (!Number.isInteger(value) || value <= 0) {
-      throw new Error(`${this.#ERROR_PREFIX} 자연`);
+      throw new Error(ERROR_MESSAGE.NOT_NATURAL_NUMBER);
     }
   }
 
--- src/constant/error.js
+++ src/constant/error.js
@@ -1,1 +1,5 @@
-export const ERROR_PREFIX = '[ERROR] ';
+const ERROR_PREFIX = '[ERROR] ';
+
+export const ERROR_MESSAGE = Object.freeze({
+  NOT_NATURAL_NUMBER: `${ERROR_PREFIX} 유효한 자연수여야 합니다..`,
+});
