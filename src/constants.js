92af1883b92fa2e8e412272ad4dbd88f3e434fb1
LeeJinyoung218 <amy021899@gmail.com>
2024-11-03 20:12:14 +0900

    feat: 에러 메세지 추가

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,6 @@ export const MSG_PURCHASE_FEE = "구입금액을 입력해 주세요.";
 export const MSG_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 export const MSG_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 export const MSG_WINNING_STAT = "당첨 통계\n---"
+export const ERR_MSG_PURCHASE_FEE = "[ERROR] 1000원 단위를 입력해주세요.";
+export const ERR_MSG_WINNING_NUMBERS = "[ERROR] 당첨 번호는 중복 없는 1 ~ 45의 숫자 6개만 가능합니다.";
+export const ERR_MSG_BONUS_NUMBER = "[ERROR] 보너스 번호는 1 ~ 45의 숫자 1개만 가능합니다."
