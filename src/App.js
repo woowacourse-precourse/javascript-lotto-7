979b2eb6dc2bb873974e94ed9bbf2af6e85f047c
Rigu1 <dlsrbtla@gmail.com>
2024-11-04 02:37:24 +0900

    feat(App): Add methods to get winning numbers and bonus number
    - getWinningNumbers와 getBonusNumber 메서드를 통해 당첨 번호와 보너스 번호 입력 받기 기능 추가
    - winningNumbers 입력값을 콤마로 분리하여 숫자 배열로 변환
    - bonusNumber 입력값을 숫자로 변환하여 반환

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import InputFormatter from "./InputFormatter";
+import LottoPurchase from "./LottoPurchase";
+import Constants from "./Constants";
+import LottoWinningStatus from "./LottoWinningStatus"
+import Lotto from "./Lotto";
 
 class App {
-  async #getInput() {
-    const purchaseAmount = await MissionUtils.Console.readLineAsync();
-    const winningNumbers = await MissionUtils.Console.readLineAsync();
-    const bonusNumber = await MissionUtils.Console.readLineAsync();
-    
-    return [purchaseAmount, winningNumbers, bonusNumber];
-  }
-  async run() {
-    const { purchaseAmount, winningNumbers, bonusNumber } = await this.#getInput();
-    const inputFormatter = new InputFormatter(
-      purchaseAmount,
-      winningNumbers,
-      bonusNumber
+  async #getPurchaseAmount() {
+    const purchaseAmount = await MissionUtils.Console.readLineAsync(
+      Constants.INPUT_MESSAGES.PURCHASE_AMOUNT
     );
+    return parseFloat(purchaseAmount);
+  }
+
+  async #getWinningNumbers() {
+    const winningNumbers = await MissionUtils.Console.readLineAsync(
+      Constants.INPUT_MESSAGES.WINNING_NUMBERS
+    );
+    return winningNumbers
+      .split(Constants.DELIMITER)
+      .map((number) => parseFloat(number.trim()));
+  }
+
+  async #getBonusNumber() {
+    const bonusNumber = await MissionUtils.Console.readLineAsync(
+      Constants.INPUT_MESSAGES.BONUS_NUMBER
+    );
+    return parseFloat(bonusNumber);
+  }
+
+  async run() {
+    const purchaseAmount = await this.#getPurchaseAmount();
+    const lottoPurchase = new LottoPurchase(purchaseAmount);
+    const purchaselottoList = lottoPurchase.getPurchaseLottoList();
+
+    const winningNumbers = await this.#getWinningNumbers();
+    const bonusNumber = await this.#getBonusNumber();
   }
 }
 
