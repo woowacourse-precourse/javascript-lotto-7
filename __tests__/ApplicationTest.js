35c3bb84a1622e692f3140ab566c1df64c0fc7d5
sky121016 <sky121016@naver.com>
2024-11-03 23:02:38 +0900

    style: 코딩 컨벤션 준수

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -19,7 +19,7 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
@@ -29,7 +29,7 @@ const runException = async (input) => {
   const logSpy = getLogSpy();
 
   const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-  const INPUT_NUMBERS_TO_END = ["1000", "1,2,3,4,5,6", "7"];
+  const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,6', '7'];
 
   mockRandoms([RANDOM_NUMBERS_TO_END]);
   mockQuestions([input, ...INPUT_NUMBERS_TO_END]);
@@ -39,7 +39,7 @@ const runException = async (input) => {
   await app.run();
 
   // then
-  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
 };
 
 const testInputError = async (input) => {
@@ -49,22 +49,21 @@ const testInputError = async (input) => {
   mockRandoms([[7, 8, 9, 10, 11, 12]]);
   mockQuestions(input);
 
-
   // when
   const app = new App();
   await app.run();
 
   // then
-  const logs = logSpy.mock.calls.map(call => call[0]);
-  expect(logs).toContainEqual(expect.stringContaining("[ERROR]"));
+  const logs = logSpy.mock.calls.map((call) => call[0]);
+  expect(logs).toContainEqual(expect.stringContaining('[ERROR]'));
 };
 
-describe("로또 테스트", () => {
+describe('로또 테스트', () => {
   beforeEach(() => {
     jest.restoreAllMocks();
   });
 
-  test("기능 테스트", async () => {
+  test('기능 테스트', async () => {
     // given
     const logSpy = getLogSpy();
 
@@ -78,7 +77,7 @@ describe("로또 테스트", () => {
       [2, 13, 22, 32, 38, 45],
       [1, 3, 5, 14, 22, 45],
     ]);
-    mockQuestions(["8000", "1,2,3,4,5,6", "7"]);
+    mockQuestions(['8000', '1,2,3,4,5,6', '7']);
 
     // when
     const app = new App();
@@ -86,21 +85,21 @@ describe("로또 테스트", () => {
 
     // then
     const logs = [
-      "8개를 구매했습니다.",
-      "[8, 21, 23, 41, 42, 43]",
-      "[3, 5, 11, 16, 32, 38]",
-      "[7, 11, 16, 35, 36, 44]",
-      "[1, 8, 11, 31, 41, 42]",
-      "[13, 14, 16, 38, 42, 45]",
-      "[7, 11, 30, 40, 42, 43]",
-      "[2, 13, 22, 32, 38, 45]",
-      "[1, 3, 5, 14, 22, 45]",
-      "3개 일치 (5,000원) - 1개",
-      "4개 일치 (50,000원) - 0개",
-      "5개 일치 (1,500,000원) - 0개",
-      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-      "6개 일치 (2,000,000,000원) - 0개",
-      "총 수익률은 62.5%입니다.",
+      '8개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 8, 11, 31, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '[2, 13, 22, 32, 38, 45]',
+      '[1, 3, 5, 14, 22, 45]',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 62.5%입니다.',
     ];
 
     logs.forEach((log) => {
@@ -108,23 +107,23 @@ describe("로또 테스트", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    await runException("1000j");
+  test('예외 테스트', async () => {
+    await runException('1000j');
   });
 
-  test("1000으로 나누어 떨어지지 않는 경우", async () => {
-    await runException("1010");
+  test('1000으로 나누어 떨어지지 않는 경우', async () => {
+    await runException('1010');
   })
 
-  test(",가 아닌 문자가 입력된 경우", async () => {
-    await testInputError(["1000", "1/2/3/4/5/6", "7"]);
+  test(',가 아닌 문자가 입력된 경우', async () => {
+    await testInputError(['1000', '1/2/3/4/5/6', '7']);
   })
   
-  test("보너스 번호가 중복된 경우", async () => {
-    await testInputError(["1000", "1,2,3,4,5,6", "3"]);
+  test('보너스 번호가 중복된 경우', async () => {
+    await testInputError(['1000', '1,2,3,4,5,6', '3']);
   })
 
-  test("당첨 번호가 범위를 벗어난 경우", async () => {
-    await testInputError(["1000", "1,200,3,4,5,6", "3"]);
-  })
+  test('당첨 번호가 범위를 벗어난 경우', async () => {
+    await testInputError(['1000', '1,200,3,4,5,6', '3']);
+  });
 });
