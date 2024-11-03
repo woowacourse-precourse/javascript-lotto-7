9a0d5070d5009e77c4bcb846365c539773d50742
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-11-03 17:56:09 +0900

    refactor : 로또결과 클래스 생성 및 작성 && 코드수정에 따른 app.js수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import { Console } from "@woowacourse/mission-utils";
-import Lotto from "./Lotto.js";
-import Bet from "./Bet.js";
 import Input from "./Inputs.js";
+import LottoResult from "./LottoResult.js";
 
 class App {
   async run() {
     const input = new Input();
     const bet = await input.inputMoney();
-    const betList = bet.getBetResults();
     const lotto = await input.InputLotto();
     await lotto.inputBonusNumber();
-    const reward = lotto.compareLottoList(betList);
-    Console.print(
-      `총 수익률은 ${((reward / bet.getMoney()) * 100).toFixed(1)}%입니다.`
-    );
+    
+    const lottoResult = new LottoResult(lotto.getLottoScore(bet.getBetResults()));
+    lottoResult.printWinningResult();
+    lottoResult.printRateOfReturn(bet.getMoney());
   }
 }
 
