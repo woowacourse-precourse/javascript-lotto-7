0ed6d25431bfb61daa4d48ed610c6496b06b3924
Baek <woonn97@gmail.com>
2024-11-04 01:28:43 +0900

    feat: 로또 validate 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from "./constants.js";
+
 class Lotto {
   #numbers;
 
@@ -8,7 +10,19 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR_MESSAGE.OVER_LENGTH_NUMBER);
+    }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error(ERROR_MESSAGE.DUPLICATE_NUMBER);
+    }
+
+    if (!numbers.every((num) => num >= 1 && num <= 45)) {
+      throw new Error(ERROR_MESSAGE.OVER_NUMBER);
+    }
+
+    if (numbers.some((num) => Number.isNaN(num))) {
+      throw new Error(ERROR_MESSAGE.NOT_NUMBER);
     }
   }
 
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const MESSAGE = {
+  // 출력 메시지
+  ERROR: "[ERROR]",
+};
+
+export const ERROR_MESSAGE = {
+  NOT_NUMBER: `${MESSAGE.ERROR} 로또 번호가 숫자가 아닙니다.`,
+  DUPLICATE_NUMBER: `${MESSAGE.ERROR} 로또 번호가 중복되었습니다.`,
+  OVER_LENGTH_NUMBER: `${MESSAGE.ERROR} 로또 번호는 6개여야 합니다.`,
+  OVER_NUMBER: `${MESSAGE.ERROR} 로또 번호는 1~45 사이여야 합니다.`,
+};
