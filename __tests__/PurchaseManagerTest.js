5a83eaf719b7982deda3f8e617f72a2151e46afc
hyeongdong <khd941018@gmail.com>
2024-11-04 03:08:54 +0900

    refactor(PurchaseManagerTest): 클래스 변경 사항에 맞게 테스트 수정

--- __tests__/PurchaseManagerTest.js
+++ __tests__/PurchaseManagerTest.js
@@ -1,21 +1,9 @@
-import BudgetManager from "../src/BudgetManger";
+import PurchaseManager from "../src/PurchaseManager";
 
-describe("BudgetManager 클래스 테스트", () => {
-  test("입력 금액이 로또 가격으로 나누어떨어지지 않으면 예외가 발생한다.", () => {
-    expect(() => {
-      new BudgetManager(5200, 1000);
-    }).toThrow("[ERROR]");
-  });
-
-  test("입력 금액이 로또 가격보다 작으면 예외가 발생한다.", () => {
-    expect(() => {
-      new BudgetManager(200, 1000);
-    }).toThrow("[ERROR]");
-  });
-
+describe("PurchaseManager 클래스 테스트", () => {
   test("구매가능한 최대 로또 개수를 구할 수 있다.", () => {
-    const budgetManager = new BudgetManager(7000, 1000);
+    const purchaseManager = new PurchaseManager(1000);
 
-    expect(budgetManager.calculateMaxLottos()).toBe(7);
+    expect(purchaseManager.calculateMaxLottos(7000)).toBe(7);
   });
 });
