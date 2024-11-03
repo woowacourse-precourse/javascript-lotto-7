a907fffa982a81e70bb33b252bc9f1cb047ba8d5
erica-jeong <wjddbwjd1205@gmail.com>
2024-11-04 04:16:15 +0900

    style: 불필요한 공백 삭제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import LottoManager from "./LottoManager.js";
 class App {
   async run() {
-    const lottoManager = new LottoManager();  
+    const lottoManager = new LottoManager();
     await lottoManager.start();
   }
 }
