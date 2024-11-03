67937621f15dab56967cbc76b63157356e08acc3
taeyomi <terry11ty@naver.com>
2024-11-03 21:50:21 +0900

    feat(error): 당첨번호 유효성검사 에러 메세지 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,6 +1,10 @@
 const ERROR_MESSAGE = Object.freeze({
   INPUT_AMOUNT:
     "[ERROR] 구입 금액은 1,000원 단위여야 하며, 1,000원으로 나누어 떨어져야 합니다.",
+  WINNING_NUMBER_RANGE:
+    "[ERROR] 당첨번호는 1~45 사이의 숫자로 이루어져야 합니다.",
+  WINNING_NUMBER_DUP:
+    "[ERROR] 당첨번호는 겹치지 않는 6개의 숫자로 이루어져야 합니다.",
 });
 
 export default ERROR_MESSAGE;
