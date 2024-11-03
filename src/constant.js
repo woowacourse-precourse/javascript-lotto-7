0d9652ec3dd05ac4bfc769c76376cc36366ad96d
osw6858 <osw7875@naver.com>
2024-11-03 20:04:45 +0900

    feat: 상수값 정의 객체 추가

---
+++ src/constant.js
@@ -0,0 +1,13 @@
+export const MESSAGE = {
+  inputAmount: '구입금액을 입력해 주세요.\n',
+  inputWinningNumber: '\n당첨 번호를 입력해 주세요.\n',
+  inputBonusNumber: '\n보너스 번호를 입력해 주세요.\n',
+  printLottoCount: '개를 구매했습니다.',
+};
+
+export const ERROR = {
+  message: '[ERROR] 올바른 값을 입력해 주세요.',
+};
+
+Object.freeze(MESSAGE);
+Object.freeze(ERROR);
