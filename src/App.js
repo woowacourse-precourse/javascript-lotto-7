a357240c497d7f6c122e27e21c720a625a95fe07
jinho-waah <andrewjo950515@gmail.com>
2024-11-03 20:19:50 +0900

    feat($App.js): 구입 금액 입력 구현
    입력값에 대한 에러 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const purchase = await this.getPurchase();
+    } catch (error) {
+      throw new Error("[ERROR]" + error.message);
+    }
+  }
+
+  async getPurchase() {
+    const moneyString = await Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    const moneyNumber = parseInt(moneyString);
+    if (isNaN(moneyNumber) || moneyNumber % 1000 !== 0) {
+      throw new Error("구입 금액은 1,000원 단위로 입력해야 합니다.");
+    }
+    return moneyNumber;
+  }
 }
 
 export default App;
