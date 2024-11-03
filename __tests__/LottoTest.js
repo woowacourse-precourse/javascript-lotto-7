7bf1d40de865083547792bf18cb1c5e45fd92645
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-11-04 05:12:48 +0900

    feat(LottoTest): 로또 객체 생성 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,17 +1,23 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
+import Validation from '../src/Validation';
+import Logic from '../src/Logic';
+import Input from '../src/Input';
+import { ErrorMessage } from '../src/Enum';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+      if (!Validation.validateWinningNumbers('1,2,3,4,5,5')) {
+        throw new Error(ErrorMessage.WIN_NUM_ERROR);
+      }
+    }).toThrow(ErrorMessage.WIN_NUM_ERROR);
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
