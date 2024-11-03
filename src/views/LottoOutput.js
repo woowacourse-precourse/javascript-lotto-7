ed1b45e1c4333d3ad96629896da68a6778054768
yeeun426 <thsudkcla7@naver.com>
2024-11-04 01:53:46 +0900

    feat : 발행 수량 만큼 로또 번호 출력

--- src/views/LottoOutput.js
+++ src/views/LottoOutput.js
@@ -1,9 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
 import { LOTTO_OUTPUT } from "../constants/Constants.js";
+import LottoController from "../controllers/LottoController.js";
 
 class LottoOutput {
+  static lottoController = new LottoController();
+
   static printLottoCnt(lottoCnt) {
     Console.print(LOTTO_OUTPUT.LOTTO_CNT_OUTPUT(lottoCnt));
+    this.printLottoNum(lottoCnt);
+  }
+
+  static printLottoNum(lottoCnt) {
+    for (let i = 0; i < lottoCnt; i++) {
+      let lottoNum = this.lottoController.makeLottoNum();
+      Console.print(lottoNum);
+    }
   }
 }
 
