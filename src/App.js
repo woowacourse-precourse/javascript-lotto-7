ef798d04ecb4da98d30154b42f3d7eb7f9193534
handbefore <jiwon010206@gmail.com>
2024-11-03 22:33:14 +0900

    구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Random, Console, MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
   async run() {
+    // 로또 구입금액 숫자 입력 받기
+    const inputlottospurchase = await Console.readLineAsync(
+      '구입금액을 입력해 주세요.\n'
+    );
+    
+    // 입력값을 숫자로 변환
+    const purchaseAmount = parseInt(inputlottospurchase, 10);
+
+    
   }
 }
 
 
 export default App;
+접기
