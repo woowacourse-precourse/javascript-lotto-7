2105aeb875de403b8f8ae38be0cf89596dd2e08a
SanghyunLee <lgw9736@naver.com>
2024-11-03 17:28:43 +0900

    test: 로또 결과 테스트 코드 추가 및 리팩토링
    - 로또 결과 금액 검증 추가
    - 테스트 수행때 이루어지는 상수 문자열 lottoConfig 파일 이용해서 처리

--- __tests__/LottoResultTest.js
+++ __tests__/LottoResultTest.js
@@ -1,88 +1,170 @@
-import { lottoConfig } from '../src/models/lottoConfig.js';
+import {
+  LOTTO_CONFIG,
+  WINNING_CONDITIONS_DESC,
+} from '../src/constants/lottoConfig.js';
 import LottoResult from '../src/models/LottoResult.js';
 
 describe('로또 결과 기능 테스트', () => {
   let lottoResult;
 
+  // 각 테스트마다 새로운 인스턴스를 생성
   beforeEach(() => {
-    lottoResult = new LottoResult(); // 각 테스트마다 새로운 인스턴스를 생성
+    lottoResult = new LottoResult();
   });
 
   test('3개 일치, 보너스 번호 불일치 결과 검증', () => {
-    lottoResult.saveResult(3, false);
+    // given
+    const matchCount = 3;
+    const isBonusMatch = false;
 
+    // when
+    lottoResult.saveResult(matchCount, isBonusMatch);
+
+    // then
     const result = lottoResult.getResult();
+    const resultPrice = lottoResult.getResultPrice();
+
     expect(result).toEqual({
-      '3개 일치': 1,
-      '4개 일치': 0,
-      '5개 일치': 0,
-      '5개 일치, 보너스 볼 일치': 0,
-      '6개 일치': 0,
+      [WINNING_CONDITIONS_DESC.MATCH_3]: 1,
+      [WINNING_CONDITIONS_DESC.MATCH_4]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5_AND_BONUS]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_6]: 0,
     });
+    expect(resultPrice).toBe(
+      LOTTO_CONFIG.WINNING_PRIZE_MAP[
+        LOTTO_CONFIG.WINNING_CONDITIONS[matchCount][isBonusMatch]
+      ],
+    );
   });
 
   test('3개 일치, 보너스 번호 일치 결과 검증', () => {
-    lottoResult.saveResult(3, true);
+    // given
+    const matchCount = 3;
+    const isBonusMatch = true;
 
+    // when
+    lottoResult.saveResult(matchCount, isBonusMatch);
+
+    // then
     const result = lottoResult.getResult();
+    const resultPrice = lottoResult.getResultPrice();
+
     expect(result).toEqual({
-      '3개 일치': 1,
-      '4개 일치': 0,
-      '5개 일치': 0,
-      '5개 일치, 보너스 볼 일치': 0,
-      '6개 일치': 0,
+      [WINNING_CONDITIONS_DESC.MATCH_3]: 1,
+      [WINNING_CONDITIONS_DESC.MATCH_4]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5_AND_BONUS]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_6]: 0,
     });
+    expect(resultPrice).toBe(
+      LOTTO_CONFIG.WINNING_PRIZE_MAP[
+        LOTTO_CONFIG.WINNING_CONDITIONS[matchCount][isBonusMatch]
+      ],
+    );
   });
 
   test('4개 일치, 보너스 번호 일치 결과 검증', () => {
-    lottoResult.saveResult(4, true);
+    // given
+    const matchCount = 4;
+    const isBonusMatch = true;
 
+    // when
+    lottoResult.saveResult(matchCount, isBonusMatch);
+
+    // then
     const result = lottoResult.getResult();
+    const resultPrice = lottoResult.getResultPrice();
+
     expect(result).toEqual({
-      '3개 일치': 0,
-      '4개 일치': 1,
-      '5개 일치': 0,
-      '5개 일치, 보너스 볼 일치': 0,
-      '6개 일치': 0,
+      [WINNING_CONDITIONS_DESC.MATCH_3]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_4]: 1,
+      [WINNING_CONDITIONS_DESC.MATCH_5]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5_AND_BONUS]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_6]: 0,
     });
+    expect(resultPrice).toBe(
+      LOTTO_CONFIG.WINNING_PRIZE_MAP[
+        LOTTO_CONFIG.WINNING_CONDITIONS[matchCount][isBonusMatch]
+      ],
+    );
   });
 
   test('5개 일치, 보너스 번호 불일치 결과 검증', () => {
-    lottoResult.saveResult(5, false);
+    // given
+    const matchCount = 5;
+    const isBonusMatch = false;
 
+    // when
+    lottoResult.saveResult(matchCount, isBonusMatch);
+
+    // then
     const result = lottoResult.getResult();
+    const resultPrice = lottoResult.getResultPrice();
+
     expect(result).toEqual({
-      '3개 일치': 0,
-      '4개 일치': 0,
-      '5개 일치': 1,
-      '5개 일치, 보너스 볼 일치': 0,
-      '6개 일치': 0,
+      [WINNING_CONDITIONS_DESC.MATCH_3]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_4]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5]: 1,
+      [WINNING_CONDITIONS_DESC.MATCH_5_AND_BONUS]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_6]: 0,
     });
+    expect(resultPrice).toBe(
+      LOTTO_CONFIG.WINNING_PRIZE_MAP[
+        LOTTO_CONFIG.WINNING_CONDITIONS[matchCount][isBonusMatch]
+      ],
+    );
   });
 
   test('5개 및 보너스 번호 일치 결과 검증', () => {
-    lottoResult.saveResult(5, true);
+    // given
+    const matchCount = 5;
+    const isBonusMatch = true;
 
+    // when
+    lottoResult.saveResult(matchCount, isBonusMatch);
+
+    // then
     const result = lottoResult.getResult();
+    const resultPrice = lottoResult.getResultPrice();
+
     expect(result).toEqual({
-      '3개 일치': 0,
-      '4개 일치': 0,
-      '5개 일치': 0,
-      '5개 일치, 보너스 볼 일치': 1,
-      '6개 일치': 0,
+      [WINNING_CONDITIONS_DESC.MATCH_3]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_4]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5_AND_BONUS]: 1,
+      [WINNING_CONDITIONS_DESC.MATCH_6]: 0,
     });
+    expect(resultPrice).toBe(
+      LOTTO_CONFIG.WINNING_PRIZE_MAP[
+        LOTTO_CONFIG.WINNING_CONDITIONS[matchCount][isBonusMatch]
+      ],
+    );
   });
 
   test('6개 일치, 보너스 번호 일치 결과 검증', () => {
-    lottoResult.saveResult(6, true);
+    // given
+    const matchCount = 6;
+    const isBonusMatch = true;
 
+    // when
+    lottoResult.saveResult(matchCount, isBonusMatch);
+
+    // then
     const result = lottoResult.getResult();
+    const resultPrice = lottoResult.getResultPrice();
+
     expect(result).toEqual({
-      '3개 일치': 0,
-      '4개 일치': 0,
-      '5개 일치': 0,
-      '5개 일치, 보너스 볼 일치': 0,
-      '6개 일치': 1,
+      [WINNING_CONDITIONS_DESC.MATCH_3]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_4]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_5_AND_BONUS]: 0,
+      [WINNING_CONDITIONS_DESC.MATCH_6]: 1,
     });
+    expect(resultPrice).toBe(
+      LOTTO_CONFIG.WINNING_PRIZE_MAP[
+        LOTTO_CONFIG.WINNING_CONDITIONS[matchCount][isBonusMatch]
+      ],
+    );
   });
 });
