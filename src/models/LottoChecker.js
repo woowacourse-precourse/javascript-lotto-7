0e4bbeaf758c4eddeb22dd45e44b974d1f03080d
정해성 <gotjd0230@naver.com>
2024-11-03 17:20:57 +0900

    feat: 로또 확인 클래스 기능 구현

--- src/models/LottoChecker.js
+++ src/models/LottoChecker.js
@@ -9,23 +9,60 @@ class LottoChecker {
     const numbers = lotto.getNumbers();
     const matchCount = this.#matchNumbers(numbers, storage.getMainNumbers());
     const isBonusMatch = this.#matchBonus(numbers, storage.getBonusNumber());
-    const ranking = this.#rank({ matchCount, isBonusMatch });
+    const ranking = this.#rank(matchCount, isBonusMatch);
     const prizeMoney = this.#calculatePrize(ranking);
 
     const lottoData = {
       numbers,
       matchCount,
       isBonusMatch,
+      ranking,
       prizeMoney,
     };
 
     return new LottoResult(lottoData);
   }
 
-  #matchNumbers(numbers, mainNumbers) {}
-  #matchBonus(numbers, bonusNumber) {}
-  #rank({ matchCount, isBonusMatch }) {}
-  #calculatePrize(ranking) {}
+  #matchNumbers(numbers, mainNumbers) {
+    return numbers.reduce((acc, cur) => acc + Number(mainNumbers.includes(cur)), 0);
+  }
+
+  #matchBonus(numbers, bonusNumber) {
+    return numbers.includes(bonusNumber);
+  }
+
+  #rank(matchCount, isBonusMatch) {
+    switch (matchCount) {
+      case 3:
+        return 5;
+      case 4:
+        return 4;
+      case 5:
+        if (isBonusMatch) return 2;
+        return 3;
+      case 6:
+        return 1;
+      default:
+        return 0;
+    }
+  }
+
+  #calculatePrize(ranking) {
+    switch (ranking) {
+      case 5:
+        return 5_000;
+      case 4:
+        return 50_000;
+      case 3:
+        return 1_500_000;
+      case 2:
+        return 30_000_000;
+      case 1:
+        return 2_000_000_000;
+      default:
+        return 0;
+    }
+  }
 }
 
 export default LottoChecker;
