7182fff3e0ee7fa71f535cdc79be8bd341ace6f5
seonae-j <wkdtjsdo135@naver.com>
2024-11-03 22:41:09 +0900

    refactor(TicketNumber): 화살표형 함수로 변경

--- src/utils/TicketNumber.js
+++ src/utils/TicketNumber.js
@@ -1,13 +1,13 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-export function randomTicket() {
+export const randomTicket = () => {
   const randomArray = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
     .sort((a, b) => a - b)
     .map(Number);
   return randomArray;
-}
+};
 
-export function ticketArray(tickets) {
+export const ticketArray = (tickets) => {
   const ticketArray = Array.from({ length: tickets }, () => randomTicket());
   return ticketArray;
-}
+};
