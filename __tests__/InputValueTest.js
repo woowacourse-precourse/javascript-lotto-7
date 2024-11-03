d36c286f1c7bee9c26ea6cbf5cce8b3d5ec6def8
hyunyeee <5004ohy@naver.com>
2024-11-03 22:32:34 +0900

    test: 입력값이 공백인 경우 테스트케이스 추가

--- __tests__/InputValueTest.js
+++ __tests__/InputValueTest.js
@@ -11,7 +11,7 @@ const mockReadLineAsync = (input) => {
 }
 
 describe('PriceInputHandler Class 테스트', () => {
-  const priceArr = ['1000j', 'asdf', '##']
+  const priceArr = ['1000j', 'asdf', '##', ''];
   test.each(priceArr)('입력 받은 금액이 숫자가 아니면 에러를 던진다.', async (price) => {
     mockReadLineAsync(price);
     const priceInputHandler = new PriceInputHandler();
@@ -22,16 +22,17 @@ describe('PriceInputHandler Class 테스트', () => {
 
 
 describe('LottoNumberInputHandler Class 테스트', () => {
-  const winningNumbersArr = ['a,b,c,d,e,f', '!,@,3,$,^,7']
+  const winningNumbersArr = ['a,b,c,d,e,f', '!,@,3,$,^,7', ',,,,,'];
   const lottoNumberInputHandler = new LottoNumberInputHandler();
 
   test.each(winningNumbersArr)('입력 받은 당첨 번호 중 숫자가 아닌 값이 존재하면 에러를 던진다.', async (winningNumbers) => {
     mockReadLineAsync(winningNumbers);
+
     await expect(lottoNumberInputHandler.readWinningNumbers()).rejects.toThrow(inValidMessages.NaN);
   })
 
 
-  const bonusNumberArr = ['a', '!', "1000j"]
+  const bonusNumberArr = ['a', '!', '1000j', ''];
 
   test.each(bonusNumberArr)('입력 받은 보너스 번호가 숫자가 아니면 에러를 던진다.', async (bonusNumber) => {
     mockReadLineAsync(bonusNumber);
