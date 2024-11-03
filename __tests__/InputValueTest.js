35586d2228685ddeb18a121e5cb78ab72d4991db
hyunyeee <5004ohy@naver.com>
2024-11-03 22:08:38 +0900

    test: 입력 받은 당첨 번호 중 숫자가 아닌 값이 존재하면 에러를 던진다

--- __tests__/InputValueTest.js
+++ __tests__/InputValueTest.js
@@ -1,6 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { PriceInputHandler } from "../src/handler/PriceInputHandler.js";
 import { inValidMessages } from "../src/constant/message.js";
+import { LottoNumberInputHandler } from "../src/handler/LottoNumberInputHandler.js";
 
 const mockReadLineAsync = (input) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -9,11 +10,24 @@ const mockReadLineAsync = (input) => {
   })
 }
 
-describe('입력값 validate 테스트', () => {
+describe('PriceInputHandler Class 테스트', () => {
   const priceArr = ['1000j', 'asdf', '##']
   test.each(priceArr)('입력 받은 금액이 숫자가 아니면 에러를 던진다.', async (price) => {
     mockReadLineAsync(price);
     const priceInputHandler = new PriceInputHandler();
+
     await expect(priceInputHandler.readPrice()).rejects.toThrow(inValidMessages.NaN);
+  })
+});
+
+
+describe('LottoNumberInputHandler Class 테스트', () => {
+  const winningNumbersArr = ['a,b,c,d,e,f', '!,@,3,$,^,7']
+
+  test.each(winningNumbersArr)('입력 받은 당첨 번호 중 숫자가 아닌 값이 존재하면 에러를 던진다.', async (winningNumbers) => {
+    mockReadLineAsync(winningNumbers);
+    const lottoNumberInputHandler = new LottoNumberInputHandler();
+
+    await expect(lottoNumberInputHandler.readWinningNumbers()).rejects.toThrow(inValidMessages.NaN);
   })
 });
