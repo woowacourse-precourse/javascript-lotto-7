bbbd3d38ec51bc63b52dcc9161a6acf584a539f2
dlsxjzld <sinjuk1@naver.com>
2024-11-03 22:46:37 +0900

    feat(App.js): 로또 구입 금액 만큼 로또 발행해

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import { validateLottoBuyPrice } from './validateFunctions.js';
+import Lotto from './Lotto.js';
 
 const prompt = async (message, validation) => {
   const input = await Console.readLineAsync(message);
@@ -9,12 +10,29 @@ const prompt = async (message, validation) => {
   return prompt(message, validation);
 };
 
+const ONE_LOTTO_PRICE = 1000;
+const getLottoCount = (lottoBuyPrice) =>
+  Number(lottoBuyPrice) / ONE_LOTTO_PRICE;
+
+const makeLottos = (lottoCount) =>
+  Array.from({ length: lottoCount }, () => {
+    const lotto = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (a, b) => a - b,
+    );
+    return new Lotto(lotto);
+  });
+
 class App {
   async run() {
     const lottoBuyPrice = await prompt(
       '구입금액을 입력해 주세요.\n',
       validateLottoBuyPrice,
     );
+
+    const lottoCount = getLottoCount(lottoBuyPrice);
+    Console.print(`${lottoCount}개를 구매했습니다.\n`);
+
+    const myLottos = makeLottos(lottoCount);
   }
 }
 
