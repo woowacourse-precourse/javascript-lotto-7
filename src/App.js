1911104807b8954611675109e8946f1ea3ce81e8
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-04 05:02:59 +0900

    feat(app): 잘못된 구입 금액에 대한 예외 처리 구현

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ class App {
       '구입금액을 입력해 주세요.\n'
     );
 
+    if (isNaN(orderPrice)) {
+      throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
+    }
+
+    if (orderPrice % LOTTO_PER_PRICE !== 0) {
+      throw new Error('[ERROR] 구입 금액은 구입 금액은 1,000원 단위여야 합니다.');
+    }
+
     const lottoQuantity = orderPrice / LOTTO_PER_PRICE;
 
     const lottoGame = new LottoGame(lottoQuantity);
