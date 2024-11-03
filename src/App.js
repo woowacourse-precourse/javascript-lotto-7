487c2d691bc8979c9f87dfc9413a3bc57ea82175
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-04 05:40:59 +0900

    feat(app): ApplicationTest 코드 통과를 위해 try-catch를 이용한 에러 헨들링 구현

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import { Console } from '@woowacourse/mission-utils';
 import LottoGame from './LottoGame.js';
 class App {
   async run() {
-    const LOTTO_PER_PRICE = 1000;
-    const orderPrice = await Console.readLineAsync(
-      '구입금액을 입력해 주세요.\n'
-    );
-
-    if (isNaN(orderPrice)) {
-      throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
-    }
-
-    if (orderPrice % LOTTO_PER_PRICE !== 0) {
-      throw new Error(
-        '[ERROR] 구입 금액은 구입 금액은 1,000원 단위여야 합니다.'
+    try {
+      const LOTTO_PER_PRICE = 1000;
+      const orderPrice = await Console.readLineAsync(
+        '구입금액을 입력해 주세요.\n'
       );
+
+      if (isNaN(orderPrice)) {
+        throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
+      }
+
+      if (orderPrice % LOTTO_PER_PRICE !== 0) {
+        throw new Error('[ERROR] 구입 금액은 1,000원 단위여야 합니다.');
+      }
+
+      const lottoQuantity = orderPrice / LOTTO_PER_PRICE;
+      const lottoGame = new LottoGame(lottoQuantity);
+      await lottoGame.start();
+    } catch (error) {
+      Console.print(error.message);
     }
-
-    const lottoQuantity = orderPrice / LOTTO_PER_PRICE;
-
-    const lottoGame = new LottoGame(lottoQuantity);
-
-    lottoGame.start();
   }
 }
 
