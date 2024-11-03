b16105cd418513b280c508ace53a11cecea13d93
DodoJBNU <dh723@jbnu.ac.kr>
2024-11-04 03:25:59 +0900

    test: add test to output rate of return

---
+++ __tests__/unit/feature/RateOfReturnOutputTest.js
@@ -0,0 +1,64 @@
+import { Console } from '@woowacourse/mission-utils';
+import LottoController from '../../../src/components/LottoController.js';
+
+describe('수익률 출력 테스트', () => {
+  test('로또 구매 금액 : 1000원, 5,000원 당첨 1개, 수익률 500.0% 출력', () => {
+    const mockWinningResult = { 3: 1, 4: 0, 5: 0, '5B': 0, 6: 0 };
+    const logSpy = jest.spyOn(Console, 'print');
+    const logs = '총 수익률은 500.0%입니다.';
+
+    const lottoController = new LottoController();
+    lottoController.setPurchaseAmount(1000);
+    lottoController.printRateOfReturn(mockWinningResult);
+
+    expect(logSpy).toHaveBeenCalledWith(logs);
+  });
+
+  test('로또 구매 금액 : 1000원, 50,000원 당첨 1개, 수익률 5000.0% 출력', () => {
+    const mockWinningResult = { 3: 0, 4: 1, 5: 0, '5B': 0, 6: 0 };
+    const logSpy = jest.spyOn(Console, 'print');
+    const logs = '총 수익률은 5000.0%입니다.';
+
+    const lottoController = new LottoController();
+    lottoController.setPurchaseAmount(1000);
+    lottoController.printRateOfReturn(mockWinningResult);
+
+    expect(logSpy).toHaveBeenCalledWith(logs);
+  });
+
+  test('로또 구매 금액 : 1000원, 1,500,000원 당첨 1개, 수익률 150000.0% 출력', () => {
+    const mockWinningResult = { 3: 0, 4: 0, 5: 1, '5B': 0, 6: 0 };
+    const logSpy = jest.spyOn(Console, 'print');
+    const logs = '총 수익률은 150000.0%입니다.';
+
+    const lottoController = new LottoController();
+    lottoController.setPurchaseAmount(1000);
+    lottoController.printRateOfReturn(mockWinningResult);
+
+    expect(logSpy).toHaveBeenCalledWith(logs);
+  });
+
+  test('로또 구매 금액 : 30,000,000원, 30,000,000원 당첨 1개, 수익률 100.0% 출력', () => {
+    const mockWinningResult = { 3: 0, 4: 0, 5: 0, '5B': 1, 6: 0 };
+    const logSpy = jest.spyOn(Console, 'print');
+    const logs = '총 수익률은 100.0%입니다.';
+
+    const lottoController = new LottoController();
+    lottoController.setPurchaseAmount(30000000);
+    lottoController.printRateOfReturn(mockWinningResult);
+
+    expect(logSpy).toHaveBeenCalledWith(logs);
+  });
+
+  test('로또 구매 금액 : 1000원, 2,000,000,000원 당첨 1개, 수익률 200000000.0% 출력', () => {
+    const mockWinningResult = { 3: 0, 4: 0, 5: 0, '5B': 0, 6: 1 };
+    const logSpy = jest.spyOn(Console, 'print');
+    const logs = '총 수익률은 200000000.0%입니다.';
+
+    const lottoController = new LottoController();
+    lottoController.setPurchaseAmount(1000);
+    lottoController.printRateOfReturn(mockWinningResult);
+
+    expect(logSpy).toHaveBeenCalledWith(logs);
+  });
+});
