22bfeefa6bf4de5962112a829cdfa8ed3f77eefe
장정안 <wjddks96@naver.com>
2024-11-04 03:24:34 +0900

    feat: 입력을 받는 부분과, 에러 처리 로직 분리
    - 입력을 받는 부분과, 에러 처리 로직 분리하여 메서드의 역할을 더욱 명확히 나누었습니다.

--- src/Controller/Game.js
+++ src/Controller/Game.js
@@ -9,6 +9,7 @@ import { validateLottoPurchase } from '../validate/purchaseValidator.js';
 import { outputView } from '../views/outputView.js';
 import { validateWinningNumber } from '../validate/winningNumberValidator.js';
 import { validateBonusNumber } from '../validate/bonusNumberValidator.js';
+import throwError from '../util/errorThrower.js';
 
 class Game {
   constructor() {
@@ -37,21 +38,29 @@ class Game {
     this.resultView.printEarningRate(earningRate);
   }
 
+  async processPurchase() {
+    const purchaseAmount = await this.user.readUserInput(GAME_MESSAGE.PURCHASE);
+    const count = Number(purchaseAmount) / LOTTO_CONFIG.PRICE_PER;
+    validateLottoPurchase(purchaseAmount);
+
+    const lottos = this.lottoService.createLottos(count);
+    this.resultView.printPurchaseResult(count, lottos);
+
+    return { lottos, purchaseAmount };
+  }
+
+  handleErrorMessage(error) {
+    outputView.printMessage(error.message);
+  }
+
   async handleUserPurchase() {
     while (true) {
       try {
-        const purchaseAmount = await this.user.readUserInput(
-          GAME_MESSAGE.PURCHASE
-        );
-        const count = Number(purchaseAmount) / LOTTO_CONFIG.PRICE_PER;
-        validateLottoPurchase(purchaseAmount);
-
-        const lottos = this.lottoService.createLottos(count);
-        this.resultView.printPurchaseResult(count, lottos);
+        const { lottos, purchaseAmount } = await this.processPurchase();
 
         return { lottos, purchaseAmount };
       } catch (error) {
-        outputView.printMessage(error.message);
+        this.handleErrorMessage(error);
       }
     }
   }
@@ -59,15 +68,19 @@ class Game {
   async getWinningNumbers() {
     while (true) {
       try {
-        const winningNumber = await this.user.readUserInput(
-          GAME_MESSAGE.WINNING_NUMBER
-        );
-        validateWinningNumber(winningNumber.split(',').map(Number));
-        return winningNumber.split(',').map((num) => Number(num.trim()));
+        return await this.processWinningNumbers();
       } catch (error) {
-        outputView.printMessage(error.message);
+        this.handleErrorMessage(error);
       }
     }
+  }
+
+  async processWinningNumbers() {
+    const winningNumber = await this.user.readUserInput(
+      GAME_MESSAGE.WINNING_NUMBER
+    );
+    validateWinningNumber(winningNumber.split(',').map(Number));
+    return winningNumber.split(',').map((num) => Number(num.trim()));
   }
 
   async getBonusNumber() {
@@ -75,7 +88,7 @@ class Game {
       try {
         const input = await this.user.readUserInput(GAME_MESSAGE.BONUS_NUMBER);
         validateBonusNumber(input, this.winningNumbers);
-        return Number(input);
+        return input;
       } catch (error) {
         outputView.printMessage(error.message);
       }
