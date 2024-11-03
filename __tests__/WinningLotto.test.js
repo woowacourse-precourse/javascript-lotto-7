457ad9115184a8807a26962511cb254f6d7ebf23
2wndrhs <2wndrhs@gmail.com>
2024-11-03 20:06:59 +0900

    test: 발행한 로또의 등수를 계산하는 기능 테스트 코드 작성

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -56,4 +56,58 @@ describe('WinningLotto 클래스 테스트', () => {
       ).toThrow('[ERROR]');
     });
   });
+
+  describe('calculateRank() 메서드 테스트', () => {
+    let winningLotto;
+
+    beforeEach(() => {
+      const winningNumbers = '1,2,3,4,5,6';
+      const bonusNumber = '7';
+      winningLotto = new WinningLotto();
+      winningLotto.setWinningNumbers(winningNumbers);
+      winningLotto.setBonusNumber(bonusNumber);
+    });
+
+    test('6개 번호가 일치하면 1등을 반환한다.', () => {
+      const lottoNumbers = [1, 2, 3, 4, 5, 6];
+      const rank = winningLotto.calculateRank(lottoNumbers);
+
+      expect(rank).toBe(1);
+    });
+
+    test('5개 번호와 보너스 번호가 일치하면 2등을 반환한다.', () => {
+      const lottoNumbers = [1, 2, 3, 4, 5, 7];
+      const rank = winningLotto.calculateRank(lottoNumbers);
+
+      expect(rank).toBe(2);
+    });
+
+    test('5개 번호가 일치하면 3등을 반환한다.', () => {
+      const lottoNumbers = [1, 2, 3, 4, 5, 8];
+      const rank = winningLotto.calculateRank(lottoNumbers);
+
+      expect(rank).toBe(3);
+    });
+
+    test('4개 번호가 일치하면 4등을 반환한다.', () => {
+      const lottoNumbers = [1, 2, 3, 4, 8, 9];
+      const rank = winningLotto.calculateRank(lottoNumbers);
+
+      expect(rank).toBe(4);
+    });
+
+    test('3개 번호가 일치하면 5등을 반환한다.', () => {
+      const lottoNumbers = [1, 2, 3, 8, 9, 10];
+      const rank = winningLotto.calculateRank(lottoNumbers);
+
+      expect(rank).toBe(5);
+    });
+
+    test('3개 미만의 번호가 일치하면 null을 반환한다.', () => {
+      const lottoNumbers = [1, 2, 8, 9, 10, 11];
+      const rank = winningLotto.calculateRank(lottoNumbers);
+
+      expect(rank).toBeNull();
+    });
+  });
 });
