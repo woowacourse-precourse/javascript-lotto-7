53c7dbd461186605b980e579552f02aa288bdb5e
young24n <won8560@gmail.com>
2024-11-03 22:09:46 +0900

    test: 로또 번호 테스트 작성 -로또 길이 검증

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,23 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 아니면 예외 발생.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 로또 번호가 6자리보다 많이 생성됨!');
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow('[ERROR] 로또 번호가 6자리보다 적게 생성됨!');
   });
 
   // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 로또 번호에 중복된 값이 있음!');
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
 });
