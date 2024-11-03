acb0553e003bae3239f1ed31e9de01a4cf16a2ee
osw6858 <osw7875@naver.com>
2024-11-03 20:44:07 +0900

    feat: 우승번호 유효성 검사를 위한 메서드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { ERROR } from './constant.js';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,36 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.#validateWinningNumber(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  #validRange(numberArray) {
+    return (
+      numberArray.length === 6 &&
+      numberArray.every((number) => number >= 1 && number <= 45)
+    );
+  }
+
+  #validFormat(numberArray) {
+    return numberArray.every((number) => !Number.isNaN(Number(number.trim())));
+  }
+
+  #validDuplication(numberArray) {
+    const uniqueNumbers = new Set(numberArray);
+    return uniqueNumbers.size === numberArray.length;
+  }
+
+  #validateWinningNumber(winningNumber) {
+    const numberArray = winningNumber.split(',');
+
+    if (
+      !this.#validRange(numberArray) ||
+      !this.#validFormat(numberArray) ||
+      !this.#validDuplication(numberArray)
+    ) {
+      throw Error(ERROR.message);
+    }
+  }
 }
 
 export default Lotto;
