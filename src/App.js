66026ba8c8eaff626dd555e1b95f60160b4b1c75
yoojin <everywavyday@gmail.com>
2024-11-03 20:15:52 +0900

    feat: 입력받은 로또 구입 금액에 해당하는 만큼 로또를 발행하고 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Input from "./input.js";
+import LottoController from "./lottoController.js";
 
 class App {
   async run() {
     const userInput = new Input();
+    const lottoController = new LottoController();
+
     const inputMoney = await userInput.getMoney();
+    const lottos = await lottoController.buyLotto(inputMoney);
+    lottoController.printLottos(lottos);
+
+    
   }
 }
 
