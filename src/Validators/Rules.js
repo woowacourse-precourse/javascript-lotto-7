714933da6a5b86adcefcd2857158f4dc44c0f523
Lee <dltmdgns0508@gmail.com>
2024-11-04 03:46:50 +0900

    refactor(Rule): Rules import, export 변경
    Rules.js 변경
    - lottoNumbersRules.js 추가로 인한 Rules export,import 구문 변경
    - lottoNumberListRules, lottoNumberRules import,export 제거
    - lottoNumbersRules import,export 추가

--- src/Validators/Rules.js
+++ src/Validators/Rules.js
@@ -1,13 +1,11 @@
 import DEFAULT_RULES from './defaultRules.js';
 import PURCHASE_AMOUNT_RULES from './purchaseAmountRules.js';
-import LOTTO_NUMBER_LIST_RULES from './lottoNumberListRules.js';
-import LOTTO_NUMBER_RULES from './lottoNumberRules.js';
+import LOTTO_NUMBERS_RULES from './lottoNumbersRules.js';
 import BONUS_NUMBER_RULES from './bonusNumberRules.js';
 
 export {
   DEFAULT_RULES,
   PURCHASE_AMOUNT_RULES,
-  LOTTO_NUMBER_LIST_RULES,
-  LOTTO_NUMBER_RULES,
+  LOTTO_NUMBERS_RULES,
   BONUS_NUMBER_RULES,
 };
