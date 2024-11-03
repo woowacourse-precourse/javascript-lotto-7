9aee63bf6790d11d00fcda7e0b7d5b0ccf414227
Eunjin Cho <whdmswls9207@gmail.com>
2024-11-04 00:05:34 +0900

    feat(App): 당첨 번호 입력

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ class App {
   async run() {
     const cost = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
     purchaseLotto(cost);
+
+    const inputWinningNumbers = await Console.readLineAsync(
+      "당첨 번호를 입력해 주세요.\n"
+    );
+    const winningNumbers = inputWinningNumbers.split(",");
   }
 }
 
