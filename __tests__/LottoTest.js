7f82963f8f9ca413fd7317a474892bc80b898c6b
mun-kyeong <horse6953@naver.com>
2024-11-03 19:26:04 +0900

    [TEST] (LottoTest) : 생성된 로또 출력 test
    - 생성된 로또 출력 test
    - 배열로 주어졌을 때 출력 기댓값과 동일한지 판단
    
    로또 출력 형식이 주어진 값과 동일한지 test 합니다

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,6 @@
 import Lotto from "../src/features/lotto/Lotto";
+import { printOneLine } from "../src/utils/console";
+import { getLogSpy } from "../src/utils/testUtils";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -11,6 +13,20 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
+  });
+
+  //TODO : 오름차순 정렬 테스트 추가
+});
+
+describe("로또 클래스 출력 테스트", () => {
+  test("로또 numbers가 형식에 맞추어 잘 출력되는지 확인한다.", () => {
+    const logSpy = getLogSpy();
+
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const PRINT_MESSAGE = "[1, 2, 3, 4, 5, 6]";
+    printOneLine(lotto.printNumbers());
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(PRINT_MESSAGE));
   });
 
   //TODO : 오름차순 정렬 테스트 추가
