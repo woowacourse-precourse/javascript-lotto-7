247e8c7d6c960ac02c89d1c7df12356551152952
unknown <suyeonshim@gm.gist.ac.kr>
2024-11-03 15:16:08 +0900

    feat(1): 구입 금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const money = await Console.readLineAsync("구입금액을 입력해 주세요.");
+    if (money % 1000 !== 0)
+      throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
+    const lottoCount = parseInt(money / 1000);
+    Console.print(`${lottoCount}개를 구매했습니다.`);
+  }
 }
 
 export default App;
