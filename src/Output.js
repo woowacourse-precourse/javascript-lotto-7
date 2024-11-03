306335e89c34d5897e19fa0e82fe4b9fb4c0090d
jsy <suyeonb1db@gmail.com>
2024-11-03 23:00:56 +0900

    fix(Output): 당첨내역 함수 중복으로 삭제

--- src/Output.js
+++ src/Output.js
@@ -20,15 +20,6 @@ class Output {
     Console.print(`6개 일치 (2,000,000,000원) - ${results[1]}개`);
   }
 
-  static printStatics(results) {
-    Console.print("\n당첨 통계\n---");
-    Console.print(`3개 일치 (5,000원) - ${results[5]}개`);
-    Console.print(`4개 일치 (50,000원) - ${results[4]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${results[3]}개`);
-    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${results[2]}개`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${results[1]}개`);
-  }
-
   static printProfitRate(rate) {
     Console.print(`총 수익률은 ${rate}%입니다.`);
   }
