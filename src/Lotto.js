4b76f6d2c464dc00eca26996b07af980402edad2
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-04 04:48:00 +0900

    feat(lotto): 구매한 번호와 당첨번호, 보너스 번호를 비교하는 checkWinningStatus 메서드 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,10 +11,21 @@ class Lotto {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
-  
+
   // TODO: 추가 기능 구현
   get numbers() {
-    return [...this.#numbers];;
+    return [...this.#numbers];
+  }
+
+  checkWinningStatus(winningNumbers, bonusNumber) {
+    let matchCount = 0;
+    let hasBonus = false;
+    this.numbers.forEach((number) => {
+      if (winningNumbers.includes(number)) matchCount++;
+      if (bonusNumber === number) hasBonus = true;
+    });
+
+    return { matchCount, hasBonus };
   }
 }
 
