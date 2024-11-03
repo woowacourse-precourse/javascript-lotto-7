00d418f66d4a102a37510fe9b59732b3f4cd9084
oko_jin <dswvgw6412@naver.com>
2024-11-03 15:59:14 +0900

    refactor(lottoInput): 로또 당첨 번호 입력시 문자열을 테스트하도록 변경

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,10 +1,11 @@
-import { lottoTestCase } from '../src/constant/lottoTestCase.js';
-import Lotto from '../src/Lotto';
+import { lottoInputErrorTestCase, lottoErrorTestCase } from '../src/constant/lottoTestCase.js';
+import { validateNumbers } from '../src/Validator.js';
 
-describe('로또 클래스 테스트', () => {
-  test.each(lottoTestCase)('$description 예외가 발생한다.', ({ numbers, errorMessage }) => {
-    expect(() => {
-      new Lotto(numbers);
-    }).toThrow(errorMessage);
-  });
+describe('로또 입력 테스트', () => {
+  test.each(lottoInputErrorTestCase)(
+    '$description 예외가 발생한다.',
+    ({ numbers, errorMessage }) => {
+      expect(() => validateNumbers(numbers)).toThrow(errorMessage);
+    }
+  );
 });
