ce0b335910dfc992ed37948a8e573fc15c3cdc39
Jeong-Ag <kjungu1105@gmail.com>
2024-11-03 21:03:25 +0900

    feat: 로또 결과 출력 함수 생성
    로또 결과를 안내하는 콘솔 출력 함수를 생성합니다.

--- src/utils/output.js
+++ src/utils/output.js
@@ -1,3 +1,4 @@
+import { Console } from "@woowacourse/mission-utils";
 import {
   LOTTO_COAST_UNIT,
   LOTTO_FIFTH_PRIZE,
@@ -22,4 +23,26 @@ function calculateRate(results, quantity) {
   const total = quantity * LOTTO_COAST_UNIT;
 
   return (sumResults / total) * 100;
+}
+
+export function output(results, quantity) {
+  const rate = calculateRate(results, quantity);
+
+  Console.print(`당첨 통계\n---`);
+  Console.print(
+    `3개 일치 (${LOTTO_FIFTH_PRIZE.toLocaleString()}원) - ${results[0]}개`
+  );
+  Console.print(
+    `4개 일치 (${LOTTO_FOURTH_PRIZE.toLocaleString()}원) - ${results[1]}개`
+  );
+  Console.print(
+    `5개 일치 (${LOTTO_THIRD_PRIZE.toLocaleString()}원) - ${results[2]}개`
+  );
+  Console.print(
+    `5개 일치, 보너스 볼 일치 (${LOTTO_SECOND_PRIZE.toLocaleString()}원) - ${results[3]}개`
+  );
+  Console.print(
+    `6개 일치 (${LOTTO_FIRST_PRIZE.toLocaleString()}원) - ${results[4]}개`
+  );
+  Console.print(`총 수익률은 ${rate}%입니다.\n`);
 }
