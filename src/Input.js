401f34902d1082f33dc6c707e66aee3f1018149b
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-11-03 22:33:56 +0900

    refactor(Input): Input 모듈에서 Enum 모듈의 ErrorMessage 를 사용하도록 변경

--- src/Input.js
+++ src/Input.js
@@ -2,14 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 import Validation from './Validation.js';
 import Logic from './Logic.js';
 import Lotto from './Lotto.js';
-
-const errorMessage = {
-  PUR_AMOUNT_ERROR: ' 구입금액은 1,000원 단위여야 합니다.',
-  WIN_NUM_ERROR:
-    ' 당첨 번호는 6개의 숫자와 쉼표로 이루어져야 하며, 각각의 숫자는 중복되지 않는 1~45 사이의 숫자여야 합니다.',
-  BONUS_NUM_ERROR:
-    ' 보너스 번호는 1~45 사이의 숫자이고 당첨 번호와 중복되지 않아야 합니다.',
-};
+import { ErrorMessage } from './Enum.js';
 
 class Input {
   static async getPurchaseAmount() {
@@ -22,7 +15,7 @@ class Input {
         }
         return Number(input);
       } catch (e) {
-        Console.print(e.message + errorMessage.PUR_AMOUNT_ERROR);
+        Console.print(e.message + ErrorMessage.PUR_AMOUNT_ERROR);
       }
     }
   }
@@ -37,7 +30,7 @@ class Input {
         }
         return new Lotto(Logic.parseWinningNumbers(input));
       } catch (e) {
-        Console.print(e.message + errorMessage.WIN_NUM_ERROR);
+        Console.print(e.message + ErrorMessage.WIN_NUM_ERROR);
       }
     }
   }
@@ -52,7 +45,7 @@ class Input {
         }
         return Number(input);
       } catch (e) {
-        Console.print(e.message + errorMessage.BONUS_NUM_ERROR);
+        Console.print(e.message + ErrorMessage.BONUS_NUM_ERROR);
       }
     }
   }
