c866d4fe68b7d8f42235000d2dabd9721f6a6d0b
jin123457 <jin_ox@naver.com>
2024-11-03 15:18:21 +0900

    feat : 저장된 결과 값을 출력 및 당첨 금액 계산
    수정된 파일명 : App.js
    App.js : 저장된 결과 값을 순회하며 결과 값들을 출력하며 당첨된 금액들을 더한다.

--- src/App.js
+++ src/App.js
@@ -45,6 +45,14 @@ class App {
       const rank = lotto.matchRank(RANK_TABLE, winNumbers, readUserBonusNumber);
       if (rank) lottoResults[rank - 1][2] += 1;
     });
+
+    const totalMoney = lottoResults.reverse().reduce((acc, cur) => {
+      Console.print(`${cur[0]} (${cur[1].toLocaleString()}원) - ${cur[2]}개`);
+      const addMoney = acc + cur[1] * cur[2];
+      return addMoney;
+    }, 0);
+
+    Console.print(totalMoney);
   }
 }
 
