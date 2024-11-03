cea189a6b07d0dffb207f3ef09cced257b3b889b
hyeongdong <khd941018@gmail.com>
2024-11-04 01:07:52 +0900

    refactor(BudgetManager): 의존성 주입을 위해 budget을 멤버변수에서 제거
    budget은 사용자로부터 입력받는 구입 금액인데, 이 것을 상태로 가지고 있으면
    budgetManager객체를 생성할 때, user input view에 의존하게되기 때문에,
    ui로직과 비즈니스로직을 분리하기 힘들다고 판단해 budget을 멤버변수에서 제거
    하였고, 따라서 budget을 validate하는 메서드도 제거
    
    또, 사용자 입력값인 budget은 값이 입력되었을 때 검증을 하는 것이 맞다고 생각해
    validate또한 제거
    
    budget을 제거했기 때문에 좀 더 어울리는 클래스명으로 수정

--- src/BudgetManger.js
+++ src/BudgetManger.js
@@ -1,22 +1,13 @@
-class BudgetManager {
-  #budget;
+class PurchaseManager {
   #lottoPrice;
 
-  constructor(budget, lottoPrice) {
-    this.#validate(budget, lottoPrice);
-    this.#budget = budget;
+  constructor(lottoPrice) {
     this.#lottoPrice = lottoPrice;
   }
 
-  #validate(budget, lottoPrice) {
-    if (budget % lottoPrice !== 0) {
-      throw new Error(`[ERROR] 구입 금액은 ${lottoPrice}원 단위로 입력해주세요.`);
-    }
-  }
-
-  calculateMaxLottos() {
-    return this.#budget / this.#lottoPrice;
+  calculateMaxLottos(budget) {
+    return budget / this.#lottoPrice;
   }
 }
 
-export default BudgetManager;
+export default PurchaseManager;
