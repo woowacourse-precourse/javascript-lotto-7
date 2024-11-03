166df56b478cd06fbe85e6a610e47e158ce6e246
2wndrhs <2wndrhs@gmail.com>
2024-11-03 16:51:03 +0900

    test: 로또 구입 금액만큼 로또를 발행하는지 테스트 코드 작성

--- __tests__/LottoIssuer.test.js
+++ __tests__/LottoIssuer.test.js
@@ -1,29 +1,40 @@
 import LottoIssuer from '../src/Model/LottoIssuer.js';
 
 describe('LottoIssuer 클래스 테스트', () => {
-  test('구입 금액이 0보다 크고 1000원 단위일 경우 예외가 발생하지 않는다.', () => {
-    expect(() => new LottoIssuer('3000')).not.toThrow();
+  describe('생성자 테스트', () => {
+    test('구입 금액이 0보다 크고 1000원 단위일 경우 예외가 발생하지 않는다.', () => {
+      expect(() => new LottoIssuer('3000')).not.toThrow();
+    });
+
+    test('구입 금액이 0보다 크지만 1000원 단위가 아닐 경우 예외가 발생한다.', () => {
+      expect(() => new LottoIssuer('3500')).toThrow('[ERROR]');
+    });
+
+    test('구입 금액이 0보다 작고 1000원 단위일 경우 예외가 발생한다.', () => {
+      expect(() => new LottoIssuer('-3000')).toThrow('[ERROR]');
+    });
+
+    test('구입 금액이 0일 경우 예외가 발생한다.', () => {
+      expect(() => new LottoIssuer('0')).toThrow('[ERROR]');
+    });
+
+    test('구입 금액이 소수일 경우 예외가 발생한다.', () => {
+      expect(() => new LottoIssuer('1000.5')).toThrow(
+        '[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.',
+      );
+    });
+
+    test('구입 금액이 숫자가 아닌 문자열일 경우 예외가 발생한다.', () => {
+      expect(() => new LottoIssuer('abc')).toThrow('[ERROR]');
+    });
   });
 
-  test('구입 금액이 0보다 크지만 1000원 단위가 아닐 경우 예외가 발생한다.', () => {
-    expect(() => new LottoIssuer('3500')).toThrow('[ERROR]');
-  });
+  describe('issue() 메서드 테스트', () => {
+    test('issue() 메서드는 구입 금액에 해당하는 수량의 로또를 발행한다.', () => {
+      const issuer = new LottoIssuer('3000');
+      const issuedLottos = issuer.issue();
 
-  test('구입 금액이 0보다 작고 1000원 단위일 경우 예외가 발생한다.', () => {
-    expect(() => new LottoIssuer('-3000')).toThrow('[ERROR]');
-  });
-
-  test('구입 금액이 0일 경우 예외가 발생한다.', () => {
-    expect(() => new LottoIssuer('0')).toThrow('[ERROR]');
-  });
-
-  test('구입 금액이 소수일 경우 예외가 발생한다.', () => {
-    expect(() => new LottoIssuer('1000.5')).toThrow(
-      '[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.',
-    );
-  });
-
-  test('구입 금액이 숫자가 아닌 문자열일 경우 예외가 발생한다.', () => {
-    expect(() => new LottoIssuer('abc')).toThrow('[ERROR]');
+      expect(issuedLottos).toHaveLength(3);
+    });
   });
 });
