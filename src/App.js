921dffa953ef3700f350ca05a46c84b556e0b61a
ahhyun <wndkgus0225@naver.com>
2024-11-04 01:38:02 +0900

    feat(App) : 당첨 통계 관련 실행함수 작성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 import { getCost, displayLottosCount, displayLottosNumber, userPickedNumbers, userPickedBonusNumber, printResults } from '../src/IO.js';
 import { generateLottos } from '../src/Lotto.js';
-import { evaluateResults } from '../src/result.js';
+import { evaluateResults, countMatches } from '../src/result.js';
 
 class App {
-  // 1. 필드 : 이 클래스에서 필요한 속성 정의
   cost;
-  // 2. 생성자 : 필드를 초기화
+
   constructor(){
     this.cost = 0;
   }
-  // 3. 메서드 : 클래스의 동작 정의
+
   async run() {
     try{
+      /*
       const count = await getCost();
       displayLottosCount(count);
       displayLottosNumber(count);
@@ -20,13 +20,26 @@ class App {
       const pickedNumbers = await userPickedNumbers();
       const pickedBounsNumber = await userPickedBonusNumber(pickedNumbers);
 
-      // displayLottosCount(count);
-      // displayLottosNumber(lottos);
+      const winningNumbers = await getWinningNumbers();
+      const bonusNumber = await getBonusNumber();
+      evaluateResults(lottos, winningNumbers, bonusNumber);
 
-      // const winningNumbers = await getWinningNumbers();
-      // const bonusNumber = await getBonusNumber();
+      */
 
-      // evaluateResults(lottos, winningNumbers, bonusNumber);
+
+      const count = await getCost();
+      displayLottosCount(count);
+  
+      const lottos = generateLottos(count);
+      displayLottosNumber(count);
+  
+      const pickedNumbers = await userPickedNumbers();
+      const pickedBonusNumber = await userPickedBonusNumber(pickedNumbers);
+  
+      const rankCount = evaluateResults(lottos, pickedNumbers, pickedBonusNumber);
+      printResults(rankCount, count * 1000); // cost에 맞게 수익률 계산
+
+
       
     } catch(error){
       Console.print(`[ERROR] 오류가 발생했습니다: ${error.message}`)
