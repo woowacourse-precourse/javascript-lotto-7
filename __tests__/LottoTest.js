931921a3fb4320a4a111129f38a5b49177ba70e9
minjeong9919 <minjeong9919@naver.com>
2024-11-04 03:21:17 +0900

    refactor: 클래스별로 test 파일 분리

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,7 +1,4 @@
 import Lotto from "../src/Lotto";
-import MoneyValidator from "../src/MoneyValidator.js";
-import LottoGenerator from "../src/LottoGenerator.js";
-import lotto from "../src/Lotto";
 
 describe("로또 클래스 테스트", () => {
   const lotto = new Lotto();
@@ -45,38 +42,4 @@ describe("로또 클래스 테스트", () => {
       lotto.validateAdditionalNumber(80);
     }).toThrow("[ERROR]");
   });
-}),
-  describe("moneyValidator 클래스 테스트", () => {
-    test("구매 금액이 1000 단위가 아닐 경우 예외가 발생한다.", () => {
-      expect(() => {
-        new MoneyValidator(13987);
-      }).toThrow("[ERROR]");
-    });
-
-    test("구매 금액이 숫자가 아닐 경우 예외가 발생한다.", () => {
-      expect(() => {
-        new MoneyValidator("dd");
-      }).toThrow("[ERROR]");
-    });
-
-    test("추가 당첨 번호가 숫자가 아닐 경우 예외가 발생한다.", () => {
-      expect(() => {
-        new MoneyValidator("dd");
-      }).toThrow("[ERROR]");
-    });
-  }),
-  describe("lottoGenerator 클래스 테스트", () => {
-    test("금액 입력 시, 구매한 복권들을 출력한다.", () => {
-      const lottoGenerator = new LottoGenerator(3000);
-      const lottos = lottoGenerator.getLottos();
-
-      expect(lottos).toHaveLength(3),
-        lottos.forEach(lotto => {
-          expect(lotto).toHaveLength(6);
-          lotto.forEach(number => {
-            expect(number).toBeGreaterThanOrEqual(1);
-            expect(number).toBeLessThanOrEqual(45);
-          });
-        });
-    });
-  });
+});
