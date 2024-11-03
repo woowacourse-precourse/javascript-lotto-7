8d986599b6e20f0ea86d93b9ee777b020d6dd375
BoRamSon <onbrson1@gmail.com>
2024-11-03 19:21:42 +0900

    feat(LottoController): 구매금액입력, 랜덤로또번호생성, 당첨번호입력까지 연동 동작이 가능하게 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,15 +1,18 @@
 import { InputPriceModel } from "../model/inputPriceModel.js";
 import { OutputLottoNumbers } from "../model/OutputLottoModel.js";
+import { InputPrizeNumModel } from "../model/InputPrizeNumModel.js";
 
 export class LottoController {
   constructor() {
     this.inputPriceModel = new InputPriceModel();
     this.outputLottoNumbers = new OutputLottoNumbers();
+    this.inputPrizeNumModel = new InputPrizeNumModel();
   }
 
   async startLottoGame() {
     const price = await this.getPriceResult();
     this.getLottoNumberResult(price);
+    await this.getPrizeNumbersResult();
   }
 
   async getPriceResult() {
@@ -18,5 +21,9 @@ export class LottoController {
 
   getLottoNumberResult(price) {
     return this.outputLottoNumbers.getRandomNumbers(price);
+  }
+
+  async getPrizeNumbersResult() {
+    return await this.inputPrizeNumModel.getPrizeNumbers();
   }
 }
