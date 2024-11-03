edfd070dce6e09c08c9f2ab0149281ea6663fb06
gumbi <gusn9719@gmail.com>
2024-11-03 16:51:54 +0900

    feat: 사용자 메시지 및 상금 정보 상수화
    - 사용자 입력을 위한 메시지(USER_PROMPT_MESSAGES) 추가
    - 확인 메시지(CONFIRMATION_MESSAGES) 추가
    - 에러 메시지(ERROR_MESSAGES) 추가
    - 등수에 따른 상금 정보(PRIZE_AMOUNTS) 추가

---
+++ src/constants/messages.js
@@ -0,0 +1,34 @@
+const USER_PROMPT_MESSAGES = Object.freeze({
+  GET_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  GET_WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  GET_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+});
+
+const CONFIRMATION_MESSAGES = Object.freeze({
+  PURCHASE_COMPLETE_MESSAGE: (count) => `${count}개를 구매했습니다.`,
+  WINNING_STATISTICS_HEADER: '당첨 통계\n---',
+  TOTAL_YIELD_MESSAGE: (rate) => `총 수익률은 ${rate}%입니다.`,
+});
+
+const ERROR_MESSAGES = Object.freeze({
+  INVALID_PURCHASE_AMOUNT: '[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.',
+  INVALID_WINNING_NUMBER:
+    '[ERROR] 당첨 번호는 1~45 사이의 숫자여야 하며, 중복되지 않아야 합니다.',
+  INVALID_BONUS_NUMBER:
+    '[ERROR] 보너스 번호는 1~45 사이의 숫자여야 하며, 당첨 번호와 중복되지 않아야 합니다.',
+});
+
+const PRIZE_AMOUNTS = Object.freeze({
+  FIRST_PRIZE: 2000000000,
+  SECOND_PRIZE: 30000000,
+  THIRD_PRIZE: 1500000,
+  FOURTH_PRIZE: 50000,
+  FIFTH_PRIZE: 5000,
+});
+
+export {
+  USER_PROMPT_MESSAGES,
+  CONFIRMATION_MESSAGES,
+  ERROR_MESSAGES,
+  PRIZE_AMOUNTS,
+};
