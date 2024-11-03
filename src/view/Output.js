7c52d3b9e4f9bbda8d8fcd6422cfafdbb7734788
ChanLee_KR <cl700120@gmail.com>
2024-11-03 23:47:49 +0900

    feat: 당첨 통계 출력 함수 구현

--- src/view/Output.js
+++ src/view/Output.js
@@ -1,4 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
+import { LOTTO_PRIZE_LIST } from '../Constants.js';
+import { formatKRW } from '../Utils.js';
 
 class Output {
   /** @param {number[][]} allNumbers */
@@ -15,6 +17,42 @@ class Output {
   /** @param {number} length */
   static #printNumberOfPurchase(length) {
     Console.print(`\n${length}개를 구매했습니다.`);
+  }
+
+  /** @param {number[]} winningCountList */
+  static printStatistics(winningCountList) {
+    Output.#printStatisticsTitle();
+    [...winningCountList].reverse().forEach((winningCount, i) => {
+      const rankIndex = LOTTO_PRIZE_LIST.length - (i + 1);
+      Output.#printRankAndCount(winningCount, rankIndex);
+    });
+  }
+
+  /**
+   * @param {number} winningCount
+   * @param {number} rank
+   */
+  static #printRankAndCount(winningCount, rank) {
+    const prize = LOTTO_PRIZE_LIST[rank];
+    const comment = Output.#getPrizeComment(winningCount, prize);
+
+    Console.print(comment);
+  }
+
+  /**
+   * @param {number} winningCount
+   * @param {(typeof LOTTO_PRIZE_LIST)[number]} prize
+   */
+  static #getPrizeComment(winningCount, { count, bonus, cash }) {
+    if (bonus) {
+      return `${count}개 일치, 보너스 볼 일치 (${formatKRW(cash)}원) - ${winningCount}개`;
+    }
+
+    return `${count}개 일치 (${formatKRW(cash)}원) - ${winningCount}개`;
+  }
+
+  static #printStatisticsTitle() {
+    Console.print('\n당첨 통계\n---');
   }
 }
 
