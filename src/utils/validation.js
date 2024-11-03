9cb346f65647344a0cc059d0ca47e8a9e1eb0668
subsub-e <ab13696802@gmail.com>
2024-11-03 22:46:44 +0900

    fix(validation) : 예외 입력 시 다시 작성하기 위한 로직을 위해 리팩토링

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,7 +4,7 @@ export function validatePurchaseAmount(purchaseAmount) {
   if (purchaseAmount === '' || isNaN(Number(purchaseAmount))) {
     throw new Error(ERROR_MESSAGES.PURCHASE_AMOUNT_IS_NOT_NUMBER);
   }
-  if (purchaseAmount % 1000 != 0) {
+  if (purchaseAmount % 1000 !== 0) {
     throw new Error(ERROR_MESSAGES.PURCHASE_AMOUNT_IS_NOT_DIVIDE_BY_THOUSAND);
   }
   if (Number(purchaseAmount) <= 0) {
@@ -13,39 +13,39 @@ export function validatePurchaseAmount(purchaseAmount) {
 }
 
 export function validateWinningNumber(winningNumber) {
-  const numberArray = winningNumber.split(',');
+  const numberArray = winningNumber.split(',').map(Number);
+
+  if (numberArray.length !== 6) {
+    throw new Error(ERROR_MESSAGES.INVALID_WINNING_NUMBER_COUNT);
+  }
+
   numberArray.forEach((number) => {
-    if (!checkPositiveInteger(Number(number))) {
+    if (!checkPositiveInteger(number)) {
       throw new Error(ERROR_MESSAGES.WINNING_NUMBER_IS_NOT_NUMBER);
     }
-  });
-
-  const numbers = numberArray.map(Number);
-  numbers.forEach((number) => {
     if (checkNumberOutOfRange(number)) {
       throw new Error(ERROR_MESSAGES.WINNING_NUMBER_OUT_OF_BOUNDS);
     }
   });
+
+  const uniqueNumbers = new Set(numberArray);
+  if (uniqueNumbers.size !== numberArray.length) {
+    throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER_IN_WINNING_NUMBER);
+  }
 }
 
-export function validateBonus(bonus) {
-  if (!checkPositiveInteger(Number(bonus))) {
+export function validateBonus(bonus, winningNumber) {
+  const bonusNumber = Number(bonus);
+
+  if (!checkPositiveInteger(bonusNumber)) {
     throw new Error(ERROR_MESSAGES.BONUS_NUMBER_IS_NOT_NUMBER);
   }
-
-  if (checkNumberOutOfRange(Number(bonus))) {
+  if (checkNumberOutOfRange(bonusNumber)) {
     throw new Error(ERROR_MESSAGES.BONUS_NUMBER_OUT_OF_BOUNDS);
   }
-}
 
-export function checkDuplicateNumber(winningNumberArray, bonus) {
-  const numberSet = new Set();
-  const numberArray = winningNumberArray.split(',');
-  numberArray.forEach((number) => {
-    numberSet.add(number);
-  });
-  numberSet.add(bonus);
-  if (numberSet.size !== numberArray.length + 1) {
+  const winningNumbers = winningNumber.split(',').map(Number);
+  if (winningNumbers.includes(bonusNumber)) {
     throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER_IN_WINNING_AND_BONUS);
   }
 }
