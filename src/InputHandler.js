3fbd034b1e1d1d6ef761cc1d9c42135b5b6eaf5f
junly21 <jaejoon0721@ajou.ac.kr>
2024-11-03 17:56:40 +0900

    feat: 당첨 번호 입력 예외테스트
    보너스 번호 입력도 당첨 번호이므로 포함.

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -2,10 +2,19 @@ import { Console } from '@woowacourse/mission-utils';
 import { INPUT_MESSAGE, ERROR_MESSAGE } from './lib/constant.js';
 
 class InputHandler {
+  static async getUserInput() {
+    const buyPrice = await this.getBuyPrice();
+    const winningNumbers = await this.getWinningNumbers();
+    const bonusNumber = await this.getBonusNumber();
+
+    this.#validateBonusNumberDuplicate(winningNumbers, bonusNumber);
+
+    return { buyPrice, winningNumbers, bonusNumber };
+  }
+
   static async getBuyPrice() {
     const buyPrice = await Console.readLineAsync(INPUT_MESSAGE.BUY_PRICE);
-    const parsedBuyPrice = parseInt(buyPrice, 10);
-    this.#validateBuyPrice(parsedBuyPrice);
+    this.#validateBuyPrice(buyPrice);
     return buyPrice;
   }
 
@@ -13,18 +22,38 @@ class InputHandler {
     const winningNumbers = await Console.readLineAsync(
       INPUT_MESSAGE.WINNING_NUMBERS,
     );
+    this.#validateWinningNumbers(winningNumbers);
     return winningNumbers;
   }
 
   static async getBonusNumber() {
     const bonusNumber = await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
+    this.#validateBonusNumber(bonusNumber);
     return bonusNumber;
   }
 
   static #validateBuyPrice(buyPrice) {
-    this.#validateBuyPriceType(buyPrice);
+    this.#validateNumberType(buyPrice);
     this.#validatePositiveNumber(buyPrice);
     this.#validateBuyPriceUnit(buyPrice);
+  }
+
+  static #validateWinningNumbers(winningNumbers) {
+    const numbers = winningNumbers.split(',');
+    this.#validateWinningNumberCount(numbers);
+    numbers.forEach((number) => {
+      this.#validateNumberType(number);
+      this.#validatePositiveNumber(number);
+      this.#validateWinningNumberRange(number);
+    });
+    this.#validateWinningNumberDuplicate(numbers);
+  }
+
+  static #validateBonusNumber(bonusNumber) {
+    this.#validateNumberType(bonusNumber);
+    this.#validatePositiveNumber(bonusNumber);
+    this.#validateWinningNumberRange(bonusNumber);
+    this.#validateBonusNumberDuplicate(bonusNumber);
   }
 
   static #validateBuyPriceUnit(buyPrice) {
@@ -33,15 +62,40 @@ class InputHandler {
     }
   }
 
-  static #validateBuyPriceType(buyPrice) {
-    if (Number.isNaN(buyPrice)) {
-      throw new Error(ERROR_MESSAGE.BUY_PRICE_TYPE);
+  static #validateNumberType(buyPrice) {
+    if (Number.isNaN(parseInt(buyPrice, 10))) {
+      throw new Error(ERROR_MESSAGE.INPUT_TYPE);
     }
   }
 
   static #validatePositiveNumber(number) {
     if (number <= 0) {
-      throw new Error(ERROR_MESSAGE.BUY_PRICE_POSITIVE);
+      throw new Error(ERROR_MESSAGE.NUMBER_POSITIVE);
+    }
+  }
+
+  static #validateWinningNumberCount(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error(ERROR_MESSAGE.WINNING_NUMBER_COUNT);
+    }
+  }
+
+  static #validateWinningNumberRange(number) {
+    if (parseInt(number, 10) < 1 || parseInt(number, 10) > 45) {
+      throw new Error(ERROR_MESSAGE.WINNING_NUMBER_RANGE);
+    }
+  }
+
+  static #validateWinningNumberDuplicate(winningNumbers) {
+    const winningUniqueNumbers = new Set(winningNumbers);
+    if (winningNumbers.length !== winningUniqueNumbers.size) {
+      throw new Error(ERROR_MESSAGE.WINNING_NUMBER_DUPLICATE);
+    }
+  }
+
+  static #validateBonusNumberDuplicate(winningNumbers, bonusNumber) {
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error(ERROR_MESSAGE.WINNING_NUMBER_DUPLICATE);
     }
   }
 }
