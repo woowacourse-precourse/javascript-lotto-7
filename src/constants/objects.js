85c6447cc5244ebc5fc21a623035dc9cb08137d3
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 05:47:08 +0900

    feat: 출력 객체 및 일치 카운터로 사용되는 전역 객체 선언

---
+++ src/constants/objects.js
@@ -0,0 +1,14 @@
+export const MATCH_COUNTER = {
+    three: {cnt: 0, money: 5000},
+    four: {cnt: 0, money: 50000},
+    five: {cnt: 0, money: 1500000},
+    bonus: {cnt: 0, money: 30000000},
+    six: {cnt: 0, money: 2000000000},
+}
+
+export const OUTPUT_OBJ = [
+    {content: "3개 일치 (5,000원) - "},
+    {content: "4개 일치 (50,000원) -"},
+    {content: "5개 일치 (1,500,000원) - "},
+    {content: "5개 일치, 보너스 볼 일치 (30,000,000원) - "},
+    {content: "6개 일치 (2,000,000,000원) - "}]
