e0bbc442dd06797d0765e69063ee5e4b736a3d76
조혜린 <hyer1205@gmail.com>
2024-11-03 23:29:59 +0900

    Chore: 대소문자 관련 캐시 제거

--- src/constants/inputConstant.js
+++
@@ -1,8 +0,0 @@
-const INPUT_PROMPT = Object.freeze({
-  PURCHASE_PRICE: '구입금액을 입력해 주세요.\n',
-  WINNING_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
-  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
-});
-const WINNING_NUMBERS_DELIMITER = ',';
-
-export { INPUT_PROMPT, WINNING_NUMBERS_DELIMITER };
