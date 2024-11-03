76a260f4c21eecda1f09eb29a874f240b2fd64da
subsub-e <ab13696802@gmail.com>
2024-11-03 23:55:55 +0900

    fix(OutputView) : async 제거

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -2,25 +2,25 @@ import OUTPUT_MESSAGES from '../constants/outputConstants.js';
 import { Console } from '@woowacourse/mission-utils';
 
 class OutputView {
-  static async printLottoAmount(amount) {
+  static printLottoAmount(amount) {
     Console.print(`${amount}${OUTPUT_MESSAGES.PURCHASE_LOTTO}`);
   }
 
-  static async printLottoNumbers(lottoRepository) {
+  static printLottoNumbers(lottoRepository) {
     lottoRepository.forEach((lotto) => {
       Console.print(`[${lotto.getLotto().join(', ')}]`);
     });
   }
 
-  static async printWinningStatistics() {
+  static printWinningStatistics() {
     Console.print(OUTPUT_MESSAGES.WINNING_STATISTICS);
   }
 
-  static async printUnderBar() {
+  static printUnderBar() {
     Console.print(OUTPUT_MESSAGES.UNDERBAR);
   }
 
-  static async printFifthPlaceWinner(result) {
+  static printFifthPlaceWinner(result) {
     Console.print(
       `${OUTPUT_MESSAGES.FIFTH_PLACE_WINNER}${result.getFifthPlaceCount()}${
         OUTPUT_MESSAGES.AMOUNT
@@ -28,7 +28,7 @@ class OutputView {
     );
   }
 
-  static async printFourthPlaceWinner(result) {
+  static printFourthPlaceWinner(result) {
     Console.print(
       `${OUTPUT_MESSAGES.FOURTH_PLACE_WINNER}${result.getFourthPlaceCount()}${
         OUTPUT_MESSAGES.AMOUNT
@@ -36,7 +36,7 @@ class OutputView {
     );
   }
 
-  static async printThirdPlaceWinner(result) {
+  static printThirdPlaceWinner(result) {
     Console.print(
       `${OUTPUT_MESSAGES.THIRD_PLACE_WINNER}${result.getThirdPlaceCount()}${
         OUTPUT_MESSAGES.AMOUNT
@@ -44,7 +44,7 @@ class OutputView {
     );
   }
 
-  static async printSecondPlaceWinner(result) {
+  static printSecondPlaceWinner(result) {
     Console.print(
       `${OUTPUT_MESSAGES.SECOND_PLACE_WINNER}${result.getSecondPlaceCount()}${
         OUTPUT_MESSAGES.AMOUNT
@@ -52,7 +52,7 @@ class OutputView {
     );
   }
 
-  static async printFirstPlaceWinner(result) {
+  static printFirstPlaceWinner(result) {
     Console.print(
       `${OUTPUT_MESSAGES.FIRST_PLACE_WINNER}${result.getFirstPlaceCount()}${
         OUTPUT_MESSAGES.AMOUNT
@@ -60,13 +60,13 @@ class OutputView {
     );
   }
 
-  static async printTotalReturn(totalReturn) {
+  static printTotalReturn(totalReturn) {
     Console.print(
       `${OUTPUT_MESSAGES.TOTAL_RETURN}${totalReturn}${OUTPUT_MESSAGES.PERCENT_PRINT}`
     );
   }
 
-  static async printNewLine() {
+  static printNewLine() {
     Console.print('');
   }
 
