2daab126015414356cdb9e28110008768d14f18d
Eunjin Cho <whdmswls9207@gmail.com>
2024-11-04 00:05:57 +0900

    feat(App): 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ class App {
       "당첨 번호를 입력해 주세요.\n"
     );
     const winningNumbers = inputWinningNumbers.split(",");
+
+    const bonusNumber = await Console.readLineAsync(
+      "\n보너스 번호를 입력해 주세요.\n"
+    );
   }
 }
 
