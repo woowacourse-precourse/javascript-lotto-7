fa9b5e70ce4d7775eef9e92548db3ddcd5cf7f26
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-11-03 19:53:42 +0900

    fix : 출력 순서 변경

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ class App {
   async run() {
     const input = new Input();
     const bet = await input.inputMoney();
+    const betList = bet.getBetResults()
     const lotto = await input.inputLotto();
     await lotto.inputBonusNumber();
     
-    const lottoResult = new LottoResult(lotto.getLottoScore(bet.getBetResults()));
+    const lottoResult = new LottoResult(lotto.getLottoScore(betList));
     lottoResult.printWinningResult();
     lottoResult.printRateOfReturn(bet.getMoney());
   }
