8481071b79da6a8a79975faaabaf9a1095886ec2
yheseul <kangyheseul@naver.com>
2024-11-03 21:53:18 +0900

    feat: LottoView에서 로또 개수 및 번호 출력 기능 추가

---
+++ src/views/LottoView.js
@@ -0,0 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+import { RUN_MESSAGE } from "../constants/runMessages.js";
+import { generateLottoNumbersByCount } from "../services/generateLottoNumbersByCount.js";
+
+class LottoView {
+  displayLottoCount(count) {
+    Console.print(RUN_MESSAGE.PURCHASED_LOTTO_COUNT(count));
+  }
+
+  displayLottoNumbers(lottoCount) {
+    const lottoNumbers = generateLottoNumbersByCount(lottoCount);
+    lottoNumbers.forEach((numbers) => {
+      Console.print(`[${numbers.join(", ")}]`);
+    });
+  }
+}
+
+export default LottoView;
