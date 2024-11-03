1c02098d6ba1b6c9c1cca8507fe7c6f112ba5f40
hyeongdong <khd941018@gmail.com>
2024-11-03 15:19:17 +0900

    feat(BudgetManager): 유저 입력 구입금액 유효성 검사 기능 구현

---
+++ src/BudgetManger.js
@@ -0,0 +1,20 @@
+class BudgetManager {
+  #budget;
+  #lottoPrice;
+
+  constructor(budget, lottoPrice) {
+    this.#validate(budget, lottoPrice);
+    this.#budget = budget;
+    this.#lottoPrice = lottoPrice;
+  }
+
+  #validate(budget, lottoPrice) {
+    if (budget % lottoPrice !== 0) {
+      throw new Error(`[ERROR] 구입 금액은 ${lottoPrice}원 단위로 입력해주세요.`);
+    }
+  }
+
+  calculateMaxLottos() {
+    return;
+  }
+}
