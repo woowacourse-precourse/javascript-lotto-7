6fc5880d8c9c54f1b265dd75c57c18aff5c613d5
osw6858 <osw7875@naver.com>
2024-11-03 21:02:59 +0900

    feat: 보너스 번호의 범위를 검증하는 메서드 추가

--- src/Bonus.js
+++ src/Bonus.js
@@ -8,5 +8,14 @@ export class Bonus {
     this.#validate(bonus, lottoList);
   }
 
-  #validate(bonus, lottoList) {}
+  #validate(bonus, lottoList) {
+    this.#validBonusRange(bonus);
+  }
+
+  #validBonusRange(bonus) {
+    const number = Number(bonus.trim());
+    if (number < 1 || number > 45) {
+      throw new Error('보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  }
 }
