bb73a49938747d420caef4ccc17b3cb2c2aa0225
CJY <cjy3458@naver.com>
2024-11-04 04:32:25 +0900

    chore: 상수명 변경

--- src/constants/UI.js
+++ src/constants/UI.js
@@ -28,7 +28,7 @@ export const ERROR = Object.freeze({
 });
 
 export const MESSAGE = Object.freeze({
-  INPUT_PRICE: '구입금액을 입력해 주세요.\n',
+  INPUT_PURCHASE: '구입금액을 입력해 주세요.\n',
   PURCHASE_COUNT: (count) => `${count}개를 구매했습니다.\n`,
   INPUT_WINNING_NUMBER: '\n당첨 번호를 입력해 주세요.\n',
   INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
