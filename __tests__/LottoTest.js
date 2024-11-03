40cfa03d5c0ce34b49a86dd030073f973f81f123
wowalswjd <alswjd5491@naver.com>
2024-11-04 06:00:28 +0900

    test(LottoTest): 로또 클래스 단위 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,34 @@
 import Lotto from "../src/Lotto";
+import { WINNING_NUMBERS_ERROR } from "../src/Message/Message";
 
 describe("로또 클래스 테스트", () => {
+  test("로또 번호 중에 문자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, "a", 6]);
+    }).toThrow(WINNING_NUMBERS_ERROR.IS_NOT_NUMBER);
+  });
+
+  test("로또 번호 중에 실수가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1.1, 2, 3, 4, 5, 6]);
+    }).toThrow(WINNING_NUMBERS_ERROR.IS_NOT_INTEGER);
+  });
+
+  test("로또 번호의 범위가 1~45까지가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow(WINNING_NUMBERS_ERROR.IS_NOT_RANGE);
+  });
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(WINNING_NUMBERS_ERROR.IS_NOT_SIX_LENGTH);
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(WINNING_NUMBERS_ERROR.IS_DUPLICATE_NUMBER);
   });
-
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 });
--- src/Message/Message.js
+++ src/Message/Message.js
@@ -2,5 +2,13 @@ export const PRICE_ERROR = {
   IS_NOT_NUMBER: "[ERROR] 구입금액으로 문자를 입력할 수 없습니다.",
   IS_NOT_INTEGER: "[ERROR] 구입금액으로 실수를 입력할 수 없습니다.",
   IS_NOT_POSITIVE: "[ERROR] 구입금액으로 음수 및 0을 입력할 수 없습니다.",
-  IS_NOT_THOUSAND: "[ERROR] 구입금액은 1000원 단위여야 합니다."
+  IS_NOT_THOUSAND: "[ERROR] 구입금액은 1000원 단위여야 합니다.",
+};
+
+export const WINNING_NUMBERS_ERROR = {
+  IS_NOT_NUMBER: "[ERROR] 문자를 입력할 수 없습니다.",
+  IS_NOT_INTEGER: "[ERROR] 실수를 입력할 수 없습니다.",
+  IS_NOT_RANGE: "[ERROR] 1~45까지의 번호만 입력할 수 있습니다.",
+  IS_DUPLICATE_NUMBER: "[ERROR] 다른 번호와 중복된 번호를 사용할 수 없습니다.",
+  IS_NOT_SIX_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
 };
