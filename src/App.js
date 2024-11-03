2fb9c875878f9567231a62e3a253f2732b5acdac
kochungwon <yud01063@gmail.com>
2024-11-03 19:31:46 +0900

    feat(app): 로또 애플리케이션 메인 로직 구현
    - 사용자 입력, 티켓 생성 및 통계 계산을 위한 App 클래스 추가
    - 구입 금액, 당첨 번호 및 보너스 번호에 대한 유효성 검사 메서드 구현
    - 사용자 입력에 기반하여 티켓을 생성하기 위해 LottoGenerator 통합
    - 생성된 티켓과 사용자 정의 당첨 번호를 바탕으로 통계를 계산하고 출력하기 위해 LottoStatistics 사용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import UserInput from "../../javascript-lotto-7/src/UserInput.js";
+import LottoGenerator from '../../javascript-lotto-7/src/LottoGenerator.js';
+import LottoStatistics from '../../javascript-lotto-7/src/LottoStatistics.js'; 
+import Lotto from '../../javascript-lotto-7/src/Lotto.js';
 
 class App {
   async run() {
     const userInput = new UserInput();
+    const lottoGenerator = new LottoGenerator();
+    const lottoStatistics = new LottoStatistics();
+
+    // 구입 금액 입력 및 유효성 검사
     await userInput.inputPurchaseAmount();
+
+    // 티켓 번호 생성
+    const ticketNumbers = lottoGenerator.generateTickets(userInput.purchaseAmount);
+    const tickets = ticketNumbers.map(numbers => new Lotto(numbers)); 
+    lottoGenerator.printTickets(ticketNumbers.length, ticketNumbers);
+
+    // 당첨 번호 입력 및 유효성 검사
     await userInput.inputWinningNumbers();
+
+    // 보너스 번호 입력 및 유효성 검사
     await userInput.inputBonusNumber();
 
-    const validator = new InputValidator();
-    validator.validateAmount(userInput.purchaseAmount); 
-    validator.validateWinningNumbers(userInput.winningNumbers);
-    validator.validateBonusNumber(userInput.bonusNumber);
+    // 통계 계산
+    lottoStatistics.calculateStatistics(tickets, userInput.winningNumbers, userInput.bonusNumber);
+    
+    // 통계 출력
+    lottoStatistics.printStatistics(userInput.purchaseAmount);
   }
 }
 
