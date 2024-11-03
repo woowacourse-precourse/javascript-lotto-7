0d508c8fbc3e1f296d21ce722b0b7cee0d8f5853
youdame <112458620+youdame@users.noreply.github.com>
2024-11-03 23:52:51 +0900

    refactor : 인풋 파싱과 로또 개수 계산 로직 분리 - 파싱과 로또 개수를 구하는 로직을 합친 후 유효성 검사를 하는 건 어렵다는 걸 깨닫고 분리 - 파싱 -> 유효성 검사 -> 로또 개수 구하는 방식으로 변경

--- src/input/input-parser.js
+++ src/input/input-parser.js
@@ -1,9 +1,15 @@
+import { LOTTO_PRICE } from '../constant/index.js';
+
 class InputParser {
   static parsePurchaseAmount(input) {
     if (input.trim() === '') {
       return null;
     }
     return Number(input);
+  }
+
+  static calculateLottoCount(purchaseAmount) {
+    return purchaseAmount / LOTTO_PRICE;
   }
 }
 
