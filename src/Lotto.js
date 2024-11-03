b9cd99fd81a0f1afb822b698ab43f0eafda2ae49
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-11-04 03:00:12 +0900

    refactor(Lotto.js): 함수 길이 리팩토링

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,39 +1,58 @@
 import { ERROR_MESSAGE, RULE } from './constants/index.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    this.#validateNumbers(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  #validateNumbers(numbers) {
+    this.#validateEmpty(numbers);
+    this.#validateNumberType(numbers);
+    this.#validateLength(numbers);
+    this.#validateDuplication(numbers);
+    this.#validateRange(numbers);
+  }
+
+  #validateEmpty(numbers) {
     if (numbers.includes('')) {
       throw new Error(ERROR_MESSAGE.NO_BLANK);
     }
+  }
+
+  #validateNumberType(numbers) {
     if (numbers.some(isNaN)) {
       throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
     }
+  }
 
-    if (numbers.length !== 6) {
+  #validateLength(numbers) {
+    if (numbers.length !== RULE.LOTTO_BALL_NUMBER) {
       throw new Error(ERROR_MESSAGE.NOT_ENOUGH_NUMBERS(RULE.LOTTO_BALL_NUMBER));
     }
+  }
+
+  #validateDuplication(numbers) {
     const uniqueNumbers = new Set(numbers);
     if (uniqueNumbers.size !== numbers.length) {
       throw new Error(ERROR_MESSAGE.INPUT_DUPLICATION);
     }
-    if (
-      numbers.some((number) => {
-        return number < RULE.LOTTO_MIN_NUMBER || number > RULE.LOTTO_MAX_NUMBER;
-      })
-    ) {
+  }
+
+  #validateRange(numbers) {
+    if (this.#isOutOfRange(numbers)) {
       throw new Error(
-        ERROR_MESSAGE.OUT_OF_RANGE_NUMBER(
-          RULE.LOTTO_MIN_NUMBER,
-          RULE.LOTTO_MAX_NUMBER,
-        ),
+        ERROR_MESSAGE.OUT_OF_RANGE_NUMBER(RULE.LOTTO_MIN_NUMBER, RULE.LOTTO_MAX_NUMBER),
       );
     }
+  }
+
+  #isOutOfRange(numbers) {
+    return numbers.some(
+      (number) => number < RULE.LOTTO_MIN_NUMBER || number > RULE.LOTTO_MAX_NUMBER,
+    );
   }
 
   get numbers() {
