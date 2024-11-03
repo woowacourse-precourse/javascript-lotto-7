6ff55214a917839bdc66342f98c3c0d02b4b72fc
BoRamSon <onbrson1@gmail.com>
2024-11-03 22:03:43 +0900

    fix(errorMessages): 필요한 에러메세지 추가

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -7,13 +7,16 @@ const ERR_MESSAGE_NUMBERS = {
     "[ERROR] 소수점 자릿수가 너무 깁니다. 다시 입력해주세요.",
   IS_NOT_POSITIVE_NUMBER:
     "[ERROR] 양의 정수가 아닙니다. 양의 정수로 다시 입력해주세요.",
-  IS_NOT_NEGATIVE_NUMBER: "[ERROR] 음의 정수만 입력할 수 있습니다. 다시 입력해주세요.",
-  TOO_LONG_LENGTH_NUMBER: `[ERROR] 숫자입력 6자리를 초과할 수 없습니다. 다시 입력해주세요.`,
-  LIMIT_RANGE_NUMBER: `[ERROR] 숫자는 최소 1,000원부터 최대 100,000원 사이의 값으로 입력할 수 있습니다. 다시 입력해주세요.`,
+  IS_NOT_NEGATIVE_NUMBER:
+    "[ERROR] 음의 정수만 입력할 수 있습니다. 다시 입력해주세요.",
+  LIMIT_LENGTH_PRICE_NUMBER: `[ERROR] 숫자입력 6자리를 초과할 수 없습니다. 다시 입력해주세요.`,
+  LIMIT_LENGTH_BONUS_NUMBER: `[ERROR] 숫자입력 2자리를 초과할 수 없습니다. 다시 입력해주세요.`,
+  LIMIT_RANGE_PRICE_NUMBER: `[ERROR] 숫자는 최소 1,000원부터 최대 100,000원 사이의 값으로 입력할 수 있습니다. 다시 입력해주세요.`,
+  LIMIT_RANGE_BONUS_NUMBER: `[ERROR] 보너스 번호는 최소 1부터 최대 45 사이의 값으로 입력할 수 있습니다. 다시 입력해주세요.`,
   TOO_EXCESSIVE_NUMBER:
     "[ERROR] 과도한 숫자를 입력하셨습니다. 다시 입력해주세요.",
-  INPUT_KILO_UNIT: "[ERROR] 천원단위로만 입력이 가능합니다. ex) 1000, 3000, 20000, 50000..."
-
+  INPUT_KILO_UNIT:
+    "[ERROR] 천원단위로만 입력이 가능합니다. ex) 1000, 3000, 20000, 50000",
 };
 
 export { ERR_MESSAGE_NUMBERS };
