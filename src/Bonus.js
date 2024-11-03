ab81d81498f122884a8744b7f870884afca5c13a
osw6858 <osw7875@naver.com>
2024-11-03 21:14:30 +0900

    fix: 타입이 맞지 않아 일어난 오류 수정

--- src/Bonus.js
+++ src/Bonus.js
@@ -15,24 +15,19 @@ export class Bonus {
   }
 
   #validBonusRange(bonus) {
-    const number = Number(bonus.trim());
-    if (number < 1 || number > 45) {
+    if (bonus < 1 || bonus > 45) {
       throw new Error('보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
     }
   }
 
   #validBonusFormat(bonus) {
-    if (Number.isNaN(Number(bonus.trim()))) {
+    if (Number.isNaN(bonus)) {
       throw new Error('보너스 번호는 숫자여야 합니다.');
     }
   }
 
   #validBonusDuplication(bonus, lottoList) {
-    const bonusNumber = Number(bonus.trim());
-    const winningNumbers = lottoList
-      .split(',')
-      .map((num) => Number(num.trim()));
-    if (winningNumbers.includes(bonusNumber)) {
+    if (lottoList.includes(bonus)) {
       throw new Error('보너스 번호는 당첨 번호와 중복될 수 없습니다.');
     }
   }
