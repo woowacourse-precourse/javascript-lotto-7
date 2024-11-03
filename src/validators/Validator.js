c5165f07bd5d169a9c2bd90010f93e77ffdcd9c8
suhwa <tnghk9611@naver.com>
2024-11-03 17:39:31 +0900

    refactor(Validator): 공통 예외처리 로직을 true/false 형태로 반환하도록 수정
    - Validator만 수정하면 일반적으로 검사하는 에러 케이스들의 로직을 수정할 수 있도록 수정

--- src/validators/Validator.js
+++ src/validators/Validator.js
@@ -1,10 +1,19 @@
-import { generateError } from "../utils/generateError.js";
-
 class Validator {
   static isNotNumber(value) {
     if (isNaN(value)) {
-      generateError("현재 입력은 숫자가 아닙니다. 숫자만 입력할 수 있습니다.");
+      return true;
     }
+    return false;
+  }
+
+  static containNotNumber(values) {
+    values.forEach((value) => {
+      if (Validator.isNaN(value)) {
+        return true;
+      }
+    });
+
+    return false;
   }
 }
 
