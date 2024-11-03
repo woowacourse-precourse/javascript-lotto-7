6e6aee8357e84c5af17959de719dcf291e4817d9
gumbi <gusn9719@gmail.com>
2024-11-03 22:26:50 +0900

    feat: 구매 금액 입력에 관련된 기능 추가
    - 사용자에게 구매 금액 입력을 요청하는 함수 추가
      - promptUserAmount
    - 구매 금액을 입력받고 유효성을 확인하고 반환하는 함수 추가
      - getPurchaseAmount
      - 유효하지 않은 입력의 경우 에러 메시지를 출력하고 재입력 요청

---
+++ src/utils/get-parchase-amount.js
@@ -0,0 +1,25 @@
+import { USER_PROMPT_MESSAGES } from '../constants/messages.js';
+import { promptUserInput, printMessage } from './console.js';
+import validateAmount from '../validations/purchase-amount.js';
+
+const { GET_PURCHASE_AMOUNT } = USER_PROMPT_MESSAGES;
+
+const promptUserAmount = () => {
+  return promptUserInput(GET_PURCHASE_AMOUNT);
+};
+
+const getPurchaseAmount = async () => {
+  while (true) {
+    try {
+      const amount = await promptUserAmount();
+
+      const validatedAmount = validateAmount(amount);
+
+      return validatedAmount;
+    } catch (error) {
+      printMessage(error.message);
+    }
+  }
+};
+
+export default getPurchaseAmount;
