63612326d8ea7183bf33fd0c0cd5a5a62b154cc9
jiyeeeah <sije3757@snu.ac.kr>
2024-11-03 16:18:37 +0900

    test: 로또 번호 관련 예외 사항 테스트 케이스 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,5 +15,31 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  const notNumbers = [
+    [1, 2, 3, 4, 5, "hi"],
+    [1, 2, 3, 4, 5, ":"],
+    [10, 12, 23, 34, 45, ""],
+  ];
+  test.each(notNumbers)("로또 번호에 숫자가 아닌 값이 들어오면 예외가 발생한다.", (numbers) => {
+    expect(() => {
+      new Lotto(numbers);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 정수가 아닌 값이 들어오면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 10.3]);
+    }).toThrow("[ERROR]");
+  });
+
+  const outOfRangeNumbers = [
+    [1, 2, 3, 4, 60],
+    [0, 4, 10, 18, 30, 45],
+    [1, -3, 26, 43, 28, 30],
+  ];
+  test.each(outOfRangeNumbers)("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.", (numbers) => {
+    expect(() => {
+      new Lotto(numbers);
+    }).toThrow("[ERROR]");
+  });
 });
