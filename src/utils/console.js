1ff507f130eb84c60b43883b5831b4f322a22a47
Daeuning <happyjde3@gmail.com>
2024-11-03 16:56:06 +0900

    refactor(utils/console): 입력과 출력이 반복될 때 개행이 되도록 수정

--- src/utils/console.js
+++ src/utils/console.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 import { TAGS } from '../constants/message.js';
 
-const readAsyncInput = (message) => Console.readLineAsync(message);
+const readAsyncInput = (message) => Console.readLineAsync(`${message}\n`);
 
 const printMessage = (message) => Console.print(message);
 
