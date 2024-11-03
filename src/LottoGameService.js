3dbfabfdf9a4dd3f494371e5ceacd14af72e13b6
Kim_Yjoo <pas4976@gmail.com>
2024-11-03 23:57:43 +0900

    feat : 구입한  개수 만큼 로또를 생성하는 기능 추가

--- src/LottoGameService.js
+++ src/LottoGameService.js
@@ -1,5 +1,9 @@
+import { randomNumber } from "./utils/randomNumber"
+
 class LottoGameService{
-    
+    makeLottos(amount) {
+        return Array.from({length: amount}, () => randomNumber.getUniqueRandomNumbers(1, 45, amount));
+    }
 }
 
 export default LottoGameService
