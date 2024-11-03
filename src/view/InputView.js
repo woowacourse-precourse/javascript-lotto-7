0597843298b03803074f2ff37fb681e273f3f358
leelsj2007 <leelsj2007@gmail.com>
2024-11-04 02:52:42 +0900

    feat : 입력에 관한 InputView 클래스 작성

---
+++ src/view/InputView.js
@@ -0,0 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "../constants/constants";
+
+class InputView {
+  #read
+
+  constructor() {
+    this.#read = Console.readLineAsync;
+  }
+
+  async readPurchaseAmount() {
+    const inputPrice = await this.#read(INPUT_MESSAGE.inputPurchaseAmount);
+    return inputPrice;
+  }
+
+  async readWinningLotto() {
+    const winningNumber = await this.#read(INPUT_MESSAGE.inputWinningNumbers);
+    return winningNumber;
+  }
+
+  async readBonusNumber() {
+    const bonusNumber = await this.#read(INPUT_MESSAGE.inputBonusNumber);
+    return bonusNumber;
+  }
+}
+
+export default InputView
