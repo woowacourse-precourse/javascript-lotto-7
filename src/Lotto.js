e61a5c4619f458edcbe4752f0e6b9896fc3157fa
Jinny <ggab.challenge@gmail.com>
2024-11-03 23:42:00 +0900

    refactor: Lotto클래스 함수 길이 15줄 이내로 맞추기 위해 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,24 +2,26 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
     this.#numbers = numbers;
+
+    this.uniqueWinningNumbers = new Set(numbers);
+    this.allNumbersCheck = numbers.every((element) => typeof element === 'number');
+    this.rangeCheck = numbers.every((element) => element > 0 && element < 46);
+
+    this.#validate(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-    const uniqueWinningNumbers = new Set(numbers);
-    if (numbers.length !== uniqueWinningNumbers.size) {
+    if (numbers.length !== this.uniqueWinningNumbers.size) {
       throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
     }
-    const allNumbersCheck = numbers.every((element) => typeof element === 'number');
-    if (allNumbersCheck !== true) {
+    if (this.allNumbersCheck !== true) {
       throw new Error('[ERROR] 모든 로또 번호가 숫자가 아닙니다.');
     }
-    const rangeCheck = numbers.every((element) => element > 0 && element < 46);
-    if (rangeCheck !== true) {
+    if (this.rangeCheck !== true) {
       throw new Error('[ERROR] 로또 번호가 1부터 45까지가 아닙니다.');
     }
   }
