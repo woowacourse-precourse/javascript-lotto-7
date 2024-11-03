7700e666508d5cc2785158558afde7e256fe4521
oko_jin <dswvgw6412@naver.com>
2024-11-03 16:11:03 +0900

    test(lottoInputTestCase): 로또 번호 입력시 테스트 케이스 작성

--- src/constant/LottoTestCase.js
+++ src/constant/LottoTestCase.js
@@ -1,23 +1,38 @@
 import { errorMessage } from './errorMessage.js';
 
-export const lottoTestCase = [
+export const lottoInputErrorTestCase = [
   {
-    numbers: [1, 2, 3, 4, 5, 6, 7],
+    numbers: '1, 2,3,4,5,6',
+    description: '구분자가 ,가 아니면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
+  },
+  {
+    numbers: '1.2,3.4,5,6',
+    description: '구분자가 ,가 아니면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
+  },
+  {
+    numbers: '1;2,3,4,5,6',
+    description: '구분자가 ,가 아니면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
+  },
+  {
+    numbers: '1,2,3,4,5,6,7',
     description: '로또 번호의 개수가 6개가 넘어가면',
     errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
   },
   {
-    numbers: [1, 2, 3, 4, 5, 5],
+    numbers: '1,2,3,4,5,5',
     description: '로또 번호에 중복된 숫자가 있으면',
     errorMessage: `${errorMessage.prefix} ${errorMessage.duplicatedNumber}`,
   },
   {
-    numbers: [1, 2, 3, 4, 5],
+    numbers: '1,2,3,4,5',
     description: '로또 번호의 개수가 6개보다 적으면',
     errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
   },
   {
-    numbers: [1, 2, 3, 4, 5, 55],
+    numbers: '1,2,3,4,5,55',
     description: '로또 번호에 1~45 범위를 벗어나는 숫자가 있으면',
     errorMessage: `${errorMessage.prefix} ${errorMessage.invalidNumberRange}`,
   },
