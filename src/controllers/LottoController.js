2506683472fcd84ec68377ea2623db3327e30fec
yheseul <kangyheseul@naver.com>
2024-11-03 21:54:07 +0900

    feat: LottoController에서 로또 구매 개수 및 번호 출력을 위한 로직 통합

---
+++ src/controllers/LottoController.js
@@ -0,0 +1,16 @@
+import { calculateLottoCountFromAmount } from "../services/calculateLottoCountFromAmount.js";
+import LottoView from "../views/LottoView.js";
+
+class LottoController {
+  constructor() {
+    this.lottoView = new LottoView();
+  }
+
+  run(purchaseAmount) {
+    const lottoCount = calculateLottoCountFromAmount(purchaseAmount);
+    this.lottoView.displayLottoCount(lottoCount);
+    this.lottoView.displayLottoNumbers(lottoCount);
+  }
+}
+
+export default LottoController;
