506eb7f153d41afc8a845ee1ba6264dcc91f4e11
yena <rladpsk1117@gmail.com>
2024-11-04 06:59:28 +0900

    feat(Result): 로또 당첨 번호 비교 로직 구현

--- src/model/Result.js
+++ src/model/Result.js
@@ -16,5 +16,38 @@ class Result {
       (this.#lotteryNumbers = lotteryNumbers),
       (this.#purchaseAmount = purchaseAmount);
   }
+
+  #parsedNumbers(numbersString) {
+    const splittedNumbers = numbersString.split(',');
+    return splittedNumbers.map((item) => parseInt(item.trim()));
+  }
+
+  #matchingCount(numbersString) {
+    let result = 0;
+    this.#parsedNumbers(numbersString).forEach((num) => {
+      if (this.#winningLottoNumbers.includes(num)) result++;
+      if (result === 5 && this.#bonusNumber === num) result += 0.5;
+    });
+    return result;
+  }
+
+  #winningRank(result) {
+    if (result === 6) this.#ranks.first++;
+    if (result === 5.5) this.#ranks.second++;
+    if (result === 5) this.#ranks.third++;
+    if (result === 4) this.#ranks.fourth++;
+    if (result === 3) this.#ranks.fifth++;
+  }
+
+  getWinningRank() {
+    return this.#ranks;
+  }
+
+  winningResult() {
+    for (let i = 0; i < this.#lotteryNumbers.length; i++) {
+      const result = this.#matchingCount(this.#lotteryNumbers[i]);
+      this.#winningRank(result);
+    }
+  }
 }
 export default Result;
