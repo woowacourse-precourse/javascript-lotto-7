fadbe08ef89e151482524417077190840a22bad6
yheseul <kangyheseul@naver.com>
2024-11-03 20:07:02 +0900

    refactor: 객체 지향으로 리팩토링 및 MVC 패턴 적용을 위한 ConsoleView 클래스 추가
    절차 지향에서 객체 지향으로 전환하여 코드의 재사용성과 유지보수성을 향상
    MVC 패턴 적용의 첫 단계로 ConsoleView 클래스를 생성하여 사용자 인터페이스 로직을 분리
    사용자 입력을 처리하는 메서드인 getPurchaseAmount를 구현하여 뷰의 책임을 명확히 함
    전체 코드 구조를 MVC 패턴에 맞춰 정리하여 이후 모델과 컨트롤러 구현에 대비함

---
+++ src/views/ConsoleView.js
@@ -0,0 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+import { RUN_MESSAGE } from "../constants/runMessages.js";
+
+class ConsoleView {
+  async purchaseAmount() {
+    return await Console.readLineAsync(RUN_MESSAGE.PURCHASE_AMOUNT);
+  }
+}
+
+export default ConsoleView;
