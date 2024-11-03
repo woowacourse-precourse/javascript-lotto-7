889b2750a72e09f49c792911b9546c1fe22e4e5c
장정안 <wjddks96@naver.com>
2024-11-04 04:39:19 +0900

    feat: jsdoc으로 타입체크안 한 부분 추가

--- src/views/LottoResultView.js
+++ src/views/LottoResultView.js
@@ -1,21 +1,31 @@
+//@ts-check
+
 import { outputView } from './outputView.js';
 import {
   GAME_MESSAGE,
   getMessagesByStatistics,
 } from '../constants/gameMessage.js';
+import Lotto from '../Lotto.js';
 
 class LottoResultView {
+  /**
+   * @param {number} count
+   * @param {Lotto[]} lottos
+   * @returns {void}
+   */
   printPurchaseResult(count, lottos) {
     outputView.printMessage(`${count}${GAME_MESSAGE.BOUGHT}`);
     this.printLottos(lottos);
   }
 
+  /**@param {Lotto[]} lottos  */
   printLottos(lottos) {
     lottos.forEach((lotto) => {
       outputView.printMessage(`[${lotto.getNumbers().join(', ')}]`);
     });
   }
 
+  /**@param {{ 3: number, 4: number, 5: number, BONUS: number, 6: number }} results*/
   printResults(results) {
     outputView.printMessage(GAME_MESSAGE.WINNING_STATISTICS);
     outputView.printMessage(GAME_MESSAGE.DIVIDING_LINE);
@@ -27,6 +37,7 @@ class LottoResultView {
     });
   }
 
+  /**@param {string} rate  */
   printEarningRate(rate) {
     outputView.printMessage(`총 수익률은 ${rate}%입니다.`);
   }
--- src/views/outputView.js
+++ src/views/outputView.js
@@ -1,8 +1,16 @@
+//@ts-check
+
 import { Console } from '@woowacourse/mission-utils';
 
 const outputView = {
+  /**@param {string} message  */
   printMessage(message) {
     Console.print(message);
+  },
+
+  /**@param {Error} error */
+  printErrorMessage(error) {
+    outputView.printMessage(error.message);
   },
 };
 
