05eeb53bc227ad9abd325ba5a060cae635cc7738
YujeongLEE <dldbwjd24@gmail.com>
2024-11-03 14:56:59 +0900

    refactor : purchasePrice 유효성검사 수정
    - Error발생은 validate함수에서 발생시킴
    - validationCheck는 boolean형 리턴으로 변경(함수명을 더 직관적으로 표현)

--- src/utils/purchasePrice.utils.js
+++ src/utils/purchasePrice.utils.js
@@ -1,11 +1,16 @@
-import {PURCHASE_PRICE} from "../constants/constants.js";
+import {ERROR_CODE, PURCHASE_PRICE} from "../constants/constants.js";
 import {validationCheck} from "../validation/validationCheck.js";
 
 export const purchasePriceUtils = {
     validate(purchasePrice) {
-        purchasePrice = validationCheck.isPositiveNumber(purchasePrice)
-        purchasePrice = validationCheck.isInRange(purchasePrice)
-        return validationCheck.isDividedNumberByValue(purchasePrice, PURCHASE_PRICE.MIN_CURR_UNIT)
+        if (!validationCheck.isPositiveNumber(purchasePrice))
+            throw new Error(ERROR_CODE.NOT_POSITIVE_NUMBER);
+        if (!validationCheck.isInRange(purchasePrice)) {
+            throw new Error(ERROR_CODE.OUT_OF_RANGE(1, Number.MAX_SAFE_INTEGER));
+        }
+        if (!validationCheck.isDividedNumberByValue(purchasePrice, PURCHASE_PRICE.MIN_CURR_UNIT))
+            throw new Error(ERROR_CODE.NOT_DIVIDED_BY_VALUE(PURCHASE_PRICE.MIN_CURR_UNIT));
+        return Number(purchasePrice);
     },
 
     getLottoAmount(purchasePrice) {
