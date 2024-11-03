907de3d86a8a41d060970b636d90431d668ac9c3
Won JongIn <wonggamggik@naver.com>
2024-11-04 06:07:19 +0900

    feat: Implement initial input handling in App class with run() method
    - Added input prompts for purchase amount, winning numbers, and bonus number in the run() method.
    
    - Verified input functionality using Console.print() to ensure correct data flow.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+
+    const winningNumbers = await Console.readLineAsync(
+      "\n당첨 번호를 입력해 주세요.\n"
+    );
+
+    const bonusNumber = await Console.readLineAsync(
+      "\n보너스 번호를 입력해 주세요.\n"
+    );
+
+    Console.print(`구입금액: ${purchaseAmount}`);
+    Console.print(`당첨 번호: ${winningNumbers}`);
+    Console.print(`보너스 번호: ${bonusNumber}`);
+  }
 }
 
 export default App;
