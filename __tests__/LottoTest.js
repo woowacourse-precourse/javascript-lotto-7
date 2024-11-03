066e0992c46932ff90850aac9b71072217d0cef6
salmonco <xkqjsslsek80@naver.com>
2024-11-04 02:11:05 +0900

    test(LottoTest): 기능 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,39 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test.each([[[1, 2, 3, 4, 5, -4]], [[1, 2, 3, 4, 5, 0]], [[1, 2, 3, 4, 5, 46]]])(
+    '로또 번호가 1~45 사이의 숫자가 아닌 경우 예외가 발생한다. %o',
+    (numbers) => {
+      expect(() => {
+        new Lotto(numbers);
+      }).toThrow('[ERROR]');
+    },
+  );
+
+  test('getMatchedCountWithWinningNumbers', () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.getMatchedCountWithWinningNumbers(winningNumbers)).toBe(6);
+  });
+
+  test('isMatchedWithBonusNumber', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.isMatchedWithBonusNumber(7)).toBe(false);
+    expect(lotto.isMatchedWithBonusNumber(3)).toBe(true);
+  });
+
+  test('toString', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.toString()).toBe('[1, 2, 3, 4, 5, 6]');
+  });
 });
