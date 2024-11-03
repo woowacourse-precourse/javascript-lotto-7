5d19479994b609c1b6dfb5e698c9d3caf0b567cb
hyeongdong <khd941018@gmail.com>
2024-11-04 02:57:22 +0900

    refactor: LottoService에서 WinningStatsManager로 코드 분리
    비즈니스로직을 담당하는 객체의 상호작용을 위해 LottoService를 구현했기 때문에
    분리한 기능들은 다른 클래스에게 책임을 맡겨야 한다고 생각
    또 winningStats는 상태로 관리할 필요를 느꼈기 때문에 새로운 클래스로 기능을 분리

--- src/LottoService.js
+++ src/LottoService.js
@@ -2,78 +2,49 @@ class LottoService {
   #lottoMatchChecker;
   #purchaseManager;
   #lottoIssuer;
+  #winningStatsManager;
   #lottos = [];
 
-  constructor(purchaseManager, lottoMatchChecker, lottoIssuer) {
+  constructor(purchaseManager, lottoMatchChecker, lottoIssuer, winningStatsManager) {
     this.#lottoMatchChecker = lottoMatchChecker;
     this.#purchaseManager = purchaseManager;
     this.#lottoIssuer = lottoIssuer;
-  }
-
-  getLottos() {
-    return this.#lottos;
+    this.#winningStatsManager = winningStatsManager;
   }
 
   addLotto(numbers) {
     this.#lottos.push(this.#lottoIssuer.createLotto(numbers));
   }
 
+  getLottos() {
+    return this.#lottos;
+  }
+
   getMaxLottoCount(budget) {
-    this.#purchaseManager.calculateMaxLottos(budget);
+    return this.#purchaseManager.calculateMaxLottos(budget);
   }
 
-  getWinningStats() {
-    const lottoResults = this.aggregateLottoResults();
-    const rankCounts = {
-      match_6: 0,
-      match_5_bonus: 0,
-      match_5: 0,
-      match_4: 0,
-      match_3: 0,
-    };
+  getWinningStats(bonusNumber, winningNumbers) {
+    const lottoResults = this.aggregateLottoResults(bonusNumber, winningNumbers);
+    this.#winningStatsManager.updateWinningStats(lottoResults);
 
-    lottoResults.forEach(([matchCount, hasBonusNumber]) => {
-      const totalMatchCount = matchCount + hasBonusNumber;
+    return this.#winningStatsManager.getStats();
+  }
 
-      if (matchCount === 5 && hasBonusNumber) {
-        rankCounts[`match_${matchCount}_bonus`] += 1;
-        return;
-      }
+  getProfitMargin(lottoPrice) {
+    const lottoPurchaseCost = lottoPrice * this.#lottos.length;
+    this.#winningStatsManager.calculateTotalProfit(prizeMoneyByRank);
 
-      rankCounts[`match_${totalMatchCount}`] += 1;
+    return this.#winningStatsManager.calculateProfitMargin(lottoPurchaseCost);
+  }
+
+  aggregateLottoResults(bonusNumber, winningNumbers) {
+    return this.#lottos.map((lotto) => {
+      const matchCount = this.#lottoMatchChecker.countMatches(winningNumbers, lotto);
+      const hasBonusNumber = this.#lottoMatchChecker.hasBonusNumber(bonusNumber, lotto);
+
+      return [matchCount, hasBonusNumber];
     });
-
-    return rankCounts;
-  }
-
-  aggregateLottoResults() {
-    const lottoResults = this.#lottos.map((lotto) => {
-      return [this.#lottoMatchChecker.countMatchesIn(lotto), this.#lottoMatchChecker.isBonusNumberIn(lotto)];
-    });
-
-    return lottoResults;
-  }
-
-  calculateTotalProfit(rankCounts) {
-    const prizeMoneyByRank = {
-      match_6: 2000000000,
-      match_5_bonus: 30000000,
-      match_5: 1500000,
-      match_4: 50000,
-      match_3: 5000,
-    };
-
-    return Object.keys(rankCounts).reduce((totalProfit, rank) => {
-      return totalProfit + prizeMoneyByRank[rank] * rankCounts[rank];
-    }, 0);
-  }
-
-  calculateProfitMargin(lottoPrice, rankCounts) {
-    const lottoTotalProfit = this.calculateTotalProfit(rankCounts);
-    const lottoPurchaseCost = this.#lottos.length * lottoPrice;
-    const lottoProfitMargin = lottoTotalProfit / lottoPurchaseCost;
-
-    return lottoProfitMargin.toFixed(1);
   }
 }
 
---
+++ src/WinningStatsManager.js
@@ -0,0 +1,42 @@
+class WinningStatsManager {
+  #winningStats;
+  #totalProfit;
+
+  constructor() {
+    this.#winningStats = {
+      match_6: 0,
+      match_5_bonus: 0,
+      match_5: 0,
+      match_4: 0,
+      match_3: 0,
+    };
+    this.#totalProfit = 0;
+  }
+
+  getStats() {
+    return this.#winningStats;
+  }
+
+  updateWinningStats(lottoResults) {
+    lottoResults.forEach(([matchCount, hasBonusNumber]) => {
+      if (matchCount === 5 && hasBonusNumber) {
+        this.#winningStats[`match_${matchCount}_bonus`]++;
+        return;
+      }
+
+      this.#winningStats[`match_${matchCount + hasBonusNumber}`]++;
+    });
+  }
+
+  calculateTotalProfit(prizeMoneyByRank) {
+    this.#totalProfit += Object.keys(this.#winningStats).reduce((totalProfit, rank) => {
+      return totalProfit + prizeMoneyByRank[rank] * this.#winningStats[rank];
+    }, 0);
+  }
+
+  calculateProfitMargin(lottoPurchaseCost) {
+    const lottoProfitMargin = this.#totalProfit / lottoPurchaseCost;
+
+    return lottoProfitMargin.toFixed(1);
+  }
+}
