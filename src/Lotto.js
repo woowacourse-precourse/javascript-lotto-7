8a67b11369123cf1a45371173fb2b0d95575aaea
hyeongdong <khd941018@gmail.com>
2024-11-03 13:40:47 +0900

    feat(Lotto): 로또를 오름차순으로 정렬하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,11 +5,19 @@ class Lotto {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
-
+  // 왜 private으로 ?
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  sortAscending() {
+    this.#numbers.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
+  }
+
+  contains(winningNumber) {
+    return true;
   }
 
   // TODO: 추가 기능 구현
