96aafc1c545131a3c8cd4c7e3129b4bc24ea1334
seonae-j <wkdtjsdo135@naver.com>
2024-11-03 23:40:46 +0900

    feat(Calculation): 수익률 계산하는 함수 추가

--- src/utils/Calculation.js
+++ src/utils/Calculation.js
@@ -1,6 +1,10 @@
 import { UNIT } from '../constants/Constants.js';
 
 // 사용자가 입력한 구매 값을 이용해 ticket 개수 출력
-export function ticketCount(cost) {
+export const ticketCount = (cost) => {
   return Number(cost) / UNIT.TICKET_PRICE;
-}
+};
+
+export const ROI = (total, cost) => {
+  return ((total / cost) * 100).toFixed(1);
+};
