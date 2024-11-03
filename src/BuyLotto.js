80b2d47c424fcfb9f0aad20821dcd42196f358f5
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-03 20:22:10 +0900

    feat: 입력 시도 횟수 제한

--- src/BuyLotto.js
+++ src/BuyLotto.js
@@ -15,14 +15,23 @@ export default class BuyLotto {
   #BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
   #PURCHASE_LOTTO_PROMPT = "개를 구매했습니다.";
   #EMPTY_STRING = "";
+  #ATTEMPTS_ERROR_MESSAGE =
+    "[ERROR]최대 입력 횟수를 초과했습니다. 다시 시도해 주세요.";
 
   constructor() {
     this.#validator = new Validator();
     let input;
   }
 
-  async enterLottoPrice() {
+  #limitAttempts(attempts) {
+    if (attempts >= 10) {
+      throw new Error(this.#ATTEMPTS_ERROR_MESSAGE);
+    }
+  }
+
+  async enterLottoPrice(attempts = 0) {
     try {
+      this.#limitAttempts(attempts);
       Console.print(this.#PRICE_PROMPT);
       this.#inputPrice = await Console.readLineAsync("");
       Console.print(this.#EMPTY_STRING);
@@ -31,37 +40,39 @@ export default class BuyLotto {
       await this.enterWinningNumber();
     } catch (error) {
       Console.print(error.message);
-      await this.enterLottoPrice();
+      await this.enterLottoPrice(attempts + 1);
     }
   }
 
-  async enterWinningNumber() {
-    try{
+  async enterWinningNumber(attempts = 0) {
+    try {
+      this.#limitAttempts(attempts);
       Console.print(this.#WINNING_NUMBER_PROMPT);
       this.input = await Console.readLineAsync("");
       this.#validator.validateWinningNumber(this.input);
       this.#winningLotto = new Lotto(this.input.split(",").map(Number));
       Console.print(this.#EMPTY_STRING);
       await this.enterBonusNumber();
-    }catch (error) {
+    } catch (error) {
       Console.print(error.message);
-      await this.enterWinningNumber();
+      await this.enterWinningNumber(attempts + 1);
     }
-    
   }
 
-  async enterBonusNumber() {
-    try{
+  async enterBonusNumber(attempts = 0) {
+    try {
       Console.print(this.#BONUS_NUMBER_PROMPT);
       this.input = await Console.readLineAsync("");
-      this.#validator.validateBonusNumnber(this.input, this.#winningLotto.getNumbers());
+      this.#validator.validateBonusNumnber(
+        this.input,
+        this.#winningLotto.getNumbers()
+      );
       this.#bonusNumber = Number(this.input);
       Console.print(this.#EMPTY_STRING);
-    }catch (error) {
+    } catch (error) {
       Console.print(error.message);
-      await this.enterBonusNumber();
+      await this.enterBonusNumber(attempts + 1);
     }
-    
   }
 
   #getAmountOfLotto(price) {
@@ -71,7 +82,11 @@ export default class BuyLotto {
 
   #generateLottoTickets(purchaseAmount) {
     for (let i = 0; i < purchaseAmount; i += 1) {
-      const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1,45,6);
+      const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
+        1,
+        45,
+        6
+      );
       this.#purchasedLotto.push(randomNumber);
       this.#validator.validateDuplicateNumber(randomNumber);
     }
