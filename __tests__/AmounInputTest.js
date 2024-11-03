c81e6f80a8cdc134ca5a3fed0171f3cd34d8b288
oko_jin <dswvgw6412@naver.com>
2024-11-03 10:10:24 +0900

    test(amountInputTest): 구매 금액 입력 테스트 코드 구현

---
+++ __tests__/AmounInputTest.js
@@ -0,0 +1,41 @@
+import { amountInputTestCase } from '../src/constant/amountInputTestCase.js';
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
+
+describe('구매 금액 입력 테스트', () => {
+  test.each(amountInputTestCase)(
+    '$description 예외가 발생한다.',
+    async ({ amount, errorMessage }) => {
+      // given
+      const logSpy = getLogSpy();
+
+      const AMOUNT_INPUT = `${amount}`;
+
+      mockQuestions([AMOUNT_INPUT]);
+
+      // when
+      const app = new App();
+      await app.run();
+
+      // then
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    }
+  );
+  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+});
