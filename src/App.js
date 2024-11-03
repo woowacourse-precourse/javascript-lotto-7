479caf27453e9578df04dbe8d7428165cf4283b3
yubin <yb3143@gmail.com>
2024-11-03 16:42:55 +0900

    feat: 수익률 계산

--- src/App.js
+++ src/App.js
@@ -55,7 +55,11 @@ class App {
     );
     let result = this.getResult(comparedList);
 
-    Console.print(result);
+    let rate = this.getRate(result[1], price);
+
+    Console.print("당첨 통계\n---");
+    Console.print(result[0]);
+    Console.print(`총 수익률은 ${rate}%입니다.`);
   }
 
   // 로또 번호 생성
@@ -103,11 +107,35 @@ class App {
       }
     });
 
-    return `3개 일치 (5,000원) - ${result[3]}개\n
-4개 일치 (50,000원) - ${result[4]}개\n 
-5개 일치 (1,500,000원) - ${result[5]}개\n
-5개 일치, 보너스 볼 일치 (30,000,000원) - ${result["5(bonus)"]}개\n
-6개 일치 (2,000,000,000원) - ${result[6]}개`;
+    const resultString =
+      `3개 일치 (5,000원) - ${result[3]}개\n` +
+      `4개 일치 (50,000원) - ${result[4]}개\n` +
+      `5개 일치 (1,500,000원) - ${result[5]}개\n` +
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result["5(bonus)"]}개\n` +
+      `6개 일치 (2,000,000,000원) - ${result[6]}개`;
+
+    return [resultString, result];
+  }
+
+  // 수익률 계산
+  getRate(result, price) {
+    const prizeMoney = {
+      3: 5000,
+      4: 50000,
+      5: 1500000,
+      "5(bonus)": 30000000,
+      6: 2000000000,
+    };
+
+    let totalPrize =
+      result[3] * prizeMoney[3] +
+      result[4] * prizeMoney[4] +
+      result[5] * prizeMoney[5] +
+      result["5(bonus)"] * prizeMoney["5(bonus)"] +
+      result[6] * prizeMoney[6];
+
+    let rate = ((totalPrize / price) * 100).toFixed(1);
+    return rate;
   }
 }
 
