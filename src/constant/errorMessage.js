357eb153367993f2750194072c045f0c37d3fbf2
oko_jin <dswvgw6412@naver.com>
2024-11-03 12:02:02 +0900

    feat(errorMessage): 에러메시지 구매 금액이 정수가 아닌 경우 추가

--- src/constant/errorMessage.js
+++ src/constant/errorMessage.js
@@ -1,6 +1,7 @@
 export const errorMessage = Object.freeze({
   prefix: '[ERROR]',
   invalidAmount: '금액은 숫자로 입력해주세요.',
+  notIntegerAmount: '금액은 정수로 입력해주세요.',
   negativeAmount: '구매금액은 0보다 커야합니다.',
   divisibleAmount: '구매금액은 1000원 단위로 입력해주세요.',
   invalidDelimiter: '구분자는 , 이어야 합니다.',
