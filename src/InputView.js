efb798c1abf157645187da969530c858c071c8e2
dudcns02 <duddl2460@naver.com>
2024-11-03 16:13:04 +0900

    feaat: Add validation try/catch of price

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+
 const price = await Console.readLineAsync(LOG_MESSAGE.START_MESSAGE);
 // price = '8000'
 const winningNumber = await Console.readLineAsync(
@@ -9,3 +10,21 @@ const bonusNumber = await Console.readLineAsync(
   LOG_MESSAGE.BONUS_NUMBER_MESSGE
 );
 // bonusNumber = '7'
+
+// 시도
+
+async function priceValidator() {
+  try {
+    const price = await Console.readLineAsync(LOG_MESSAGE.START_MESSAGE);
+
+    isNumber(price);
+    isUnitOfPrice(price);
+    minprice(price);
+    maxprice(price);
+
+    getLottoCount(price);
+  } catch (error) {
+    Console.print(error.message);
+    return priceValidator();
+  }
+}
