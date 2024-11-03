2f6de1d3a1b6c71d4507f329ab08b07acab34f8a
keemsebeen <sebin0580@gmail.com>
2024-11-03 23:32:16 +0900

    feat(Output) : getTicketNumber 함수 구현

--- src/io/Output.js
+++ src/io/Output.js
@@ -3,3 +3,9 @@ import { Console } from "@woowacourse/mission-utils";
 export const getOutput = async (message) => {
   return await Console.print(message);
 };
+
+export const getTicketNumber = async (tickets) => {
+  for (const ticket of tickets) {
+    await Console.print(ticket);
+  }
+};
