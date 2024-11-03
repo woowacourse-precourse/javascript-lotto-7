fdaecd942268f49ae2b0855d1383e31c0eea83bf
jjang-aaa <jadoit473@gmail.com>
2024-11-04 04:04:49 +0900

    refactor(Output) : 출력 메세지들을 const에 담아서 출력하도록 수정

--- src/lottoMachine/Output.js
+++ src/lottoMachine/Output.js
@@ -2,28 +2,34 @@ import { Console } from '@woowacourse/mission-utils';
 
 export class Output {
   printLottoTicket(lottoTicketArr) {
-    Console.print(`\n${lottoTicketArr.length}개를 구매했습니다.`);
+    const ticketCountMessage = `\n${lottoTicketArr.length}개를 구매했습니다.`;
+
+    Console.print(ticketCountMessage);
 
     lottoTicketArr.forEach((ticket) => {
       const ticketNumArr = ticket.lottoNumbers;
+      const issuedLottoTickets = `[${ticketNumArr.join(', ')}]`;
 
-      Console.print(`[${ticketNumArr.join(', ')}]`);
+      Console.print(issuedLottoTickets);
     });
   }
 
   printWinnerRank(rankCounts) {
-    Console.print(`
-당첨 통계
----
-3개 일치 (5,000원) - ${rankCounts[3].count}개
-4개 일치 (50,000원) - ${rankCounts[4].count}개
-5개 일치 (1,500,000원) - ${rankCounts[5].count}개
-5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankCounts[5.5].count}개
-6개 일치 (2,000,000,000원) - ${rankCounts[6].count}개
-    `);
+    const winningStatistics = '\n당첨 통계\n---\n';
+    const matchingLottoNumbers = [
+      `3개 일치 (5,000원) - ${rankCounts[3].count}개`,
+      `4개 일치 (50,000원) - ${rankCounts[4].count}개`,
+      `5개 일치 (1,500,000원) - ${rankCounts[5].count}개`,
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankCounts[5.5].count}개`,
+      `6개 일치 (2,000,000,000원) - ${rankCounts[6].count}개`,
+    ].join('\n');
+
+    Console.print(winningStatistics + matchingLottoNumbers);
   }
 
   printRateOfReturn(rateOfReturn) {
-    Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
+    const rateOfReturnMessage = `총 수익률은 ${rateOfReturn}%입니다.`;
+
+    Console.print(rateOfReturnMessage);
   }
 }
