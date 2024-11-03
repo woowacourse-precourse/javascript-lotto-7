0cc84549d34b44a4b99c907d5e6f69cce68d199c
COYO <dayeon.yoon@gmail.com>
2024-11-04 00:48:21 +0900

    feat(Lotto): lotto 클래스에서 내부에서 숫자 치환 삭제

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,7 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers.map((v) => +v).sort((a, b) => a - b);
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   #validate(numbers) {
