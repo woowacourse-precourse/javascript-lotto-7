8de182c088783a2c568937372fd135abcd261c4a
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-04 06:02:23 +0900

    test(lottoGameTest): 수익률 계산 테스트 코드 구현

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -74,16 +74,60 @@ describe('로또게임 클래스 테스트', () => {
 
       // 테스트용 로또 번호 설정
       lottoGame.lottos = [
-        new Lotto([1, 2, 3, 7, 8, 9]), 
-        new Lotto([1, 2, 3, 4, 5, 7]), 
-        new Lotto([1, 2, 3, 4, 5, 6]), 
+        new Lotto([1, 2, 3, 7, 8, 9]),
+        new Lotto([1, 2, 3, 4, 5, 7]),
+        new Lotto([1, 2, 3, 4, 5, 6]),
       ];
 
       lottoGame.calculateWinningResults();
 
-      expect(lottoGame.result['5th place']).toBe(1); 
-      expect(lottoGame.result['2nd place']).toBe(1); 
-      expect(lottoGame.result['1st place']).toBe(1); 
+      expect(lottoGame.result['5th place']).toBe(1);
+      expect(lottoGame.result['2nd place']).toBe(1);
+      expect(lottoGame.result['1st place']).toBe(1);
+    });
+  });
+
+  describe('수익률 계산 테스트', () => {
+    test('수익률이 정확히 계산된다', () => {
+      const lottoGame = new LottoGame(QUANTITY);
+      lottoGame.result = {
+        '5th place': 1,
+        '4th place': 1,
+        '3rd place': 0,
+        '2nd place': 0,
+        '1st place': 0,
+      };
+
+      const profitRate = lottoGame.calculateProfitRate();
+      expect(profitRate).toBe(1833.3);
+    });
+
+    test('소수점 둘째 자리에서 반올림이 정확히 된다', () => {
+      const lottoGame = new LottoGame(QUANTITY);
+      lottoGame.result = {
+        '5th place': 1,
+        '4th place': 0,
+        '3rd place': 0,
+        '2nd place': 0,
+        '1st place': 0,
+      };
+
+      const profitRate = lottoGame.calculateProfitRate();
+      expect(profitRate).toBe(166.7);
+    });
+
+    test('당첨금 총액이 정확히 계산된다', () => {
+      const lottoGame = new LottoGame(QUANTITY);
+      lottoGame.result = {
+        '5th place': 1,
+        '4th place': 1,
+        '3rd place': 1,
+        '2nd place': 1,
+        '1st place': 1,
+      };
+
+      const totalAmount = lottoGame.calculateWinningAmount();
+      expect(totalAmount).toBe(2031555000);
     });
   });
 });
