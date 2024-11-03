82cf741ec8029c446132735d8aa75ec23ccb419a
zelkovaria <hyein396@gmail.com>
2024-11-04 01:22:56 +0900

    feat: 입력 금액이 0 이하인 경우 예외처리 및 테스트 구현

--- __tests__/InputMoneyTest.js
+++ __tests__/InputMoneyTest.js
@@ -2,8 +2,29 @@ import { validateInputMoney } from "../src/validator/InputMoney";
 
 describe("입력 금액 테스트", () => {
   const inputMoney = ["1000a", "10ㅇㅇ", "100@"];
+  const notPositiveNum = [0, -3000];
+  const notMoneyUnit = [1100, 200, 10, 5];
+
   test.each(inputMoney)(
     "입력 금액에 문자가 포함되어 있으면 예외가 발생한다.",
+    (money) => {
+      expect(() => {
+        validateInputMoney(money);
+      }).toThrow("[ERROR]");
+    }
+  );
+
+  test.each(notPositiveNum)(
+    "입력 금액은 0 이하 값이면 예외가 발생한다.",
+    (money) => {
+      expect(() => {
+        validateInputMoney(money);
+      }).toThrow("[ERROR]");
+    }
+  );
+
+  test.each(notMoneyUnit)(
+    "입력 금액은 1000원 단위가 아니면 예외가 발생한다.",
     (money) => {
       expect(() => {
         validateInputMoney(money);
--- src/validator/InputMoney.js
+++ src/validator/InputMoney.js
@@ -6,6 +6,22 @@ const validateNumberOnly = (inputMoney) => {
   }
 };
 
+const validatePositiveNum = (inputMoney) => {
+  if (inputMoney <= 0) {
+    throw new Error("[ERROR] 입력 금액은 0이 넘는 금액이 입력되어야 합니다.");
+  }
+};
+
+const validateMoneyUnit = (inputMoney) => {
+  const isUnitMoney = inputMoney % 1000 === 0;
+
+  if (!isUnitMoney) {
+    throw new Error("[ERROR] 입력 금액은 1000원 단위로 입력되어야 합니다.");
+  }
+};
+
 export const validateInputMoney = (inputMoney) => {
   validateNumberOnly(inputMoney);
+  validatePositiveNum(inputMoney);
+  validateMoneyUnit(inputMoney);
 };
