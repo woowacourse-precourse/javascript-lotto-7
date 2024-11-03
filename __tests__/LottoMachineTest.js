e844461088f32318d1d33132ad818c63d56e9150
jiyeeeah <sije3757@snu.ac.kr>
2024-11-03 17:58:20 +0900

    test: 당첨 내역 출력을 위한 LottoMachine 테스트 코드 작성

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-new */
 import { Random } from "@woowacourse/mission-utils";
 import LottoMachine from "../src/LottoMachine.js";
+import Lotto from "../src/Lotto.js";
+import BonusNumber from "../src/BonusNumber.js";
 
 const mockRandoms = (numbers) => {
   Random.pickUniqueNumbersInRange = jest.fn();
@@ -44,5 +46,30 @@ describe("로또 기계 클래스 테스트", () => {
 [7, 11, 30, 40, 42, 43]
 [2, 13, 22, 32, 38, 45]
 [1, 3, 5, 14, 22, 45]`);
+  });
+
+  test("당첨 내역을 출력한다.", () => {
+    const input = "8000";
+
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    const lottoMachine = new LottoMachine(input);
+    const winningLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = new BonusNumber(winningLotto, 7);
+
+    expect(lottoMachine.getWinningLottery({ winningLotto, bonusNumber }))
+      .toBe(`3개 일치 (5,000원) - 1개
+4개 일치 (50,000원) - 0개
+5개 일치 (1,500,000원) - 0개
+5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
+6개 일치 (2,000,000,000원) - 0개`);
   });
 });
