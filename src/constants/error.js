9d3ec6cdc6ffa80c538c7e94457c1e68fc7f81da
keemsebeen <sebin0580@gmail.com>
2024-11-03 23:01:54 +0900

    feat(constants) : error 관련 메세지 상수화

---
+++ src/constants/error.js
@@ -0,0 +1,16 @@
+export const PRICE = {
+  NON_DIVISIBLE_AMOUNT: "금액은 1000원으로 나눠떨어져야 합니다.",
+};
+
+export const LOTTO_ERROR = {
+  RANGE: "1~45 사이의 값을 입력해주세요.",
+  INVALID_DELIMITER: "쉼표를 제외한 다른 특수문자는 사용할 수 없습니다.",
+  DOUBLE_COMMA:
+    "잘못된 구분이 사용되었습니다. 두 개 이상의 쉼표는 허용되지 않습니다.",
+};
+
+export const ZERO = 0;
+export const REQUEST_NUMBER_INPUT = "숫자를 입력해주세요.";
+export const MINIMUM_VALUE = "값은 0보다 커야합니다.";
+export const EMPTY = "값이 입력되지 않았습니다.";
+export const ERROR_MESSAGE = "[ERROR] ";
