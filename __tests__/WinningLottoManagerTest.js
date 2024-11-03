c096c8d3c1de525fdb6c9c84dddf2e0650867c91
jeongeunLee <leeju1009@gmail.com>
2024-11-03 20:07:12 +0900

    test: 숫자가 아닌 당첨 번호를 예외 처리하는지 테스트 추가

---
+++ __tests__/WinningLottoManagerTest.js
@@ -0,0 +1,47 @@
+import { Console } from '@woowacourse/mission-utils';
+import { ERROR_MESSAGE } from '../src/utils/Constants';
+import WinningLottoManager from '../src/WinningLottoManager';
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  inputs.reduce(
+    (prevChain, input) => prevChain.mockResolvedValueOnce(input),
+    Console.readLineAsync,
+  );
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('WinningLottoManager 테스트', () => {
+  const selectSpy = jest.spyOn(WinningLottoManager, 'selectWinningNumbers');
+
+  beforeEach(() => {
+    selectSpy.mockClear();
+  });
+
+  test.each([
+    ['1,2,3,4,5,숫자아님'],
+    ['1,2,null,4,5,6'],
+    ['1,2,3,4,5,undefined'],
+    ['1,2,3,4,$,6'],
+    ['번호,2,3,4,5,6'],
+  ])('숫자가 아닌 당첨 번호를 예외 처리하는지 테스트 (%s)', async (inputs) => {
+    const VALID_NUMBERS = '1,2,3,4,5,6';
+    const ASKING_TIMES = 2;
+    const logSpy = getLogSpy();
+
+    mockQuestions([inputs, VALID_NUMBERS]);
+
+    await WinningLottoManager.selectWinningNumbers(inputs);
+
+    expect(selectSpy).toHaveBeenCalledTimes(ASKING_TIMES);
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(ERROR_MESSAGE.notNumber),
+    );
+  });
+});
