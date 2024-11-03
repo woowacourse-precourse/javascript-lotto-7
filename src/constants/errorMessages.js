17f62d9eb06392f01b8a6f266e1b600c83cbd108
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 01:18:43 +0900

    feat: 당첨 번호 입력에 대한 에러 상수 정의

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -8,6 +8,15 @@ const ERROR_MESSAGES = Object.freeze({
     '로또 1장의 가격은 1,000원입니다. 반드시 1,000원 단위로 입력해 주세요.',
   PURCHASE_PRICE_NOT_A_NUMBER:
     '구입금액이 잘못 입력되었습니다. 구입금액은 반드시 정수로 입력해 주세요.',
+
+  WINNING_NUMBER_COUNT_INVALID:
+    '당첨 번호의 개수가 올바르지 않습니다. 반드시 6개의 번호를 입력해 주세요.',
+  WINNING_NUMBER_DUPLICATE:
+    '중복된 당첨 번호가 존재합니다. 반드시 서로 다른 6개의 번호를 입력해 주세요.',
+  WINNING_NUMBER_FORMAT_INVALID:
+    '잘못 입력된 당첨 번호가 존재합니다. 반드시 6개의 정수를 입력해 주세요.',
+  WINNING_NUMBER_OUT_OF_RANGE:
+    '범위를 벗어난 당첨 번호가 존재합니다. 반드시 1 ~ 45 사이의 번호를 입력해 주세요.',
 });
 
 export default ERROR_MESSAGES;
