5d71bacd81811443835066d20d4f0b5997ac1a53
jeongeunLee <leeju1009@gmail.com>
2024-11-03 20:55:46 +0900

    test: 반복되는 selectWinningNumbers 메서드 로직을 함수로 분리

--- __tests__/WinningLottoManagerTest.js
+++ __tests__/WinningLottoManagerTest.js
@@ -24,13 +24,7 @@ describe('WinningLottoManager 테스트', () => {
     selectSpy.mockClear();
   });
 
-  test.each([
-    ['1, 2, 3, 4, 5, 숫자아님'],
-    ['1, 2, null, 4, 5, 6'],
-    ['1, 2, 3, 4, 5, undefined'],
-    ['1, 2, 3, 4, $, 6'],
-    ['번호, 2, 3, 4, 5, 6'],
-  ])('숫자가 아닌 당첨 번호를 예외 처리하는지 테스트 (%s)', async (inputs) => {
+  const testSelectWinningNumbers = async (inputs, errorMessage) => {
     const VALID_NUMBERS = '1, 2, 3, 4, 5, 6';
     const ASKING_TIMES = 2;
     const logSpy = getLogSpy();
@@ -40,9 +34,19 @@ describe('WinningLottoManager 테스트', () => {
     await WinningLottoManager.selectWinningNumbers(inputs);
 
     expect(selectSpy).toHaveBeenCalledTimes(ASKING_TIMES);
-    expect(logSpy).toHaveBeenCalledWith(
-      expect.stringContaining(ERROR_MESSAGE.notNumber),
-    );
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
+  };
+
+  test.each([
+    ['1, 2, 3, 4, 5, 숫자아님'],
+    ['1, 2, null, 4, 5, 6'],
+    ['1, 2, 3, 4, 5, undefined'],
+    ['1, 2, 3, 4, $, 6'],
+    ['번호, 2, 3, 4, 5, 6'],
+  ])('숫자가 아닌 당첨 번호를 예외 처리하는지 테스트 (%s)', async (inputs) => {
+    const errorMessage = ERROR_MESSAGE.notNumber;
+
+    await testSelectWinningNumbers(inputs, errorMessage);
   });
 
   test.each([
@@ -54,18 +58,9 @@ describe('WinningLottoManager 테스트', () => {
   ])(
     '지정 범위가 아닌 당첨 번호를 예외 처리하는지 테스트 (%s)',
     async (inputs) => {
-      const VALID_NUMBERS = '1, 2, 3, 4, 5, 6';
-      const ASKING_TIMES = 2;
-      const logSpy = getLogSpy();
+      const errorMessage = ERROR_MESSAGE.notInRangeNumber;
 
-      mockQuestions([inputs, VALID_NUMBERS]);
-
-      await WinningLottoManager.selectWinningNumbers(inputs);
-
-      expect(selectSpy).toHaveBeenCalledTimes(ASKING_TIMES);
-      expect(logSpy).toHaveBeenCalledWith(
-        expect.stringContaining(ERROR_MESSAGE.notInRangeNumber),
-      );
+      await testSelectWinningNumbers(inputs, errorMessage);
     },
   );
 
@@ -76,18 +71,9 @@ describe('WinningLottoManager 테스트', () => {
     ['44.17, 2, 3, 4, 5, 6'],
     ['7.7, 2, 3, 4, 5, 6'],
   ])('정수가 아닌 당첨 번호를 예외 처리하는지 테스트 (%s)', async (inputs) => {
-    const VALID_NUMBERS = '1, 2, 3, 4, 5, 6';
-    const ASKING_TIMES = 2;
-    const logSpy = getLogSpy();
+    const errorMessage = ERROR_MESSAGE.notInteger;
 
-    mockQuestions([inputs, VALID_NUMBERS]);
-
-    await WinningLottoManager.selectWinningNumbers(inputs);
-
-    expect(selectSpy).toHaveBeenCalledTimes(ASKING_TIMES);
-    expect(logSpy).toHaveBeenCalledWith(
-      expect.stringContaining(ERROR_MESSAGE.notInteger),
-    );
+    await testSelectWinningNumbers(inputs, errorMessage);
   });
 
   test.each([
@@ -97,18 +83,9 @@ describe('WinningLottoManager 테스트', () => {
   ])(
     '당첨 번호가 지정된 개수만큼 뽑히지 않았을때 예외 처리하는지 테스트 (%s)',
     async (inputs) => {
-      const VALID_NUMBERS = '1, 2, 3, 4, 5, 6';
-      const ASKING_TIMES = 2;
-      const logSpy = getLogSpy();
+      const errorMessage = ERROR_MESSAGE.notValidNumberCount;
 
-      mockQuestions([inputs, VALID_NUMBERS]);
-
-      await WinningLottoManager.selectWinningNumbers(inputs);
-
-      expect(selectSpy).toHaveBeenCalledTimes(ASKING_TIMES);
-      expect(logSpy).toHaveBeenCalledWith(
-        expect.stringContaining(ERROR_MESSAGE.notValidNumberCount),
-      );
+      await testSelectWinningNumbers(inputs, errorMessage);
     },
   );
 });
