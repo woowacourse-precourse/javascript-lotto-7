1ab8b8ce04d93fc5b6fc769047bd9f1971956de7
songudeum <songudeum@gmail.com>
2024-11-04 01:39:58 +0900

    feat: App class에 구현한 기능들 연결 및 재질문을 위한 분기 처리 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
+import { Console } from "@woowacourse/mission-utils";
+import Script from "./script.js";
+import LotteryGenerator from "./LotteryGenerator.js";
+import LottoMatcher from "./LottoMatcher.js";
+import ProfitCalculator from "./ProfitCalculator.js";
 class App {
-  async run() {}
+    async run() {
+        const lottoList = await this.buyLotto();
+        const lottoMatcher = new LottoMatcher();
+        await this.makeWinNumber(lottoMatcher);
+        await this.makeBonusNumber(lottoMatcher);
+        this.makeProfitResult(lottoList, lottoMatcher);
+    }
+
+    async buyLotto() {
+        try {
+            const buyingCostInput = await Console.readLineAsync(
+                Script.PLEASE_INPUT_BUYINGCOST
+            );
+            const lottoList = new LotteryGenerator(buyingCostInput);
+            Console.print(Script.showLottoInfo(lottoList));
+
+            return lottoList;
+        } catch (error) {
+            Console.print(error.message);
+            return await this.buyLotto();
+        }
+    }
+
+    async makeWinNumber(lottoMatcher) {
+        try {
+            const winNumbersInput = await Console.readLineAsync(
+                Script.PLEASE_INPUT_WIN_NUMBERS
+            );
+
+            lottoMatcher.winNumberList = winNumbersInput;
+        } catch (error) {
+            Console.print(error.message);
+            await this.makeWinNumber(lottoMatcher);
+        }
+    }
+
+    async makeBonusNumber(lottoMatcher) {
+        try {
+            const bonusNumberInput = await Console.readLineAsync(
+                Script.PLEASE_INPUT_BONUS_NUMBER
+            );
+            lottoMatcher.bounusNumber = bonusNumberInput;
+        } catch (error) {
+            Console.print(error.message);
+            await this.makeBonusNumber(lottoMatcher);
+        }
+    }
+
+    makeProfitResult(lottoList, lottoMatcher) {
+        lottoMatcher.makeResult(lottoList);
+
+        const profitCalculator = new ProfitCalculator(lottoMatcher, lottoList);
+        Console.print(Script.showTotalProfit(profitCalculator));
+    }
 }
 
 export default App;
