229e7ac1acf2baa1f8fb9f945b6b0b59fca2ead8
yheseul <kangyheseul@naver.com>
2024-11-03 21:44:11 +0900

    fix: 당첨 번호 입력 안내 메시지에 개행 추가

--- src/constants/runMessages.js
+++ src/constants/runMessages.js
@@ -1,6 +1,6 @@
 export const RUN_MESSAGE = Object.freeze({
   PURCHASE_AMOUNT: "로또 구입 금액을 입력해 주세요.\n",
-  WINNING_NUMBERS: "당첨 번호를 입력해 주세요.\n",
+  WINNING_NUMBERS: "\n당첨 번호를 입력해 주세요.\n",
   BONUS_NUMBER: "보너스 번호를 입력해 주세요.\n",
   PURCHASED_LOTTO_COUNT: (count) => `\n${count}개를 구매했습니다.`,
 });
