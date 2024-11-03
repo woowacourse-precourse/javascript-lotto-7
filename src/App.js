531838abfa8e20b4e4184383241931aed34c4b29
jllee000 <jllee000@naver.com>
2024-11-03 21:56:27 +0900

    feat : 로또 자동 랜덤 생성 로직

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App {
     const LOTTO_WIN_NUM = [1, 2, 3, 4, 5, 6];
     const lottoResult = new Lotto(LOTTO_WIN_NUM); // 변수 이름을 lottoResult로 변경
     // 비동기 함수 호출 후 결과를 출력
-    const lottoCount = await lottoResult.InputCost();
-    Console.print(`구입 가능한 로또 개수: ${lottoCount}`);
+    lottoResult.GenerateLottoResult();
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -18,6 +18,7 @@ class Lotto {
   async InputCost() {
     const USER_COST = await Console.readLineAsync("구입 금액을 입력해주세요: ");
     const LOTTO_CNT = Math.floor(Number(USER_COST) / 1000);
+    Console.print(`\n${LOTTO_CNT}개를 구매했습니다.`);
     return LOTTO_CNT;
   }
 
@@ -27,7 +28,22 @@ class Lotto {
     );
     return Number(BONUS_NUM);
   }
-  GenerateLottoResult() {}
+
+  async GenerateLottoResult() {
+    const LOTTO_CNT = await this.InputCost(); // 구매 개수 가져오기
+    const USER_LOTTO_LIST = [];
+
+    for (let i = 0; i < LOTTO_CNT; i++) {
+      const RANDOM_LOTTO = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+        (a, b) => a - b
+      );
+
+      // 유효성 검사 부분
+      const lotto = new Lotto(RANDOM_LOTTO);
+      USER_LOTTO_LIST.push(lotto.#numbers);
+      Console.print(lotto.#numbers);
+    }
+  }
 }
 
 export default Lotto;
