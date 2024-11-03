be379f50096ce270486a97a3153748b3b9c41bd9
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-04 01:39:49 +0900

    ✨ feat: 당첨 결과 출력 기능 구현

--- src/view/OuputHandler.js
+++ src/view/OuputHandler.js
@@ -7,8 +7,23 @@ class OutputHandler {
       MissionUtils.Console.print(`[${numbers.join(', ')}]`),
     );
   }
-
-  
+  printResult(rankCounts) {
+    MissionUtils.Console.print('당첨 통계');
+    MissionUtils.Console.print('---');
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${rankCounts['fifth']}개`);
+    MissionUtils.Console.print(
+      `4개 일치 (50,000원) - ${rankCounts['fourth']}개`,
+    );
+    MissionUtils.Console.print(
+      `5개 일치 (1,500,000원) - ${rankCounts['third']}개`,
+    );
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankCounts['second']}개`,
+    );
+    MissionUtils.Console.print(
+      `6개 일치 (2,000,000,000원) - ${rankCounts['first']}개`,
+    );
+  }
 }
 
 export default OutputHandler;
