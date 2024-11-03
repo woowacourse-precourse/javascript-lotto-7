e3dee580fa4b473b8014829212a97c90c858d9bc
osw6858 <osw7875@naver.com>
2024-11-03 21:04:17 +0900

    feat: 보너스 번호가 기존 우승번호와 중복되는지 검증하는 메서드 추가

--- src/Bonus.js
+++ src/Bonus.js
@@ -11,6 +11,7 @@ export class Bonus {
   #validate(bonus, lottoList) {
     this.#validBonusRange(bonus);
     this.#validBonusFormat(bonus);
+    this.#validBonusDuplication(bonus, lottoList);
   }
 
   #validBonusRange(bonus) {
@@ -23,6 +24,16 @@ export class Bonus {
   #validBonusFormat(bonus) {
     if (Number.isNaN(Number(bonus.trim()))) {
       throw new Error('보너스 번호는 숫자여야 합니다.');
+    }
+  }
+
+  #validBonusDuplication(bonus, lottoList) {
+    const bonusNumber = Number(bonus.trim());
+    const winningNumbers = lottoList
+      .split(',')
+      .map((num) => Number(num.trim()));
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error('보너스 번호는 당첨 번호와 중복될 수 없습니다.');
     }
   }
 }
