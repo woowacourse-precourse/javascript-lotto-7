dafcfe24f5b04a1f4af47c0f351b70a701b35fe0
oko_jin <dswvgw6412@naver.com>
2024-11-03 12:08:26 +0900

    test(amountInput): 구매 금액 입력 테스트 케이스 추가
    - 구매 금액이 숫자가 아닌 경우
    - 구매 금애깅 정수가 아닌 경우

--- src/constant/amountInputTestCase.js
+++ src/constant/amountInputTestCase.js
@@ -1,6 +1,6 @@
 import { errorMessage } from './errorMessage.js';
 
-export const amountInputTestCase = [
+export const amountInputErrorTestCase = [
   {
     amount: 0,
     description: '구매 금액이 0원이면',
@@ -15,5 +15,20 @@ export const amountInputTestCase = [
     amount: 5100,
     description: '구매 금액이 1,000원 단위가 아니라면',
     errorMessage: `${errorMessage.prefix} ${errorMessage.divisibleAmount}`,
+  },
+  {
+    amount: 'abc',
+    description: '구매 금액이 숫자가 아니라면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidAmount}`,
+  },
+  {
+    amount: 123.45,
+    description: '구매 금액이 정수가 아니라면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.notIntegerAmount}`,
+  },
+  {
+    amount: '12300a',
+    description: '구매 금액이 정수가 아니라면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.notIntegerAmount}`,
   },
 ];
