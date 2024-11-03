dbc769c8c30ce2fd1dc99ba5c16279fbab9fd625
MyungJiwoo <1206jiwoo@gmail.com>
2024-11-04 04:20:22 +0900

    refactor: 출력 메시지 및 패키지 구조 업데이트

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -4,7 +4,7 @@ import Lotto from "../models/Lotto.js";
 
 class OutputView {
     async outputLottoCount(lottoCount) {
-        return Console.print(`\n${lottoCount}${CONSOLE_MESSAGE.OUTPUT_LOTTO_COUNT}`);
+        return Console.print(`${lottoCount}${CONSOLE_MESSAGE.OUTPUT_LOTTO_COUNT}`);
     }
 
     async outputLotto(lotto) {
