9c89bde4a7d5edbf88583dccb3ca9cf76002584d
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-04 01:14:28 +0900

    style: 당첨번호 에러 메시지 수정 및 추가

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,12 +1,14 @@
 const ERROR_TAG = "[ERROR]";
 const ERROR_MESSAGE = Object.freeze({
-  EMPTY_INPUT: `${ERROR_TAG} 입력된 금액이 없습니다. 구입 금액을 다시 입력해 주세요.`,
+  EMPTY_INPUT: `${ERROR_TAG} 입력된 값이 없습니다. 값을 다시 입력해 주세요.`,
   PURCHASE_INVALID: `${ERROR_TAG} 유효하지 않은 값입니다. 숫자를 입력해 주세요.`,
   PURCHASE_POSITIVE: `${ERROR_TAG} 구입 금액은 양수여야 합니다.`,
   PURCHASE_MULTIPLE_OF_THOUSAND: `${ERROR_TAG} 구입 금액은 1000원 단위로만 입력해 주세요.`,
 
+  WINNING_PURCHASE_INVALID: `${ERROR_TAG} 당첨 번호는 숫자로 구성되어야 합니다.`,
   WINNING_NUMBER_COUNT: `${ERROR_TAG} 당첨 번호는 총 6개로 이루어져야 합니다.`,
-  WINNING_NUMBER_FORMAT: `${ERROR_TAG} 당첨 번호는 쉼표로 구분된 1-45 사이의 숫자로 입력해야 합니다.`,
+  WINNING_NUMBER_FORMAT: `${ERROR_TAG} 당첨 번호는 1-45 사이의 숫자로 입력해야 합니다.`,
+  WINNING_NUMBER_DUPLICATE: `${ERROR_TAG} 당첨 번호는 서로 중복되지 않아야 합니다.`,
 
   BONUS_NUMBER_FORMAT: `${ERROR_TAG} 보너스 번호는 1-45 사이의 숫자로 입력해야 합니다.`,
   BONUS_NOT_IN_WINNING_NUMBERS: `${ERROR_TAG} 보너스 번호는 당첨 번호에 포함되어 있지 않은 수여야 합니다.`,
