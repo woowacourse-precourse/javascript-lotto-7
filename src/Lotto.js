227a6ee51eaed904da05bf5a5ed9efe3d4c633e6
Teayeon-Lim <wingofyeon@naver.com>
2024-11-03 21:59:25 +0900

    refactor(lotto): Separate validate functions
    seprated function list:
        - #checkIterator(numbers)
        - #checkNumberCount(numbers)
        - #checkNumberOrComma(numbers)
        - #checkInRangeNumbers(numbers)
        - #checkDifferentNumbers(numbers)

--- src/Lotto.js
+++ src/Lotto.js
@@ -18,8 +18,9 @@ class Lotto {
     while (true) {
       try {
         const numbers = await this.#getNumbers();
+        const parsing = numbers.split(",").map(Number);
 
-        return new Lotto(numbers.split(",").map(Number));
+        return new Lotto(parsing);
       } catch ({ message }) {
         LottoIO.print(message || "알 수 없는 에러");
       }
@@ -35,31 +36,46 @@ class Lotto {
   }
 
   static #validate(numbers) {
+    this.#checkIterator(numbers);
+    this.#checkNumberCount(numbers);
+    this.#checkNumberOrComma(numbers);
+    this.#checkInRangeNumbers(numbers);
+    this.#checkDifferentNumbers(numbers);
+  }
+
+  static #checkIterator(numbers) {
     if (typeof numbers !== "object") {
       LottoIO.throwError(
         "당첨 번호는 콤마(,)를 기준으로 숫자를 입력해 주세요. (ex: 1,2,3)"
       );
     }
+  }
 
+  static #checkNumberCount(numbers) {
     if (numbers.length !== NUMBER_COUNT_BY_LOTTO) {
       LottoIO.throwError(
         `${NUMBER_COUNT_BY_LOTTO}개의 당첨 번호를 입력해 주세요.`
       );
     }
-
+  }
+  static #checkNumberOrComma(numbers) {
     if (numbers.some((n) => !isNumber(n))) {
       LottoIO.throwError("숫자와 콤마(,)만 입력해 주세요.");
     }
+  }
 
-    const isIncludeOutRangeNumber = numbers.some((n) =>
-      isOutRangeNumber(n, LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX)
-    );
-    if (isIncludeOutRangeNumber) {
+  static #checkInRangeNumbers(numbers) {
+    const isInRangeLotto = (number) =>
+      isOutRangeNumber(number, LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX);
+
+    if (numbers.some(isInRangeLotto)) {
       LottoIO.throwError(
         `당첨 번호는 ${LOTTO_NUMBER_MIN} ~ ${LOTTO_NUMBER_MAX} 사이로 입력해 주세요.`
       );
     }
+  }
 
+  static #checkDifferentNumbers(numbers) {
     if (duplicateNumbers(numbers)) {
       LottoIO.throwError("모든 당첨 번호를 서로 다르게 입력해 주세요.");
     }
