7db3d9e5b509c139b95cf7217b416b7fdca9a66b
jiyeeeah <sije3757@snu.ac.kr>
2024-11-03 19:02:03 +0900

    test: 보너스 번호 유효성 검사 테스트케이스 추가

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -3,12 +3,32 @@ import BonusNumber from "../src/BonusNumber.js";
 import Lotto from "../src/Lotto.js";
 
 describe("보너스 번호 클래스 테스트", () => {
-  test("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.", () => {
-    const winningLotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const bonusNumber = 2;
+  let winningLotto;
+  beforeEach(() => {
+    winningLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  });
 
+  test("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.", () => {
     expect(() => {
-      new BonusNumber(winningLotto, bonusNumber);
+      new BonusNumber(winningLotto, 2);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 숫자가 아닌 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new BonusNumber(winningLotto, ",");
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 정수가 아닌 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new BonusNumber(winningLotto, 10.3);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 숫자가 범위 밖에 있는 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new BonusNumber(winningLotto, 70);
     }).toThrow("[ERROR]");
   });
 });
