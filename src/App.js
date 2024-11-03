2f1d875b7eba7ddfac28d0dda674d74896a53277
SNXWXH <snxwxh@gmail.com>
2024-11-03 23:08:47 +0900

    feat(app): 로또 구입 금액 입력 받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+import LottoPurchase from './models/LottoPurchase.js';
+import LottoGenerator from './utils/LottoGenerator.js';
+
 class App {
-  async run() {}
+  async run() {
+    const PRICE = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    const PURCHASE = new LottoPurchase(PRICE.trim());
+  }
 }
 
 export default App;
