01fd21fe7a6810ab810bab7976d1b757b8b0b61b
kimyou1102 <yoosk5485@naver.com>
2024-11-03 17:43:14 +0900

    refactor: 변경된 클래스에 맞게 테스트 파일명 및 테스트 코드 수정
    - 클래스명 변경에 따른 테스트 파일명 업데이트
    - matchLotto 메서드 호출 제거 등 관련 테스트 코드 리팩터링

--- __tests__/LottoResultTest.js
+++ __tests__/LottoResultTest.js
@@ -1,4 +1,4 @@
-import MatchingMachine from '../src/model/MatchingMachine';
+import LottoResult from '../src/model/LottoResult';
 
 describe('매칭 머신 클래스 테스트', () => {
   test('케이스 1 - 로또 번호가 당첨 번호와 매칭되었을 때 각 등수에 해당하는 당첨 결과를 반환한다.', () => {
@@ -17,14 +17,9 @@ describe('매칭 머신 클래스 테스트', () => {
       1: 0,
     };
 
-    const matchingMachine = new MatchingMachine(
-      winningNumber,
-      bonusNumber,
-      lottos,
-    );
-    matchingMachine.matchLotto();
+    const lottoResult = new LottoResult({ winningNumber, bonusNumber }, lottos);
 
-    expect(matchingMachine.getResult()).toEqual(expectedResult);
+    expect(lottoResult.getResult()).toEqual(expectedResult);
   });
 
   test('케이스 2 - 보너스 번호가 포함된 경우 당첨 결과를 반환한다.', () => {
@@ -43,13 +38,8 @@ describe('매칭 머신 클래스 테스트', () => {
       1: 0,
     };
 
-    const matchingMachine = new MatchingMachine(
-      winningNumber,
-      bonusNumber,
-      lottos,
-    );
-    matchingMachine.matchLotto();
+    const lottoResult = new LottoResult({ winningNumber, bonusNumber }, lottos);
 
-    expect(matchingMachine.getResult()).toEqual(expectedResult);
+    expect(lottoResult.getResult()).toEqual(expectedResult);
   });
 });
