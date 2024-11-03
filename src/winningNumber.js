dd322498a06519ff801b7a1d52fdd1f94335da01
전성모 <jeonsm22@daum.net>
2024-11-04 00:02:59 +0900

    feat: 당첨 번호 입력 함수 추가

--- src/winningNumber.js
+++ src/winningNumber.js
@@ -1,1 +1,7 @@
 // 담청 번호, 보너스 번호와 관련 있는 함수들
+
+import { Console } from "@woowacourse/mission-utils";
+
+async function inputWinningNumbers(){
+    const WINNING_NUMBERS = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+}
