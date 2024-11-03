f24f9c7eec8325d277ca86423c647ffba4f78448
Minje Sung <minje813@dgu.ac.kr>
2024-11-03 18:57:45 +0900

    fix(Constants): PrizeDescription 객체 리터럴 상수 freeze 추가

--- src/Constants/prizeConfig.js
+++ src/Constants/prizeConfig.js
@@ -7,10 +7,10 @@ export const PrizeMoney = Object.freeze({
   MISS: 0,
 });
 
-export const PrizeDescription = {
+export const PrizeDescription = Object.freeze({
   FIRST: '6개 일치 (2,000,000,000원)',
   SECOND: '5개 일치, 보너스 볼 일치 (30,000,000원)',
   THIRD: '5개 일치 (1,500,000원)',
   FOURTH: '4개 일치 (50,000원)',
   FIFTH: '3개 일치 (5,000원)',
-};
+});
