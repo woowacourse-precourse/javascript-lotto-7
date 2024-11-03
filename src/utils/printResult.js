7fc62b61a177bd08ad974b815d4579955d03c782
oko_jin <dswvgw6412@naver.com>
2024-11-03 17:11:00 +0900

    refactor(printResult): 등수를 출력하는 함수 분리

--- src/utils/printResult.js
+++ src/utils/printResult.js
@@ -1,31 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
+import { prizeRanks } from '../constant/prizeRanks';
 
 export function printResult(lottos, winningNumbers, bonusNumber) {
-  const result = lottos.reduce(
-    (acc, lotto) => {
-      const matchCount = getMatchCount(lotto, winningNumbers);
-      const isHasBonusNumber = hasBonusNumber(lotto, bonusNumber);
-      const rank = getRank(matchCount, isHasBonusNumber);
+  const result = lottos.reduce((acc, lotto) => {
+    const matchCount = getMatchCount(lotto, winningNumbers);
+    const isHasBonusNumber = hasBonusNumber(lotto, bonusNumber);
+    const rank = getRank(matchCount, isHasBonusNumber);
 
-      acc[rank] += 1;
-      return acc;
-    },
-    {
-      FIRST: 0,
-      SECOND: 0,
-      THIRD: 0,
-      FOURTH: 0,
-      FIFTH: 0,
-      MISS: 0,
-    }
-  );
+    acc[rank] += 1;
+    return acc;
+  }, prizeRanks);
 
-  Console.print(`3개 일치 (5,000원) - ${result.FIFTH}개`);
-  Console.print(`4개 일치 (50,000원) - ${result.FOURTH}개`);
-  Console.print(`5개 일치 (1,500,000원) - ${result.THIRD}개`);
-  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result.SECOND}개`);
-  Console.print(`6개 일치 (2,000,000,000원) - ${result.FIRST}개`);
-  Console.print(`총 수익률은 ${getTotalProfitRate(lottos, result)}%입니다.`);
+  printPrizeRank(result, lottos);
 }
 
 function getMatchCount(lotto, winningNumbers) {
@@ -64,4 +50,13 @@ function getTotalProfitRate(lottos, result) {
     result.FIFTH * 5000;
   const totalAmount = lottos.length * 1000;
   return ((totalWinningMoney / totalAmount) * 100).toFixed(1);
+}
+
+function printPrizeRank(result, lottos) {
+  Console.print(`3개 일치 (5,000원) - ${result.FIFTH}개`);
+  Console.print(`4개 일치 (50,000원) - ${result.FOURTH}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${result.THIRD}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result.SECOND}개`);
+  Console.print(`6개 일치 (2,000,000,000원) - ${result.FIRST}개`);
+  Console.print(`총 수익률은 ${getTotalProfitRate(lottos, result)}%입니다.`);
 }
