a689f6546cf44b08f6a25d28f859df68157a6281
mywhzy <cmj2393a@naver.com>
2024-11-04 04:16:43 +0900

    feat(App): 로또 구입 금액 입력 받기 및 예외 발생(readPurchaseAmount()) 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import InputView from './InputView.js';
+import LottoMachine from './LottoMachine.js';
+import OutputView from './OutputView.js';
+import { INPUT_PROMPT } from './constants.js';
+
 class App {
-  async run() {}
+  #lottoMachine;
+
+  async run() {
+    await this.readPurchaseAmount();
+  }
+
+  async readPurchaseAmount() {
+    while (!this.#lottoMachine) {
+      try {
+        const purchaseAmount = await InputView.readUserInput(INPUT_PROMPT.purchaseAmount);
+        OutputView.printEmptyLine();
+        this.#lottoMachine = new LottoMachine(purchaseAmount);
+      } catch (error) {
+        OutputView.printErrorMessage(error.message);
+      }
+    }
+  }
 }
 
 export default App;
