d5f7c0bbdcd672dd80a9ad01178322fd478de52e
jennaaaaaaaaa <ggm4954@gmail.com>
2024-11-03 12:01:36 +0900

    feat: 로또 구매 기능 및 로또 번호 출력 로또 구매 금액, 로또 생성 기능, 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from '@woowacourse/mission-utils';
+import { Input } from './inputHandler.js';
+import LotteryTickets from './lotteryTicket.js';
+// import Lotto from './Lotto.js';
+// import { Validator } from './validate.js';
+
 class App {
-  async run() {}
+   async run() {
+      const purchaseAmount = await Input.getPurchaseAmount(); //로또 장 수 lotteryTickets를 위한 구매 금액
+      const lotteryTickets = new LotteryTickets(purchaseAmount);
+      const tickets = lotteryTickets.getTickets(); //인스턴스를 통해 메서드 호출
+      Console.print(`${tickets.length}개를 구매했습니다.`);
+      tickets.forEach((ticket) => Console.print(ticket));
+
+      const winningNumbers = await Input.getWinningNumbers();
+      const bonusNumber = await Input.getBonusNumber();
+
+      console.log('app.js에서의 winningNumbers ', winningNumbers);
+      console.log('app.js에서의 bonusNumber ', bonusNumber);
+      console.log('app.js에서의 purchaseAmount', purchaseAmount);
+      console.log('app.js에서의 lotteryTickets', lotteryTickets);
+      console.log('app.js에서의 tickets', tickets);
+   }
 }
 
 export default App;
