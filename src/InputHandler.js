58da33e107f4aab7238d5ab6eaa0d6b5345d1910
junly21 <jaejoon0721@ajou.ac.kr>
2024-11-03 17:09:18 +0900

    feat: 구입 금액 validation 추가
    구입단위, 구입액이 숫자인지, 구입액이 자연수인지 확인

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -1,22 +1,47 @@
 import { Console } from '@woowacourse/mission-utils';
-import { INPUT_MESSAGE } from './lib/constant.js';
+import { INPUT_MESSAGE, ERROR_MESSAGE } from './lib/constant.js';
 
 class InputHandler {
-  async getBuyPrice() {
+  static async getBuyPrice() {
     const buyPrice = await Console.readLineAsync(INPUT_MESSAGE.BUY_PRICE);
+    this.#validateBuyPrice(buyPrice);
     return buyPrice;
   }
 
-  async getWinningNumbers() {
+  static async getWinningNumbers() {
     const winningNumbers = await Console.readLineAsync(
       INPUT_MESSAGE.WINNING_NUMBERS,
     );
     return winningNumbers;
   }
 
-  async getBonusNumber() {
+  static async getBonusNumber() {
     const bonusNumber = await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
     return bonusNumber;
+  }
+
+  static #validateBuyPrice(buyPrice) {
+    this.#validateBuyPriceUnit(buyPrice);
+    this.#validateBuyPriceType(buyPrice);
+    this.#validatePositiveNumber(buyPrice);
+  }
+
+  static #validateBuyPriceUnit(buyPrice) {
+    if (buyPrice % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGE.BUY_PRICE_UNIT);
+    }
+  }
+
+  static #validateBuyPriceType(buyPrice) {
+    if (Number.isNaN(buyPrice)) {
+      throw new Error(ERROR_MESSAGE.BUY_PRICE_TYPE);
+    }
+  }
+
+  static #validatePositiveNumber(number) {
+    if (number <= 0) {
+      throw new Error(ERROR_MESSAGE.BUY_PRICE_UNIT);
+    }
   }
 }
 
