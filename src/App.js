b9afa1b937ddb7fef9a56e497fe95d7db018e0f2
LeeJinyoung218 <amy021899@gmail.com>
2024-11-03 21:10:44 +0900

    feat: 구입 개수만큼 로또를 발행

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { MSG_PURCHASE_FEE } from "./constants.js";
-import { getInput } from "./handlers/IOhandler.js";
+import { getInput, printOutput } from "./handlers/IOhandler.js";
 import getFee from "./processors/feeProcessor.js";
+import getLottos from "./processors/lottoGenerator.js";
 
 class App {
   async run() {
@@ -8,6 +9,10 @@ class App {
       const feeInput = await getInput(MSG_PURCHASE_FEE);
       const fee = getFee(feeInput);
       const amount = fee / 1000;
+
+      const lottos = getLottos(amount);
+      printOutput(`\n${amount}개를 구매했습니다.`)
+      printOutput(lottos.map(lotto => "[" + lotto.join(", ") + "]").join("\n"));
     } catch (err) {
       throw err;
     }
