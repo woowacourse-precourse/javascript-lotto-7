8414813adb143ef75f4e636384f7f7924ba12486
jeheecheon <jeheecheon@gmail.com>
2024-11-04 04:09:29 +0900

    test: Lotto 클래스 단위 테스트 추가
    - 로또 번호가 6개가 맞으면 로또 객체가 생성된다.
    - 로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.
    - 로또 번호의 개수가 6개 미만이면 예외가 발생한다.
    - 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.
    - 로또 번호에 숫자가 아닌 값이 있으면 예외가 발생한다.
    - 로또 번호에 1이상 45이하가 아닌 값이 있으면 예외가 발생한다.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,42 @@
 import Lotto from '../src/models/Lotto.js';
 
-describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
-  });
+describe('로도 클래스 정상 테스트', () => {
+  test('로또 번호가 6개가 맞으면 로또 객체가 생성된다.', () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(numbers);
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    expect(lotto.getNumbers()).toEqual(numbers);
   });
+});
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+describe('로또 클래스 에러 테스트', () => {
+  test.each([
+    {
+      description: '로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.',
+      numbers: [1, 2, 3, 4, 5, 6, 7],
+      expected: '[ERROR]'
+    },
+    {
+      description: '로또 번호의 개수가 6개 미만이면 예외가 발생한다.',
+      numbers: [1, 2, 3, 4, 5],
+      expected: '[ERROR]'
+    },
+    {
+      description: '로또 번호에 중복된 숫자가 있으면 예외가 발생한다.',
+      numbers: [1, 2, 3, 4, 5, 5],
+      expected: '[ERROR]'
+    },
+    {
+      description: '로또 번호에 숫자가 아닌 값이 있으면 예외가 발생한다.',
+      numbers: [1, 2, 3, 4, 5, Number.NaN],
+      expected: '[ERROR]'
+    },
+    {
+      description: '로또 번호에 1이상 45이하가 아닌 값이 있으면 예외가 발생한다.',
+      numbers: [1, 2, 3, 4, 5, 46],
+      expected: '[ERROR]'
+    }
+  ])('$description', ({ numbers, expected }) => {
+    expect(() => new Lotto(numbers)).toThrow(expected);
+  });
 });
