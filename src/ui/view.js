7ce7305058fbeeb1963371776f9093cb20883a2f
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 03:21:04 +0900

    feat: 출력 형식에 맞춰 통계 출력, 구매 로또 출력 기능 추가

--- src/ui/view.js
+++ src/ui/view.js
@@ -1,9 +1,20 @@
 import {Console} from "@woowacourse/mission-utils";
+import {OUTPUT_OBJ} from "../constants/objects.js";
+import {OUTPUT} from "../constants/message.js";
 
 export const input = async (message) => {
     return await Console.readLineAsync(message)
 }
 
-export const output = async (message) => {
-    return await Console.readLineAsync(message)
+export const purchaseOutput = (purchasedLottos) => {
+    const purchasesNum = purchasedLottos.length
+    Console.print(purchasesNum + OUTPUT.PURCHASE)
+    purchasedLottos.map((lotto) => Console.print("[" + lotto + "]"))
+}
+
+export const winningStatsOutput = (resultObj) => {
+    Console.print(`당첨통계\n---`)
+    Object.values(resultObj).map((elem, idx) => {
+        Console.print(OUTPUT_OBJ[idx].content + elem + "개")
+    })
 }
