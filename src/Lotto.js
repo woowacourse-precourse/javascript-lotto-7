25b4a0b36a96b3e6e8f4831809c2736253bc9258
hyeongdong <khd941018@gmail.com>
2024-11-03 13:48:30 +0900

    feat(Lotto): 로또번호 출력을 위해 로또번호를 리턴하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,11 +5,15 @@ class Lotto {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
-  // 왜 private으로 ?
+
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  getNumbers() {
+    return this.#numbers;
   }
 
   sortAscending() {
