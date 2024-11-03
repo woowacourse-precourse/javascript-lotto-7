3513e0f76ce69c496e394f52805303f9f3f0e262
jennaaaaaaaaa <ggm4954@gmail.com>
2024-11-03 18:54:08 +0900

    feat: 로또 결과 출력 기능 추가 몇등인지, 수익률 계산

---
+++ src/lotteryResult.js
@@ -0,0 +1,63 @@
+import { Console } from '@woowacourse/mission-utils';
+import { LOTTO_PRICE } from './constants.js';
+
+export default class LotteryResult {
+   static printResults(tickets, winningNumbers, bonusNumber, purchaseAmount) {
+      const resultCount = {
+         '3개 일치 (5,000원)': 0,
+         '4개 일치 (50,000원)': 0,
+         '5개 일치 (1,500,000원)': 0,
+         '5개 일치, 보너스 볼 일치 (30,000,000원)': 0,
+         '6개 일치 (2,000,000,000원)': 0,
+      };
+
+      tickets.forEach((ticket) => {
+         const matchCount = this.countMatches(ticket, winningNumbers);
+         if (matchCount === 6) {
+            resultCount['6개 일치 (2,000,000,000원)']++;
+         } else if (matchCount === 5 && ticket.includes(bonusNumber)) {
+            resultCount['5개 일치, 보너스 볼 일치 (30,000,000원)']++;
+         } else if (matchCount === 5) {
+            resultCount['5개 일치 (1,500,000원)']++;
+         } else if (matchCount === 4) {
+            resultCount['4개 일치 (50,000원)']++;
+         } else if (matchCount === 3) {
+            resultCount['3개 일치 (5,000원)']++;
+         }
+      });
+
+      //   Console.print(`${tickets.length}개를 구매했습니다.`);
+      //   tickets.forEach((ticket) => Console.print(`[${ticket.join(', ')}]`));
+      this.printResultSummary(resultCount, purchaseAmount);
+   }
+
+   static countMatches(ticketNumbers, winningNumbers) {
+      return ticketNumbers.filter((num) => winningNumbers.includes(num)).length;
+   }
+
+   static printResultSummary(resultCount, purchaseAmount) {
+      Console.print('당첨 통계\n---');
+
+      for (const key in resultCount) {
+         if (resultCount.hasOwnProperty(key)) {
+            Console.print(`${key} - ${resultCount[key]}개`);
+         }
+      }
+
+      const totalEarnings = this.calculateTotalEarnings(resultCount);
+
+      const profit = ((totalEarnings / purchaseAmount) * 100).toFixed(1);
+
+      Console.print(`총 수익률은 ${profit}%입니다.`);
+   }
+
+   static calculateTotalEarnings(resultCount) {
+      return (
+         resultCount['3개 일치 (5,000원)'] * 5000 +
+         resultCount['4개 일치 (50,000원)'] * 50000 +
+         resultCount['5개 일치 (1,500,000원)'] * 1500000 +
+         resultCount['5개 일치, 보너스 볼 일치 (30,000,000원)'] * 30000000 +
+         resultCount['6개 일치 (2,000,000,000원)'] * 2000000000
+      );
+   }
+}
