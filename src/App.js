39b8761f891c04ab228883bfcfd1a5b5f173f0ed
jeongeunLee <leeju1009@gmail.com>
2024-11-04 03:52:13 +0900

    feat: 요구사항에 맞게 데이터가 출력되도록 run 로직 작성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import { Console } from '@woowacourse/mission-utils';
-import inputView from './userInterface/InputView.js';
 import Lotto from './Lotto.js';
 import LottoMachine from './LottoMachine.js';
-import Utils from './utils/Utils.js';
 import WinningLottoManager from './WinningLottoManager.js';
 import Analyzer from './Analyzer.js';
 import outputView from './userInterface/OutputView.js';
@@ -10,18 +7,34 @@ import PrizeCalculator from './PrizeCalculator.js';
 
 class App {
   async run() {
-    const lottoMachine = await this.getLottoMachine();
+    const payment = await App.getUserPayment();
+    const lottoMachine = App.getLottoMachine(payment, Lotto);
+
     const lottos = lottoMachine.getLottos();
+    const amount = lottoMachine.getAmount();
+
+    outputView.printPurchaseAmount(amount);
     outputView.printLotto(lottos);
 
     const winningLottoNumbers = await App.getWinningLottoNumbers();
     const analyzer = App.getAnalyzer(lottos, winningLottoNumbers);
     const matchingTable = analyzer.getMatchingTable();
+    outputView.printStatisticsHeader();
+    outputView.printSeparatingMark();
     outputView.printStatistics(matchingTable);
 
-    const payment = lottoMachine.getPayment();
     const profit = App.estimateProfit(matchingTable, payment);
     outputView.printProfit(profit);
+  }
+
+  static async getUserPayment() {
+    const payment = await LottoMachine.getPayment();
+    return payment;
+  }
+
+  static getLottoMachine(payment, lotto) {
+    const lottoMachine = new LottoMachine(payment, lotto);
+    return lottoMachine;
   }
 
   static async getWinningLottoNumbers() {
