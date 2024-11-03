2b0ac1b4e5df90eb4432f62f20070b08a558c926
CJY <cjy3458@naver.com>
2024-11-04 05:39:55 +0900

    chore: 함수 단일 책임 원칙 준수를 위한 분리

--- src/utils/validateInput.js
+++ src/utils/validateInput.js
@@ -1,12 +1,19 @@
 import ERROR from '../constants/error.js';
 import CustomError from './CustomError.js';
 
-export const validatePurchaseAmount = (purchaseAmount) => {
+export const validateAmountType = (purchaseAmount) => {
   if (typeof purchaseAmount !== 'number' || Number.isNaN(purchaseAmount)) {
     throw new CustomError(ERROR.INVALID_NUMBER_TYPE);
   }
+};
 
+export const validateAmountUnit = (purchaseAmount) => {
   if (purchaseAmount % 1000 !== 0) {
     throw new CustomError(ERROR.INVALID_PURCHASE_AMOUNT);
   }
+};
+
+export const validatePurchaseAmount = (purchaseAmount) => {
+  validateAmountType(purchaseAmount);
+  validateAmountUnit(purchaseAmount);
 };
