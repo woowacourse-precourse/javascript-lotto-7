a3ebb7065b5fa59f350d598dff27cd2693bcb609
Kimyerim <yeye092302@gmail.com>
2024-11-04 03:21:33 +0900

    feat(Input): 당첨 통계 계산 및 출력 추가
    - 로또 당첨 결과 계산 및 출력 기능 추가
    - 구입 금액 대비 수익률 계산 및 출력 기능 추가
    - LottoResultCalculator 클래스를 사용하여 당첨 결과와 수익률 계산하고 출력 수행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import  Input  from './Input.js';
 import Lotto from './Lotto.js';
+import LottoResultCalculator from './LottoResultCalculator.js';
 import { validateBonusNum, validatePurchaseAmount, validateWinnigNum } from './validate.js';
 import { COMMON_MESSAGE } from './message.js';
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
@@ -8,9 +9,10 @@ class App {
   async run() {
     const inputPurchaseAmount = new Input(validatePurchaseAmount.validation);
     await inputPurchaseAmount.inputValue(COMMON_MESSAGE.INPUT_PURCHASE);
+    const purchaseAmount = inputPurchaseAmount.getValue();
     const lottoCount = inputPurchaseAmount.getLottoCount();
 
-    Console.print(`${lottoCount}개를 구매했습니다.`);
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
 
     const lottos = [];
     for(let i = 0; i < lottoCount; i++){
@@ -23,20 +25,25 @@ class App {
       Console.print(lotto.getNumbers());
     });
 
+    const inputWinningNums = new Input(validateWinnigNum.validation);
+    await inputWinningNums.inputValue(COMMON_MESSAGE.INPUT_WINNING_NUM);
+    inputWinningNums.changeArray();
+    const winnigNums = inputWinningNums.getValue();
 
-    const inputWinningNum = new Input(validateWinnigNum.validation);
-    await inputWinningNum.inputValue(COMMON_MESSAGE.INPUT_WINNING_NUM);
-    inputWinningNum.changeArray();
-    const winnigNum = inputWinningNum.getValue();
-
-    const inputBonusNum = new Input((bonusNum) => validateBonusNum.validation(bonusNum, winnigNum));
+    const inputBonusNum = new Input((bonusNum) => validateBonusNum.validation(bonusNum, winnigNums));
     await inputBonusNum.inputValue(COMMON_MESSAGE.INPUT_BONUS_NUM);
-    inputBonusNum.changeNum();
     const bonusNum = inputBonusNum.getValue();
+
+    Console.print(COMMON_MESSAGE.WINNING_STATS);
+    const lottoResultCalculator = new LottoResultCalculator();
+    lottoResultCalculator.calculateResults(winnigNums, bonusNum, lottos);
+    lottoResultCalculator.printLottoResult();
+    lottoResultCalculator.calculateRate(purchaseAmount);
+    lottoResultCalculator.printReturnOfRate();
   }
 
   generateLottoNumbers(){
-    return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a- b);
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
   }
 }
 
