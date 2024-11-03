bead62044434205d91dfa5b5360e8e9bc0b10316
leelsj2007 <leelsj2007@gmail.com>
2024-11-04 03:14:35 +0900

    feat : 출력에 관한 OutputView 클래스 작성

---
+++ src/view/OutputView.js
@@ -0,0 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+import { OUTPUT_MESSAGE } from "../constants/constants";
+
+
+class OutputView {
+  #print
+
+  constructor() {
+    this.#print = Console.print;
+  }
+
+  printMyLotto(purchaseNumber) {
+    this.#print(OUTPUT_MESSAGE.printPurchaseNumber(purchaseNumber));
+  }
+
+  printResult() {
+    this.#print(OUTPUT_MESSAGE.printWinningStatistics);
+    this.#print(OUTPUT_MESSAGE.printFifth(0));
+    this.#print(OUTPUT_MESSAGE.printFourth(0));
+    this.#print(OUTPUT_MESSAGE.printThird(0));
+    this.#print(OUTPUT_MESSAGE.printSecond(0));
+    this.#print(OUTPUT_MESSAGE.printFirst(0));
+    this.#print(OUTPUT_MESSAGE.printRateReturn(0));
+  }
+}
+
+export default OutputView;
