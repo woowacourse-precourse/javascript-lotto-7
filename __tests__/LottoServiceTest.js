de544fd6fb762600a915ccaf8ee8ce5bd3ec29b9
hyeongdong <khd941018@gmail.com>
2024-11-04 03:50:53 +0900

    refactor(LottoServiceTest): 클래스 변경에 따른 테스트 수정

--- __tests__/LottoServiceTest.js
+++ __tests__/LottoServiceTest.js
@@ -1,25 +1,36 @@
 import LottoMatchChecker from "../src/LottoMatchChecker";
-import Lotto from "../src/Lotto";
 import LottoService from "../src/LottoService";
-// 리팩토링 필요
-describe("LottoService 클래스 테스트", () => {
-  test("당첨 번호와 비교결과를 담은 배열을 얻을 수 있다..", () => {
-    const lottos = [new Lotto([1, 3, 5, 7, 9, 10]), new Lotto([1, 3, 4, 5, 7, 9])];
-    const lottoMatchChecker = new LottoMatchChecker(4, [1, 3, 5, 7, 9, 10]);
-    const lottoService = new LottoService(lottoMatchChecker, lottos);
+import WinningStatsManager from "../src/WinningStatsManager";
+import LottoIssuer from "../src/LottoIssuer";
+import PurchaseManager from "../src/PurchaseManager";
+import { PRIZE_MONEY_BY_RANK, LOTTO_PRICE } from "../src/constants";
 
-    expect(lottoService.aggregateLottoResults()).toEqual([
+describe("LottoService 클래스 테스트", () => {
+  const purchaseManager = new PurchaseManager(LOTTO_PRICE);
+  const lottoMatchChecker = new LottoMatchChecker();
+  const lottoIssuer = new LottoIssuer();
+  const winningStatsManager = new WinningStatsManager();
+  const lottoService = new LottoService(purchaseManager, lottoMatchChecker, lottoIssuer, winningStatsManager);
+
+  const bonusNumber = 4;
+  const winningNumbers = [1, 3, 5, 7, 9, 10];
+
+  test("구매가능한 로또의 개수를 구할 수 있다.", () => {
+    expect(lottoService.getMaxLottoCount(3000)).toBe(3);
+  });
+
+  test("당첨번호와 일치 개수를 담은 배열을 받을 수 있다.", () => {
+    lottoService.addLotto([1, 3, 5, 7, 9, 10]);
+    lottoService.addLotto([1, 3, 4, 5, 7, 9]);
+
+    expect(lottoService.aggregateLottoResults(bonusNumber, winningNumbers)).toEqual([
       [6, false],
       [5, true],
     ]);
   });
 
-  test("등수 별 최종 당첨 통계를 얻을 수 있다.", () => {
-    const lottos = [new Lotto([1, 3, 5, 7, 9, 10]), new Lotto([1, 3, 4, 5, 7, 9])];
-    const lottoMatchChecker = new LottoMatchChecker(4, [1, 3, 5, 7, 9, 10]);
-    const lottoService = new LottoService(lottoMatchChecker, lottos);
-
-    expect(lottoService.getWinningStats()).toEqual({
+  test("당첨 통계를 구할 수 있다.", () => {
+    expect(lottoService.getWinningStats(bonusNumber, winningNumbers)).toEqual({
       match_6: 1,
       match_5_bonus: 1,
       match_5: 0,
@@ -28,21 +39,7 @@ describe("LottoService 클래스 테스트", () => {
     });
   });
 
-  test("최종 수익을 구할 수 있다.", () => {
-    const lottos = [new Lotto([1, 3, 5, 7, 9, 10]), new Lotto([1, 3, 4, 5, 7, 9])];
-    const lottoMatchChecker = new LottoMatchChecker(4, [1, 3, 5, 7, 9, 10]);
-    const lottoService = new LottoService(lottoMatchChecker, lottos);
-    const winningStats = lottoService.getWinningStats();
-
-    expect(lottoService.calculateTotalProfit(winningStats)).toBe(2030000000);
-  });
-
-  test("최종 수익률을 구할 수 있다.", () => {
-    const lottos = [new Lotto([1, 3, 5, 7, 9, 10]), new Lotto([1, 3, 4, 5, 7, 9])];
-    const lottoMatchChecker = new LottoMatchChecker(4, [1, 3, 5, 7, 9, 10]);
-    const lottoService = new LottoService(lottoMatchChecker, lottos);
-    const winningStats = lottoService.getWinningStats();
-
-    expect(lottoService.calculateProfitMargin(1000, winningStats)).toBe("1015000.0");
+  test("수익률을 구할 수 있다.", () => {
+    expect(lottoService.getProfitMargin(LOTTO_PRICE, PRIZE_MONEY_BY_RANK)).toEqual("1015000.0");
   });
 });
