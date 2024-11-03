76ad9887306f7093cc30af96f7c74bc37e627b22
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-04 00:41:40 +0900

    refactor: static 메서드의 집한인 Validation클래스를  단순 객체로 변환

--- src/Contoller/Validation.js
+++ src/Contoller/Validation.js
@@ -1,25 +1,25 @@
 import { ERROR_MESSAGES } from '../constants/Message.js';
 
-class Validation {
-  static validateThousandUnit(amount) {
+const Validation = {
+  validateThousandUnit(amount) {
     if (amount % 1000 !== 0) {
       throw new Error(ERROR_MESSAGES.INVALID_THOUSAND_UNIT);
     }
-  }
+  },
 
-  static validateNotEmpty(input) {
+  validateNotEmpty(input) {
     if (input === '') {
       throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
     }
-  }
+  },
 
-  static validateIsNumber(input) {
+  validateIsNumber(input) {
     if (isNaN(input)) {
       throw new Error(ERROR_MESSAGES.NON_NUMERIC_INPUT);
     }
-  }
+  },
 
-  static validateCommaSeparatedFormat(numbers) {
+  validateCommaSeparatedFormat(numbers) {
     if (
       numbers.includes(',,') ||
       numbers.startsWith(',') ||
@@ -27,38 +27,38 @@ class Validation {
     ) {
       throw new Error(ERROR_MESSAGES.INVALID_COMMA_FORMAT);
     }
-  }
+  },
 
-  static validateSixNumbers(numbers) {
+  validateSixNumbers(numbers) {
     if (numbers.length !== 6) {
       throw new Error(ERROR_MESSAGES.INCORRECT_NUMBER_COUNT);
     }
-  }
+  },
 
-  static validateUniqueNumbers(numbers) {
+  validateUniqueNumbers(numbers) {
     const uniqueNumbers = new Set(numbers);
     if (uniqueNumbers.size !== numbers.length) {
       throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER);
     }
-  }
+  },
 
-  static validatePositiveInteger(number) {
+  validatePositiveInteger(number) {
     if (!(Number.isInteger(number) && number > 0)) {
       throw new Error(ERROR_MESSAGES.NON_POSITIVE_INTEGER);
     }
-  }
+  },
 
-  static validateNumberInRange(number) {
+  validateNumberInRange(number) {
     if (!(number >= 1 && number <= 45)) {
       throw new Error(ERROR_MESSAGES.NUMBER_OUT_OF_RANGE);
     }
-  }
+  },
 
-  static validateUniqueBonusNumber(numbers, bonusNumber) {
+  validateUniqueBonusNumber(numbers, bonusNumber) {
     if (numbers.includes(bonusNumber)) {
       throw new Error(ERROR_MESSAGES.DUPLICATE_BONUS_NUMBER);
     }
-  }
-}
+  },
+};
 
 export default Validation;
