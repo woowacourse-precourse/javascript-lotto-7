5935fe684a3ba96b7558251c82ba11cadb72517a
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-11-04 00:08:34 +0900

    feat: App 클래스에 로또실행 기능 추가
    - PurchaseAmountGenerator로 구입 금액 입력 및 유효성 검사
    - LottoTicketGenerator로 로또 티켓 리스트 생성
    - Lotto로 당첨 번호 입력받기
    - BonusNumber로 보너스 번호 생성 및 검증
    - LottoResults를 통해 당첨 내역 확인 및 수익률 출력
    - 에러 시 에러메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import PurchaseAmountGenerator from './getInput/PurchaseAmountGenerator.js';
+import BonusNumber from './getInput/BonusNumber.js';
+import LottoTicketGenerator from './output/LottoTicketGenerator.js';
+import LottoResults from './output/LottoResults.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const money = await PurchaseAmountGenerator.getPurchaseAmount();
+      const LottoList = LottoTicketGenerator.generateLottoTickets(money);
+
+      const winnigNumber = await Lotto.createLottoWithInput();
+
+      const bonusNumberInstance =
+        await BonusNumber.createBonusNumber(winnigNumber);
+      const bonusNumber = bonusNumberInstance.getBonusNumber();
+
+      const winnigList = new LottoResults();
+      winnigList.checkWinningNumbers(
+        LottoList,
+        winnigNumber,
+        bonusNumber,
+        money,
+      );
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
 }
 
 export default App;
