323e08c5bd93215e517af2e1781d60dfdf3955d8
rwaeng <rwaeng0421@gmail.com>
2024-11-04 00:45:48 +0900

    feat(App): 에러 발생 시 해당 함수 다시 시작하는 기능 작성

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import Lotto from "./model/Lotto.js";
 import LottoRanker from "./model/LottoRanker.js";
 import LottoGenerator from "./model/LottoGenerator.js";
-import WinningStatistics from "./service/WinningStatistics.js";
-import View from "./view/View.js";
+import OutputView from "./view/OutputView.js";
+import InputView from "./view/InputView.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  async #restart(func) {
+    while (true) {
+      try {
+        return await func();
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
   async run() {
-    const payment = await View.getPayment();
+    const payment = await this.#restart(() => InputView.getPayment());
 
     // 로또 생성
     const lottoGenerator = new LottoGenerator(payment);
-    const lotto = lottoGenerator.buyLotto();
+    const amount = lottoGenerator.buyLotto();
+    OutputView.printLottoQuantity(amount);
+    const lotto = lottoGenerator.generateLotto();
+    OutputView.printLottoBundle(lotto);
 
     // 당첨번호 및 보너스 번호 입력
-    const winningNums = await View.getWinningNumbers();
+    const winningNums = await this.#restart(() => InputView.getWinningNumbers());
     const winningLotto = new Lotto(winningNums);
-    const bonusNum = await View.getBonusNumber();
+    const bonusNum = await this.#restart(() => InputView.getBonusNumber());
 
     // 로또 번호와 당첨 번호 비교
     const lottoRanker = new LottoRanker(winningLotto.getNumbers(), bonusNum);
     const winning = lottoRanker.countWinning(lotto);
+    const returnRate = lottoRanker.calculateReturnRate(payment);
 
     // 통계 출력
-    const statistics = new WinningStatistics(winning);
-    statistics.printStatistics();
-    statistics.printReturnRate(payment);
+    OutputView.printStatistics(winning);
+    OutputView.printReturnRate(returnRate);
   }
 }
 
