8f0fd76ab2f581a8900f0c87c194098e3a18f10e
전성모 <jeonsm22@daum.net>
2024-11-04 00:07:39 +0900

    feat: 당첨 번호가 양수인지 확인하는 함수 추가

--- src/winningNumber.js
+++ src/winningNumber.js
@@ -5,3 +5,11 @@ import { Console } from "@woowacourse/mission-utils";
 async function inputWinningNumbers(){
     const WINNING_NUMBERS = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
 }
+
+function isPositiveNumber(number){
+    if(!/^[0-9]+$/.test(number) || parseInt(number) === 0){
+        throw new Error("[ERROR] 양수인 당첨 번호를 입력하세요.");
+    }
+}
+
+export {isPositiveNumber}
