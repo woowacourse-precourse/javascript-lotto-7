52e72722393ac603c6e930bd1d237a7dbc2638ad
hyunyeee <5004ohy@naver.com>
2024-11-03 21:43:08 +0900

    refactor: 상수를 static 으로 변경

--- src/handler/ResultPrintHandler.js
+++ src/handler/ResultPrintHandler.js
@@ -1,20 +1,23 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 export class ResultPrintHandler {
-  message = `\n당첨 통계\n---\n`;
-  bonusMessage = `, 보너스 볼 일치`;
+  static RESULT_HEADER = "\n당첨 통계\n---\n";
+  static BONUS_MESSAGE = ", 보너스 볼 일치";
+  static REWARD_RATE_MESSAGE = "총 수익률은 ";
+
+  message = ResultPrintHandler.RESULT_HEADER;
 
   printResult(lottoResult, rewardRate) {
     this.message += this.#price(lottoResult);
-    this.message += `총 수익률은 ${rewardRate}%입니다.`;
-    MissionUtils.Console.print(this.message)
+    this.message += `${ResultPrintHandler.REWARD_RATE_MESSAGE}${rewardRate}%입니다.`;
+    MissionUtils.Console.print(this.message);
   }
 
   #price(lottoResult) {
     let message = '';
     Object.values(lottoResult).forEach((result) => {
       const resultObj = result.getResult();
-      const isBonus = (resultObj.match === 5 && resultObj.price === 30_000_000) ? this.bonusMessage : '';
+      const isBonus = (resultObj.match === 5 && resultObj.price === 30_000_000) ? ResultPrintHandler.BONUS_MESSAGE : '';
       message += `${resultObj.match}개 일치${isBonus} (${resultObj.price.toLocaleString()}원) - ${resultObj.count}개\n`
     })
     return message;
