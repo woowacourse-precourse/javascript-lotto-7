60d66e176f9bff3e19bc4edefc9f9f199e3d3e0e
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-11-04 02:31:41 +0900

    test: Lotto 클래스 유효성 검사 단위 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,7 +1,7 @@
 import Lotto from '../src/Lotto';
 import { ERROR_MESSAGE, RULE } from '../src/constants/index.js';
 
-describe('로또 번호 유효성 검사', () => {
+describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 아니면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -29,5 +29,17 @@ describe('로또 번호 유효성 검사', () => {
     expect(() => {
       new Lotto(['', 1, 2, 3, 4, 5]);
     }).toThrow(ERROR_MESSAGE.NO_BLANK);
+  });
+
+  test('로또 번호가 숫자가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto(['a', 1, 2, 3, 4, 5]);
+    }).toThrow(ERROR_MESSAGE.NOT_A_NUMBER);
+  });
+
+  test('정상적인 로또 번호로 인스턴스가 생성된다.', () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(numbers);
+    expect(lotto.numbers).toEqual(numbers);
   });
 });
