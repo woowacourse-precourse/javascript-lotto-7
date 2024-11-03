65135ec1d9c2e60a8a27efe11cca9817b9cf8b73
keemsebeen <sebin0580@gmail.com>
2024-11-04 00:41:22 +0900

    fix(constants) : 줄바꿈 문자 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,7 +1,7 @@
 export const INPUT = {
   PURCHASE_PRICE: "구입금액을 입력해 주세요.\n",
-  LOTTO_NUMBER: "당첨 번호를 입력해 주세요.\n",
-  BONUS: "보너스 번호를 입력해 주세요.\n",
+  LOTTO_NUMBER: "\n당첨 번호를 입력해 주세요.\n",
+  BONUS: "\n보너스 번호를 입력해 주세요.\n",
 };
 
 export const OUTPUT = {
