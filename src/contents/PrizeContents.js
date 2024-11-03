8c37ee2c216378a203e9904674ef50d37bceb86e
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-03 22:30:04 +0900

    ✨ feat: 당첨 금액 상수화 및 (당첨기준-등수)키 저장

---
+++ src/contents/PrizeContents.js
@@ -0,0 +1,26 @@
+export const PRIZE = Object.freeze({
+  first: 2000000000,
+  second: 30000000,
+  third: 1500000,
+  fourth: 50000,
+  fifth: 5000,
+});
+
+export const INITIAL_RANK = Object.freeze({
+  first: 0,
+  second: 0,
+  third: 0,
+  fourth: 0,
+  fifth: 0,
+});
+
+export const PRIZE_KEYS = Object.freeze({
+  '6-true': 'first',
+  '6-false': 'first',
+  '5-true': 'second',
+  '5-false': 'third',
+  '4-true': 'fourth',
+  '4-false': 'fourth',
+  '3-true': 'fifth',
+  '3-false': 'fifth',
+});
