467c9409b9112d68236e28084b0d58a553d0b04f
jennaaaaaaaaa <ggm4954@gmail.com>
2024-11-03 12:53:46 +0900

    fix: input 클래스 개선

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -10,8 +10,8 @@ export class Input {
 
    static async getWinningNumbers() {
       const winningNumbers = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
-      Validator.validateWinningNumber(winningNumbers.trim());
-      return winningNumbers;
+      const numbers = Validator.validateWinningNumber(winningNumbers.trim());
+      return numbers;
    }
 
    static async getBonusNumber() {
