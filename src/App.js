7b42d8a185e23914520ad5ad34673389aa20b891
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-04 02:05:00 +0900

    feat(app): 구입금액 입력 기능 및 lottoGame 인스턴스 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+import LottoGame from './LottoGame.js';
 class App {
-  async run() {}
+  async run() {
+    const LOTTO_PER_PRICE = 1000;
+    const orderPrice = await Console.readLineAsync(
+      `구입금액을 입력해 주세요.\n`
+    );
+
+    const lottoQuantity = orderPrice / LOTTO_PER_PRICE;
+
+    const lottoGame = new LottoGame(lottoQuantity);
+  }
 }
 
 export default App;
