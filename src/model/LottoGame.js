dcd74ffb8dca076de40173584ee2fc891109240c
ChanLee_KR <cl700120@gmail.com>
2024-11-03 23:37:21 +0900

    feat: 당첨 번호 길이 검증 기능 구현

--- src/model/LottoGame.js
+++ src/model/LottoGame.js
@@ -1,4 +1,5 @@
 import { Exception } from '../Utils.js';
+import { LOTTO_NUMBER_LENGTH } from '../Constants.js';
 import LottoValidator from './LottoValidator.js';
 
 class LottoGame {
@@ -8,17 +9,26 @@ class LottoGame {
   /** @param {number[]} winningNumbers */
   constructor(winningNumbers) {
     LottoGame.validateWinningNumbers(winningNumbers);
+    this.#winningNumbers = winningNumbers;
   }
 
   /** @param {number[]} numbers */
   static validateWinningNumbers(numbers) {
     LottoGame.#validateWinningNumbersType(numbers);
+    LottoGame.#validateWinningNumbersLength(numbers);
   }
 
   /** @param {number[]} numbers */
   static #validateWinningNumbersType(numbers) {
     if (!LottoValidator.isSafeNumberAll(numbers)) {
       throw new Exception('유효하지 않은 당첨 번호입니다.');
+    }
+  }
+
+  /** @param {number[]} numbers */
+  static #validateWinningNumbersLength(numbers) {
+    if (numbers.length !== LOTTO_NUMBER_LENGTH) {
+      throw new Exception(`당첨 번호는 ${LOTTO_NUMBER_LENGTH}개여야 합니다.`);
     }
   }
 }
