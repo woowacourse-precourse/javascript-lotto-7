d8296ad6fc23794df9842aca9f15e6b4736177c3
yheseul <kangyheseul@naver.com>
2024-11-03 16:12:13 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+import { RUN_MESSAGE } from "./constants/constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    const getPurchaseAmount = await Console.readLineAsync(
+      RUN_MESSAGE.PURCHASE_AMOUNT
+    );
+  }
 }
 
 export default App;
