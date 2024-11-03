5db0927b28fbaf9780698b6883a0e1c7f0a0e7ae
kochungwon <yud01063@gmail.com>
2024-11-03 19:42:09 +0900

    refactor:(UserInput) UserInput 클래스 리팩토링 및 유효성 검사 통합
    - InputValidator 클래스를 도입하여 로또 구입 금액, 당첨 번호, 보너스 번호의 유효성 검사를 수행하도록 수정
    - 구입 금액 입력 시 유효성 검사 추가
    - 당첨 번호 입력 시 불필요한 공백 제거 후 유효성 검사 추가
    - 보너스 번호 입력 시 유효성 검사 추가 및 중복 여부 확인

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,28 +1,33 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import InputValidator from './InputValidator.js'; 
 
 class UserInput {
     constructor() {
         this.purchaseAmount = 0;
         this.winningNumbers = [];
         this.bonusNumber = 0;
+        this.validator = new InputValidator();
     }
 
     // 로또 구입 금액 입력
     async inputPurchaseAmount() {
-        const input = await MissionUtils.Console.readLineAsync("구입 금액을 입력해 주세요.");
+        const input = await MissionUtils.Console.readLineAsync("구입 금액을 입력해 주세요.\n");
+        this.validator.validateAmount(input); 
         this.purchaseAmount = Number(input);
     }
 
     // 당첨 번호 입력
     async inputWinningNumbers() {
-        const input = await MissionUtils.Console.readLineAsync("당첨 번호를 입력해 주세요.");
-        this.winningNumbers = input.split(',').map(num => parseInt(num, 10));
+        const input = await MissionUtils.Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
+        this.winningNumbers = input.split(',').map(num => parseInt(num.trim(), 10));
+        this.validator.validateWinningNumbers(this.winningNumbers); 
     }
 
     // 보너스 번호 입력
     async inputBonusNumber() {
-        const input = await MissionUtils.Console.readLineAsync("보너스 번호를 입력해 주세요.");
-        this.bonusNumber = Number(input);
+        const input = await MissionUtils.Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
+        this.validator.validateBonusNumber(input, this.winningNumbers);
+        this.bonusNumber = Number(input); 
     }
 }
 
