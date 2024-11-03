e95e76fbbcb0fc8e7f8cff873382dd29307f459f
hyunyeee <5004ohy@naver.com>
2024-11-04 02:05:14 +0900

    test: 보너스 번호가 이미 당첨 번호에 포함된 경우 에러를 던진다

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,6 @@
 import Lotto from "../src/lotto/Lotto";
 import { inValidMessages } from "../src/constant/message.js";
+import { WinningLotto } from "../src/lotto/WinningLotto.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -18,5 +19,13 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([0, 1, 2, 3, 4, 40]);
     }).toThrow(inValidMessages.range);
+  });
+});
+
+describe("당첨 로또 클래스 테스트", () => {
+  test("보너스 번호가 이미 당첨 번호에 포함된 경우 에러를 던진다.", () => {
+    expect(() => {
+      new WinningLotto([1, 2, 3, 4, 5, 6], 6);
+    }).toThrow(inValidMessages.duplicateWithWinningNumbers);
   });
 });
