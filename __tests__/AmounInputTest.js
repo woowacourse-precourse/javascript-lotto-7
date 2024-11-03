82987cfe61c5d2615732f4136d2c312566ff0694
oko_jin <dswvgw6412@naver.com>
2024-11-03 12:50:43 +0900

    test(amountInputTest): 구매 금액 입력 성공 테스트 코드 구현

--- __tests__/AmounInputTest.js
+++ __tests__/AmounInputTest.js
@@ -1,6 +1,11 @@
-import { amountInputTestCase } from '../src/constant/amountInputTestCase.js';
+import {
+  amountInputErrorTestCase,
+  amountInputSuccessTestCase,
+} from '../src/constant/amountInputTestCase.js';
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { inputAmount } from '../src/utils/inputService.js';
+import { outputPayment } from '../src/utils/outputService.js';
 
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
@@ -18,8 +23,8 @@ const mockQuestions = (inputs) => {
   });
 };
 
-describe('구매 금액 입력 테스트', () => {
-  test.each(amountInputTestCase)(
+describe('구매 금액 입력 에러 테스트', () => {
+  test.each(amountInputErrorTestCase)(
     '$description 예외가 발생한다.',
     async ({ amount, errorMessage }) => {
       // given
@@ -35,6 +40,27 @@ describe('구매 금액 입력 테스트', () => {
 
       // then
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
+    }
+  );
+});
+
+describe('구매 금액 입력 성공 테스트', () => {
+  test.each(amountInputSuccessTestCase)(
+    '$description $expected를 반환한다.',
+    async ({ amount, expected }) => {
+      // given
+      const logSpy = getLogSpy();
+
+      const AMOUNT_INPUT = `${amount}`;
+
+      mockQuestions([AMOUNT_INPUT]);
+
+      // when
+      const input = await inputAmount();
+      outputPayment(input);
+
+      // then
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expected));
     }
   );
 });
