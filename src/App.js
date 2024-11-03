bf37e97d1668e9006db178f310668c222a8f4084
zzz664 <zzz664>
2024-11-04 02:06:05 +0900

    feat(App): 구현한 각각의 내용을 하나로 병합
    구현한 InputManagement, LottoPublication, VerifyingLotto, RevenueRate클래스를 이용하여,
    하나의 간단한 로또 발매기의 기능을 하도록 run메서드의 코드를 채워 넣음.
    사용자의 입력에 대해서 예외가 발생하는 부분은 try/catch구문을 이용하여
    어떤 에러가 발생했는지 알 수 있도록 ERROR의 message를 출력하도록 함.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputManagement from './InputManagement.js';
+import LottoPublication from './LottoPublication.js';
+import VerifyingLotto from './VerifyingLotto.js';
+import RevenueRate from './RevenueRate.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputManagement = new InputManagement();
+    const lottoPublication = new LottoPublication();
+    const verifyingLotto = new VerifyingLotto();
+    const revenueRate = new RevenueRate();
+
+    try {
+      await inputManagement.inputPurchaseAmount();
+
+      lottoPublication.publicationLotto(inputManagement.getPublicationCount());
+      lottoPublication.showPublishedLottoList();
+
+      await inputManagement.inputWinningNumbers();
+      await inputManagement.inputBonusNumber();
+    } catch (error) {
+      Console.print(error.message);
+    }
+
+    verifyingLotto.verifyWinningLottoList(
+      inputManagement.getWinningNumbers(),
+      inputManagement.getBonusNumber(),
+      lottoPublication.getPublishedLottoList()
+    );
+    verifyingLotto.printWinningHistory();
+
+    revenueRate.calculate(
+      inputManagement.getPurchaseAmount(),
+      verifyingLotto.getWinningHistory()
+    );
+    revenueRate.print();
+  }
 }
 
 export default App;
