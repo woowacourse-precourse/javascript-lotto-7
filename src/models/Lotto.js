be4ce7f084fce58a43323c441fe86c30051e38cd
eunseopi <asxzqw12@naver.com>
2024-11-03 19:21:29 +0900

    feat: Implement Lotto class with validation and matching logic
    - Create Lotto class to manage Lotto numbers and validate input.
    - Add validation to ensure 6 unique numbers within the defined range.
    - Implement match method to check matching numbers and bonus number.
    - Include getNumbers method to retrieve Lotto numbers.

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -1,18 +1,41 @@
+import {LOTTO_NUMBER_COUNT, MAX_NUMBER, MIN_NUMBER} from "../constants/gameConstants.js";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a,b) => a - b);
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
+    if (numbers.length !== LOTTO_NUMBER_COUNT) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    if (new Set(numbers).size !== LOTTO_NUMBER_COUNT) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.")
+    }
+    if (!numbers.every(num =>
+        Number.isInteger(num) &&
+        num >= MIN_NUMBER &&
+        num <= MAX_NUMBER
+    )) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 정수여야 합니다.')
     }
   }
 
-  // TODO: 추가 기능 구현
+  match(winnersNumbers, bonusNumber) {
+    const matchCount = this.#numbers.filter(num =>
+    winnersNumbers.includes(num)
+    ).length;
+
+    const matchBonus = this.#numbers.includes(bonusNumber);
+    return { matchCount, matchBonus };
+  }
+
+  getNumbers() {
+    return [...this.#numbers];
+  }
 }
 
 export default Lotto;
