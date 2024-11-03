f6ddfc134a6ff503de9773ed5c07755da1c30cc8
ChanLee_KR <cl700120@gmail.com>
2024-11-03 23:36:09 +0900

    feat: 당첨 번호 타입 검증 기능 구현

--- src/model/LottoGame.js
+++ src/model/LottoGame.js
@@ -1,3 +1,26 @@
-class LottoGame {}
+import { Exception } from '../Utils.js';
+import LottoValidator from './LottoValidator.js';
+
+class LottoGame {
+  /** @type {number[]} */
+  #winningNumbers;
+
+  /** @param {number[]} winningNumbers */
+  constructor(winningNumbers) {
+    LottoGame.validateWinningNumbers(winningNumbers);
+  }
+
+  /** @param {number[]} numbers */
+  static validateWinningNumbers(numbers) {
+    LottoGame.#validateWinningNumbersType(numbers);
+  }
+
+  /** @param {number[]} numbers */
+  static #validateWinningNumbersType(numbers) {
+    if (!LottoValidator.isSafeNumberAll(numbers)) {
+      throw new Exception('유효하지 않은 당첨 번호입니다.');
+    }
+  }
+}
 
 export default LottoGame;
