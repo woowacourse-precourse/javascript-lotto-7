299bda5527261cdc9209106e77c5ab6382bccec9
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-03 15:54:38 +0900

    feat(input): read amount input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGE } from './constants/message.js';
+
 class App {
-  async run() {}
+  async run() {
+    const amount = await this.userAmountInput();
+  }
+
+  async userAmountInput() {
+    const amountInput = await Console.readLineAsync(`${INPUT_MESSAGE.AMOUNT}\n`);
+  }
 }
 
 export default App;
