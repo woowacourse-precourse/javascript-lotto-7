8bf7281900f061ccea96716dc0de375a3c057872
hyunyeee <5004ohy@naver.com>
2024-11-04 01:53:32 +0900

    docs(readme): 구현할 기능 목록 업데이트

--- src/lotto/RewardCalculator.js
+++ src/lotto/RewardCalculator.js
@@ -1,8 +1,8 @@
-import { LOTTO_PRICE } from "../constant/constants.js";
+import { LOTTO_UNIT_PRICE } from "../constant/constants.js";
 
 export class RewardCalculator {
   calculateRewardRate(myLottoCount, result) {
-    const paidPrice = myLottoCount * LOTTO_PRICE;
+    const paidPrice = myLottoCount * LOTTO_UNIT_PRICE;
     let reward = 0;
     Object.values(result).forEach((r) => reward += (r.getResult().price * r.getResult().count))
     return (reward / paidPrice * 100).toFixed(1)
