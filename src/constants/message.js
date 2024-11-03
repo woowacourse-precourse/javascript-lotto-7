ccab8b10c7cdf8c564569480c1030352d123166c
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 03:24:01 +0900

    refactor: 입력 상수 배열에서 객체로 변경

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,10 +1,9 @@
 export const INPUT = {
-    PURCHASE: "구입금액을 입력해 주세요.",
-    WINNING_NUMBER: "당첨 번호를 입력해 주세요.",
-    BONUS_NUMBER: "보너스 번호를 입력해 주세요."
+    PURCHASE: "구입금액을 입력해 주세요.\n",
+    WINNING_STATS: "당첨 번호를 입력해 주세요.\n",
+    BONUS_NUM: "보너스 번호를 입력해 주세요.\n"
 }
 export const OUTPUT = {
     PURCHASE: "개를 구매했습니다.",
-    WINNING_STATISTICS: "",
     TOTAL_RETURN: "총 수익률은"
 }
