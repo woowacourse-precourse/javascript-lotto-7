76f349df0d2bc3a913241e61de05c0a247632c1c
silver0108 <kimsoeun0108@gmail.com>
2024-11-04 03:18:52 +0900

    test: Lotto 클래스 테스트 코드 내 문자열 상수 import

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,33 +1,34 @@
+import { ERROR_MESSAGES } from "../src/constants/messages";
 import Lotto from "../src/model/Lotto";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+    }).toThrow(`${ERROR_MESSAGES.HEADER}${ERROR_MESSAGES.INCORRECT_LENGTH}`);
   });
 
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }).toThrow(`${ERROR_MESSAGES.HEADER}${ERROR_MESSAGES.DUPLICATE_FOUND}`);
   });
 
   test("로또 번호가 없다면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto();
-    }).toThrow("[ERROR] 입력값이 없습니다.");
+    }).toThrow(`${ERROR_MESSAGES.HEADER}${ERROR_MESSAGES.EMPTY_INPUT}`);
   });
 
   test("로또 번호가 숫자 타입이 아니라면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto(["a", 2, 3, 4, 5, 6]);
-    }).toThrow("[ERROR] 입력값이 숫자 타입이 아닙니다.");
+    }).toThrow(`${ERROR_MESSAGES.HEADER}${ERROR_MESSAGES.NON_NUMERIC}`);
   });
 
   test("로또 번호가 1부터 45 사이의 숫자가 아니라면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([0, 2, 3, 4, 5, 6]);
-    }).toThrow("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }).toThrow(`${ERROR_MESSAGES.HEADER}${ERROR_MESSAGES.OUT_OF_RANGE}`);
   });
 });
