94bd0bfc0bda932a08bac9a9b811f615d144a519
BoRamSon <onbrson1@gmail.com>
2024-11-03 22:06:19 +0900

    fix(validInputPrice): errorMessages 관련하여 validate 코드 변경에 따른 코드 수정
    - errorMessage를 직접 사용할 때 지정할 수 있도록 수정하였습니다.

--- src/validator/validInputPrice.js
+++ src/validator/validInputPrice.js
@@ -3,13 +3,14 @@ import { isPositiveNumber } from "./function/validIsPositiveNum.js";
 import { limitMaxDigitNum } from "./function/validLimitMaxDigitNum.js";
 import { limitRangeNum } from "./function/validLimitRangeNum.js";
 import { kiloUnitNum } from "./function/validKiloUnitNum.js";
+import { ERR_MESSAGE_NUMBERS } from "../constants/errorMessages.js";
 
 export const validInputPrice = (inputPrice) => {
   try {
     emptyOrIsNum(inputPrice);
     isPositiveNumber(inputPrice);
-    limitMaxDigitNum(inputPrice, 6);
-    limitRangeNum(inputPrice, 1000, 100000);
+    limitMaxDigitNum(inputPrice, 6, ERR_MESSAGE_NUMBERS.LIMIT_LENGTH_PRICE_NUMBER);
+    limitRangeNum(inputPrice, 1000, 100000, ERR_MESSAGE_NUMBERS.LIMIT_RANGE_PRICE_NUMBER);
     kiloUnitNum(inputPrice);
     return true;
   } catch (error) {
