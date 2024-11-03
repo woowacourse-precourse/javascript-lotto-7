f44ff3d3026f30f52edb2914380a42d1f50fb4b0
Jinny <ggab.challenge@gmail.com>
2024-11-04 07:07:39 +0900

    style: 개행 추가

--- src/Model/rateOfReturnCalculator.js
+++ src/Model/rateOfReturnCalculator.js
@@ -7,11 +7,13 @@ const calculateWinningAmount = () => {
     matchCount.fiveMatch * 1500000 +
     matchCount.fiveAndBonusMatch * 30000000 +
     matchCount.sixMatch * 2000000000;
+
   return winningAmount;
 };
 
 export const calculateRateOfReturn = (purchasePrice) => {
   const winningAmount = calculateWinningAmount();
   const rateOfReturn = ((winningAmount / purchasePrice) * 100).toFixed(1);
+  
   return rateOfReturn;
 };
