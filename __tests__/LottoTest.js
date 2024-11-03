7b05e0ed9b40543a8065b5882b0a0cc1e723b8b1
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-04 01:24:43 +0900

    test: 로또 클래스 테스트 케이스 구현
    - 로또 번호 개수 6개 초과 여부 검증
    - 중복된 숫자포함 여부 검증
    - 빈 입력 값 검증
    - 숫자가 아닌 값 포함 검증
    - 로또 번호 범위 검증(1-45)

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,36 @@
 import Lotto from "../src/Lotto";
+import { ERROR_MESSAGE } from "../src/constants/errorMessage";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGE.WINNING_NUMBER_COUNT);
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGE.WINNING_NUMBER_DUPLICATE);
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호가 입력되지 않으면 예외가 발생한다.", () => {
+    expect(() => {
+      // 입력값이 없으면 빈 문자열이 number로 형변환되어 0이 되기 때문에
+      // [0]이 입력된 경우로 가정하여 예외 발생 처리
+      new Lotto([0]);
+    }).toThrow(ERROR_MESSAGE.EMPTY_INPUT);
+  });
+
+  test("로또 번호가 숫자가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, "three", 4, 5, 6]);
+    }).toThrow(ERROR_MESSAGE.WINNING_PURCHASE_INVALID);
+  });
+
+  test("로또 번호가 1과 45 사이가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 46, 5, 6]);
+    }).toThrow(ERROR_MESSAGE.WINNING_NUMBER_FORMAT);
+  });
 });
