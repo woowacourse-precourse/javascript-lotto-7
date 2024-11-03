ff5200b8529534ae66dfd3c47202f436deaafa47
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 03:24:26 +0900

    refactor: 입력 상수 배열에서 객체로 변경으로 인한 프로퍼티 호출로 변경

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import Lotto from "./services/Lotto.js";
 class App {
 
     async run() {
-        const purchaseNum = amountToNumber(await input(INPUT[0])) //입력금액 갯수로 변환 한걸 담은 변수
+        const purchaseNum = amountToNumber(await input(INPUT.PURCHASE)) //입력금액 갯수로 변환 한걸 담은 변수
 
         const lottoMachine = new LottoMachine(purchaseNum)
         const purchasedLottos = lottoMachine.lottoRelease()
-        console.log(purchasedLottos)
 
         purchaseOutput(purchasedLottos)
 
-        const winningNum = stringToNumberArray(await input(INPUT[1]))
+        const winningNum = stringToNumberArray(await input(INPUT.WINNING_STATS))
 
         const lotto = new Lotto(winningNum)
         const bonusNum = await input(INPUT[2]) //보너스 번호는 유효성 검사만 필요하다
