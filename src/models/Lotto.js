b8d4aed1653d671d6de802fedd5ea65b458fe9a7
suhwa <tnghk9611@naver.com>
2024-11-03 17:42:03 +0900

    feat(Lotto): lotto 클래스 생성 시 숫자가 아닌 요소가 포함되어 있는지 확인하는 예외 처리 구현
    - Validator의 공통 로직을 사용하는 방식으로 구현

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -1,15 +1,34 @@
+import { generateError } from "../utils/generateError";
+import Validator from "../validators/Validator";
+
 class Lotto {
   // 로또 하나를 저장하는 6개의 숫자 배열
   #numbers; // [1, 2, 3, 4, 5, 6]
 
   constructor(numbers) {
-    this.#validate(numbers); // 내부 유효성 검사
+    this.#validateNumberCount(numbers);
+    this.#validateUniqueNumber(numbers);
+    this.#validateContainNotNumber(numbers);
     this.#numbers = numbers.sort((a, b) => a - b); // 이상 없으면 저장
   }
 
-  #validate(numbers) {
+  #validateNumberCount(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      generateError("현재 발행된 로또는 6개가 아닙니다. 각 로또는 6개의 수로 구성되어야 합니다.");
+    }
+  }
+
+  #validateUniqueNumber(numbers) {
+    const set = new Set(...numbers);
+
+    if (numbers.length !== set.size) {
+      generateError("발행된 로또에 중복된 수가 포함되어 있습니다.");
+    }
+  }
+
+  #validateContainNotNumber(numbers) {
+    if (Validator.containNotNumber(numbers)) {
+      generateError("발행된 로또에 숫자가 아닌 요소가 포함되어 있습니다.");
     }
   }
 
