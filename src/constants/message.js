4f9604ab4bc168e8a1e719589d1c08f37327dc62
keemsebeen <sebin0580@gmail.com>
2024-11-03 22:58:48 +0900

    feat(constants) : 구매 금액, 당첨통계, section, delimiter 상수화

---
+++ src/constants/message.js
@@ -0,0 +1,10 @@
+export const INPUT = {
+  PURCHASE_PRICE: "구입금액을 입력해 주세요.\n",
+  LOTTO_NUMBER: "당첨 번호를 입력해 주세요.\n",
+  BONUS: "보너스 번호를 입력해 주세요.\n",
+};
+
+export const PRICE_NUMBER = 1000;
+export const RATE = "당첨 통계";
+export const SECTION = "---";
+export const DELIMITER = ",";
