0d1202477bd5efca96cef2849b8c5f57311765dd
Kim <kevin2695@naver.com>
2024-11-03 23:10:35 +0900

    feat(lotto) : validate 를 Lotto class 내부로 이동

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,61 +1,90 @@
 import { LOTTO_NUMBERS } from "./constants/lotto.js";
-import Validate from "./Validate.js";
+import { ERROR_MESSAGE } from "./constants/message.js";
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.#validateLottoNumbers(numbers);
     this.#numbers = numbers;
   }
 
+  #validateLottoNumbers(lottoArray) {
+    const isNotSixNumbers = lottoArray.size != LOTTO_NUMBERS.COUNT_6;
+
+    if (isNotSixNumbers) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_INPUT.NOT_SIX_NUMBERS);
+    }
+
+    const hasNaN = [...lottoArray].some(
+      (lottoNumber) => !Number.isInteger(lottoNumber)
+    );
+
+    if (hasNaN) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_INPUT.NOT_A_NUMBER);
+    }
+
+    const hasOutOfRange = [...lottoArray].some((lottoNumber) => {
+      const checkOutOfRange =
+        lottoNumber < LOTTO_NUMBERS.MIN_RANGE_1 ||
+        lottoNumber > LOTTO_NUMBERS.MAX_RANGE_45;
+
+      return checkOutOfRange
+    });
+
+    if (hasOutOfRange) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_INPUT.OUT_OF_RANGE_1_to_45);
+    }
+  }
+
   checkLottoNumbers(purchasedLotto, bonusNumber) {
-    const result = Array(5).fill(0)
+    const result = Array(5).fill(0);
 
     purchasedLotto.forEach((lotto) => {
-      let matchingNumberCount = 0
-      
-      lotto.forEach((number) => {
-        if(this.#numbers.has(number)){
-          matchingNumberCount += 1
-        }
-      })
+      let matchingNumberCount = 0;
 
-      // 1등 
-      if(matchingNumberCount === 6){
-        result[0] += 1
+      lotto.forEach((number) => {
+        if (this.#numbers.has(number)) {
+          matchingNumberCount += 1;
+        }
+      });
+
+      // 1등
+      if (matchingNumberCount === 6) {
+        result[0] += 1;
       }
 
       // 2등, 3등
-      if(matchingNumberCount === 5 && lotto.includes(bonusNumber)){
-        result[1] += 1
-      } else if(matchingNumberCount === 5) {
-        result[2] += 1
+      if (matchingNumberCount === 5 && lotto.includes(bonusNumber)) {
+        result[1] += 1;
+      } else if (matchingNumberCount === 5) {
+        result[2] += 1;
       }
 
       // 4등
-      if(matchingNumberCount === 4){
-        result[3] += 1
+      if (matchingNumberCount === 4) {
+        result[3] += 1;
       }
 
       // 5등
-      if(matchingNumberCount === 3){
-        result[4] += 1
+      if (matchingNumberCount === 3) {
+        result[4] += 1;
       }
     });
 
     return result;
   }
 
-  getProfitRate(lottoResult, purchaseAmount){
+  getProfitRate(lottoResult, purchaseAmount) {
     const prizeAmount = [2000000000, 30000000, 1500000, 50000, 5000];
-    
-    let profitSum = 0
 
-    for(let i = 0; i<5; i += 1){
-      profitSum += prizeAmount[i] * lottoResult[i]
+    let profitSum = 0;
+
+    for (let i = 0; i < 5; i += 1) {
+      profitSum += prizeAmount[i] * lottoResult[i];
     }
 
-    return Math.round(profitSum / purchaseAmount * 100 * 100) / 100
+    return Math.round((profitSum / purchaseAmount) * 100 * 100) / 100;
   }
 }
 
