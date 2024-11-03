3506ac93cabb307b9f7de9d39ed853a027f70f67
youdame <112458620+youdame@users.noreply.github.com>
2024-11-03 20:59:11 +0900

    fix : 에러 메세지를 출력하는 코드 추가 - 테스트 코드 실행 시 에러 메세지를 출력하는 요구 사항을 만족 시키지 않아 fail이 뜨는 문제 해결

--- src/App.js
+++ src/App.js
@@ -6,31 +6,37 @@ import WinningLotto from './winning-lotto.js';
 
 class App {
   async run() {
-    const purchaseAmount = await InputPrompt.getPurchaseAmount();
-    const isValid = /^[0-9]+$/.test(purchaseAmount);
-    if (!isValid) throw new Error('[ERROR]');
-    const lottoCount = parseInt(purchaseAmount, 10) / 1000;
-    // Console.print('\n');
+    try {
+      const purchaseAmount = await InputPrompt.getPurchaseAmount();
+      const isValid = /^[0-9]+$/.test(purchaseAmount);
+      if (!isValid) throw new Error('[ERROR]');
 
-    Console.print(`${lottoCount}개를 구매했습니다.`);
+      const lottoCount = parseInt(purchaseAmount, 10) / 1000;
+      // Console.print(`\n`);
+      Console.print(`${lottoCount}개를 구매했습니다.`);
 
-    const purchasedLottos = new PurchasedLottos();
-    await purchasedLottos.generateLottos(lottoCount);
+      const purchasedLottos = new PurchasedLottos();
+      await purchasedLottos.generateLottos(lottoCount);
+      // App 클래스에서 로또 번호를 출력할 때 문자열로 변환하여 출력
+      // purchasedLottos.getLottos().forEach((lotto) => {
+      //   Console.print(`[${lotto.getNumbers().join(', ')}]`);
+      // });
 
-    // Console.print('\n');
-    const winningNumberInput = await InputPrompt.getWinningNumber();
-    const winningNumbers = winningNumberInput
-      .trim()
-      .split(',')
-      .map((number) => parseInt(number, 10));
+      const winningNumberInput = await InputPrompt.getWinningNumber();
+      const winningNumbers = winningNumberInput
+        .trim()
+        .split(',')
+        .map((number) => parseInt(number, 10));
 
-    // Console.print('\n');
-    const bonusNumberInput = await InputPrompt.getBonusNumber();
-    const bonusNumber = parseInt(bonusNumberInput, 10);
+      const bonusNumberInput = await InputPrompt.getBonusNumber();
+      const bonusNumber = parseInt(bonusNumberInput, 10);
 
-    const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
-    const game = new Game(purchasedLottos, winningLotto);
-    game.play();
+      const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+      const game = new Game(purchasedLottos, winningLotto);
+      game.play();
+    } catch (error) {
+      Console.print(error.message); // [ERROR] 출력
+    }
   }
 }
 export default App;
