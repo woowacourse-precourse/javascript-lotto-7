b871c4c7ba628a5641f4636a1964f3e29dad3152
erica-jeong <wjddbwjd1205@gmail.com>
2024-11-04 02:56:26 +0900

    feat: 구매한 로또 번호와 당첨 번호, 보너스 번호 비교를 통한 당첨 등수 확인 기능 추가
    - 구매한 로또 번호와 당첨 번호를 비교하여 당첨 여부 확인
    - 일치하는 번호 개수와 보너스 번호 포함 여부에 따라 1등부터 5등까지 판별
    - #checkWinningRank 메서드로 각 로또 티켓의 당첨 등수를 계산하고 결과를 저장

--- src/LottoManager.js
+++ src/LottoManager.js
@@ -2,7 +2,8 @@ import { Random } from '@woowacourse/mission-utils';
 import InputHandler from "./InputHandler.js";
 import {
   LOTTO_PRICE, LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX,
-  LOTTO_NUMBER_COUNT
+  LOTTO_NUMBER_COUNT,
+  PRIZE_CONDITION
 } from "./lottoConstants.js";
 import OutputHandler from "./OutputHandler.js";
 import Lotto from './Lotto.js';
@@ -28,6 +29,7 @@ class LottoManager {
       this.#outputHandler.printLottoTickets(lottoTickets);
       const winningLotto = await this.#createWinningLotto();
       const bonusNumber = await this.#createBonusNumber(winningLotto);
+      const winningResult = this.#checkWinningRank(lottoTickets, winningLotto, bonusNumber);
     } catch (error) {
       throw error;
     }
@@ -84,6 +86,41 @@ class LottoManager {
         this.#outputHandler.printErrorMessage(error.message);
       }
     }
+  }
+
+  #checkWinningRank(lottoTickets, winningLotto, bonusNumber) {
+    const result = { first: 0, second: 0, third: 0, fourth: 0, fifth: 0 };
+
+    lottoTickets.forEach(lottoTicket => {
+      const matchCount = this.#countMatchNumbers(lottoTicket, winningLotto);
+      const isBonusMatch = lottoTicket.getNumbers().includes(bonusNumber);
+
+      switch (matchCount) {
+        case PRIZE_CONDITION.FIRST.MATCH_COUNT:
+          result.first += 1;
+          break;
+        case PRIZE_CONDITION.THIRD.MATCH_COUNT:
+          isBonusMatch ? result.second += 1 : result.third += 1;
+          break;
+        case PRIZE_CONDITION.FOURTH.MATCH_COUNT:
+          result.fourth += 1;
+          break;
+        case PRIZE_CONDITION.FIFTH.MATCH_COUNT:
+          result.fifth += 1;
+          break;
+        default:
+          break;
+      }
+    });
+
+    return result;
+  }
+
+  #countMatchNumbers(lottoTicket, winningLotto) {
+    const ticketNumbers = lottoTicket.getNumbers();
+    const winningNumbers = winningLotto.getNumbers();
+
+    return ticketNumbers.filter(number => winningNumbers.includes(number)).length;
   }
 }
 
