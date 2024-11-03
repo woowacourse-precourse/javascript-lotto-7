3c741acddd4d0d43830681296d4f418dd167f594
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-03 16:30:15 +0900

    feat: 에러 발생시 재입력할 수 있는 기능 추가

--- src/View/UserInput.js
+++ src/View/UserInput.js
@@ -1,21 +1,56 @@
 import { Console } from '@woowacourse/mission-utils';
 import { PROGRESS_MESSAGE } from '../constants/Message.js';
+import InputHandler from '../Contoller/InputHandler.js';
 
 class UserInput {
-  static async inputPurchaseAmount() {
-    return await Console.readLineAsync(
-      `${PROGRESS_MESSAGE.ENTER_PURCHASE_AMOUNT}\n`
-    );
+  inputHandler;
+
+  constructor() {
+    this.inputHandler = new InputHandler();
   }
-  static async inputWinningNumbers() {
-    return await Console.readLineAsync(
-      `${PROGRESS_MESSAGE.ENTER_WINNING_NUMBERS}\n`
-    );
+
+  async inputPurchaseAmount() {
+    try {
+      const input = await Console.readLineAsync(
+        `${PROGRESS_MESSAGE.ENTER_PURCHASE_AMOUNT}\n`
+      );
+      const purchaseAmount = this.inputHandler.getValidatedPurchseAmount(input);
+
+      return purchaseAmount;
+    } catch (error) {
+      Console.print(error.message + '\n');
+      return this.inputPurchaseAmount();
+    }
   }
-  static async inputBonusNumber() {
-    return await Console.readLineAsync(
-      `${PROGRESS_MESSAGE.ENTER_BONUS_NUMBER}\n`
-    );
+  async inputWinningNumbers() {
+    try {
+      const input = await Console.readLineAsync(
+        `\n${PROGRESS_MESSAGE.ENTER_WINNING_NUMBERS}\n`
+      );
+      const winningNumbers =
+        this.inputHandler.getValidatedWinningNumbers(input);
+
+      return winningNumbers;
+    } catch (error) {
+      Console.print(error.message + '\n');
+      return this.inputWinningNumbers();
+    }
+  }
+  async inputBonusNumber(winningNumbers) {
+    try {
+      const input = await Console.readLineAsync(
+        `\n${PROGRESS_MESSAGE.ENTER_BONUS_NUMBER}\n`
+      );
+      const bonusNumber = this.inputHandler.getValidatedBonusNumber(
+        input,
+        winningNumbers
+      );
+
+      return bonusNumber;
+    } catch (error) {
+      Console.print(error.message + '\n');
+      return this.inputBonusNumber(winningNumbers);
+    }
   }
 }
 
