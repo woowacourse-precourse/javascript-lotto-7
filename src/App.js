e01cf22de94dda6e2b6d5a195fceeae8e588aad1
jllee000 <jllee000@naver.com>
2024-11-03 22:33:55 +0900

    feat : 출력 형식에 따른 함수 분리 및 로직수정

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import Lotto from "./Lotto.js";
 
 class App {
   async run() {
-    const LOTTO_WIN_NUM = [1, 2, 3, 4, 5, 6];
-    const lottoResult = new Lotto(LOTTO_WIN_NUM); // 변수 이름을 lottoResult로 변경
-    // 비동기 함수 호출 후 결과를 출력
-    lottoResult.GenerateLottoResult();
+    await this.GenerateLottoResult();
+    const WIN_LOTTO_NUM = await this.InputWinLotto();
+    const lotto = new Lotto(WIN_LOTTO_NUM); // 변수 이름을 lottoResult로 변경
+    await lotto.InputBonus();
+  }
+  async InputCost() {
+    const USER_COST = await Console.readLineAsync("구입 금액을 입력해주세요: ");
+    const LOTTO_CNT = Math.floor(Number(USER_COST) / 1000);
+    Console.print(`\n${LOTTO_CNT}개를 구매했습니다.`);
+    return LOTTO_CNT;
+  }
+  async InputWinLotto() {
+    const INPUT_WIN_NUM = await Console.readLineAsync(
+      "당첨 번호를 입력해 주세요."
+    );
+    const INPUT_WIN_NUM_LIST = INPUT_WIN_NUM.split(",").map(Number);
+    return INPUT_WIN_NUM_LIST;
+  }
+
+  async GenerateLottoResult() {
+    const LOTTO_CNT = await this.InputCost(); // 구매 개수 가져오기
+    const USER_LOTTO_LIST = [];
+
+    for (let i = 0; i < LOTTO_CNT; i++) {
+      const RANDOM_LOTTO = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+        (a, b) => a - b
+      );
+
+      Console.print(RANDOM_LOTTO);
+    }
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -14,35 +14,11 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
-  // 인풋 구현
-  async InputCost() {
-    const USER_COST = await Console.readLineAsync("구입 금액을 입력해주세요: ");
-    const LOTTO_CNT = Math.floor(Number(USER_COST) / 1000);
-    Console.print(`\n${LOTTO_CNT}개를 구매했습니다.`);
-    return LOTTO_CNT;
-  }
-
   async InputBonus() {
     const BONUS_NUM = await Console.readLineAsync(
       "보너스 번호를 입력해 주세요: "
     );
     return Number(BONUS_NUM);
-  }
-
-  async GenerateLottoResult() {
-    const LOTTO_CNT = await this.InputCost(); // 구매 개수 가져오기
-    const USER_LOTTO_LIST = [];
-
-    for (let i = 0; i < LOTTO_CNT; i++) {
-      const RANDOM_LOTTO = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
-        (a, b) => a - b
-      );
-
-      // 유효성 검사 부분
-      const VALIDATE_LOTTO = new Lotto(RANDOM_LOTTO);
-      USER_LOTTO_LIST.push(VALIDATE_LOTTO.#numbers);
-      Console.print(VALIDATE_LOTTO.#numbers);
-    }
   }
 }
 
