9f6f669a5d0eeec3b1dbe10d40223253dff2cdac
oko_jin <dswvgw6412@naver.com>
2024-11-03 16:52:03 +0900

    test(lottoTest): 당첨 번호 입력 성공 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,7 @@
-import { lottoInputErrorTestCase, lottoErrorTestCase } from '../src/constant/lottoTestCase.js';
+import {
+  lottoInputErrorTestCase,
+  lottoInputSuccessTestCase,
+} from '../src/constant/lottoTestCase.js';
 import { validateNumbers } from '../src/Validator.js';
 
 describe('로또 입력 테스트', () => {
@@ -6,6 +9,13 @@ describe('로또 입력 테스트', () => {
     '$description 예외가 발생한다.',
     ({ numbers, errorMessage }) => {
       expect(() => validateNumbers(numbers)).toThrow(errorMessage);
+    }
+  );
+
+  test.each(lottoInputSuccessTestCase)(
+    '$description $expected를 반환한다.',
+    async ({ numbers, expected }) => {
+      expect(validateNumbers(numbers)).toEqual(expected);
     }
   );
 });
--- src/constant/LottoTestCase.js
+++ src/constant/LottoTestCase.js
@@ -36,4 +36,27 @@ export const lottoInputErrorTestCase = [
     description: '로또 번호에 1~45 범위를 벗어나는 숫자가 있으면',
     errorMessage: `${errorMessage.prefix} ${errorMessage.invalidNumberRange}`,
   },
+  {
+    numbers: '너무 유효하지 않은 입력값',
+    description: '유효한 입력값이 아니면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
+  },
+  {
+    numbers: '1,2,3,$4,%5,6',
+    description: '다른 문자가 포함되어 있으면',
+    errorMessage: `${errorMessage.prefix} ${errorMessage.invalidLotto}`,
+  },
+];
+
+export const lottoInputSuccessTestCase = [
+  {
+    numbers: '1,2,3,4,5,6',
+    description: '로또 번호가 1,2,3,4,5,6이면',
+    expected: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    numbers: '1,12,23,32,43,45',
+    description: '로또 번호가 1,12,23,32,43,45이면',
+    expected: [1, 12, 23, 32, 43, 45],
+  },
 ];
