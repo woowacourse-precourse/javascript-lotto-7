579a259b25755b191baf84562ec064274eb0a844
yheseul <kangyheseul@naver.com>
2024-11-04 00:10:39 +0900

    refactor: isNumber 함수 리팩토링하여 재사용성 개선
    - 다양한 상황에서 재사용이 가능하도록 변경

--- src/validation/validatePurchaseAmount/isNumber.js
+++
@@ -1,6 +0,0 @@
-import { LOTTO_PURCHASE_AMOUNT } from "../../constants/validationMessages/lottoPurchaseAmount.js";
-
-export const isNumber = function (purchaseAmount) {
-  const isValid = !isNaN(Number(purchaseAmount));
-  if (!isValid) throw new Error(LOTTO_PURCHASE_AMOUNT.NOT_A_NUMBER);
-};
