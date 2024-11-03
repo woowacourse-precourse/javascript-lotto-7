365803b16ff3a8c29f13c9f9d461852b080a8f25
oko_jin <dswvgw6412@naver.com>
2024-11-03 10:12:41 +0900

    chore(lottoTest): : 에러 출력 테스트를 '[ERROR]'가 아닌  errorMessage에 할당된 문자열을 출력하는지 확인하도록 변경

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,11 +1,11 @@
-import { LottoTestCase } from '../src/constant/LottoTestCase.js';
+import { lottoTestCase } from '../src/constant/lottoTestCase.js';
 import Lotto from '../src/Lotto';
 
 describe('로또 클래스 테스트', () => {
-  test.each(LottoTestCase)('$description 예외가 발생한다.', ({ numbers }) => {
+  test.each(lottoTestCase)('$description 예외가 발생한다.', ({ numbers, errorMessage }) => {
     expect(() => {
       new Lotto(numbers);
-    }).toThrow('[ERROR]');
+    }).toThrow(errorMessage);
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
