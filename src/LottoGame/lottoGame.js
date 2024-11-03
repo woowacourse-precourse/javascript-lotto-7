2b16150f2fc90561e08072b357b6eeedd94dd689
heeyoung123 <heeyoung321@naver.com>
2024-11-03 16:04:52 +0900

    feat: 당첨 번호 유효성 검증 기능 추가

--- src/LottoGame/lottoGame.js
+++ src/LottoGame/lottoGame.js
@@ -1,10 +1,13 @@
 import { readLineAsync } from '../Util/console.js';
 import { INPUT_MESSAGES } from '../Constant/inPutMessages.js';
 import { LOTTO_PRICE_VALIDATION } from '../Validator/lottoPriceValidation.js';
+import { WINNING_NUMBER_VALIDATION } from '../Validator/winningNumberValidation.js';
 class LottoGame {
   async start() {
     const LOTTO_PRICE = await readLineAsync(INPUT_MESSAGES.buyLottoPrice);
     LOTTO_PRICE_VALIDATION(LOTTO_PRICE);
+    const WINNING_NUMBER = await readLineAsync(INPUT_MESSAGES.winningNumber);
+    WINNING_NUMBER_VALIDATION(WINNING_NUMBER);
   }
 }
 export default LottoGame;
---
+++ src/Validator/winningNumberValidation.js
@@ -0,0 +1,36 @@
+import { SEPARATE_NUMBER } from '../Constant/regexp.js';
+import { ERROR_MESSAGES } from '../Constant/error.js';
+
+const SEPARATE_COMMA = (winningNumber) => {
+  return SEPARATE_NUMBER.test(winningNumber);
+};
+
+const WINNING_NUMBER_SIX = (winningNumberArray) => {
+  return winningNumberArray.length !== 6;
+};
+
+const WINNING_NUMBER_DUPLICATION = (winningNumberArray) => {
+  return winningNumberArray.some(
+    (item, index) => winningNumberArray.indexOf(item) !== index,
+  );
+};
+const WINNING_NUMBER_RANGE = (winningNumberArray) => {
+  return winningNumberArray.every((number) => number >= 1 && number <= 45);
+};
+
+export const WINNING_NUMBER_VALIDATION = (winningNumber) => {
+  const winningNumberArray = winningNumber.split(',').map(Number);
+  console.log('Converted winningNumberArray:', winningNumberArray);
+  if (!SEPARATE_COMMA(winningNumber)) {
+    throw new Error(ERROR_MESSAGES.IS_SEPARATE_COMMA);
+  }
+  if (WINNING_NUMBER_SIX(winningNumberArray)) {
+    throw new Error(ERROR_MESSAGES.IS_WINNING_NUMBER_SIX);
+  }
+  if (WINNING_NUMBER_DUPLICATION(winningNumberArray)) {
+    throw new Error(ERROR_MESSAGES.IS_WINNING_NUMBER_DUPLICATION);
+  }
+  if (!WINNING_NUMBER_RANGE(winningNumberArray)) {
+    throw new Error(ERROR_MESSAGES.IS_WINNING_NUMBER_RANGE);
+  }
+};
