b06dceb718a5bb2d355b6e70a72d9a5058e526a9
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-03 14:53:04 +0900

    feat(constants): add errors constants for lotto

---
+++ src/constants/errors.js
@@ -0,0 +1,12 @@
+export const ERRORS = Object.freeze({
+  INVALID_PURCHASE_PRICE_TYPE: '[ERROR] 로또 구매 금액은 숫자여야 합니다.',
+  INVALID_DUPLICATE_LOTTO_NUMBER: '[ERROR] 로또 번호에 중복된 숫자가 있습니다.',
+  INVALID_PURCHASE_PRICE: '[ERROR] 로또 구매 금액은 0원 이상이어야 합니다.',
+  INVALID_PURCHASE_PRICE_UNIT: '[ERROR] 로또 구매 금액은 1000원 단위로 입력해주세요.',
+  INVALID_NUMBER_LENGTH: '[ERROR] 당첨 및 보너스 번호는 6개여야 합니다.',
+  INVALID_NUMBER_TYPE: '[ERROR] 당첨 및 보너스 번호는 숫자(정수)여야 합니다.',
+  INVALID_NUMBER_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  INVALID_DUPLICATE_WINNING_NUMBER: '[ERROR] 당첨 번호에 중복된 숫자가 있습니다.',
+  INVALID_WINNING_NUMBER_LENGTH: '[ERROR] 당첨 번호는 6개여야 합니다.',
+  INVALID_DUPLICATE_BONUS_NUMBER: '[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.',
+});
