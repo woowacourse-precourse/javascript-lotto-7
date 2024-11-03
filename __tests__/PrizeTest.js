24578a73a3752e582c34bacda38c166b83f60c71
KimYoungWoo <rladuddn3924@gmail.com>
2024-11-03 17:12:25 +0900

    test: 테스트 항목 업데이트
    한 가지로 되어있는 테스트 항목을 each를 통하여 여러가지 값을 받도록 수정

--- __tests__/PrizeTest.js
+++ __tests__/PrizeTest.js
@@ -4,10 +4,15 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 describe('Prize 클래스 테스트', () => {
 
 
-  test("생성 테스트", () => {
-    const prize = new Prize(3, 5000, false);
+  test.each([
+    {matchingNumber: 3, price: 5000, isBonus: false, log: '3개 일치 (5,000원) - 0개'},
+    {matchingNumber: 4, price: 50000, isBonus: false, log: '4개 일치 (50,000원) - 0개'},
+    {matchingNumber: 5, price: 1500000, isBonus: false, log: '5개 일치 (1,500,000원) - 0개'},
+    {matchingNumber: 5, price: 30000000, isBonus: true, log: '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개'},
+    {matchingNumber: 6, price: 2000000000, isBonus: false, log: '6개 일치 (2,000,000,000원) - 0개'},
+  ])("생성 테스트 %o", ({matchingNumber, price, isBonus, log}) => {
+    const prize = new Prize(matchingNumber, price, isBonus);
 
-    const log = '3개 일치 (5,000원) - 0개';
     const logSpy = jest.spyOn(MissionUtils.Console, "print");
     logSpy.mockClear();
 
@@ -16,12 +21,16 @@ describe('Prize 클래스 테스트', () => {
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
   });
 
-  test("addCount 테스트", () => {
-    const prize = new Prize(3, 5000, false);
+  test.each([
+    {matchingNumber: 3, price: 5000, isBonus: false,},
+    {matchingNumber: 4, price: 50000, isBonus: false,},
+    {matchingNumber: 5, price: 30000000, isBonus: true,},
+  ])("addCount 테스트 %o", ({matchingNumber, price, isBonus, log}) => {
+    const prize = new Prize(matchingNumber, price, isBonus);
     expect(prize.getPrizeCount()).toBe(0);
 
     prize.addCount();
 
-    expect(prize.getPrizeCount()).toBe(5000);
+    expect(prize.getPrizeCount()).toBe(price);
   });
 });
