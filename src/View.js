3db25bffb680101684d806fb5d398936f4e5eb1b
Chaesunbak <coanstjd0422@gmail.com>
2024-11-04 03:53:03 +0900

    fix : 결과 출력 함수의 오타 수정

--- src/View.js
+++ src/View.js
@@ -32,7 +32,6 @@ class View {
 
   printLottos = (LOTTOS) => {
     const PURCHASED_LOTTO_COUNT = LOTTOS.length;
-    Console.print("");
     Console.print(`${PURCHASED_LOTTO_COUNT}개를 구매했습니다.`);
     for (const LOTTO of LOTTOS) {
       this.printLotto(LOTTO);
@@ -41,7 +40,8 @@ class View {
   };
 
   printLotto(LOTTO) {
-    Console.print(LOTTO.getNumbers());
+    const numbersString = `[${LOTTO.getNumbers().join(", ")}]`;
+    Console.print(numbersString);
   }
 
   getWinningNumbers = async () => {
@@ -136,12 +136,12 @@ class View {
   printSTATS = (RESULTS, PROFIT_RATE) => {
     Console.print("");
     Console.print("당첨 통계");
-    Console.print("---------");
-    Console.print(`3개 일치 (5,000원)- ${RESULTS[5]}개`);
-    Console.print(`4개 일치 (50,000원)- ${RESULTS[4]}개`);
-    Console.print(`5개 일치 (1,500,000원)- ${RESULTS[3]}개`);
-    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원)- ${RESULTS[2]}개`);
-    Console.print(`6개 일치 (2,000,000,000원)- ${RESULTS[1]}개`);
+    Console.print("---");
+    Console.print(`3개 일치 (5,000원) - ${RESULTS[5]}개`);
+    Console.print(`4개 일치 (50,000원) - ${RESULTS[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${RESULTS[3]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${RESULTS[2]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${RESULTS[1]}개`);
     Console.print(`총 수익률은 ${PROFIT_RATE}%입니다.`);
   };
 }
