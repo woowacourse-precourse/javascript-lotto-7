33cf728e546a6968f35a9a6159277ed35a60233f
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-03 22:41:31 +0900

    refactor : message 수정

--- src/constants/inputMessage.js
+++ src/constants/inputMessage.js
@@ -2,7 +2,7 @@ export const INPUT_MESSAGE = Object.freeze({
   purchaseAmountPrompt: '구입금액을 입력해 주세요.\n',
   purchaseMessage: (count) => `\n${count}개를 구매했습니다.`,
   winningNumbersPrompt: '\n당첨 번호를 입력해 주세요.\n',
-  bonusNumberPrompt: '\n보너스 번호를 입력해 주세요\n',
+  bonusNumberPrompt: '\n보너스 번호를 입력해 주세요.\n',
   winningTotalMessage: '\n당첨 통계\n---',
   match3Message: (count) => `3개 일치 (5,000원) - ${count}개`,
   match4Message: (count) => `4개 일치 (50,000원) - ${count}개`,
