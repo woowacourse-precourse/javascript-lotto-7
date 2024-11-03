a53078121f5dedee6f338eec2a0376a9099af8cd
jiyun1006 <jiyun4251@gmail.com>
2024-11-03 21:33:08 +0900

    ✨ feat : 메인 실행함수 App 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { createObject, outputMethod } from './utils/index.js';
+import { NUM, IOMESSAGE } from './constants/index.js';
+import Lotto from './Lotto.js';
+
 class App {
-  async run() {}
+  #purchaseAmount;
+  #lottoCount;
+
+  async run() {
+    await this.#inputPurchaseAmountAndLottoCount();
+
+    outputMethod(`\n${this.#lottoCount}${IOMESSAGE.OUTPUT_PURCHASE_AMOUNT}`);
+
+    const myLotto = createObject.createMyLotto(this.#lottoCount);
+    const winningNumber = new Lotto(await createObject.createWinningNumber());
+    const bonusNumber = await createObject.createBonusNumber();
+
+    outputMethod(`\n${IOMESSAGE.WINNING_RESULT}`);
+    outputMethod(`${IOMESSAGE.DIVIDER}`);
+
+    this.#printWinningResult(myLotto, bonusNumber, winningNumber);
+  }
+
+  async #inputPurchaseAmountAndLottoCount() {
+    this.#purchaseAmount = await createObject.createPurchaseAmount();
+    this.#lottoCount = this.#purchaseAmount / NUM.LOTTO_PRICE;
+  }
+
+  #printWinningCnt(winningResult) {
+    let idx = 0;
+    for (const value of Object.values(IOMESSAGE.WINNING_AMOUNT)) {
+      outputMethod(`${value}${winningResult[idx]}개`);
+      idx += 1;
+    }
+  }
+
+  #printReturn(winningReturn) {
+    outputMethod(
+      `${IOMESSAGE.RETURN_FRONT}${winningReturn}${IOMESSAGE.RETURN_BACK}`,
+    );
+  }
+
+  #printWinningResult(myLotto, bonusNumber, winningNumber) {
+    const winningResult = winningNumber.checkWinning(myLotto, bonusNumber);
+    this.#printWinningCnt(winningResult);
+    this.#printReturn(
+      winningNumber.calculateReturn(this.#purchaseAmount, winningResult),
+    );
+  }
 }
 
 export default App;
