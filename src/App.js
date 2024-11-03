df18840f04acdc6778727a3dca4721e5efeaf02a
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-03 16:31:38 +0900

    feat: 복권 발매기 실행 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import UserInput from './View/UserInput.js';
+import UserOutput from './View/UserOutput.js';
+import MoneyToLottos from './Contoller/MoneyToLottos.js';
+import LottoResultAnalysis from './Contoller/LottoResultAnalysis.js';
+
 class App {
-  async run() {}
+  userInput;
+
+  constructor() {
+    this.userInput = new UserInput();
+  }
+  async run() {
+    const purchaseAmount = await this.userInput.inputPurchaseAmount();
+    const moneyToLottos = new MoneyToLottos(purchaseAmount);
+    const userLottos = moneyToLottos.generateLottoTickets();
+    UserOutput.showPurchsedLotto(userLottos);
+    const winningNumbers = await this.userInput.inputWinningNumbers();
+    const bonusNumber = await this.userInput.inputBonusNumber(winningNumbers);
+    const lottoResultAnalysis = new LottoResultAnalysis(
+      winningNumbers,
+      bonusNumber
+    );
+    const { winningStatus, profitRate } =
+      lottoResultAnalysis.winningStatusAnalaysis(userLottos);
+    UserOutput.showWinningStatus(winningStatus);
+    UserOutput.showProfitRate(profitRate);
+  }
 }
 
 export default App;
