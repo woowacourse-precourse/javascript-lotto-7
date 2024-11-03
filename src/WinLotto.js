9d7a2a924f4c709b3a56de9197f7bd00607a62d6
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-03 17:05:03 +0900

    feat: 당첨 결과를 출력

--- src/WinLotto.js
+++ src/WinLotto.js
@@ -1,3 +1,5 @@
+import { Console} from "@woowacourse/mission-utils";
+
 export default class WinLotto {
   #purchasedLotto;
   #winningLotto;
@@ -10,6 +12,16 @@ export default class WinLotto {
     "5+bonus": 30000000,
     6: 2000000000,
   };
+  #WINNING_STATISTICS_PROMPT = '당첨 통계'
+  #DIVIDING_LINE = '---'
+  #MATCH_THREE_PROMPT= `3개 일치 (5,000원) - `
+  #MATCH_FOUR_PROMPT='4개 일치 (50,000원) - '
+  #MATCH_FIVE_PROMPT ='5개 일치 (1,500,000원) - '
+  #MATCH_FIVE_PLUS_PROMPT ='5개 일치, 보너스 볼 일치 (30,000,000원) - '
+  #MATCH_SIX_PROMPT = '6개 일치 (2,000,000,000원) - '
+  #TOTAL_RETURN='총 수익률은 '
+
+
 
   constructor(purchasedLotto, winningLotto, bonusNumber) {
     this.#purchasedLotto = purchasedLotto;
@@ -27,6 +39,7 @@ export default class WinLotto {
 
       this.#determineRank(matchedCount, hasBonus);
     });
+    this.printResults();
   }
 
   #determineRank(matchedCount, hasBonus) {
@@ -41,5 +54,25 @@ export default class WinLotto {
     } else if (matchedCount === 3) {
       this.#results[3]++;
     }
+  }
+
+  printResults() {
+    Console.print(this.#WINNING_STATISTICS_PROMPT);
+    Console.print(this.#DIVIDING_LINE);
+    Console.print(`${this.#MATCH_THREE_PROMPT}${this.#results[3]}개`);
+    Console.print(`${this.#MATCH_FOUR_PROMPT}${this.#results[4]}개`);
+    Console.print(`${this.#MATCH_FIVE_PROMPT}${this.#results[5]}개`);
+    Console.print(
+      `${this.#MATCH_FIVE_PLUS_PROMPT}${this.#results["5+bonus"]}개`
+    );
+    Console.print(`${this.#MATCH_SIX_PROMPT}${this.#results[6]}개`);
+
+    const totalPrize = Object.entries(this.#results).reduce(
+      (acc, [key, count]) => acc + this.#PRIZES[key] * count,
+      0
+    );
+    const purchaseAmount = this.#purchasedLotto.length * 1000; 
+    const profitRate = ((totalPrize / purchaseAmount) * 100).toFixed(1);
+    Console.print(`${this.#TOTAL_RETURN}${profitRate}%입니다.`);
   }
 }
