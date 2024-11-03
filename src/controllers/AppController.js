f926aff20b7f22b6148c00b66238fc05286fa7bb
yheseul <kangyheseul@naver.com>
2024-11-03 22:00:46 +0900

    feat: AppController에서 LottoController 호출
    - 사용자 입력에 따라 로또 구매 금액을 검증하고 LottoController를 통해 로또 구매 개수를 계산 및 로또 번호를 발행하도록 기능을 구현함.
    - LottoController를 호출하여 책임을 분리함으로써 코드의 가독성과 유지보수성을 향상시킴.

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -1,14 +1,18 @@
 import { validationPipe } from "../validation/validationPipe.js";
 import ConsoleView from "../views/ConsoleView.js";
+import LottoController from "./LottoController.js";
 
 class AppController {
   constructor() {
     this.consoleView = new ConsoleView();
+    this.lottoController = new LottoController();
   }
 
   async run() {
     const purchaseAmount = await this.consoleView.getPurchaseAmount();
     validationPipe(purchaseAmount);
+
+    this.lottoController.run(purchaseAmount);
   }
 }
 
