1a8758319384e70e6a9aa04656bfeee07ac1cdf5
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-04 05:39:50 +0900

    feat(lotto): 중복된 숫자에 대한 예외 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,10 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개의 서로 다른 숫자여야 합니다.');
+    }
   }
 
   get numbers() {
