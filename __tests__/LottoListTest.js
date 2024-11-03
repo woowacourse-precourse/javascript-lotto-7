e9e8ce7886fccd6cf2286bb1ba23b6303995edc6
govl0407 <govl0407@gmail.com>
2024-11-03 21:25:28 +0900

    feat : 로또 번호 범위, 중복 체크, 보너스 번호 범위, 중복 체크

--- __tests__/LottoListTest.js
+++ __tests__/LottoListTest.js
@@ -28,30 +28,53 @@ describe("LottoList 테스트", () => {
   test("당첨 번호 테스트1 : 문자열 입력", () => {
     expect(() => {
       let ll = new LottoList(1000);
-      ll.winnubmers = [8, 21, 23, 41, "string"];
+      ll.winnubmers = [8, 21, 23, 41, 29, "string"];
       ll.winnubmers.forEach(num=>{ll.checkwinnumbers(num);})
     }).toThrow("[Error] : 당첨번호는 숫자입니다.");
   });
   test("당첨 번호 테스트2 : 범위 밖 입력", () => {
     expect(() => {
       let ll = new LottoList(1000);
-      ll.winnubmers = [8, 21, 23, 41, 100];
+      ll.winnubmers = [8, 21, 23, 41, 32, 100];
       ll.winnubmers.forEach(num=>{ll.checkwinnumbers(num);})
     }).toThrow("[Error] : 당첨번호는 1~45 값입니다.");
+  });
+  test("당첨 번호 테스트3 : 중복 수 입력", () => {
+    expect(() => {
+      let ll = new LottoList(1000);
+      ll.winnubmers = [8, 21, 23, 14, 41, 41];
+      ll.winnubmers.forEach(num=>{ll.checkwinnumbers(num);})
+    }).toThrow("[Error] : 당첨번호는 unique 해야합니다");
+  });
+  test("당첨 번호 테스트4 : 입력 부족", () => {
+    expect(() => {
+      let ll = new LottoList(1000);
+      ll.winnubmers = [8, 21, 23, 41, 15];
+      ll.winnubmers.forEach(num=>{ll.checkwinnumbers(num);})
+    }).toThrow("[Error] : 당첨번호는 6개 입니다");
   });
   test("보너스 번호 테스트1: 범위 밖 입력", () => {
     expect(() => {
       let ll = new LottoList(1000);
       ll.bonusnubmer = 100;
-      ll.checkwinnumbers(ll.bonusnubmer);
+      ll.winnubmers = [8, 21, 23, 14, 39, 41];
+      ll.checkwinnumbers();
     }).toThrow("[Error] : 당첨번호는 1~45 값입니다.");
   });
-  test("당첨 번호 테스트2 : 문자열 입력", () => {
+  test("보너스 번호 테스트2 : 문자열 입력", () => {
     expect(() => {
       let ll = new LottoList(1000);
       ll.bonusnubmer = "string"
+      ll.winnubmers = [8, 21, 23, 14, 40, 41];
       ll.checkwinnumbers(ll.bonusnubmer);
     }).toThrow("[Error] : 당첨번호는 숫자입니다.");
   });
-
+  test("보너스 번호 테스트3 : 중복 입력", () => {
+    expect(() => {
+      let ll = new LottoList(1000);
+      ll.bonusnubmer = 41
+      ll.winnubmers = [8, 21, 23, 14, 41, 40];
+      ll.checkwinnumbers(ll.bonusnubmer);
+    }).toThrow("[Error] : 보너스 번호가 중복됩니다.");
+  });
 });
--- src/LottoList.js
+++ src/LottoList.js
@@ -10,22 +10,29 @@ class LottoList{
         this.getlotto();
     }
 
-    checkwinnumbers(num){
+    checksinglenum(num){
         if(isNaN(num))
             this.makeError("당첨번호는 숫자입니다.");
 
         if(num<0 || num>45)
             this.makeError("당첨번호는 1~45 값입니다.");
+    }
+    checkwinnumbers(){
+        this.winnubmers.forEach(num=>{this.checksinglenum(num);})
+        this.checksinglenum(this.bonusnubmer);
+        if(this.winnubmers.length!==6)
+            this.makeError("당첨번호는 6개 입니다");
+        if(this.winnubmers.includes(this.bonusnubmer))
+            this.makeError("보너스 번호가 중복됩니다.");
+        if(this.winnubmers.length!== new Set(this.winnubmers).length)
+            this.makeError("당첨번호는 unique 해야합니다");
 
     }
     async setwinnumbers(){
         let numsInput = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
         this.winnubmers = numsInput.split(',');
-        if(this.winnubmers.length!==6)
-            this.makeError("당첨번호는 6개 입니다");
-        this.winnubmers.forEach(num=>{checkwinnumbers(num);})
         this.bonusnubmer = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
-        this.checkwinnumbers(this.bonusnubmer);
+        this.checkwinnumbers()
     }
 
     printbuyotto(){
