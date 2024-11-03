b6cc88df5a82cb37a76c8026d961a52b2da01763
ZUITOPIA <korhwngml123@naver.com>
2024-11-03 12:16:53 +0900

    feat(Lotto): 로또 발행 횟수만큼 1부터 45사이의 6개의 서로 다른 랜덤 값 뽑고 출력

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import InputHandler from "./InputHandler.js";
+import Lotto from "./Lotto.js";
 
 class App {
     async run() {
         const inputHandler = new InputHandler();
-
         const lottoCount = await inputHandler.getLottoTryCount();
         console.log(`${lottoCount}개를 구매했습니다.`);
 
         await inputHandler.getJackpotNumbers();
         await inputHandler.getBonusNumber();
+
+        const lottoNumbers = Lotto.generateLottoNumbers(lottoCount);
+        Lotto.printLottoNumbers(lottoNumbers);
     }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,37 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    #validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    static generateLottoNumbers(lottoCount) {
+        const lottoNumbers = [];
+        for (let i = 0; i < lottoCount; i++) {
+            const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+            lottoNumbers.push(numbers.sort((a, b) => a - b));
+        }
+        return lottoNumbers;
+    }
+
+    get numbers() {
+        return this.#numbers;
+    }
+
+    static printLottoNumbers(lottoNumbers) {
+        lottoNumbers.forEach(numbers => {
+            console.log(numbers);
+        });
+    }
 }
 
 export default Lotto;
