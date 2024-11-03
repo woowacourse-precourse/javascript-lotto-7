6ca8db645ddffc908b623ca6fec73b0ffbfa48ae
parkyugyeong <qkrb8019@gmai.com>
2024-11-03 17:32:50 +0900

    feat : 메세지 상수화

---
+++ src/utils/messege.js
@@ -0,0 +1,19 @@
+export const INPUT_MESSAGES = {
+  MONEY_INPUT: "로또 구입 금액을 입력해주세요 (1000원 단위로 입력)",
+  WINNING_NUMBER: "당첨 번호를 입력해주세요 (쉼표(,)로 구분해서 입력)",
+  BONUS_NUMBER: "보너스 번호를 입력해주세요",
+};
+export const OUTPUT_MESSAGES = {
+  WINNING_RESULT: "딩첨통계",
+};
+export const ERROR_MESSAGES = {
+  ERROR_MESSAGE_PREFIX: "[ERROR]",
+  MONEY_DEGREE: "1000원단위로 구매할 수 있습니다.",
+  NOT_A_NUMBER: "숫자만 입력가능합니다.",
+  INVALID_NUMBER_COUNT: "로또 번호는 6개여야 합니다.",
+  DUPLICATE_NUMBERS: "로또번호는 중복될 수 없습니다.",
+  OUT_OF_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  BONUS_NUMBER_INVALID_COUNT: "[ERROR] 보너스 번호는 하나만 입력가능합니다.",
+  BONUS_NUMBER_DUPLICATE:
+    "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.",
+};
