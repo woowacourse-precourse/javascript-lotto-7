63693a5c6c021517343ad110469a347af3762442
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 05:38:01 +0900

    feat: 수익률 계산 메서드 호출 및 출력 함수에 전달

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {amountToNumber, stringToNumberArray} from "./services/preprocessing.js";
+import {amountToNumber, stringToNumberArray} from "./services/utils/preprocessing.js";
 import {INPUT} from "./constants/message.js";
 import {input, purchaseOutput, winningStatsOutput} from "./ui/view.js";
 import LottoMachine from "./services/LottoMachine.js";
@@ -7,20 +7,17 @@ import Lotto from "./services/Lotto.js";
 class App {
 
     async run() {
-        const purchaseNum = amountToNumber(await input(INPUT.PURCHASE)) //입력금액 갯수로 변환 한걸 담은 변수
-
+        const purchaseAmount = await input(INPUT.PURCHASE)
+        const purchaseNum = amountToNumber(purchaseAmount) //입력금액 갯수로 변환 한걸 담은 변수
         const lottoMachine = new LottoMachine(purchaseNum)
         const purchasedLottos = lottoMachine.lottoRelease()
-
         purchaseOutput(purchasedLottos)
-
         const winningNum = stringToNumberArray(await input(INPUT.WINNING_STATS))
-
         const lotto = new Lotto(winningNum)
-        const bonusNum = await input(INPUT[2]) //보너스 번호는 유효성 검사만 필요하다
-
-        const resultObj = lotto.setStats(purchasedLottos, bonusNum)
-        winningStatsOutput(resultObj)
+        const bonusNum = await input(INPUT.BONUS_NUM) //보너스 번호는 유효성 검사만 필요하다
+        lotto.recordStats(purchasedLottos, bonusNum)
+        const calculateYield = lotto.calculateYield(purchaseAmount)
+        winningStatsOutput(calculateYield)
     }
 }
 
