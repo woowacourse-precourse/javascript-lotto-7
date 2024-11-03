7d3fe55cf25e31722473f97f3c7cfd64519323a9
devjaeyoon <devjaeyoon@gmail.com>
2024-11-03 13:27:11 +0900

    feat(OutputView): 당첨 통계 관련 print 메소드 추가
    - '당첨 통계' 문구 출력 printWinningStaticsTitle
    - 당첨 통계 구분선 출력 printWinningStaticsDivideLine
    - 등수 통계 출력 printRankStatics
    - 수익률 출력 printRateOfReturn

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import RANK_STATICS_MESSAGES from '../constants/messages/rankStaticsMessages.js';
 
 class OutputView {
   static printNewLine() {
@@ -11,6 +12,24 @@ class OutputView {
 
   static printLottoCount(lottoCount) {
     MissionUtils.Console.print(`${lottoCount}개를 구매했습니다.`);
+  }
+
+  static printWinningStaticsTitle() {
+    MissionUtils.Console.print('당첨 통계');
+  }
+
+  static printWinningStaticsDivideLine() {
+    MissionUtils.Console.print('---');
+  }
+
+  static printRankStatics(rankStatics) {
+    RANK_STATICS_MESSAGES.forEach(({ rank, message }) => {
+      this.printMessage(`${message} - ${rankStatics[rank]}개`);
+    });
+  }
+
+  static printRateOfReturn(rateOfReturn) {
+    MissionUtils.Console.print(`총 수익률은 ${rateOfReturn.toFixed(1)}%입니다.`);
   }
 }
 
