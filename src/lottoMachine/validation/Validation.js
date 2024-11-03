76457e180f2239f33f88c40c2f10401f0b4a5a07
jjang-aaa <jadoit473@gmail.com>
2024-11-04 02:49:48 +0900

    refactor(Validation) : 에러문구 중복으로 조건 수정

--- src/lottoMachine/validation/Validation.js
+++ src/lottoMachine/validation/Validation.js
@@ -8,11 +8,7 @@ export class Validation {
       throw new Error('[ERROR] 숫자를 입력해 주세요.');
     }
 
-    if (purchasePrice === '0') {
-      throw new Error('[ERROR] 1,000원이상 입력해 주세요.');
-    }
-
-    if (Number(purchasePrice) % 1000 !== 0) {
+    if (Number(purchasePrice) % 1000 !== 0 || purchasePrice === '0') {
       throw new Error('[ERROR] 1,000원단위로 입력해 주세요.');
     }
   }
