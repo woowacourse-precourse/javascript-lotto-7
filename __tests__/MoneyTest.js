a71b11b00c3da26d4000ce28ac1ca73e4962050e
devjaeyoon <devjaeyoon@gmail.com>
2024-11-04 00:09:19 +0900

    refactor(MoneyTest): 에러 문구 에러 메시지 상수로 리팩토링

--- __tests__/MoneyTest.js
+++ __tests__/MoneyTest.js
@@ -1,3 +1,4 @@
+import ERROR_MESSAGES from '../src/constants/messages/errorMessages.js';
 import Money from '../src/models/Money.js';
 
 describe('Money 클래스 테스트', () => {
@@ -9,12 +10,12 @@ describe('Money 클래스 테스트', () => {
   test('구입금액으로 숫자가 아닌 값을 입력한 경우 예외가 발생한다.', () => {
     expect(() => {
       new Money('abcd');
-    }).toThrowError('[ERROR] 구매 금액으로 숫자가 아닌 값을 입력할 수 없습니다!');
+    }).toThrowError(`${ERROR_MESSAGES.ERROR_PREFIX}${ERROR_MESSAGES.MONEY_TYPE_NOT_NUMBER}`);
   });
 
   test('구입금액이 1000원으로 나누어떨어지지 않는 경우 예외가 발생한다.', () => {
     expect(() => {
       new Money('1500');
-    }).toThrowError('[ERROR] 구매 금액을 1000원 단위로 입력해 주세요!');
+    }).toThrowError(`${ERROR_MESSAGES.ERROR_PREFIX}${ERROR_MESSAGES.MONEY_UNIT_NOT_RIGHT}`);
   });
 });
