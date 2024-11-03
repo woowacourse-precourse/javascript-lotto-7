d02351b431e61778b3ceb79eb41adede0a14fed4
silver93hq <gkagjsrb@gmail.com>
2024-11-03 14:14:42 +0900

    test(validator): 테스트 케이스 세분화

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -9,29 +9,66 @@ describe('Validator 테스트', () => {
     [1500, false],
     [1000.3, false],
   ])('구매 금액에 대한 테스트 입력 : %d 결과 : %s', (amount, expectedResult) => {
-    // when & then
     expect(validator.validateAmount(amount)).toBe(expectedResult);
   });
 
-  test.each([
-    [[1, 10, 15, 25, 35, 45], true],
-    [[1, 2, 3, 4, 5, 5], false],
-    [[0, 1, 2, 3, 4, 5], false],
-    [[1, 2, 3, 4, 5, 46], false],
-  ])('당첨 번호에 대한 테스트 입력 : %s 결과 : %s', (numbers, expectedResult) => {
+  test('중복된 값에 대한 유효성 검사', () => {
+    // given
+    const LOTTO_NUMBERS = [1, 2, 3, 4, 5, 5];
+    const EXPECTED_RESULT = false;
+
     // when & then
-    expect(validator.validateLottoNumbers(numbers)).toBe(expectedResult);
+    expect(validator.validateDuplicatedNumber(LOTTO_NUMBERS)).toBe(EXPECTED_RESULT);
   });
 
-  test.each([
-    [[1, 2, 3, 4, 5, 6], 7, true],
-    [[1, 2, 3, 4, 5, 6], 6, false],
-    [[1, 2, 3, 4, 5, 6], 46, false],
-  ])(
-    '보너스 번호에 대한 테스트 당첨번호 : %s 보너스번호 : %s 결과 : %s',
-    (winningNumbers, bonusNumber, expectedResult) => {
-      // when & then
-      expect(validator.validateBonusNumber(winningNumbers, bonusNumber)).toBe(expectedResult);
+  test.each([[[0, 1, 2, 3, 4, 5], false]], [[1, 2, 3, 4, 5, 46], false])(
+    '',
+    (lottoNumbers, expectedResult) => {
+      expect(validator.validateNumberRange(lottoNumbers)).toBe(expectedResult);
     },
   );
+
+  test('잘못된 로또 번호 수량에 대한 유효성 검사', () => {
+    // given
+    const LOTTO_NUMBERS = [1, 2, 3, 4, 5, 6, 7];
+    const EXPECTED_RESULT = false;
+
+    // when & then
+    expecet(validator.validateNumbersLength(LOTTO_NUMBERS)).toBe(EXPECTED_RESULT);
+  });
+  test('유효한 로또 번호 테스트 케이스', () => {
+    // given
+    const LOTTO_NUMBERS = [1, 2, 3, 4, 5, 6];
+    const EXPECTED_RESULT = true;
+
+    // when
+    const methods = [
+      validator.validateNumberRange,
+      validator.validateDuplicatedNumber,
+      validator.validateNumbersLength,
+    ];
+    const result = methods.map((method) => method(LOTTO_NUMBERS)).every(Boolean);
+
+    // then
+    expect(result).toBe(EXPECTED_RESULT);
+  });
+
+  describe.each([1, 2, 3, 4, 5, 6])('', (lottoNumbers) => {
+    test('당첨번호와 중복되지 않는 보너스 번호에 대한 검사', () => {
+      // given
+      const BONUS_NUMBER = 6;
+      const EXPECTED_RESULT = true;
+
+      // when & then
+      expect(validator.validateBonusNumber(lottoNumbers, BONUS_NUMBER)).toBe(EXPECTED_RESULT);
+    });
+    test('당첨번호와 중복되는 보너스 번호에 대한 검사', () => {
+      // given
+      const BONUS_NUMBER = 7;
+      const EXPECTED_RESULT = false;
+
+      // when & then
+      expect(validator.validateBonusNumber(lottoNumbers, BONUS_NUMBER)).toBe(EXPECTED_RESULT);
+    });
+  });
 });
