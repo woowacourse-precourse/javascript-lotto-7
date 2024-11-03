b6d159aa2a2bcf4f914fb3eb0fe3d57a334c6e16
jeongwoo903 <www8565@naver.com>
2024-11-04 02:03:18 +0900

    feat(LottoValidator): 로또 번호와 관련된 validation 구현

--- src/domain/LottoValidator.js
+++ src/domain/LottoValidator.js
@@ -1,39 +1,82 @@
 import { ERROR_MESSAGES } from '../constants/messages.js';
-import { PRICE_MAX_AMOUNT, PRICE_PER_LOTTO } from '../constants/constants.js';
+import {
+  LOTTO_CONDITION,
+  PRICE_MAX_AMOUNT,
+  PRICE_PER_LOTTO,
+} from '../constants/constants.js';
 
 class LottoValidator {
   validatePurchaseAmount(amount) {
     if (this.#isEmpty(amount)) throw new Error(ERROR_MESSAGES.empty);
 
-    if (this.#isNumeric(amount)) throw new Error(ERROR_MESSAGES.numeric);
+    if (this.#isNotNumeric(amount)) throw new Error(ERROR_MESSAGES.numeric);
 
-    if (this.#isPositive(amount)) throw new Error(ERROR_MESSAGES.positive);
+    if (this.#isNotPositive(amount)) throw new Error(ERROR_MESSAGES.positive);
 
-    if (this.#isValidAmount(amount))
+    if (this.#isInvalidAmount(amount))
       throw new Error(ERROR_MESSAGES.invalid_amount);
 
-    if (this.#isOverValidRange(amount))
-      throw new Error(ERROR_MESSAGES.invalid_range);
+    if (this.#isExceedRange(amount))
+      throw new Error(ERROR_MESSAGES.exceed_range);
   }
 
+  validateLottoNumber(numbers) {
+    if (this.#hasInvalidNumber(numbers))
+      throw new Error(ERROR_MESSAGES.invalid_lotto_input);
+
+    if (this.#isInvalidLottoLength(numbers))
+      throw new Error(ERROR_MESSAGES.invalid_lotto_length);
+
+    if (this.#hasOutOfRange(numbers))
+      throw new Error(ERROR_MESSAGES.invalid_lotto_range);
+
+    if (this.#isDuplicate(numbers))
+      throw new Error(ERROR_MESSAGES.invalid_lotto_unique);
+  }
+
+  // 공통
+  #isNotNumeric(input) {
+    return Number.isNaN(Number(input));
+  }
+
+  // validatePurchaseAmount 관련
   #isEmpty(input) {
     return input == null || input.trim() === '';
   }
 
-  #isNumeric(input) {
-    return Number.isNaN(Number(input));
-  }
-
-  #isPositive(input) {
+  #isNotPositive(input) {
     return Number(input) <= 0;
   }
 
-  #isValidAmount(input) {
+  #isInvalidAmount(input) {
     return Number(input) % PRICE_PER_LOTTO !== 0;
   }
 
-  #isOverValidRange(input) {
+  #isExceedRange(input) {
     return Number(input) > PRICE_MAX_AMOUNT;
+  }
+
+  // validateWinningLotto 관련
+  #hasInvalidNumber(numbers) {
+    return numbers.some((number) => this.#isNotNumeric(number));
+  }
+
+  #isInvalidLottoLength(numbers) {
+    return numbers.length !== LOTTO_CONDITION.length;
+  }
+
+  #hasOutOfRange(numbers) {
+    return numbers.some((number) => this.#isInvalidLottoRange(number));
+  }
+
+  #isInvalidLottoRange(number) {
+    return !(
+      number >= LOTTO_CONDITION.startRange && number <= LOTTO_CONDITION.endRange
+    );
+  }
+
+  #isDuplicate(numbers) {
+    return new Set(numbers).size !== LOTTO_CONDITION.length;
   }
 }
 
