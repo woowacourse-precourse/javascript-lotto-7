a043b1c45472802f590983b9ac6bdf15912f3684
BoRamSon <onbrson1@gmail.com>
2024-11-04 02:05:03 +0900

    fix(InputPrizeNumModel): 입력 재시도 횟수 3회에서 5회로 늘리기 코드 변경

--- src/model/InputPrizeNumModel.js
+++ src/model/InputPrizeNumModel.js
@@ -12,9 +12,9 @@ export class InputPrizeNumModel {
       const inputPrizeNumbers = await this.inputView.inputPrizeNumbers();
       const isNoProblem = validInputPrizeNum(inputPrizeNumbers);
       if (isNoProblem) return (inputPrizeNumbersResult = inputPrizeNumbers);
-      if (i === 3)
+      if (i === 5)
         throw new Error(
-          `[ERROR] 3회 이상 잘 못 입력하여 초기화되었습니다. 다시 실행해 주세요.`
+          `[ERROR] 5회 이상 잘 못 입력하여 초기화되었습니다. 다시 실행해 주세요.`
         );
     }
     return inputPrizeNumbersResult;
