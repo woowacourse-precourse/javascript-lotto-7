186b05d765044e5515e13c056b5ef001d37ad590
조승우 <choseung97@gmail.com>
2024-11-03 19:02:35 +0900

    test(game): 로또 생성 결과를 출력하는 함수를 테스트하는 기능을 추가한다
    Lotto 객체가 갖는 로또 번호를 알맞게 출력하는지 확인한다.

---
+++ __tests__/OutputViewTest.js
@@ -0,0 +1,30 @@
+import { Console } from '@woowacourse/mission-utils';
+import { printGeneratedList } from '../src/utils/OutputView.js';
+import Lotto from '../src/components/Lotto/Lotto.js';
+
+jest.mock('@woowacourse/mission-utils', () => ({
+  Console: {
+    print: jest.fn(),
+  },
+}));
+
+describe('printGeneratedList 함수 테스트', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('로또 리스트 출력 테스트', () => {
+    const lottoList = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([7, 8, 9, 10, 11, 12]),
+    ];
+
+    printGeneratedList(lottoList);
+
+    expect(Console.print).toHaveBeenNthCalledWith(1, '');
+    expect(Console.print).toHaveBeenNthCalledWith(2, '2개를 구매했습니다.');
+    expect(Console.print).toHaveBeenNthCalledWith(3, '[1, 2, 3, 4, 5, 6]');
+    expect(Console.print).toHaveBeenNthCalledWith(4, '[7, 8, 9, 10, 11, 12]');
+    expect(Console.print).toHaveBeenNthCalledWith(5, '');
+  });
+});
