1113c8251251c627e6f741805e5226aff2414cea
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-03 17:05:34 +0900

    refactor: 에러 메시지 상수화

--- src/Constants/Message.js
+++ src/Constants/Message.js
@@ -13,3 +13,15 @@ export const LOTTO_RESULT_MESSAGES = Object.freeze({
   FOURTH: '4개 일치 (50,000원) - ',
   FIFTH: '3개 일치 (5,000원) - ',
 });
+
+export const ERROR_MESSAGES = Object.freeze({
+  INVALID_THOUSAND_UNIT: '[ERROR] 구매 금액은 1000원 단위여야 합니다.',
+  EMPTY_INPUT: '[ERROR] 입력값이 없습니다.',
+  NON_NUMERIC_INPUT: '[ERROR] 숫자로 입력해야 합니다.',
+  INVALID_COMMA_FORMAT: '[ERROR] 구분자가 잘못되었습니다.',
+  INCORRECT_NUMBER_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
+  DUPLICATE_NUMBER: '[ERROR] 중복되는 번호가 존재합니다.',
+  NON_POSITIVE_INTEGER: '[ERROR] 모든 숫자는 양의 정수여야 합니다.',
+  NUMBER_OUT_OF_RANGE: '[ERROR] 모든 숫자는 1부터 45 사이여야 합니다.',
+  DUPLICATE_BONUS_NUMBER: '[ERROR] 당첨번호와 중복되는 번호입니다.',
+});
--- src/Contoller/Validation.js
+++ src/Contoller/Validation.js
@@ -1,19 +1,21 @@
+import { ERROR_MESSAGES } from '../constants/Message.js';
+
 class Validation {
   static validateThousandUnit(amount) {
     if (amount % 1000 !== 0) {
-      throw new Error('[ERROR] 구매 금액은 1000원 단위여야 합니다.');
+      throw new Error(ERROR_MESSAGES.INVALID_THOUSAND_UNIT);
     }
   }
 
   static validateNotEmpty(input) {
     if (input === '') {
-      throw new Error('[ERROR] 입력값이 없습니다.');
+      throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
     }
   }
 
   static validateIsNumber(input) {
     if (isNaN(input)) {
-      throw new Error('[ERROR] 숫자로 입력해야 합니다.');
+      throw new Error(ERROR_MESSAGES.NON_NUMERIC_INPUT);
     }
   }
 
@@ -23,38 +25,38 @@ class Validation {
       numbers.startsWith(',') ||
       numbers.endsWith(',')
     ) {
-      throw new Error('[ERROR] 구분자가 잘못되었습니다.');
+      throw new Error(ERROR_MESSAGES.INVALID_COMMA_FORMAT);
     }
   }
 
   static validateSixNumbers(numbers) {
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error(ERROR_MESSAGES.INCORRECT_NUMBER_COUNT);
     }
   }
 
   static validateUniqueNumbers(numbers) {
     const uniqueNumbers = new Set(numbers);
     if (uniqueNumbers.size !== numbers.length) {
-      throw new Error('[ERROR] 중복되는 번호가 존재합니다.');
+      throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER);
     }
   }
 
   static validatePositiveInteger(number) {
     if (!(Number.isInteger(number) && number > 0)) {
-      throw new Error('[ERROR] 모든 숫자는 양의 정수여야 합니다.');
+      throw new Error(ERROR_MESSAGES.NON_POSITIVE_INTEGER);
     }
   }
 
   static validateNumberInRange(number) {
     if (!(number >= 1 && number <= 45)) {
-      throw new Error('[ERROR] 모든 숫자는 1부터 45 사이여야 합니다.');
+      throw new Error(ERROR_MESSAGES.NUMBER_OUT_OF_RANGE);
     }
   }
 
   static validateUniqueBonusNumber(numbers, bonusNumber) {
     if (numbers.includes(bonusNumber)) {
-      throw new Error('[ERROR] 당첨번호와 중복되는 번호입니다.');
+      throw new Error(ERROR_MESSAGES.DUPLICATE_BONUS_NUMBER);
     }
   }
 }
