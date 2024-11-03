24aadad50ae4feabbbc3fac448919c5edd5a6d81
장정안 <wjddks96@naver.com>
2024-11-04 04:36:55 +0900

    refactor: Game 클래스의 과중한 책임을 각 컨트롤러로 분리

--- src/Controller/Game.js
+++ src/Controller/Game.js
@@ -1,120 +1,42 @@
 //@ts-check
 
-import { GAME_MESSAGE } from '../constants/gameMessage.js';
-import { LOTTO_CONFIG } from '../constants/lotto.js';
 import User from '../User/User.js';
 import LottoService from '../Service/LottoService.js';
 import LottoResultView from '../views/LottoResultView.js';
-import { validateLottoPurchase } from '../validate/purchaseValidator.js';
-import { outputView } from '../views/outputView.js';
-import { validateWinningNumber } from '../validate/winningNumberValidator.js';
-import { validateBonusNumber } from '../validate/bonusNumberValidator.js';
-import Lotto from '../Lotto.js';
+import WinningNumberController from './WinningNumberController.js';
+import PurchaseController from './PurchaseController.js';
+import ResultController from './ResultController.js';
 
 class Game {
   constructor() {
-    this.user = new User();
-    this.lottoService = new LottoService();
-    this.resultView = new LottoResultView();
-    this.winningNumbers = null;
+    const user = new User();
+    const lottoService = new LottoService();
+    const resultView = new LottoResultView();
+
+    this.purchaseController = new PurchaseController(
+      user,
+      lottoService,
+      resultView
+    );
+    this.winningNumberController = new WinningNumberController(user);
+    this.resultController = new ResultController(lottoService, resultView);
   }
 
   async process() {
-    const { lottos, purchaseAmount } = await this.handleUserPurchase();
-    this.winningNumbers = await this.getWinningNumbers();
-    const bonusNumber = await this.getBonusNumber();
+    const { lottos, purchaseAmount } =
+      await this.purchaseController.handlePurchase();
+    const winningNumbers =
+      await this.winningNumberController.getWinningNumbers();
+    const bonusNumber = await this.winningNumberController.getBonusNumber(
+      winningNumbers
+    );
 
-    const results = this.lottoService.calculateResults(
+    this.resultController.processResults(
       lottos,
-      this.winningNumbers,
-      bonusNumber
+      winningNumbers,
+      bonusNumber,
+      purchaseAmount
     );
-    this.resultView.printResults(results);
-
-    const earningRate = this.lottoService.calculateEarningRate(
-      purchaseAmount,
-      results
-    );
-    this.resultView.printEarningRate(earningRate);
-  }
-
-  async processPurchase() {
-    const purchaseAmount = await this.getPurchaseAmount();
-    const count = this.calculateLottoCount(purchaseAmount);
-    const lottos = this.generateLottos(count);
-    this.displayPurchaseResult(count, lottos);
-
-    return { lottos, purchaseAmount };
-  }
-
-  async getPurchaseAmount() {
-    const purchaseAmount = await this.user.readUserInput(GAME_MESSAGE.PURCHASE);
-    validateLottoPurchase(purchaseAmount);
-    return purchaseAmount;
-  }
-
-  /**@param {string} purchaseAmount  */
-  calculateLottoCount(purchaseAmount) {
-    return Number(purchaseAmount) / LOTTO_CONFIG.PRICE_PER;
-  }
-
-  /**@param {number} count  */
-  generateLottos(count) {
-    return this.lottoService.createLottos(count);
-  }
-
-  /**
-   * @param {number} count
-   * @param {Lotto[]} lottos
-   * */
-  displayPurchaseResult(count, lottos) {
-    this.resultView.printPurchaseResult(count, lottos);
-  }
-
-  /**@param {Error} error */
-  handleErrorMessage(error) {
-    outputView.printMessage(error.message);
-  }
-
-  async handleUserPurchase() {
-    while (true) {
-      try {
-        const { lottos, purchaseAmount } = await this.processPurchase();
-        return { lottos, purchaseAmount };
-      } catch (error) {
-        this.handleErrorMessage(error);
-      }
-    }
-  }
-
-  async getWinningNumbers() {
-    while (true) {
-      try {
-        return await this.processWinningNumbers();
-      } catch (error) {
-        this.handleErrorMessage(error);
-      }
-    }
-  }
-
-  async processWinningNumbers() {
-    const winningNumber = await this.user.readUserInput(
-      GAME_MESSAGE.WINNING_NUMBER
-    );
-    validateWinningNumber(winningNumber.split(',').map(Number));
-    return winningNumber.split(',').map((num) => Number(num.trim()));
-  }
-
-  async getBonusNumber() {
-    while (true) {
-      try {
-        const input = await this.user.readUserInput(GAME_MESSAGE.BONUS_NUMBER);
-        validateBonusNumber(input, this.winningNumbers);
-        return input;
-      } catch (error) {
-        outputView.printMessage(error.message);
-      }
-    }
   }
 }
 
