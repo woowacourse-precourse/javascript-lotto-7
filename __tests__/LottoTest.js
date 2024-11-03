3fc40dc006c38d16492f9ac91fdbe147091445e4
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-11-04 02:43:50 +0900

    test: 로또 객체 테스트 케이스 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,21 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('hasInNumbers 메서드는 특정 숫자가 로또 번호에 포함되었는지 확인한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.hasInNumbers(3)).toBe(true);
+    expect(lotto.hasInNumbers(7)).toBe(false);
+  });
+
+  test('countMatchingNumbers 메서드는 다른 로또와 일치하는 번호의 개수를 반환한다.', () => {
+    const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto2 = new Lotto([4, 5, 6, 7, 8, 9]);
+
+    expect(lotto1.countMatchingNumbers(lotto2)).toBe(3);
+
+    const lotto3 = new Lotto([10, 11, 12, 13, 14, 15]);
+
+    expect(lotto1.countMatchingNumbers(lotto3)).toBe(0);
+  });
 });
