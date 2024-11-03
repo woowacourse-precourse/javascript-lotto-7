15c2c985ebbee62e2ffb5a1b11be00598c8571a1
yheseul <kangyheseul@naver.com>
2024-11-04 00:55:33 +0900

    refactor: 숫자 검증 로직 재사용
    - 로또 금액 입력받아 검증하는 곳에서 숫자 검증 로직 재적용

--- src/validation/validatePurchaseAmount/validatePurchaseAmountPipe.js
+++ src/validation/validatePurchaseAmount/validatePurchaseAmountPipe.js
@@ -1,11 +1,12 @@
-import { isNumber } from "./isNumber.js";
+import { isNumber } from "../isNumber.js";
 import { isPurchaseAmountEmpty } from "./isPurchaseAmountEmpty.js";
 import { isAmountDivisibleByBaseAmount } from "./isAmountDivisibleByBaseAmount.js";
 import { isAmountGreaterThanOrEqualToMinimum } from "./isAmountGreaterThanOrEqualToMinimum.js";
+import { LOTTO_PURCHASE_AMOUNT } from "../../constants/validationMessages/lottoPurchaseAmount.js";
 
 export const validatePurchaseAmountPipe = function (purchaseAmount) {
   isPurchaseAmountEmpty(purchaseAmount);
-  isNumber(purchaseAmount);
+  isNumber(purchaseAmount, LOTTO_PURCHASE_AMOUNT.NOT_A_NUMBER);
   isAmountGreaterThanOrEqualToMinimum(purchaseAmount);
   isAmountDivisibleByBaseAmount(purchaseAmount);
 };
