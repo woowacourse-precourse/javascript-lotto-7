cf82a24437b44a5cd70e19872aa2b4445d965a09
ockko <ocko119@gmail.com>
2024-11-03 23:34:02 +0900

    feat: 당첨 번호 유효성 건사 구현 및 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,35 @@
-import Lotto from "../src/Lotto";
+import Lotto from "../src/models/Lotto.js";
+import { ERROR_MESSAGES } from "../src/constants/Errors.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGES.invalidLottoNumberLength);
   });
 
   // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGES.invalidLottoNumberDuplicate);
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호에 문자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, "a", 5, 6]);
+    }).toThrow(ERROR_MESSAGES.invalidLottoNumberType);
+  });
+
+  test("로또 번호의 범위가 1부터 45 사이가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 66]);
+    }).toThrow(ERROR_MESSAGES.invalidLottoNumberRange);
+  });
+
+  test("로또 번호에 올바른 값이 입력되면 예외가 발생하지 않는다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6]);
+    }).not.toThrow();
+  });
 });
--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -1,3 +1,5 @@
+import Validation from "./Validation.js";
+
 class Lotto {
   #numbers;
 
@@ -7,31 +9,14 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    numbers.forEach((number) => {
-      if (Number(number) < 1 || Number(number) > 45) {
-        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-      }
-    });
-    if (this.isDupulicate(numbers)) {
-      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
-    }
-  }
-
-  isDupulicate(numbers) {
-    return numbers.some(
-      (number) => numbers.indexOf(number) !== numbers.lastIndexOf(number)
-    );
+    Validation.validateLottoNumberLength(numbers);
+    Validation.validateLottoNumberRange(numbers);
+    Validation.validateLottoNumberDuplicate(numbers);
+    Validation.validateLottoNumberType(numbers);
   }
 
   getNumbers() {
     return this.#numbers;
-  }
-
-  getValidate() {
-    return this.#validate(this.#numbers);
   }
 }
 
