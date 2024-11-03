79540e367e95a11517e2f3a0fdce40a04da92e11
장정안 <wjddks96@naver.com>
2024-11-04 01:37:14 +0900

    feat: Lotto 클래스에서 유효성 검사 제거로 관심사 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,32 +2,7 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
     this.#numbers = numbers.sort((a, b) => a - b);
-  }
-
-  #validate(numbers) {
-    this.#validateLength(numbers);
-    this.#validateDuplicate(numbers);
-    this.#validateRange(numbers);
-  }
-
-  #validateLength(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
-  }
-
-  #validateDuplicate(numbers) {
-    if (new Set(numbers).size !== numbers.length) {
-      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
-    }
-  }
-
-  #validateRange(numbers) {
-    if (numbers.some((num) => num < 1 || num > 45)) {
-      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
-    }
   }
 
   getNumbers() {
