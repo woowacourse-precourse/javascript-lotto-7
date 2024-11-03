9844e4dd44ae9b01b015e3ac065544bb1571123d
subsub-e <ab13696802@gmail.com>
2024-11-03 22:49:13 +0900

    fix(LottoController) : 잘못된 값 입력시 다시 입력하는 로직을 위해 inputController로 분리

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -1,4 +1,4 @@
-import InputView from '../views/InputView.js';
+import InputController from '../controllers/InputController.js';
 import OutputView from '../views/OutputView.js';
 import LottoNumberGenerator from '../services/LottoNumberGenerator.js';
 import LottoRepository from '../models/LottoRepository.js';
@@ -6,20 +6,34 @@ import LottoDrawingMachine from '../services/LottoDrawingMachine.js';
 import CalculatingMachine from '../services/CalculatingMachine.js';
 
 class LottoController {
+  constructor() {
+    this.inputController = new InputController();
+  }
+
   async start() {
-    const purchaseAmount = await InputView.getPurchaseAmount();
+    const purchaseAmount = await this.inputController.getValidPurchaseAmount();
     const lottoRepository = new LottoRepository();
     const lottoNumberGenerator = new LottoNumberGenerator(purchaseAmount);
     lottoNumberGenerator.generateLotto(lottoRepository);
+
     OutputView.printNewLine();
     OutputView.printLottoAmount(lottoRepository.getLottoAmount());
     OutputView.printLottoNumbers(lottoRepository.getLottoArray());
     OutputView.printNewLine();
-    const winningNumber = await InputView.getWinningNumber();
+
+    const winningNumber = await this.inputController.getValidWinningNumber();
     OutputView.printNewLine();
-    const bonusNumber = await InputView.getBonusNumber();
-    const lottoDrawingMachine = new LottoDrawingMachine(winningNumber, bonusNumber, lottoRepository);
+
+    const bonusNumber = await this.inputController.getValidBonusNumber(
+      winningNumber
+    );
+    const lottoDrawingMachine = new LottoDrawingMachine(
+      winningNumber,
+      bonusNumber,
+      lottoRepository
+    );
     const drawResult = lottoDrawingMachine.drawLotto();
+
     OutputView.printNewLine();
     OutputView.printWinningStatistics();
     OutputView.printUnderBar();
@@ -28,7 +42,11 @@ class LottoController {
     OutputView.printThirdPlaceWinner(drawResult);
     OutputView.printSecondPlaceWinner(drawResult);
     OutputView.printFirstPlaceWinner(drawResult);
-    const calculatingMachine = new CalculatingMachine(purchaseAmount, drawResult);
+
+    const calculatingMachine = new CalculatingMachine(
+      purchaseAmount,
+      drawResult
+    );
     const totalReturn = calculatingMachine.calculate();
     OutputView.printTotalReturn(totalReturn);
   }
