577420c8cb8c6e1bd0ba39fc35c3649fc922a5e0
ChangOKIM <kk104202@naver.com>
2024-11-04 01:34:43 +0900

    feat: LottoGame 클래스에 로또 구매 로직 추가

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,8 +1,46 @@
+import Lotto from "./Lotto.js";
 import { Console } from "@woowacourse/mission-utils";
 
 class LottoGame {
+  #lottoTickets = [];
+
   async start() {
-    // 로또게임메인로직
+    await this.#handlePurchase();
+    this.#displayTickets();
+  }
+
+  async #handlePurchase() {
+    const amount = await this.#getValidatedAmount();
+    this.#generateTickets(amount / 1000);
+  }
+
+  async #getValidatedAmount() {
+    return new Promise(resolve => {
+      Console.readLineAsync("구입금액을 입력해 주세요.\n").then(input => {
+        const amount = Number(input);
+        if (this.#isValidAmount(amount)) {
+          resolve(amount);
+        } else {
+          Console.print("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+          resolve(this.#getValidatedAmount());
+        }
+      });
+    });
+  }
+
+  #isValidAmount(amount) {
+    return !isNaN(amount) && amount % 1000 === 0;
+  }
+
+  #generateTickets(count) {
+    for (let i = 0; i < count; i++) {
+      this.#lottoTickets.push(Lotto.generate());
+    }
+  }
+
+  #displayTickets() {
+    Console.print(`${this.#lottoTickets.length}개를 구매했습니다.`);
+    this.#lottoTickets.forEach(ticket => Console.print(`[${ticket.getNumbers().join(", ")}]`));
   }
 }
 
