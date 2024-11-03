ce333dbf45e38419627af5df14a1e9a3c8eaccb0
ChanLee_KR <cl700120@gmail.com>
2024-11-03 23:07:17 +0900

    feat: 구매 금액 타입 검증 기능 구현

--- src/model/VendingMachine.js
+++ src/model/VendingMachine.js
@@ -1,3 +1,27 @@
-class VendingMachine {}
+import { Exception } from '../Utils.js';
+
+class VendingMachine {
+  /** @param {number} money */
+  constructor(money) {
+    this.#putInMoney(money);
+  }
+
+  /** @param {number} money */
+  #putInMoney(money) {
+    VendingMachine.validateMoney(money);
+  }
+
+  /** @param {number} money */
+  static validateMoney(money) {
+    VendingMachine.#validateMoneyType(money);
+  }
+
+  /** @param {number} money */
+  static #validateMoneyType(money) {
+    if (Number.isNaN(money) || !Number.isSafeInteger(money)) {
+      throw new Exception('유효하지 않은 금액입니다.');
+    }
+  }
+}
 
 export default VendingMachine;
