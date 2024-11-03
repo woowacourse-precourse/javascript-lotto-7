4ea97ec457d4aab4eb9d77617c4c229d869e532f
jeongwoo903 <www8565@naver.com>
2024-11-03 15:16:15 +0900

    refactor(constants, messages): 변수명을 더 명확하게 변경
    - MSG보단 복수의 의미로 MESSAGES로 변경.
    - airbnb code style에 따라 객체 내부의 key는 snake case로 변경.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,3 +3,6 @@ export const LOTTO_CONDITION = Object.freeze({
   endRange: 45,
   length: 6,
 });
+
+export const PRICE_PER_LOTTO = Object.freeze(1000);
+export const PRICE_MAX_AMOUNT = Object.freeze(100000000);
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,21 @@
-export const INPUT_MSG = Object.freeze({
-  purchaseAmount: '구입금액을 입력해 주세요.\n',
-  winningNumbers: '당첨 번호를 입력해 주세요.\n',
-  bonusNumber: '보너스 번호를 입력해 주세요.\n',
+import {
+  LOTTO_CONDITION,
+  PRICE_MAX_AMOUNT,
+  PRICE_PER_LOTTO,
+} from './constants';
+
+export const PREFIX_ERROR = '[ERROR]';
+
+export const INPUT_MESSAGES = Object.freeze({
+  purchase_amount: '구입금액을 입력해 주세요.\n',
+  winning_numbers: '당첨 번호를 입력해 주세요.\n',
+  bonus_number: '보너스 번호를 입력해 주세요.\n',
+});
+
+const ERROR_MESSAGES = Object.freeze({
+  empty: `${PREFIX_ERROR} 값을 입력해주세요.`,
+  numeric: `${PREFIX_ERROR} 숫자만 입력 가능합니다.`,
+  positive: `${PREFIX_ERROR} 양의 정수만 입력 가능합니다.`,
+  invalid_amount: `${PREFIX_ERROR} 구입 금액은 ${PRICE_PER_LOTTO}원으로 나누어 떨어져야 합니다.`,
+  over_max_amount: `${PREFIX_ERROR} 구입 금액은 ${PRICE_MAX_AMOUNT}원 이하여야 합니다.`,
 });
