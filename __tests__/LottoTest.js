f6083e89baa4e9afcacc796c17da905445ef7f39
kimywann <Kimywan10@gmail.com>
2024-11-04 22:39:21 +0900

    test(LottoTest): 로또 번호 유효성 검증에 대한 테스트 코드 추가
    - 로또 번호에 유효하지 않은 값이 들어올 경우 예외가 발생하는지 검증하는 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,37 @@
-import Lotto from "../src/Lotto";
+import Lotto from "../src/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
-  });
+    test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+        expect(() => {
+            // eslint-disable-next-line no-new
+            new Lotto([1, 2, 3, 4, 5, 6, 7]);
+        }).toThrow("[ERROR]");
+    });
 
   // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
+    test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+        expect(() => {
+            // eslint-disable-next-line no-new
+            new Lotto([1, 2, 3, 4, 5, 5]);
+        }).toThrow("[ERROR]");
+    });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    const checkLottoCreationError = (input, expectedMessage) => {
+        expect(() => {
+            // eslint-disable-next-line no-new
+            new Lotto(input);
+        }).toThrow(expectedMessage);
+    };
+
+    test.each([
+        { input: [1, 2, 3, 4, 5, 46], message: "[ERROR]" },
+        { input: [1, 2, 3, 4, 5, 'd'], message: "[ERROR]" },
+        { input: [1, 2, 3, 4, 5, -7], message: "[ERROR]" },
+    ])(
+        "로또 번호에 유효하지 않은 값이 들어오면 예외가 발생한다.",
+        ({ input, message }) => {
+            checkLottoCreationError(input, message);
+        }
+    );
 });
