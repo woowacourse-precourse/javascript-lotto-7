0125d714bc79abf3925f713bec2e642dec99548a
standard-chan <starp321@naver.com>
2024-11-04 22:30:38 +0900

    feat : 로또 번호 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Customer from "./model/Customer.js";
+import Lotto from "./model/Lotto.js";
 import LottoValidator from "./validators/LottoValidator.js";
 import InputView from "./view/InputView.js";
 
@@ -6,20 +7,24 @@ class App {
   async run() {
     const input = new InputView();
     const customer = new Customer();
+    const lotto = new Lotto();
     
 
     // 로또 구매 및 출력
     const purchaseAmount = await input.readPurchaseAmount();
     customer.purchaseLotto(purchaseAmount);
     customer.getLottoNumberList();
-    customer.lottoPrint();
-
+    
+    // 생성한 번호 출력
 
     const winningNumbers = await input.readWinningNumbers();
     const bonusNumber = await input.readBonusNumber();
     const allLottoNumbers = [...winningNumbers, bonusNumber];
     LottoValidator.validateDuplicateNumbers(allLottoNumbers);
     allLottoNumbers.forEach(LottoValidator.validatorSingleNumber);
+
+    lotto.setWinningAndBonusNumbers(winningNumbers, bonusNumber);
+    lotto.print();
 
     
 
--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -3,9 +3,24 @@ import { Random } from "@woowacourse/mission-utils";
 export default class Lotto {
   static MIN_LOTTO_NUMBER = 1;
   static MAX_LOTTO_NUMBER = 45;
+  #winningNumbers;
+  #bonusNumber;
+
+  setWinningAndBonusNumbers(winningNumbers, bonusNumber) {
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumber = bonusNumber;
+  }
 
   static generateLottoNumbers() {
     const lottoNumbers = Random.pickUniqueNumbersInRange(Lotto.MIN_LOTTO_NUMBER, Lotto.MAX_LOTTO_NUMBER, 6);
     return lottoNumbers;
+  }
+
+  print() {
+    console.log(this.#winningNumbers, this.#bonusNumber);
+  }
+
+  static checkWinningNumber(LottoNumbers) {
+
   }
 }
