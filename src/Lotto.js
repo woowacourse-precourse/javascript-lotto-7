9fbec23bd649ed215bc8b20c3fd2441894e89cc7
yeeun426 <thsudkcla7@naver.com>
2024-11-04 17:29:49 +0900

    feat : 로또 번호가 중복되는 경우 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,15 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.#validSameValue(numbers);
+  }
+
+  #validSameValue(numbers) {
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error(
+        "[ERROR] 당첨 번호 추첨 시 6개의 숫자와 보너스번호 1개가 중복되지 않아야 합니다."
+      );
+    }
   }
 
   getNumbers() {
