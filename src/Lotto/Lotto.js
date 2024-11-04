e3c06a77c1bd968a352483fe544c0b99d160bf83
Junseo Kim <semiwest2@gmail.com>
2024-11-04 22:59:08 +0900

    feat: 로또 번호 관련 기능 추가
    - 로또 번호를 출력하는 `print` 메서드 추가.
      - 로또 번호를 포맷팅하여 콘솔에 출력.
    
    - 특정 번호가 로또 번호에 포함되어 있는지 확인하는 `hasNumber` 메서드 추가.
      - 입력된 번호가 로또 번호 배열에 존재하는지 판별.
    
    - 당첨 등수를 판단하는 `getWinningRank` 메서드 로직 강화.
      - 당첨 번호와 보너스 번호를 비교하여 해당 등수를 반환.
      - 등수는 상수로 정의된 RANK_CONFIG를 사용하여 관리.
      - 일치하는 번호의 개수에 따라 적절한 등수를 반환하며, 조건에 맞지 않을 경우 `null` 반환.

--- src/Lotto/Lotto.js
+++ src/Lotto/Lotto.js
@@ -1,6 +1,8 @@
 import LOTTO_CONFIG from '../constants/lottoConfig.js';
+import RANK_CONFIG from '../constants/rankConfig.js';
 import ERROR_MESSAGE from '../constants/errorMessage.js';
 import printString from '../output/printString.js';
+
 
 class Lotto {
   #numbers;
@@ -10,35 +12,60 @@ class Lotto {
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== LOTTO_CONFIG.NUMBER_COUNT) {
-      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_OVER_COUNT);
+  print(){
+    printString(`[${this.#numbers.join(', ')}]`);
+  }
+
+  hasNumber(number) {
+    return this.#numbers.includes(number);
+  }
+
+  getWinningRank(winningLotto, bonusNumber) {
+    const matchingCount = this.#getMatchingCount(winningLotto);
+    if (matchingCount === RANK_CONFIG.FIRST.COUNT) {
+      return RANK_CONFIG.FIRST.RANK;
     }
+    if (matchingCount === RANK_CONFIG.SECOND.COUNT && winningLotto.hasNumber(bonusNumber)) {
+      return RANK_CONFIG.SECOND.RANK;
+    }
+    if (matchingCount === RANK_CONFIG.THIRD.COUNT) {
+      return RANK_CONFIG.THIRD.RANK;
+    }
+    if (matchingCount === RANK_CONFIG.FOURTH.COUNT) {
+      return RANK_CONFIG.FOURTH.RANK;
+    }
+    if (matchingCount === RANK_CONFIG.FIFTH.COUNT) {
+      return RANK_CONFIG.FIFTH.RANK;
+    }
+    return null;
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== LOTTO_CONFIG.NUMBER_COUNT) throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_OVER_COUNT);
+
     const numbersSet = new Set(numbers);
 
-    if (numbersSet.size !== LOTTO_CONFIG.NUMBER_COUNT) {
-      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_DUPLICATE);
-    }
+    if (numbersSet.size !== LOTTO_CONFIG.NUMBER_COUNT) throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_DUPLICATE);
+
     numbersSet.forEach(number => {
-      if(typeof number !== 'number' || Number.isNaN(number)) {
-        throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_IMPOSSIBLE);
-      }
-      if (!Number.isInteger(number)) {
-        throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_NOT_INTEGER);
-      }
-      if (number < LOTTO_CONFIG.MIN_NUMBER) {
-        throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_TOO_SMALL);
-      }
-      if (number > LOTTO_CONFIG.MAX_NUMBER) {
-        throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_TOO_LARGE);
-      }
+      if(typeof number !== 'number' || Number.isNaN(number)) throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_IMPOSSIBLE);
+      if (!Number.isInteger(number)) throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_NOT_INTEGER);
+      if (number < LOTTO_CONFIG.MIN_NUMBER) throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_TOO_SMALL);
+      if (number > LOTTO_CONFIG.MAX_NUMBER) throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_TOO_LARGE);
+
     });
   }
 
-  print(){
-    printString(`[${this.#numbers.join(', ')}]`);
-    }
-  // TODO: 추가 기능 구현
+  #getMatchingCount(winningLotto){
+    let count = 0;
+    this.#numbers.forEach(number => {
+      if(winningLotto.hasNumber(number)) {
+        count++;
+      }
+
+    })
+    return count;
+  }
 }
 
 export default Lotto;
