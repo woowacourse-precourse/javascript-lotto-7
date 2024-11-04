4cdf2b01ad179d14a83f8042517329d48611b5d4
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-11-04 18:42:02 +0900

    feat(constant): 에러 메시지 추가

--- src/constant.js
+++ src/constant.js
@@ -6,7 +6,8 @@ export const ERROR_MESSAGE = {
     '[ERROR] 로또 번호 6개를 쉼표로 구분하여 입력해야 합니다.',
   LOTTO_AMOUNT_UNIT_ERROR: '[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.',
   BONUS_NUM_NOT_UNIQUE_ERROR:
-    'ERROR] 보너스 번호는 당첨 번호에 포함되지 않아야 합니다.',
+    '[ERROR] 보너스 번호는 당첨 번호에 포함되지 않아야 합니다.',
+  LOTTO_NUM_NOT_UNIQUE_ERROR: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
 };
 
 export const INPUT_MESSAGE = {
