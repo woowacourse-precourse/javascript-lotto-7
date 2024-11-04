a6c91d468010bb506ef4cd745ca0fd1b7ec75be7
YJS96 <wnt9864@hotmail.com>
2024-11-04 21:38:39 +0900

    feat: 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      await this.#getPrice();
+      await this.#getWinNumber();
+      await this.#getBonusNumber();
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  async #getPrice() {
+    const input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    const price = Number(input);
+
+    if (isNaN(price) || price % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1000원 단위입니다.');
+    }
+
+    return price;
+  }
+
+  async #getWinNumber() {
+    const input = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+    const numbers = input.split(',').map(Number);
+
+    if (numbers.some(isNaN)) {
+      throw new Error('[ERROR] 당첨 번호는 숫자만 입력 가능합니다.');
+    }
+
+    return numbers;
+  }
+
+  async #getBonusNumber() {
+    const input = await Console.readLineAsync(
+      '\n보너스 번호를 입력해 주세요.\n'
+    );
+    const number = Number(input);
+
+    if (isNaN(number) || number < 1 || number > 45) {
+      throw new Error('[ERROR] 유효하지 않은 보너스 번호입니다.');
+    }
+
+    return number;
+  }
 }
 
 export default App;
