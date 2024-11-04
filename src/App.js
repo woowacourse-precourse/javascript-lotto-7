db5b51e1b88e71e67efedc91601c4f9c51db9f2a
AlgeMoya <kjhobd2@naver.com>
2024-11-04 18:19:31 +0900

    feat: 로또 구입 금액 검증 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ class App {
   async run() {
     Console.print("구입금액을 입력해 주세요.");
     let purchaseAmount = await Console.readLineAsync("");
+    if (isNaN(purchaseAmount)) {
+      throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
+    }
+    purchaseAmount = parseInt(purchaseAmount);
+    if (purchaseAmount < 1000) {
+      throw new Error("[ERROR] 최소 구매금액은 1000원입니다.");
+    }
+    if (purchaseAmount % 1000 !== 0) {
+      throw new Error("[ERROR] 1000원 단위로만 입력할 수 있습니다.");
+    }
   }
 }
 
