442f45bfa9297f08b6d1669b1d0a8760f182bb19
dlsxjzld <sinjuk1@naver.com>
2024-11-04 17:57:42 +0900

    refactor(validation/constant.js): 검증에 있는 상수들 모듈화

---
+++ src/validation/constants.js
@@ -0,0 +1,19 @@
+export const ERROR_MESSAGES = Object.freeze({
+  SPACE_ERROR: '공백이 포함되어 있습니다.',
+  EMPTY_STRING: '빈 문자열 입니다.',
+  NON_INTEGER: '숫자가 아닌 문자가 있습니다.',
+  MINIMUM_POSITIVE: '1000 이상의 양수만 가능합니다.',
+  DIVISIBLE_BY_1000: '1000의 배수만 가능합니다.',
+  FORMAT_ERROR: '올바른 형식으로 입력해주세요. ex) 1,2,3,4,5,6',
+  RANGE_ERROR: '1~45의 수만 가능합니다.',
+  DUPLICATE_ERROR: '중복되는 수는 안됩니다.',
+  BONUS_DUPLICATE_ERROR: '중복되는 보너스 수는 안됩니다.',
+});
+
+export const NUMBERS = Object.freeze({
+  ONE_LOTTO_PRICE: 1000,
+  NUMBERS_COUNT: 6,
+  MAX_RANGE: 45,
+  MIN_RANGE: 1,
+  SEPARATOR: ',',
+});
