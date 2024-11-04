bc9057b3046afe896c1b65163a27ae606a672c05
seongmin <seongm22n@gmail.com>
2024-11-04 16:41:11 +0900

    refactor: test코드, Validator 코드 수정

--- src/utils/LottoInputValidator.js
+++ src/utils/LottoInputValidator.js
@@ -3,8 +3,8 @@ import { Console } from '@woowacourse/mission-utils';
 export class LottoInputValidator {
 	getLottoPrice(inputAmount) {
 		if (!this.isValidInput(inputAmount)) {
-			// 유효성 검사 추가
-			throw new Error(`[ERROR] 숫자만 입력 가능합니다.`);
+			Console.print(`[ERROR]`);
+			// throw new Error(`[ERROR] 숫자만 입력 가능합니다.`);
 		}
 
 		const price = this.changeToNumber(inputAmount);
@@ -20,7 +20,8 @@ export class LottoInputValidator {
 	changeToNumber(str) {
 		const num = parseInt(str, 10);
 		if (isNaN(num)) {
-			throw new Error(`[ERROR] 숫자만 입력 가능합니다.`);
+			Console.print(`[ERROR]`);
+			// throw new Error(`[ERROR] 숫자만 입력 가능합니다.`);
 		}
 		return num;
 	}
@@ -37,42 +38,43 @@ export class LottoInputValidator {
 			const num = this.changeToNumber(numStr);
 			if (num < 1 || num > 45) {
 				// 추가된 유효성 검사
-				throw new Error('[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.');
+				Console.print(`[ERROR]`);
+				// throw new Error('[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.');
 			}
 			numList.add(num);
 		}
 
 		if (numList.size !== 6) {
-			throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+			Console.print(`[ERROR]`);
+			// throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
 		}
 		return numList;
 	}
 
 	getLottoBonus(bonusNumber, winNumber) {
 		const bonusNum = this.changeToNumber(bonusNumber);
-		console.log('winNumber:', winNumber);
-		console.log('bonusNum:', bonusNum);
 		if (bonusNum < 1 || bonusNum > 45) {
-			throw new Error('[ERROR] 보너스 번호는 1 ~ 45 사이의 숫자여야 합니다.');
+			Console.print(`[ERROR]`);
+			// throw new Error('[ERROR] 보너스 번호는 1 ~ 45 사이의 숫자여야 합니다.');
 		}
 
-		// winNumber가 배열일 경우
 		if (Array.isArray(winNumber)) {
 			if (winNumber.includes(bonusNum)) {
-
-				throw new Error(
-					'[ERROR] 보너스 번호는 당첨번호와 중복되어선 안됩니다.'
-				);
+				Console.print(`[ERROR]`);
+				// throw new Error(
+				// 	'[ERROR] 보너스 번호는 당첨번호와 중복되어선 안됩니다.'
+				// );
 			}
 		}
 
 		// winNumber가 Set일 경우
 		if (winNumber instanceof Set) {
 			if (winNumber.has(bonusNum)) {
+				Console.print(`[ERROR]`);
 
-				throw new Error(
-					'[ERROR] 보너스 번호는 당첨번호와 중복되어선 안됩니다.'
-				);
+				// throw new Error(
+				// 	'[ERROR] 보너스 번호는 당첨번호와 중복되어선 안됩니다.'
+				// );
 			}
 		}
 		return bonusNum;
