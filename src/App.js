6528b4af38f4b098a36285a90adc6261aab32e3e
Ryu1216 <r12dh16@gmail.com>
2024-11-04 13:32:54 +0900

    feat: Add printing and picking random lotto numbers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
   getPositivePrice(input) {
@@ -20,19 +21,29 @@ class App {
   async run() {
     let isValid = false;
     let purchasePrice;
+    let lottoQuantity;
+
     while (!isValid) {
       try {
         let purchasePrice = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
         purchasePrice = Number(this.getPositivePrice(purchasePrice));
-        const lottoQuantity = this.getThousandPrice(purchasePrice) / 1000;
+        lottoQuantity = this.getThousandPrice(purchasePrice) / 1000;
+        
         isValid = true;
       } catch (error) {
         MissionUtils.Console.print(error.message);
       }
     }
+    MissionUtils.Console.print(`\n${lottoQuantity}개를 구매했습니다.`)
+    let lottoArray = [];
 
-
-
+    for (let i = 0; i < lottoQuantity; i++) {
+      lottoArray.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+      MissionUtils.Console.print(`[${lottoArray[i]}]`);
+    }
+    const lottoNumber = await MissionUtils.Console.readLineAsync("\n당첨번호를 입력해 주세요.\n");
+    const bonusNumber = await MissionUtils.Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
+    
 
   }
 
