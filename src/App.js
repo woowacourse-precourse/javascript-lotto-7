7cfc33e60513da2087b9142a8ee85de137fc7f9c
parksiwoon <parksw003@gmail.com>
2024-11-04 18:58:08 +0900

    feat: Implement purchase amount input validation in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import LotteryMachine from "./LotteryMachine.js";
-import PrizeCalculator from "./PrizeCalculator.js";
-class App {
-  async run() {}
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
+class LotteryMachine {
+  constructor(purchaseAmount) {
+    this.purchaseAmount = purchaseAmount;
+  }
+
+  generateLottoTickets() {
+    const count = this.purchaseAmount / 1000;
+    const tickets = [];
+    for (let i = 0; i < count; i++) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      tickets.push(new Lotto(numbers));
+    }
+    return tickets;
+  }
 }
 
-export default App;
+export default LotteryMachine;
