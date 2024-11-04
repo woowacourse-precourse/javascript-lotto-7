c53ba963bff9ce5db5db58c47f8cc4ba6e927b89
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 23:16:19 +0900

    feat: LottoController.js 생성

---
+++ src/controller/LottoController.js
@@ -0,0 +1,9 @@
+import InputView from "../view/InputView.js";
+import OutputView from "../view/OutputView.js";
+import LottoGame from "../LottoGame.js";
+
+class LottoController {
+  
+}
+
+export default LottoController;
