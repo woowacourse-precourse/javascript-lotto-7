1a5bb4e539dad9310b32c86b174c26d4d13d7504
crolvlee <leecrolv@gmail.com>
2024-11-04 20:10:56 +0900

    feat: 수익률 출력

--- src/App.js
+++ src/App.js
@@ -68,7 +68,24 @@ function countPrizeResults(tickets, winningNumbers, bonusNumber) {
   return prizeResults;
 }
 
-function showStatistics(prizeResults) {
+function showStatistics(prizeResults, ticketCount) {
+  const prizeMoney = {
+    '3': 5_000,
+    '4': 50_000,
+    '5': 1_500_000,
+    '5+bonus': 30_000_000,
+    '6': 2_000_000_000
+  }
+
+  let totalMoney = 0;
+  for (const [key, value] of Object.entries(prizeResults)) {
+    totalMoney += value * prizeMoney[key]
+  }
+
+  const totalMoneySpent = ticketCount * 1000
+  const profitRate = Math.round((totalMoney / totalMoneySpent) * 100 * 10) / 10;
+
+
   Console.print('\n당첨 통계');
   Console.print('---');
   Console.print(`3개 일치 (5,000원) - ${prizeResults['3']}개`);
@@ -76,4 +93,5 @@ function showStatistics(prizeResults) {
   Console.print(`5개 일치 (1,500,000원) - ${prizeResults['5']}개`);
   Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${prizeResults['5+bonus']}개`);
   Console.print(`6개 일치 (2,000,000,000원) - ${prizeResults['6']}개`);
+  Console.print(`총 수익률은 ${profitRate}%입니다.`);
 }
