a2e74b3309a3faeb55edb5bd0e9b32a8a3091ea0
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 20:34:31 +0900

    refactor: 에러 발생 시 다음 입력에 대한 줄바꿈을 위한 줄바꿈 문자 추가

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,7 +1,7 @@
 import ERROR_MESSAGES from '../constants/errorMessages.js';
 
 const handleError = (message) => {
-  throw new Error(`${ERROR_MESSAGES.PREFIX} ${message}`);
+  throw new Error(`${ERROR_MESSAGES.PREFIX} ${message}\n`);
 };
 
 export default handleError;
