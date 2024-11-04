53b1e5ed58d8847dc2e48bbe7a0fb7bee306f14c
nerdchanii <nerdchanii@gmail.com>
2024-11-04 11:48:31 +0900

    feat(model): implement WInningNumbers

--- src/model/WinningNumbers.js
+++ src/model/WinningNumbers.js
@@ -11,9 +11,16 @@ class WinningNumbers {
     this.#bonus = bonus;
   }
 
-  #validate (bonus) {}
+  #validate (bonus) {
+    const lotto = new Lotto(lotto);
+    if (lotto.has(bonus)) {
+      throw new Error('[ERROR] 당첨번호에 포함된 숫자는 보너스 숫자로 입력할 수 없습니다.');
+    }
+  }
 
   test(lotto) {
-
+    const matched = intersect(lotto.numbers, [this.#winningLotto.numbers]);
+    const bonusMatch = lotto.has(this.#bonus);
+    return { matched, bonusMatch };
   }
 }
