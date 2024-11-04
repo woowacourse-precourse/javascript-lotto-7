e36bb7360c28c67f39be65073bdd346fdec543c1
seongmin <seongm22n@gmail.com>
2024-11-04 21:12:08 +0900

    refactor: 가독성있게 수정

--- src/App.js
+++ src/App.js
@@ -6,55 +6,14 @@ import { LottoInputValidator } from './utils/LottoInputValidator.js';
 class App {
 	async run() {
 		const inputHandler = new LottoInputHandler();
-		const validator = new LottoInputValidator();
 		const lottoHandler = new LottoHandler();
 
-		// 구입금액 입력 및 검증
-		let lottoCount;
-		while (true) {
-			const inputAmount = await Console.readLineAsync(
-				'구입금액을 입력해 주세요.\n'
-			);
-
-			try {
-				lottoCount = validator.getLottoPrice(inputAmount); // 유효성 검사 호출
-				break; // 유효한 입력을 받으면 루프 종료
-			} catch (error) {
-				// 에러 메시지를 Console에 출력하고 루프를 계속하여 재입력 요청
-				Console.print(error.message);
-				// 다시 입력을 요청하므로 아무 동작도 하지 않고 while 루프가 반복됨
-			}
-		}
+		const lottoCount = await inputHandler.getLottoCount();
 
 		const lottos = lottoHandler.buyLotto(lottoCount);
 
-		// 당첨 번호 입력 및 검증
-		let winNumber;
-		while (true) {
-			winNumber = await Console.readLineAsync(`\n당첨 번호를 입력해 주세요.\n`);
-
-			try {
-				inputHandler.getWinningNumbers(winNumber);
-				break;
-			} catch (error) {
-				Console.print(error.message);
-			}
-		}
-
-		// 보너스 번호 입력 및 검증
-		let bonusNumber;
-		while (true) {
-			bonusNumber = await Console.readLineAsync(
-				`\n보너스 번호를 입력해 주세요.\n`
-			);
-
-			try {
-				inputHandler.getBonusNumber(bonusNumber, winNumber);
-				break;
-			} catch (error) {
-				Console.print(error.message);
-			}
-		}
+		const winNumber = await inputHandler.getWinningNumbers();
+		const bonusNumber = await inputHandler.getBonusNumber(winNumber);
 
 		Console.print('\n당첨 통계\n---');
 		const winMoney = lottoHandler.checkLottoWin(lottos, winNumber, bonusNumber);
--- src/LottoInputHandler.js
+++ src/LottoInputHandler.js
@@ -1,16 +1,50 @@
+import { Console } from '@woowacourse/mission-utils';
 import { LottoInputValidator } from './utils/LottoInputValidator.js';
 
-const validator = new LottoInputValidator();
-
 export class LottoInputHandler {
-	// 당첨 번호
-	getWinningNumbers(winNumbers) {
-		const winNumber = validator.getLottoWinner(winNumbers);
-		return winNumber;
+	constructor() {
+		this.validator = new LottoInputValidator();
 	}
-	//보너스 번호
-	getBonusNumber(bonusNumber, winNumber) {
-		const bonusNum = validator.getLottoBonus(bonusNumber, winNumber);
-		return bonusNum;
+	async getLottoCount() {
+		let lottoCount;
+		while (true) {
+			const inputAmount = await Console.readLineAsync(
+				`구입금액을 입력해 주세요.\n`
+			);
+			try {
+				lottoCount = this.validator.getLottoPrice(inputAmount);
+				return lottoCount;
+			} catch (error) {
+				Console.print(error.message);
+			}
+		}
+	}
+
+	async getWinningNumbers() {
+		let winNumber;
+		while (true) {
+			winNumber = await Console.readLineAsync(`\n당첨 번호를 입력해 주세요.\n`);
+
+			try {
+				return this.validator.getLottoWinner(winNumber);
+			} catch (error) {
+				Console.print(error.message);
+			}
+		}
+	}
+
+	async getBonusNumber(winNumber) {
+		let bonusNumber;
+		while (true) {
+			bonusNumber = await Console.readLineAsync(
+				`\n보너스 번호를 입력해 주세요.\n`
+			);
+
+			try {
+				return this.validator.getLottoBonus(bonusNumber, winNumber);
+			} catch (error) {
+				Console.print(error.message);
+			}
+		}
 	}
 }
