2c35fc524ed9c0feba209716b1f419b5409affdf
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-11-04 17:40:08 +0900

    refactor: 숫자로만 이루어진지 정규식검사로 변경
    - validateNoDecimal : 소수점있는지 검사하는 로직 삭제
    - validateIsInteger : 정수인지 검사하는 로직 삭제
    - validateIsPositive : 양수인지 검사하는 로직 삭제

--- src/utils/NumberValidator.js
+++ src/utils/NumberValidator.js
@@ -7,27 +7,9 @@ class NumberValidator {
     }
   }
 
-  static validateIsNumber(value) {
-    if (Number.isNaN(parseFloat(value))) {
+  static validateIsOnlyDigits(value) {
+    if (!/^\d+$/.test(value)) {
       throw new Error(ERROR_MESSAGES.NOT_NUMBER);
-    }
-  }
-
-  static validateNoDecimal(value) {
-    if (value.toString().includes('.')) {
-      throw new Error(ERROR_MESSAGES.INCLUDE_DECIMAL);
-    }
-  }
-
-  static validateIsInteger(value) {
-    if (!Number.isInteger(value)) {
-      throw new Error(ERROR_MESSAGES.NOT_INTEGER);
-    }
-  }
-
-  static validateIsPositive(value) {
-    if (value <= 0) {
-      throw new Error(ERROR_MESSAGES.NOT_POSITIVE);
     }
   }
 }
