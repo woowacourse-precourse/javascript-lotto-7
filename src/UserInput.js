ae1240a4dd9d0af700abe82fa01a0f6b3f3917ce
syg0629 <syg0629@hanmail.net>
2024-11-04 23:57:12 +0900

    style: 오타 수정

--- src/UserInput.js
+++ src/UserInput.js
@@ -3,7 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 const INPUT_MESSAGE = {
   purchaseAmount: "구입금액을 입력해 주세요.\n",
   winningNumber: "\n당첨 번호를 입력해 주세요.\n",
-  bonusNummber: "\n보너스 번호를 입력해 주세요.\n",
+  bonusNumber: "\n보너스 번호를 입력해 주세요.\n",
 };
 
 class UserInput {
