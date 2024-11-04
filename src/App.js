2a1a79830d908f9817931e43269ec652f3b9efe0
ekkk1126 <cabi0203@naver.com>
2024-11-04 17:42:14 +0900

    feat: 로또 게임 App 시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from '@woowacourse/mission-utils';
+import { inputBonusNum, inputBuyCash, inputWinningNums } from './console/input.js';
+import { IO_MESSAGE, TEXT_LOTTO_RESULT } from './constants/message.js';
+import Game from './Game.js';
+import { getPurchaseNums } from './utils/getPurchaseNums.js';
+import Rate from './utils/rate.js';
+
 class App {
-  async run() {}
+  async run() {
+    const cash = await inputBuyCash();
+    const count = cash / 1000;
+    Console.print(`\n${count}${IO_MESSAGE.INPUT_ALARM_BUY}`);
+
+    let lottoNums = getPurchaseNums(count);
+    lottoNums.forEach((val) => {
+      Console.print(`[${val.join(', ')}]`);
+    });
+
+    const winningNumbers = await inputWinningNums();
+
+    const bonusNumber = await inputBonusNum(winningNumbers);
+
+    const results = Game(lottoNums);
+
+    Console.print(IO_MESSAGE.OUTPUT_TEXT);
+    Console.print(TEXT_LOTTO_RESULT());
+    Rate();
+  }
 }
 
 export default App;
