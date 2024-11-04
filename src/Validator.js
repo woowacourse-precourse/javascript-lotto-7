97eb6b3339dbe6d687a7441f8696861fdb839e62
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-04 18:43:31 +0900

    feat: 보너스 번호 검증 메서드 추가

--- src/Validator.js
+++ src/Validator.js
@@ -6,6 +6,7 @@ import { LOTTO } from './constants/LOTTO_CONSTANTS.js';
 import { hasWhitespace } from './utils/hasWhitespace.js';
 import { isEmptyString } from './utils/isEmptyString.js';
 import { isNaturalNumber } from './utils/isNaturalNumber.js';
+import { stringToNumber } from './utils/stringToNumber.js';
 
 class Validator {
   static validateWhitespace(input) {
@@ -33,6 +34,13 @@ class Validator {
     ) {
       throw new Error(LOTTO_NUMBER_ERROR_MESSAGES.LOTTO_NUMBER_OUT_OF_RANGE);
     }
+  }
+
+  static validateBonusNumber(input) {
+    this.validateNaturalNumber(input);
+    const bonusNumber = stringToNumber(input);
+    this.validateLottoNumberRange(bonusNumber);
+    return bonusNumber;
   }
 }
 
