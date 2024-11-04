363f0b92f3438141ce3eae192e0d27c08853648a
keemsebeen <sebin0580@gmail.com>
2024-11-04 13:39:01 +0900

    chore(Lotto) : 불필요한 import 삭제

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,6 +1,3 @@
-import { ERROR_MESSAGE, LOTTO_ERROR } from "../constants/error.js";
-import { DELIMITER } from "../constants/message.js";
-import { DELIMITER_REGEX } from "../utils/regex.js";
 import LottoValidate from "../validation/LottoValidate.js";
 
 class Lotto {
