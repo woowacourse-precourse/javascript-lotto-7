deeae4dd9e67799483774e3e4483dd624122b2a3
Lim-shweety <smile70200@naver.com>
2024-11-04 16:02:55 +0900

    feat App.js : 구입 금액 만큼 로또 생성하기
    로또가 중복되는 것이 없도록 하고, 정렬함

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import { Console, Random } from "@woowacourse/mission-utils"
+import { Console, MissionUtils, Random } from "@woowacourse/mission-utils"
+
 class App {
   async run() {
     const USER_MONEY = await this.getMoney();
+    const USER_LOTTO = this.makeUserLotto(USER_MONEY);
 
   }
 
-  async getMoney(){
+  async getMoney() {
     let inputMoney = await Console.readLineAsync("구입금액을 입력해 주세요.");
     return this.checkMoney(Number(inputMoney));
   }
 
-  checkMoney(money){
-    if(isNaN(money)){
+  checkMoney(money) {
+    if (isNaN(money)) {
       throw new Error("[ERROR] 입력된 값이 숫자가 아닙니다.");
     }
-    if(money%1000!==0){
+    if (money % 1000 !== 0) {
       throw new Error("[ERROR] 로또 구입금액이 1000원 단위가 아닙니다.")
     }
     return money;
   }
+
+  makeUserLotto(money) {
+    let count = Math.floor(money / 1000);
+    let userLotto = []
+    for (let i = 0; i < count; i++) {
+      let oneLotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      while (userLotto.some(lotto => JSON.stringify(lotto) === JSON.stringify(oneLotto))) {
+        oneLotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      }
+      userLotto.push(oneLotto);
+    }
+    return userLotto;
+  }
+
+
 }
 
 export default App;
