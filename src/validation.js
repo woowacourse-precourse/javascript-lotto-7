1113616808ba89cad4f4e237665d1c7c05994e78
taeyomi <terry11ty@naver.com>
2024-11-04 22:57:18 +0900

    fix: import 에러 수정

--- src/validation.js
+++ src/validation.js
@@ -1,7 +1,6 @@
 import ERROR_MESSAGE from "./constants/error.js";
 
 export const validatePurchasingAmount = (userInputAmounts) => {
-  // 입력이 숫자가 아닌 경우, 또는 1000으로 나누어 떨어지지 않는 경우, 또는 음수나 0인 경우 예외 발생
   if (
     !/^\d+$/.test(userInputAmounts) ||
     userInputAmounts <= 0 ||
