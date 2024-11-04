c37e831817c29dd1a0e190ec2d61f63b470d1458
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-11-04 15:23:57 +0900

    test: validationLotto의 winningNumbers 함수 테스트 추가
    - 당첨 번호가 올바른 정수 문자열일 때 오류가 발생하지 않는지 테스트
    - 당첨 번호에 숫자가 아닌 값이 포함될 경우 오류를 발생시키는지 테스트

--- __tests__/validations/validationLottoTest.js
+++ __tests__/validations/validationLottoTest.js
@@ -17,4 +17,28 @@ describe('validationLotto 테스트', () => {
       );
     });
   });
+
+  describe('winningNumbers 함수', () => {
+    test('당첨 번호가 올바른 정수 문자열로 주어질 때 오류가 발생하지 않는지 테스트', () => {
+      expect(() => validationLotto.winningNumbers('1,2,3,4,5,6')).not.toThrow();
+    });
+
+    test('당첨 번호에 숫자가 아닌 값이 포함되면 오류를 발생시키는지 테스트', () => {
+      expect(() => validationLotto.winningNumbers(' ,2,3,4,5,6')).toThrow(
+        ERROR_MESSAGES.INVALID_NUMBER
+      );
+      expect(() => validationLotto.winningNumbers('-1,2,3,4,5,6')).toThrow(
+        ERROR_MESSAGES.INVALID_NUMBER
+      );
+      expect(() => validationLotto.winningNumbers('1.1,2,3,4,5,6')).toThrow(
+        ERROR_MESSAGES.INVALID_NUMBER
+      );
+      expect(() => validationLotto.winningNumbers('1e2,2,3,4,5,6')).toThrow(
+        ERROR_MESSAGES.INVALID_NUMBER
+      );
+      expect(() => validationLotto.winningNumbers('a,2,3,4,5,6')).toThrow(
+        ERROR_MESSAGES.INVALID_NUMBER
+      );
+    });
+  });
 });
