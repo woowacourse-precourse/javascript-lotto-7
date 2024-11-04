40a4884de4ca273f40baa09fd78d9e07291120b1
deokbin <deokbin@naver.com>
2024-11-04 09:41:45 +0900

    feat(App): 사용자 입력값 처리 변수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { InputService } from './service/InputService.js';
+
 class App {
-  async run() {}
+  async run() {
+    const purchasedLottoInfo = await InputService.getPurchasedLotto();
+    const winningLotto = await InputService.getWinningNumbers();
+    const bonusNum = await InputService.getBonusNumber(winningLotto);
+  }
 }
 
 export default App;
