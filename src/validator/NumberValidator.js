d6bc7ad0109b44cee1c805d6f172785ff6e7de56
dudcns02 <duddl2460@naver.com>
2024-11-04 23:04:25 +0900

    chore: Import error message and add export

--- src/validator/NumberValidator.js
+++ src/validator/NumberValidator.js
@@ -1,18 +1,20 @@
+import { NUMBER_INPUT_ERROR_MESSAGE } from "../Constants.js";
+
 ///당첨번호 유효성
-function CheckWinningNumberInput() {
+export function CheckWinningNumberInput() {
   if (!/^[0-9]+(,\s*[0-9]+)*$/.test(winningNumber)) {
     throw new Error(NUMBER_INPUT_ERROR_MESSAGE.ONLY_NUMBER_COMMA);
   }
 }
 
-function isWinningNumberLength() {
+export function isWinningNumberLength() {
   const MAX_WINNING_NUMBER_LENGTH = 6;
   if (winningNumber.length !== MAX_WINNING_NUMBER_LENGTH) {
     throw new Error(NUMBER_INPUT_ERROR_MESSAGE.INCORRECT_NUMBER_COUNT);
   }
 }
 
-function checkNumberRange() {
+export function checkNumberRange() {
   // 보너스 번호, 당첨 번호 범위도 여기서 함
   const MIN_NUMBER = 1;
   const MAX_NUMBER = 45;
@@ -21,7 +23,7 @@ function checkNumberRange() {
   }
 }
 
-function isDuplicateNumber() {
+export function isDuplicateNumber() {
   checkduplicate = new set(winningNumber);
   if (checkduplicate.size !== winningNumber.length) {
     throw new Error(NUMBER_INPUT_ERROR_MESSAGE.DUPLICATE_NUMBER);
@@ -29,7 +31,7 @@ function isDuplicateNumber() {
 }
 
 // 보너스 번호 유효성 검사
-function isDuplicateBonusNumber() {
+export function isDuplicateBonusNumber() {
   if (winningNumber.includes(bonusNumber)) {
     throw new Error(NUMBER_INPUT_ERROR_MESSAGE.DUPLICATE_BONUS_NUMBER);
   }
