e2964b3ac1c352237b1c9a6dc53eadf139ac92a5
조승우 <choseung97@gmail.com>
2024-11-04 16:14:08 +0900

    feat(validator): 당첨 번호 내 중복되는 수가 존재할 때 출력하는 에러 메시지를 추가한다

--- src/resources/WINNINGNUMBERSERROR.js
+++ src/resources/WINNINGNUMBERSERROR.js
@@ -1,7 +1,10 @@
 export default {
-  NOT_TYPED: '[ERROR] 당첨 번호가 입력되지 않았어요. 당첨 번호를 입력해주세요.',
+  NOT_TYPED:
+    '[ERROR] 당첨 번호가 입력되지 않았어요. 당첨 번호를 입력해 주세요.',
   INVALID_DELIMITER_SPLIT:
-    '[ERROR] 올바르지 않은 형식이에요. 숫자 여섯 개를 쉼표를 기준으로 구분하여 입력해주세요.',
+    '[ERROR] 올바르지 않은 형식이에요. 숫자 여섯 개를 쉼표를 기준으로 구분하여 입력해 주세요.',
   OVER_NUMBER_RANGE:
     '[ERROR] 1과 45 범위 밖의 정수가 존재해요. 범위 내의 정수로 당첨 번호를 구성해 주세요.',
+  DUPLICATED_NUMBER:
+    '[ERROR] 당첨 번호에 중복되는 수가 있어요. 중복되지 않는 당첨 번호를 입력해 주세요.',
 };
