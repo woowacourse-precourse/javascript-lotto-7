7067d5e3c3a1f0c17077c3c63436f4ab1dc35260
Castillou <joajoa70584@gmail.com>
2024-11-04 15:30:01 +0900

    feat(controller): add function to generate lottos with amount

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,33 +1,49 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import InputView from '../views/InputView.js';
 import ValidatePurchaseAmount from '../models/ValidatePurchaseAmount.js';
+import Lotto from '../Lotto.js';
 
 class LottoController {
 	#inputView;
-
-	#validatePurchaseAmount;
-
+	#outputView;
+	#lottos;
+	#winningLotto;
 	#purchaseAmount;
+	#bonusNumber;
+	#validatePurchaseAmount;
 
 	constructor() {
 		this.#inputView = new InputView();
+		this.#lottos = [];
+		this.#winningLotto = null;
 		this.#purchaseAmount = 0;
 		this.#validatePurchaseAmount = new ValidatePurchaseAmount();
 	}
 
 	async play() {
 		try {
-			await this.handlePurchase();
-			Console.print(this.#purchaseAmount);
+			await this.#handlePurchase();
+			Console.print(this.#lottos);
 		} catch (error) {
 			Console.print(error.message);
 		}
 	}
 
-	async handlePurchase() {
+	async #handlePurchase() {
 		const amount = await this.#inputView.readAmountInput();
 		this.#validatePurchaseAmount.validate(amount);
 		this.#purchaseAmount = amount;
+		this.#lottos = this.#generateLottos(amount);
+	}
+
+	#generateLottos(amount) {
+		const quantity = Math.floor(amount / 1000);
+		return Array.from({ length: quantity }, () => this.#createLotto());
+	}
+
+	#createLotto() {
+		const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+		return new Lotto(numbers);
 	}
 }
 
