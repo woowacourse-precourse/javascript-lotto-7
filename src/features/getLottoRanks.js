f233f70100e3a8594631fff62b9d8e0580475f77
Jaeho Cha <ischar@naver.com>
2024-11-04 21:39:49 +0900

    refactor(lotto): update getLottoRanks to add condition
    - getLottoRanks 수정
      - rank가 "BONUS"가 아니고, 2개 이하일 경우 continue

--- src/features/getLottoRanks.js
+++ src/features/getLottoRanks.js
@@ -1,3 +1,4 @@
+import { BONUS } from "../config/config.js";
 import { checkLotto } from "./checkLotto.js";
 
 export const getLottoRanks = (lottoList, winningNumbers, bonusNumber) => {
@@ -5,7 +6,7 @@ export const getLottoRanks = (lottoList, winningNumbers, bonusNumber) => {
 
   for (const lotto of lottoList) {
     const rank = checkLotto(lotto.getNumbers(), winningNumbers, bonusNumber);
-    if (rank <= 2) {
+    if (rank !== BONUS && rank <= 2) {
       continue;
     }
     if (!lottoRankMap.has(rank)) {
