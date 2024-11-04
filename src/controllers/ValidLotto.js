cef563b42641bff3eea1b308698f8bba0ba1c6fd
yeeun426 <thsudkcla7@naver.com>
2024-11-04 18:18:28 +0900

    feat : 로또 구입 금액이 1000원 단위가 아닌 경우 예외 처리

--- src/controllers/ValidLotto.js
+++ src/controllers/ValidLotto.js
@@ -1,7 +1,7 @@
 class ValidLotto {
   validLottoPrice(lottoPrice) {
     const validPrice = Number(lottoPrice);
-    if (isNaN(validPrice)) {
+    if (isNaN(validPrice) || validPrice % 1000 !== 0) {
       throw new Error(
         "[ERROR] 로또 구입 금액은 1,000원 단위의 숫자여야 합니다."
       );
