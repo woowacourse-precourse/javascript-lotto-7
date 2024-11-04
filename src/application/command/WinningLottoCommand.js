4f7c0069d1f03965c7e125eff71af3af4bab73df
rheefine <rheefine@gmail.com>
2024-11-04 16:31:25 +0900

    feat: 당첨 번호를 입력 받아 도메인에 전달하는 기능

---
+++ src/application/command/WinningLottoCommand.js
@@ -0,0 +1,32 @@
+import WinningLotto from "../../domain/WinningLotto/WinningLotto.js";
+import retry from "../utils/retry.js";
+
+class WinningLottoCommand {
+  #inputPort;
+  #validator;
+
+  constructor(inputPort, validator) {
+    this.#inputPort = inputPort;
+    this.#validator = validator;
+  }
+
+  async execute() {
+    const winningLotto = await this.handleWinningLotto();
+
+    return winningLotto;
+  }
+
+  async handleWinningLotto() {
+    const handleWinnigLottoFlow = async() => {
+      const numbers = await this.#inputPort.readWinningLotto();
+      const parsedNumbers = numbers.split(",");
+      parsedNumbers.forEach((number) => this.#validator.validate(number));
+      return WinningLotto.create(parsedNumbers);
+    };
+
+    return retry(handleWinnigLottoFlow);
+  }
+
+}
+
+export default WinningLottoCommand;
