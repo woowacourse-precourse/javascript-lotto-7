1951a9ca9c43c7faf745c1c2c15a5c5dba379f09
hamo-o <yheel369@gmail.com>
2024-11-04 14:50:09 +0900

    style(OutputView): 개행 통일

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -20,6 +20,7 @@ const OutputView = {
   printLottoCount(count) {
     MissionUtils.Console.print(`\n${count}개를 구매했습니다.`);
   },
+
   /**
    *
    * @param {number[][]} lottos
@@ -30,21 +31,27 @@ const OutputView = {
     });
     OutputView.printNewLine();
   },
+
   printNewLine() {
     MissionUtils.Console.print("");
   },
+
   printResultHeader() {
     MissionUtils.Console.print("당첨 통계\n---");
   },
+
   printResult(condition, money, count) {
     MissionUtils.Console.print(`${condition}개 일치 (${money}원) - ${count}개`);
   },
+
   printBonusResult(condition, money, count) {
     MissionUtils.Console.print(`${condition}개 일치, 보너스 볼 일치 (${money}원) - ${count}개`);
   },
+
   printReturnRate(rate) {
     MissionUtils.Console.print(`총 수익률은 ${rate}%입니다.`);
   },
+
   printError(message) {
     MissionUtils.Console.print(message);
   },
