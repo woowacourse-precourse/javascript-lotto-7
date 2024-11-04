ac8be8c2e900f11cf25a951349bdde439758e975
kimgho <kgh67811@naver.com>
2024-11-04 15:42:09 +0900

    feat(test-lotto): 로또 테스트 작성
    - 로또 번호에 대한 테스트 작성

---
+++ __tests__/entities/LottoTest.js
@@ -0,0 +1,15 @@
+import Lotto from '../../src/entities/Lotto.js';
+
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow('[ERROR]');
+  });
+});
