a25b7d466f76235e59dd18707e7381456b705edb
taeyomi <terry11ty@naver.com>
2024-11-04 11:47:03 +0900

    feat(statistics): 당첨통계 상수파일 생성 및 메세지 추가

---
+++ src/constants/statistics.js
@@ -0,0 +1,12 @@
+const STATISTICS_MESSAGE = Object.freeze({
+  STATISTICS_DEFAULT_MESSAGE: "당첨 통계\n---",
+  FIFTH: "3개 일치 (5,000원) - ",
+  FOURTH: "4개 일치 (50,000원) - ",
+  THIRD: "5개 일치 (1,500,000원) - ",
+  SECOND: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  FIRST: "6개 일치 (2,000,000,000원) - ",
+  RATE_OF_RETURN: "총 수익률은 ",
+  PERCENT: "%입니다.",
+});
+
+export default STATISTICS_MESSAGE;
