5c1dbf6519f368f1d425022a10022ab4e68a2f9f
mynameSumin <msm4167@naver.com>
2024-11-04 20:38:33 +0900

    fix(message.js): 상수 메세지 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -6,13 +6,17 @@ const INPUT_MESSAGE = Object.freeze({
 
 const OUTPUT_MESSAGE = Object.freeze({
   BUYING_COUNT: (count) => `\n${count}개를 구매했습니다.`,
-  THREE_MATCH: '3개 일치 (5,000원) - ',
-  FOUR_MATCH: '4개 일치 (50,000원) - ',
-  FIVE_MATCH: '5개 일치 (1,500,000원) - ',
-  FIVE_MATCH_WITH_BONUS: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
-  SIX_MATCH: '6개 일치 (2,000,000,000원) - ',
+  WINNING_RESULT_TITLE: '\n당첨 통계\n---',
+  PERCENTAGE: (percent) => `총 수익률은 ${percent}%입니다.`
 });
 
-const MESSAGE = Object.freeze({ INPUT_MESSAGE, OUTPUT_MESSAGE });
+const RESULT = Object.freeze({
+  THREE_MATCH: (count) => `3개 일치 (5,000원) - ${count}개`,
+  FOUR_MATCH: (count) => `4개 일치 (50,000원) - ${count}개`,
+  FIVE_MATCH: (count) => `5개 일치 (1,500,000원) - ${count}개`,
+  FIVE_MATCH_WITH_BONUS: (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+  SIX_MATCH: (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
+});
+const MESSAGE = Object.freeze({ INPUT_MESSAGE, OUTPUT_MESSAGE, RESULT });
 
 export default MESSAGE;
