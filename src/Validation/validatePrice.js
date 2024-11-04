2a1caba8743a2df0253cbac01cb17666865b9928
BaeJia <qowldk1030@gmail.com>
2024-11-04 15:31:48 +0900

    refactor: validatePrice 각각의 유효성 검사 함수 분리

--- src/Validation/validatePrice.js
+++ src/Validation/validatePrice.js
@@ -1,18 +1,31 @@
 import { ERROR_MESSAGES, throwError } from "../Error/Error.js";
 
-function validatePrice(price) {
+// 입력이 비어있는지 확인
+function checkIfEmpty(price) {
   if (price === undefined || price === null || price === "") {
     throwError(ERROR_MESSAGES.price.NOT_ENTERED);
   }
+}
+
+// 숫자인지 확인
+function checkIfNumeric(price) {
   if (isNaN(price)) {
     throwError(ERROR_MESSAGES.price.MUST_BE_NUMERIC);
   }
+}
 
-  // 숫자가 1,000원 단위로 나누어 떨어지지 않는 경우 예외 처리
+// 1,000원 단위로 나누어 떨어지는지 확인
+function checkIfDivisibleByThousand(price) {
   if (price % 1000 !== 0) {
     throwError(ERROR_MESSAGES.price.MUST_BE_DIVISIBLE_BY_1000);
   }
+}
 
+// 전체 가격 유효성 검사 함수
+function validatePrice(price) {
+  checkIfEmpty(price);
+  checkIfNumeric(price);
+  checkIfDivisibleByThousand(price);
   return price;
 }
 
