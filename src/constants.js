0d868e01079d67e3c9608e9c6365be81ed0031be
mywhzy <cmj2393a@naver.com>
2024-11-04 10:28:37 +0900

    feat: 로또 한 장 금액 및 번호 구성 조건 상수화

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,11 @@ export const ERROR_MESSAGE = {
   prefix: '[ERROR]',
   purchaseAmountError: '로또 구입은 최소 1,000원부터 시작해 1,000원 단위로만 구매 가능합니다.',
 };
+
+export const LOTTO_SINGLE_TICKET_PRICE = 1000;
+
+export const LOTTO_NUMBERS_CONDITION = {
+  count: 6,
+  minNumber: 1,
+  maxNumber: 45,
+};
