a5ebeeac4293776119fab4c857d82753acbc4f91
jiHeeFlee <esder1310@naver.com>
2024-11-04 23:14:38 +0900

    feat(App.js/Lotto.js) : 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import Inputs from "./utils/Inputs";
+import Prints from "./utils/Prints";
+import Calculate from "./utils/Calculate";
+// import Random from "./utils/Random";
+
+import match_results from './constants/MatchResult';
+
 class App {
-  async run() {}
+  async run() {
+    const input_function = new Inputs();
+    const calculate_function = new Calculate();
+    const print_function = new Prints();
+
+    const money=input_function.input_money();
+    const lotto_count=calculate_function.buy_lotto(money);
+    const buy_lotto_list=RandomNumber.make_random_number(lotto_count);
+    const winning_number=input_function.input_winning_number();
+    const bonus_number=input_function.input_bonus_number();
+    const match_count=calculate_function.match_lotto(buy_lotto_list,winning_number,bonus_number);
+    const final_result=print_function.print_winning_result(match_count);
+    const total_prize=calculate_function.total_prize(match_count,match_results);
+    const total_rate=calculate_function.total_rate(money,total_prize)
+    Console.print(`총 수익률은 ${total_rate}입니다`);
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,11 @@
+import { random_number_count } from "./constants/RandomNumbers";
+import { input_error_unique} from '../constants/Messages';
+import { make_random_number_constant } from '../constants/CalculateNumbers';
+import { input_error_length_not_6,
+    input_error_unique,
+    input_error_number_range } from '../constants/Messages';
+
+
 class Lotto {
   #numbers;
 
@@ -7,12 +15,31 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.#check_count_input(numbers);
+    this.#check_input_unique(numbers);
+    this.#check_input_number_range(numbers);
   }
 
   // TODO: 추가 기능 구현
+  #check_count_input(numbers) {
+    if(!Array.isArray(numbers)){
+        throw new Error('[ERROR] 입력값은 배열이어야함')
+    }
+    if (numbers.length !== make_random_number_constant) {
+        throw new Error(input_error_length_not_6);
+    }
+  }
+  #check_input_unique(numbers){
+    const is_unique = new Set(numbers);
+    if(is_unique.size!==numbers.length){
+        throw new Error(input_error_unique)
+    }
+  }
+  #check_input_number_range(numbers){
+    if(numbers.some(num => num > 46 || num < 1)){
+        throw new Error(input_error_number_range)
+    }
+  }
 }
 
 export default Lotto;
