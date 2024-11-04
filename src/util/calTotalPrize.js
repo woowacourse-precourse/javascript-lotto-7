586ecfe2089c66197aeefd5f7ad608e0a70a2462
deokbin <deokbin@naver.com>
2024-11-04 09:46:02 +0900

    feat(calTotalPrize): 총 수상 금액 계산 함수 추가

---
+++ src/util/calTotalPrize.js
@@ -0,0 +1,15 @@
+import { MagicNumber } from '../constants/MagicNumber.js';
+
+export function calTotalPrize(matchCounts) {
+  const prizeMapping = {
+    MATCH_3: MagicNumber.MATCH_3_PRIZE,
+    MATCH_4: MagicNumber.MATCH_4_PRIZE,
+    MATCH_5: MagicNumber.MATCH_5_PRIZE,
+    MATCH_5_BONUS: MagicNumber.MATCH_5_BONUS_PRIZE,
+    MATCH_6: MagicNumber.MATCH_6_PRIZE,
+  };
+
+  return Object.entries(matchCounts).reduce((totalPrize, [key, count]) => {
+    return totalPrize + prizeMapping[key] * count;
+  }, 0);
+}
