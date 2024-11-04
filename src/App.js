10474533d26ca69e803c20255bf5bdb79040e0b4
이창준 <2018110021@dongguk.edu>
2024-11-04 16:18:50 +0900

    feat(App): 사용자 입출력 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const userPay = await this.getUserPay(); // await 추가
+      const lottoCount = this.calculateLottoCount(userPay);
+
+      Console.print(`${lottoCount}개를 구매했습니다.`);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  /**
+   * @description 사용자로부터 구매 금액을 입력받는다.
+   * @returns {number} - 검증된 구입 금액
+   */
+  async getUserPay() {
+    const amountInput = await Console.readLineAsync("구입금액을 입력해 주세요.\n"); // await 추가
+    const amount = parseInt(amountInput, 10);
+
+    return amount;
+  }
+
+  /**
+   * @description 로또 개수를 계산하는 함수
+   * @param {number} amount - 유효한 구입 금액
+   * @returns {number} 구입 금액에 따른 로또 개수
+   */
+  calculateLottoCount(amount) {
+    return Math.floor(amount / 1000);
+  }
 }
 
 export default App;
