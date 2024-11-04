ed761accae9bc2ac6f32c575ae9a0b104731949c
nerdchanii <nerdchanii@gmail.com>
2024-11-04 18:29:38 +0900

    feat(constants): seperates constants

--- src/constants/lotto.js
+++ src/constants/lotto.js
@@ -1,7 +1,39 @@
 export const LOTTO = Object.freeze({
+  SEPARATOR: ',',
+  NO_LUCK_RANK: 0,
   PRICE: 1000,
   NUMBER_RANGE: Object.freeze({
-  MIN: 1,
-  MAX: 45,
+    MIN: 1,
+    MAX: 45,
   }),
 });
+
+export const WIN_WITH_BONUS_RANK = 2;
+export const NEED_TO_CHHECK_BONUS_RANK = 3;
+export const UPGRADE_RANK_NUMBER = -1;
+
+export const PRIZE_TUPLE = Object.freeze([
+  [1, 2_000_000_000],
+  [2, 30_000_000],
+  [3, 1_500_000],
+  [4, 50_000],
+  [5, 5_000],
+]);
+
+export const RANK_TUPLE = Object.freeze([
+  [6, 1],
+  [5, 3],
+  [4, 4],
+  [3, 5],
+  [2, LOTTO.NO_LUCK_RANK],
+  [1, LOTTO.NO_LUCK_RANK],
+  [0, LOTTO.NO_LUCK_RANK],
+]);
+
+export const matchedAmoutMap = new Map(
+  [[1, 6],
+  [2, 5],
+  [3, 5],
+  [4, 4],
+  [5, 3],]
+)
--- src/constants/message.js
+++ src/constants/message.js
@@ -1,10 +1,13 @@
-const INPUT_MESSAGE = Object.freeze({
-  PURCAHSE_AMOUNT: '구입금액을 입력해주세요.',
-  WINNING_NUMBERS: '당첨번호를 입력해주세요',
-  BONUS_NUMBER: '보너스 번호를 입력해주세요',
+export const NEW_LINE = '\n';
+
+export const INPUT_MESSAGE = Object.freeze({
+  PURCAHSE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBERS: '당첨번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
 });
 
-const OUTPUT_MESSAGE = Object.freeze({
-  WINNING_STATISTICS: '당첨 통계',
+export const OUTPUT_MESSAGE = Object.freeze({
+  WINNING_STATISTICS: `${NEW_LINE}당첨 통계`,
   DIVIDER: '---',
+  BUY_AMOUNT: `개를 구매했습니다.`
 });
