a1521d55ae3880d2c08d6c2df646148bb086f6c0
kr-younghoon <sullanta0802@gmail.com>
2024-11-04 20:48:34 +0900

    test(lottogame): 당첨 번호 및 로또 번호 비교와 당첨 결과 출력 테스트 추가
    - 로또 번호와 당첨 번호를 비교하여 정확히 등수를 계산하는지 테스트
    - 당첨 결과 출력이 올바르게 이루어지는지 Console.print 모킹하여 검증

--- __tests__/LottoGamesTest.js
+++ __tests__/LottoGamesTest.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import LottoGame from '../src/controllers/LottoGames';
+import Lotto from '../src/Lotto';
 
 describe('로또게임 클래스 테스트', () => {
   test('구입 금액이 1,000원 단위가 아니면 예외가 발생한다.', () => {
@@ -99,5 +100,54 @@ describe('로또게임 클래스 테스트', () => {
     expect(() => {
       lottoGame.setWinningNumbers(validWinningNumbers, invalidBonusNumber);
     }).toThrow('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+  });
+
+  test('당첨 번호와 보너스 번호를 기준으로 로또 등수를 정확히 계산한다.', () => {
+    const lottoGame = new LottoGame();
+    lottoGame.purchaseLottos(3000);
+    lottoGame.setWinningNumbers([1, 2, 3, 4, 5, 6], 7);
+
+    const mockLottos = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 8],
+    ];
+
+    lottoGame.lottos = mockLottos.map((numbers) => new Lotto(numbers));
+
+    lottoGame.checkResults();
+    const results = lottoGame.getResults();
+
+    expect(results[1]).toBe(1);
+    expect(results[2]).toBe(1);
+    expect(results[3]).toBe(1);
+    expect(results[4]).toBe(0);
+    expect(results[5]).toBe(0);
+  });
+
+  test('당첨 결과가 올바르게 출력된다.', () => {
+    const lottoGame = new LottoGame();
+    lottoGame.purchaseLottos(3000);
+    lottoGame.setWinningNumbers([1, 2, 3, 4, 5, 6], 7);
+
+    const logSpy = jest.spyOn(Console, 'print');
+    const mockLottos = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 8],
+    ];
+
+    lottoGame.lottos = mockLottos.map((numbers) => new Lotto(numbers));
+
+    lottoGame.checkResults();
+    lottoGame.printResults();
+
+    expect(logSpy).toHaveBeenCalledWith('당첨 통계');
+    expect(logSpy).toHaveBeenCalledWith('---');
+    expect(logSpy).toHaveBeenCalledWith('3개 일치 (5,000원) - 0개');
+    expect(logSpy).toHaveBeenCalledWith('4개 일치 (50,000원) - 0개');
+    expect(logSpy).toHaveBeenCalledWith('5개 일치 (1,500,000원) - 1개');
+    expect(logSpy).toHaveBeenCalledWith('5개 일치, 보너스 볼 일치 (30,000,000원) - 1개');
+    expect(logSpy).toHaveBeenCalledWith('6개 일치 (2,000,000,000원) - 1개');
   });
 });
