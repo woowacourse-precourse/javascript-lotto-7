4e2fa214dcbd99f8499e0e0dfb02a6b4b6ccd142
Erin <fls4804@gmail.com>
2024-11-04 19:21:36 +0900

    fix: 로또 결과 출력 함수 및 오탈자 수정

--- src/App.js
+++ src/App.js
@@ -22,27 +22,33 @@ class App {
       this.winningLottoNumbers,
       this.bonusLottoNumber
     );
+    this.displayLottoResult(result);
+    const earningsRate = this.calculateEarningsRate(
+      this.amountOfPurchasedMoney,
+      result
+    );
+    MissionUtils.Console.print(`총 수익률은 ${earningsRate}%입니다.`);
   }
 
   async getPurchasedAmount() {
     while (true) {
       const inputtedMoney = await MissionUtils.Console.readLineAsync(
-        `구입금액을 입력해주세요./n`
+        `구입금액을 입력해주세요.`
       );
       if (await this.isValidMoneyInput(inputtedMoney)) return inputtedMoney;
     }
   }
   async isValidMoneyInput(inputtedMoney) {
     try {
-      this.isValidMoneyInput(inputtedMoney);
+      this.validateMoneyInput(inputtedMoney);
       return true;
     } catch (e) {
-      MissionUtils.Console.print(e.messsage);
+      MissionUtils.Console.print(e.message);
       return false;
     }
   }
   validateMoneyInput(inputtedMoney) {
-    if (isNan(inputtedMoney))
+    if (isNaN(inputtedMoney))
       throw new Error("[ERROR] 숫자를 입력해야 합니다.");
     if (inputtedMoney <= 0)
       throw new Error("[ERROR] 1000원 이상 입력해야 합니다.");
@@ -69,7 +75,7 @@ class App {
   async getWinningLottoNumbers() {
     while (true) {
       const inputtedWinningNumber = await MissionUtils.Console.readLineAsync(
-        `당첨 번호를 입력해주세요`
+        `당첨 번호를 입력해주세요.`
       );
       if (await this.isValidWinningNumbersInput(inputtedWinningNumber))
         return inputtedWinningNumber.split(",").map(Number);
@@ -80,7 +86,7 @@ class App {
       this.validateWinningNumbers(inputtedWinningNumber);
       return true;
     } catch (e) {
-      MissionUtils.Console.print(e.messsage);
+      MissionUtils.Console.print(e.message);
       return false;
     }
   }
@@ -91,7 +97,7 @@ class App {
     if (numberArray.length !== 6 || numberArray.length !== uniqueNumersSet.size)
       throw new Error("[ERROR] 6개의 숫자를, 중복 없이 입력해야 합니다.");
 
-    numberArray.forEacn((num) => {
+    numberArray.forEach((num) => {
       if (isNaN(num) || num % 1 !== 0 || num < 1 || num > 45)
         throw new Error(
           "[ERROR] 숫자는 반드시 정수이며, 범위는 1-45이어야 합니다."
@@ -134,7 +140,7 @@ class App {
       );
 
     if (winningNumbers.includes(Number(inputtedBonusNumber)))
-      throw new Error("[Error] 보너스 번호가 당첨 번호와 중복되면 안됩니다.");
+      throw new Error("[ERROR] 보너스 번호가 당첨 번호와 중복되면 안됩니다.");
   }
   calculateLottoResults(lottoList, winningNumbers, bonusNumber) {
     return lottoList.reduce(
@@ -165,6 +171,28 @@ class App {
         break;
     }
   }
+  displayLottoResult(result) {
+    MissionUtils.Console.print("당첨 통계\n---");
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${result[0]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${result[1]}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${result[2]}개`);
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[3]}개`
+    );
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${result[4]}개`);
+  }
+  calculateEarningsRate(purchasedAmount, result) {
+    const prizeAmounts = [5000, 50000, 1500000, 30000000, 2000000000];
+    const totalEarnings = prizeAmounts.reduce(
+      (accumulatedEarnings, prizeAmount, index) =>
+        accumulatedEarnings + prizeAmount * result[index],
+      0
+    );
+    return ((totalEarnings / purchasedAmount) * 100).toFixed(1);
+  }
 }
 
 export default App;
+
+const app = new App();
+app.run();
