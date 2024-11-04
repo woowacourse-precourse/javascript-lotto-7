40954343e27eabdede6be6af4ddfe89411dacdc4
suhwa <tnghk9611@naver.com>
2024-11-04 18:11:30 +0900

    refactor(OutputView): 개행을 처리하는 함수 분리

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -2,18 +2,24 @@ import { Console } from "@woowacourse/mission-utils";
 
 class OutputView {
   static printError(message) {
-    Console.print(message + "\n");
+    Console.print(message);
+    this.#printNewLine();
   }
 
   static printPurchaseInfo(purchaseDTO) {
-    Console.print(`\n${purchaseDTO.lottoCount}개를 구매했습니다.`);
+    this.#printNewLine();
+    Console.print(`${purchaseDTO.lottoCount}개를 구매했습니다.`);
     purchaseDTO.lottos.forEach((lotto) => {
       Console.print(`[${lotto.join(", ")}]`);
     });
   }
 
   static printRankResult(rankDTO) {
-    Console.print("\n당첨 통계\n---");
+    this.#printNewLine();
+    Console.print("당첨 통계");
+    this.#printNewLine();
+    Console.print("---");
+
     const rank = rankDTO.lottoRanks;
     Console.print(`3개 일치 (5,000원) - ${rank.fifth}개`);
     Console.print(`4개 일치 (50,000원) - ${rank.fourth}개`);
@@ -21,6 +27,10 @@ class OutputView {
     Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank.second}개`);
     Console.print(`6개 일치 (2,000,000,000원) - ${rank.first}개`);
     Console.print(`총 수익률은 ${rankDTO.profitRate}%입니다.`);
+  }
+
+  static #printNewLine() {
+    Console.print("\n");
   }
 }
 
