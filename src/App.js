0e4e354a9e2e6da4b9fbaefe342a4f6e4d688408
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 21:34:43 +0900

    feat: 로또 발매기 동작 코드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import LottoMachine from './LottoMachine.js';
+import LottoPrinter from './LottoPrinter.js';
+import validatePrice from './utils/validate/validatePrice.js';
+import validateWinningNumber from './utils/validate/validateWinningNumber.js';
+import validateBonusNumber from './utils/validate/validateBonusNumber.js';
+import calculateTotalReturn from './utils/calculateTotalReturn.js';
+
 class App {
-  async run() {}
+  async run() {
+    const price = await validatePrice();
+    const lottoMachine = new LottoMachine(price);
+    lottoMachine.generateLottoNumbers();
+    LottoPrinter.lottoNumbers(lottoMachine.getLottoNumbers());
+
+    const lottoNumbers = await validateWinningNumber();
+    const bonusNumber = await validateBonusNumber(lottoNumbers);
+    lottoMachine.checkLottoNumbers(lottoNumbers, bonusNumber);
+
+    LottoPrinter.winningCount(lottoMachine.getWinningCount());
+    LottoPrinter.totalReturn(
+      calculateTotalReturn(lottoMachine.getWinningCount(), price)
+    );
+  }
 }
 
 export default App;
