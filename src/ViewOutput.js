fe875f0f6b915350a0d813aaab8911fee6bb867c
YeWon Seo <syw1236@naver.com>
2024-11-04 11:52:54 +0900

    feat(ViewOutput): 사용자의 로또 번호를 출력하는 기능 구현

--- src/ViewOutput.js
+++ src/ViewOutput.js
@@ -1,8 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export default class ViweOutput {
-    
+
     static printText(content) {
         Console.print(content);
+    }
+    
+    static printLottoList(LottoList) {
+        LottoList.forEach(lotto => {
+            Console.print(lotto);
+        });
     }
 }
