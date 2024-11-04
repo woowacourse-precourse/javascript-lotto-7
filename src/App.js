67075e7ac406506a56d332f0e07799d78f5de78a
kim-museong <antjd0419@gmail.com>
2024-11-04 21:38:18 +0900

    feature(input) : 사용자 입력 기능 구현
    사용자의 입력이 필요한 곳에서 호출하여 사용할 수 있는 함수입니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
+
 class App {
-  async run() {}
+  async run() {
+    Console.print('구입금액을 입력해 주세요.');
+    const input = await this.answer();
+  }
+
+  async answer() {
+    const input = await Console.readLineAsync('');
+    return input;
+  }
 }
 
 export default App;
