78a77c40b0e23cefffd3a5f3203b79ae5611ea0d
2wndrhs <2wndrhs@gmail.com>
2024-11-04 13:51:59 +0900

    feat: 당첨 내역을 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import LottoIssuer from './Model/LottoIssuer.js';
+import LottoResultCalculator from './Model/LottoResultCalculator.js';
 import WinningLotto from './Model/WinningLotto.js';
 import InputView from './View/InputView.js';
 import OutputView from './View/OutputView.js';
 
 class App {
+  #inputView = new InputView();
+
+  #outputView = new OutputView();
+
   async run() {
-    const purchaseAmount = await InputView.readLottoPurchaseAmount();
+    const purchaseAmount = await this.#inputView.readLottoPurchaseAmount();
+    const issuedLottos = this.issueLottos(purchaseAmount);
+    this.printIssuedLottos(issuedLottos);
 
+    const winningLotto = await this.readWinningLotto();
+
+    this.printResults(issuedLottos, winningLotto);
+  }
+
+  issueLottos(purchaseAmount) {
     const lottoIssuer = new LottoIssuer(purchaseAmount);
-    const issuedLottos = lottoIssuer.issue();
+    return lottoIssuer.issue();
+  }
 
-    OutputView.printPurchasedLottosAmount(issuedLottos.length);
+  printIssuedLottos(issuedLottos) {
+    this.#outputView.printPurchasedLottosAmount(issuedLottos.length);
     issuedLottos.forEach((lotto) =>
-      OutputView.printPurchasedLottoNumbers(lotto.getSortedNumbers()),
+      this.#outputView.printPurchasedLottoNumbers(lotto.getSortedNumbers()),
     );
+  }
 
+  async readWinningLotto() {
     const winningLotto = new WinningLotto();
-    const winningNumbers = await InputView.readWinningNumbers();
+
+    const winningNumbers = await this.#inputView.readWinningNumbers();
     winningLotto.setWinningNumbers(winningNumbers);
 
-    const bonusNumber = await InputView.readBonusNumber();
+    const bonusNumber = await this.#inputView.readBonusNumber();
     winningLotto.setBonusNumber(bonusNumber);
+
+    return winningLotto;
+  }
+
+  printResults(issuedLottos, winningLotto) {
+    const resultCalculator = new LottoResultCalculator(
+      issuedLottos,
+      winningLotto,
+    );
+
+    const result = resultCalculator.calculateResults();
+    this.#outputView.printLottoResult(result);
+
+    const profitRate = resultCalculator.calculateProfitRate();
+    this.#outputView.printLottoProfitRate(profitRate);
   }
 }
 
--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,28 +1,24 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export default class InputView {
-  static #MESSAGE = Object.freeze({
+  #MESSAGE = Object.freeze({
     PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
     WINNING_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
     BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
   });
 
-  static async readLottoPurchaseAmount() {
-    const input = await Console.readLineAsync(
-      InputView.#MESSAGE.PURCHASE_AMOUNT,
-    );
+  async readLottoPurchaseAmount() {
+    const input = await Console.readLineAsync(this.#MESSAGE.PURCHASE_AMOUNT);
     return input;
   }
 
-  static async readWinningNumbers() {
-    const input = await Console.readLineAsync(
-      InputView.#MESSAGE.WINNING_NUMBERS,
-    );
+  async readWinningNumbers() {
+    const input = await Console.readLineAsync(this.#MESSAGE.WINNING_NUMBERS);
     return input;
   }
 
-  static async readBonusNumber() {
-    const input = await Console.readLineAsync(InputView.#MESSAGE.BONUS_NUMBER);
+  async readBonusNumber() {
+    const input = await Console.readLineAsync(this.#MESSAGE.BONUS_NUMBER);
     return input;
   }
 }
