cd0d286e50885caf7e0133b09215d7cc7a01d62b
@u_limit_ <yurim@ajou.ac.kr>
2024-11-04 22:20:31 +0900

    feat(CONSOLE_MESSAGES): 문구 출력 앞에 개행 추가
    당첨 번호 입력을 요구하는 콘솔 메세지 앞에 개행을 추가해 출력 요구사항을 충족시키고자 했습니다.

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,5 +1,5 @@
 export const CONSOLE_MESSAGES = Object.freeze({
   buyPrice: '구입금액을 입력해 주세요.\n',
-  winNumber: '당첨 번호를 입력해 주세요.\n',
+  winNumber: '\n당첨 번호를 입력해 주세요.\n',
   bonusNumber: '보너스 번호를 입력해 주세요.\n',
 })
