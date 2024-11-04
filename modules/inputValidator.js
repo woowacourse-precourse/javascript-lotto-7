cfcd9b5a1a5a71f4c58555a57385b7bbf4a3bb30
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-04 21:50:50 +0900

    feat: 당첨 번호 유효성 검사 함수 추가
    inputValidator.js 파일에 이하 내용 추가:
    - 당첨 번호 입력값의 유효성을 검사하는 winningNumbersValidate 함수 추가

--- modules/inputValidator.js
+++ modules/inputValidator.js
@@ -15,3 +15,36 @@ export function purchaseAmountValidate(amount) {
     throwError("구입 금액은 1,000원 단위의 양의 정수로 입력해 주세요.");
   }
 }
+
+export function winningNumbersValidate(numbers) {
+  const numbersArray = numbers.split(",");
+  const numbersSet = new Set(numbersArray);
+
+  if (!numbers.trim()) {
+    throwError("번호를 입력해 주세요.");
+  }
+
+  if (numbersArray.length !== 6) {
+    throwError("로또 번호는 6개여야 합니다.");
+  }
+
+  if (numbersSet.size !== numbersArray.length) {
+    throwError("중복된 번호가 있습니다.");
+  }
+
+  numbersArray.forEach((number) => {
+    const numericValue = Number(number);
+
+    if (isNaN(number)) {
+      throwError("잘못된 입력이거나, 쉼표로 구분된 숫자열이 아닙니다.");
+    }
+
+    if (!Number.isInteger(numericValue)) {
+      throwError("번호는 정수로 입력해주세요.");
+    }
+
+    if (numericValue < 1 || numericValue > 45) {
+      throwError("숫자는 1 과 45 사이로 입력해주세요.");
+    }
+  });
+}
