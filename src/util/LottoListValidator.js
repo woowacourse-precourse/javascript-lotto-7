adfc6cf283cca83e42f9d18076f9b85affc3e3c6
deokbin <deokbin@naver.com>
2024-11-04 09:35:47 +0900

    feat(LottoListValidator) : 로또 당첨 번호 배열 단위 유효성 검사 추가

---
+++ src/util/LottoListValidator.js
@@ -0,0 +1,23 @@
+import { ErrorMessage } from '../constants/ErrorMessage.js';
+import { MagicNumber } from '../constants/MagicNumber.js';
+
+class LottoListValidator {
+  #numbers;
+
+  constructor(numbers) {
+    this.validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  static validate(numbers) {
+    if (numbers.length !== MagicNumber.LOTTO_COUNT) {
+      throw new Error(ErrorMessage.NOT_6_NUMS);
+    }
+
+    const numSet = new Set(numbers);
+    if (numbers.length !== numSet.size) {
+      throw new Error(ErrorMessage.DUPLICATED_NUM);
+    }
+  }
+}
+export default LottoListValidator;
