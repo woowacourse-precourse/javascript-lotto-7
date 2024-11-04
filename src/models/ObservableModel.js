5691bff4e55b1cc7aa827d5e53af038a2852c69a
hamo-o <yheel369@gmail.com>
2024-11-04 13:28:36 +0900

    feat(ObservableModel): 옵저버블 객체로 쓰일 클래스

---
+++ src/models/ObservableModel.js
@@ -0,0 +1,21 @@
+class ObservableModel {
+  #observers;
+
+  constructor() {
+    this.#observers = [];
+  }
+
+  subscribe(func) {
+    this.#observers.push(func);
+  }
+
+  unsubscribe(func) {
+    this.#observers = this.#observers.filter((observer) => observer !== func);
+  }
+
+  notify(data) {
+    this.#observers.forEach((observer) => observer(data));
+  }
+}
+
+export default ObservableModel;
