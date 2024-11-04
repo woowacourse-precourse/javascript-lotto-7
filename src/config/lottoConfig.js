88cceb03ab3718351fe2534d4b4001c6ebe53477
Aing-bim <yeonju0908@gmail.com>
2024-11-04 13:30:01 +0900

    feat : 에러메세지 및 로또 상수 정의

---
+++ src/config/lottoConfig.js
@@ -0,0 +1,11 @@
+export const LOTTO_PRICE = 1000;
+export const LOTTO_NUMBER_RANGE = { min: 1, max: 45 };
+export const LOTTO_NUM_COUNT = 6;
+
+export const PRIZES = {
+  match3: 5000,
+  match4: 50000,
+  match5: 1500000,
+  match5PlusBonus: 30000000,
+  match6: 2000000000,
+};
