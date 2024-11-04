54e036f216e05542a4951ea4d1be3564455b256a
jiyun1006 <jiyun4251@gmail.com>
2024-11-04 12:46:32 +0900

    🗑️ remove : 당첨 확인 기능 삭제 및 이전
    BonusNumber클래스가 당첨 확인 기능을 담당하기에
    Lotto클래스의 메서드 삭제

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,51 +15,6 @@ class Lotto {
       condition(numbers);
     });
   }
-  /**
-   *
-   * @param {Array<Array<number>>} myLotto
-   * @param {string} bonusNumber
-   */
-  checkWinning(myLotto, bonusNumber) {
-    let winningResult = Array.from({ length: 5 }).fill(0);
-    myLotto.forEach((lotto) => {
-      winningResult = this.#checkWinningCnt(
-        this.#compareLottoNumber(lotto, bonusNumber),
-        winningResult,
-      );
-    });
-    return winningResult;
-  }
-
-  #compareLottoNumber(lotto, bonusNumber) {
-    const winning = {
-      winningCnt: 0,
-      bonus: false,
-    };
-    lotto.forEach((number) => {
-      if (this.#numbers.indexOf(number.toString()) >= 0)
-        winning.winningCnt += 1;
-    });
-
-    if (lotto.indexOf(Number(bonusNumber)) >= 0) winning.bonus = true;
-    return winning;
-  }
-
-  #checkWinningCnt(winning, winningResult) {
-    if (winning.winningCnt - NUM.MINIMUM_WINNING_CNT >= 0) {
-      if (winning.winningCnt === NUM.FIVE_WINNING) {
-        winningResult[this.#checkWinningBonusNumber(winning.bonus)] += 1;
-        return winningResult;
-      }
-      winningResult[winning.winningCnt - NUM.MINIMUM_WINNING_CNT] += 1;
-    }
-    return winningResult;
-  }
-
-  #checkWinningBonusNumber(bonusBoolean) {
-    if (bonusBoolean) return NUM.FIVE_WINNING_BONUS - NUM.MINIMUM_WINNING_CNT;
-    return NUM.FIVE_WINNING - NUM.MINIMUM_WINNING_CNT;
-  }
 
   calculateReturn(purchaseAmount, winningResult) {
     const winningReturnSum = this.#sumWinningReturn(winningResult);
