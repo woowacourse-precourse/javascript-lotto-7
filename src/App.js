eb811dfc4d557b73b8378f6a62077efc1294e831
inji0212 <in17100170@kakao.com>
2024-11-04 20:54:17 +0900

    Feat 구입금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const moneyInput =
+        await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      // 에러 발생시 재입력받게 해줌
+      await this.run();
+    }
+  }
 }
 
 export default App;
