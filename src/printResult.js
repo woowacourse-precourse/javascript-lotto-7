ece6b7da8075ec5882e7c21575d93a3b233c6f9d
sooinice <sooin9674@naver.com>
2024-11-04 20:56:40 +0900

    refactor : 당첨 통계 출력문 수정

--- src/printResult.js
+++ src/printResult.js
@@ -1,7 +1,9 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 export function printResult(counts) {
-  console.log(`3개 일치(5,000원) - ${counts[3]}`);
-  console.log(`4개 일치(50,000원) - ${counts[4]}`);
-  console.log(`5개 일치(1,500,000원) - ${counts[5]}`);
-  console.log(`5개 일치(30,000,000원) - ${counts["5_bonus"]}`);
-  console.log(`6개 일치(2,000,000,000원) - ${counts[6]}`);
+  MissionUtils.Console.print(`3개 일치(5,000원) - ${counts[3]}`);
+  MissionUtils.Console.print(`4개 일치(50,000원) - ${counts[4]}`);
+  MissionUtils.Console.print(`5개 일치(1,500,000원) - ${counts[5]}`);
+  MissionUtils.Console.print(`5개 일치(30,000,000원) - ${counts["5_bonus"]}`);
+  MissionUtils.Console.print(`6개 일치(2,000,000,000원) - ${counts[6]}`);
 }
