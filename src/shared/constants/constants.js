aaab4d581bb2465399d7ec62c69da6ebc3017ce8
kimgho <kgh67811@naver.com>
2024-11-04 15:08:19 +0900

    feat(constants): 메시지를 묶은 index 작성
    - 필요한 메세지를 묶어 export하는 index 작성

---
+++ src/shared/constants/constants.js
@@ -0,0 +1,6 @@
+import { MESSAGE } from './message.js';
+import { ERROR_MESSAGE } from './error.js';
+import { LOTTO } from './lotto.js';
+import { PRIZE } from './prize.js';
+
+export { MESSAGE, ERROR_MESSAGE, LOTTO, PRIZE };
