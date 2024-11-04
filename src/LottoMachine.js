9c1e58799a45c6bdceafd7a446e77c70290f8d1d
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-11-04 18:50:36 +0900

    feat(lottoMachine): 로또 번호 발행 및 로또 번호 비교 함수 작성

---
+++ src/LottoMachine.js
@@ -0,0 +1,19 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
+
+class LottoMachine {
+  drawLottoNumbers() {
+    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return new Lotto(numbers);
+  }
+  generateLottos(cnt) {
+    return Array.from({ length: cnt }, () => this.drawLottoNumbers());
+  }
+
+  compareWinningNumbers(numbers, winningNumbers) {
+    return numbers.filter((num) => winningNumbers.includes(num)).length;
+  }
+  compareBonusNumber(numbers, bonus) {
+    return numbers.includes(bonus);
+  }
+}
