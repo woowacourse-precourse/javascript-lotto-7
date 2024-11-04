17b8317fdf0cafbd595c0383cb7533b39ecdecf0
Minji Kim <minjikim2000@hotmail.com>
2024-11-04 15:34:18 +0900

    feat: 로또 구매 금액 유효성 검사 로직 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,22 @@ class App {
 
   async inputPurchaseAmount() {
     const amount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
-    Console.print(amount);
+    this.validatePurchaseAmount(amount);
+  }
+
+  validatePurchaseAmount(amount) {
+    const purchaseAmount = Number(amount);
+
+    if (Number.isNaN(purchaseAmount) || purchaseAmount % 1000 !== 0) {
+      this.handleError("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+    } else {
+      this.purchaseAmount = purchaseAmount;
+    }
+  }
+
+  handleError(message) {
+    Console.print(message);
+    this.inputPurchaseAmount(); // 잘못된 입력일 경우 재입력 요청
   }
 }
 
