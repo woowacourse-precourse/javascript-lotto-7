396378a13e42b96f03b608df386936584280951c
aoooec <foottable@gmail.com>
2024-11-04 21:51:38 +0900

    remove: 불필요한 유틸 파일 삭제

--- src/utils/checkDuplicateValue.js
+++
@@ -1,9 +0,0 @@
-export const hasDuplicates = (array) => {
-  const uniqueValues = new Set(array);
-
-  return array.length > uniqueValues.size;
-};
-
-export const isDuplicateInArray = (array, value) => {
-  return array.includes(value);
-};
