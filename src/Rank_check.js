8410d15c195c9cd14ccaf7e45d1e91a4d9512010
Jang Eunhye <hh961130@naver.com>
2024-11-04 19:44:25 +0900

    fix: 출력 문구 수정(5000 => 5,000)

--- src/Rank_check.js
+++ src/Rank_check.js
@@ -50,11 +50,11 @@ class Rank_check {
   printRank(match_three, match_four, match_five, match_five_bonus, match_six) {
     Console.print("\n당첨 통계");
     Console.print("---");
-    Console.print("3개 일치 (5000원) - " + match_three + "개")
-    Console.print("4개 일치 (50,000원) - " + match_four + "개")
-    Console.print("5개 일치 (1,500,000원) - " + match_five + "개")
-    Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + match_five_bonus + "개")
-    Console.print("6개 일치 (2,000,000,000원) - " + match_six + "개")
+    Console.print("3개 일치 (5,000원) - " + match_three + "개");
+    Console.print("4개 일치 (50,000원) - " + match_four + "개");
+    Console.print("5개 일치 (1,500,000원) - " + match_five + "개");
+    Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + match_five_bonus + "개");
+    Console.print("6개 일치 (2,000,000,000원) - " + match_six + "개");
   }
 
   revenue_percent(myLottoArray, match_three, match_four, match_five, match_five_bonus, match_six) {
