9416d624634ef50ac7e32b90b1c907b12d185e2d
YSW2 <jrgyber@gmail.com>
2024-11-04 18:07:42 +0900

    feat<InputValidator>: 당첨 번호 기능 구현
    - 당첨번호 입력 및 유효 검증 기능 추가
    - 보너스번호 입력 및 유효 검증 기능 추가

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -16,4 +16,46 @@ class InputValidator {
 
     return parsedAmount;
   }
+
+  static validateWinningNumbers(input) {
+    const numbers = input.split(',').map((num) => {
+      const parsed = Number(num.trim());
+      if (isNaN(parsed)) {
+        throw new Error('[ERROR] 당첨 번호는 숫자여야 합니다.');
+      }
+      return parsed;
+    });
+
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+
+    if (numbers.some((num) => num < 1 || num > 45)) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 중복될 수 없습니다.');
+    }
+
+    return numbers;
+  }
+
+  static validateBonusNumber(number, winningNumbers) {
+    const parsed = Number(number);
+
+    if (isNaN(parsed)) {
+      throw new Error('[ERROR] 보너스 번호는 숫자여야 합니다.');
+    }
+
+    if (parsed < 1 || parsed > 45) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+
+    if (winningNumbers.includes(parsed)) {
+      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+    }
+
+    return parsed;
+  }
 }
