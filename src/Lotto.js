24163617b100d9250cb1d38c6282dbae24290468
(hoooooony) <(cjh41820@gmail.com)>
2024-11-04 21:38:06 +0900

    feat : 로또 필드 및 메소드 정의

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,51 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class Lotto {
+  static NUMBER_RANGE = { MIN: 1, MAX: 45 };
+  static NUM_COUNT = 6;
+
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers = Lotto.generateRandomNumbers()) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
 
+  static generateRandomNumbers() {
+    return Random.pickUniqueNumbersInRange(
+      Lotto.NUMBER_RANGE.MIN,
+      Lotto.NUMBER_RANGE.MAX,
+      Lotto.NUM_COUNT
+    );
+  }
+
   #validate(numbers) {
-    if (numbers.length !== 6) {
+    if (numbers.length !== Lotto.NUM_COUNT) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    if (!numbers.every((num) => this.isNumberInRange(num))) {
+      throw new Error("[ERROR] 숫자는 1~45 사이의 수여야 합니다.");
+    }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  isNumberInRange(number) {
+    return number >= Lotto.NUMBER_RANGE.MIN && number <= Lotto.NUMBER_RANGE.MAX;
+  }
+
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  matchCount(winningNumbers) {
+    return this.#numbers.filter((num) => winningNumbers.includes(num)).length;
+  }
+
+  hasBonusNumber(bonusNumber) {
+    return this.#numbers.includes(bonusNumber);
+  }
 }
 
 export default Lotto;
