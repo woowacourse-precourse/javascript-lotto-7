7503acda85db57f05924ba76ebc3c17bab507db9
SeokHun <tommya98@naver.com>
2024-11-04 20:06:02 +0900

    feat(App): 로또 구입 금액 입력 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    this.#askPurchaseAmount();
+  }
+
+  #askPurchaseAmount() {
+    Console.readLine("구입금액을 입력해 주세요.\n", (input) => {
+      this.#validatePurchaseAmount(input);
+      const amount = parseInt(input, 10);
+      const lottoCount = Math.floor(amount / 1000);
+      Console.print(`\n${lottoCount}개를 구매하였습니다.`);
+      // TODO: 로또 자동 발행 기능 호출
+    });
+  }
+
+  #validatePurchaseAmount(input) {
+    const amount = parseInt(input, 10);
+    if (isNaN(amount) || amount % 1000 !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.");
+    }
+  }
 }
 
 export default App;
