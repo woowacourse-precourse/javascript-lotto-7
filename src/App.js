0bf7c2414cb18d2bc7f276ddbd825599bd526975
minseok <alstjrwjd99@naver.com>
2024-11-04 09:41:03 +0900

    refactor(input): Split role all input to woowahanInput

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
 import WoowahanInput from "./woowahanInput.js";
-import { bonusNumberValidator, buyMoneyValidator, winInputValidator, winNumberValidator } from "./utils/validators.js";
 import LottoStore from "./LottoStore.js";
 import { GameOutput } from "./woowahanOutput.js";
 
 class App {
   async run() {
     const lottoStore = new LottoStore();
-
-    let isBuyMoneyValidate = true;
-    let isWinNumberValidate = true;
-    let isBonusNumberValidate = true;
-    let buyMoney = 0;
-    let winNumber = [];
-    let bonusNumber = 0;
-
-    while (isBuyMoneyValidate) {
-      buyMoney = await WoowahanInput.getBuyMoney();
-      isBuyMoneyValidate = !buyMoneyValidator(buyMoney);
-    }
-
+    const buyMoney = await WoowahanInput.getBuyMoney();
     const lottoTicketCount = lottoStore.getLottoTicketCount(buyMoney);
 
     GameOutput.printLottoTicketCount(lottoTicketCount);
 
     const lottos = lottoStore.generateLottos();
-
-    while (isWinNumberValidate) {
-      const winInput = await WoowahanInput.getWinNumber();
-      winNumber = winInput.split(',').map(Number);
-      if (!winInputValidator(winNumber)) continue;
-      isWinNumberValidate = winNumber.some((number) => !winNumberValidator(number));
-    }
-
-    while (isBonusNumberValidate) {
-      const inputBonus = await WoowahanInput.getBonusNumber();
-      bonusNumber = Number(inputBonus);
-      isBonusNumberValidate = !bonusNumberValidator(bonusNumber);
-    }
+    const winNumber = await WoowahanInput.getWinNumber();
+    const bonusNumber = await WoowahanInput.getBonusNumber();
 
     GameOutput.printWinningStatistics();
 
--- src/woowahanInput.js
+++ src/woowahanInput.js
@@ -1,15 +1,45 @@
 import { Console } from "@woowacourse/mission-utils";
 import { MESSAGES } from "./utils/constants.js";
+import { bonusNumberValidator, buyMoneyValidator, winInputValidator, winNumberValidator } from "./utils/validators.js";
 
 export default class WoowahanInput {
 
     static async getBuyMoney() {
-        return await Console.readLineAsync(`${MESSAGES.BUY_MONEY_INPUT}`);
+        let isBuyMoneyValidate = true;
+        let buyMoney = 0;
+
+        while (isBuyMoneyValidate) {
+            buyMoney = await Console.readLineAsync(`${MESSAGES.BUY_MONEY_INPUT}`);
+            isBuyMoneyValidate = !buyMoneyValidator(buyMoney);
+        }
+
+        return buyMoney;
     }
+
     static async getWinNumber() {
-        return await Console.readLineAsync(`${MESSAGES.WIN_NUMBER_INPUT}`);
+        let isWinNumberValidate = true;
+        let winNumber = [];
+
+        while (isWinNumberValidate) {
+            const winInput = await Console.readLineAsync(`${MESSAGES.WIN_NUMBER_INPUT}`);
+            winNumber = winInput.split(',').map(Number);
+            if (!winInputValidator(winNumber)) continue;
+            isWinNumberValidate = winNumber.some((number) => !winNumberValidator(number));
+        }
+
+        return winNumber;
     }
+    
     static async getBonusNumber() {
-        return await Console.readLineAsync(`${MESSAGES.BONUS_NUMBER_INPUT}`);
+        let isBonusNumberValidate = true;
+        let bonusNumber = 0;
+
+        while (isBonusNumberValidate) {
+            const inputBonus = await Console.readLineAsync(`${MESSAGES.BONUS_NUMBER_INPUT}`);
+            bonusNumber = Number(inputBonus);
+            isBonusNumberValidate = !bonusNumberValidator(bonusNumber);
+        }
+
+        return bonusNumber;
     }
 }
