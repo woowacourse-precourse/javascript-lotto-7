3f3f90f86ce7e3c56c6b7a0276ede3ad67f40e40
YeWon Seo <syw1236@naver.com>
2024-11-04 19:01:54 +0900

    feat(ViewOutput): 화면에 당첨 통계 출력 기능 구현

--- src/ViewOutput.js
+++ src/ViewOutput.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { winningLottoCountPrice, lottoMesaage, statisticsMesssage, numbers} from './constants.js';
 
 export default class ViweOutput {
 
@@ -9,6 +10,17 @@ export default class ViweOutput {
     static printLottoList(LottoList) {
         LottoList.forEach(lotto => {
             Console.print(lotto);
+        });
+    }
+
+    static printStatistics(statisticsCountMap) {
+        winningLottoCountPrice.forEach((lotto) => {
+            let matchCount = `${lotto.count}${statisticsMesssage.PRINT_MATCH}`;
+            if(lotto.count === numbers.FIVE_BONUS)
+                matchCount = `${numbers.FIVE}${statisticsMesssage.PRINT_MATCH}, ${statisticsMesssage.PRINT_BONUS}`;
+            this.printText(
+                `${matchCount} ${statisticsMesssage.OPENING_PARENTHESIS}${lotto.price}${statisticsMesssage.CLOSING_PARENTHESIS} ${statisticsMesssage.PRINT_BAR} ${statisticsCountMap.get(lotto.count)}${lottoMesaage.PRINT_COUNT}`
+            );       
         });
     }
 }
