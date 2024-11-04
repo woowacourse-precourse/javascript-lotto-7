e70b71bb2bd877fb1d4fd980311f60b54c2b3e20
Seio924 <seio9241@gmail.com>
2024-11-04 20:33:51 +0900

    feat(Lotto): 예외 처리 추가

--- src/Domain/Lotto.js
+++ src/Domain/Lotto.js
@@ -7,13 +7,39 @@ class Lotto {
   }
 
   #validate(numbers) {
+    this.#validateLength(numbers);
+    this.#validateIsNumber(numbers);
+    this.#validateRange(numbers);
+    this.#validateUnique(numbers);
+  }
+
+  #validateLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 반드시 6개여야 합니다.');
     }
+  }
 
+  #validateIsNumber(numbers) {
+    numbers.forEach(number => {
+      const changeNumber = Number(number);
+      if (Number.isNaN(changeNumber)) {
+        throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+      }
+    });
+  }
+
+  #validateRange(numbers) {
+    numbers.forEach(number => {
+      if (number < 1 || number > 45) {
+        throw new Error('[ERROR] 로또 번호는 1~45 사이여야 합니다.');
+      }
+    });
+  }
+
+  #validateUnique(numbers) {
     const uniqueNumbers = new Set(numbers);
     if (uniqueNumbers.size !== numbers.length) {
-      throw new Error('[ERROR] 중복된 당첨 번호가 있습니다.');
+      throw new Error('[ERROR] 중복된 로또 번호가 있습니다.');
     }
   }
 
