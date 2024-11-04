03942f80690a63f829d201cc20fb1b42be633a9d
seongmin <seongm22n@gmail.com>
2024-11-04 14:29:27 +0900

    feat : 기능4 예외처리

--- src/utils/LottoInputValidator.js
+++ src/utils/LottoInputValidator.js
@@ -1,17 +1,35 @@
 export class LottoInputValidator {
-	getLottoPrice(value) {
-		const price = this.changeToNumber(value);
+	getLottoPrice(inputAmount) {
+		const price = this.changeToNumber(inputAmount);
 		if (price % 1000 > 0) {
 			throw new Error(`[Error] 로또 금액은 1000원으로 나누어 떨어져야 합니다.`);
 		}
 		return price / 1000;
 	}
-	
+
 	changeToNumber(str) {
 		const num = parseInt(str, 10);
 		if (isNaN(num)) {
 			throw new Error(`[Error] 숫자만 입력 가능합니다.`);
 		}
 		return num;
+	}
+
+	getLottoWinner(winNumbers) {
+		const numsStr = winNumbers.split(',');
+		if (numsStr.length < 6) {
+			throw new Error('[Error] 숫자 사이의 구분자는 , 여야 합니다.');
+		}
+
+		const numList = new Set();
+		for (const numStr of numsStr) {
+			const num = this.changeToNumber(numStr);
+			numList.add(num);
+		}
+
+		if (numList.size !== 6) {
+			throw new Error('[Error] 로또 번호는 6개여야 합니다.');
+		}
+		return numList;
 	}
 }
