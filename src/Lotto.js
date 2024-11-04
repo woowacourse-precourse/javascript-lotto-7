35d0d73e93fb4aecf1cec6d5c0ac93e16ba08487
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-04 16:31:13 +0900

    feat: 당첨 번호 검증 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,58 @@
+import { LOTTO_NUMBER_ERROR_MESSAGES } from './constants/ERROR_MESSAGES.js';
+import { LOTTO } from './constants/LOTTO_CONSTANTS.js';
+import { splitString } from './utils/splitString.js';
+import { stringToNumber } from './utils/stringToNumber.js';
+import Validator from './Validator.js';
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+  constructor(input) {
+    this.#numbers = this.#validate(input);
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  #validate(input) {
+    Validator.validateWhitespace(input);
+    Validator.validateEmpty(input);
+
+    const splitNumbers = splitString(input);
+    this.#validateLottoNumberCount(splitNumbers);
+
+    const lottoNumbers = splitNumbers.map((value) => {
+      Validator.validateNaturalNumber(value);
+      const number = stringToNumber(value);
+      this.#validateLottoNumberRange(number);
+    });
+    this.#validateDuplicateLottoNumber(lottoNumbers);
+    return lottoNumbers;
+  }
+
+  #validateDuplicateLottoNumber(numbers) {
+    const isDuplicate = numbers.length !== new Set(numbers).size;
+
+    if (isDuplicate) {
+      throw new Error(
+        LOTTO_NUMBER_ERROR_MESSAGES.LOTTO_DUPLICATE_NUMBERS_FOUND
+      );
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateLottoNumberRange(number) {
+    if (
+      number < LOTTO.NUMBER_MINIMUM_RANGE &&
+      LOTTO.NUMBER_MAXIMUM_RANGE < number
+    ) {
+      throw new Error(LOTTO_NUMBER_ERROR_MESSAGES.LOTTO_NUMBER_OUT_OF_RANGE);
+    }
+  }
+
+  #validateLottoNumberCount(splitNumbers) {
+    if (splitNumbers.length !== LOTTO.NUMBER_COUNT) {
+      throw new Error(
+        LOTTO_NUMBER_ERROR_MESSAGES.LOTTO_NUMBER_COUNT_MUST_BE_SIX
+      );
+    }
+  }
 }
 
 export default Lotto;
