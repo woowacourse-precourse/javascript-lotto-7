0b3cbfcb84d417a8fcf4ee9b5d4d5c403061cdc4
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-04 17:28:56 +0900

    ✅ test: Lotto클래스의 getNumbers메서드 테스트 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,27 +1,26 @@
 import ValidateNumber from './controller/ValidateNumber.js';
 import { NUMBER_ERROR_MESSAGES } from './contents/InputErrorMessages.js';
 
+const LOTTO_NUMBER_COUNT = 6;
+
 class Lotto {
-  #numbers; // 당첨 번호
+  #numbers;
 
   constructor(numbers) {
-    this.#numbers = this.#validate(numbers);
+    this.#validate(numbers);
+    this.#numbers = numbers;
   }
 
-  // 당첨번호 검증
   #validate(numbers) {
-    //로또 번호 개수
     ValidateNumber.checkArrayLength(
       numbers,
-      6,
+      LOTTO_NUMBER_COUNT,
       NUMBER_ERROR_MESSAGES.winningNumberGuid,
     );
-    //중복 확인
     ValidateNumber.checkForDuplicates(
       numbers,
       NUMBER_ERROR_MESSAGES.dupicateNumber,
     );
-    // 당첨번호 영역 확인
     numbers.forEach((num) => {
       ValidateNumber.validateNumber(
         num,
@@ -31,7 +30,6 @@ class Lotto {
     return numbers;
   }
 
-  // 당첨 번호 반환
   getNumbers() {
     return [...this.#numbers];
   }
