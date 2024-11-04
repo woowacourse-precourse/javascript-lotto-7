4a2eed56493cd5d6c872f702e591ccd591abde95
PangKiYeon <pdrkeamy@gmail.com>
2024-11-04 19:49:02 +0900

    feat: lotto calculator 클래스 및 보너스 번호, 당첨 번호 유효성 검사 구현

---
+++ src/LottoCalculator.js
@@ -0,0 +1,46 @@
+import Lotto from './Lotto.js';
+import { PRIZE_MONEY } from './constant.js';
+import { Console } from '@woowacourse/mission-utils';
+
+class LottoCalculator {
+    #winningNumber;
+    #bonusNumber;
+
+    constructor(winningNumber, bonusNumber) {
+        this.#validateWinning(winningNumber);
+        this.#winningNumber = winningNumber;
+        this.#validateBonus(winningNumber, bonusNumber);
+        this.#bonusNumber = Number(bonusNumber);
+    }
+
+    #validateWinning(winningNumber) {
+        if (!/^[0-9,]+$/.test(winningNumber)) {
+            throw new Error('[ERROR] 숫자와 쉼표 외에는 입력할 수 없습니다.');
+        }
+        /*if (winningNumber.some((x) => 1 <= x <= 45)) {
+            throw new Error('[ERROR] 당첨 번호는 1~45 숫자여야 합니다.');
+        }*/
+        if (new Set(winningNumber).size !== winningNumber.length) {
+            throw new Error('[ERROR] 당첨 번호는 중복되면 안됩니다.');
+        }
+        if (winningNumber.length !== 6) {
+            throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+        }
+    }
+
+    #validateBonus(winningNumber, bonusNumber) {
+        if (!/^[0-9]*$/.test(bonusNumber)) {
+            throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
+        }
+        /*if (1 <= bonusNumber <= 45) {
+            throw new Error('[ERROR] 보너스 번호는 1~45 숫자여야 합니다.');
+        }*/
+        if (winningNumber.includes(bonusNumber)) {
+            throw new Error(
+                '[ERROR] 보너스 번호는 당첨번호와 중복되면 안됩니다.'
+            );
+        }
+    }
+}
+
+export default LottoCalculator;
