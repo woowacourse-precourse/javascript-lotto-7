4f69edde6d1dd48ce8acbc4fdd5ead5f6e060b1c
Jaeho Cha <ischar@naver.com>
2024-11-04 18:22:40 +0900

    refactor(parse): update parseNumbers to add exceptions
    - parseNumbers 기능 수정
      - 입력값이 공백일 때 에러 발생
      - 구분자가 달라 파싱이 안될 경우 에러 발생

--- src/features/parseNumbers.js
+++ src/features/parseNumbers.js
@@ -1,9 +1,15 @@
-import { DELIMITER } from "../config/config.js";
+import { DELIMITER, ERROR } from "../config/config.js";
 import { convertNumber } from "./convertNumber.js";
 
 export const parseNumbers = (strings) => {
-  const numbers = strings.split(DELIMITER);
+  if (strings === "") {
+    throw new Error(ERROR.INPUT_EMPTY);
+  }
   const result = [];
+  const numbers = strings.split(DELIMITER);
+  if (numbers.length <= 1) {
+    throw new Error(ERROR.NOT_NUMBER);
+  }
 
   for (let number of numbers) {
     result.push(convertNumber(number));
