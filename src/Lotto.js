93575c96ed1cc50d35798321e44f7397dce4ee12
parksiwoon <parksw003@gmail.com>
2024-11-04 18:59:19 +0900

    feat: Add validation for lotto numbers in Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,24 @@
 class Lotto {
-  #numbers;
-
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+    this.validate(numbers);
+    this.numbers = numbers;
   }
 
-  #validate(numbers) {
+  validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+    if (numbers.some((num) => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.numbers;
+  }
 }
 
 export default Lotto;
