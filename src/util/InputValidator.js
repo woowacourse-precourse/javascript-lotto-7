6c58b43bbb62ae712ef0b8407290cda979b63e7a
deokbin <deokbin@naver.com>
2024-11-04 09:32:57 +0900

    feat(InputValidator) : 입력값 유효성 검사 클래스 추가

---
+++ src/util/InputValidator.js
@@ -0,0 +1,26 @@
+import { ErrorMessage } from '../constants/ErrorMessage.js';
+
+class InputValidator {
+  static validateNotEmpty(value) {
+    if (value === '' || value === undefined || value === null) {
+      throw new Error(ErrorMessage.EMPTY_INPUT);
+    }
+  }
+
+  static validateIsNumber(value) {
+    if (Number.isNaN(value) || !/^-?\d+$/.test(value)) {
+      throw new Error(ErrorMessage.NOT_A_NUMBER);
+    }
+  }
+
+  static validatePositive(value) {
+    if (Number(value) === 0) {
+      throw new Error(ErrorMessage.ZERO_PRICE);
+    }
+    if (Number(value) < 0) {
+      throw new Error(ErrorMessage.NEGATIVE_NUM);
+    }
+  }
+}
+
+export default InputValidator;
