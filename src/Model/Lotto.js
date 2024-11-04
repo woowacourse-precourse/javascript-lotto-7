9b17027eff8d22135743c3c7715b27e147ce0f9a
chichi <seonrye.yun@gmail.com>
2024-11-04 23:57:36 +0900

    feat: Lotto 클래스 내부 로직 작성

--- src/Model/Lotto.js
+++ src/Model/Lotto.js
@@ -1,3 +1,6 @@
+import { DELIMITER } from '../constants/constants.js';
+import { ERROR_MESSAGES } from '../constants/messages.js';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +10,31 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if (!numbers) {
+      throw new Error(ERROR_MESSAGES.EMPTY_LOTTO_NUMBERS);
+    }
+
+    if (numbers.some((number) => isNaN(number))) {
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_NUMBERS);
+    }
+
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_COUNT);
+    }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error(ERROR_MESSAGES.DUPLICATE_LOTTO_NUMBERS);
     }
   }
 
-  // TODO: 추가 기능 구현
+  getLottoNumbers() {
+    return this.#numbers;
+  }
+
+  getLottoNumbersToString() {
+    return `[${this.#numbers.join(DELIMITER)}]`;
+  }
 }
 
 export default Lotto;
