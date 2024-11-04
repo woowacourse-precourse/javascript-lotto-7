85326b0283f2c1404eb2f8e4c5acf9d199ba56fa
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-11-04 18:15:04 +0900

    feat(LottoController): 사용자 입력함수에 validate 추가

--- src/LottoController.js
+++ src/LottoController.js
@@ -1,21 +1,28 @@
 import { INPUT_MESSAGE } from './constant';
 import { Console } from '@woowacourse/mission-utils';
+import { validateAmount, validateBonus, validateNumbers } from './validate';
 
 class LottoController {
   async getLottoAmount() {
     const amount = await Console.readLineAsync(
       INPUT_MESSAGE.INPUT_LOTTO_AMOUNT
     );
+    validateAmount(amount);
+    return parseInt(amount, 10);
   }
   async getLottoNumber() {
     const numbers = await Console.readLineAsync(
       INPUT_MESSAGE.INPUT_LOTTO_NUMBERS
     );
+    validateNumbers(numbers);
+    return numbers.split(',').map((num) => parseInt(num, 10));
   }
   async getLottoBonusNumber() {
     const bonus = await Console.readLineAsync(
       INPUT_MESSAGE.INPUT_BONUS_NUMBERS
     );
+    validateBonus(bonus);
+    return bonus;
   }
 }
 
