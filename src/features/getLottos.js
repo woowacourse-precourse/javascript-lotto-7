7a10dd8759614046f97dc0619d4018cc89749841
Jaeho Cha <ischar@naver.com>
2024-11-04 21:14:07 +0900

    refactor(lotto): update getLottos to add exception
    - getLottos 예외 처리 추가
      - 공백이거나, 숫자가 아닐 때 에러 발생

--- src/features/getLottos.js
+++ src/features/getLottos.js
@@ -1,7 +1,11 @@
 import { getLotto } from "./getLotto.js";
 import { createLottoMsg } from "./createLottoMsg.js";
+import { ERROR } from "../config/config.js";
 
 export const getLottos = (lottoCount) => {
+  if (lottoCount === "" || Number.isNaN(lottoCount)) {
+    throw new Error(ERROR.INPUT_EMPTY);
+  }
   const lottoList = [];
   let lottoStrings = "";
   for (let i = 0; i < lottoCount; i++) {
