a2b2131f10e7043f5cf6e0e15971fa1d7adcdf17
Junseo Kim <semiwest2@gmail.com>
2024-11-04 23:37:31 +0900

    refactor: App.js 에러시 재입력  함수 분리

--- src/App.js
+++ src/App.js
@@ -13,17 +13,7 @@ import LOTTO_CONFIG from './constants/lottoConfig.js';
 
 class App {
   async run() {
-    let purchasePrice;
-    while (true) {
-      try {
-        purchasePrice = Number(await getInput('구입금액을 입력해 주세요.\n'));
-        checkLottoPurchasePrice(purchasePrice);
-        break;
-      } catch (error) {
-        printString(`${error.message}`);
-      }
-    }
-
+    const purchasePrice = await this.getPurchasePrice();
     const lottoPurchaseAmount = purchasePrice / LOTTO_CONFIG.TICKET_PRICE;
     printString(`\n${lottoPurchaseAmount}개를 구매했습니다.`);
 
@@ -32,33 +22,54 @@ class App {
       lotto.print();
     });
 
-    let winningNumbers;
-    while (true) {
-      try {
-        winningNumbers = winningNumbersParser(await getInput('\n당첨 번호를 입력해 주세요.\n'));
-        break;
-      } catch (error) {
-        printString(`${error.message}`);
-      }
-    }
-
+    const winningNumbers = await this.getWinningNumbers();
     const winningLotto = new Lotto(winningNumbers);
-    let lottoBonusNumber;
-    while (true) {
-      try {
-        lottoBonusNumber = Number(await getInput('\n보너스 번호를 입력해 주세요.\n'));
-        checkLottoBonusNumber(winningLotto, lottoBonusNumber);
-        break;
-      } catch (error) {
-        printString(`${error.message}`);
-      }
-    }
+
+    const lottoBonusNumber = await this.getBonusNumber(winningLotto);
 
     printString('\n당첨 통계\n---');
     const lottoResults = calculateLottoStatistics(lottoList, winningLotto, lottoBonusNumber);
     printLottoStatistics(lottoResults);
     const profitRate = lottoProfitRate(purchasePrice, lottoResults);
     printString(`총 수익률은 ${profitRate}%입니다.`);
+  }
+
+  async getPurchasePrice() {
+    let purchasePrice;
+    while (true) {
+      try {
+        purchasePrice = Number(await getInput('구입금액을 입력해 주세요.\n'));
+        checkLottoPurchasePrice(purchasePrice);
+        return purchasePrice; // 유효한 입력이면 반환
+      } catch (error) {
+        printString(`${error.message}`);
+      }
+    }
+  }
+
+  async getWinningNumbers() {
+    let winningNumbers;
+    while (true) {
+      try {
+        winningNumbers = winningNumbersParser(await getInput('\n당첨 번호를 입력해 주세요.\n'));
+        return winningNumbers; // 유효한 입력이면 반환
+      } catch (error) {
+        printString(`${error.message}`);
+      }
+    }
+  }
+
+  async getBonusNumber(winningLotto) {
+    let lottoBonusNumber;
+    while (true) {
+      try {
+        lottoBonusNumber = Number(await getInput('\n보너스 번호를 입력해 주세요.\n'));
+        checkLottoBonusNumber(winningLotto, lottoBonusNumber);
+        return lottoBonusNumber; // 유효한 입력이면 반환
+      } catch (error) {
+        printString(`${error.message}`);
+      }
+    }
   }
 }
 
