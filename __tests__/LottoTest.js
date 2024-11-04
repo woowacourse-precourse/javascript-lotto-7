0bb68475fbb044fcefcf6be1de9813b58bf0c86c
Baek <woonn97@gmail.com>
2024-11-04 17:14:41 +0900

    feat: Lotto 클래스 테스트 추가 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,30 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+  test("로또 번호의 개수가 6개 미만이면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호가 1보다 작거나 45보다 큰 숫자가 포함되어 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 1부터 45 사이의 숫자이고 중복이 없으면 정상적으로 로또가 생성된다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
