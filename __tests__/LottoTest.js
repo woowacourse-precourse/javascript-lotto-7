ceb81ebe06654f27cfa14356c7064930a9bd13db
nerdchanii <nerdchanii@gmail.com>
2024-11-04 10:46:15 +0900

    feat(lotto): add validations rule for validate number range

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,17 +1,29 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호의 숫자가 45를 초과하면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1,2,3,4,5,46]);
+    }).toThrow('[ERROR]');
+  });
+  
+  test('로또 번호의 숫자가 1보다 작으면 예외가 발생한다.', () => {
+    expect(()=>{
+      new Lotto([0,1,2,3,4,5]);
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,15 +3,24 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = new Set(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    const set = new Set(numbers);
+    if(set.size !== numbers.length){
+      throw new Error('[ERROR] 로또 번호는 증복될 수 없습니다.');
+    }
+    numbers.forEach(value => {
+      if (value < 1 || 45 < value) {
+        throw new Error('[ERROR] 로또번호는 1~45의 숫자만 가능합니다.');
+      }
+    });
   }
-
+  
   // TODO: 추가 기능 구현
 }
 
