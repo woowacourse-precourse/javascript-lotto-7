80a32a4f24ecbc23449f7d4ee85b31ea6bc1d172
nerdchanii <nerdchanii@gmail.com>
2024-11-04 21:09:43 +0900

    fix(view): add missing space

--- src/constants/message.js
+++ src/constants/message.js
@@ -2,7 +2,7 @@ export const NEW_LINE = '\n';
 
 export const INPUT_MESSAGE = Object.freeze({
   PURCAHSE_AMOUNT: '구입금액을 입력해 주세요.\n',
-  WINNING_NUMBERS: '당첨번호를 입력해 주세요.\n',
+  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
   BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
 });
 
