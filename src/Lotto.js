90721c628e36b36438fafb1a4fa56f0c25ae38e3
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-04 18:59:34 +0900

    refactor: 로또 발행 시 번호 유효성 검사 재활용

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+import Validation from "./Validation.js";
 import { ERROR_MESSAGE as ERROR } from "./constants/errorMessage.js";
 
 class Lotto {
@@ -9,17 +10,11 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error(ERROR.INVALID_NUMBERS_LENGTH);
-    }
-
-    if (numbers.some((number) => number < 1 || number > 45)) {
-      throw new Error(ERROR.INVALID_NUMBERS_RANGE);
-    }
-
-    if (numbers.some((number) => isNaN(number))) {
-      throw new Error(ERROR.NOT_NUMBER);
-    }
+    Validation.checkNumbersLength(numbers);
+    numbers.forEach((number) => {
+      Validation.checkIsNumber(number);
+      Validation.checkNumberRange(number);
+    })
   }
 
   // TODO: 추가 기능 구현
