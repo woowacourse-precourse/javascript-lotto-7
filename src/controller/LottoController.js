7362ae132407bfe80d858e72d6ed8c570d8b5cf1
suhwa <tnghk9611@naver.com>
2024-11-04 15:09:38 +0900

    refactor(LottoController): 컨트롤러 로직 추상화 및 함수 분리

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -10,32 +10,16 @@ import OutputView from "../view/OutputView.js";
 class LottoController {
   async execute() {
     const purchaseMoney = await this.#repeatUntilValidInput(() => this.#getPurChaseMoney());
-
-    // 로또 머신을 생성
-    const lottoMachine = new LottoMachine(purchaseMoney);
-
-    // 로또 생성 후 생성된 로또 배열을 반환
-    const lottoHistory = lottoMachine.generateLotto();
-
-    // 로또 출력
-    const purchaseHistory = lottoHistory.getPurchaseHistory();
+    const purchaseHistory = this.#purchaseLotto(purchaseMoney);
 
     OutputView.printPurchaseInfo(purchaseHistory.lottoCount, purchaseHistory.lottos);
 
-    // 사용자가 올바른 입력을 할 때까지 당첨 번호 입력
     const winningNumbers = await this.#repeatUntilValidInput(() => this.#getWinningNumbers());
-
-    // 사용자가 올바른 입력을 할 때까지 보너스 번호 입력
     const bonusNumber = await this.#repeatUntilValidInput(() =>
       this.#getBonusNumber(winningNumbers)
     );
 
-    // 등수 계산기 생성
-    const rankCalculator = new RankCalculator(purchaseHistory, winningNumbers, bonusNumber);
-
-    // 당첨 등수 구하기
-    const rankResult = rankCalculator.calculate();
-
+    const rankResult = this.#calculateRank(purchaseHistory, winningNumbers, bonusNumber);
     OutputView.printRankResult(rankResult.getLottoRankResult());
   }
 
@@ -66,6 +50,17 @@ class LottoController {
       OutputView.printError(error.message);
       return this.#repeatUntilValidInput(callback);
     }
+  }
+
+  #purchaseLotto(purchaseMoney) {
+    const lottoMachine = new LottoMachine(purchaseMoney);
+    const lottoHistory = lottoMachine.generateLotto();
+    return lottoHistory.getPurchaseHistory();
+  }
+
+  #calculateRank(purchaseHistory, winningNumbers, bonusNumber) {
+    const rankCalculator = new RankCalculator(purchaseHistory, winningNumbers, bonusNumber);
+    return rankCalculator.calculate();
   }
 }
 
