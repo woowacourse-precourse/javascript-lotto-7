42040a86a9427b1ebd209e0af5c597f9d20e3128
suhwa <tnghk9611@naver.com>
2024-11-04 17:25:02 +0900

    refactor(OutputView): 등수를 출력하는 방식 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -16,11 +16,11 @@ class OutputView {
   static printRankResult(lottoRankResult) {
     Console.print("\n당첨 통계\n---");
     const rank = lottoRankResult.rank;
-    Console.print(`3개 일치 (5,000원) - ${rank.fifth[0]}개`);
-    Console.print(`4개 일치 (50,000원) - ${rank.fourth[0]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${rank.third[0]}개`);
-    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank.second[0]}개`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${rank.first[0]}개`);
+    Console.print(`3개 일치 (5,000원) - ${rank.fifth.getCount()}개`);
+    Console.print(`4개 일치 (50,000원) - ${rank.fourth.getCount()}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${rank.third.getCount()}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank.second.getCount()}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${rank.first.getCount()}개`);
     Console.print(`총 수익률은 ${lottoRankResult.profit}%입니다.`);
   }
 }
