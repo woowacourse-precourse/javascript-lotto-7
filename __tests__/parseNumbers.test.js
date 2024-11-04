53a0beab398163266f4d6a5cca7471476ceda42c
Jaeho Cha <ischar@naver.com>
2024-11-04 21:08:20 +0900

    test(parse): update parseNumbers test to change  input and output
    - parseNumbers 테스트 변경
      - 정상적으로 값을 반환하는 지 확인 테스트의 input, expected 변경

--- __tests__/parseNumbers.test.js
+++ __tests__/parseNumbers.test.js
@@ -3,8 +3,8 @@ import { ERROR } from "../src/config/config.js";
 
 describe("parseNumbers 테스트", () => {
   test("정상적으로 값을 반환하는 지 확인", () => {
-    const INPUT = "1,2,3,4";
-    const EXPECTED = [1, 2, 3, 4];
+    const INPUT = "1,2,3,4,5,6";
+    const EXPECTED = [1, 2, 3, 4, 5, 6];
     expect(parseNumbers(INPUT)).toEqual(EXPECTED);
   });
 
