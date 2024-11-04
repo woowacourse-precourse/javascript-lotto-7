0d864b45a64fd7408b85829789fe2bbf24efe345
Jang Eunhye <hh961130@naver.com>
2024-11-04 16:40:11 +0900

    test: Lotto 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,9 +11,20 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    
+    if (numbers.some(number => number < 1) || numbers.some(number => 45 < number)) {
+      throw new Error("[ERROR] 로또 번호는 1 - 45 만 가능합니다.");
+    }
+    // 중복숫자
+    let setNumbers = new Set(numbers);
+    if (numbers.length !== setNumbers.size) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+    // 문자 포함
+    if (numbers.some(number => isNaN(number))) {
+      throw new Error("[ERROR] 로또 번호에 문자가 포함될 수 없습니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
 }
-
 export default Lotto;
