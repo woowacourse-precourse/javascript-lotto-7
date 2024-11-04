b2a05ac51eb3cc4726ad3f63e347b2d7a2eeda2e
yheseul <kangyheseul@naver.com>
2024-11-04 10:30:13 +0900

    fix: 로또 당첨번호 구분자 검증 테스트 케이스 수정 - 입력 값 예시를 '1/2/3'에서 '1/2/3/4/5/6'으로 변경하여 더 정확한 검증

--- __tests__/LottoWinningNumbersTest.js
+++ __tests__/LottoWinningNumbersTest.js
@@ -1,8 +1,9 @@
-import { WINNING_NUMBERS } from "../src/constants/validationMessages/winningNumbers";
+import { WINNING_NUMBERS } from "../src/constants/validationMessages/winningNumbers.js";
+import { validateWinningNumbersPipe } from "../src/validation/validateWinningNumbers/validateWinningNumbersPipe.js";
 
 describe("로또 당첨 번호 테스트", () => {
   test.each([
-    ["1/2/3", WINNING_NUMBERS.INVALID_SEPARATOR],
+    ["1/2/3/4/5/6", WINNING_NUMBERS.INVALID_SEPARATOR],
     ["q,w,e", WINNING_NUMBERS.NOT_A_NUMBER],
     ["1,2,3,4,5,1", WINNING_NUMBERS.DUPLICATE_NUMBERS],
     ["1,2,3,4,5,90", WINNING_NUMBERS.OUT_OF_RANGE],
