4ab395d6e516709743928084d88e5e2ad2854f08
keemsebeen <sebin0580@gmail.com>
2024-11-04 13:10:02 +0900

    feat(Output) : LottoNumber 출력 함수 추가

--- src/io/Output.js
+++ src/io/Output.js
@@ -1,11 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
+import { WINNING_RANK } from "../constants/message.js";
 
 export const getOutput = async (message) => {
   return await Console.print(message);
 };
 
-export const getTicketNumber = async (tickets) => {
-  for (const ticket of tickets) {
-    await Console.print(ticket);
+export const getLottoNumber = async (lottos) => {
+  for (const lotto of lottos) {
+    await Console.print(lotto);
   }
 };
