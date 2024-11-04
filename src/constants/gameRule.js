85e22311e82fe88ce27b552e052677f18813a29e
Daeuning <happyjde3@gmail.com>
2024-11-04 17:08:25 +0900

    fix(retryOnError): 무한대로 재입력을 받지 않도록 수정

--- src/constants/gameRule.js
+++ src/constants/gameRule.js
@@ -4,17 +4,45 @@ const GAME_RULES = Object.freeze({
   MAX_LOTTO_NUMBER: 45, // 로또 번호의 최대값 (포함)
   LOTTO_NUMBER_COUNT: 6,
   DELIMITER: ',',
+  MAX_RETRIES: 1000,
 });
 
 const PRIZE_CRITERIA = Object.freeze({
-  RANK1: { rank: 1, matchCount: 6, isBonusMatched: false, prize: 2000000000, description: "6개 일치 (2,000,000,000원)" },
-  RANK2: { rank: 2, matchCount: 5, isBonusMatched: true, prize: 30000000, description: "5개 일치, 보너스 볼 일치 (30,000,000원)" },
-  RANK3: { rank: 3, matchCount: 5, isBonusMatched: false, prize: 1500000, description: "5개 일치 (1,500,000원)" },
-  RANK4: { rank: 4, matchCount: 4, isBonusMatched: false, prize: 50000, description: "4개 일치 (50,000원)" },
-  RANK5: { rank: 5, matchCount: 3, isBonusMatched: false, prize: 5000, description: "3개 일치 (5,000원)" }
+  RANK1: {
+    rank: 1,
+    matchCount: 6,
+    isBonusMatched: false,
+    prize: 2000000000,
+    description: '6개 일치 (2,000,000,000원)',
+  },
+  RANK2: {
+    rank: 2,
+    matchCount: 5,
+    isBonusMatched: true,
+    prize: 30000000,
+    description: '5개 일치, 보너스 볼 일치 (30,000,000원)',
+  },
+  RANK3: {
+    rank: 3,
+    matchCount: 5,
+    isBonusMatched: false,
+    prize: 1500000,
+    description: '5개 일치 (1,500,000원)',
+  },
+  RANK4: {
+    rank: 4,
+    matchCount: 4,
+    isBonusMatched: false,
+    prize: 50000,
+    description: '4개 일치 (50,000원)',
+  },
+  RANK5: {
+    rank: 5,
+    matchCount: 3,
+    isBonusMatched: false,
+    prize: 5000,
+    description: '3개 일치 (5,000원)',
+  },
 });
 
-export {
-  GAME_RULES,
-  PRIZE_CRITERIA,
-};
+export { GAME_RULES, PRIZE_CRITERIA };
--- src/utils/retryOnError.js
+++ src/utils/retryOnError.js
@@ -1,13 +1,19 @@
-import { printMessage } from "./console.js";
+import { printMessage, throwError } from './console.js';
+import { GAME_RULES } from '../constants/gameRule.js';
 
 async function retryOnError(func) {
-  while (true) {
+  let attempts = 0;
+
+  while (attempts < GAME_RULES.MAX_RETRIES) {
     try {
       return await func();
     } catch (error) {
-      printMessage(error.message); 
+      printMessage(error.message);
+      attempts += 1;
     }
   }
+
+  throwError('최대 재입력 횟수를 넘어가셨습니다');
 }
 
 export default retryOnError;
