775d2cc10ae15c53038d1ea3cd974ee0ceb143ea
suhwa <tnghk9611@naver.com>
2024-11-04 15:52:59 +0900

    refactor(App): 다른 컨트롤러를 주입

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import LottoController from "./controller/LottoController.js";
+import LottoPurchaseController from "./controller/LottoPurchaseController.js";
+import LottoResultController from "./controller/LottoResultController.js";
+import WinningNumberController from "./controller/WinningNumberController.js";
 
 class App {
   async run() {
-    await new LottoController().execute();
+    await new LottoController(
+      new LottoPurchaseController(),
+      new WinningNumberController(),
+      new LottoResultController()
+    ).execute();
   }
 }
 
