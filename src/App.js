0f952e4a6ecae6854b3d2c593beb6660eef07edd
rhtjddls123 <jjjk0605@naver.com>
2024-11-04 17:31:58 +0900

    feat: 구입 금액을 입력받아 발행할 로또 개수를 반환하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
+  static ERROR_MESSAGE = {
+    MONEY_INPUT_ERROR: '올바른 숫자를 입력해주세요!',
+    MONEY_INPUT_MINIMUM_ERROR: '0이상의 숫자를 입력해주세요!',
+    MONEY_INPUT_DIVISIBILITY_ERROR:
+      '1000으로 나누어 떨어지는 숫자를 입력해주세요!',
+  };
+
   async run() {}
 
   getRandomLottoNumber() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  async inputMoney() {
+    const moneyInput = await this.input('구입금액을 입력해 주세요.');
+    return this.parseMoney(moneyInput);
+  }
+
+  parseMoney(moneyInput) {
+    const money = Number(moneyInput);
+    this.validateMoney(money);
+    return this.calculateLottoCount(money);
+  }
+
+  validateMoney(money) {
+    if (Number.isNaN(money)) {
+      throw Error(`[Error] ${App.ERROR_MESSAGE.MONEY_INPUT_ERROR}`);
+    }
+    if (money < 0) {
+      throw Error(`[Error] ${App.ERROR_MESSAGE.MONEY_INPUT_MINIMUM_ERROR}`);
+    }
+    if (money % 1000 !== 0) {
+      throw Error(
+        `[Error] ${App.ERROR_MESSAGE.MONEY_INPUT_DIVISIBILITY_ERROR}`,
+      );
+    }
+  }
+
+  calculateLottoCount(money) {
+    return money / 1000;
+  }
+
+  async input(msg) {
+    const input = await Console.readLineAsync(`${msg}\n`);
+    return input;
   }
 }
 
