b520b548b9d5946799692a7a2796c2c72b1729db
underthemo-onlight <underthemo_onlight@naver.com>
2024-11-04 22:20:55 +0900

    refactor: 로또 컨트롤러로 분리

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import Lottos from "./Lottos.js";
-import InputView from "./views/InputView.js";
-import OutputView from "./views/OutputView.js";
-import Validator from "./Validator.js";
-import Matcher from "./Matcher.js";
+import LottoController from "./LottoConstoller.js";
 
 class App {
+  #lottoController = new LottoController();
+
   async run() {
-    const moneyPaid = await InputView.readMoneyPaid();
-    const countGame = this.getCount(moneyPaid);
-    OutputView.writeLottoCounts(countGame);
-
-    const lottos = new Lottos(countGame).getLottos();
-    OutputView.writeLottos(lottos)
-
-    const winningNumbers = await InputView.readWinningNumbers();
-    const bonusNumber = await InputView.readBonusNumber();
-    Validator.validateWinningNumbersWithBonusNumber(winningNumbers, bonusNumber);
-
-    const matcher = this.#generateMatcher(lottos, winningNumbers, bonusNumber)
-    const { matchResult, matchFiveNumbersWithBonusNumber } = matcher.getResults();
-
-    OutputView.writeMatchStatistics(matchResult, matchFiveNumbersWithBonusNumber);
-    OutputView.writeRateOfReturn(matchResult, matchFiveNumbersWithBonusNumber, moneyPaid);
-  }
-
-  getCount(moneyPaid) {
-    return moneyPaid / 1000;
-  }
-
-  #generateMatcher(lottos, winningNumbers, bonusNumber) {
-    return new Matcher(lottos, winningNumbers, bonusNumber);
+    await this.#lottoController.gameStart();
   }
 }
 
---
+++ src/LottoConstoller.js
@@ -0,0 +1,36 @@
+import Lottos from "./Lottos.js";
+import InputView from "./views/InputView.js";
+import OutputView from "./views/OutputView.js";
+import Validator from "./Validator.js";
+import Matcher from "./Matcher.js";
+
+class LottoController {
+    async gameStart() {
+        const moneyPaid = await InputView.readMoneyPaid();
+        const countGame = this.getCount(moneyPaid);
+        OutputView.writeLottoCounts(countGame);
+
+        const lottos = new Lottos(countGame).getLottos();
+        OutputView.writeLottos(lottos)
+
+        const winningNumbers = await InputView.readWinningNumbers();
+        const bonusNumber = await InputView.readBonusNumber();
+        Validator.validateWinningNumbersWithBonusNumber(winningNumbers, bonusNumber);
+
+        const matcher = this.#generateMatcher(lottos, winningNumbers, bonusNumber)
+        const { matchResult, matchFiveNumbersWithBonusNumber } = matcher.getResults();
+
+        OutputView.writeMatchStatistics(matchResult, matchFiveNumbersWithBonusNumber);
+        OutputView.writeRateOfReturn(matchResult, matchFiveNumbersWithBonusNumber, moneyPaid);
+    }
+
+    getCount(moneyPaid) {
+        return moneyPaid / 1000;
+    }
+
+    #generateMatcher(lottos, winningNumbers, bonusNumber) {
+        return new Matcher(lottos, winningNumbers, bonusNumber);
+    }
+}
+
+export default LottoController;
