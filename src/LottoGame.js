254a396b68dfdcedcc68e34c2c4c186a8f014410
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2024-11-04 20:12:20 +0900

    feat: 결과출력 및 수익률 계산 기능 추가

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -92,6 +92,74 @@ class LottoGame {
     }
     this.bonusNumber = bonus;
   }
+
+  #isValidLottoNumbers(numbers) {
+    return numbers.every((num) => num >= 1 && num <= 45);
+  }
+
+  #getMatchCount(ticket) {
+    return ticket
+      .getNumbers()
+      .filter((num) => this.winningNumbers.includes(num)).length;
+  }
+
+  #hasBonusNumber(ticket) {
+    return ticket.getNumbers().includes(this.bonusNumber);
+  }
+
+  #printLottoTickets() {
+    Console.print(`${this.tickets.length}개를 구매했습니다.`);
+    this.tickets.forEach((ticket) => {
+      Console.print(`[${ticket.getNumbers().join(", ")}]`);
+    });
+  }
+
+  #printResults() {
+    const results = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
+
+    this.tickets.forEach((ticket) => {
+      const matchCount = this.#getMatchCount(ticket);
+      const hasBonus = this.#hasBonusNumber(ticket);
+
+      if (matchCount === 6) results.first += 1;
+      else if (matchCount === 5 && hasBonus) results.second += 1;
+      else if (matchCount === 5) results.third += 1;
+      else if (matchCount === 4) results.fourth += 1;
+      else if (matchCount === 3) results.fifth += 1;
+    });
+
+    this.#printWinningStatistics(results);
+  }
+
+  #printWinningStatistics(results) {
+    Console.print("당첨 통계\n---");
+    Console.print(`3개 일치 (5,000원) - ${results.fifth}개`);
+    Console.print(`4개 일치 (50,000원) - ${results.fourth}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${results.third}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${results.second}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${results.first}개`);
+    this.#printYield(results);
+  }
+
+  #printYield(results) {
+    const prizeMoney =
+      results.fifth * 5000 +
+      results.fourth * 50000 +
+      results.third * 1500000 +
+      results.second * 30000000 +
+      results.first * 2000000000;
+    const purchaseAmount = this.tickets.length * 1000;
+    const yieldPercentage = ((prizeMoney / purchaseAmount) * 100).toFixed(1);
+    Console.print(`총 수익률은 ${yieldPercentage}%입니다.`);
+  }
 }
 
 export default LottoGame;
