7435277048bd2dffff9b4e047e6c922a058d3567
Eunicekk <cho_lyn@naver.com>
2024-11-04 19:00:58 +0900

    [feat] Lotto Controller 구조 생성

---
+++ src/controllers/LottoController.js
@@ -0,0 +1,16 @@
+export class LottoController {
+	// 실행 함수
+	run() {}
+
+	// 로또 발행 함수
+	issueLotto() {}
+
+	// 로또 번호 생성 함수
+	createLottoNumbers() {}
+
+	// 당첨 결과 반환하는 함수
+	getLottoResult() {}
+
+	// 수익율 계산하는 함수
+	calculateProfitRate() {}
+}
