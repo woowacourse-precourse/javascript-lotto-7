ffadce50a7105592e8fdb0b2f95c65cd9f321f19
이서현 <mellifluous3380@gmail.com>
2024-11-04 22:38:09 +0900

    feat: 보너스 번호를 포함하는 로또 확인 기능

--- src/VerifyLotto.js
+++ src/VerifyLotto.js
@@ -1,8 +1,13 @@
-export function verifyLotto(lottoArr, correctNum) {
+export function verifyLotto(lottoArr, correctNum, bonus) {
     const result = Array(7).fill(0);
 
     for (let i = 0; i < lottoArr.length; i++) {
         let outcome = numOfCorrected(lottoArr[i], correctNum);
+        // 보너스 번호를 포함하는 경우
+        if (outcome == 4 && chkBonusLotto(lottoArr[i], bonus)) {
+            result[2] += 1;
+            continue;
+        }
         if (outcome > 2) result[outcome] += 1;
     }
     return result;
@@ -14,4 +19,11 @@ function numOfCorrected(nums, correctNum) {
         if (nums.includes(correctNum[i])) cnt++;
     }
     return cnt;
+}
+
+function chkBonusLotto(nums, bonus) {
+    for (let i = 0; i < 6; i++) {
+        if (nums[i] == bonus) return true;
+    }
+    return false;
 }
