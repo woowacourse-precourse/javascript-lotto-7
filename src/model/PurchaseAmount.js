3fba13c48d99838733bbdd902a4f59bc134ad421
nerdchanii <nerdchanii@gmail.com>
2024-11-04 11:49:10 +0900

    feat(model): implement PurchaseAmount

--- src/model/PurchaseAmount.js
+++ src/model/PurchaseAmount.js
@@ -1,12 +1,23 @@
+const LOTTO_PURCHASE_UNIT = 1000;
 class PurcahseAmount{
   #amount;
-  
+
   constructor(amount){
     this.#validate(amount);
-    this.#amount = amount;
+    this.#amount = Number(amount);
   }
 
-  #validate(amount){}
+  #validate(amount){
+    const numericStringRegex = /^[\d]+$/;
+    if (!numericStringRegex.test(amount)){
+      throw new Error('[ERROR] 당첨금액은 숫자로만 입력가능합니다.');
+    }
+    if (!isUnitFormmat(amount, LOTTO_PURCHASE_UNIT)) {
+      throw new Error('[ERROR] 당첨금액은 1000원 단위로 입력해야합니다.');
+    }
+  }
 
-
+  get amount () {
+    return this.amount;
+  }
 }
