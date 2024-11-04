837aa5ed16ac5c487da3f73b9fe326df1fe4cd5e
zelkovaria <hyein396@gmail.com>
2024-11-04 21:01:35 +0900

    refactor: OutputView 출력 메시지 상수화

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -4,6 +4,16 @@ export const INPUT_VIEWS_MESSAGE = Object.freeze({
   PRINT_INPUT_BONUS_NUMBER: "보너스 번호를 입력해 주세요.\n",
 });
 
+export const OUTPUT_VIEWS_MESSAGE = Object.freeze({
+  PRINT_OUTPUT_COUNT: (lottoCount) => `${lottoCount}개를 구매했습니다.`,
+  PRINT_STATISTICS_START: "당첨 통계\n---",
+  PRINT_STATISTICS_MATCH: (number, price, count) =>
+    `${number}개 일치 (${price.toLocaleString()}원) - ${count}개`,
+  PRINT_STATISTICS_MATCH_BONUS: (price, count) =>
+    `5개 일치, 보너스 볼 일치 (${price.toLocaleString()}원) - ${count}개`,
+  PRINT_EARNINGS_RATE: (earningsRate) => `총 수익률은 ${earningsRate}%입니다.`,
+});
+
 export const ERROR_MESSAGE = Object.freeze({
   INVALID_LOTTO_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
   INVALID_DUPLICATE_NUMBER: "[ERROR] 로또 번호는 중복없이 입력되어야 합니다.",
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 import { LOTTO_STATISTICS } from "../constants/Statistics.js";
+import { OUTPUT_VIEWS_MESSAGE } from "../constants/Message.js";
 
 export default class OutputView {
   static async printLottoCount(lottoCount) {
-    return await Console.print(`${lottoCount}개를 구매했습니다. `);
+    return await Console.print(
+      OUTPUT_VIEWS_MESSAGE.PRINT_OUTPUT_COUNT(lottoCount)
+    );
   }
 
   static printLottos(lottos) {
@@ -13,24 +16,24 @@ export default class OutputView {
   }
 
   static printLottoStatistics(totalSpent) {
-    Console.print("당첨 통계\n---");
+    Console.print(OUTPUT_VIEWS_MESSAGE.PRINT_STATISTICS_START);
 
     Object.entries(LOTTO_STATISTICS).forEach(
       ([key, { number, price, count }]) => {
         if (key === "bonus") {
           Console.print(
-            `5개 일치, 보너스 볼 일치 (${price.toLocaleString()}원) - ${count}개`
+            OUTPUT_VIEWS_MESSAGE.PRINT_STATISTICS_MATCH_BONUS(price, count)
           );
         } else {
           Console.print(
-            `${number}개 일치 (${price.toLocaleString()}원) - ${count}개`
+            OUTPUT_VIEWS_MESSAGE.PRINT_STATISTICS_MATCH(number, price, count)
           );
         }
       }
     );
 
     const earningsRate = OutputView.calculateEarningsRate(totalSpent);
-    Console.print(`총 수익률은 ${earningsRate}%입니다.`);
+    Console.print(OUTPUT_VIEWS_MESSAGE.PRINT_EARNINGS_RATE(earningsRate));
   }
 
   static calculateEarningsRate(totalSpent) {
