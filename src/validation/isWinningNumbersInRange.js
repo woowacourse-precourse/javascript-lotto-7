2a199ea91e40c0b4a0670f4db8a157e258060fa4
yheseul <kangyheseul@naver.com>
2024-11-04 19:53:16 +0900

    refactor: 콤마 상수화 적용

--- src/validation/isWinningNumbersInRange.js
+++ src/validation/isWinningNumbersInRange.js
@@ -1,10 +1,11 @@
 import {
+  WINNING_NUMBER_DELIMITER,
   WINNING_NUMBER_MAX,
   WINNING_NUMBER_MIN,
 } from "../constants/lottoConstants.js";
 
 export const isWinningNumbersInRange = function (winningNumbers, errorMessage) {
-  const winningNumbersArray = winningNumbers.split(",");
+  const winningNumbersArray = winningNumbers.split(WINNING_NUMBER_DELIMITER);
   winningNumbersArray.map((winningNumber) => {
     const isValid =
       winningNumber >= WINNING_NUMBER_MIN &&
