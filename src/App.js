90db10a6b07fd763845a0a7cd6f82d0cffd6bcce
syg0629 <syg0629@hanmail.net>
2024-11-04 20:44:22 +0900

    feat(useInput): 사용자 입력 처리 - 구입금액 - 당첨 번호 - 보너스 번호

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import UserInput from "./UserInput.js";
+
 class App {
-  async run() {}
+  #userInput;
+
+  constructor() {
+    this.#userInput = new UserInput();
+  }
+
+  async run() {
+    const amount = await this.#userInput.getUserInput("purchaseAmount");
+    const numbers = await this.#userInput.getUserInput("winningNumber");
+    const bonus = await this.#userInput.getUserInput("bonusNumber");
+  }
 }
 
 export default App;
