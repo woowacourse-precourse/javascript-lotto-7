66d6717492d22b050d82b45a720fc43577eb4ecd
unknown <kevin123488@naver.com>
2024-11-04 14:50:10 +0900

    feat(input): 보너스 번호 입력 기능 개발

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import Lotto from './Lotto.js';
 
+
+// 사용자가 문자열로 입력하는 값을 , 를 바탕으로 쪼개어 리스트에 담아 리턴
 function parse_numbers(INPUT_NUM) {
-  // 사용자가 문자열로 입력하는 값을 , 를 바탕으로 쪼개어 리스트에 담아 리턴
   INPUT_NUM.split(',').map((input) => {
     try {
       const PARSED_INPUT = parseInt(input.trim(), 10);
@@ -16,6 +17,24 @@ function parse_numbers(INPUT_NUM) {
   })
 };
 
+
+// 보너스번호 입력값 유효성 검사
+function check_bonus(BONUS_NUM, NUMBERS) {
+  try {
+    if (isNaN(BONUS_NUM) || BONUS_NUM > 45 || BONUS_NUM < 1) {
+      throw new Error("[ERROR] 보너스번호를 잘못 입력하였습니다.")
+    }
+    NUMBERS.forEach((num) => {
+      if (num === BONUS_NUM) {
+        throw new Error("[ERROR] 로또 번호와 동일한 보너스번호를 입력하였습니다.");
+      }
+    })
+  } catch (error) {
+    throw error;
+  }
+};
+
+
 class App {
   async run() {
     const LOTTO = new Lotto();
@@ -23,6 +42,9 @@ class App {
     const NUMBERS = parse_numbers(INPUT_NUM); // 입력값 유효성 1차 검증
     await LOTTO.constructor(NUMBERS);
     const BONUS_NUM = MissionUtils.Console.readLineAsync();
+    
+    // 보너스번호 입력값 유효성 검사
+    check_bonus(BONUS_NUM, NUMBERS);
   }
 }
 
