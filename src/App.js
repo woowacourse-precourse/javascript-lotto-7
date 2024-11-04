f18cb1676658966249ec497ea9f03e5dcc149fe1
ongheong <pudding4271@gmail.com>
2024-11-04 21:24:03 +0900

    feat: 기본 생성자 및 메소드 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
+
 class App {
-  async run() {}
+  constructor() {
+    this.lottos = [];
+    this.answerNumbers = [];
+    this.bonusNumber = null;
+    this.result = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
+  }
+
+  async run() {
+    try {
+      const moneyAmount = await this.getMoneyAmount();
+      const lottoCount = this.calculateLottoCount(moneyAmount);
+      this.printLottos();
+      await this.getAnswerNumbers();
+      await this.getBonusNumber();
+      this.calculateLottoResults();
+      this.printResults(moneyAmount);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+    }
+  }
+
+  async getMoneyAmount() {
+  };
+
+  calculateLottoCount(moneyAmount) {
+  }
+
+  printLottos(){
+  }
+
+  async getAnswerNumbers() {
+  }
+
+  async getBonusNumber() {
+  }
+
+  calculateLottoResults() {
+  }
+
+  printResults(moneyAmount) {
+  }
 }
 
 export default App;
