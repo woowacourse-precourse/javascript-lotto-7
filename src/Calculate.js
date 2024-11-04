f8453cdc96a7eecd26f22ac4435d40ebfef20737
osw6858 <osw7875@naver.com>
2024-11-04 11:21:17 +0900

    feat: 로또 결과를 출력하도록 수정

--- src/Calculate.js
+++ src/Calculate.js
@@ -76,5 +76,16 @@ export class Calculate {
   printResults() {
     const { winningStats, totalPrize } = this.#calculateAllResults();
     const returnRate = this.#calculateReturnRate(totalPrize);
+
+    Console.print('\n당첨 통계');
+    Console.print('---');
+    Console.print(`3개 일치 (5,000원) - ${winningStats[4]}개`);
+    Console.print(`4개 일치 (50,000원) - ${winningStats[3]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${winningStats[2]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningStats[1]}개`,
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${winningStats[0]}개`);
+    Console.print(`총 수익률은 ${returnRate}%입니다.`);
   }
 }
