7affd2b612174926d3cc6428b3d5d12967adfd80
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 22:03:22 +0900

    feat: 구입 금액 입력에 대한 에외 처리 로직 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,6 +6,10 @@ const InputView = {
     const input = await Console.readLineAsync();
     const amount = parseInt(input);
     
+    if (isNaN(amount) || amount % 1000 !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위여야 합니다.")
+    }
+    
     return amount;
   }
 }
