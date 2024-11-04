106143b46f71cf18330c5c544b6768186821494f
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-04 23:27:59 +0900

    test: LottoTest 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,39 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("6개 번호가 일치하면 1 반환", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getResult([1, 2, 3, 4, 5, 6], 7)).toBe(1);
+  });
+
+  test("5개 번호와 보너스 번호가 일치하면 2 반환", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getResult([1, 2, 3, 4, 5, 9], 6)).toBe(2);
+  });
+
+  test("5개 번호만 일치하면 3 반환", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 8]);
+    expect(lotto.getResult([1, 2, 3, 4, 5, 10], 7)).toBe(3);
+  });
+
+  test("4개 번호가 일치하면 4 반환", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 9, 10]);
+    expect(lotto.getResult([1, 2, 3, 4, 15, 20], 7)).toBe(4);
+  });
+
+  test("3개 번호가 일치하면 5 반환", () => {
+    const lotto = new Lotto([1, 2, 3, 8, 9, 10]);
+    expect(lotto.getResult([1, 2, 3, 15, 20, 25], 7)).toBe(5);
+  });
+
+  test("일치하는 번호가 2개 이하일 경우 -1 반환", () => {
+    const lotto = new Lotto([1, 2, 8, 9, 10, 11]);
+    expect(lotto.getResult([3, 4, 5, 6, 7, 12], 7)).toBe(-1);
+  });
 });
