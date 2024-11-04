568e6f6fb0c9395184b5ecbea2b41896a509cb06
Ko Daeun <drechsu@gmail.com>
2024-11-04 21:40:15 +0900

    feat(getWinningNumbers): 불필요한 try/catch문 삭제

--- src/getInput/getWinningNumbers.js
+++ src/getInput/getWinningNumbers.js
@@ -1,16 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export default async function getWinningNumber() {
-  try {
-    const winningNumber = await Console.readLineAsync(
-      "당첨 번호를 입력해 주세요.\n"
-    );
-    const winningNumberArr = winningNumber
-      .split(",")
-      .map((number) => Number(number));
+  const winningNumber = await Console.readLineAsync(
+    "당첨 번호를 입력해 주세요.\n"
+  );
+  const winningNumberArr = winningNumber
+    .split(",")
+    .map((number) => Number(number));
 
-    return winningNumberArr;
-  } catch (error) {
-    throw Error(error);
-  }
+  return winningNumberArr;
 }
