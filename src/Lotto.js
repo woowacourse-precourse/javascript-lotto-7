95c4a844763251351ce94b28adf35281e208a0d3
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-04 23:13:33 +0900

    feat: 로또 당첨 결과 매칭

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,32 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 중복된 숫자가 있습니다.');
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getResult(resultNumbers, bonusNumber) {
+    const matchCount = this.#numbers.filter((number) => resultNumbers.includes(number)).length;
+    const isBonusMatched = resultNumbers.includes(bonusNumber);
+
+    if (matchCount === 6) {
+      return 1;
+    }
+    if (isBonusMatched && matchCount === 5) {
+      return 2;
+    }
+    if (matchCount === 5) {
+      return 3;
+    }
+   if (matchCount === 4) {
+     return 4;
+   }
+   if (matchCount === 3) {
+     return 5;
+   }
+    return -1;
+  }
 }
 
 export default Lotto;
