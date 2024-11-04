de299f3bfcb284e9b613103a889b7867e12f55f7
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-11-04 11:15:52 +0000

    feat: MESSAGES 입출력 메세지 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Console } from "@woowacourse/mission-utils";
+
 const MESSAGES = Object.freeze({
   INPUT: {
-    LOTTO_AMOUT: "구입금액을 입력해 주세요.\n",
-    
+    PURCHASE_LOTTO_MONEY: "구입금액을 입력해 주세요.\n",
+    WIN_NUMBERS: "당첨 번호를 입력해 주세요.\n",
+    BOUNS_NUMBER: "보너스 번호를 입력해 주세요.\n",
   },
   OUTPUT: {
-
+    PURCHASE_LOTTO_NUMBER: (n) => `${n}개를 구매했습니다.`,
+    WIN_RATE: "당첨 통계\n---\n",
+    WIN_DETAIL_5th: (n) => `3개 일치 (5,000원) - ${n}개\n`,
+    WIN_DETAIL_4th: (n) => `4개 일치 (50,000원) - ${n}개\n`,
+    WIN_DETAIL_3rd: (n) => `5개 일치 (1,500,000원) - ${n}개\n`,
+    WIN_DETAIL_2nd: (n) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${n}개\n`,
+    WIN_DETAIL_1st: (n) => `6개 일치 (2,000,000,000원) - ${n}개`,
+    PROFIT_RATE: (profitRate) => `총 수익률은 ${profitRate}%입니다.\n`
   },
   ERROR: {
-
+    PREFIX: "[ERROR] ",
   }
 });
 
