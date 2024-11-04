73d08f615899b3622e0d645a86a945f6f1786a46
Injun Jeong <jijun897@naver.com>
2024-11-04 15:34:52 +0900

    fix(app): 입력받을 때 에러가 발생하면 에러 처리 후 그 부분부터 시작하도록 수정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   LOTTO_STRING,
   WINNING_LOTTO_STRING,
   BONUS_NUMBER_STRING,
+  BUY_ERROR,
 } from "./Constant.js";
 import {
   makeLottos,
@@ -17,24 +18,48 @@ import {
 
 class App {
   async run() {
-    const buyCount = await MissionUtils.Console.readLineAsync(
-      BUY_STRING + ENTER
-    );
-    buyCountErrorCheck(buyCount);
+    let valid = false;
+    let buyCount = 0;
+    while (!valid) {
+      try {
+        buyCount = await MissionUtils.Console.readLineAsync(BUY_STRING + ENTER);
+        buyCountErrorCheck(buyCount);
+        valid = true;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
 
     const lottoCount = buyCount / 1000;
     MissionUtils.Console.print(lottoCount + LOTTO_STRING);
     const lottos = makeLottos(lottoCount);
 
-    const winningLottoString = await MissionUtils.Console.readLineAsync(
-      ENTER + WINNING_LOTTO_STRING + ENTER
-    );
-
-    const winningLotto = makeWinningLotto(winningLottoString);
-    const bonusNumber = await MissionUtils.Console.readLineAsync(
-      ENTER + BONUS_NUMBER_STRING + ENTER
-    );
-    winningLotto.bonusErrorCheck(bonusNumber);
+    valid = false;
+    let winningLotto;
+    while (!valid) {
+      try {
+        const winningLottoString = await MissionUtils.Console.readLineAsync(
+          ENTER + WINNING_LOTTO_STRING + ENTER
+        );
+        winningLotto = makeWinningLotto(winningLottoString);
+        valid = true;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
+    valid = false;
+    let bonusNumber;
+    while (!valid) {
+      try {
+        bonusNumber = await MissionUtils.Console.readLineAsync(
+          ENTER + BONUS_NUMBER_STRING + ENTER
+        );
+        winningLotto.bonusErrorCheck(bonusNumber);
+        valid = true;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
 
     const equalCounts = calculateLottos(lottos, winningLotto, bonusNumber);
     printResult(equalCounts);
