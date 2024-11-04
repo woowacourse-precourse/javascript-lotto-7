c80693d94cce9cf64d1986944d9c1c8608139d28
AlgeMoya <kjhobd2@naver.com>
2024-11-04 19:16:36 +0900

    feat: 당첨 통계 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -24,6 +24,17 @@ class App {
         lottoInstances[i].checkWinStatus(winningNumbers, bonusNumber)
       ]++;
     }
+
+    Console.print("");
+    Console.print("당첨 통계");
+    Console.print("---");
+    Console.print(`3개 일치 (5,000원) - ${lottoResult[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${lottoResult[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${lottoResult[5]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult[7]}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${lottoResult[6]}개`);
   }
 
   async getPurchaseAmount() {
