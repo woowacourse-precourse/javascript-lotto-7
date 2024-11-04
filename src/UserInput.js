eda274a9255aa97ab66e79aff3c41024a1bca1af
kochungwon <yud01063@gmail.com>
2024-11-04 09:13:39 +0900

    refactor(UserInput):UserInput 클래스에 입력 유효성 검사 및 오류 처리 로직 리팩토링 - inputPurchaseAmount, inputWinningNumbers, inputBonusNumber 메서드에 입력 반복 처리 추가 - try-catch 블록을 통해 잘못된 입력 시 오류 메시지를 출력하고 재입력 요청 - InputValidator를 사용하여 입력값을 검증하고 유효하지 않을 경우 재입력 기능 구현

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,5 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
-import InputValidator from './InputValidator.js'; 
+import InputValidator from './InputValidator.js';
 
 class UserInput {
     constructor() {
@@ -11,23 +11,44 @@ class UserInput {
 
     // 로또 구입 금액 입력
     async inputPurchaseAmount() {
-        const input = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
-        this.validator.validateAmount(input); 
-        this.purchaseAmount = Number(input);
+        while (true) {
+            try {
+                const input = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
+                this.validator.validateAmount(input);
+                this.purchaseAmount = Number(input);
+                break;
+            } catch (error) {
+                Console.print(error.message);
+            }
+        }
     }
 
     // 당첨 번호 입력
     async inputWinningNumbers() {
-        const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
-        this.winningNumbers = input.split(',').map(num => parseInt(num.trim(), 10));
-        this.validator.validateWinningNumbers(this.winningNumbers); 
+        while (true) {
+            try {
+                const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
+                this.winningNumbers = input.split(',').map(num => parseInt(num.trim(), 10));
+                this.validator.validateWinningNumbers(this.winningNumbers);
+                break;
+            } catch (error) {
+                Console.print(error.message);
+            }
+        }
     }
 
     // 보너스 번호 입력
     async inputBonusNumber() {
-        const input = await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
-        this.validator.validateBonusNumber(input, this.winningNumbers);
-        this.bonusNumber = Number(input); 
+        while (true) {
+            try {
+                const input = await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
+                this.validator.validateBonusNumber(input, this.winningNumbers);
+                this.bonusNumber = Number(input);
+                break; 
+            } catch (error) {
+                Console.print(error.message);
+            }
+        }
     }
 }
 
