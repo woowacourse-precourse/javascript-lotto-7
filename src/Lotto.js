c10e5a0ac328a506db853fcc735a4884a592bc1c
GhKTW <rlaxodn00100@naver.com>
2024-11-04 23:49:54 +0900

    feat: 주요 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,20 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const uniqueNumbers = new Set(numbers); 
+    if (uniqueNumbers.size !== 6) { 
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다."); }
   }
 
-  // TODO: 추가 기능 구현
-}
+  getNumbers() {
+    return this.#numbers;
+  }
 
+  static getMatchedCount(numbers, winNumbers) {
+    let numbersToArray = numbers.getNumbers();
+    return numbersToArray.filter(num => winNumbers.includes(num)).length;
+  }
+   
+
+}
 export default Lotto;
