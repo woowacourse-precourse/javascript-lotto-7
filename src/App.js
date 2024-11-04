1229d3d6eb454904a0303f3e2c9950a629ba09cf
HW-K <rlaql99@gmail.com>
2024-11-04 20:27:38 +0900

    Issue the Lotto number
    - Except duplicate numbers
    - Limit it to a number between 1 and 45

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
+  static PRICE_PER_TICKET = 1000;
+
   async run() {
     try {
       const money = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
@@ -15,6 +18,9 @@ class App {
     if (!Number.isInteger(money) || money <= 0 || money % App.PRICE_PER_TICKET !== 0) {
       throw new Error("[ERROR] 구입 금액은 1,000원 단위의 양의 정수여야 합니다.");
     }
+    
+    const count = money / App.PRICE_PER_TICKET;
+    return Array.from({ length: count }, () => Lotto.generateLotto());
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -12,9 +12,18 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다.");
+    }
+    if (!numbers.every((num) => num >= 1 && num <= 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  static generateLotto() {
+    const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return new Lotto(numbers);
+  }
 }
 
 export default Lotto;
