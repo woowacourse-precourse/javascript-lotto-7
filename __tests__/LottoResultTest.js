a915d093d93ba01ac17e3bf93240af9b226a9039
choiihwa <chldlghk@gmail.com>
2024-11-04 23:16:41 +0900

    test(lottoResult): 로또 당첨 수익률 출력 테스트 추가

--- __tests__/LottoResultTest.js
+++ __tests__/LottoResultTest.js
@@ -3,6 +3,7 @@ import Lotto from '../src/lotto/data/Lotto.js';
 import lottoConfig from '../src/config.js';
 import OutputPrinter from '../src/lotto/view/OutputPrinter.js';
 import LottoResultManager from '../src/lotto/model/LottoResultManager.js';
+import RateCalculator from '../src/lotto/model/RateCalculator.js';
 
 const winningRules = lottoConfig.WINNING_RULES;
 
@@ -72,5 +73,35 @@ describe('로또 결과 출력 테스트', () => {
     OutputPrinter.printWinningResults(winningResults);
 
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+
+  describe('로또 당첨 수익률 테스트', () => {
+    test.each([
+      {
+        name: '로또 당첨 금액과 구입 금액에 따라서 수익률이 출력된다.',
+        purchaseAmount: 8000,
+        lottos: [
+          new Lotto([8, 21, 23, 41, 42, 43]),
+          new Lotto([3, 5, 11, 16, 32, 38]),
+          new Lotto([7, 11, 16, 35, 36, 44]),
+          new Lotto([1, 8, 11, 31, 41, 42]),
+          new Lotto([13, 14, 16, 38, 42, 45]),
+          new Lotto([7, 11, 30, 40, 42, 43]),
+          new Lotto([2, 13, 22, 32, 38, 45]),
+          new Lotto([1, 3, 5, 14, 22, 45]),
+        ],
+        winningNumbers: [1, 2, 3, 4, 5, 6],
+        bonusNumber: 7,
+        log: '총 수익률은 62.5%입니다.',
+      }
+    ])(`$name`, ({ purchaseAmount, lottos, winningNumbers, bonusNumber, log }) => {
+      const logSpy = getLogSpy();
+      const lottoResultManager = new LottoResultManager(winningRules);
+      lottoResultManager.generateWinningResult(lottos, winningNumbers, bonusNumber);
+
+      const rateOfReturn = RateCalculator.calculateRateOfReturn(lottoResultManager.getTotalPrize(), purchaseAmount);
+      OutputPrinter.printTotalRateOfReturn(rateOfReturn);
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
   });
 });
