9bf36a68455d53155678feb063a4fece25361fa8
hienjoy <wisdom16no@naver.com>
2024-11-04 17:19:24 +0900

    test: 로또 클래스 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,20 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호에 1~45가 아닌 번호가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 51]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("유효한 생성 번호 테스트", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 8]);
+    expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 8]);
+  });
 });
