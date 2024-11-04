b5d13d4545025628237042f4b7cdeced804c80cb
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-11-04 17:23:34 +0900

    chore(Lotto): 함수 구분을 위한 주석 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,12 +6,18 @@ import IOProcessor from './IOProcessor.js';
 class Lotto {
   #numbers;
 
+  /**
+   *
+   */
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
     this.#numbers.sort((a, b) => a - b);
   }
 
+  /**
+   *
+   */
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
