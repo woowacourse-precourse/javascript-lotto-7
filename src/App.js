9dff05b576e9aecc2954e2eaac0a6ae18727714d
nerdchanii <nerdchanii@gmail.com>
2024-11-04 18:32:02 +0900

    feat(app): implement entry point

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import View from './view/view.js';
+import io from './utils/io.js';
+import LottoController from './lotto.controller.js';
+import LottoService from './lotto.service.js';
 class App {
-  async run() {}
+  async run() {
+    const lottoController = new LottoController(new View(io), LottoService);
+    await lottoController.run();
+  }
 }
 
 export default App;
