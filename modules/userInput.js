c23310fff4ef507b7c3a1da43044f6f52f2fc602
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-04 20:51:22 +0900

    feat: 구입 금액 입력 함수 추가
    userInput.js 파일에 이하 내용 추가:
    - 구입 금액 입력 함수 getValidatedPurchaseAmount 함수 추가
    - 입력값 유효성 검사 중 오류 발생 시, 오류 메시지 출력 후 재입력

---
+++ modules/userInput.js
@@ -0,0 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+import { purchaseAmountValidate } from "./inputValidator";
+
+export async function getValidatedPurchaseAmount() {
+  while (true) {
+    const purchaseAmount = await Console.readLineAsync(
+      "구입 금액을 입력해 주세요.\n"
+    );
+
+    try {
+      purchaseAmountValidate(purchaseAmount);
+      return purchaseAmount;
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+}
