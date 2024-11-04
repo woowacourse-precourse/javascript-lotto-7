8a9bd9f70f24b1e7ec5342f5d4ac607bf0349c05
HW-K <rlaql99@gmail.com>
2024-11-04 20:22:21 +0900

    Input purchase amount
    - Except for incorrect input value

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const money = await Console.readLineAsync("구입 금액을 입력해 주세요.\n");
+      const lottos = this.purchaseLottos(Number(money));
+
+    } catch (error) {
+      Console.print(error.message); 
+    }
 }
+
+  purchaseLottos(money) {
+    if (!Number.isInteger(money) || money <= 0 || money % App.PRICE_PER_TICKET !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위의 양의 정수여야 합니다.");
+    }
+  }
+}
+
 
 export default App;
