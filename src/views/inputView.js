73a8ec13cfee3aa6d053a0be402a213816b72b9f
jinoo <jinoo0306@naver.com>
2024-11-04 17:12:26 +0900

    feat(3) : 로또 당첨 번호와 보너스 번호를 입력 받는 inputView 파일 생성

--- src/views/inputView.js
+++ src/views/inputView.js
@@ -1,9 +1,19 @@
 import { Console } from "@woowacourse/mission-utils";
-import { INPUT_MESSAGES } from "../utils/message.js";
+import { INPUT_MESSAGES } from "../utils/constants.js";
 
 const inputView = {
   async readPurchaseAmount() {
     const input = await Console.readLineAsync(INPUT_MESSAGES.PURCHASE_AMOUNT);
+    return input.trim();
+  },
+
+  async readWinningNumbers() {
+    const input = await Console.readLineAsync(INPUT_MESSAGES.WINNING_NUMBERS);
+    return input.trim();
+  },
+
+  async readBonusNumber() {
+    const input = await Console.readLineAsync(INPUT_MESSAGES.BONUS_NUMBER);
     return input.trim();
   },
 };
