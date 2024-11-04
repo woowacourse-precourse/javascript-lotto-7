293aaa9e94f6b275346aa9766817103ae804312d
changyeon <changyeon@changyeonui-MacBookAir.local>
2024-11-04 20:58:38 +0900

    Lotto purchase method

--- src/Lotto.js
+++ src/Lotto.js
@@ -35,3 +35,33 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호에 중복된 숫자가 없어야 합니다.");
     }
   }
+
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  static generateRandomLotto() {
+    const numbers = MissionUtilsRandom.pickUniqueNumbersInRange(1, 45, 6);
+    return new Lotto(numbers);
+  }
+
+  async getInputMoney() {
+    const inputMoney = await Console.readLineAsync("로또 구입 금액을 입력해주세요.(천원 단위로 입력해주세요)\n");
+    const amount = parseInt(inputMoney, 10);
+    if (isNaN(amount) || amount % 1000 !== 0) {
+      Console.print("[ERROR] 금액은 천 원 단위로 입력해야 합니다.");
+      throw new Error("[ERROR] 금액은 천 원 단위로 입력해야 합니다.");
+    }
+    return amount;
+  }
+
+  purchaseLottos(count) {
+    Console.print(`${count}개를 구매했습니다.`);
+    const lottos = [];
+    for (let i = 0; i < count; i++) {
+      const lotto = Lotto.generateRandomLotto();
+      lottos.push(lotto);
+      Console.print(`[${lotto.getNumbers().join(", ")}]`);
+    }
+    return lottos;
+  }
