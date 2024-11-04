1ce648c7326805b57308aee20e2cc56cec0d64c4
taeyomi <terry11ty@naver.com>
2024-11-04 18:15:31 +0900

    feat(validation): 구입금액 문자열일경우 유효성검사 추가

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,12 @@
 import ERROR_MESSAGE from "./constants/error.js";
 
 export const validatePurchasingAmount = (userInputAmounts) => {
-  if (userInputAmounts % 1000 !== 0) {
-    throw new Error(ERROR_MESSAGE.INPUT_AMOUNT);
-  } else if (userInputAmounts <= 0) {
+  // 입력이 숫자가 아닌 경우, 또는 1000으로 나누어 떨어지지 않는 경우, 또는 음수나 0인 경우 예외 발생
+  if (
+    !/^\d+$/.test(userInputAmounts) ||
+    userInputAmounts <= 0 ||
+    userInputAmounts % 1000 !== 0
+  ) {
     throw new Error(ERROR_MESSAGE.INPUT_AMOUNT);
   }
 };
