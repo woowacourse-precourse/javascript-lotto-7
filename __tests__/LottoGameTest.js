3bacab0f23179e3b748e790c3bfe374cf3b44fbd
jeongwoo903 <www8565@naver.com>
2024-11-04 17:22:13 +0900

    test(LottoGame): LottoGame의 매서드 들에 대한 단위 테스트 추가

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -1,5 +1,4 @@
 import LottoGame from '../src/domain/LottoGame.js';
-import { ERROR_MESSAGES } from '../src/constants/messages.js';
 import { LOTTO_CONDITION } from '../src/constants/constants.js';
 
 describe('로또 게임 테스트', () => {
@@ -8,10 +7,10 @@ describe('로또 게임 테스트', () => {
 
   beforeEach(() => {
     lottoGame = new LottoGame();
-    lottoGame.setLottos(tickets);
   });
 
   test(`${tickets}장의 로또에 숫자 배열 길이가 ${LOTTO_CONDITION.length}개가 맞는지 확인한다.`, () => {
+    lottoGame.setLottos(tickets);
     const lottos = lottoGame.getLottos();
 
     lottos.forEach((lotto) => {
@@ -19,4 +18,66 @@ describe('로또 게임 테스트', () => {
       expect(lotto).toHaveLength(LOTTO_CONDITION.length);
     });
   });
+
+  test(`유저의 로또 번호의 당첨 여부를 확인한다. - 당첨 번호만 맞은 경우`, () => {
+    const compareResults = [{ matchingCount: 3, hasBonusNumber: false }];
+    const result = lottoGame.calculateStatics(compareResults);
+
+    expect(result).toEqual({
+      fifth_prize: 1,
+      fourth_prize: 0,
+      third_prize: 0,
+      second_prize: 0,
+      first_prize: 0,
+    });
+  });
+
+  test(`유저의 로또 번호의 당첨 여부를 확인한다. - 당첨 번호와 보너스 번호 모두 맞은 경우`, () => {
+    const compareResults = [{ matchingCount: 5, hasBonusNumber: true }];
+    const result = lottoGame.calculateStatics(compareResults);
+
+    expect(result).toEqual({
+      fifth_prize: 0,
+      fourth_prize: 0,
+      third_prize: 0,
+      second_prize: 1,
+      first_prize: 0,
+    });
+  });
+
+  test(`당첨된 로또의 합산 금액을 제대로 구하는 지 확인한다. - 로또 1개를 사서, 5등인 경우`, () => {
+    const totalPrizeAmount = lottoGame.getTotalPrizeAmount({
+      fifth_prize: 1,
+      fourth_prize: 0,
+      third_prize: 0,
+      second_prize: 0,
+      first_prize: 0,
+    });
+
+    expect(totalPrizeAmount).toEqual(5_000);
+  });
+
+  test(`당첨된 로또의 합산 금액을 제대로 구하는 지 확인한다. - 로또 3개를 사서, 1,2,3등인 경우`, () => {
+    const totalPrizeAmount = lottoGame.getTotalPrizeAmount({
+      fifth_prize: 0,
+      fourth_prize: 0,
+      third_prize: 1,
+      second_prize: 1,
+      first_prize: 1,
+    });
+
+    expect(totalPrizeAmount).toEqual(2_031_500_000);
+  });
+
+  test.each([
+    [5_000, 8, '62.5'],
+    [15_000, 3, '500.0'],
+    [1_500_000, 10, '15,000.0'],
+  ])(
+    '총 합산 금액 "%s"에 대해서, 티켓 %s 장을 구매했을때, 수익률은 %s%여야 한다.',
+    (totalPrizeAmount, tickets, profitRatio) => {
+      lottoGame.setLottos(tickets);
+      expect(lottoGame.getProfitRatio(totalPrizeAmount)).toEqual(profitRatio);
+    },
+  );
 });
