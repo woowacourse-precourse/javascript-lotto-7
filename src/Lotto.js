931153abc81c8e4c3b6805b37656d71959d95115
pengdyuo <121721388+Pengdyuo@users.noreply.github.com>
2024-11-04 11:55:16 +0900

    Update Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
+    }
+
+    numbers.forEach((number) => {
+      if (typeof number !== "number" || number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return [...this.#numbers];
+  }
 }
 
 export default Lotto;
