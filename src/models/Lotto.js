ef2ae87864b829acc6a1316ec66f0eb7a461cc2a
Yusi <yusi4781@naver.com>
2024-11-04 16:35:51 +0900

    feat(Lotto): 로또 클래스 예외 상황 추가

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -10,6 +10,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const testNumbers = new Set(numbers);
+    if (testNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
+    }
+    if (numbers.some(num => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
