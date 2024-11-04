bb5b702cb1267b32c48ecdc8f4f0d0ef5439722f
beomjo <dongjun1315@naver.com>
2024-11-04 12:45:27 +0900

    feat(#ErrorMessage) : 에러 메시지 상수화해서 초기 구현

---
+++ src/constant/ErrorMessage.js
@@ -0,0 +1,6 @@
+const ErrorMessage = {
+    inputCashInvalidValue : "[ERROR] 구입 금액은 1000원 단위로 입력해 주세요.",
+    inputZeroCashValue : "[ERROR] 로또 구매를 해주세요.",
+    inputLottoNumbersInvalid : "[ERROR] 로또 번호는 1 ~ 45사이의 자연수여야 합니다.",
+    nullBonusNumber : "[ERROR] 보너스 번호를 입력해주세요."
+};
