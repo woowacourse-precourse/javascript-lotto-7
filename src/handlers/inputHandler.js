6174d7008340210a43fdad6edc3843e626aa7856
Jaeho Cha <ischar@naver.com>
2024-11-04 14:39:44 +0900

    chore(input): add space

--- src/handlers/inputHandler.js
+++ src/handlers/inputHandler.js
@@ -3,6 +3,7 @@ import { ERROR } from "../config/config.js";
 
 export const inputHandler = async (message) => {
   Console.print(message);
+
   const input = await Console.readLineAsync("");
 
   if (input === "") {
