3ac93219261bd957e151121142728146c93e875d
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 23:17:37 +0900

    fix: 메서드 이름 오타 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,7 +1,7 @@
 import { Console } from "@woowacourse/mission-utils"
 
 const OutputView = {
-  printLottoTickes(tickets) {
+  printLottoTickets(tickets) {
     Console.print(`\n${tickets.length}개를 구매했습니다.`);
     tickets.forEach((ticket) => Console.print(`[${ticket.numbers.join(", ")}]`));
   },
