1c9fa187cbba164e2fdbcf93b3e7dc43b70ddfaf
Aing-bim <yeonju0908@gmail.com>
2024-11-04 17:21:28 +0900

    feat: App 클래스 구현 및 프로그램 실행 흐름 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import InputView from './views/InputView.js';
+import OutputView from './views/OutputView.js';
+import LottoService from './services/LottoService.js';
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await InputView.getPurchaseAmount();
+    const ticketCount = Math.floor(purchaseAmount / 1000);
+    const lottos = Array.from({ length: ticketCount }, () => LottoService.generateLotto());
+
+    OutputView.displayLottoCount(ticketCount);
+    OutputView.displayLottoNumbers(lottos);
+
+    const winningNumbers = await InputView.getWinningNumbers();
+    const bonusNumber = await InputView.getBonusNumber();
+
+    const results = LottoService.checkWinning(lottos, winningNumbers, bonusNumber);
+    const summary = results.reduce((acc, result) => {
+      if (result) acc[result] = (acc[result] || 0) + 1;
+      return acc;
+    }, {});
+
+    OutputView.displayWinningStatistics(summary);
+
+    const totalPrize = LottoService.calculateTotalPrize(summary);
+    const yieldRate = ((totalPrize / purchaseAmount) * 100).toFixed(1);
+    OutputView.displayYield(yieldRate);
+  }
 }
 
 export default App;
+
