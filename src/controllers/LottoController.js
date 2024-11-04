2f7c7d3e614dbfbd93555ad6d680718d3e3d63b0
Eunicekk <cho_lyn@naver.com>
2024-11-04 19:05:29 +0900

    [feat] 로또 발행 메서드 구현

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -1,9 +1,21 @@
+import Lotto from "../models/Lotto";
+
 export class LottoController {
 	// 실행 함수
 	run() {}
 
 	// 로또 발행 함수
-	issueLotto() {}
+	issueLotto(price) {
+		const lottoList = [];
+		const lottoCount = price / 1000;
+
+		for (let i = 0; i < lottoCount; i++) {
+			const randomNumbers = this.createLottoNumbers();
+			lottoList.push(new Lotto(randomNumbers));
+		}
+
+		return lottoList;
+	}
 
 	// 로또 번호 생성 함수
 	createLottoNumbers() {}
