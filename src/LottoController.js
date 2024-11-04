204f0b83567aab9bd5187a2e99c03674622652ba
YeWon Seo <syw1236@naver.com>
2024-11-04 13:01:24 +0900

    feat(LottoController): 오름차순 정렬 기능 구현

--- src/LottoController.js
+++ src/LottoController.js
@@ -5,12 +5,14 @@ import Lotto from "./Lotto.js";
 export default class LottoController {
 
     static getUserLotto(lottoCount) {
-        return Array.from({ length: lottoCount }, () => RandomNum.getNumber()); 
+        return Array.from({ length: lottoCount }, () => RandomNum.getNumber().sort((a,b) => a-b)); 
     }
 
     static createrWinningLottoNumbers(winningNumbers) {
         const winningNumbersList = Parser.separaterLottoNumber(winningNumbers);
         return new Lotto(winningNumbersList);
     }
+
+
 
 }
