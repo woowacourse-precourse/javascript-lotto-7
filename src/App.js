3fc1259012aa0b52ee274706ead93da2e399a73b
YeRimmm-Cho <em514@dgu.ac.kr>
2024-11-04 16:23:23 +0900

    Feat(App): 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const purchaseAmount = await this.getPurchaseAmount();
+      MissionUtils.Console.print(`${purchaseAmount}`);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      await this.run(); // 금액 입력 재시도
+    }
+  }
+
+  async getPurchaseAmount() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    const amount = Number(input);
+
+    // 금액이 숫자고 1,000원 단위의 양수인지 확인
+    if (isNaN(amount) || amount <= 0 || amount % 1000 !== 0) {
+      throw new Error("[ERROR] 금액은 1,000원 단위의 양수로 입력해 주세요.");
+    }
+
+    return amount;
+  }
 }
 
 export default App;
