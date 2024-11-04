cb20f8937418d66761636584bd360f8fceb73a43
AlgeMoya <kjhobd2@naver.com>
2024-11-04 19:08:59 +0900

    feat: 보너스 번호 입력, 검증 기능 구현

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ class App {
     Console.print(`${purchaseAmount / 1000}개를 구매했습니다.`);
     const lottoInstances = createLottoInstances(purchaseAmount / 1000);
 
+    // 로또 번호 출력
     for (let i = 0; i < lottoInstances.length; i++) {
       lottoInstances[i].showNumber();
     }
 
     const winningNumbers = await this.getWinningNumbers();
+
+    Console.print("");
+    Console.print("보너스 번호를 입력해 주세요.");
+    const bonusInput = await Console.readLineAsync("");
+    const bonusNumber = LottoInputValidator.validateBonusNumber(
+      bonusInput,
+      winningNumbers
+    );
   }
 
   async getPurchaseAmount() {
--- src/validators.js
+++ src/validators.js
@@ -1,5 +1,10 @@
 export class LottoInputValidator {
   static validatePurchaseAmount(amount) {
+    // 입력값이 비어있는지 확인
+    if (!amount.trim()) {
+      throw new Error("[ERROR] 구입 금액이 입력되지 않았습니다.");
+    }
+
     // 입력받은 값이 숫자가 아닌지 확인
     if (isNaN(amount)) {
       throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
@@ -21,7 +26,17 @@ export class LottoInputValidator {
   }
 
   static validateWinningNumbers(numbers) {
+    // 입력값이 비어있는지 확인
+    if (!numbers.trim()) {
+      throw new Error("[ERROR] 당첨 번호가 입력되지 않았습니다.");
+    }
+
     const parsedNumbers = numbers.split(",").map((num) => {
+      const trimmed = num.trim();
+      if (!trimmed) {
+        throw new Error("[ERROR] 빈 값은 입력할 수 없습니다.");
+      }
+
       const parsed = parseInt(num.trim());
       if (isNaN(parsed)) {
         throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
@@ -41,5 +56,33 @@ export class LottoInputValidator {
     }
 
     return parsedNumbers;
+  }
+
+  static validateBonusNumber(bonusNumber, winningNumbers) {
+    // 입력값이 비어있는지 확인
+    if (!bonusNumber.trim()) {
+      throw new Error("[ERROR] 보너스 번호가 입력되지 않았습니다.");
+    }
+
+    // 여러 개의 숫자가 입력되었는지 확인
+    if (bonusNumber.includes(",")) {
+      throw new Error("[ERROR] 보너스 번호가 여러 개 입력되었습니다.");
+    }
+
+    const parsed = parseInt(bonusNumber);
+
+    if (isNaN(parsed)) {
+      throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
+    }
+
+    if (parsed < 1 || parsed > 45) {
+      throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    if (winningNumbers.includes(parsed)) {
+      throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+
+    return parsed;
   }
 }
