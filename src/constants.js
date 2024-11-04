972a9341d1384009b1f25263c6301fa9515c236c
gwansikk <gwansik.kim@gwansik.dev>
2024-11-04 21:27:49 +0900

    feat(constants): 로또 번호 에러 메세지 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -28,6 +28,7 @@ export const OUTPUT_MESSAGE = {
 export const ERROR_MESSAGE = {
   LOTTO_CHECK_LENGTH: `로또 번호는 ${LOTTO_LENGTH}개여야 합니다.`,
   LOTTO_CHECK_NUMBER: '로또 번호는 숫자이어야 합니다.',
+  LOTTO_CHECK_DUPLICATES: `로또 번호는 중복되지 않아야 합니다.`,
   LOTTO_CHECK_NUMBER_RANGE: `로또 번호는 ${LOTTO_NUMBER_START}부터 ${LOTTO_NUMBER_END} 사이의 숫자여야 합니다.`,
   LOTTO_CHECK_PURCHASE_AMOUNT: (price) =>
     `구입 금액은 ${price}원 이상이어야 합니다.`,
