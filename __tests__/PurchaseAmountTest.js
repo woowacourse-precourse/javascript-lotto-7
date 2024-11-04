05fe347a8e8c835b19ab32d10269f3b1388d7bce
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-11-04 18:59:59 +0900

    test: 구매수량 클래스 테스트 에러메세지 상수로 변경

--- __tests__/PurchaseAmountTest.js
+++ __tests__/PurchaseAmountTest.js
@@ -1,4 +1,5 @@
 import PurchaseAmount from '../src/PurchaseAmount.js';
+import { ERROR_MESSAGE } from '../src/constants/error.js';
 
 describe('PurchaseAmount 클래스 테스트', () => {
   test('올바른 금액을 입력하면 로또 구매 개수를 계산한다', () => {
@@ -9,19 +10,19 @@ describe('PurchaseAmount 클래스 테스트', () => {
   test('숫자가 아닌 입력의 경우 예외가 발생한다', () => {
     expect(() => {
       new PurchaseAmount('abc');
-    }).toThrow('[ERROR] 구매 금액은 숫자여야 합니다.');
+    }).toThrow(ERROR_MESSAGE.INVALID_NUMBER);
   });
 
   test('1000원 미만 금액 입력 시 예외가 발생한다', () => {
     expect(() => {
       new PurchaseAmount(900);
-    }).toThrow('[ERROR] 구매 금액은 1000원 이상이어야 합니다.');
+    }).toThrow(ERROR_MESSAGE.MIN_AMOUNT);
   });
 
   test('1000원 단위가 아닌 금액 입력 시 예외가 발생한다', () => {
     expect(() => {
       new PurchaseAmount(1500);
-    }).toThrow('[ERROR] 구매 금액은 1000원 단위여야 합니다.');
+    }).toThrow(ERROR_MESSAGE.DIVISIBLE_AMOUNT);
   });
 
   test('정상적인 여러 금액에 대해 올바른 로또 수량을 반환한다', () => {
