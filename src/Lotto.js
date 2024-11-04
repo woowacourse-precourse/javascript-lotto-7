097d892abae43b018f06ab44d59ea10e3b59013d
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2024-11-04 19:56:38 +0900

    feat: Lotto 클래스 추가 및 검증 로직 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,31 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if (!Array.isArray(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 배열이어야 합니다.");
+    }
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const hasDuplicate = new Set(numbers).size !== numbers.length;
+    if (hasDuplicate) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+
+    numbers.forEach((number) => {
+      if (!Number.isInteger(number)) {
+        throw new Error("[ERROR] 로또 번호는 정수여야 합니다.");
+      }
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1에서 45 사이여야 합니다.");
+      }
+    });
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
