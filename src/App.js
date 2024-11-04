0762824cb17b4744064d6ca6956097d4fd968776
chickenisamazing <dieswing@naver.com>
2024-11-04 22:53:50 +0900

    refactor: 사용자 입력을 받는 메서드 분리

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ class App {
   async run() {
     const userInput = new UserInput();
     try {
-      const price = await userInput.inputPrice();
-      const numbers = await userInput.inputWinningNumbers();
-      const lotto = new Lotto(numbers);
-      const bonusNum = await userInput.inputBonusNumber();
-      lotto.validateBonusNumber(bonusNum);
+      const price = await this.getUserPrice(userInput);
+      const { numbers, lotto } = await this.getUserWinningNumbers(userInput);
+      const bonusNum = await this.getUserBonusNumber(userInput, lotto);
 
       const bills = price / 1000;
       console.log("");
@@ -24,6 +22,22 @@ class App {
     } catch (error) {
       MissionUtils.Console.print(error.message);
     }
+  }
+
+  async getUserPrice(userInput) {
+    return await userInput.inputPrice();
+  }
+
+  async getUserWinningNumbers(userInput) {
+    const numbers = await userInput.inputWinningNumbers();
+    const lotto = new Lotto(numbers);
+    return { numbers, lotto };
+  }
+
+  async getUserBonusNumber(userInput, lotto) {
+    const bonusNum = await userInput.inputBonusNumber();
+    lotto.validateBonusNumber(bonusNum);
+    return bonusNum;
   }
 }
 
