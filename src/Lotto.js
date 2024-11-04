a037c7d4d68db332fad9f1b95178753c3d10feaf
mejin <wls4482@gmail.com>
2024-11-04 22:18:23 +0900

    feat(Lotto): 로또 티켓 생성 및 관리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,10 @@
+// 로또 티켓을 생성하고 번호를 관리하는 클래스
 class Lotto {
-  #numbers;
+  #numbers; // 프라이빗 필드로 선언
 
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#validate(numbers); // 유효성 검사
+    this.#numbers = numbers; //인스턴스 변수에 저장
   }
 
   #validate(numbers) {
@@ -11,7 +12,11 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자 6개여야 합니다.");
     }
   }
-
+  
+  // 프라이빗 필드에 접근할 수 있는 안전한 인터페이스를 제공
+  getNumbers(){
+    return this.#numbers; // #numbers 를 외부에 반환하여, 클래스 외부에서 로또 번호 목록을 읽을 수 있도록 한다.
+  }
   // TODO: 추가 기능 구현
 }
 
