a39ad7c815089afa111c66c748e1ce7c5b55e433
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 15:12:20 +0900

    fix: 공백 미입력된 부분 공백 추가

--- src/constants/objects.js
+++ src/constants/objects.js
@@ -8,7 +8,7 @@ export const MATCH_COUNTER = {
 
 export const OUTPUT_OBJ = [
     {content: "3개 일치 (5,000원) - "},
-    {content: "4개 일치 (50,000원) -"},
+    {content: "4개 일치 (50,000원) - "},
     {content: "5개 일치 (1,500,000원) - "},
     {content: "5개 일치, 보너스 볼 일치 (30,000,000원) - "},
     {content: "6개 일치 (2,000,000,000원) - "}]
