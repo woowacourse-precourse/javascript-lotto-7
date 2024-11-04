ba101acb3a3d4c1c9062d1a6bca5e72726f76e9e
Dobbymin <gmin990624@gmail.com>
2024-11-04 20:12:13 +0900

    feat(model): model 함수 모듈화 구현

---
+++ src/model/model.js
@@ -0,0 +1,22 @@
+import { calculateLottoCount } from './functions/calculateLottoCount.js';
+
+import {
+  initializeResults,
+  calculateResult,
+  calculateTotalProfit,
+  calculateProfitRate,
+} from './functions/calculateResult.js';
+
+import { createRandomLottoNumber } from './functions/createRandomNumber.js';
+
+import { validateLottoNumbers } from './functions/validation/validation.js';
+
+export {
+  calculateLottoCount,
+  initializeResults,
+  calculateResult,
+  calculateTotalProfit,
+  calculateProfitRate,
+  createRandomLottoNumber,
+  validateLottoNumbers,
+};
