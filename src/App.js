2c3307abf142d1b5eabd8ea55f7c113344b10329
JHLEE325 <ljntiger325@naver.com>
2024-11-04 14:08:18 +0900

    feat : 입력받는 함수를 이용하여 로또 구매하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { purchase } from "./Purchase.js";
+import { getInput } from "./Input.js";
+import { LOTTO } from "./constant.js";
 
 class App {
   async run() {
-    purchase(1000, 10000);
+    const budget = await getInput('구입금액을 입력해 주세요.');
+    const lotto = purchase(budget/LOTTO.PRICE);
+    const winnumInput = await getInput('당첨 번호를 입력해 주세요.');
+    const winnum = winnumInput.split(',').map(num => num.trim());
+    const bonusnum = await getInput('보너스 번호를 입력해 주세요.');
   }
 }
 
--- src/Purchase.js
+++ src/Purchase.js
@@ -1,9 +1,9 @@
 import Lotto from './Lotto.js';
 import { picknumber } from './Random.js';
 import { LOTTO } from './constant.js';
+import { Console } from '@woowacourse/mission-utils';
 
-export const purchase = (price, budget) => {
-    const trynum = parseInt(budget / price);
+export const purchase = (trynum) => {
     const lottoTickets = [];
 
     for (let i = 0; i < trynum; i++) {
@@ -12,5 +12,18 @@ export const purchase = (price, budget) => {
         lottoTickets.push(lotto);
     }
 
+    printpurchase(trynum);
+    printnum(lottoTickets);
+
     return lottoTickets;
+}
+
+const printpurchase = (trynum) => {
+    Console.print(`${trynum}개를 구매했습니다.`);
+}
+
+const printnum = (lottotickets) => {
+    lottotickets.forEach(lotto => {
+        Console.print(`[${lotto.getNumbers().join(', ')}]`);
+    });
 }
