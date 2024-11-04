f7d9bcad264b4e0afd8d6ff254fe6b9d3d799569
AlgeMoya <kjhobd2@naver.com>
2024-11-04 18:18:15 +0900

    feat: 로또 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     Console.print("구입금액을 입력해 주세요.");
+    let purchaseAmount = await Console.readLineAsync("");
   }
 }
 
