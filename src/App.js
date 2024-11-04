1599a7e47fa44651a54ab6a2b4550b052d1828a0
Lim-shweety <smile70200@naver.com>
2024-11-04 14:30:56 +0900

    feat App.js : 로또 구입 금액 입력 처리
    구입 금액 입력 받고, 예외 처리하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils"
 class App {
-  async run() {}
+  async run() {
+    const USER_MONEY = await this.getMoney();
+
+  }
+
+  async getMoney(){
+    let inputMoney = await Console.readLineAsync("구입금액을 입력해 주세요.");
+    return this.checkMoney(Number(inputMoney));
+  }
+
+  checkMoney(money){
+    if(isNaN(money)){
+      throw new Error("[ERROR] 입력된 값이 숫자가 아닙니다.");
+    }
+    if(money%1000!==0){
+      throw new Error("[ERROR] 로또 구입금액이 1000원 단위가 아닙니다.")
+    }
+    return money;
+  }
 }
 
 export default App;
