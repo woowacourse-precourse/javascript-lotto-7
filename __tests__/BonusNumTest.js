dd9f9f143feb0dfea84fe35f99a94d0224121241
deokbin <deokbin@naver.com>
2024-11-04 16:24:56 +0900

    test(BonusNumTest): 보너스 숫자 로또번호와 중복될 경우 에러 케이스 테스트 코드 추가

--- __tests__/BonusNumTest.js
+++ __tests__/BonusNumTest.js
@@ -4,10 +4,15 @@ import { ErrorMessage } from '../src/constants/ErrorMessage.js';
 describe('BonusNum 유효성 검사', () => {
   test('유효한 보너스 숫자는 정상적으로 생성된다', () => {
     const mockLottoNums = { getNumbers: jest.fn().mockReturnValue([1, 2, 3, 4, 5, 6]) };
-
     expect(() => {
       new BonusNum(7, mockLottoNums);
     }).not.toThrow();
+  });
+  test('로또 당첨번호와 중복되는 보너스 숫자는 오류를 반환한다', () => {
+    const mockLottoNums = { getNumbers: jest.fn().mockReturnValue([1, 2, 3, 4, 5, 6]) };
+    expect(() => {
+      new BonusNum(3, mockLottoNums);
+    }).toThrowError(ErrorMessage.EXIST_LOTTONUM);
   });
 });
 
