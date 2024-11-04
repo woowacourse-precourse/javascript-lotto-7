c35a85ca5eb85716922aa49a2e1a9622bc7e0b95
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2024-11-04 20:14:44 +0900

    feat: 예외 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,32 @@
 import Lotto from "../src/Lotto";
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+describe("로또 테스트", () => {
+  test("유효한 로또 번호로 생성", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test("유효하지 않은 입력(비배열)로 생성 시 오류 발생", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+      new Lotto("1, 2, 3, 4, 5, 6");
+    }).toThrow("[ERROR] 로또 번호는 배열이어야 합니다.");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("유효하지 않은 입력(숫자 부족)으로 생성 시 오류 발생", () => {
+    expect(() => {
+      new Lotto([1, 2, 3]);
+    }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+  });
+
+  test("중복된 숫자가 있는 경우 오류 발생", () => {
+    expect(() => {
+      new Lotto([1, 2, 2, 4, 5, 6]);
+    }).toThrow("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+  });
+
+  test("유효하지 않은 숫자 범위로 생성 시 오류 발생", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR] 로또 번호는 1에서 45 사이여야 합니다.");
+  });
 });
