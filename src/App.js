a4d6fddffffd757879db499178f4005e6d897494
Junseo Kim <semiwest2@gmail.com>
2024-11-04 23:30:08 +0900

    feat: 에러메세지를 만나면 재입력으로 돌아가는 기능

--- src/App.js
+++ src/App.js
@@ -9,30 +9,55 @@ import {
   lottoProfitRate,
   printLottoStatistics
 } from './utils/lottoStatistics.js';
-
+import LOTTO_CONFIG from './constants/lottoConfig.js';
 
 class App {
   async run() {
-    const purchasePrice = Number(await getInput('구입금액을 입력해 주세요.\n'));
-    checkLottoPurchasePrice(purchasePrice);
-    const lottoPurchaseAmount = purchasePrice/1000;
+    let purchasePrice;
+    while (true) {
+      try {
+        purchasePrice = Number(await getInput('구입금액을 입력해 주세요.\n'));
+        checkLottoPurchasePrice(purchasePrice);
+        break;
+      } catch (error) {
+        printString(`${error.message}`);
+      }
+    }
+
+    const lottoPurchaseAmount = purchasePrice / LOTTO_CONFIG.TICKET_PRICE;
     printString(`\n${lottoPurchaseAmount}개를 구매했습니다.`);
 
     const lottoList = lottoGenerator(lottoPurchaseAmount);
     lottoList.forEach(lotto => {
       lotto.print();
-    })
+    });
 
+    let winningNumbers;
+    while (true) {
+      try {
+        winningNumbers = winningNumbersParser(await getInput('\n당첨 번호를 입력해 주세요.\n'));
+        break;
+      } catch (error) {
+        printString(`${error.message}`);
+      }
+    }
 
-    const winningNumbers = winningNumbersParser(await getInput('\n당첨 번호를 입력해 주세요.\n'));
     const winningLotto = new Lotto(winningNumbers);
-    const lottoBonusNumber = Number(await getInput('\n보너스 번호를 입력해 주세요.\n'));
-    checkLottoBonusNumber(winningLotto, lottoBonusNumber);
+    let lottoBonusNumber;
+    while (true) {
+      try {
+        lottoBonusNumber = Number(await getInput('\n보너스 번호를 입력해 주세요.\n'));
+        checkLottoBonusNumber(winningLotto, lottoBonusNumber);
+        break;
+      } catch (error) {
+        printString(`${error.message}`);
+      }
+    }
 
     printString('\n당첨 통계\n---');
-    const lottoResults= calculateLottoStatistics(lottoList, winningLotto, lottoBonusNumber);
+    const lottoResults = calculateLottoStatistics(lottoList, winningLotto, lottoBonusNumber);
     printLottoStatistics(lottoResults);
-    const profitRate = lottoProfitRate(purchasePrice, lottoResults)
+    const profitRate = lottoProfitRate(purchasePrice, lottoResults);
     printString(`총 수익률은 ${profitRate}%입니다.`);
   }
 }
