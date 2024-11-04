b057202818a105eb2e796f5e352906b7dd1d583e
jjang-aaa <jadoit473@gmail.com>
2024-11-04 13:43:18 +0900

    refactor(Lotto) : 에러문구 const로 담아서 사용하도록 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,13 +11,17 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const numberSizwErrorMessage = '[ERROR] 로또 번호는 6개여야 합니다.';
+
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error(numberSizwErrorMessage);
     }
 
     const set = new Set(numbers);
+    const numberDuplicateErrorMessage = '[ERROR] 로또 번호가 중복되었습니다. 다시 실행해주세요.';
+
     if (numbers.length !== set.size) {
-      throw new Error('[ERROR] 로또 번호가 중복되었습니다. 다시 실행해주세요.');
+      throw new Error(numberDuplicateErrorMessage);
     }
   }
 }
