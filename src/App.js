b519fe6a7f48db82e6cde92441756769b88fa468
chickenisamazing <dieswing@naver.com>
2024-11-04 14:18:33 +0900

    fix: Lotto 인스턴스 생성 순서 변경

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Lotto from "./Lotto.js";
 
 class App {
   async run() {
+    //사용자로부터 입력 받고 유효한지 검증하는 로직
     const userInput = new UserInput();
-    const price = await userInput.inputPrice();
-    const winNum = await userInput.inputWinningNumbers();
-    const bonusNum = await userInput.inputBonusNumber();
-    const lotto = new Lotto(winNum);
-    lotto.validateBonusNumber(bonusNum);
+    try {
+      const price = await userInput.inputPrice();
+      const numbers = await userInput.inputWinningNumbers();
+      // Lotto 인스턴스 생성
+      const lotto = new Lotto(numbers);
+      const bonusNum = await userInput.inputBonusNumber();
+      // console.log("당첨 번호", lotto);
+      lotto.validateBonusNumber(bonusNum);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 }
 
