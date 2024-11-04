4676042468c422457d92b9bb2ebdc570a57d6375
keemsebeen <sebin0580@gmail.com>
2024-11-04 13:45:14 +0900

    feat(Output) : 결과 출력 함수 구현

--- src/io/Output.js
+++ src/io/Output.js
@@ -10,3 +10,15 @@ export const getLottoNumber = async (lottos) => {
     await Console.print(lotto);
   }
 };
+export const getResult = async (winningStatus) => {
+  const formattedResults = WINNING_RANK.map(
+    ({ description, reward }, index) => {
+      const count = winningStatus[index];
+      return `${description} (${reward.toLocaleString()}원) - ${count}개`;
+    }
+  );
+
+  for (const resultMessage of formattedResults) {
+    await Console.print(resultMessage);
+  }
+};
