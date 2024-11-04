7232e1b1f370c85da2c3c8ebf51e2d18909ffa90
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2024-11-04 20:06:30 +0900

    feat: 당첨 번호 , 보너스 번호 입력 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -40,5 +40,50 @@ class LottoGame {
 
     return parsedAmount / 1000;
   }
+
+  async #getWinningNumbers() {
+    const numbersInput = await Console.readLineAsync(
+      "당첨 번호를 입력해 주세요.\n"
+    );
+    const numbers = numbersInput.split(",").map((num) => {
+      const parsedNum = Number(num.trim());
+
+      if (!Number.isInteger(parsedNum)) {
+        throw new Error("[ERROR] 당첨 번호는 정수여야 합니다.");
+      }
+      return parsedNum;
+    });
+
+    this.winningNumbers = numbers;
+
+    if (
+      this.winningNumbers.length !== 6 ||
+      !this.#isValidLottoNumbers(this.winningNumbers)
+    ) {
+      throw new Error(
+        "[ERROR] 당첨 번호는 1부터 45 사이의 숫자 6개여야 합니다."
+      );
+    }
+  }
+
+  async #getBonusNumber() {
+    const bonusInput = await Console.readLineAsync(
+      "보너스 번호를 입력해 주세요.\n"
+    );
+    const bonus = parseInt(bonusInput, 10);
+
+    if (
+      isNaN(bonus) ||
+      bonus < 1 ||
+      bonus > 45 ||
+      this.winningNumbers.includes(bonus)
+    ) {
+      throw new Error(
+        "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 하며, 당첨 번호와 중복되지 않아야 합니다."
+      );
+    }
+    this.bonusNumber = bonus;
+  }
 }
+
 export default LottoGame;
