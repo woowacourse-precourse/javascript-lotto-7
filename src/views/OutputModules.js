ed0a0385b28fd10300d29680b23e195f3992ff6b
Parksangwoo <psu8430@gmail.com>
2024-11-04 15:19:45 +0900

    feat(OutputModule) : 로또 당첨 결과 출력 로직 분리

--- src/views/OutputModules.js
+++ src/views/OutputModules.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { RANK_INFO } from '../constants.js';
 
 class OutputModules {
   static printMessage(message) {
@@ -10,6 +11,21 @@ class OutputModules {
       const numbers = lotto.getNumbers().join(', ');
       Console.print(`[${numbers}]`);
     });
+  }
+
+  static printRankStatus(rank, result) {
+    const { correctNumber, correctBonus, price } = RANK_INFO[rank];
+    const formatedPrice = price.toLocaleString();
+
+    if (correctBonus) {
+      OutputModules.printMessage(
+        `${correctNumber}개 일치, 보너스 볼 일치 (${formatedPrice}원) - ${result}개`,
+      );
+
+      return;
+    }
+
+    OutputModules.printMessage(`${correctNumber}개 일치 (${formatedPrice}원) - ${result}개`);
   }
 }
 
