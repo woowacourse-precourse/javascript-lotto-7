01098becc5516c0b9693803dc4ab95fa9eb169f5
chan8632 <duscks8632@gmail.com>
2024-11-04 16:52:33 +0900

    feat: 로또 당첨 번호 비교 및 등수 선정 기능 추가
    - 랜덤 로또 번호와 입력받은 당첨 번호를 비교하여 일치 개수 계산하는 로직 추가
    - 일치 개수에 따라 등수를 선정하는 로직 추가 (1등~5등 분류)
    - 로또 번호 출력 기능과 당첨 번호 입력 기능 보완
    - switch문을 사용하여 일치 개수에 따른 등수 분류

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App {
     this.inputMoney = await MissionUtils.Console.readLineAsync("");
     this.로또번호몇번출력할건지();
     MissionUtils.Console.print("당첨번호를 입력해주세요");
-    const 당첨번호 = await MissionUtils.Console.readLineAsync("");
+    this.당첨번호 = await MissionUtils.Console.readLineAsync("");
     MissionUtils.Console.print("보너스 번호를 입력해주세요");
-    const 보너스번호 = await MissionUtils.Console.readLineAsync("");
+    this.보너스번호 = await MissionUtils.Console.readLineAsync("");
   }
   lottoNumberprint() {
     const randomNumberList = MissionUtils.Random.pickUniqueNumbersInRange(
@@ -16,6 +16,7 @@ class App {
       45,
       6
     );
+
     randomNumberList.sort((a, b) => a - b);
     MissionUtils.Console.print(randomNumberList);
   }
@@ -23,6 +24,34 @@ class App {
     const 로또번호출력횟수 = parseInt(this.inputMoney) / 1000;
     for (let i = 0; i < 로또번호출력횟수; i++) {
       this.lottoNumberprint();
+    }
+  }
+  랜덤로또번호와당첨번호비교(로또번호) {
+    let 일치갯수 = 0;
+    로또번호.forEach((num) => {
+      if (this.당첨번호.includes(num)) {
+        일치갯수++;
+      }
+    });
+    return 일치갯수;
+  }
+  등수선정() {
+    let 등수;
+    switch (일치개수) {
+      case 6:
+        등수 = 1;
+        break;
+      case 5:
+        등수 = 2;
+      case 5:
+        등수 = 3;
+        break;
+      case 4:
+        등수 = 4;
+        break;
+      case 3:
+        등수 = 5;
+        break;
     }
   }
 }
