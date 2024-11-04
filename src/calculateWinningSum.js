36e2dcd7c98209a49f1cd9d722a203c8b6937b50
eunjin11 <whdmswls9207@gmail.com>
2024-11-04 14:29:26 +0900

    fix(calculateWinningSum): 상금 총합 수식 오류 fix

--- src/calculateWinningSum.js
+++ src/calculateWinningSum.js
@@ -1,9 +1,14 @@
 const calculateWinningSum = (matchCountList) => {
   const totalWinning = Object.keys(matchCountList)
     .map((matchCount) => {
-      const { winning, isBonusMatchWinning = 0 } = matchCountList[matchCount];
+      const {
+        count,
+        winning,
+        isBonusMatchCount = 0,
+        isBonusMatchWinning = 0,
+      } = matchCountList[matchCount];
 
-      return winning + isBonusMatchWinning;
+      return winning * count + isBonusMatchWinning * isBonusMatchCount;
     })
     .reduce((acc, curr) => acc + curr, 0);
 
--- src/printRateOfReturn.js
+++ src/printRateOfReturn.js
@@ -1,7 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const printRateOfReturn = (cost, winnings) => {
-  const rateOfReturn = (winnings / cost).toFixed(2);
+  const rateOfReturn = ((winnings / cost) * 100).toFixed(1);
 
   Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
 };
