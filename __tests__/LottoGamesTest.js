a992e86b3dca2f0f5552f7989ca5dce6a19729e3
kr-younghoon <sullanta0802@gmail.com>
2024-11-04 20:29:52 +0900

    test(lottogame): 당첨 번호 및 보너스 번호 유효성 테스트 추가
    - 당첨 번호와 보너스 번호가 중복되지 않도록 유효성 검사 테스트
    - 당첨 번호 개수, 1~45 범위 내 유효성 테스트
    - 중복된 보너스 번호 예외 처리 테스트

--- __tests__/LottoGamesTest.js
+++ __tests__/LottoGamesTest.js
@@ -30,8 +30,8 @@ describe('로또게임 클래스 테스트', () => {
     lottos.forEach((lotto) => {
       const numbers = lotto.getNumbers();
       expect(numbers.length).toBe(6);
-      expect(new Set(numbers).size).toBe(6); // 중복 없는지 확인
-      expect(numbers.every((num) => num >= 1 && num <= 45)).toBe(true); // 범위 확인
+      expect(new Set(numbers).size).toBe(6);
+      expect(numbers.every((num) => num >= 1 && num <= 45)).toBe(true);
     });
   });
 
@@ -48,5 +48,56 @@ describe('로또게임 클래스 테스트', () => {
       const numbers = lotto.getNumbers().join(', ');
       expect(logSpy).toHaveBeenCalledWith(`[${numbers}]`);
     });
+  });
+
+  test('당첨 번호와 보너스 번호가 중복 없이 1-45 범위 내에 있는지 확인', () => {
+    const lottoGame = new LottoGame();
+
+    const validWinningNumbers = [1, 2, 3, 4, 5, 6];
+    const validBonusNumber = 7;
+
+    expect(() => {
+      lottoGame.setWinningNumbers(validWinningNumbers, validBonusNumber);
+    }).not.toThrow();
+
+    const { winningNumbers, bonusNumber } = lottoGame.getWinningNumbers();
+    expect(winningNumbers).toEqual(validWinningNumbers);
+    expect(bonusNumber).toBe(validBonusNumber);
+  });
+
+  test('당첨 번호와 보너스 번호가 중복되면 예외가 발생한다.', () => {
+    const lottoGame = new LottoGame();
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 6;
+
+    expect(() => {
+      lottoGame.setWinningNumbers(winningNumbers, bonusNumber);
+    }).toThrow('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+  });
+
+  test('당첨 번호가 6개가 아니면 예외가 발생한다.', () => {
+    const lottoGame = new LottoGame();
+    const invalidWinningNumbers = [1, 2, 3, 4, 5];
+    const bonusNumber = 6;
+
+    expect(() => {
+      lottoGame.setWinningNumbers(invalidWinningNumbers, bonusNumber);
+    }).toThrow('[ERROR] 당첨 번호는 6개여야 합니다.');
+  });
+
+  test('당첨 번호 또는 보너스 번호가 1~45 범위를 벗어나면 예외가 발생한다.', () => {
+    const lottoGame = new LottoGame();
+
+    const invalidWinningNumbers = [0, 2, 3, 4, 5, 6];
+    const validBonusNumber = 7;
+    expect(() => {
+      lottoGame.setWinningNumbers(invalidWinningNumbers, validBonusNumber);
+    }).toThrow('[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.');
+
+    const validWinningNumbers = [1, 2, 3, 4, 5, 6];
+    const invalidBonusNumber = 46;
+    expect(() => {
+      lottoGame.setWinningNumbers(validWinningNumbers, invalidBonusNumber);
+    }).toThrow('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
   });
 });
