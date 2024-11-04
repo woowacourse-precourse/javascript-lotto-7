247d6718c86a999a7a69643b6a3eb45ce5f68da9
leelsj2007 <leelsj2007@gmail.com>
2024-11-04 09:15:32 +0900

    feat : controller에 LottoService 의존성 주입

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,22 +1,26 @@
 import InputView from "../view/InputView.js";
 import OutputView from "../view/OutputView.js";
+import LottoService from "../domain/LottoService.js";
 
 class Controller {
   #inputView;
   #outputView;
+  #lottoService;
 
   constructor() {
     this.#inputView = new InputView();
     this.#outputView = new OutputView();
+    this.#lottoService = new LottoService();
   }
 
   async run() {
-    const purchaseNumber = await this.#inputView.readPurchaseAmount();
+    const payment = await this.#inputView.readPurchaseAmount();       // 지불 가격 유효성 검사 필요
+    const numberOfPurchase = this.#lottoService.purchaseLottos(payment);
 
-    this.#outputView.printMyLotto(0);
+    this.#outputView.printMyLotto(numberOfPurchase);
 
-    const winningNumber = await this.#inputView.readWinningLotto();
-    const bonusNumber = await this.#inputView.readBonusNumber();
+    const winningNumber = await this.#inputView.readWinningLotto();    // 당첨 번호 유효성 검사 필요
+    const bonusNumber = await this.#inputView.readBonusNumber();       // 보너스 번호 유효성 검사 필요
 
     this.#outputView.printResult();
   }
