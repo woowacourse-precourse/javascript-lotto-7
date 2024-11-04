a31197eb83a49bdd86ddedac4b35e788b5406347
yheseul <kangyheseul@naver.com>
2024-11-04 19:54:07 +0900

    feat: ResultView 구현 및 당첨 결과 출력 기능 추가

---
+++ src/views/ResultView.js
@@ -0,0 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import { RUN_MESSAGE } from "../constants/runMessages.js";
+import { calculateLottoResults } from "../services/calculateLottoResults.js";
+import { calculateWinningDetails } from "../services/calculateWinningDetails.js";
+import { lottoResultPrint } from "../services/lottoResultPrint.js";
+import { calculateEarningRate } from "../services/calculateEarningRate.js";
+
+class ResultView {
+  displayResultMessage() {
+    Console.print(RUN_MESSAGE.WINNING_STATISTICS);
+    Console.print(RUN_MESSAGE.BORDERLINE);
+  }
+
+  displayResult(winningNumbers, lottoNumbers) {
+    const lottoResults = calculateLottoResults(winningNumbers, lottoNumbers);
+    const winningDetails = calculateWinningDetails(lottoResults);
+    lottoResultPrint(winningDetails);
+    calculateEarningRate(winningDetails);
+  }
+}
+
+export default ResultView;
