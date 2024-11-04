ee6027189eb4ceab6cb7714a44bc20e248b181ce
Eunicekk <cho_lyn@naver.com>
2024-11-04 23:36:50 +0900

    [feat] 예외 처리 및 테스트 코드 추가

--- src/views/Input.js
+++ src/views/Input.js
@@ -1,42 +1,32 @@
 import { Console } from "@woowacourse/mission-utils";
+import Lotto from "../models/Lotto";
+import { validateBonusNumber, validatePrice } from "../utils/Validate";
 
 export class Input {
 	async getLottoPrice() {
 		Console.print("구입금액을 입력해 주세요.");
 		const input = await Console.readLineAsync("");
+		const price = Number(input);
+		validatePrice(price);
 
-		// 가능한 에러
-		// 정수가 아닌 경우
-		// 금액이 1000 미만일 경우
-		// 금액이 1000 단위가 아닐 경우
-		// 구입 금액 제한
-
-		return Number(input);
+		return price;
 	}
 
 	async getLottoNumbers() {
 		Console.print("당첨 번호를 입력해 주세요.");
 		const input = await Console.readLineAsync("");
-		const numbers = input.split(",").map((number) => Number(number));
-
-		// 가능한 에러
-		// 입력한 숫자 개수가 6개가 아닌 경우
-		// 중복된 값이 있는 경우
-		// 정수가 아닌 값이 들어있는 경우
-		// 1 ~ 45 사이의 수가 아닌 경우
+		const numbers = input.split(",").map(Number);
+		const lotto = new Lotto(numbers);
 
 		return numbers;
 	}
 
-	async getLottoBonusNumber() {
+	async getLottoBonusNumber(winningNumbers) {
 		Console.print("보너스 번호를 입력해 주세요.");
 		const input = await Console.readLineAsync("");
+		const bonusNumber = Number(input);
+		validateBonusNumber(bonusNumber, winningNumbers);
 
-		// 가능한 에러
-		// 정수가 아닌 값인 경우
-		// 1 ~ 45 사이의 수가 아닌 경우
-		// 당첨 번호와 중복된 경우
-
-		return Number(input);
+		return bonusNumber;
 	}
 }
