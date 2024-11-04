8116b8442553fd3e67c3c4084043a888c2515550
changyeon <changyeon@changyeonui-MacBookAir.local>
2024-11-04 21:24:47 +0900

    feat : Number Verificatio

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,3 +7,35 @@ class Lotto {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
+
+  #validate(numbers) {
+    this.#validateLength(numbers);
+    this.#validateRange(numbers);
+    this.#validateUnique(numbers);
+  }
+
+  #validateLength(numbers) {
+    if (!Array.isArray(numbers) || numbers.length !== 6) {
+      Console.print("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  #validateRange(numbers) {
+    if (!numbers.every((num) => num >= 1 && num <= 45)) {
+      Console.print("[ERROR] 로또 번호는 1과 45 사이여야 합니다.");
+      throw new Error("[ERROR] 로또 번호는 1과 45 사이여야 합니다.");
+    }
+  }
+
+  #validateUnique(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      Console.print("[ERROR] 로또 번호에 중복된 숫자가 없어야 합니다.");
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 없어야 합니다.");
+    }
+  }
+
+  getNumbers() {
+    return this.#numbers;
+  }
