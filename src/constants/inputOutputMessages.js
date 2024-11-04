c50990f34d637fc6abdc39ca12773cc2e3211c68
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 20:28:51 +0900

    fix: 입력 메시지 줄바꿈 문자 추가

--- src/constants/inputOutputMessages.js
+++ src/constants/inputOutputMessages.js
@@ -3,7 +3,7 @@ import { WINNINGS } from './constants.js';
 export const INPUT_MESSAGE = Object.freeze({
   PURCHASE_PRICE: '구입금액을 입력해 주세요.\n',
   WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
-  BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
 });
 
 export const OUTPUT_MESSAGE = Object.freeze({
