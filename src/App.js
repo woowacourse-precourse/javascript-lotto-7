df7e31937bc0cfb23b016d0728928f0d429b0a24
Erin <fls4804@gmail.com>
2024-11-04 15:15:07 +0900

    feat: class 정의

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
+  amountOfPurchasedMoney;
+  purchasedLottoList = [];
+  winningLottoNumbers = [];
+  bonusLottoNumber;
+
   async run() {}
 }
 
