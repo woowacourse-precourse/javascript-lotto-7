e96b82184f5fb117ba676222b242544941f95086
Baek <woonn97@gmail.com>
2024-11-04 16:37:19 +0900

    refactor: 로또 개수와 로또 리스트 출력 나누기

--- src/LottoView.js
+++ src/LottoView.js
@@ -16,10 +16,13 @@ class LottoView {
     return Number(input);
   }
 
+  showLottoCount(count) {
+    Console.print(`${count}개를 구매했습니다.`);
+  }
+
   showLottoList(lottos) {
-    Console.print(`${lottos.length}개를 구매했습니다.`);
     lottos.forEach((lotto) => {
-      Console.print(lotto.getNumbers());
+      Console.print(`[${lotto.getNumbers().join(", ")}]`);
     });
   }
 
