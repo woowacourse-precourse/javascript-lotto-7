a65b34d02d21d8530b4ca0512d0df88349915017
zzzzzuuuuu <agu0830@naver.com>
2024-11-04 21:21:07 +0900

    fix: 숫자 6개 될 때까지 생성하도록 수정

--- src/LottoMarket.js
+++ src/LottoMarket.js
@@ -4,7 +4,7 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils";
 export class LottoMarket {
   makeLottoNumbers() {
     const numbers = new Set();
-    for (let i = 1; i <= LOTTO.NUMBERS_COUNT; i++) {
+    while (numbers.size < LOTTO.NUMBERS_COUNT) {
       const number = MissionUtils.Random.pickNumberInRange(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER);
       numbers.add(number);
     }
