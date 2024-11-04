c209256cf4df702967351294a13e9a51a5cbafb4
blaireinse <blaireinse@gmail.com>
2024-11-04 21:48:32 +0900

    feat(constants): add prize information

--- src/constants/lotto.js
+++ src/constants/lotto.js
@@ -1,1 +1,9 @@
 export const LOTTO_PRICE = 1000;
+
+export const PRIZE = [
+  { rank: 1, numberOfMatch: 6, prizeMoney: 2_000_000_000 },
+  { rank: 2, numberOfMatch: 5, prizeMoney: 30_000_000 },
+  { rank: 3, numberOfMatch: 5, prizeMoney: 1_500_000 },
+  { rank: 4, numberOfMatch: 4, prizeMoney: 50_000 },
+  { rank: 5, numberOfMatch: 3, prizeMoney: 5_000 },
+];
