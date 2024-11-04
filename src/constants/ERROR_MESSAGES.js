efd95f29a337264b003834c26cf822316e60ab9a
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-04 14:24:19 +0900

    feat: 구입 금액 관련 에러 메시지 상수 정의

---
+++ src/constants/ERROR_MESSAGES.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = Object.freeze({
+  NOT_A_NATURAL_NUMBER: '[ERROR] 해당 값은 자연수여야 합니다.',
+  WINNING_AMOUNT_MUST_BE_MULTIPLE_OF_1000:
+    '[ERROR] 유효하지 않은 금액입니다. 당첨 금액은 1,000원 단위로 입력해 주세요.'
+});
