228ac5a94d667d1d8433073eb3f1b7bf6cd16303
syg0629 <syg0629@hanmail.net>
2024-11-04 22:49:06 +0900

    feat(app): 로또 생성 및 출력 부분 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import LottoCreator from "./LottoCreator.js";
 import UserInput from "./UserInput.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   #userInput;
+  #lottoCreator;
 
   constructor() {
     this.#userInput = new UserInput();
+    this.#lottoCreator = new LottoCreator();
   }
 
   async run() {
     const amount = await this.#userInput.getUserInput("purchaseAmount");
+    const lottoCount = this.#lottoCreator.getLottoCount(amount);
+
+    MissionUtils.Console.print(`\n${lottoCount}개를 구매했습니다.`);
+
+    const lottos = this.#lottoCreator.createLotto(lottoCount);
+    this.#lottoCreator.printLottos(lottos);
+
     const numbers = await this.#userInput.getUserInput("winningNumber");
     const bonus = await this.#userInput.getUserInput("bonusNumber");
   }
