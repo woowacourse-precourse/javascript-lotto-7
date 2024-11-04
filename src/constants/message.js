c0b9dd0edf7ca0c6dc137a31460e4e97f609458e
keemsebeen <sebin0580@gmail.com>
2024-11-04 13:03:15 +0900

    feat(constants) : reward, count를 담고있는 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -14,6 +14,19 @@ export const LOTTO_VALUES = {
   COUNT: 6,
 };
 
+export const WINNING_RANK = [
+  { name: "FIFTH", count: 3, reward: 5000, description: "3개 일치" },
+  { name: "FOURTH", count: 4, reward: 50000, description: "4개 일치" },
+  { name: "THIRD", count: 5, reward: 1500000, description: "5개 일치" },
+  {
+    name: "SECOND",
+    count: 5.5,
+    reward: 30000000,
+    description: "5개 일치, 보너스 볼 일치",
+  },
+  { name: "FIRST", count: 6, reward: 2000000000, description: "6개 일치" },
+];
+
 export const PRICE_NUMBER = 1000;
 export const RATE = "당첨 통계";
 export const SECTION = "---";
