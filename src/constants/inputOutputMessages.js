0f09b119bbfd549590bed218b6c6d4557765ad77
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 18:10:54 +0900

    refactor: 당첨 상금 변동에 따른 결과 상수 반영을 위한 수정

--- src/constants/inputOutputMessages.js
+++ src/constants/inputOutputMessages.js
@@ -1,3 +1,5 @@
+import { WINNINGS } from './constants.js';
+
 export const INPUT_MESSAGE = Object.freeze({
   PURCHASE_PRICE: '구입금액을 입력해 주세요.\n',
   WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
@@ -8,11 +10,11 @@ export const OUTPUT_MESSAGE = Object.freeze({
   PURCHASE_COUNT: '개를 구매했습니다.',
   WINNING_STATISTICS: '당첨 통계',
   HYPHENS: '---',
-  MATCH_THREE: '3개 일치 (5,000원) - ',
-  MATCH_FOUR: '4개 일치 (50,000원) - ',
-  MATCH_FIVE: '5개 일치 (1,500,000원) - ',
-  MATCH_FIVE_AND_BONUS: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
-  MATCH_SIX: '6개 일치 (2,000,000,000원) - ',
+  MATCH_THREE: `3개 일치 (${WINNINGS.THREE.toLocaleString()}원) - `,
+  MATCH_FOUR: `4개 일치 (${WINNINGS.FOUR.toLocaleString()}원) - `,
+  MATCH_FIVE: `5개 일치 (${WINNINGS.FIVE.toLocaleString()}원) - `,
+  MATCH_FIVE_BONUS: `5개 일치, 보너스 볼 일치 (${WINNINGS.FIVE_BONUS.toLocaleString()}원) - `,
+  MATCH_SIX: `6개 일치 (${WINNINGS.SIX.toLocaleString()}원) - `,
   MATCH_COUNT_UNIT: '개',
   TOTAL_RETURN: '총 수익률은 ',
   PERCENTAGE_SUFFIX: '%입니다.',
