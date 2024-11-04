517ca6878c2013df471e2860deb55204d5642b91
Heesoo Jun <acesk123@gmail.com>
2024-11-04 22:17:51 +0900

    feat: 로또 번호 비교 기능 추가.

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,10 +11,21 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
   }
 
   getNumbers() {
     return this.#numbers;
+  }
+
+  getMatchCount(winningNumbers) {
+    return this.#numbers.filter((num) => winningNumbers.includes(num)).length;
+  }
+
+  hasBonusNumber(bonusNumber) {
+    return this.#numbers.includes(bonusNumber);
   }
 }
 
