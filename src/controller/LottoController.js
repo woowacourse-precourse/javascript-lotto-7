bfdddb4c3a6005d8bf8c72b178a3b021e4530d7a
taeyomi <terry11ty@naver.com>
2024-11-04 17:12:02 +0900

    refactor(LottoController): 기능별 함수 분리

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -6,6 +6,7 @@ import {
 } from "../validation.js";
 import LottoGenerator from "../model/LottoGenerator.js";
 import Statistics from "../model/statistics.js";
+
 class LottoController {
   constructor() {
     this.view = new LottoView();
@@ -25,49 +26,53 @@ class LottoController {
       this.printStatistics();
     } catch (error) {
       this.handleInitError(error);
-      await this.init();
     }
   }
 
   handleInitError(error) {
     this.view.printError(error.message);
   }
+
+  // 구매 금액 설정 함수 분리
   async setLottoAmounts() {
+    const purchaseAmount = await this.getPurchaseAmount();
+    this.totalSpent = purchaseAmount;
+    const lottoCount = this.calculateLottoCount(purchaseAmount);
+    this.totalSpent = lottoCount * 1000;
+    this.generateAndPrintLottos(lottoCount);
+  }
+
+  async getPurchaseAmount() {
     try {
-      const purchaseAmount = await this.getLottoAmounts();
-      this.totalSpent = purchaseAmount;
-      const lottoCount = this.calculateLotto(purchaseAmount);
-      this.generator.generateLottos(lottoCount);
-      const lottos = this.generator.getLottos();
-      this.view.printGetLottos(lottoCount);
-      this.view.printLottos(lottos);
+      const userInputAmounts = await this.view.inputLottoAmount();
+      this.validatePurchaseAmount(userInputAmounts);
+      return Number(userInputAmounts);
     } catch (error) {
       this.view.printError(error.message);
-      return await this.getLottoAmounts();
+      return await this.getPurchaseAmount();
     }
   }
 
-  //로또 숫자 입력
-  async getLottoAmounts() {
-    const userInputAmounts = await this.view.inputLottoAmount();
-    this.checkLottoAmounts(userInputAmounts);
-    return Number(userInputAmounts);
-  }
-  //입력숫자 검증
-  checkLottoAmounts(userInputAmounts) {
-    validatePurchasingAmount(userInputAmounts);
+  validatePurchaseAmount(amount) {
+    validatePurchasingAmount(amount);
   }
 
-  //로또 개수 계산
-  calculateLotto(count) {
-    return Math.floor(count / 1000);
+  calculateLottoCount(purchaseAmount) {
+    return Math.floor(purchaseAmount / 1000);
   }
 
-  //당첨번호 입력
+  generateAndPrintLottos(lottoCount) {
+    this.generator.generateLottos(lottoCount);
+    const lottos = this.generator.getLottos();
+    this.view.printGetLottos(lottoCount);
+    this.view.printLottos(lottos);
+  }
+
+  // 당첨 번호 입력
   async getWinningNumbers() {
     try {
-      const getWinningNumber = await this.view.inputWinningNumber();
-      const winningLottos = new Lotto(getWinningNumber);
+      const winningNumbersInput = await this.view.inputWinningNumber();
+      const winningLottos = new Lotto(winningNumbersInput);
       this.winningNumbers = winningLottos.getLottoNumbers();
     } catch (error) {
       this.view.printError(error.message);
@@ -75,11 +80,10 @@ class LottoController {
     }
   }
 
-  //보너스번호 입력
   async getBonusNumbers() {
     try {
-      const getBonusNumber = await this.view.inputBonusNumber();
-      const bonusNumber = Number(getBonusNumber);
+      const bonusNumberInput = await this.view.inputBonusNumber();
+      const bonusNumber = Number(bonusNumberInput);
       validateBonusNumber(bonusNumber, this.winningNumbers);
       this.bonusNumbers = bonusNumber;
     } catch (error) {
