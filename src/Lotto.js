e8b6fb67a6bf881940063b926257755b53135a84
changyeon <changyeon@changyeonui-MacBookAir.local>
2024-11-04 21:23:47 +0900

    feat : random number generation

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,9 @@
+import { Random as MissionUtilsRandom, Console } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
-
-  constructor(numbers) {
+  
+  constructor(numbers = MissionUtilsRandom.pickUniqueNumbersInRange(1, 45, 6)) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  // TODO: 추가 기능 구현
-}
-
-export default Lotto;
