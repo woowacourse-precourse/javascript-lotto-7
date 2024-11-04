12bbb4d6fbd515aad296e2a38ae83f945cdf4ad6
jinoo <jinoo0306@naver.com>
2024-11-04 17:34:11 +0900

    feat(4) : 당첨 통계 메세지 상수 처리

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,17 @@ export const INPUT_MESSAGES = {
   BONUS_NUMBER: "\n보너스 번호를 입력해 주세요.\n",
 };
 
-export const OUTPUT_MESSAGES = {};
+export const OUTPUT_MESSAGES = {
+  PURCHASE_COUNT: (count) => `${count}개를 구매했습니다.`,
+  LOTTO_NUMBERS: (numbers) => `[${numbers.join(", ")}]`,
+  RESULTS_HEADER: "\n당첨 통계\n---",
+  MATCH_3: (count) => `3개 일치 (5,000원) - ${count}개`,
+  MATCH_4: (count) => `4개 일치 (50,000원) - ${count}개`,
+  MATCH_5: (count) => `5개 일치 (1,500,000원) - ${count}개`,
+  MATCH_5_BONUS: (count) =>
+    `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+  MATCH_6: (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
+};
 
 export const ERROR_MESSAGE = {
   ERROR_MESSAGE_PREFIX: "[ERROR]",
