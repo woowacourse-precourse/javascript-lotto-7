3f2eb73b7f0f1e16044a4dc415a2929b6303865b
Jaeho Cha <ischar@naver.com>
2024-11-04 13:33:56 +0900

    test(total): update filename and function to match revisted function
    - calculateProfit에 맞춰 테스트 파일 및 함수 변경

--- __tests__/calculateProfit.test.js
+++ __tests__/calculateProfit.test.js
@@ -1,23 +1,23 @@
-import { calcTotalReturn } from "../src/features/calcTotalReturn.js";
+import { calculateProfit } from "../src/features/calculateProfit.js";
 import { ERROR, PERCENT } from "../src/config/config.js";
 
-describe("calcTotalReturn 테스트", () => {
+describe("calculateProfit 테스트", () => {
   test("정상적으로 값을 반환하는 지 확인", () => {
     const AMOUNT = 200;
     const TOTAL = 450;
-    const EXPECTED = `2.3${PERCENT}`;
-    expect(calcTotalReturn(AMOUNT, TOTAL)).toBe(EXPECTED);
+    const EXPECTED = `225.0${PERCENT}`;
+    expect(calculateProfit(AMOUNT, TOTAL)).toBe(EXPECTED);
   });
 
   test("구매 금액이 숫자가 아닐 경우 에러를 발생시키는 지 확인", () => {
     const AMOUNT = "not a number";
     const TOTAL = 450;
-    expect(() => calcTotalReturn(AMOUNT, TOTAL)).toThrow(ERROR.NOT_NUMBER);
+    expect(() => calculateProfit(AMOUNT, TOTAL)).toThrow(ERROR.NOT_NUMBER);
   });
 
   test("당첨 금액이 숫자가 아닐 경우 에러를 발생시키는 지 확인", () => {
     const AMOUNT = 200;
     const TOTAL = "not a number";
-    expect(() => calcTotalReturn(AMOUNT, TOTAL)).toThrow(ERROR.NOT_NUMBER);
+    expect(() => calculateProfit(AMOUNT, TOTAL)).toThrow(ERROR.NOT_NUMBER);
   });
 });
