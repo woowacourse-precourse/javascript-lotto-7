ae06d494f200265d90267d84864299adf059104e
deokbin <deokbin@naver.com>
2024-11-04 09:28:52 +0900

    feat(ErrorMessage) : 에러 메시지 상수 추가

---
+++ src/constants/ErrorMessage.js
@@ -0,0 +1,13 @@
+export const ErrorMessage = Object.freeze({
+  NOT_MULTIPLE_OF_1000: `로또 구입 금액은 1000원 단위로 입력해주세요.`,
+  ZERO_PRICE: `로또 구입 금액은 최소 1,000원 이어야 합니다.`,
+  NEGATIVE_NUM: `음수 값은 입력할 수 없습니다.`,
+  TOO_BIG_PRICE: `구입 금액이 너무 큽니다.`,
+  NOT_A_NUMBER: `문자열은 입력할 수 없습니다.`,
+  EMPTY_INPUT: `빈 값은 입력할 수 없습니다.`,
+  OVER_RANGE: `로또 번호는 1~45 범위 내에 있어야 합니다.`,
+  INVALID_INPUT: `유효하지 않은 입력입니다.`,
+  NOT_6_NUMS: `당첨 번호의 개수는 6개여야 합니다. 6개의 숫자를 입력해주세요.`,
+  DUPLICATED_NUM: `당첨 번호에 중복된 숫자가 올 수 없습니다.`,
+  EXIST_LOTTONUM: `보너스 번호는 당첨 번호와 중복될 수 없습니다.`,
+});
