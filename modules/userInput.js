760082f1c887e6bdf089c9cf1cdc6eb2a8145b16
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-04 22:04:49 +0900

    feat: 보너스 번호 입력 함수 추가
    userInput.js 파일에 이하 내용 추가:
    - 보너스 번호를 입력받고, 유효성 검사 후 보너스 번호를 반환하는 getValidatedBonusNumber 함수 추가

--- modules/userInput.js
+++ modules/userInput.js
@@ -1,5 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import {
+  bonusNumberValidate,
   purchaseAmountValidate,
   winningNumbersValidate,
 } from "./inputValidator.js";
@@ -28,6 +29,21 @@ export async function getValidatedWinningNumbers() {
     try {
       winningNumbersValidate(winningNumbers);
       return winningNumbers.split(",").map(Number);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+}
+
+export async function getValidatedBonusNumber() {
+  while (true) {
+    const bonusNumber = await Console.readLineAsync(
+      "\n보너스 번호를 입력해 주세요.\n"
+    );
+
+    try {
+      bonusNumberValidate(bonusNumber);
+      return Number(bonusNumber);
     } catch (error) {
       Console.print(error.message);
     }
