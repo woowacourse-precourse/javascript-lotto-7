e5057714a0a2d1fe17ec6b6d21eae9e58a7a7d3c
COMPOSEDKIM <composedkim@naver.com>
2024-11-04 19:39:11 +0900

    feat: 당첨 확인 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { PROMPTS, INFO } from './constants.js';
+import { PROMPTS, INFO, PRIZE } from './constants.js';
 import Lotto from './Lotto.js';
 
 class App {
@@ -7,6 +7,13 @@ class App {
     const userMoney = await this.getUserMoney();
     const countLotto = userMoney / 1000;
     const userLotto = [];
+    const totalWinningLotto = {
+      three: 0,
+      four: 0,
+      five: 0,
+      fivebounus: 0,
+      six: 0,
+    };
     MissionUtils.Console.print(`\n${countLotto}${INFO.USER_BUY}`);
     for (let i = 0; i < countLotto; i++) {
       userLotto.push(
@@ -15,8 +22,25 @@ class App {
       MissionUtils.Console.print(userLotto[i].showNumber());
     }
     const userWinningNumber = await this.getUserWinningNumber();
-    const winningNumber = userWinningNumber.split(',');
+    const winningNumber = userWinningNumber.split(',').map((num) => num.trim());
     const bonusNumber = await this.getBonusNumber();
+
+    for (let i = 0; i < countLotto; i++) {
+      const resultLotto = this.findWinningMatch(
+        userLotto[i].showNumber(),
+        winningNumber,
+        bonusNumber
+      );
+      this.countWinningLotto(resultLotto, totalWinningLotto);
+    }
+    MissionUtils.Console.print(INFO.USER_WINNING_RESULT);
+    MissionUtils.Console.print(INFO.userThreeMatch(totalWinningLotto.three));
+    MissionUtils.Console.print(INFO.userFourMatch(totalWinningLotto.four));
+    MissionUtils.Console.print(INFO.userFiveMatch(totalWinningLotto.five));
+    MissionUtils.Console.print(
+      INFO.userFiveBounusMatch(totalWinningLotto.fivebounus)
+    );
+    MissionUtils.Console.print(INFO.userSixMatch(totalWinningLotto.six));
   }
 
   async getUserMoney() {
@@ -38,6 +62,44 @@ class App {
       PROMPTS.INPUT_USER_BONUS_NUMBER
     );
     return userBonusNumber;
+  }
+
+  findWinningMatch(userLotto, winningLotto, bonusNumber) {
+    const userNumLotto = userLotto.map(Number);
+    const winningNumLotto = winningLotto.map(Number);
+    const resultLotto = winningNumLotto.filter((x) => userNumLotto.includes(x));
+    if (resultLotto.length === 5) {
+      return this.findBounusMatch(userNumLotto, bonusNumber, resultLotto);
+    }
+    return resultLotto.length;
+  }
+
+  findBounusMatch(userNumLotto, bonusNumber, resultLotto) {
+    if (userNumLotto.includes(Number(bonusNumber))) {
+      return 'bonus';
+    }
+    return resultLotto.length;
+  }
+
+  countWinningLotto(resultLotto, totalWinningLotto) {
+    switch (resultLotto) {
+      case 3:
+        totalWinningLotto.three = totalWinningLotto.three + 1;
+        break;
+      case 4:
+        totalWinningLotto.four = totalWinningLotto.four + 1;
+        break;
+      case 5:
+        totalWinningLotto.five = totalWinningLotto.five + 1;
+        break;
+      case 'bonus':
+        totalWinningLotto.fivebounus = totalWinningLotto.fivebounus + 1;
+        break;
+      case 6:
+        totalWinningLotto.six = totalWinningLotto.six + 1;
+        break;
+    }
+    return totalWinningLotto;
   }
 }
 
--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,34 @@ const PROMPTS = Object.freeze({
 
 const INFO = Object.freeze({
   USER_BUY: '개를 구매했습니다.',
+  USER_WINNING_RESULT: '\n당첨 통계\n---',
+
+  userThreeMatch(result) {
+    return `3개 일치 (5,000원) - ${result}개`;
+  },
+  userFourMatch(result) {
+    return `4개 일치 (50,000원) - ${result}개`;
+  },
+  userFiveMatch(result) {
+    return `5개 일치 (1,500,000원) - ${result}개`;
+  },
+  userFiveBounusMatch(result) {
+    return `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result}개`;
+  },
+  userSixMatch(result) {
+    return `6개 일치 (2,000,000,000원) - ${result}개`;
+  },
+  printTotalEarningsRate(rate) {
+    return `총 수익률은 ${rate}% 입니다.`;
+  },
 });
 
-export { PROMPTS, INFO };
+const PRIZE = Object.freeze({
+  THREE_MATCH: 5000,
+  FOUR_MATCH: 50000,
+  FIVE_MATCH: 1500000,
+  FIVE_BONUS_MATCH: 30000000,
+  SIX_MATCH: 2000000000,
+});
+
+export { PROMPTS, INFO, PRIZE };
