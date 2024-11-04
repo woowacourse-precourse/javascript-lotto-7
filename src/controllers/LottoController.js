f182806311de47710a4ffce2af0a0cc2473967c7
CJY <cjy3458@naver.com>
2024-11-04 10:26:15 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -2,6 +2,7 @@ import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
 import { validatePurchaseAmount } from '../utils/validateInput.js';
 import CustomError from '../utils/CustomError.js';
+import { validateLottoNumbers } from '../utils/validateLottoNumbers.js';
 
 class LottoController {
   static async handlePurchaseAmount() {
@@ -14,6 +15,23 @@ class LottoController {
       if (error instanceof CustomError) {
         OutputView.printError(error.message, error.name);
         return this.handlePurchaseAmount();
+      }
+      throw error;
+    }
+  }
+
+  static async getWinningNumbers() {
+    try {
+      const winningNumbersString = await InputView.readWinningNumbers();
+      const winningNumbers = winningNumbersString
+        .split(',')
+        .map((num) => parseInt(num.trim(), 10));
+      validateLottoNumbers(winningNumbers);
+      return winningNumbers;
+    } catch (error) {
+      if (error instanceof CustomError) {
+        OutputView.printError(error.message, error.name);
+        return this.getWinningNumbers();
       }
       throw error;
     }
