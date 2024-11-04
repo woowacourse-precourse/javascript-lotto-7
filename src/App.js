356c2f1639340d42da3376a02bd64d890c67c576
luii0415 <becky45210193@gmail.com>
2024-11-04 18:04:10 +0900

    feat : create
    사용자의 로또를 생성한다.
    오름차 순으로 정렬한 번호를 출력한다.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,32 @@ class App {
 
     return amount;
   }
+  printLottos() {
+    this.lottos.forEach((lotto) => {
+      const numbers = lotto.getNumbers();
+      Console.print(`[${numbers.join(", ")}]`);
+    });
+  }
+
+  async makeUserLottos() {
+    const amount = await this.UserInput();
+    const howmany = amount / 1000;
+
+    for (let i = 0; i < howmany; i++) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.lottos.push(new Lotto(numbers));
+    }
+
+    Console.print(`\n${howmany}개를 구매했습니다.`);
+    this.printLottos();
+  }
   async run() {
     let lottos = [];
+    try {
+      await this.makeUserLottos();
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,8 @@ class Lotto {
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.lottonumrange(numbers);
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   #validate(numbers) {
@@ -13,6 +15,15 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  lottonumrange(numbers) {
+    const numrange = numbers.some((num) => num < 1 || num > 45);
+    if (numrange) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+  }
+  getNumbers() {
+    return [...this.#numbers];
+  }
 }
 
 export default Lotto;
