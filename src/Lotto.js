1d7113c2c71521bb8963dbfb581f2f6459a99b46
JaeSeungJae <valkyrie9923@gmail.com>
2024-11-04 16:59:59 +0900

    fix: 출력 형식 올바르게 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복이 있습니다.");
+    }
+
+    if (numbers.some((num) => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이어야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
