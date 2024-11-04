ccecb3fee8faad02ea44cfa4d6887aa1064af17f
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-11-04 15:31:47 +0900

    test: validationLotto의 bonusNumber 함수 테스트 추가
    - 보너스 번호가 올바른 범위 내에 있을 때 오류가 발생하지 않는지 테스트
    - 보너스 번호가 범위를 벗어나거나 당첨 번호와 중복될 경우 오류를 발생시키는지 테스트

--- __tests__/validations/validationLottoTest.js
+++ __tests__/validations/validationLottoTest.js
@@ -41,4 +41,29 @@ describe('validationLotto 테스트', () => {
       );
     });
   });
+
+  describe('bonusNumber 함수', () => {
+    const winningLotto = { numbers: [1, 2, 3, 4, 5, 6] };
+
+    test('보너스 번호가 올바른 범위 내에 있을 때 오류가 발생하지 않는지 테스트', () => {
+      expect(() =>
+        validationLotto.bonusNumber('7', winningLotto)
+      ).not.toThrow();
+    });
+
+    test('보너스 번호가 범위를 벗어나면 오류를 발생시키는지 테스트', () => {
+      expect(() => validationLotto.bonusNumber('0', winningLotto)).toThrow(
+        ERROR_MESSAGES.OUT_OF_RANGE
+      );
+      expect(() => validationLotto.bonusNumber('46', winningLotto)).toThrow(
+        ERROR_MESSAGES.OUT_OF_RANGE
+      );
+    });
+
+    test('보너스 번호가 당첨 번호와 중복될 때 오류를 발생시키는지 테스트', () => {
+      expect(() => validationLotto.bonusNumber('3', winningLotto)).toThrow(
+        ERROR_MESSAGES.BONUS_NUMBER_DUPLICATE
+      );
+    });
+  });
 });
