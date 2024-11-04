7f5078e0d5e65a050bb3227c399db8140c0cdd2e
daun-up <awhekdns@kookmin.ac.kr>
2024-11-04 19:49:05 +0900

    feat: Lotto.js 추가 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,28 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    // TODO: 추가 기능 구현
+    if (!this.#isValidRange(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    if (this.#hasDuplicates(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  #isValidRange(numbers) {
+    return numbers.every((number) => number >= 1 && number <= 45);
+  }
+
+  #hasDuplicates(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    return uniqueNumbers.size !== numbers.length;
+  }
+
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
