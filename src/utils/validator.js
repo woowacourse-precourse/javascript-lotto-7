fe1cc3dc35befb2fe2b4f01319bec30c5fc00ac2
somm12 <sominan8744@gmail.com>
2024-11-04 22:49:31 +0900

    fix(validator): 보너스 번호 정수가 아닐 때 예외 발생 조건 추가

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -9,48 +9,40 @@ export const validator = {
   // 로또 구입 금액 유효성 검사
   validateLottoAmount(amount) {
     if (isNaN(amount)) {
-      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_AMOUNT_TYPE);
+      throw Error(ERROR_MESSAGES.invalid_lotto_amount_type);
     }
 
     if (amount < 0 || amount % LOTTO_TICKET_PRICE_UNIT !== 0) {
-      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_AMOUNT_DIVIDE_BY_1000);
+      throw Error(ERROR_MESSAGES.invalid_lotto_amount_divisible_1000);
     }
   },
   // 로또 당첨 번호 유효성 검사
   validateWinningLottoNumbers(numbers) {
-    try {
-      const lotto = new Lotto(numbers.split(",").map((e) => +e));
-      return lotto.getLottoNumbers();
-    } catch (error) {
-      throw error;
-    }
+    const lotto = new Lotto(numbers.split(",").map((e) => +e));
+    return lotto.getLottoNumbers();
   },
 
   validateBonusNumberType(bonusNumber) {
-    if (isNaN(bonusNumber)) {
-      throw new Error(ERROR_MESSAGES.INVALID_BONUS_NUMBER_TYPE);
+    if (isNaN(bonusNumber) || parseInt(bonusNumber) !== bonusNumber) {
+      throw Error(ERROR_MESSAGES.invalid_bonus_number_type);
     }
   },
   // 보너스 번호가 로또 번호가 겹치지 않는지 확인
   validateBonusNumberUniqueness(winningNumbers, bonusNumber) {
     if (winningNumbers.some((number) => bonusNumber === number)) {
-      throw new Error(ERROR_MESSAGES.INVALID_BONUS_NUMBER_UNIQUENESS);
+      throw Error(ERROR_MESSAGES.invalid_bonus_number_uniqueness);
     }
   },
 
   validateBonusNumberRange(bonusNumber) {
     if (bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX) {
-      throw new Error(ERROR_MESSAGES.INVALID_BONUS_NUMBER_RANGE);
+      throw Error(ERROR_MESSAGES.invalid_bonus_number_range);
     }
   },
   // 전체적인 보너스 번호 유효성 검사.
   validateBonusNumber(winningNumbers, bonusNumber) {
-    try {
-      validator.validateBonusNumberType(bonusNumber);
-      validator.validateBonusNumberUniqueness(winningNumbers, bonusNumber);
-      validator.validateBonusNumberRange(bonusNumber);
-    } catch (error) {
-      throw error;
-    }
+    validator.validateBonusNumberType(bonusNumber);
+    validator.validateBonusNumberUniqueness(winningNumbers, bonusNumber);
+    validator.validateBonusNumberRange(bonusNumber);
   },
 };
