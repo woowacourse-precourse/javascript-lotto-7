af3e8b2dd9ba765ab13885d2e40ed23ec8526e41
Oliy <74090200+eunsoA@users.noreply.github.com>
2024-11-04 19:37:46 +0900

    [style] 코드 큰 틀 잡기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 사용자로부터 로또 구매 금액 입력 받기
+      
+
+      // 로또 티켓 생성
+      
+      
+      // 생성된 로또 티켓 출력
+      
+
+      // 사용자로부터 당첨 번호 입력 받기
+      
+
+      // 사용자로부터 보너스 번호 입력 받기
+      
+
+      // 로또 결과 계산
+      
+
+      // 결과 출력
+      
+    } catch (error) {
+      // 예외가 발생하면 오류 메시지를 출력
+      Console.print(error.message);
+    }
+  }
 }
 
 export default App;
