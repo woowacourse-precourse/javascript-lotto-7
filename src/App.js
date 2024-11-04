a78820c1351d9ee779d7439daeb522174fdcc820
Eun0713 <daeunlee0713@naver.com>
2024-11-04 22:02:25 +0900

    feat: 모듈 임포트 및 readline 인터페이스 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-class App {
-  async run() {}
-}
+import Lotto from './modules/Lotto.js';
+import LottoGenerator from './modules/LottoGenerator.js';
+import PurchaseModule from './modules/PurchaseModule.js';
+import ResultModule from './modules/ResultModule.js';
+import WinningNumberModule from './modules/WinningNumberModule.js';
+import { createInterface } from 'readline';
 
-export default App;
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
