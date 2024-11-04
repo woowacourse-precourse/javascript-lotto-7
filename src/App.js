16311aa6967248194cd8c8449dc79851e22661a2
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2024-11-04 21:34:04 +0900

    docs(README.md): 기준에 따라 당첨 값, 수익률 반환 체크

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
-
+import LottoArr from "./LottoArr.js";
+import Win from "./Win.js";
 
 class App {
   async run() {
     const PAY_INPUT = await Console.readLineAsync("구입금액을 입력해 주세요.\n")
+    const LOTTO_ARRAY = new LottoArr(PAY_INPUT)
+    Console.print("\n" + LOTTO_ARRAY.print())
     const WINNING_NUMBERS = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n")
+    const Wisn = new Win(WINNING_NUMBERS)
     const BONUS_NUMBER = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n")
+    Wisn.getBonusNumber(BONUS_NUMBER)
+    Wisn.calculator(LOTTO_ARRAY.toArr())
+    Console.print(Wisn.print())
   }
 }
 
 export default App;
+
+
