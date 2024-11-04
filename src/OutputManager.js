6f846c590086f5811397841d804dd7ea90f6072d
inaemin <inaemin@gmail.com>
2024-11-04 21:44:37 +0900

    feat: OutputManager 클래스 구현

---
+++ src/OutputManager.js
@@ -0,0 +1,39 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export class OutputManager {
+  static printLotteryPurchase(lotteryGroup) {
+    Console.print(`\n${lotteryGroup.length}개를 구매했습니다.`);
+    lotteryGroup.forEach((lotto) => {
+      Console.print(
+        `[${lotto
+          .getLottoNumbers()
+          .sort((a, b) => a - b)
+          .join(", ")}]`
+      );
+    });
+  }
+
+  static printLottoStatistic(
+    lottoManager,
+    inputMoney,
+    winningNumbers,
+    bonusNumber
+  ) {
+    Console.print("\n당첨 통계");
+    Console.print("---");
+
+    const result = lottoManager.calculatePrize(winningNumbers, bonusNumber);
+    result.forEach((prize) => {
+      const { reward, desc, count } = prize;
+      Console.print(`${desc} (${this.formatCurrency(reward)}원) - ${count}개`);
+    });
+
+    Console.print(
+      `총 수익률은 ${lottoManager.calculateROI(inputMoney)}%입니다.`
+    );
+  }
+
+  static formatCurrency(money) {
+    return money.toLocaleString();
+  }
+}
