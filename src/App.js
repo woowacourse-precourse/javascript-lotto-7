18842900f104f8f62956f9f06311c762b67ef2a1
kim-museong <antjd0419@gmail.com>
2024-11-04 21:48:17 +0900

    feature(Amount) : 티켓 개수 출력
    사용자가 입력한 금액에 맞게 티켓 개수를 계산하여 출력하는 기능을 구현하였습니다.
    유효성 검사 함수와 계산 함수를 분리하여 구현하였습니다.
    생성자에 티켓값을 관리하도록 하였습니다.
    
    예외상황
    1. 입력값이 숫자가 아니거나 0보다 작을 경우
    2. 금액이 1000단위로 입력받아 나누어 떨어지지 않는 경우

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import { Console } from '@woowacourse/mission-utils';
-
-
 class App {
+  constructor() {
+    this.ITEM_PRICE = 1000;
+  }
+
   async run() {
     Console.print('구입금액을 입력해 주세요.');
     const input = await this.answer();
+
+    const amount = this.getAmount(input);
+    Console.print(`\n${amount}개를 구매했습니다.`);
   }
 
   async answer() {
     const input = await Console.readLineAsync('');
     return input;
+  }
+
+  getAmount(money) {
+    const amount = this.validateAmount(money);
+    return this.calculateAmount(amount);
+  }
+
+  validateAmount(money) {
+    const amount = Number(money);
+
+    if (isNaN(amount) || amount <= 0) {
+      throw new Error("[ERROR] 구입금액이 유효하지 않습니다.");
+    }
+
+    return amount;
+  }
+
+  calculateAmount(amount) {
+    const result = amount / this.ITEM_PRICE;
+
+    if (!Number.isInteger(result)) {
+      throw new Error("[ERROR] 구입금액은 1,000원의 배수여야 합니다.");
+    }
+
+    return result;
   }
 }
 
