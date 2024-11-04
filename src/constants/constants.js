7c3804b50dbad26cb2d18645915951f23cbf4de3
chichi <seonrye.yun@gmail.com>
2024-11-04 23:57:12 +0900

    add: constants 작성

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,7 @@
 export const LOTTO_PRICE = 1000;
-export const DELIMITER = ',';
+export const DELIMITER = ', ';
+export const FIRST_PRIZE_MONEY = 200000000;
+export const SECOND_PRIZE_MONEY = 30000000;
+export const THIRD_PRIZE_MONEY = 1500000;
+export const FOURTH_PRIZE_MONEY = 50000;
+export const FIFTH_PRIZE_MONEY = 5000;
