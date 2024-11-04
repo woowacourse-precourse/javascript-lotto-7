009f99feaea0286cd322e0347c182f784961bfb7
parksiwoon <parksw003@gmail.com>
2024-11-04 19:09:43 +0900

    refactor: #numbers 추가 in Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,10 +1,12 @@
 class Lotto {
+  #numbers;
+
   constructor(numbers) {
-    this.validate(numbers);
-    this.numbers = numbers;
+    this.#validate(numbers);
+    this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
@@ -16,8 +18,9 @@ class Lotto {
     }
   }
 
+  // TODO: 추가 기능 구현
   getNumbers() {
-    return this.numbers;
+    return this.#numbers;
   }
 }
 
