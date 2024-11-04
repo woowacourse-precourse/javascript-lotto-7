c0a789ba959e98290b4349fd28876e634d9f335c
zelkovaria <hyein396@gmail.com>
2024-11-04 14:35:38 +0900

    feat: 당첨 번호 범위 유효성 테스트 구현

--- __tests__/WinningNumTest.js
+++ __tests__/WinningNumTest.js
@@ -2,10 +2,24 @@ import { validateWinningNumber } from "../src/validator/WinningNum";
 
 describe("입력 로또 번호 테스트", () => {
   const notNumber = ["1ㅇ", , 13, 15, 17, 19, 20];
+  const notInRangeNumbers = [
+    [1, 2, 3, 4, 5, 0],
+    [-1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 66],
+  ];
 
   test("로또 번호에 숫자가 아닌 문자가 들어가면 예외가 발생한다.", () => {
     expect(() => {
       validateWinningNumber(notNumber);
     }).toThrow("[ERROR]");
   });
+
+  test.each(notInRangeNumbers)(
+    "로또 번호는 1~45 이외의 범위가 포함되면 예외가 발생한다.",
+    (lottoNumbers) => {
+      expect(() => {
+        validateWinningNumber(lottoNumbers);
+      }).toThrow("[ERROR]");
+    }
+  );
 });
--- src/validator/WinningNum.js
+++ src/validator/WinningNum.js
@@ -5,6 +5,17 @@ const validateNumberOnly = (winningNumber) => {
   }
 };
 
+const validateNumberRange = (winningNumber) => {
+  const isInRange = winningNumber.every((num) => 1 <= num && num <= 45);
+  if (!isInRange) {
+    throw new Error("[ERROR] 로또 번호는 1~45 사이로 입력되어야 합니다.");
+  }
+};
+
 export const validateWinningNumber = (winningNumber) => {
+  if (!Array.isArray(winningNumber)) {
+    throw new Error("[ERROR] 잘못된 형식의 당첨 번호가 입력되었습니다.");
+  }
   validateNumberOnly(winningNumber);
+  validateNumberRange(winningNumber);
 };
