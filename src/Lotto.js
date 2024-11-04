413a355b11a1281527ce06dc8794db64fdf16891
“HYUNJIN105” <hyunjin.011005@gmail.com>
2024-11-04 19:08:38 +0900

    feat: 로또 번호 생성 기능 추가
    로또 번호를 생성하는 클래스 Lotto.js 를 작성했다.
    @woowacourse/mission-utils의 Random 클래스의
    pickUniqueNumbersInRange 함수를 사용하여 1~45 사이의
    중복되지 않는 랜덤한 6개의 숫자를 추출한다.
    
    generateRandomNumbers 에서 생성한 6개의 숫자의 유효성 검사를
    1. 생성된 번호의 갯수가 6개인가.
    2. 로또 번호가 중복되었는가.
    3. 로또 번호가 1~45 사이의 숫자인가.
    의 유효성 검사에서 에러가 나지 않으면,
    Lotto 객체를 생성한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -6,13 +8,24 @@ class Lotto {
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  static generateRandomNumbers() {
+    const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return numbers.sort((a, b) => a - b);
   }
 
-  // TODO: 추가 기능 구현
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
+    numbers.forEach((num) => {
+      if (num < 1 || num > 45) {
+        throw new Error('[ERROR] 로또 번호는 1부터 45 사이여야 합니다.');
+      }
+    });
+  }
 }
 
 export default Lotto;
