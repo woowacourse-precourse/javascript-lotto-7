368014c0d6ef524f874d9951445e71254b6137a3
o2jsg <oji5656@naver.com>
2024-11-04 16:04:11 +0900

    feat: 당첨 번호 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,11 +7,17 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if(!isNaN(numbers)) {
+      throw new Error("[ERROR] 잘못된 값 입니다.")
+    }
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
     if (new Set(numbers).size !== numbers.length) {
-      throw new Error("[]")
+      throw new Error("[ERROR] 로또 당첨 번호는 중복될 수 없습니다.");
+    }
+    if (!numbers.every(num => num >= 1 && num <= 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이에 숫자여야 합니다.")
     }
   }
 
