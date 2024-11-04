ac88977939f0a4ff2cb4e65033690bfe9047c089
norangsuji <sujihong93@gmail.com>
2024-11-04 12:51:22 +0900

    feat<InputMassage.js> 사용자 입력 제어 메시지 추가

---
+++ src/messages/InputMessage.js
@@ -0,0 +1,9 @@
+class InputMessage {
+  static INPUT_PRICE = "구입금액을 입력해주세요.\n";
+
+  static INPUT_WINNING_NUMBERS = "당첨 번호를 입력해주세요.\n";
+
+  static INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해주세요.\n";
+}
+
+export default InputMessage;
