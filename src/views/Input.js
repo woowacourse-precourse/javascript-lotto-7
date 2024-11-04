f7774f8e2ac330c0a8f9709a38a77b6a8e8835af
Eunicekk <cho_lyn@naver.com>
2024-11-04 09:36:40 +0900

    [feat] 로또 당첨 번호 입력 기능 구현

--- src/views/Input.js
+++ src/views/Input.js
@@ -13,4 +13,18 @@ export class Input {
 
 		return Number(input);
 	}
+
+	static async getLottoNumbers() {
+		Console.print("당첨 번호를 입력해 주세요.");
+		const input = await Console.readLineAsync("");
+		const numbers = input.split(",").map((number) => Number(number));
+
+		// 가능한 에러
+		// 입력한 숫자 개수가 6개가 아닌 경우
+		// 중복된 값이 있는 경우
+		// 정수가 아닌 값이 들어있는 경우
+		// 1 ~ 45 사이의 수가 아닌 경우
+
+		return numbers;
+	}
 }
