18e1a6b71296e41fb528f20e674444bf51a52bb0
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-04 15:15:17 +0900

    chore: 에러 메시지 추가

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -4,4 +4,7 @@ const ERROR_PREFIX = "[ERROR]";
 
 export const ERROR_MESSAGE = Object.freeze({
   INVALID_PURCHASE_AMOUNT: `${ERROR_PREFIX} 구입 금액은 로또 한 장 가격인 ${LOTTO_PRICE}원 단위여야 합니다.`,
+  INVALID_NUMBERS_LENGTH: `${ERROR_PREFIX} 로또 번호는 6개여야 합니다.`,
+  INVALID_NUMBERS_RANGE: `${ERROR_PREFIX} 로또 번호는 1부터 45 사이여야 합니다.`,
+  NOT_NUMBER: `${ERROR_PREFIX} 숫자가 아닌 값이 포함되어 있습니다.`,
 });
