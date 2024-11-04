8d95414dc9f72a220cda1fc62452dba71ae76fc8
taeyomi <terry11ty@naver.com>
2024-11-04 23:03:41 +0900

    fix: 에러 수정

--- src/constants/statistics.js
+++ src/constants/statistics.js
@@ -1,6 +1,6 @@
 const STATISTICS_MESSAGE = Object.freeze({
-  STATISTICS_DEFAULT_MESSAGE: "\n당첨 통계\n---",
   FIFTH: "3개 일치 (5,000원) - ",
+  STATISTICS_DEFAULT_MESSAGE: "\n당첨 통계\n---",
   FOURTH: "4개 일치 (50,000원) - ",
   THIRD: "5개 일치 (1,500,000원) - ",
   SECOND: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
