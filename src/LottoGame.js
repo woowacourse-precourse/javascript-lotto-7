68beae47f1b6028fa7fe6833d50a1b1ec87c80cc
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-04 22:58:50 +0900

    feat: LottoGame 클래스 생성

---
+++ src/LottoGame.js
@@ -0,0 +1,15 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+
+class LottoGame {
+  #lottoCount;
+  #lotto = [];
+  #resultNumbers = [];
+  #bonusNumber;
+  #matchResults = [0, 0, 0, 0, 0, 0];
+  #profitRate;
+
+  constructor() {}
+}
+
+export default LottoGame;
