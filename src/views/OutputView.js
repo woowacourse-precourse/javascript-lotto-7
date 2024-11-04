db6fce1b2824ffbc4ef4728e25fbfd32a82a4869
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-04 17:45:06 +0900

    feat: 에러 메시지 출력 함수 구현

---
+++ src/views/OutputView.js
@@ -0,0 +1,9 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class OutputView {
+  static printErrorMessage(message) {
+    MissionUtils.Console.print(message);
+  }
+}
+
+export default OutputView;
