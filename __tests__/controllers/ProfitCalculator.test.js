00c82baced819eb531fc05563355507d879f63fe
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-04 17:05:29 +0900

    test(ProfitCalculator): ProfitCalculator 클래스에 맞춰 테스트 코드 업데이트
    - rackCounts와 LOTTO_REWARD 키 매칭 문제 해결
    - 계산 로직 개선

--- __tests__/controllers/ProfitCalculator.test.js
+++ __tests__/controllers/ProfitCalculator.test.js
@@ -1,46 +1,51 @@
 import ProfitCalculator from '../../src/controllers/ProfitCalculator.js';
-import { GAME_SETTINGS, LOTTO_REWARD } from '../../src/utils/constants.js';
+import {
+  GAME_SETTINGS,
+  LOTTO_REWARD,
+  RANK_KEYS,
+} from '../../src/utils/constants.js';
 
-describe.skip('ProfitCalculator 클래스 테스트', () => {
+describe('ProfitCalculator 클래스 테스트', () => {
   test('수익률이 올바르게 계산된다.', () => {
+    // given
     const rankCounts = {
-      threeMatch: 3,
-      fourMatch: 1,
-      fiveMatch: 0,
-      fiveWithBonusMatch: 1,
-      sixMatch: 0,
+      [RANK_KEYS.THREE_MATCH]: 3,
+      [RANK_KEYS.FOUR_MATCH]: 1,
+      [RANK_KEYS.FIVE_MATCH]: 0,
+      [RANK_KEYS.FIVE_WITH_BONUS_MATCH]: 1,
+      [RANK_KEYS.SIX_MATCH]: 0,
     };
     const purchaseAmount = 100000;
 
     const expectedProfit =
-      ((LOTTO_REWARD.threeMatch.prize * 3 +
-        LOTTO_REWARD.fourMatch.prize * 1 +
-        LOTTO_REWARD.fiveWithBonusMatch.prize * 1) /
+      ((LOTTO_REWARD[RANK_KEYS.THREE_MATCH].prize * 3 +
+        LOTTO_REWARD[RANK_KEYS.FOUR_MATCH].prize * 1 +
+        LOTTO_REWARD[RANK_KEYS.FIVE_WITH_BONUS_MATCH].prize * 1) /
         purchaseAmount) *
       GAME_SETTINGS.PERCENTAGE_MULTIPLIER;
 
-    const profitRate = ProfitCalculator.calculateProfitRate(
-      rankCounts,
-      purchaseAmount
-    );
+    // when
+    const profitCalculator = new ProfitCalculator(rankCounts, purchaseAmount);
+    const profitRate = Number(profitCalculator.profitRate);
 
-    expect(Number(profitRate)).toBeCloseTo(expectedProfit, 1);
+    // then
+    expect(profitRate).toBeCloseTo(expectedProfit, 1);
   });
 
   test('당첨금이 없을 때 수익률은 0%로 계산된다.', () => {
+    // given
     const rankCounts = {
-      threeMatch: 0,
-      fourMatch: 0,
-      fiveMatch: 0,
-      fiveWithBonusMatch: 0,
-      sixMatch: 0,
+      [RANK_KEYS.THREE_MATCH]: 0,
+      [RANK_KEYS.FOUR_MATCH]: 0,
+      [RANK_KEYS.FIVE_MATCH]: 0,
+      [RANK_KEYS.FIVE_WITH_BONUS_MATCH]: 0,
+      [RANK_KEYS.SIX_MATCH]: 0,
     };
     const purchaseAmount = 50000;
 
-    const profitRate = ProfitCalculator.calculateProfitRate(
-      rankCounts,
-      purchaseAmount
-    );
+    // when
+    const profitCalculator = new ProfitCalculator(rankCounts, purchaseAmount);
+    const profitRate = profitCalculator.profitRate;
 
     expect(profitRate).toBe('0.0');
   });
