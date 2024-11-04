10a69200d76039ea513068535e26fe41fe8b24ab
deokbin <deokbin@naver.com>
2024-11-04 09:48:54 +0900

    feat(App): 결과 출력 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { InputService } from './service/InputService.js';
+import { OutputService } from './service/OutputService.js';
 
 class App {
   async run() {
     const purchasedLottoInfo = await InputService.getPurchasedLotto();
     const winningLotto = await InputService.getWinningNumbers();
     const bonusNum = await InputService.getBonusNumber(winningLotto);
+
+    OutputService.printResults(purchasedLottoInfo, winningLotto, bonusNum);
   }
 }
 
