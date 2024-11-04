961049346a12d4bc047b91878e32256b24855a32
leesangjae0307 <lee4lee37@naver.com>
2024-11-04 23:45:25 +0900

    111

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,15 +3,14 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b); // 정렬을 여기에 위치시킵니다.
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    if (new Set(numbers).size !== 6) {
-      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    if (numbers.length !== 6 || new Set(numbers).size !== 6) {
+      throw new Error(
+        "[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다."
+      );
     }
     if (!numbers.every((num) => num >= 1 && num <= 45)) {
       throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
@@ -19,7 +18,7 @@ class Lotto {
   }
 
   getNumbers() {
-    return this.#numbers;
+    return this.#numbers; // private field의 값 반환
   }
 }
 
