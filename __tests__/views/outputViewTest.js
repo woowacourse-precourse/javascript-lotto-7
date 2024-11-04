9bbb0fdb02b2fb8522c682fcc13eaa4cfa29a61e
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-11-04 13:15:40 +0900

    test: outputView의 displayLottos 메서드 테스트 추가
    - displayLottos 메서드가 로또 번호 목록을 올바르게 출력하는지 검증

--- __tests__/views/outputViewTest.js
+++ __tests__/views/outputViewTest.js
@@ -12,6 +12,7 @@ describe('outputView 테스트', () => {
 
   test('displayEmptyLine가 빈 줄을 출력하는지 테스트', () => {
     outputView.displayEmptyLine();
+
     expect(Console.print).toHaveBeenCalledWith('');
   });
 
@@ -23,5 +24,17 @@ describe('outputView 테스트', () => {
     expect(Console.print).toHaveBeenCalledWith(
       `${lottoCount}개를 구매했습니다.`
     );
+  });
+
+  test('displayLottos가 로또 번호 목록을 출력하는지 테스트', () => {
+    const lottos = [
+      { numbers: [1, 2, 3, 4, 5, 6] },
+      { numbers: [7, 8, 9, 10, 11, 12] },
+    ];
+    outputView.displayLottos(lottos);
+
+    expect(Console.print).toHaveBeenCalledWith('[1, 2, 3, 4, 5, 6]');
+    expect(Console.print).toHaveBeenCalledWith('[7, 8, 9, 10, 11, 12]');
+    expect(Console.print).toHaveBeenCalledWith('');
   });
 });
