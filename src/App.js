61225f09dc6093e12f8c9d493193136bd157c0f8
“HYUNJIN105” <hyunjin.011005@gmail.com>
2024-11-04 20:43:28 +0900

    feat: 로또 구입 기능 및 구입 금액 입력 유효성 검증 기능 구현
    로또 구입 기능을 추가하고, 사용자가 입력한 금액이 1,000원으로 나누어
    떨어지는지 확인하는 유효성 검증 기능을 추가했다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import { Lotto } from './Lotto.js';
+import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const winningNumbers = Lotto.generateRandomNumbers();
-    const bonusNumber = Lotto.generateBonusNumber(winningNumbers);
+    Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+      const purchaseAmount = parseInt(input, 10);
 
-    console.log('당첨 번호:', winningNumbers);
-    console.log('보너스 번호:', bonusNumber);
+      try {
+        this.validatePurchaseAmount(purchaseAmount);
+
+        const lottoCount = purchaseAmount / 1000;
+        Console.print(`\n${lottoCount}개를 구매했습니다.`);
+
+        const lottos = this.generateLottos(lottoCount);
+        lottos.forEach((lotto) => {
+          Console.print(lotto.numbers);
+        });
+
+      } catch (error) {
+        Console.print(error.message);
+      }
+    });
+  }
+
+  validatePurchaseAmount(amount) {
+    if (isNaN(amount) || amount % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.');
+    }
+  }
+
+  generateLottos(count) {
+    const lottos = [];
+    for (let i = 0; i < count; i++) {
+      lottos.push(new Lotto());
+    }
+    return lottos;
   }
 }
 
