382e477385060fef9f0ccf84c93ca6342ef52361
Parksangwoo <psu8430@gmail.com>
2024-11-04 15:18:46 +0900

    feat(OutputModule) : 생성된 로또 번호 출력 로직 분리

--- src/views/OutputModules.js
+++ src/views/OutputModules.js
@@ -4,4 +4,13 @@ class OutputModules {
   static printMessage(message) {
     Console.print(message);
   }
+
+  static printLottos(lottos) {
+    lottos.forEach((lotto) => {
+      const numbers = lotto.getNumbers().join(', ');
+      Console.print(`[${numbers}]`);
+    });
+  }
 }
+
+export default OutputModules;
