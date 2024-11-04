28de44c56b3e0a535c2295f132af8dea0c8798b7
Jang Eunhye <hh961130@naver.com>
2024-11-04 17:25:05 +0900

    test: Lotto_purchase 예외 처리

--- src/Lotto_purchase.js
+++ src/Lotto_purchase.js
@@ -9,6 +9,18 @@ class Lotto_purchase {
     return myLottoArray;
   }
   validate(payment) {
+    if (isNaN(payment)) {
+      throw new Error("[ERROR] 구입 금액에 문자가 포함될 수 없습니다.");
+    }
+
+    if (payment <= 0) {
+      throw new Error("[ERROR] 구입 금액을 음수로 입력할 수 없습니다.");
+    }
+
+    if (payment == "") {
+      throw new Error("[ERROR] 구입 금액을 입력하지 않았습니다.");
+    }
+
     if (payment % 1000 !== 0) {
       throw new Error("[ERROR] 구입 금액은 1000원 단위로 가능합니다.");
     }
@@ -22,19 +34,22 @@ class Lotto_purchase {
   randomLottoDraw(lottoQuantity) {
     let myLottoArray = [];
     for (let index = 0; index < lottoQuantity; index++) {
-        let myLotto = Random.pickUniqueNumbersInRange(1, 45, 6).sort(function(a, b) {
-            return a - b;
-        });
-        myLottoArray.push(myLotto);
+      let myLotto = Random.pickUniqueNumbersInRange(1, 45, 6).sort(function (
+        a,
+        b
+      ) {
+        return a - b;
+      });
+      myLottoArray.push(myLotto);
     }
     return myLottoArray;
   }
 
-  printMyLottoArray(lottoQuantity, myLottoArray){
-    Console.print("\n" + lottoQuantity + "개를 구매했습니다.")
-    myLottoArray.forEach(myLotto => {
-        let myLottoStr = myLotto.join(", ")
-        Console.print("[" + myLottoStr + "]")
+  printMyLottoArray(lottoQuantity, myLottoArray) {
+    Console.print("\n" + lottoQuantity + "개를 구매했습니다.");
+    myLottoArray.forEach((myLotto) => {
+      let myLottoStr = myLotto.join(", ");
+      Console.print("[" + myLottoStr + "]");
     });
     Console.print("");
   }
