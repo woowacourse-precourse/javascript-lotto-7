4e881a543a66fb659ea04ec29381ba20308020e9
daun-up <awhekdns@kookmin.ac.kr>
2024-11-04 20:17:08 +0900

    refactor: 공통 피드백 반영

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,12 +6,11 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  // 번호 유효성 검사
   #validate(numbers) {
-    if (numbers.length !== 6) {
+    if (!this.#isValidLength(numbers)) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-
-    // TODO: 추가 기능 구현
     if (!this.#isValidRange(numbers)) {
       throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
@@ -20,7 +19,12 @@ class Lotto {
     }
   }
 
+  // 번호 개수 확인
+  #isValidLength(numbers) {
+    return numbers.length === 6;
+  }
 
+  // 번호 범위 확인
   #isValidRange(numbers) {
     return numbers.every((number) => number >= 1 && number <= 45);
   }
@@ -31,6 +35,7 @@ class Lotto {
     return uniqueNumbers.size !== numbers.length;
   }
 
+  // 번호 반환
   getNumbers() {
     return this.#numbers;
   }
