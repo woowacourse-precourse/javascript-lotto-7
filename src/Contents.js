d754befc77348ebaadee06d320595345c6d9a382
dudcns02 <duddl2460@naver.com>
2024-11-04 17:56:20 +0900

    chore: Change output message

--- src/Contents.js
+++ src/Contents.js
@@ -3,16 +3,13 @@ const LOG_MESSAGE = Object.freeze({
   WINNING_NUMBER_MESSAGE: "당첨 번호를 입력해 주세요.\n",
   BONUS_NUMBER_MESSGE: "보너스 번호를 입력해 주세요.\n",
   WINNING_STATISTICS: "당첨 통계\n---",
-  FIFTH_WINNING_DETAILS: `3개 일치 (5,000원) - ${a}개\n`,
-  FOURTH_WINNING_DETAILS: `4개 일치 (50,000원) - ${a}개\n`,
-  THIRD_WINNING_DETAILS: `5개 일치 (1,500,000원) - ${a}개\n`,
-  SECOND_WINNING_DETAILS: `5개 일치, 보너스 볼 일치 (30,000,000원) - ${a}개\n`,
-  FIRST_WINNING_DETAILS: `6개 일치 (2,000,000,000원) - ${a}개\n`,
+  BASIC_WINNING_DETAILS: `${3}개 일치 (${5}) - ${a}개\n`,
+  BONUS_WINNING_DETAILS: `${5}개 일치, 보너스 볼 일치 (${3}) - ${a}개\n`,
 });
 const numberOfOrder = (a) => ({
   NUBER_OF_ORDER: `${a}개를 구입했습니다.\n`,
 });
-const rateOfReturn = (c) => ({
+const RateOfReturn = (c) => ({
   RATE_OF_RETURN: `총 수익률은 ${c}%입니다.`,
 });
 
@@ -31,3 +28,14 @@ const NUMBER_INPUT_ERROR_MESSAGE = Object.freeze({
   DUPLICATE_BONUS_NUMBER:
     "[ERROR] 당첨 번호에 포함된 숫자는 입력할 수 없습니다.",
 });
+
+const THREE_MATCH = 3;
+const FOUR_MATCH = 4;
+const FIVE_MATCH = 5;
+const SIX_MATCH = 6;
+
+const FIFTH = 5000;
+const FORUTH = 50000;
+const THIRD = 1500000;
+const SECOND = 30000000;
+const FIRST = 2000000000;
