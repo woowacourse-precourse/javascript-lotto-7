75a6378176b7fc69f87de3ab3f48fb812a917e2f
kimgho <kgh67811@naver.com>
2024-11-04 15:24:25 +0900

    feat(feature-index): feature index 작성
    - 기능을 묶어 내보내는 index작성

---
+++ src/features/index.js
@@ -0,0 +1,7 @@
+import { ProfitCalculator } from './profitRate/ProfitCalculator.js';
+
+import { WinningNumberMatcher } from './matching/WinningNumberMatcher.js';
+
+import { LottoGenerator } from './lotto/LottoGenerator.js';
+
+export { LottoGenerator, ProfitCalculator, WinningNumberMatcher };
