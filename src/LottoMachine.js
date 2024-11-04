24d6fc2f0f20432abb1c50188aa257154fe0baa7
thisisyejin <yejink321@gmail.com>
2024-11-04 18:40:23 +0900

    refactor: 함수 기능 분리
    함수가 한 가지 일만 잘 수행할 수 있도록 기능을 더
    상세히 분리하였습니다.

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -4,7 +4,7 @@ import { MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT, PRICE } from "./constants/lottoRu
 import Lotto from "./Lotto.js"
 import ERROR_MESSAGE from "./constants/errorMessages.js"
 
-const ASK_PAYMENT_MESSAGE = '구입금액을 입력해 주세요.\n'
+const ASK_PAYMENT_MESSAGE = '\n구입금액을 입력해 주세요.\n'
 const ASK_WINNING_NUMBERS_MESSAGE = '\n당첨 번호를 입력해 주세요.\n'
 const ASK_BOUNS_NUMBER_MESSAGE = '\n보너스 번호를 입력해 주세요.\n'
 
@@ -22,7 +22,6 @@ const LottoMachine = {
       const input = await MissionUtils.Console.readLineAsync(ASK_PAYMENT_MESSAGE);
       LottoMachine.isValidPayment(input);
       this.quantity = Number(input) / PRICE;
-      MissionUtils.Console.print(`\n${this.quantity}개를 구매했습니다.`);
 
     } catch (err) {
       MissionUtils.Console.print(err.message);
@@ -36,14 +35,10 @@ const LottoMachine = {
     if ((input % PRICE) !== 0) throw Error(ERROR_MESSAGE.PAYMENT_IS_NOT_PRICE_PER_UNIT);
   },
 
-  // 당첨 번호 입력
   async askWinningNumbers() {
     try {
       const input = await MissionUtils.Console.readLineAsync(ASK_WINNING_NUMBERS_MESSAGE);
-      const winningNumbers = input.split(',').map((element) => {
-        isInteger(element);
-        return Number(element);
-      })
+      const winningNumbers = this.stringToNumbers(input);
       this.winningNumbers = new Lotto(winningNumbers)
 
     } catch (err) {
@@ -52,13 +47,17 @@ const LottoMachine = {
     }
   },
 
-  // 보너스 번호 입력
+  stringToNumbers(string) {
+    return string.split(',').map((element) => {
+      isInteger(element);
+      return Number(element);
+    })
+  },
+
   async askBonusNumber() {
     try {
       const input = await MissionUtils.Console.readLineAsync(ASK_BOUNS_NUMBER_MESSAGE);
-      isInteger(input);
-      Lotto.isValidNumber(input);
-      if (this.winningNumbers.numbers.includes(Number(input))) throw Error(ERROR_MESSAGE.NUMBERS_ARE_REPEATED)
+      this.isValidBonusNumber(input);
       this.bonusNumber = Number(input);
 
     } catch (err) {
@@ -67,7 +66,13 @@ const LottoMachine = {
     }
   },
 
-  // 로또 번호 발행
+  isValidBonusNumber(input) {
+    isInteger(input);
+    Lotto.isValidNumber(input);
+    if (this.winningNumbers.numbers.includes(Number(input)))
+      throw Error(ERROR_MESSAGE.NUMBERS_ARE_REPEATED)
+  },
+
   getLottoNumber() {
     this.lottoList = Array.from({ length: this.quantity }, () =>
       new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT))
@@ -76,21 +81,19 @@ const LottoMachine = {
     this.printLotto();
   },
 
-  // 로또 번호 정렬
   sortLotto() {
     this.lottoList.map(lotto =>
       lotto.numbers.sort((a, b) => a - b)
     );
   },
 
-  // 로또 번호 출력
   printLotto() {
+    MissionUtils.Console.print(`\n${this.quantity}개를 구매했습니다.`);
     this.lottoList.forEach(lotto =>
       MissionUtils.Console.print(`[${lotto.numbers.join(', ')}]`)
     );
   },
 
-  // 당첨 내역 집계
   getResult() {
     this.lottoList.forEach((lotto) => {
       this.countScore(lotto);
@@ -99,7 +102,6 @@ const LottoMachine = {
     this.printScore();
   },
 
-  // 당첨 내역 확인
   countScore(lotto) {
     // 당첨 번호와 일치하는 로또 번호의 개수
     const matches = lotto.numbers.filter(number =>
@@ -117,17 +119,15 @@ const LottoMachine = {
     if (matches === 3) return this.score.THREE_MATCHES += 1;
   },
 
-  // 수익률 계산
   countProfit() {
     const profit = Math.round(
       ((5000 * this.score.THREE_MATCHES) + (50000 * this.score.FOUR_MATCHES)
-      + (1500000 * this.score.FIVE_MATCHES) + (30000000 * this.score.FIVE_BONUS_MATCHES)
-      + (2000000000 * this.score.SIX_MATCHES)) // 총 수익
+        + (1500000 * this.score.FIVE_MATCHES) + (30000000 * this.score.FIVE_BONUS_MATCHES)
+        + (2000000000 * this.score.SIX_MATCHES)) // 총 수익
       / (this.quantity * PRICE) * 100 * 10) / 10;
     this.profit = profit;
   },
 
-  // 당첨 내역 및 수익률 출력
   printScore(score = this.score) {
     const str = `
 당첨 통계
