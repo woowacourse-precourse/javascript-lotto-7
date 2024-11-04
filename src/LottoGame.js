3d9c4b35f339d2a647d266cfbc88dbcdf5b1a510
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-11-04 23:58:18 +0900

    fix: 에러 메시지 수정

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -136,7 +136,7 @@ class LottoGame {
     }
     resultNumbers.split(',').forEach(number => {
       if (Number(number) < 1 || Number(number) > 45) {
-        throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+        throw new Error('[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.');
       }
     });
   }
