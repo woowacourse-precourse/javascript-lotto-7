7d59b0b75db9282e26758bb66aae68f80c65e997
Hwigeon <bb7788@naver.com>
2024-11-04 15:29:59 +0900

    feat(Purchase): 금액 검증과 출력을 포함한 Purchase 클래스 구현
    - 구입 금액이 정수인지, 1000원 단위인지 검사합니다.
    - 구입한 로또 티켓 개수를 출력합니다.
    - get 메서드로 티켓 수량과 금액을 확인 가능합니다.

---
+++ src/Purchase.js
@@ -0,0 +1,34 @@
+
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class Purchase {
+  #money;
+  #tickets;
+  
+  constructor(money) {
+    this.#validateMoney(money);
+    this.#money = money;
+    this.#tickets = money / 1000;
+  }
+
+  //구입 금액의 유효성을 검증
+  #validateMoney(money) {
+    if (!Number.isInteger(money) || money < 1000 || money % 1000 !== 0) {
+        throw new Error("[ERROR] 구입 금액은 1,000원 단위의 양의 정수여야 합니다.");
+      }
+  }
+
+  getTickets() {
+    return this.#tickets;
+  }
+
+  getMoney() {
+    return this.#money;
+  }
+  //구입한 로또 티켓 개수를 출력
+  printTickets() {
+    MissionUtils.Console.print(`${this.#tickets}개를 구매했습니다.`);
+  }
+}
+
+export default Purchase;
