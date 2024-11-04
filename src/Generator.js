d98d64e907596363bba9c53006231424bafca4c6
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-04 14:28:31 +0900

    feat: 구입 금액이 로또 1장 값으로 나누어지지 않을 경우 예외 발생 기능 구현

--- src/Generator.js
+++ src/Generator.js
@@ -1,16 +1,27 @@
 import { isNaturalNumber } from './utils/isNaturalNumber.js';
 import { ERROR_MESSAGE } from './constants/ERROR_MESSAGES.js';
+import { stringToNumber } from './utils/stringToNumber.js';
+import { LOTTO } from './constants/LOTTO_CONSTANTS.js';
 
 class Generator {
   #purchaseAmount;
 
   constructor(input) {
     this.#validateNaturalNumber(input);
+    const numberInput = stringToNumber(input);
+    this.#validateDivisibleByTicketPrice(numberInput);
+    this.#purchaseAmount = numberInput;
   }
 
   #validateNaturalNumber(input) {
     if (!isNaturalNumber(input)) {
       throw new Error(ERROR_MESSAGE.NOT_A_NATURAL_NUMBER);
+    }
+  }
+
+  #validateDivisibleByTicketPrice(numberInput) {
+    if (numberInput % LOTTO.TICKET_PRICE !== 0) {
+      throw new Error(ERROR_MESSAGE.WINNING_AMOUNT_MUST_BE_MULTIPLE_OF_1000);
     }
   }
 }
