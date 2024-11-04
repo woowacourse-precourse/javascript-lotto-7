9406ff98c702bc70d7c03e692f63f5f4c8fe3443
seongmin <seongm22n@gmail.com>
2024-11-04 16:25:50 +0900

    feat: 기능6 당첨 내역 출력

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import { Console } from '@woowacourse/mission-utils';
 import { LottoInputHandler } from './LottoInputHandler.js';
+import { LottoHandler } from './LottoHandler.js';
+import { LottoInputValidator } from './utils/LottoInputValidator.js';
 
 class App {
 	async run() {
 		const inputHandler = new LottoInputHandler();
+		const validator = new LottoInputValidator();
+		const lottoHandler = new LottoHandler();
+
 		const inputAmount = await Console.readLineAsync(
 			'구입금액을 입력해 주세요.\n'
 		);
-		inputHandler.getPurchaseAmount(inputAmount);
+		const lottoCount = validator.getLottoPrice(inputAmount);
+		const lottos = lottoHandler.buyLotto(lottoCount);
 
-		const winNumbers = await Console.readLineAsync(
+		const winNumber = await Console.readLineAsync(
 			`\n당첨 번호를 입력해 주세요.\n`
 		);
-		inputHandler.getWinningNumbers(winNumbers);
+		inputHandler.getWinningNumbers(winNumber);
 
 		const bonusNumber = await Console.readLineAsync(
 			`\n보너스 번호를 입력해 주세요.\n`
 		);
-		inputHandler.getBonusNumber(bonusNumber,winNumbers);
+		inputHandler.getBonusNumber(bonusNumber, winNumber);
+
+		Console.print('\n당첨 통계\n---');
+		const winMoney = lottoHandler.checkLottoWin(lottos, winNumber, bonusNumber);
+		lottoHandler.checkProfitRate(winMoney, lottoCount);
 	}
 }
 
