4a7f76e60808a80b1551b40c3d3fbb4a86fd136f
luii0415 <becky45210193@gmail.com>
2024-11-04 17:53:24 +0900

    feat : create
    사용자 입력처리
    구입금액에 대한 예외처리 (구입금액이 0이거나, 음수인 경우, 사용자 금액의
    최소단위가 1000이 아닌 경우)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async run() {}
+  async UserInput() {
+    const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    const amount = Number(input);
+
+    if (isNaN(amount) || amount % 1000 !== 0 || amount <= 0) {
+      throw new Error("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+    }
+
+    return amount;
+  }
+  async run() {
+    let lottos = [];
+  }
 }
 
 export default App;
