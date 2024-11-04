4eac2f4c867db160e84d4424a5d54b476260a275
zzzzzuuuuu <agu0830@naver.com>
2024-11-04 18:03:35 +0900

    refactor: 에러메시지 내용 수정

--- Constants.js
+++ Constants.js
@@ -27,9 +27,9 @@ export const ERROR_MESSAGES = {
   INVALID_PURCHASE_AMOUNT: "[ERROR] 구입 금액은 1,000원 단위여야 합니다.",
   INVALID_AMOUNT_TYPE: "[ERROR] 구입 금액은 숫자로 입력해 주세요.",
   NEGATIVE_OR_ZERO_AMOUNT: "[ERROR] 구입 금액은 양수로 입력해 주세요.",
-  INVALID_WINNING_NUMBERS_COUNT: "[ERROR] 당첨 번호는 6개여야 합니다.",
-  WINNING_NUMBER_RANGE: "[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다.",
-  WINNING_NUMBER_DUPLICATE: "[ERROR] 당첨 번호에는 중복된 숫자가 없어야 합니다.",
+  INVALID_WINNING_NUMBERS_COUNT: "[ERROR] 로또 번호는 6개여야 합니다.",
+  WINNING_NUMBER_RANGE: "[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.",
+  WINNING_NUMBER_DUPLICATE: "[ERROR] 로또 번호에는 중복된 숫자가 없어야 합니다.",
   BONUS_NUMBER_RANGE: "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.",
   BONUS_NUMBER_DUPLICATE: "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.",
 };
