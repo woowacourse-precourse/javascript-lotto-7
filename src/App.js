728792eefbb8abedd1405deb5cdcf137b8292fcb
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-11-04 19:30:21 +0900

    feat(App): 실행 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import LottoController from './LottoController';
+import LottoMachine from './LottoMachine';
+
 class App {
-  async run() {}
+  async run() {
+    const controller = new LottoController();
+    const machine = new LottoMachine();
+
+    const amount = await controller.getLottoAmount();
+    const lottos = machine.generateLottos(amount / 1000);
+    controller.printLottoCount(lottos.length);
+    controller.printLottos(lottos);
+
+    const winningNumbers = await controller.getLottoNumber();
+    const bonusNumber = await controller.getLottoBonusNumber(winningNumbers);
+
+    const statistics = controller.calculateStatistics(
+      lottos,
+      winningNumbers,
+      bonusNumber,
+      machine
+    );
+    const profitRate = controller.calculateProfitRate(statistics, amount);
+    controller.printStatistics(statistics, profitRate);
+  }
 }
 
 export default App;
