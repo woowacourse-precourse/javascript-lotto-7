c0fd6b09b108705a16fcc8662f470c7f6d9d307a
Jang Eunhye <hh961130@naver.com>
2024-11-04 16:41:36 +0900

    style: 주석 삭제

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,12 +15,12 @@ class Lotto {
     if (numbers.some(number => number < 1) || numbers.some(number => 45 < number)) {
       throw new Error("[ERROR] 로또 번호는 1 - 45 만 가능합니다.");
     }
-    // 중복숫자
+
     let setNumbers = new Set(numbers);
     if (numbers.length !== setNumbers.size) {
       throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
-    // 문자 포함
+
     if (numbers.some(number => isNaN(number))) {
       throw new Error("[ERROR] 로또 번호에 문자가 포함될 수 없습니다.");
     }
