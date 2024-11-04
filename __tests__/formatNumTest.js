e8042fd0161a81365854aff4810d33b48a981da2
deokbin <deokbin@naver.com>
2024-11-04 09:52:11 +0900

    test(formatNumTest): 숫자 포매팅 함수 테스트 코드 추가

---
+++ __tests__/formatNumTest.js
@@ -0,0 +1,13 @@
+import formatNum from '../src/util/formatNum.js';
+
+jest.mock('../src/util/formatNum.js');
+
+formatNum.mockReturnValue('1,000');
+
+describe('formatNum 함수 테스트', () => {
+  test('formatNum 함수가 1,000을 반환하는지 확인', () => {
+    const result = formatNum(1000);
+    expect(result).toBe('1,000');
+    expect(formatNum).toHaveBeenCalledWith(1000);
+  });
+});
