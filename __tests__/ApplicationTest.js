6fbfb4ab034dab089ef8104614c528399039e446
JHLEE325 <ljntiger325@naver.com>
2024-11-04 17:50:00 +0900

    feat : 오류시 재입력 받는 기능 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -92,6 +92,6 @@ describe("로또 테스트", () => {
   });
 
   test("예외 테스트", async () => {
-    await expect(runException("1000j")).rejects.toThrow('[ERROR] 금액은 숫자만 입력 가능합니다.');
-});
+    await runException("1000j");
+  });
 });
--- src/App.js
+++ src/App.js
@@ -3,36 +3,58 @@ import { getInput } from "./Input.js";
 import { LOTTO } from "./constant.js";
 import { prize } from "./Prize.js";
 import { validateBonusnum, validateBudget, validateWinnum } from "./Validation.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const budget = await this.getBudget();
-    const lotto = purchase(budget / LOTTO.PRICE);
+    let budget, lotto, winnum, bonusnum;
 
-    const winnum = await this.getWinnum();
-    const bonusnum = await this.getBonusnum(winnum);
+    while (true) {
+      try {
+        budget = await getInput('구입금액을 입력해 주세요.');
+        validateBudget(budget);
+        lotto = purchase(budget / LOTTO.PRICE);
+        break;
+      } catch (error) {
+        if (error.message.startsWith("[ERROR]")) {
+          Console.print(error.message);
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    while (true) {
+      try {
+        const winnumInput = await getInput('당첨 번호를 입력해 주세요.');
+        winnum = winnumInput.split(',').map(num => parseInt(num.trim(), 10));
+        validateWinnum(winnum);
+        break;
+      } catch (error) {
+        if (error.message.startsWith("[ERROR]")) {
+          Console.print(error.message);
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    while (true) {
+      try {
+        const bonusnumInput = await getInput('보너스 번호를 입력해 주세요.');
+        bonusnum = parseInt(bonusnumInput.trim(), 10);
+        validateBonusnum(bonusnum, winnum);
+        break;
+      } catch (error) {
+        if (error.message.startsWith("[ERROR]")) {
+          Console.print(error.message);
+        } else {
+          throw error;
+        }
+      }
+    }
 
     prize(lotto, winnum, bonusnum);
-  }
-
-  async getBudget() {
-    const budget = await getInput('\n구입금액을 입력해 주세요.\n');
-    validateBudget(budget);
-    return budget;
-  }
-
-  async getWinnum() {
-    const winnumInput = await getInput('\n당첨 번호를 입력해 주세요.\n');
-    const winnum = winnumInput.split(',').map(num => parseInt(num.trim(), 10));
-    validateWinnum(winnum);
-    return winnum;
-  }
-
-  async getBonusnum(winnum) {
-    const bonusnumInput = await getInput('\n보너스 번호를 입력해 주세요.\n');
-    const bonusnum = parseInt(bonusnumInput.trim(), 10);
-    validateBonusnum(bonusnum, winnum);
-    return bonusnum;
   }
 }
 
