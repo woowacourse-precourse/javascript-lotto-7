67298c2a6e6e15022108265db7edfc6700594b78
dudcns02 <duddl2460@naver.com>
2024-11-04 23:07:21 +0900

    chore: Import log message, price validator fuction and remove async

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,53 +1,66 @@
 import { Console } from "@woowacourse/mission-utils";
+import { LOG_MESSAGE } from "../Constants.js";
+import {
+  isNumber,
+  isUnitOfPrice,
+  minPrice,
+  maxPrice,
+} from "../validator/PriceValidator.js";
 
 class InputView {
-  static async getPriceInput() {
+  static getPriceInput() {
     try {
-      const price = await Console.readLineAsync(LOG_MESSAGE.START_MESSAGE);
+      const price = Console.readLineAsync(LOG_MESSAGE.START_MESSAGE);
       // price = '8000'
       isNumber(price);
       isUnitOfPrice(price);
-      minprice(price);
-      maxprice(price);
+      minPrice(price);
+      maxPrice(price);
 
-      getLottoCount(price);
+      return getLottoCount(price);
     } catch (error) {
       Console.print(error.message);
       return getPriceInput();
     }
   }
 
-  static async getWinningNumber() {
+  static getWinningNumber() {
     try {
-      const winningNumber = await Console.readLineAsync(
+      const winningNumber = Console.readLineAsync(
         LOG_MESSAGE.WINNING_NUMBER_MESSAGE
       );
       // winningNuber = '1,2,3,4,5,6'
       winningNumber = winningNumber.split(",");
 
-      CheckWinningNumberInput(winningNumber);
-      isWinningNumberLength(winningNumber);
-      checkNumberRange(winningNumber);
-      isDuplicateNumber(winningNumber);
-    } catch (erro) {
+      NumberValidator.CheckWinningNumberInput(winningNumber);
+      NumberValidator.isWinningNumberLength(winningNumber);
+      NumberValidator.checkNumberRange(winningNumber);
+      NumberValidator.isDuplicateNumber(winningNumber);
+    } catch (error) {
       Console.print(error.message);
       return getWinningNumber();
     }
   }
 
-  static async getBonusNumber() {
+  static getBonusNumber() {
     try {
-      const bonusNumber = await Console.readLineAsync(
+      const bonusNumber = Console.readLineAsync(
         LOG_MESSAGE.BONUS_NUMBER_MESSGE
       ); // bonusNumber = '7'
       bonusNumber = bonusNumber.trim();
 
-      isNumber(bonusNumber);
-      checkNUmberRange(bonusNumber);
-      isDuplicateBonusNumber(bonusNumber);
+      PriceValidator.isNumber(bonusNumber);
+      NumberValidator.checkNUmberRange(bonusNumber);
+      NumberValidator.isDuplicateBonusNumber(bonusNumber);
     } catch (erro) {
       Console.print(erro.message);
       return getBonusNumber();
     }
   }
+  static getLottoCount(price) {
+    const lottoCount = parseInt(price / 1000, 10);
+    return lottoCount;
+  }
 }
+
+export default InputView;
