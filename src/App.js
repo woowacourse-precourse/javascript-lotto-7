475d19cf7ffca734d2bc9f9a6b84442c784df7ba
joarthvr <ioi456859@gmail.com>
2024-11-04 22:58:45 +0900

    feat: 구입량 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+import purchaseAmount from "./util/purchaseAmount.js";
 class App {
-  async run() {}
+  async run() {
+    const amount = await purchaseAmount();
+    Console.print(`구입량: ${amount}`);
+  }
 }
 
 export default App;
