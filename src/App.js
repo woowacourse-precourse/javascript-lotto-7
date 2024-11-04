8ce3c275ed16ba9a4eee02ba22d902d129e6f8fd
eunjin11 <whdmswls9207@gmail.com>
2024-11-04 09:46:50 +0900

    refactor(App): 당첨 번호 로또로 발행

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 import purchaseLotto from "./purchaseLotto.js";
 import matchLotto from "./matchLotto.js";
 import printMatchResult from "./printMatchResult.js";
@@ -12,14 +13,21 @@ class App {
     const inputWinningNumbers = await Console.readLineAsync(
       "당첨 번호를 입력해 주세요.\n"
     );
-    const winningNumbers = inputWinningNumbers.split(",").map(Number);
+
+    const winningNumbers = new Lotto(
+      inputWinningNumbers.split(",").map(Number)
+    );
 
     const inputBonusNumber = await Console.readLineAsync(
       "\n보너스 번호를 입력해 주세요.\n"
     );
     const bonusNumber = Number(inputBonusNumber);
 
-    const matchCountList = matchLotto(lottoList, winningNumbers, bonusNumber);
+    const matchCountList = matchLotto(
+      lottoList,
+      winningNumbers.getNumbers(),
+      bonusNumber
+    );
 
     printMatchResult(matchCountList);
 
