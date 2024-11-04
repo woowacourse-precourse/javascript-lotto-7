4fe1fc5e33629430c26e6e93cb672cc958eef72d
jiyeeeah <sije3757@snu.ac.kr>
2024-11-04 10:09:12 +0900

    refactor: lotto관련 util들 LottoUtil class의 메서드로 수정
    - lotto 관련 util 함수들을 LottoUtil class의 static 메서드로 수정하여
      객체 지향에 어울리는 모양새로 만듦
    - LottoMachine에만 쓰이는 Util들이라서 LottoUtil class로 묶음

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -1,12 +1,6 @@
 import { ERROR_MESSAGE } from "./constants/messages.js";
 import { LOTTO_RULE } from "./constants/rule.js";
-import {
-  buyMultipleTickets,
-  calculateProfit,
-  createWinningRankCount,
-  getRankType,
-  parseWinningLottoString,
-} from "./utils/lotto.js";
+import LottoUtil from "./utils/lotto.js";
 
 class LottoMachine {
   #amount;
@@ -17,7 +11,7 @@ class LottoMachine {
     const inputNumber = Number(input);
     this.#validate(inputNumber);
     this.#amount = inputNumber / LOTTO_RULE.PRICE;
-    this.#tickets = buyMultipleTickets(this.#amount);
+    this.#tickets = LottoUtil.buyMultipleTickets(this.#amount);
   }
 
   #validate(number) {
@@ -34,21 +28,21 @@ class LottoMachine {
   }
 
   getWinningLottoString({ winningLotto, bonusNumber }) {
-    this.#winningRankCount = createWinningRankCount();
+    this.#winningRankCount = LottoUtil.createWinningRankCount();
 
     this.#tickets.forEach((ticket) => {
       const matchCount = winningLotto.getMatchCountFrom(ticket);
       const isBonusMatch = bonusNumber.hasBonusNumberIn(ticket);
-      const rank = getRankType(matchCount, isBonusMatch);
+      const rank = LottoUtil.getRankType(matchCount, isBonusMatch);
       if (rank) this.#winningRankCount[rank] += 1;
     });
 
-    return parseWinningLottoString(this.#winningRankCount);
+    return LottoUtil.parseWinningLottoString(this.#winningRankCount);
   }
 
   getProfitRateString() {
     const investAmount = this.#amount * LOTTO_RULE.PRICE;
-    const profitAmount = calculateProfit(this.#winningRankCount);
+    const profitAmount = LottoUtil.calculateProfit(this.#winningRankCount);
 
     return `총 수익률은 ${((profitAmount / investAmount) * 100).toFixed(1)}%입니다.`;
   }
--- src/utils/lotto.js
+++ src/utils/lotto.js
@@ -1,39 +1,44 @@
 import { Random } from "@woowacourse/mission-utils";
 import { LOTTO_RULE, LOTTO_WIN_RANK } from "../constants/rule.js";
 
-export const buyOneLotto = () =>
-  Random.pickUniqueNumbersInRange(LOTTO_RULE.MIN_NUMBER, LOTTO_RULE.MAX_NUMBER, 6);
+class LottoUtil {
+  static #buyOneLotto = () =>
+    Random.pickUniqueNumbersInRange(LOTTO_RULE.MIN_NUMBER, LOTTO_RULE.MAX_NUMBER, 6);
 
-export const buyMultipleTickets = (amount) =>
-  Array.from({ length: amount }, () => buyOneLotto().sort((a, b) => a - b));
+  static buyMultipleTickets = (amount) =>
+    Array.from({ length: amount }, () => LottoUtil.#buyOneLotto().sort((a, b) => a - b));
 
-export const getRankType = (matchCount, isBonusMatch) => {
-  const [rankType] = Object.entries(LOTTO_WIN_RANK)
-    .filter(
-      ([, details]) => details.matchCount === matchCount && details.requiresBonus === isBonusMatch,
-    )
-    .map(([rank]) => rank);
+  static getRankType = (matchCount, isBonusMatch) => {
+    const [rankType] = Object.entries(LOTTO_WIN_RANK)
+      .filter(
+        ([, details]) =>
+          details.matchCount === matchCount && details.requiresBonus === isBonusMatch,
+      )
+      .map(([rank]) => rank);
 
-  return rankType;
-};
+    return rankType;
+  };
 
-export const createWinningRankCount = () =>
-  Object.keys(LOTTO_WIN_RANK).reduce((acc, rank) => {
-    acc[rank] = 0;
-    return acc;
-  }, {});
+  static createWinningRankCount = () =>
+    Object.keys(LOTTO_WIN_RANK).reduce((acc, rank) => {
+      acc[rank] = 0;
+      return acc;
+    }, {});
 
-export const parseWinningLottoString = (
-  winningRankCount,
-) => `${LOTTO_WIN_RANK.threeMatch.string} (${LOTTO_WIN_RANK.threeMatch.prize.toLocaleString()}원) - ${winningRankCount.threeMatch}개
-${LOTTO_WIN_RANK.fourMatch.string} (${LOTTO_WIN_RANK.fourMatch.prize.toLocaleString()}원) - ${winningRankCount.fourMatch}개
-${LOTTO_WIN_RANK.fiveMatch.string} (${LOTTO_WIN_RANK.fiveMatch.prize.toLocaleString()}원) - ${winningRankCount.fiveMatch}개
-${LOTTO_WIN_RANK.fiveMatchAndBonus.string} (${LOTTO_WIN_RANK.fiveMatchAndBonus.prize.toLocaleString()}원) - ${winningRankCount.fiveMatchAndBonus}개
-${LOTTO_WIN_RANK.allMatch.string} (${LOTTO_WIN_RANK.allMatch.prize.toLocaleString()}원) - ${winningRankCount.allMatch}개`;
+  static parseWinningLottoString = (
+    winningRankCount,
+  ) => `${LOTTO_WIN_RANK.threeMatch.string} (${LOTTO_WIN_RANK.threeMatch.prize.toLocaleString()}원) - ${winningRankCount.threeMatch}개
+    ${LOTTO_WIN_RANK.fourMatch.string} (${LOTTO_WIN_RANK.fourMatch.prize.toLocaleString()}원) - ${winningRankCount.fourMatch}개
+    ${LOTTO_WIN_RANK.fiveMatch.string} (${LOTTO_WIN_RANK.fiveMatch.prize.toLocaleString()}원) - ${winningRankCount.fiveMatch}개
+    ${LOTTO_WIN_RANK.fiveMatchAndBonus.string} (${LOTTO_WIN_RANK.fiveMatchAndBonus.prize.toLocaleString()}원) - ${winningRankCount.fiveMatchAndBonus}개
+    ${LOTTO_WIN_RANK.allMatch.string} (${LOTTO_WIN_RANK.allMatch.prize.toLocaleString()}원) - ${winningRankCount.allMatch}개`;
 
-export const calculateProfit = (winningRankCount) =>
-  winningRankCount.threeMatch * LOTTO_WIN_RANK.threeMatch.prize +
-  winningRankCount.fourMatch * LOTTO_WIN_RANK.fourMatch.prize +
-  winningRankCount.fiveMatch * LOTTO_WIN_RANK.fiveMatch.prize +
-  winningRankCount.fiveMatchAndBonus * LOTTO_WIN_RANK.fiveMatchAndBonus.prize +
-  winningRankCount.allMatch * LOTTO_WIN_RANK.allMatch.prize;
+  static calculateProfit = (winningRankCount) =>
+    winningRankCount.threeMatch * LOTTO_WIN_RANK.threeMatch.prize +
+    winningRankCount.fourMatch * LOTTO_WIN_RANK.fourMatch.prize +
+    winningRankCount.fiveMatch * LOTTO_WIN_RANK.fiveMatch.prize +
+    winningRankCount.fiveMatchAndBonus * LOTTO_WIN_RANK.fiveMatchAndBonus.prize +
+    winningRankCount.allMatch * LOTTO_WIN_RANK.allMatch.prize;
+}
+
+export default LottoUtil;
