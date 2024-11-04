c4a357f12adcad6c1ceb26c78a9ecb0c76b67b7c
goodbyebada <goodbyebada@naver.com>
2024-11-04 21:07:32 +0900

    feat(emptyStr) : ONLY_WHITE_SPACE_REGEX  추가

--- src/Utills/Check/emptyStr.js
+++ src/Utills/Check/emptyStr.js
@@ -1,4 +1,7 @@
 export function emptyString(input) {
+  const ONLY_WHITE_SPACE_REGEX = /^\s*$/;
+
+  if (ONLY_WHITE_SPACE_REGEX.test(input)) return true;
   if (input === '' || input === undefined || input === null) return true;
   return false;
 }
