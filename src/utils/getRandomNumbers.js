fe697939fff0383fb648317a51906c9109748e4a
CJY <cjy3458@naver.com>
2024-11-04 10:19:13 +0900

    chore: 단일 export 가져오기 수정

--- src/utils/getRandomNumbers.js
+++ src/utils/getRandomNumbers.js
@@ -1,5 +1,5 @@
 import { Random } from '@woowacourse/mission-utils';
-import { LOTTO_NUMBERS } from '../constants/config.js';
+import LOTTO_NUMBERS from '../constants/config.js';
 
 const getRandomLottoNumbers = () =>
   Random.pickUniqueNumbersInRange(
