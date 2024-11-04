42c3dd5a0d80403ca58008e69278f66e11520797
erica-jeong <wjddbwjd1205@gmail.com>
2024-11-04 22:04:39 +0900

    test: Lotto 클래스 예외 및 기능 검증 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,24 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호가 유효 범위를 벗어나면 예외가 발생한다.", () => {
+    expect(() => new Lotto([0, 2, 3, 4, 5, 6])).toThrow('[ERROR]')
+    expect(() => new Lotto([1, 2, 3, 4, 5, 46])).toThrow('[ERROR]')
+  });
+
+  test("로또 번호가 올바르게 정렬되어 저장된다.", () => {
+    const lotto = new Lotto([6, 2, 3, 1, 5, 4]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test("toString 메서드가 올바른 문자열을 반환한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.toString()).toBe("[1, 2, 3, 4, 5, 6]");
+  });
+
+  test("getNumbers 메서드가 번호 배열을 올바르게 반환한다.", () => {
+    const lottoNumbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(lottoNumbers);
+    expect(lotto.getNumbers()).toEqual(lottoNumbers);
+  });
 });
