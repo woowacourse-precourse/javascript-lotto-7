2e5704d8fc468d37703a587bb163f4f32124a741
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 19:45:20 +0900

    feat: 로또 관련 UI 로직을 담당하는 LottoPrinter class 구현

---
+++ src/LottoPrinter.js
@@ -0,0 +1,31 @@
+import printOutput from './utils/printOutput.js';
+import { OUTPUT_MESSAGE } from './constants/inputOutputMessages.js';
+
+class LottoPrinter {
+  static lottoNumbers(lottoNumbers) {
+    printOutput(`\n${lottoNumbers.length}${OUTPUT_MESSAGE.PURCHASE_COUNT}`);
+    lottoNumbers.forEach((lotto) => printOutput(lotto));
+    printOutput('');
+  }
+
+  static winningCount(winningCount) {
+    printOutput(`\n${OUTPUT_MESSAGE.WINNING_STATISTICS}`);
+    printOutput(OUTPUT_MESSAGE.HYPHENS);
+
+    Object.entries(winningCount).forEach(([key, count]) => {
+      printOutput(
+        `${OUTPUT_MESSAGE[`MATCH_${key}`]}${count}${
+          OUTPUT_MESSAGE.MATCH_COUNT_UNIT
+        }`
+      );
+    });
+  }
+
+  static totalReturn(totalReturn) {
+    printOutput(
+      `${OUTPUT_MESSAGE.TOTAL_RETURN}${totalReturn}${OUTPUT_MESSAGE.PERCENTAGE_SUFFIX}`
+    );
+  }
+}
+
+export default LottoPrinter;
