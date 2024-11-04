a6c05b0f27817ad958376b8d5b3dfb0abdca61ae
KimPuro <puro@protonmail.ch>
2024-11-04 18:50:21 +0900

    feat(Lotto): #validate에 try catch 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,9 +7,17 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    let isValid = false;
+    while(!isValid) {
+      try{
+        if (numbers.length !== 6) {
+          throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+      } catch(e) {
+        console.error(e.message);
+      }
     }
+
   }
 
   // TODO: 추가 기능 구현
