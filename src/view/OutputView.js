06e7d344c16317e4a640ab3432496cd9e59cb38c
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 22:48:36 +0900

    feat: 당첨 통계를 출력하는 로직 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,6 +4,14 @@ const OutputView = {
   printLottoTickes(tickets) {
     Console.print(`\n${tickets.length}개를 구매했습니다.`);
     tickets.forEach((ticket) => Console.print(`[${ticket.numbers.join(", ")}]`));
+  },
+
+  printResults(ranks, earningRate) {
+    Console.print("\n당첨 통계\n---")
+    Object.entries(ranks).forEach(([rank, count]) => {
+      Console.print(`${rank} - ${count}개`)
+    });
+    Console.print(`총 수익률은 ${earningRate}% 입니다.`);
   }
 }
 
