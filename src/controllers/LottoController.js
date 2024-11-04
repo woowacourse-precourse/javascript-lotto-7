65457a5e6526bfde2b30fd46e26d1474f397020a
위영진 <qser155@naver.com>
2024-11-04 17:49:54 +0900

    feat: 로또 구매와 결과 조회 동작 구현

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -1,1 +1,50 @@
-//
+import { Input, Output } from '../views/index.js';
+import Lotto from '../models/Lotto.js';
+
+class LottoController {
+    #lottos;
+    #money;
+    #winningNumbers;
+    #bonusNumber;
+
+    constructor() {
+        this.#lottos = [];
+        this.#money = 0;
+        this.#winningNumbers = [];
+        this.#bonusNumber = 0;
+    }
+
+    async run() {
+        await this.#purchaseLottos();
+        await this.#getWinningInfo();
+        this.#displayResults();
+    }
+
+    async #purchaseLottos() {
+        this.#money = await Input.getMoney();
+        this.#lottos = Lotto.createLottos(this.#money);
+
+        Output.printPurchaseAmount(this.#lottos.length);
+        Output.printLottos(this.#lottos);
+    }
+
+    async #getWinningInfo() {
+        this.#winningNumbers = await Input.getWinNumbers();
+        this.#bonusNumber = await Input.getBonusNumber(this.#winningNumbers);
+    }
+
+    #displayResults() {
+        const results = Lotto.calculateResults(
+            this.#lottos,
+            this.#winningNumbers,
+            this.#bonusNumber
+        );
+
+        const earningRate = Lotto.calculateEarningRate(results, this.#money);
+
+        Output.printWinningStatus(results);
+        Output.printEarningsRate(earningRate);
+    }
+}
+
+export default LottoController;
