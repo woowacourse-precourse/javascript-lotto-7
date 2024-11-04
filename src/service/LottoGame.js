a9f388ab845880c71eae338a8b7fc35acefc8383
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 22:55:40 +0900

    feat: LottoGame 생성자 메서드 추가

--- src/service/LottoGame.js
+++ src/service/LottoGame.js
@@ -1,7 +1,14 @@
 import Lotto from "../model/Lotto.js";
 
 class LottoGame {
+  constructor(purchaseAmount) {
+    this.lottoTickets = this.generateLottoTickets(purchaseAmount);
+  }
 
+  generateLottoTickets(amount) {
+    const ticketCount = Math.floor(amount / 1000);
+    return Array.from({ length: ticketCount }, () => new Lotto());
+  }
 }
 
 export default LottoGame;
