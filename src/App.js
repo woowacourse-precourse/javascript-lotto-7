cda844209a3c2bf530502084b68cb3abdc0f67ab
kwonsoonook <sunoogy99@naver.com>
2024-11-04 18:56:31 +0900

    feat: 구입 금액 입력 함수 inputAmount 구현
    - 사용자로부터 구입 금액 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export async function inputAmount() {
+  const amount = await Console.readLineAsync("구입금액을 입력해 주세요.");
+  return amount;
+}
+
 class App {
   async run() {}
 }
