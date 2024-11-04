60fb80436db20640c9efd0e8c658067703f43693
조승우 <choseung97@gmail.com>
2024-11-04 16:08:51 +0900

    feat(game): 보너스 번호가 올바를 때까지 입력 받는 기능을 추가한다
    `inputBonusNumber()`메서드에서 `validateBonusNumber()`를 통과할 때까지 입력을 받는다.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import LottoGenerator from './components/Lotto/LottoGenerator.js';
 import ResultCalculator from './components/ResultCalculator.js';
 import StatisticCalculation from './components/StatisticCalculation.js';
 import { printGeneratedList, printFinalResults } from './utils/OutputView.js';
-import { inputPurchaseAmount, inputWinningNumbers } from './utils/InputView.js';
+import {
+  inputBonusNumber,
+  inputPurchaseAmount,
+  inputWinningNumbers,
+} from './utils/InputView.js';
 
 class App {
   async run() {
     const inputMoney = await inputPurchaseAmount();
     const lottoList = LottoGenerator(inputMoney);
     printGeneratedList(lottoList);
-    const winningNumbers = [1, 2, 3, 4, 5, 6]; // 가정
     const winningNumbers = await inputWinningNumbers();
-    const bonusNumber = [7]; // 가정
+    const bonusNumber = await inputBonusNumber(winningNumbers);
     const resultCalculator = new ResultCalculator(winningNumbers, bonusNumber);
     const prizes = resultCalculator.calculatePrizes(lottoList);
     const statistics = StatisticCalculation(prizes, inputMoney);
--- src/utils/InputView.js
+++ src/utils/InputView.js
@@ -3,6 +3,7 @@ import VIEWMESSAGES from '../resources/VIEWMESSAGES.js';
 import validateInputMoney from './validator/validateInputMoney.js';
 import validateWinningNumbers from './validator/validateWinningNumbers.js';
 import RULES from '../resources/RULES.js';
+import validateBonusNumber from './validator/validateBonusNumber.js';
 
 export async function inputPurchaseAmount() {
   while (true) {
@@ -26,6 +27,20 @@ export async function inputWinningNumbers() {
       );
       validateWinningNumbers(winningNumbers);
       return winningNumbers.split(RULES.DELIMITER_WINNING_NUMBERS).map(Number);
+    } catch (error) {
+      Console.print(error);
+    }
+  }
+}
+
+export async function inputBonusNumber(winningNumbers) {
+  while (true) {
+    try {
+      const bonusNumber = await Console.readLineAsync(
+        VIEWMESSAGES.INPUT_BONUS_NUMBER_PROMPT,
+      );
+      validateBonusNumber(bonusNumber, winningNumbers);
+      return [Number(bonusNumber)];
     } catch (error) {
       Console.print(error);
     }
