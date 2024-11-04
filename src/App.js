7cf8033011e481c16d71cd03a6e35a5b0fbcf863
YSW2 <jrgyber@gmail.com>
2024-11-04 17:55:25 +0900

    feat<App>: 유저 입력 기능 구현
    - 구입할 금액 입력 기능
    - 입력한 값으로 로또 구매 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { InputValidator, Game } = require('./src/Game');
+
 class App {
-  async run() {}
+  async run() {
+    const game = new Game();
+    const amount = await this.#getPurchaseAmount();
+    const count = await game.purchaseLottos(amount);
+    game.printPurchaseResult(count);
+  }
+
+  async #getPurchaseAmount() {
+    const input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    return InputValidator.validatePurchaseAmount(input);
+  }
 }
 
-export default App;
+module.exports = App;
