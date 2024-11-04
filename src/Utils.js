c58406c39b7b65131a00cd3a46e4fb8861602ee5
Xen-alpha <hky7548@naver.com>
2024-11-04 21:11:33 +0900

    fix(printLottoArray): fix console output implementation

--- src/Utils.js
+++ src/Utils.js
@@ -2,11 +2,8 @@ import { ERROR_MESSAGE_FORMAT_OUTPUT } from "./ErrorMessage.js";
 import { Console } from "@woowacourse/mission-utils";
 
 export function printLottoArray(lottoArray) {
-  if (!Array.isArray(lottoArray))
-    throw new Error(ERROR_MESSAGE_FORMAT_OUTPUT.nonArray);
-  lottoArray.forEach((value) => {
-    Console.print(JSON.stringify(value.getNumbers()).replace(/,/g, ", "));
-  });
+  if (!Array.isArray(lottoArray)) throw new Error(ERROR_MESSAGE_FORMAT_OUTPUT.nonArray);
+  return lottoArray.reduce((previous, current) => `${previous}\n${JSON.stringify(current.getNumbers()).replace(/,/g, ", ")}`, "");
 }
 
 export function formatNumber(number) {
