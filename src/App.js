bf60e6a2f2cf878462503128b2d7944aff07be97
ahhyun <wndkgus0225@naver.com>
2024-11-04 20:15:16 +0900

    refactor(App) : 테스트 코드를 통한 첨삭

--- src/App.js
+++ src/App.js
@@ -6,46 +6,28 @@ import { evaluateResults, countMatches } from '../src/result.js';
 class App {
   cost;
 
-  constructor(){
+  constructor() {
     this.cost = 0;
   }
 
   async run() {
-    try{
-      /*
+    try {
       const count = await getCost();
       displayLottosCount(count);
-      displayLottosNumber(count);
-      
-      const pickedNumbers = await userPickedNumbers();
-      const pickedBounsNumber = await userPickedBonusNumber(pickedNumbers);
 
-      const winningNumbers = await getWinningNumbers();
-      const bonusNumber = await getBonusNumber();
-      evaluateResults(lottos, winningNumbers, bonusNumber);
-
-      */
-
-
-      const count = await getCost();
-      displayLottosCount(count);
-  
       const lottos = generateLottos(count);
-      displayLottosNumber(count);
-  
+      displayLottosNumber(lottos);
+
       const pickedNumbers = await userPickedNumbers();
       const pickedBonusNumber = await userPickedBonusNumber(pickedNumbers);
-  
+
       const rankCount = evaluateResults(lottos, pickedNumbers, pickedBonusNumber);
-      printResults(rankCount, count * 1000); // cost에 맞게 수익률 계산
-
-
-      
-    } catch(error){
-      Console.print(`[ERROR] 오류가 발생했습니다: ${error.message}`)
+      printResults(rankCount, count * 1000);
+    } catch (error) {
+      Console.print(`[ERROR] 오류가 발생했습니다: ${error.message}`);
     }
   }
 }
 
-
 export default App;
+
