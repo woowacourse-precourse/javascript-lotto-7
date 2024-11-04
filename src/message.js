b44bac81ab1dc6c3b2dfe746cab84250cfbffa21
ahhyun <wndkgus0225@naver.com>
2024-11-04 20:18:17 +0900

    refactor(message) : 테스트 코드를 통한 첨삭

--- src/message.js
+++ src/message.js
@@ -22,5 +22,5 @@ export const ERROR_MESSAGE = {
   NOT_SIX_NUMBERS: "[ERROR] 당첨 번호는 6개이어야 합니다.",
   DUPLICATE_NUMBER: "[ERROR] 당첨 번호는 중복될 수 없습니다,",
   INVALID_SEPARATOR: "[ERROR] 번호는 쉼표(,)로 구분해야 합니다.",
-  OUT_OF_RANGE: "[ERROR] 번호는 1부터 45까지만 입력 가능합니다."
+  OUT_OF_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
 }
