23fb98ff67de3461bb93a0edb65f3aae699404fb
YeRimmm-Cho <em514@dgu.ac.kr>
2024-11-04 17:19:08 +0900

    Feat(App): 당첨 번호 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App {
 
       MissionUtils.Console.print(`\n${lottoCount}개를 구매했습니다.`);
       this.getLottoNumbers(lottoCount);
+
+      const winningNumbers = await this.getWinningNumbers(); // 당첨 번호 입력 받기
+      MissionUtils.Console.print(`\n당첨 번호: ${winningNumbers.join(", ")}`);
     } catch (error) {
       MissionUtils.Console.print(error.message);
       await this.run(); // 금액 입력 재시도
@@ -32,12 +35,32 @@ class App {
 
     for (let i = 0; i < lottoCount; i++) {
       const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      numbers.sort((a, b) => a - b); // 오름차순 정렬
+      numbers.sort((a, b) => a - b);
       lottoNumbers.push(numbers);
       MissionUtils.Console.print(`[${numbers.join(", ")}]`);
     }
 
     return lottoNumbers;
+  }
+
+  async getWinningNumbers() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "\n당첨 번호를 입력해 주세요.\n"
+    );
+    const winningNumbers = input.split(",").map(Number); // 쉼표로 구분
+
+    // 유효성 검사: 1~45 범위, 중복 확인, 숫자 개수 확인
+    if (
+      winningNumbers.length !== 6 ||
+      winningNumbers.some((num) => isNaN(num) || num < 1 || num > 45) ||
+      new Set(winningNumbers).size !== 6
+    ) {
+      throw new Error(
+        "[ERROR] 당첨 번호는 1부터 45 사이의 중복되지 않는 숫자 6개여야 합니다."
+      );
+    }
+
+    return winningNumbers;
   }
 }
 
