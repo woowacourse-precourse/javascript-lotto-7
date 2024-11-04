56e3efb8c83a301d913b14d97c92ed0136a85a6f
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 23:02:52 +0900

    feat: 티켓별 당첨 여부를 판단하여 카운트하는 메서드 구현

--- src/service/LottoGame.js
+++ src/service/LottoGame.js
@@ -9,6 +9,29 @@ class LottoGame {
     const ticketCount = Math.floor(amount / 1000);
     return Array.from({ length: ticketCount }, () => new Lotto());
   }
+
+  calculateRanks(winningNumbers, bonusNumber) {
+    const ranks = {
+      "3개 일치": 0,
+      "4개 일치": 0,
+      "5개 일치": 0,
+      "5개 일치 + 보너스 볼 일치": 0,
+      "6개 일치": 0,
+    };
+
+    this.lottoTickets.forEach((ticket) => {
+      const matchCount = ticket.getMatchCount(winningNumbers);
+      const hasBonus = ticket.hasBonusNumber(bonusNumber);
+
+      if (matchCount === 3) ranks["3개 일치"]++;
+      if (matchCount === 4) ranks["4개 일치"]++;
+      if (matchCount === 5) ranks["5개 일치"]++;
+      if (matchCount === 5 && hasBonus) ranks["5개 일치 + 보너스 볼 일치"]++;
+      if (matchCount === 6) ranks["6개 일치"]++;
+    });
+
+    return ranks;
+  }
 }
 
 export default LottoGame;
