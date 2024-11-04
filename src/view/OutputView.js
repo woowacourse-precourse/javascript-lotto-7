cca5a04bb3ad8387a44f262daa4427044eab3b8d
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 22:29:00 +0900

    feat: 구매한 로또 개수와 목록을 출력하는 로직 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,7 +1,10 @@
 import { Console } from "@woowacourse/mission-utils"
 
 const OutputView = {
-
+  printLottoTickes(tickets) {
+    Console.print(`\n${tickets.length}개를 구매했습니다.`);
+    tickets.forEach((ticket) => Console.print(`[${ticket.numbers.join(", ")}]`));
+  }
 }
 
 export default OutputView;
