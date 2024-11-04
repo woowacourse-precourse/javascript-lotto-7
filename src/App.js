253a68bedc46391171396ed0501628542fe126df
seongmin <seongm22n@gmail.com>
2024-11-04 17:28:29 +0900

    refactor: 입력값 잘못 입력시 재입력으로 수정

--- src/App.js
+++ src/App.js
@@ -9,21 +9,52 @@ class App {
 		const validator = new LottoInputValidator();
 		const lottoHandler = new LottoHandler();
 
-		const inputAmount = await Console.readLineAsync(
-			'구입금액을 입력해 주세요.\n'
-		);
-		const lottoCount = validator.getLottoPrice(inputAmount);
+		// 구입금액 입력 및 검증
+		let lottoCount;
+		while (true) {
+			const inputAmount = await Console.readLineAsync(
+				'구입금액을 입력해 주세요.\n'
+			);
+
+			try {
+				lottoCount = validator.getLottoPrice(inputAmount); // 유효성 검사 호출
+				break; // 유효한 입력을 받으면 루프 종료
+			} catch (error) {
+				// 에러 메시지를 Console에 출력하고 루프를 계속하여 재입력 요청
+				Console.print(error.message);
+				// 다시 입력을 요청하므로 아무 동작도 하지 않고 while 루프가 반복됨
+			}
+		}
+
 		const lottos = lottoHandler.buyLotto(lottoCount);
 
-		const winNumber = await Console.readLineAsync(
-			`\n당첨 번호를 입력해 주세요.\n`
-		);
-		inputHandler.getWinningNumbers(winNumber);
+		// 당첨 번호 입력 및 검증
+		let winNumber;
+		while (true) {
+			winNumber = await Console.readLineAsync(`\n당첨 번호를 입력해 주세요.\n`);
 
-		const bonusNumber = await Console.readLineAsync(
-			`\n보너스 번호를 입력해 주세요.\n`
-		);
-		inputHandler.getBonusNumber(bonusNumber, winNumber);
+			try {
+				inputHandler.getWinningNumbers(winNumber);
+				break;
+			} catch (error) {
+				Console.print(error.message);
+			}
+		}
+
+		// 보너스 번호 입력 및 검증
+		let bonusNumber;
+		while (true) {
+			bonusNumber = await Console.readLineAsync(
+				`\n보너스 번호를 입력해 주세요.\n`
+			);
+
+			try {
+				inputHandler.getBonusNumber(bonusNumber, winNumber);
+				break;
+			} catch (error) {
+				Console.print(error.message);
+			}
+		}
 
 		Console.print('\n당첨 통계\n---');
 		const winMoney = lottoHandler.checkLottoWin(lottos, winNumber, bonusNumber);
