3ea5a4373f87235055bbfb84f4ca5414ed8f4bdb
syg0629 <syg0629@hanmail.net>
2024-11-04 22:47:41 +0900

    feat(lotto): 로또 오름차순 정렬, 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,26 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (!numbers.every((number) => number >= 1 && number <= 45)) {
+      throw new Error("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.");
+    }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return [...this.#numbers];
+  }
 }
 
 export default Lotto;
