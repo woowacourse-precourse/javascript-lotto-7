46a52b2fa1d24d4bcfefdb2cc091dd7de63261d1
정해성 <gotjd0230@naver.com>
2024-11-04 18:02:38 +0900

    test: 앱 예외 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -25,15 +25,29 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-const runException = async (input) => {
+const runException = async (invalidInput, inputType = 'money') => {
   // given
   const logSpy = getLogSpy();
+  const VALID_MONEY = '1000';
+  const VALID_NUMBERS = '1,2,3,4,5,6';
+  const VALID_BONUS = '7';
+  const RANDOM_NUMBERS = [1, 2, 3, 4, 5, 6];
 
-  const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-  const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,6', '7'];
+  let inputs;
+  switch (inputType) {
+    case 'money':
+      inputs = [invalidInput, VALID_MONEY, VALID_NUMBERS, VALID_BONUS];
+      break;
+    case 'numbers':
+      inputs = [VALID_MONEY, invalidInput, VALID_NUMBERS, VALID_BONUS];
+      break;
+    case 'bonus':
+      inputs = [VALID_MONEY, VALID_NUMBERS, invalidInput, VALID_BONUS];
+      break;
+  }
 
-  mockRandoms([RANDOM_NUMBERS_TO_END]);
-  mockQuestions([input, ...INPUT_NUMBERS_TO_END]);
+  mockRandoms([RANDOM_NUMBERS]);
+  mockQuestions(inputs);
 
   // when
   const app = new App();
@@ -41,6 +55,7 @@ const runException = async (input) => {
 
   // then
   expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('총 수익률은'));
 };
 
 describe('로또 테스트', () => {
@@ -111,6 +126,49 @@ describe('로또 테스트', () => {
 
     logs.forEach((log) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
+
+describe('예외 테스트', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe('구입 금액 예외', () => {
+    test.each([
+      ['1500', '1000원 단위가 아닌 경우'],
+      ['a', '숫자가 아닌 경우'],
+      ['0', '0원인 경우'],
+      ['1234567890123456', '범위를 벗어난 경우'],
+    ])('%s - %s', async (input) => {
+      await runException(input, 'money');
+    });
+  });
+
+  describe('당첨 번호 입력 예외', () => {
+    test.each([
+      ['1,2,3,4,5', '숫자가 부족한 경우'],
+      ['1,2,3,4,5,6,7', '숫자가 초과된 경우'],
+      ['1,2,3,4,5,5', '중복된 숫자가 있는 경우'],
+      ['1,2,3,4,5,46', '범위를 벗어난 경우'],
+      ['1,2,3,4,a,6', '숫자가 아닌 경우'],
+      ['1,2,3,4,5,0', '0이 포함된 경우'],
+      ['1,2,3,4,5,', '빈 값이 있는 경우'],
+    ])('%s - %s', async (input) => {
+      await runException(input, 'numbers');
+    });
+  });
+
+  describe('보너스 번호 입력 예외', () => {
+    test.each([
+      ['1', '당첨 번호와 중복되는 경우'],
+      ['46', '범위를 벗어난 경우'],
+      ['0', '0인 경우'],
+      ['abc', '숫자가 아닌 경우'],
+      ['', '빈 값인 경우'],
+    ])('%s - %s', async (input) => {
+      await runException(input, 'bonus');
     });
   });
 });
