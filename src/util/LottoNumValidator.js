9c74cc0f739673c7f699e7dcd915970d2516999d
deokbin <deokbin@naver.com>
2024-11-04 09:36:09 +0900

    feat(LottoNumValidator) : 로또 당첨 번호 원소 단위 유효성 검사 추가

---
+++ src/util/LottoNumValidator.js
@@ -0,0 +1,24 @@
+import { ErrorMessage } from '../constants/ErrorMessage.js';
+import { MagicNumber } from '../constants/MagicNumber.js';
+
+class LottoNumValidator {
+  #numbers;
+
+  constructor(numbers) {
+    this.validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  static validate(numbers) {
+    if (numbers.some(num => num === undefined || num === null || num === '')) {
+      throw new Error(ErrorMessage.INVALID_INPUT);
+    }
+    if (numbers.some(num => num < MagicNumber.LOTTO_START || num > MagicNumber.LOTTO_END)) {
+      throw new Error(ErrorMessage.OVER_RANGE);
+    }
+    if (numbers.some(num => isNaN(num))) {
+      throw new Error(ErrorMessage.NOT_A_NUMBER);
+    }
+  }
+}
+export default LottoNumValidator;
