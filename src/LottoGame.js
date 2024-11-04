8f7b26039b7918438697eb19a797eb1c422a18c6
aoooec <foottable@gmail.com>
2024-11-04 21:03:32 +0900

    feat: 로또 정보 출력 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,4 +1,4 @@
-import { Random } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 import { LOTTO_PRICE } from "./constants/gameRules.js";
 
@@ -23,6 +23,16 @@ class LottoGame {
     for (let i = 0; i < this.#lottoCount; i++) {
       this.#lottos.push(new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)));
     }
+  }
+
+  printLottos() {
+    Console.print(`\n${this.#lottoCount}개를 구매했습니다.`);
+    this.#lottos.forEach((lotto) => Console.print(lotto.getNumbersByOrder()));
+  }
+
+  start() {
+    this.setLottos();
+    this.printLottos();
   }
 }
 
