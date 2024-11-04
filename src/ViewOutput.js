13aec309cb9deede38eaa69cd0fa15cf0f2bb8bb
YeWon Seo <syw1236@naver.com>
2024-11-04 23:44:50 +0900

    refactor(ViewOutput): 함수명 오타 수정

--- src/ViewOutput.js
+++ src/ViewOutput.js
@@ -15,14 +15,14 @@ export default class ViweOutput {
 
     static printStatistics(statisticsCountMap) {
         winningLottoCountPrice.forEach((lotto) => {
-            const matchCount = this.getMathCountExpression(lotto);
+            const matchCount = this.getMatchCountExpression(lotto);
             this.printText(
                 `${matchCount} ${statisticsMesssage.OPENING_PARENTHESIS}${lotto.price}${statisticsMesssage.MONEY_UNIT}${statisticsMesssage.CLOSING_PARENTHESIS} ${statisticsMesssage.PRINT_BAR} ${statisticsCountMap.get(lotto.count)}${lottoMesaage.PRINT_COUNT}`
             );       
         });
     }
 
-    static getMathCountExpression(lotto) {
+    static getMatchCountExpression(lotto) {
         if(lotto.count === number.FIVE_BONUS)
             return `${number.FIVE}${statisticsMesssage.PRINT_MATCH}, ${statisticsMesssage.PRINT_BONUS}`;
         return `${lotto.count}${statisticsMesssage.PRINT_MATCH}`;
