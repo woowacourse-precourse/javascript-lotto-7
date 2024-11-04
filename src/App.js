b134c94e478beae1d12f2c93a8133d8e2a17d783
inji0212 <in17100170@kakao.com>
2024-11-04 21:02:04 +0900

    Feat 숫자인 경우만 입력가능하게

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  checkNumber(input) {
+    //숫자가 아닌 경우 |
+    if (isNaN(input) || parseInt(input) != input) {
+      throw new Error("[ERROR] 숫자를 입력하세요.");
+    }
+  }
   async run() {
     try {
       const moneyInput =
         await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
+      this.checkNumber(moneyInput);
     } catch (error) {
       MissionUtils.Console.print(error.message);
       // 에러 발생시 재입력받게 해줌
