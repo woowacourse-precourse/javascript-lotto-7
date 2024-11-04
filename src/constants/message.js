3d70471e7079b86628adcba82b1d65588296aeaf
nerdchanii <nerdchanii@gmail.com>
2024-11-04 09:48:33 +0900

    feat(constants): add messages

---
+++ src/constants/message.js
@@ -0,0 +1,10 @@
+const INPUT_MESSAGE = Object.freeze({
+  PURCAHSE_AMOUNT: '구입금액을 입력해주세요.',
+  WINNING_NUMBERS: '당첨번호를 입력해주세요',
+  BONUS_NUMBER: '보너스 번호를 입력해주세요',
+});
+
+const OUTPUT_MESSAGE = Object.freeze({
+  WINNING_STATISTICS: '당첨 통계',
+  DIVIDER: '---',
+});
