7f6b9ea0f6d843ce8b6520b82a56d7dd598ccbaa
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 18:58:06 +0900

    refactor: 접근 제어 추가 및 이름 수정

--- src/services/LottoMachine.js
+++ src/services/LottoMachine.js
@@ -1,5 +1,5 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
-import {sortAsc} from "../utils/methods.js";
+import {sortAsc} from "../utils/conversionUtils.js";
 import {purchaseValid} from "./utils/validation.js";
 
 class LottoMachine {
@@ -17,13 +17,13 @@ class LottoMachine {
     lottoRelease() {
         const lottos = []
         for (let i = 0; i < this.#number; i++) {
-            const sortedLotto = sortAsc(this.getSixRandomValues())
+            const sortedLotto = sortAsc(this.#getSixRandomValues())
             lottos.push(sortedLotto)
         }
         return lottos
     }
 
-    getSixRandomValues() {
+    #getSixRandomValues() {
         return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
     }
 
