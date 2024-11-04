f1700aeddf5e00e4bdf5c9b37559d96b6d051699
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-11-04 16:03:19 +0900

    feat: 에러 메세지 상수 추가
    - ERROR_MESSAGES : 빈 문자열, 숫자 변환 실패, 소수점 포함, 정수 아님, 양수 아님 등의 메시지 정의
    - PURCHASE_ERRORS : 1000으로 나누어지지 않는 금액에 대한 에러 메시지 정의
    - LOTTO_ERRORS: 로또 관련 에러 메시지 추가

---
+++ src/constants/constants.js
@@ -0,0 +1,20 @@
+export const ERROR_MESSAGES = {
+  IS_EMPTY: '[ERROR] 빈 문자열입니다.',
+  NOT_NUMBER: '[ERROR] 숫자로 변환되지 않습니다.',
+  INCLUDE_DECIMAL: '[ERROR] 소수점이 포함되어 있습니다.',
+  NOT_INTEGER: '[ERROR] 정수가 아닙니다.',
+  NOT_POSITIVE: '[ERROR] 양수가 아닙니다.',
+};
+
+export const PURCHASE_ERRORS = {
+  NOT_DIVIDED_1000: '[ERROR] 1000으로 나누어지지 않습니다.',
+};
+
+export const LOTTO_ERRORS = {
+  LOTTO_INVALID_NUMBER_RANGE:
+    '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  BONUS_NUMBER_DUPLICATE:
+    '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
+  NOT_SIX_NUMBER: '[ERROR] 로또 번호는 6개여야 합니다.',
+  DUPLICATED_LOTTO_NUMBER: '[ERROR] 로또 번호가 중복됩니다.',
+};
