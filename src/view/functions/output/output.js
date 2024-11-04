574972494e37540c5c979cc8031a0538d4d84da6
Dobbymin <gmin990624@gmail.com>
2024-11-04 20:19:35 +0900

    feat(view): 출력 관련 함수 모듈화 기능 구현

---
+++ src/view/functions/output/output.js
@@ -0,0 +1,7 @@
+import { printWinningStatistics, printTotalProfit } from './printLottoResult.js';
+
+import { printRandomLottoNumber } from './printRandomLottoNumber.js';
+
+import { printLottoCount } from './printLottoCount.js';
+
+export { printWinningStatistics, printLottoCount, printTotalProfit, printRandomLottoNumber };
