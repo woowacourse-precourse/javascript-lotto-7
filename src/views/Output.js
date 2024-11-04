4656ff4a3e398380a9e6c8d498f067966f936f2f
Eunicekk <cho_lyn@naver.com>
2024-11-04 20:48:54 +0900

    [feat] 로또 당첨 결과 출력

--- src/views/Output.js
+++ src/views/Output.js
@@ -1,8 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export class Output {
-	static async printLottoCount(lottoList) {
+	static printLottoCount(lottoList) {
 		Console.print(`${lottoList.length}개의 로또를 구매했습니다.`);
 		lottoList.forEach((lotto) => Console.print(`[${lotto.getNumbers().join(", ")}]`));
+	}
+
+	static printLottoResult(lottoResult) {
+		Console.print("당첨 통계");
+		Console.print("---");
+		Console.print(`3개 일치 (5,000원) - ${lottoResult.fifth}개`);
+		Console.print(`4개 일치 (50,000원) - ${lottoResult.fourth}개`);
+		Console.print(`5개 일치 (1,500,000원) - ${lottoResult.third}개`);
+		Console.print(`5개 일치, 보너스 번호 일치 (30,000,000원) - ${lottoResult.second}개`);
+		Console.print(`6개 일치 (2,000,000,000원) - ${lottoResult.first}개`);
 	}
 }
