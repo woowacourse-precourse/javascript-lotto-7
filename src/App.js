2ed4c326ce9d7f013b67432cc70b2f3ac28102f6
Jang Eunhye <hh961130@naver.com>
2024-11-04 18:51:53 +0900

    feat: 에러 발생 시 해당 지점부터 다시 입력

--- src/App.js
+++ src/App.js
@@ -6,18 +6,44 @@ import Lotto_bonus from "./Lotto_bonus.js";
 
 class App {
   async run() {
-    Console.print("구입금액을 입력해 주세요.");
-    let payment = await Console.readLineAsync("");
-    let myLottoArray = new Lotto_purchase(payment);
-    
-    Console.print("당첨 번호를 입력해 주세요.");
-    let inputLottoNumber = await Console.readLineAsync("");
-    let lottoNumberArray = inputLottoNumber.split(",");
-    let numbers = new Lotto(lottoNumberArray);
-    
-    Console.print("\n보너스 번호를 입력해 주세요.");
-    let inputbonusNumber = await Console.readLineAsync("");
-    let bonusNumber = new Lotto_bonus(inputbonusNumber).bonusNumber;
+    let myLottoArray;
+    let numbers;
+    let bonusNumber;
+
+    let isValidPayment = false;
+    let isValidLottoNumbers = false;
+    let isValidLottoBonusNumber = false;
+    while (!isValidPayment) {
+      try {
+        Console.print("구입금액을 입력해 주세요.");
+        let payment = await Console.readLineAsync("");
+        myLottoArray = new Lotto_purchase(payment);
+        isValidPayment = true;
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
+    while (!isValidLottoNumbers) {
+      try {
+        Console.print("당첨 번호를 입력해 주세요.");
+        let inputLottoNumber = await Console.readLineAsync("");
+        let lottoNumberArray = inputLottoNumber.split(",");
+        numbers = new Lotto(lottoNumberArray);
+        isValidLottoNumbers = true;
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
+    while (!isValidLottoBonusNumber) {
+      try {
+        Console.print("\n보너스 번호를 입력해 주세요.");
+        let inputbonusNumber = await Console.readLineAsync("");
+        bonusNumber = new Lotto_bonus(inputbonusNumber).bonusNumber;
+        isValidLottoBonusNumber = true;
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
     new Rank_check(myLottoArray, numbers, bonusNumber);
   }
 }
