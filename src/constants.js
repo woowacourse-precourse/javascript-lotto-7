e2110dff8fa92fb2c477fe4bc1fa114a14a8a4d2
gwansikk <gwansik.kim@gwansik.dev>
2024-11-04 15:37:41 +0900

    feat: ERROR_MESSAGE 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = {
+  LOTTO_CHECK_LENGTH: `로또 번호는 ${LOTTO_LENGTH}개여야 합니다.`,
+  LOTTO_CHECK_DUPLICATES: '로또 번호는 중복이 없어야 합니다.',
+  LOTTO_CHECK_NUMBER: '로또 번호는 숫자이어야 합니다.',
+  LOTTO_CHECK_NUMBER_RANGE: `로또 번호는 ${LOTTO_NUMBER_START}부터 ${LOTTO_NUMBER_END} 사이의 숫자여야 합니다.`,
+};
