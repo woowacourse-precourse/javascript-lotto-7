117b9d46bf5932f28b6a4b81cce915eb1814ab55
ahhyun <wndkgus0225@naver.com>
2024-11-04 20:17:49 +0900

    refactor(Lotto) : 테스트 코드를 통한 첨삭

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,17 +1,23 @@
-import * as MissionUtils from '@woowacourse/mission-utils';
+// Lotto.js
+import * as MissionUtils from "@woowacourse/mission-utils";
+import { validatePickedNumbers } from "../src/error.js";
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers.sort((a, b) => a - b);
+    this.#numbers = [...numbers].sort((a, b) => a - b);
   }
 
   #validate(numbers) {
+    if (!Array.isArray(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 배열이어야 합니다.");
+    }
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    validatePickedNumbers(numbers);
   }
 
   getNumbers() {
@@ -20,12 +26,12 @@ class Lotto {
 }
 
 function generateLottos(count) {
-  return Array.from({ length: count }, () => {
-    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-    return new Lotto(numbers);
-  });
+return Array.from({ length: count }, () => {
+  const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  return new Lotto(numbers);
+});
 }
 
 
-export { generateLottos, Lotto as default };
-
+export default Lotto;
+export { generateLottos };
