850972f47c8be5eaf75088799ebae480bafc8dc8
yheseul <kangyheseul@naver.com>
2024-11-04 10:40:01 +0900

    refactor: 당첨 번호 갯수 검증 로직 재사용 적용

--- src/validation/validateWinningNumbers/validateWinningNumbersPipe.js
+++ src/validation/validateWinningNumbers/validateWinningNumbersPipe.js
@@ -1,14 +1,19 @@
 import { isWinningNumbers } from "./isWinningNumbers.js";
+import { isNumbersCountInRange } from "../isNumbersCountInRange.js";
 import { isWinningNumbersInRange } from "../isWinningNumbersInRange.js";
+import { WINNING_NUMBER_COUNT } from "../../constants/lottoConstants.js";
 import { isWinningNumbersSeparator } from "./isWinningNumbersSeparator.js";
 import { isDuplicateWinningNumbers } from "../isDuplicateWinningNumbers.js";
-import { isWinningNumbersCountInRange } from "./isWinningNumbersCountInRange.js";
 import { WINNING_NUMBERS } from "../../constants/validationMessages/winningNumbers.js";
 
 export const validateWinningNumbersPipe = function (winningNumbers) {
   isWinningNumbersSeparator(winningNumbers);
+  isNumbersCountInRange(
+    winningNumbers,
+    WINNING_NUMBER_COUNT,
+    WINNING_NUMBERS.INVALID_COUNT
+  );
   isWinningNumbers(winningNumbers);
   isDuplicateWinningNumbers(winningNumbers, WINNING_NUMBERS.DUPLICATE_NUMBERS);
   isWinningNumbersInRange(winningNumbers, WINNING_NUMBERS.OUT_OF_RANGE);
-  isWinningNumbersCountInRange(winningNumbers);
 };
