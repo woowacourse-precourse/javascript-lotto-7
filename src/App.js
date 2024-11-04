976619b1650c71ea683515fbc95a2d660dc69252
Injun Jeong <jijun897@naver.com>
2024-11-04 13:20:19 +0900

    feat(app): 로또 구입 금액을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { BUY_ERROR, BUY_STRING, ENTER } from "./Constant.js";
 class App {
-  async run() {}
+  async run() {
+    const buyCount = await MissionUtils.Console.readLineAsync(
+      BUY_STRING + ENTER
+    );
+    if (isNaN(buyCount) || buyCount <= 0 || buyCount % 1000 != 0) {
+      throw new Error(BUY_ERROR + ENTER);
+    }
+  }
 }
 
 export default App;
---
+++ src/Constant.js
@@ -0,0 +1,6 @@
+export const BUY_STRING = "구입금액을 입력해 주세요. ";
+
+export const ENTER = "\n";
+
+export const BUY_ERROR =
+  "[ERROR] 구입할 금액은 1,000원 단위의 숫자여야 합니다. ";
