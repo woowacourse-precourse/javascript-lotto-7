c1e7a1315105eb092a7a81c3984479173839d102
jungwho <jungwho0328@gmail.com>
2024-11-04 13:17:20 +0900

    feat(TargetNumbers) - Add TargetNumbers class

---
+++ src/GameSetting/TargetNumbers.js
@@ -0,0 +1,40 @@
+import Validation from "./Utils/Validation";
+
+class TargetNumbers {
+  #numbers;
+
+  constructor(input) {
+    let numbers = this.#stringToList(input);
+    this.#numbers = this.#validate(numbers);
+  }
+
+  #validate(numbers) {
+    // 숫자 인지 검사
+    let validatedNumbers = numbers.map((number) =>
+      Validation.inputNaturalNumber(number)
+    );
+
+    // numbers 길이 검사
+    Validation.numbersLength(validatedNumbers);
+
+    // numbers 중복성 검사
+    Validation.numbersDuplicate(validatedNumbers);
+
+    // 1-45인지 검사
+    for (let number of validatedNumbers) {
+      Validation.numberRange(number);
+    }
+
+    return validatedNumbers;
+  }
+
+  #stringToList(input) {
+    return string.split(",").map((number) => number.trim());
+  }
+
+  getTargetNumbers() {
+    return this.#numbers;
+  }
+}
+
+export default TargetNumbers;
