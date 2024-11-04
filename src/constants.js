608153e118c017e3d93c7cf1e46ce75ded8af04f
mywhzy <cmj2393a@naver.com>
2024-11-04 16:54:30 +0900

    feat: 에러 메세지 및 정규식 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -11,6 +11,12 @@ export const OUTPUT_MESSAGE = {
 export const ERROR_MESSAGE = {
   prefix: '[ERROR]',
   purchaseAmountError: '로또 구입은 최소 1,000원부터 시작해 1,000원 단위로만 구매 가능합니다.',
+  purchaseFormatError: '로또 구매 금액은 숫자형으로만 입력해야 합니다.',
+  inputFormatError: '로또 당첨 번호는 쉼표(",")로 구분한 숫자들의 조합만 가능합니다. ex) 1,2,3,4,5,6',
+  lottoNumberTypeError: '로또 번호는 숫자형만 가능합니다.',
+  duplicatedNumbersError: '로또 번호는 중복 되지 않은 수만 존재해야 합니다.',
+  lottoNumbersCountError: '로또 번호는 6개여야 합니다.',
+  lottoNumberRangeError: '로또 번호의 범위는 1부터 45까지의 숫자만 가능합니다.',
 };
 
 export const LOTTO_SINGLE_TICKET_PRICE = 1000;
@@ -19,4 +25,12 @@ export const LOTTO_NUMBERS_CONDITION = {
   count: 6,
   minNumber: 1,
   maxNumber: 45,
+  type: 'number',
+};
+
+export const NUMBER_SEPARATOR = ',';
+
+export const REGEX = {
+  validNumberFormat: /^\d+$/,
+  lottoNumberRange: /^[1-9]$|^[1-3][0-9]$|^4[0-5]$/,
 };
