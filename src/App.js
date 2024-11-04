cf9ebd7052aa2c3cb340b601920e14c96dbd62b3
yena <rladpsk1117@gmail.com>
2024-11-04 11:14:50 +0900

    fix(App): await 키워드 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import LottoController from './controller/LottoController.js';
 class App {
   async run() {
     const lottoGame = new LottoController();
-    lottoGame.start();
+    await lottoGame.start();
   }
 }
 export default App;
