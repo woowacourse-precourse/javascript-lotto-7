b75c796f0e050e48413003d6f4655ba408a6cbef
keemsebeen <sebin0580@gmail.com>
2024-11-04 13:27:12 +0900

    feat(Calculate) : 순위 계산 메소드 구현

--- src/domain/Calculate.js
+++ src/domain/Calculate.js
@@ -7,6 +7,17 @@ class Calculate {
     this.winningStatus = new Array(5).fill(0);
     this.totalReward = 0;
   }
+  determineRank(lotto) {
+    const matchedWinningCount = lotto.countMatchingNumbers(this.lottoNumbers);
+    const hasBonusNumber = this.hasBonusNumber(this.bonusNumber);
+    if (matchedWinningCount === 6) return 1;
+    if (matchedWinningCount === 5 && hasBonusNumber) return 2;
+    if (matchedWinningCount === 5) return 3;
+    if (matchedWinningCount === 4) return 4;
+    if (matchedWinningCount === 3) return 5;
+
+    return -1;
+  }
 }
 
 export default Calculate;
