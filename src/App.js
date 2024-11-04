2738a36104adce2fedcd0be5993524160bad6cd5
o2jsg <oji5656@naver.com>
2024-11-04 11:57:31 +0900

    docs: 기능 구현 및 에러처리 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils/src";
+import Lotto from "./Lotto.js";
 class App {
-  async run() {}
+  async run() {
+    async function getLottoPurchaseAmount() {
+      const PURCHASE_AMOUNT = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+      return PURCHASE_AMOUNT;
+    }
+    async function getLottoWinningNumbers() {
+      const WINNING_NUMBERS = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해주세요')
+      return WINNING_NUMBERS;
+    }
+  }
 }
 
 export default App;
