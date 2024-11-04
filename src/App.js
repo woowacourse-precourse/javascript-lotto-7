edab4fd5a4201ac667918bb04e02fe7f28da3235
crolvlee <leecrolv@gmail.com>
2024-11-04 20:00:59 +0900

    feat: 당첨 통계 결과 출력 함수 구현

--- src/App.js
+++ src/App.js
@@ -68,6 +68,12 @@ function countPrizeResults(tickets, winningNumbers, bonusNumber) {
   return prizeResults;
 }
 
-function showStatistics() {
-
+function showStatistics(prizeResults) {
+  Console.print('\n당첨 통계');
+  Console.print('---');
+  Console.print(`3개 일치 (5,000원) - ${prizeResults['3']}개`);
+  Console.print(`4개 일치 (50,000원) - ${prizeResults['4']}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${prizeResults['5']}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${prizeResults['5+bonus']}개`);
+  Console.print(`6개 일치 (2,000,000,000원) - ${prizeResults['6']}개`);
 }
