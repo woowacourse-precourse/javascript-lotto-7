a3c1d4f0af5f49b4a74e57cc83034c2dd1a4ce28
yusuShin <tlsdbtn0507@naver.com>
2024-11-04 16:09:02 +0900

    feat(printHandlers.js) Add printResult - Print how many mathes user got - Use displayMatches variable to handle bonus condition - Because LotteryStatistics use specific number 7 to distinguish bonus situation - Use totalPrice variable to print price of each wins - Because too many zeroes are uncomfortable for developers maybe only me

--- src/handler/printHandlers.js
+++ src/handler/printHandlers.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { BONUS_ARRAY, PRINT_PERCENT, ZERO_AMOUNT_TO_COMPARE } from "../constants.js";
 
 const printError = (msg) => {
   Console.print("[ERROR]" + msg);
@@ -9,4 +10,21 @@ const printParam = (msg) => {
   Console.print(msg);
 };
 
-export { printError, printParam };
+const printResult = ({ matches, price, amount }, bonus = "") => {
+  const [BONUS_MATCHES, BONUS_NUM] = BONUS_ARRAY;
+
+  let displayMatches = matches;
+
+  if (matches === BONUS_NUM) displayMatches = BONUS_MATCHES;
+
+  const totalPrice = `${price.toLocaleString()},${ZERO_AMOUNT_TO_COMPARE}`;
+
+  Console.print(`${displayMatches}개 일치${bonus} (${totalPrice}원) - ${amount}개`);
+};
+
+
+export {
+  printError,
+  printParam,
+  printResult,
+};
