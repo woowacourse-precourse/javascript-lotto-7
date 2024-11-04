786e454294647a0480c68b184e1aa8862191ee7f
kimywann <Kimywan10@gmail.com>
2024-11-04 14:35:49 +0900

    feat(App): 구입 금액 유효성 검사 추가
    - #validatePurchaseAmount 메서드 구현:
      - 구입 금액이 최소 1,000원 이상인지 확인
      - 구입 금액이 1,000원 단위인지 확인
      - 조건에 맞지 않는 경우 에러 메시지 출력하여 사용자에게 안내

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {
-    const purchasePrice = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
-
-    if (purchasePrice % 1000 !== 0) {
-      throw new Error("[ERROR] 1,000원 단위로 나누어 떨어지지 않습니다.")
+  #validatePurchaseAmount(amount) {
+    if (amount < 1000) {
+      throw new Error("[ERROR] 구입 금액은 최소 1,000원 이상이어야 합니다.");
     }
+    if (amount % 1000 !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+    }
+  }
+
+  async run() {
+
+    const purchasePrice = Number(await Console.readLineAsync("구입금액을 입력해 주세요.\n"));
+    this.#validatePurchaseAmount(purchasePrice);
 
     const lottoCount = purchasePrice / 1000;
     Console.print(`\n${lottoCount}개를 구매했습니다.`)
@@ -21,20 +28,11 @@ class App {
     const sortedLottoArr = lottoArr.map(nums => nums.map(Number).sort((a, b) => a - b));
 
     sortedLottoArr.forEach(arr => {
-        Console.print(arr);
+      Console.print(`[${arr.join(', ')}]`); // 배열을 대괄호로 감싸서 출력
     });
 
     const winningNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
     const bonusNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
-
-    const matchCounts = lottoArr.map(lottoNumbers => {
-      // 당첨 번호와 비교하여 일치하는 번호 필터링
-      const matchedNumbers = lottoNumbers.filter(num => winningNumbers.includes(num));
-      return matchedNumbers.length; // 일치하는 번호의 개수 반환
-    });
-
-    Console.print(`당첨 통계\n---\n${matchCounts}개 일치 (${})`)
-    }
+  }
 }
-
 export default App;
