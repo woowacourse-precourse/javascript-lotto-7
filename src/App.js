cde4911b3a36925341c609fe01c4a65f0afd090a
JHLEE325 <ljntiger325@naver.com>
2024-11-04 14:40:25 +0900

    feat : 당첨 확인 및 당첨문구 출력 기능 구현,  당첨 확인에 필요한 상수 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { purchase } from "./Purchase.js";
 import { getInput } from "./Input.js";
 import { LOTTO } from "./constant.js";
+import { prize } from "./Prize.js";
 
 class App {
   async run() {
     const budget = await getInput('구입금액을 입력해 주세요.');
     const lotto = purchase(budget/LOTTO.PRICE);
     const winnumInput = await getInput('당첨 번호를 입력해 주세요.');
-    const winnum = winnumInput.split(',').map(num => num.trim());
-    const bonusnum = await getInput('보너스 번호를 입력해 주세요.');
+    const winnum = winnumInput.split(',').map(num => parseInt(num.trim(), 10));
+    
+    const bonusnumInput = await getInput('보너스 번호를 입력해 주세요.');
+    const bonusnum = parseInt(bonusnumInput.trim(), 10);
+
+    prize(lotto, winnum, bonusnum);
   }
 }
 
--- src/constant.js
+++ src/constant.js
@@ -4,4 +4,18 @@ export const LOTTO = {
     NUM: 6,
     BONUS: 1,
     PRICE: 1000,
+    MATCH_COUNT: {
+        THREE: 3,
+        FOUR: 4,
+        FIVE: 5,
+        FIVE_BONUS: '5+bonus',
+        SIX: 6,
+    },
+    PRIZES: {
+        THREE_MATCH: 5000,
+        FOUR_MATCH: 50000,
+        FIVE_MATCH: 1500000,
+        FIVE_BONUS_MATCH: 30000000,
+        SIX_MATCH: 2000000000,
+    }
 };
