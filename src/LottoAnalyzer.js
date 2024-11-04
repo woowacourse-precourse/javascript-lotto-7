a04b27d0fb2d51c9f4919bea6781ce232c4b84ae
YongHyunKing <yhkim8246@gmail.com>
2024-11-04 15:44:29 +0900

    fix(LottoAnalyzer): 일치하는 로또 개수를 정확히 구하도록 수정

--- src/LottoAnalyzer.js
+++ src/LottoAnalyzer.js
@@ -3,12 +3,11 @@ class LottoAnalyzer {
   #bonusNum;
   #buyLottos;
   #winningCount = Array(5).fill(0);
-  #matchBasicCount = 0;
-  #matchBonusCount = 0;
   #prizeMoney = [5000, 50000, 1500000, 30000000, 2000000000];
   #totalProfit = 0;
   #investMoney;
   #roi = 0;
+  #loseIndex = -1;
 
   constructor(winningLotto, buyLottos, bonusNum, invest) {
     this.#winningLottoSet = new Set(winningLotto);
@@ -33,11 +32,13 @@ class LottoAnalyzer {
 
   #countWinningLotto() {
     this.#buyLottos.forEach((lotto) => {
+      let matchBonusCount = 0;
+      let matchBasicCount = 0;
       lotto.getNumbers().forEach((number) => {
-        if (number === this.#bonusNum) this.#matchBonusCount += 1;
-        if (this.#winningLottoSet.has(number)) this.#matchBasicCount += 1;
+        if (number === this.#bonusNum) matchBonusCount += 1;
+        if (this.#winningLottoSet.has(number)) matchBasicCount += 1;
       });
-      const index = this.#getIndex();
+      const index = this.#getIndex(matchBonusCount, matchBasicCount);
       if (index >= 0) this.#winningCount[index] += 1;
     });
   }
@@ -53,13 +54,13 @@ class LottoAnalyzer {
     this.#roi = Number(rateOfReturn.toFixed(2));
   }
 
-  #getIndex() {
-    if (this.#matchBasicCount === 6) return 4;
-    if (this.#matchBasicCount === 5 && this.#matchBonusCount === 1) return 3;
-    if (this.#matchBasicCount === 5) return 2;
-    if (this.#matchBasicCount === 4) return 1;
-    if (this.#matchBasicCount === 3) return 0;
-    return -1;
+  #getIndex(matchBonusCount, matchBasicCount) {
+    if (matchBasicCount === 6) return 4;
+    if (matchBasicCount === 5 && matchBonusCount === 1) return 3;
+    if (matchBasicCount === 5) return 2;
+    if (matchBasicCount === 4) return 1;
+    if (matchBasicCount === 3) return 0;
+    return this.#loseIndex;
   }
 }
 
