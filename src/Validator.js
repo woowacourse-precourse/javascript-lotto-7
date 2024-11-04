a06a8e406202964a92a4b192770c6f4b89e30d4d
jeonhyeonseon <good_pretty@naver.com>
2024-11-04 19:02:32 +0900

    feat(validator): 로또 유효성 검사 메소드 추가
    - validateAmount: 구입 금액이 유효한지 검사
    - validateTicketSize: 로또 번호 개수가 정확한지 검사
    - validateNumberRange: 번호가 지정된 범위 내에 있는지 검사
    - validateUniqueNumbers: 번호가 중복되지 않았는지 검사

--- src/Validator.js
+++ src/Validator.js
@@ -8,6 +8,35 @@ class Validator {
       throw new Error(MESSAGES.ERROR_INVALID_AMOUNT);
     }
   }
+
+  // 번호 개수 
+  static validateTicketSize(numbers) {
+    if (numbers.length !== LOTTO_RULES.NUMBERS_PER_TICKET) {
+      throw new Error(MESSAGES.ERROR_INVALID_WINNING_NUMBERS);
+    }
+  }
+
+  // 번호 범위 
+  static validateNumberRange(numbers, isBonus = false) {
+    const isValidRange = numbers.every(
+      (num) => num >= LOTTO_RULES.NUMBER_RANGE.MIN && num <= LOTTO_RULES.NUMBER_RANGE.MAX
+    );
+    if (!isValidRange) {
+      if (isBonus) {
+        throw new Error(MESSAGES.ERROR_INVALID_BONUS_NUMBER_RANGE);
+      } else {
+        throw new Error(MESSAGES.ERROR_INVALID_WINNING_NUMBERS);
+      }
+    }
+  }
+
+  // 번호 중복 
+  static validateUniqueNumbers(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error(MESSAGES.ERROR_INVALID_WINNING_NUMBERS);
+    }
+  }
 }
 
 export default Validator;
