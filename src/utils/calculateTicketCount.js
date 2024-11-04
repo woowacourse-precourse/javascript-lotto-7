6523ed645b4d1624bf82c5445dc890ad1097de4a
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-04 15:14:40 +0900

    chore: 구입 금액 입력에 대한 예외 처리 추가

--- src/utils/calculateTicketCount.js
+++ src/utils/calculateTicketCount.js
@@ -1,12 +1,21 @@
-import { LOTTO_PRICE } from "../constants/lotto";
-import { ERROR_MESSAGE as ERROR } from "../constants/errorMessage";
+import { LOTTO_PRICE } from "../constants/lotto.js";
+import { ERROR_MESSAGE as ERROR } from "../constants/errorMessage.js";
 
 const calculateTicketCount = (purchaseAmount) => {
-  if (purchaseAmount % LOTTO_PRICE !== 0) {
-    throw Error(ERROR.INVALID_PURCHASE_AMOUNT);
-  }
+  checkError(purchaseAmount);
 
   return purchaseAmount / LOTTO_PRICE;
 };
+
+const checkError = (purchaseAmount) => {
+  if (isNaN(Number(purchaseAmount))) {
+    throw Error(ERROR.NOT_NUMBER);
+  }
+
+  if (purchaseAmount % LOTTO_PRICE !== 0) {
+    throw Error(ERROR.INVALID_PURCHASE_AMOUNT);
+  }
+}
+
 
 export default calculateTicketCount;
