58bf760b26c12247dcb080aec4ff45ca62a3beb6
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-11-04 12:04:17 +0900

    refactor: InputView와 OutputView를 클래스에서 객체로 변경
    - 불필요한 인스턴스 생성을 줄이고, 바로 호출 가능한 객체로 구조 변경
    - App의 인스턴스 생성 방식 변경에 맞추어 관련 의존성 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import validationLotto from './validations/validationLotto.js';
-import OutputView from './views/OutputView.js';
-import InputView from './views/InputView.js';
+import inputView from './views/inputView.js';
+import outputView from './views/outputView.js';
 import Controller from './controllers/Controller.js';
 import InputHandler from './handlers/InputHandler.js';
 
 class App {
   async run() {
-    const outputView = new OutputView();
-    const inputView = new InputView();
     const inputHandler = new InputHandler(
       inputView,
       outputView,
