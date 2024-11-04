1f1adc9d12fa6766cbbc2eea6cdd62198c145d31
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-04 22:51:53 +0900

    feat: 로또 등수별 당첨 횟수 반환 함수 추가
    lottoService.js 파일에 이하 내용 추가:
    - 로또 등수 별 당첨 횟수를 반환하는 calculateWinsCount 함수 추가

--- modules/lottoService.js
+++ modules/lottoService.js
@@ -1,5 +1,6 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import { lotto } from "../constants/lottoConstants.js";
+import { getMatchedCount } from "../utils/lottoUtils.js";
 
 export function generateLottos(count) {
   const lottos = [];
@@ -23,4 +24,46 @@ export function printLottos(lottos) {
   lottos.forEach((lotto) => {
     Console.print(lotto.getNumbers());
   });
+}
+
+export function calculateWinsCount(lottos, winningNumbers, bonusNumber) {
+  const resultCount = {
+    FIRST: 0,
+    SECOND: 0,
+    THIRD: 0,
+    FOURTH: 0,
+    FIFTH: 0,
+  };
+
+  lottos.forEach((lotto) => {
+    const matchedCount = getMatchedCount(lotto.getNumbers(), winningNumbers);
+    const hasBonus = lotto.getNumbers().includes(bonusNumber);
+
+    if (matchedCount === 6) {
+      resultCount.FIRST += 1;
+      return;
+    }
+
+    if (matchedCount === 5 && hasBonus) {
+      resultCount.SECOND += 1;
+      return;
+    }
+
+    if (matchedCount === 5) {
+      resultCount.THIRD += 1;
+      return;
+    }
+
+    if (matchedCount === 4) {
+      resultCount.FOURTH += 1;
+      return;
+    }
+
+    if (matchedCount === 3) {
+      resultCount.FIFTH += 1;
+      return;
+    }
+  });
+
+  return resultCount;
 }
