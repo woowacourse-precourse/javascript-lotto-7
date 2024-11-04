4c656e6665ea6a31ddddc2de065d38a8bd6946b3
CJY <cjy3458@naver.com>
2024-11-04 10:44:50 +0900

    feat: 전체 프로세스 작동 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import LottoController from './controllers/LottoController.js';
+import OutputView from './views/OutputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const purchaseAmount = await LottoController.handlePurchaseAmount();
+      const lottoTickets = LottoController.issueLottoTickets(purchaseAmount);
+
+      const { winningLotto, bonusNumber } =
+        await LottoController.getWinningAndBonusNumbers();
+
+      LottoController.displayResults(
+        lottoTickets,
+        winningLotto,
+        bonusNumber,
+        purchaseAmount,
+      );
+    } catch (error) {
+      OutputView.printError(error.message, error.name);
+      throw error;
+    }
+  }
 }
 
 export default App;
