876771b3a90c30b72bc826f99406fd132f98fb78
nayeong lee <lny021102@gmail.com>
2024-11-04 18:09:14 +0900

    refactor(Lotto): validation 함수를 15줄 내 메소드들로 분리

--- src/Lotto/Lotto.js
+++ src/Lotto/Lotto.js
@@ -3,6 +3,8 @@ class Lotto {
 
   constructor(numbers) {
     this.validateLottoNumbers(numbers);
+    this.validateLottoDuplicate(numbers);
+    this.validateLottoRange(numbers);
     this.#numbers = numbers;
   }
 
@@ -10,12 +12,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
 
+  validateLottoDuplicate(numbers) {
     const uniqueNumbers = new Set(numbers);
     if (uniqueNumbers.size !== numbers.length) {
       throw new Error("[ERROR] 중복되는 숫자를 입력하실 수 없습니다.");
     }
+  }
 
+  validateLottoRange(numbers) {
     if (numbers.some((num) => num < 1 || num > 45)) {
       throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
