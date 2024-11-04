3e61a80c49bf33ba6895a5e50fec2facaf603e15
ssingfly32 <ssingfly3232@gmail.com>
2024-11-04 23:52:18 +0900

    fix: 어플리케이션 테스트 통과하도록 수정

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import LottoGame from "./LottoGame.js";
 
 class App {
   async run() {
-    const purchaseAmount = await this.getPurchaseAmount();
-    const lottoGame = new LottoGame(purchaseAmount);
-    await lottoGame.start();
+    try {
+      const purchaseAmount = await this.getPurchaseAmount();
+      const lottoGame = new LottoGame(purchaseAmount);
+      await lottoGame.start();
+      
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+    }
   }
   async getPurchaseAmount() {
      const purchaseAmount = await MissionUtils.Console.readLineAsync('로또 구입 금액을 입력해주세요.\n');
