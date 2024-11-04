abb7bbb35eec4b528625f8560e7eb15e0f6393d9
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-11-04 17:12:48 +0900

    fix(input): 에러 발생 시 중단없이 사용자가 다시 입력할 수 있도록 개선

--- src/InputProcessor.js
+++ src/InputProcessor.js
@@ -9,22 +9,37 @@ class InputProcessor {
   }
 
   static async purchasePrice() {
-    const input = await InputProcessor.get(PROMPT.PRICE_INPUT);
-    Validator.price(input);
-    return input;
+    try {
+      const input = await InputProcessor.get(PROMPT.PRICE_INPUT);
+      Validator.price(input);
+      return input;
+    } catch (error) {
+      Console.print(error.message);
+      return InputProcessor.purchasePrice();
+    }
   }
 
   static async winningNumbers() {
-    const input = await InputProcessor.get(PROMPT.WINNING_NUMBER_INPUT);
-    const winningNumbers = input.split(',');
-    Validator.lottoNumbers(winningNumbers);
-    return winningNumbers;
+    try {
+      const input = await InputProcessor.get(PROMPT.WINNING_NUMBER_INPUT);
+      const winningNumbers = input.split(',');
+      Validator.lottoNumbers(winningNumbers);
+      return winningNumbers;
+    } catch (error) {
+      Console.print(error.message);
+      return InputProcessor.purchasePrice();
+    }
   }
 
   static async bonusNumber(winningNumbers) {
-    const input = await InputProcessor.get(PROMPT.BONUS_NUMBER_INPUT);
-    Validator.bonusNumber(winningNumbers, input);
-    return input;
+    try {
+      const input = await InputProcessor.get(PROMPT.BONUS_NUMBER_INPUT);
+      Validator.bonusNumber(winningNumbers, input);
+      return input;
+    } catch (error) {
+      Console.print(error.message);
+      return InputProcessor.purchasePrice();
+    }
   }
 }
 
