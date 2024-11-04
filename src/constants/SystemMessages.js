98af4cf2a8828b574035ce2fd0ab7a1b1443b9a3
H0ngJu <hongju4385@gmail.com>
2024-11-04 15:35:32 +0900

    refac: systemMessages 개행문자 추가

--- src/constants/SystemMessages.js
+++ src/constants/SystemMessages.js
@@ -1,19 +1,19 @@
 export const SYSTEM_MESSAGES = Object.freeze({
   INPUT_MONEY: "구입금액을 입력해 주세요.\n",
-  INPUT_NUMBERS: "당첨 번호를 입력해 주세요.\n",
-  INPUT_BONUS: "보너스 번호를 입력해 주세요.\n",
-  OUPUT_STATIC: "당첨 통계\n---\n",
+  INPUT_NUMBERS: "\n당첨 번호를 입력해 주세요.\n",
+  INPUT_BONUS: "\n보너스 번호를 입력해 주세요.\n",
+  OUPUT_STATIC: "\n당첨 통계\n---\n",
 
   print_purchase_lotto(number) {
-    return `${number}개를 구입했습니다.`;
+    return `\n${number}개를 구입했습니다.`;
   },
 
   print_match_execpt_bonus(number, price, count) {
-    return `${number}개 일치 (${price}) - ${count}개`;
+    return `${number}개 일치 (${price}) - ${count}개\n`;
   },
 
   print_match_with_bonus(number, price, count) {
-    retrun`${number}개 일치, 보너스 볼 일치 (${price}) - ${count}개`;
+    retrun`${number}개 일치, 보너스 볼 일치 (${price}) - ${count}개\n`;
   },
 
   print_rate_of_return(number) {
