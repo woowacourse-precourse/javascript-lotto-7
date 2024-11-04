b367a3f7bf26a7712ecbfe5c37021fc33f4d7666
Heesoo Jun <acesk123@gmail.com>
2024-11-04 22:45:19 +0900

    test: Lotto 클래스에 대한 단위 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,47 @@
 import Lotto from "../src/Lotto";
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+  // 번호 개수가 6개가 아니면 예외 발생
+  test("로또 번호의 개수가 6개가 아니면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+  // 중복된 번호가 있으면 예외 발생
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  // 번호가 1~45 범위를 벗어나면 예외 발생
+  test("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      new Lotto([46, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+  });
+
+  // getNumbers 메서드가 번호를 올바르게 반환하는지 테스트
+  test("getNumbers 메서드는 로또 번호를 올바르게 반환해야 한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  // getMatchCount 메서드 테스트
+  test("getMatchCount 메서드는 일치하는 번호의 개수를 올바르게 반환해야 한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 2, 3, 7, 8, 9];
+    expect(lotto.getMatchCount(winningNumbers)).toBe(3);
+  });
+
+  // hasBonusNumber 메서드 테스트
+  test("hasBonusNumber 메서드는 보너스 번호를 올바르게 확인해야 한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.hasBonusNumber(3)).toBe(true);
+    expect(lotto.hasBonusNumber(7)).toBe(false);
+  });
 });
