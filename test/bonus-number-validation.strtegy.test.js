de9bfb15c9ad49f03d6cbc549ebae15d2b744cc5
L2HYUNN <donghyuni98@gmail.com>
2024-11-04 22:16:36 +0900

    chore(test): 보너스 숫자에 대한 유효성 검증 테스트에 불필요한 설명문 제거

--- test/bonus-number-validation.strtegy.test.js
+++ test/bonus-number-validation.strtegy.test.js
@@ -22,44 +22,41 @@ function generateBonusNumberValidationStrategy(bonusNumber) {
 
 describe('BonusNumberValidationStrategy', () => {
   describe('validate', () => {
-    describe('validateBonusNumber', () => {
-      it.each([
-        {
-          description: '보너스 숫자로 빈 값이 입력된 경우 에러를 발생시켜야 한다',
-          input: '',
-          expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_CAN_NOT_BE_EMPTY,
-        },
-        {
-          description: '보너스 숫자로 음수가 입력된 경우 에러를 발생시켜야 한다',
-          input: '-1',
-          expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_MUST_BE_POSITIVE_INTEGER,
-        },
-        {
-          description: '보너스 숫자로 실수가 입력된 경우 에러를 발생시켜야 한다',
-          input: '0.125',
-          expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_MUST_BE_POSITIVE_INTEGER,
-        },
-        {
-          description: '보너스 숫자로 0이 입력된 경우 에러를 발생시켜야 한다',
-          input: '0',
-          expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_MUST_BE_POSITIVE_INTEGER,
-        },
-        {
-          description:
-            '보너스 숫자로 유효하지 않은 범위의 숫자가 입력된 경우 에러를 발생시켜야 한다',
-          input: '46',
-          expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_RANGE_IS_NOT_VALID,
-        },
-        {
-          description: '보너스 숫자가 당첨 번호와 중복된 경우 에러를 발생시켜야 한다',
-          input: '6',
-          expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_HAS_NO_DUPLICATED_NUMBER,
-        },
-      ])('$description', ({ input, expectedError }) => {
-        const bonusNumberValidationStrategy = generateBonusNumberValidationStrategy(input);
+    it.each([
+      {
+        description: '보너스 숫자로 빈 값이 입력된 경우 에러를 발생시켜야 한다',
+        input: '',
+        expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_CAN_NOT_BE_EMPTY,
+      },
+      {
+        description: '보너스 숫자로 음수가 입력된 경우 에러를 발생시켜야 한다',
+        input: '-1',
+        expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_MUST_BE_POSITIVE_INTEGER,
+      },
+      {
+        description: '보너스 숫자로 실수가 입력된 경우 에러를 발생시켜야 한다',
+        input: '0.125',
+        expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_MUST_BE_POSITIVE_INTEGER,
+      },
+      {
+        description: '보너스 숫자로 0이 입력된 경우 에러를 발생시켜야 한다',
+        input: '0',
+        expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_MUST_BE_POSITIVE_INTEGER,
+      },
+      {
+        description: '보너스 숫자로 유효하지 않은 범위의 숫자가 입력된 경우 에러를 발생시켜야 한다',
+        input: '46',
+        expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_RANGE_IS_NOT_VALID,
+      },
+      {
+        description: '보너스 숫자가 당첨 번호와 중복된 경우 에러를 발생시켜야 한다',
+        input: '6',
+        expectedError: BonusNumberValidationStrategy.ERROR_MESSAGE.BONUS_HAS_NO_DUPLICATED_NUMBER,
+      },
+    ])('$description', ({ input, expectedError }) => {
+      const bonusNumberValidationStrategy = generateBonusNumberValidationStrategy(input);
 
-        expect(() => bonusNumberValidationStrategy.validate()).toThrow(expectedError);
-      });
+      expect(() => bonusNumberValidationStrategy.validate()).toThrow(expectedError);
     });
   });
 });
