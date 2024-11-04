a4c1e36e43bd15f5debafc512645bcfa39dabe47
YeWon Seo <syw1236@naver.com>
2024-11-04 11:55:06 +0900

    feat(AppController): 사용자의 입력 처리 핸들러 함수 구현

--- src/AppController.js
+++ src/AppController.js
@@ -2,15 +2,40 @@
 import { Console } from '@woowacourse/mission-utils';
 import { lottoMesaage, numbers } from './constants.js';
 import ViweOutput from './ViewOutput.js';
+import Calculator from './Calculator.js';
+import LottoController from './LottoController.js';
 
 export default class AppController {
+    #userInputMap;
+
     async control() {
-        await this.userInput();
+        this.userInputInit();
+        const money = await this.userInput(lottoMesaage.INPUT_MONEY);
+        this.handleCheckUserInput(lottoMesaage.INPUT_MONEY, money)
     }
 
-    async userInput() {
-        const inputMoney = await Console.readLineAsync(lottoMesaage.INPUT_MONEY);
-        const lottoCount = inputMoney / numbers.LOTTO_UNITS;
-        
+    async userInput(message) {
+        return await Console.readLineAsync(message);
     }
+
+    userInputInit() {
+        this.#userInputMap = new Map();
+        this.#userInputMap.set(lottoMesaage.INPUT_MONEY, this.checkUserMoney.bind(this));
+    }
+
+    handleCheckUserInput(message, value) {
+        const checkUserInput = this.#userInputMap.get(message);
+
+        return checkUserInput(value);
+    }
+
+    checkUserMoney(inputMoney) {
+        const lottoCount = Calculator.divide(inputMoney,numbers.LOTTO_UNITS);
+        ViweOutput.printText(`${lottoCount}${lottoMesaage.PRINT_BUYCOUNT}`);
+
+        const userLottoList = LottoController.getUserLotto(lottoCount);
+        ViweOutput.printLottoList(userLottoList);
+    }
+
+
 }
