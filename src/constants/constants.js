e97968499dac23c4906662bd2e855666d4a30b66
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 18:10:24 +0900

    feat: 당첨 상금 상수 정의

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,11 @@ export const LOTTO = Object.freeze({
   COUNT_NUMBER: 6,
   PRICE: 1000,
 });
+
+export const WINNINGS = Object.freeze({
+  THREE: 5000,
+  FOUR: 50000,
+  FIVE: 1500000,
+  FIVE_BONUS: 30000000,
+  SIX: 2000000000,
+});
