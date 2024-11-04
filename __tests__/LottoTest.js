c8f983d767feb6cbf41166abcee4e964c0722fe1
조승우 <choseung97@gmail.com>
2024-11-04 17:56:04 +0900

    test(lotto): 로또 클래스 테스트 기능을 추가한다
    `getNumbers` 및 validator 기능을 테스트한다.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,35 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/components/Lotto/Lotto';
+import LOTTONUMBERS from '../src/resources/ERROR/LOTTONUMBERS';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('Lotto 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 아니면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(LOTTONUMBERS.NOT_TOTALLY_PICKED);
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow(LOTTONUMBERS.NOT_TOTALLY_PICKED);
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(LOTTONUMBERS.DUPLICATED_NUMBER);
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 6]);
+    }).toThrow(LOTTONUMBERS.OVER_NUMBER_RANGE);
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow(LOTTONUMBERS.OVER_NUMBER_RANGE);
+  });
+
+  test('올바른 로또 번호 배열을 생성할 수 있다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
