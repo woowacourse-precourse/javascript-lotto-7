5051779fad9e40b4dc4fdcc902ad7c6c29b4e3f3
PangKiYeon <pdrkeamy@gmail.com>
2024-11-04 19:47:24 +0900

    feat : lotto helper 클래스 선언

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
+import LottoHelper from './LottoHelper.js';
+import LottoCalculator from './LottoCalculator.js';
 
 class App {
     async run() {
         const MONEY = await Console.readLineAsync(
             '구입금액을 입력해 주세요.\n'
         );
-        const AMOUNT = calculateLottoAmount(MONEY);
-        Console.print(`\n${AMOUNT}개를 구매했습니다.`);
+        const LOTTO_HELPER = new LottoHelper(MONEY);
+
+        const LOTTO = LOTTO_HELPER.drawLottoNumber();
 
         const WINNING_INPUT = await Console.readLineAsync(
             '\n당첨 번호를 입력 주세요.\n'
