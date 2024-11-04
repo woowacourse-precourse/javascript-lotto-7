c45b9740d0b85d71b04ed49a1712653957e69eed
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-11-04 18:21:27 +0900

    feat: 로또 번호 중복 검사 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에는 중복이 없어야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
