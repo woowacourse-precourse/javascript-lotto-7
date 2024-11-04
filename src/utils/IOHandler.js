1ea02c37c7b5006dc737b6610417069eb91fedcb
YujeongLEE <dldbwjd24@gmail.com>
2024-11-04 21:30:09 +0900

    refactor : default 값 사용대신 값이 있는지 체크
    refactor : default 값 사용대신 값이 있는지 체크
    
    - default 값을 정의하지 않고, 값이 있을때만 수행하도록 변경

--- src/utils/IOHandler.js
+++ src/utils/IOHandler.js
@@ -3,9 +3,15 @@ import {INSTRUCTION} from "../constants/constants.js";
 import {lottoUtils} from "./lotto.utils.js";
 
 export const IOHandler = {
-    async getInput(instruction, validator = (x) => x, process = (x) => x) {
-        const input = await Console.readLineAsync(instruction)
-        return validator(process(input))
+    async getInput(instruction, validator, process) {
+        let input = await Console.readLineAsync(instruction)
+
+        if (process)
+            input = process(input);
+        if (validator)
+            input = validator(input);
+
+        return input
     },
     printLottoArray(lottos) {
         lottos.map(lotto => {
@@ -14,17 +20,17 @@ export const IOHandler = {
     },
     printWinningStatistics(matchNumber, prize, matchAmount) {
         if (matchNumber < 3) return
-        if (matchNumber === 6) {
+        if (matchNumber === 6) { //2등 ( 5개 + 보너스 번호 )
             Console.print(INSTRUCTION.PRINT_WINNING_STATISTICS(matchNumber - 1, lottoUtils.makeMoneyFormat(prize), matchAmount, INSTRUCTION.EXTRA_MESSAGE_SECOND_PRIZE))
             return
         }
-        if (matchNumber === 7) {
+        if (matchNumber === 7) { // 1등 ( 2등과 구분을 위해 맞은번호(6개) + 1개 = 7 )
             Console.print(INSTRUCTION.PRINT_WINNING_STATISTICS(matchNumber - 1, lottoUtils.makeMoneyFormat(prize), matchAmount))
             return
         }
         Console.print(INSTRUCTION.PRINT_WINNING_STATISTICS(matchNumber, lottoUtils.makeMoneyFormat(prize), matchAmount))
     },
-    printWinningStatisticsAll(lottoResult){
+    printWinningStatisticsAll(lottoResult) {
         Console.print(INSTRUCTION.PRINT_TOTAL_WINNING_STATISTICS);
         lottoResult.forEach((amount, index) => {
             this.printWinningStatistics(index, lottoUtils.getPrize(index), amount)
