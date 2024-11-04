9d14a9348b3009d25fa88b33ba96a5de6cf569ce
seongmin <seongm22n@gmail.com>
2024-11-04 15:12:28 +0900

    feat: 로또 번호 중복 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,13 @@ class Lotto {
 		if (numbers.length !== 6) {
 			throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
 		}
+
+		const numSet = new Set(numbers);
+		if (numSet.size !== numbers.length) {
+			throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+		}
+
+		this.numbers = numbers;
 	}
 
 	getNumbers() {
