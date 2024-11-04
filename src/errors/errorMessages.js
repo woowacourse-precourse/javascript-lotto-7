bec14a93b4bed83452212a3a10b7ea7b245944e3
hyeonjin6530 <cola1876@naver.com>
2024-11-04 22:25:54 +0900

    feat: 에러 처리

---
+++ src/errors/errorMessages.js
@@ -0,0 +1,9 @@
+const errorMessages = {
+    RANGE_OVER_ERROR: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+    NOT_NUMBER_ERROR: "[ERROR] 로또 번호는 숫자여야 합니다.",
+    INVALID_MONEY_ERROR: "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.",
+    SAME_NUMBER_ERROR: "[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다.",
+    AMOUNT_OVER_ERROR: "[ERROR] 로또 번호는 6개여야 합니다.",
+  };
+  
+  module.exports = errorMessages;
