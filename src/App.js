19264a9d4174050f8c508e6a4070e5891064ace6
hyeongdong <khd941018@gmail.com>
2024-11-04 20:02:49 +0900

    feat(App): 모든 의존성을 관리 및 주입하는 코드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import LottoService from "./LottoService.js";
+import LottoMatchChecker from "./LottoMatchChecker.js";
+import PurchaseManager from "./PurchaseManager.js";
+import LottoIssuer from "./LottoIssuer.js";
+import WinningStatsManager from "./WinningStatsManager.js";
+import { Console } from "@woowacourse/mission-utils";
+import { LOTTO_PRICE } from "./constants.js";
+import LottoView from "./LottoView.js";
+import LottoController from "./LottoController.js";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseManager = new PurchaseManager(LOTTO_PRICE);
+    const lottoMatchChecker = new LottoMatchChecker();
+    const lottoIssuer = new LottoIssuer();
+    const winningStatsManager = new WinningStatsManager();
+    const lottoView = new LottoView(Console);
+    const lottoService = new LottoService(purchaseManager, lottoMatchChecker, lottoIssuer, winningStatsManager);
+
+    const lottoController = new LottoController(lottoService, lottoView);
+    await lottoController.init();
+  }
 }
 
 export default App;
