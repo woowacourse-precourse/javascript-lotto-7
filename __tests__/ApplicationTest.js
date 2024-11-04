ad8a1866edd1670b9a3b180f88881b137dc1917c
Parksangwoo <psu8430@gmail.com>
2024-11-04 15:07:38 +0900

    refactor(test) : 테스트에 활용되는 유틸 함수 이동
    - 유틸 함수로 활용하기 위해 파일을 분리하였으나 분리한 파일도 테스트 파일로 인식하여 테스트에 포함되는 현상 발생
    - jest.config.js를 통해 설정을 바꾸려했으나, package.json 파일 내에 존재하는 jest 세팅 값과 충돌하여 함수를 ApplicationTest로 이동하여 export

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,15 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { mockQuestions } from './testUtils.js';
+
+export const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
