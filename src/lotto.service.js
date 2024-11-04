6909b1325cc764d55e12e98e1aa030c4cb50ed0b
nerdchanii <nerdchanii@gmail.com>
2024-11-04 18:30:29 +0900

    feat(service): implement service for Lotto

---
+++ src/lotto.service.js
@@ -0,0 +1,30 @@
+import Analyzer from './model/Analyzer.js';
+import BonusNumber from './model/BonusNumber.js';
+import PurcahseAmount from './model/PurchaseAmount.js';
+import LottoStore from './model/Store.js';
+import WinningNumbers from './model/WinningNumbers.js';
+
+class LottoService {
+  
+  static createPurchase(input){
+    return new PurcahseAmount(input);
+  }
+
+  static createWinnningNumber(input){
+    return new WinningNumbers(input);
+  }
+  
+  static createBonusNumber(input, winningNumbers){
+    return new BonusNumber(input, winningNumbers);
+  }
+
+  static buyTickets(purchase){
+    return LottoStore.sell(purchase);
+  }
+
+  static analyze(purchaseAmount,winningNumbers, lottos){
+    return Analyzer.analyze(purchaseAmount, winningNumbers, lottos);
+  }
+}
+
+export default LottoService;
