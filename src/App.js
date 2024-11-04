b789fb4e33c9eba8c466c7c3c4876cebdcfa7f90
gumbi <gusn9719@gmail.com>
2024-11-04 18:14:29 +0900

    feat: 로또 발행 결과 출력 기능 추가
    - 구매한 로또 개수 출력 로직 추가
      - PURCHASE_COMPLETE_MESSAGE 사용하여 로또 개수 출력
    - 각 로또 번호 출력 기능 추가
      - printLotto 함수를 통해 오름차순 정렬된 번호를 콘솔에 출력
      - printLottoList 함수에서 모든 로또 번호를 순회하며 개별 출력 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { printMessage, printLottoList } from './utils/console.js';
+import getPurchaseAmount from './utils/get-parchase-amount.js';
+import generateLottoList from './utils/generate-lotto.js';
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await getPurchaseAmount();
+
+    const lottos = generateLottoList(purchaseAmount);
+
+    printLottoList(lottos);
+  }
 }
 
 export default App;
--- src/utils/console.js
+++ src/utils/console.js
@@ -1,4 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
+import { CONFIRMATION_MESSAGES } from '../constants/constants.js';
+
+const { PURCHASE_COMPLETE_MESSAGE } = CONFIRMATION_MESSAGES;
 
 const printMessage = (message) => {
   Console.print(message);
@@ -8,4 +11,15 @@ const promptUserInput = async (message) => {
   return await Console.readLineAsync(message);
 };
 
-export { printMessage, promptUserInput };
+const printLotto = (lotto) => {
+  Console.print(`[${lotto.getNumbers().join(', ')}]`);
+};
+
+const printLottoList = (lottos) => {
+  printMessage(`\n${PURCHASE_COMPLETE_MESSAGE(lottos.length)}`);
+  lottos.forEach((lotto) => {
+    printLotto(lotto);
+  });
+};
+
+export { printMessage, promptUserInput, printLottoList };
