2e10b76659431d0e9ded508e1168b9baabf4e7b2
seongmin <seongm22n@gmail.com>
2024-11-04 14:53:14 +0900

    feat: 기능4 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ class App {
 			'구입금액을 입력해 주세요.\n'
 		);
 		inputHandler.getPurchaseAmount(inputAmount);
+
 		const winNumbers = await Console.readLineAsync(
 			`\n당첨 번호를 입력해 주세요.\n`
 		);
 		inputHandler.getWinningNumbers(winNumbers);
+
+		const bonusNumber = await Console.readLineAsync(
+			`\n보너스 번호를 입력해 주세요.\n`
+		);
+		inputHandler.getBonusNumber(bonusNumber,winNumbers);
 	}
 }
 
