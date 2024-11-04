26b32078b93317e4ed16ac18bf291e8e4de1138e
seongmin <seongm22n@gmail.com>
2024-11-04 16:27:55 +0900

    feat: 유효성 검사 추가

--- src/LottoInputHandler.js
+++ src/LottoInputHandler.js
@@ -1,28 +1,16 @@
 import { LottoInputValidator } from './utils/LottoInputValidator.js';
-import { LottoHandler } from './LottoHandler.js';
 
-const lottoInputValidator = new LottoInputValidator();
-const lottoHandler = new LottoHandler();
+const validator = new LottoInputValidator();
 
 export class LottoInputHandler {
-	getPurchaseAmount(inputAmount) {
-		const lottoCount = lottoInputValidator.getLottoPrice(inputAmount);
-		const lottos = lottoHandler.buyLotto(lottoCount);
-		return lottos;
-	}
-
 	// 당첨 번호
 	getWinningNumbers(winNumbers) {
-		const winNumber = lottoInputValidator.getLottoWinner(winNumbers);
+		const winNumber = validator.getLottoWinner(winNumbers);
 		return winNumber;
 	}
 	//보너스 번호
-	getBonusNumber(bonusNumber,winNumber) {
-		const bonusNum = lottoInputValidator.getLottoBonus(
-			bonusNumber,
-			winNumber
-		);
-		return bonusNum
+	getBonusNumber(bonusNumber, winNumber) {
+		const bonusNum = validator.getLottoBonus(bonusNumber, winNumber);
+		return bonusNum;
 	}
-	validateNumberRange(number) {}
 }
--- src/utils/LottoInputValidator.js
+++ src/utils/LottoInputValidator.js
@@ -1,8 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 export class LottoInputValidator {
 	getLottoPrice(inputAmount) {
+		if (!this.isValidInput(inputAmount)) {
+			// 유효성 검사 추가
+			throw new Error(`[ERROR] 숫자만 입력 가능합니다.`);
+		}
+
 		const price = this.changeToNumber(inputAmount);
 		if (price % 1000 > 0) {
-			throw new Error(`[Error] 로또 금액은 1000원으로 나누어 떨어져야 합니다.`);
+			Console.print(`[ERROR]`);
+			throw new Error(
+				`[ERROR]] 로또 금액은 1000원으로 나누어 떨어져야 합니다.`
+			);
 		}
 		return price / 1000;
 	}
@@ -10,25 +20,30 @@ export class LottoInputValidator {
 	changeToNumber(str) {
 		const num = parseInt(str, 10);
 		if (isNaN(num)) {
-			throw new Error(`[Error] 숫자만 입력 가능합니다.`);
+			throw new Error(`[ERROR] 숫자만 입력 가능합니다.`);
 		}
 		return num;
 	}
 
+	isValidInput(input) {
+		return /^\d+$/.test(input);
+	}
+
 	getLottoWinner(winNumbers) {
 		const numsStr = winNumbers.split(',');
-		if (numsStr.length < 6) {
-			throw new Error('[Error] 숫자 사이의 구분자는 , 여야 합니다.');
-		}
 
 		const numList = new Set();
 		for (const numStr of numsStr) {
 			const num = this.changeToNumber(numStr);
+			if (num < 1 || num > 45) {
+				// 추가된 유효성 검사
+				throw new Error('[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.');
+			}
 			numList.add(num);
 		}
 
 		if (numList.size !== 6) {
-			throw new Error('[Error] 로또 번호는 6개여야 합니다.');
+			throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
 		}
 		return numList;
 	}
@@ -38,10 +53,27 @@ export class LottoInputValidator {
 		console.log('winNumber:', winNumber);
 		console.log('bonusNum:', bonusNum);
 		if (bonusNum < 1 || bonusNum > 45) {
-			throw new Error('[Error] 보너스 번호는 1 ~ 45 사이의 숫자여야 합니다.');
+			throw new Error('[ERROR] 보너스 번호는 1 ~ 45 사이의 숫자여야 합니다.');
 		}
-		if (winNumber.includes(bonusNum)) {
-			throw new Error('[Error] 보너스 번호는 당첨번호와 중복되어선 안됩니다.');
+
+		// winNumber가 배열일 경우
+		if (Array.isArray(winNumber)) {
+			if (winNumber.includes(bonusNum)) {
+
+				throw new Error(
+					'[ERROR] 보너스 번호는 당첨번호와 중복되어선 안됩니다.'
+				);
+			}
+		}
+
+		// winNumber가 Set일 경우
+		if (winNumber instanceof Set) {
+			if (winNumber.has(bonusNum)) {
+
+				throw new Error(
+					'[ERROR] 보너스 번호는 당첨번호와 중복되어선 안됩니다.'
+				);
+			}
 		}
 		return bonusNum;
 	}
