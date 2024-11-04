d98bd9ed51c244e24d03e3920e232c6a787f55b6
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 21:59:17 +0900

    feat: InputView 내 사용자 입력 로직 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,11 @@
 import { Console } from "@woowacourse/mission-utils"
 
 const InputView = {
-
+  async getPurchaseAmount() {
+    Console.print("구입 금액을 입력해주세요.");
+    const input = await Console.readLineAsync();
+    const amount = parseInt(input);
+  }
 }
 
 export default InputView;
