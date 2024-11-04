b677d1b971d19070d1daf16d9760a638ce7391cc
geunhwa37 <jacika37@naver.com>
2024-11-04 14:38:05 +0900

    feat: 로또 구입 금액 입력 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Lotto from './Lotto.js';
+import MissionUtils from '@woowacourse/mission-utils';
+
 class App {
   async run() {}
+}
+
+// 1. 로또 구입 금액 입력 처리
+async readLottoPurchaseAmount(){
+  const amount = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  const purchaseAmount = parseInt(amount, 10);
+
+  // 로또 구입 금액 예외 처리
+  if (isNaN(purchaseAmount) || purchaseAmount <= 0) {
+    throw new Error("[ERROR] 구입 금액은 양수여야 합니다.");
+  }
+  if (purchaseAmount % 1000 !== 0) {
+    throw new Error("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+  }
+
+  return purchaseAmount;
 }
 
 export default App;
