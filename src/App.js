3a4378b9cb26842ff8da84cbe8484d62b8fec9fb
KimPuro <puro@protonmail.ch>
2024-11-04 18:54:10 +0900

    feat(App): 당첨 로또 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import {Console} from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-
+    
   }
 
   async moneyInput(){
     const money = await Console.readLineAsync('구입금액을 입력해 주세요.')
   }
+
+  async winnerLottoInput(){
+    const winnerLotto = await Console.readLineAsync('당첨 번호를 입력해 주세요.')
+    return winnerLotto
+  }
+
 }
 
 export default App;
