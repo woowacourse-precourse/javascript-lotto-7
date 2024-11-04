14083939eeb895963b4d3b0dcc1939d4f31138fc
inji0212 <in17100170@kakao.com>
2024-11-04 21:07:30 +0900

    Feat 산 로또 갯수 계산

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ class App {
       throw new Error("[ERROR] 숫자를 입력하세요.");
     }
   }
+
+  CalculateLottoCount(input) {
+    if (input % 1000 !== 0) {
+      throw new Error("[ERROR] 구입은 1,000원 단위로 입력하세요.");
+    }
+
+    return input / 1000;
+  }
   async run() {
     try {
       const moneyInput =
         await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
       this.checkNumber(moneyInput);
+      const lottoCount = this.CalculateLottoCount(moneyInput);
+      MissionUtils.Console.print(`${lottoCount}개를 구매했습니다.`);
     } catch (error) {
       MissionUtils.Console.print(error.message);
       // 에러 발생시 재입력받게 해줌
