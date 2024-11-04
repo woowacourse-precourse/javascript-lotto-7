5f2fe75b37d5c993ae3971b550049883bdde3e3a
Rigu1 <dlsrbtla@gmail.com>
2024-11-04 15:35:08 +0900

    style(LottoPurchaseTest): Add blank lines to enhance readability

--- __tests__/LottoPurchaseTest.js
+++ __tests__/LottoPurchaseTest.js
@@ -5,6 +5,7 @@ describe("LottoPurchaseTest 클래스 테스트", () => {
   test("기능 테스트", () => {
     const lottoPurchase = new LottoPurchase(5000);
     const lottoList = lottoPurchase.getPurchaseLottoList();
+    
     expect(lottoList).toHaveLength(5);
     lottoList.forEach(lottoNumbers => {
       expect(lottoNumbers).toHaveLength(Constants.LOTTO_NUMBER_COUNT);
