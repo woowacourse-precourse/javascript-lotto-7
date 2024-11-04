9289e0bfcfe0862da9ef28b76895dc2d47ffffea
0Jaemin0 <jaemin2167@naver.com>
2024-11-04 14:46:41 +0900

    fix: 입력 메시지 형식에 맞게 수정

--- src/constants/inputOutputMessages.js
+++ src/constants/inputOutputMessages.js
@@ -1,5 +1,5 @@
 export const INPUT_MESSAGE = Object.freeze({
-  PURCHASE_PRICE: '구입금액을 입력해 주세요\n',
+  PURCHASE_PRICE: '구입금액을 입력해 주세요.\n',
   WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
   BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
 });
