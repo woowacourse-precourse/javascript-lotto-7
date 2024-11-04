0535facc68a096c5f9465fab076314d6201e3657
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-04 23:23:14 +0900

    fix: printLottos 함수 출력값 수정
    lottoService.js 파일에서 이하 내용 수정:
    - printLottos 함수에서 구매 갯수를 출력하지 않던 것을 수정

--- modules/lottoService.js
+++ modules/lottoService.js
@@ -22,6 +22,8 @@ export function generateLottos(count) {
 }
 
 export function printLottos(lottos) {
+  Console.print(`\n${lottos.length}개를 구매했습니다.`);
+
   lottos.forEach((lotto) => {
     Console.print(lotto.getNumbers());
   });
