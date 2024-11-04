ba68b70ed2d7a5579d08d1012bdf16edc3373fbb
youdame <112458620+youdame@users.noreply.github.com>
2024-11-04 21:39:20 +0900

    test: App 통합 테스트 코드 추가
    - 정상적인 로또 구매 및 당첨 결과 출력 테스트 추가
    - 유효하지 않은 입력에 대한 예외 처리 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -42,82 +42,82 @@ const runException = async (input) => {
   expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
 };
 
-// describe('로또 테스트', () => {
-//   beforeEach(() => {
-//     jest.restoreAllMocks();
-//   });
+describe('로또 테스트', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
 
-//   test('기능 테스트', async () => {
-//     // given
-//     const logSpy = getLogSpy();
+  test('기능 테스트', async () => {
+    // given
+    const logSpy = getLogSpy();
 
-//     mockRandoms([
-//       [8, 21, 23, 41, 42, 43],
-//       [3, 5, 11, 16, 32, 38],
-//       [7, 11, 16, 35, 36, 44],
-//       [1, 8, 11, 31, 41, 42],
-//       [13, 14, 16, 38, 42, 45],
-//       [7, 11, 30, 40, 42, 43],
-//       [2, 13, 22, 32, 38, 45],
-//       [1, 3, 5, 14, 22, 45],
-//     ]);
-//     mockQuestions(['8000', '1,2,3,4,5,6', '7']);
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    mockQuestions(['8000', '1,2,3,4,5,6', '7']);
 
-//     // when
-//     const app = new App();
-//     await app.run();
+    // when
+    const app = new App();
+    await app.run();
 
-//     // then
-//     const logs = [
-//       '8개를 구매했습니다.',
-//       '[8, 21, 23, 41, 42, 43]',
-//       '[3, 5, 11, 16, 32, 38]',
-//       '[7, 11, 16, 35, 36, 44]',
-//       '[1, 8, 11, 31, 41, 42]',
-//       '[13, 14, 16, 38, 42, 45]',
-//       '[7, 11, 30, 40, 42, 43]',
-//       '[2, 13, 22, 32, 38, 45]',
-//       '[1, 3, 5, 14, 22, 45]',
-//       '3개 일치 (5,000원) - 1개',
-//       '4개 일치 (50,000원) - 0개',
-//       '5개 일치 (1,500,000원) - 0개',
-//       '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
-//       '6개 일치 (2,000,000,000원) - 0개',
-//       '총 수익률은 62.5%입니다.',
-//     ];
+    // then
+    const logs = [
+      '8개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 8, 11, 31, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '[2, 13, 22, 32, 38, 45]',
+      '[1, 3, 5, 14, 22, 45]',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 62.5%입니다.',
+    ];
 
-//     logs.forEach((log) => {
-//       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-//     });
-//   });
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 
-//   test('예외 테스트', async () => {
-//     await runException('1000j');
-//   });
-// });
-// test('유효하지 않은 금액 입력 예외 처리 테스트', async () => {
-//   await runException('1000j');
-// });
+  test('예외 테스트', async () => {
+    await runException('1000j');
+  });
+});
+test('유효하지 않은 금액 입력 예외 처리 테스트', async () => {
+  await runException('1000j');
+});
 
-// test('구매 금액이 1000원 단위가 아닐 때 예외 처리 테스트', async () => {
-//   await runException('1500');
-// });
+test('구매 금액이 1000원 단위가 아닐 때 예외 처리 테스트', async () => {
+  await runException('1500');
+});
 
-// test('보너스 번호가 당첨 번호와 중복될 때 예외 처리 테스트', async () => {
-//   const logSpy = getLogSpy();
+test('보너스 번호가 당첨 번호와 중복될 때 예외 처리 테스트', async () => {
+  const logSpy = getLogSpy();
 
-//   mockRandoms([[8, 21, 23, 41, 42, 43]]);
-//   mockQuestions(['1000', '1,2,3,4,5,6', '6']);
+  mockRandoms([[8, 21, 23, 41, 42, 43]]);
+  mockQuestions(['1000', '1,2,3,4,5,6', '6']);
 
-//   const app = new App();
-//   await app.run();
+  const app = new App();
+  await app.run();
 
-//   expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.'));
-// });
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.'));
+});
 
-// test('구매 금액이 부족할 때 예외 처리 테스트', async () => {
-//   await runException('500');
-// });
+test('구매 금액이 부족할 때 예외 처리 테스트', async () => {
+  await runException('500');
+});
 
 test('정상적인 입력으로 여러 번 당첨된 경우 테스트', async () => {
   const logSpy = getLogSpy();
@@ -152,7 +152,7 @@ test('정상적인 입력으로 여러 번 당첨된 경우 테스트', async ()
     '5개 일치 (1,500,000원) - 0개',
     '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
     '6개 일치 (2,000,000,000원) - 2개',
-    '총 수익률은 2500000.0%입니다.',
+    '총 수익률은 50000062.5%입니다.',
   ];
 
   expectedLogs.forEach((expectedLog) => {
