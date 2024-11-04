3d8d13c33490d4b4b085876df811b69d2082951d
jungwho <jungwho0328@gmail.com>
2024-11-04 13:41:31 +0900

    feat(LottoStandard) - Add LOTTO_STANDARD

---
+++ src/LottoGame/Utils/LottoStandard.js
@@ -0,0 +1,9 @@
+const LOTTO_STANDARD = [
+  { rank: 5, targetCount: 3, prize: 5000 },
+  { rank: 4, targetCount: 4, prize: 50000 },
+  { rank: 3, targetCount: 5, prize: 1500000 },
+  { rank: 2, targetCount: 5, hasBonus: true, prize: 30000000 },
+  { rank: 1, targetCount: 6, prize: 2000000000 },
+];
+
+export default LOTTO_STANDARD;
