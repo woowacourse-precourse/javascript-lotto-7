2a962f697ad4b2290bf6170ce4f5f7f6d638c8ef
Lim-shweety <smile70200@naver.com>
2024-11-04 17:43:53 +0900

    feat LottoTest.js : 로또 등수 확인하는 로직 테스트
    3개 미만 일 때, 2등일 때 확인

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,11 @@ describe("로또 클래스 테스트", () => {
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 등수 출력 확인", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    let result1 = lotto.checkWinNum([1, 2, 3, 4, 5, 7], 7);
+    let result2 = lotto.checkWinNum([1, 10, 11, 12, 13, 14], 8);
+    expect(result1).toEqual(3);
+    expect(result2).toEqual(-1);
+  })
 });
