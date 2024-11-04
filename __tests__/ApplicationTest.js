25808f37f31f3da0e92b2118ce68aca7e0d6a472
CJY <cjy3458@naver.com>
2024-11-04 10:13:44 +0900

    feat: 기능 단위 테스트 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,8 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import App from '../src/App.js';
 import Lotto from '../src/Lotto.js';
+import LottoResult from '../src/models/LottoResult.js';
+import WINNING_PRICE from '../src/constants/winningPrice.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -108,7 +110,7 @@ describe('로또 테스트', () => {
 
     mockQuestions(['3000', '8,21,23,41,42,45', '43']);
     const app = new App();
-    await app.play();
+    await app.run();
 
     const expectedLogs = [
       '3개를 구매했습니다.',
@@ -128,73 +130,43 @@ describe('로또 테스트', () => {
     });
   });
 
-  test('로또 구매 수량 확인 테스트', async () => {
-    const logSpy = getLogSpy();
-    mockRandoms([
-      [8, 21, 23, 41, 42, 43],
-      [3, 5, 11, 16, 32, 38],
-      [7, 11, 16, 35, 36, 44],
-    ]);
-
-    const PURCHASE_COUNT = 3;
-    const app = new App();
-    app.purchaseLottos(PURCHASE_COUNT);
-
-    const expectedLogs = [
-      '[8, 21, 23, 41, 42, 43]',
-      '[3, 5, 11, 16, 32, 38]',
-      '[7, 11, 16, 35, 36, 44]',
-    ];
-    expectedLogs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
-  });
-
   test('수익률이 올바르게 계산 된다.', async () => {
-    const logSpy = getLogSpy();
+    // 입력을 설정
+    const purchaseAmount = 8000;
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
+    const lottoTickets = [
+      [1, 2, 3, 4, 5, 8], // 5개 일치 (MATCH_5)
+      [1, 2, 3, 4, 5, 6], // 6개 일치 (MATCH_6)
+      [1, 2, 3, 4, 10, 11], // 4개 일치 (MATCH_4)
+      [1, 2, 3, 15, 16, 17], // 3개 일치 (MATCH_3)
+    ];
 
-    mockQuestions(['8000', '1,2,3,4,5,6', '7']);
-    const app = new App();
-    await app.run();
+    // 로또 결과 계산
+    const lottoResult = new LottoResult(
+      winningNumbers,
+      bonusNumber,
+      lottoTickets,
+    );
+    const expectedWinnings =
+      1 * WINNING_PRICE.MATCH_5 +
+      1 * WINNING_PRICE.MATCH_6 +
+      1 * WINNING_PRICE.MATCH_4 +
+      1 * WINNING_PRICE.MATCH_3;
 
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('총 수익률은'));
+    const expectedRateOfReturn = (
+      (expectedWinnings / purchaseAmount) *
+      100
+    ).toFixed(1);
+
+    // when
+    expect(lottoResult.calculateRateOfReturn(purchaseAmount)).toBe(
+      expectedRateOfReturn,
+    );
   });
 
   test('올바른 로또 번호로 생성할 수 있다', () => {
     const lotto = new Lotto([8, 21, 23, 41, 42, 43]);
-    expect(lotto.numbers).toEqual([8, 21, 23, 41, 42, 43]);
-  });
-
-  test('당첨번호, 로또번호, 구매금액이 주어졌을 때 결과 출력 확인', async () => {
-    const logSpy = getLogSpy();
-
-    const MY_LOTTO_NUMBERS = [
-      [8, 21, 23, 41, 42, 43],
-      [3, 5, 11, 16, 32, 38],
-      [7, 11, 16, 35, 36, 44],
-      [1, 8, 11, 31, 41, 42],
-      [13, 14, 16, 38, 42, 45],
-      [7, 11, 30, 40, 42, 43],
-      [2, 13, 22, 32, 38, 45],
-      [1, 3, 5, 14, 22, 45],
-    ];
-    const BONUS_NUMBER = 33;
-    const PURCHASE_PRICE = 8000;
-
-    const lotto = new Lotto([8, 21, 23, 24, 25, 26]);
-    lotto.getLottoResult(MY_LOTTO_NUMBERS, BONUS_NUMBER, PURCHASE_PRICE);
-
-    const expectedLogs = [
-      '3개 일치 (5,000원) - 1개',
-      '4개 일치 (50,000원) - 0개',
-      '5개 일치 (1,500,000원) - 0개',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
-      '6개 일치 (2,000,000,000원) - 0개',
-      '총 수익률은 62.5%입니다.',
-    ];
-
-    expectedLogs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+    expect(lotto.getNumbers()).toEqual([8, 21, 23, 41, 42, 43]);
   });
 });
