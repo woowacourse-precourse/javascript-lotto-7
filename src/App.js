997d3a83b4e6c3cb1d79f80658725457f2b56689
SudoSzune <robin1415@kookmin.ac.kr>
2024-11-04 21:58:20 +0900

    feat: 발행한 로또 수량 및 번호 오름차순으로 정렬 및 출력

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import InputHandler from "./utils/InputHandler.js"
 import InputValidator from "./utils/InputValidator.js";
+import Lotto from './Lotto.js';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   constructor() {
     this.inputHandler = new InputHandler();
     this.inputValidator = new InputValidator();
+    this.lottos = []; // Lotto 객체들을 저장할 배열.
   }
   async run() {
     try{
       const purchaseCost = await this.getPurchaseCost();
+      const purchasedLotto = await this.purchaseLotto(purchaseCost);
     } catch(error){
       console.error('무슨에러인고:',error);
     }
@@ -19,7 +23,17 @@ class App {
     return await this.inputValidator.validateCost(cost);
   }
 
-
+  async purchaseLotto(cost) {
+    const purchaseNumber = Number(cost)/1000; //로또 구매 장수
+    Console.print(`${purchaseNumber}개를 구매했습니다.`);
+    for(let i = 0; i<purchaseNumber; i++){
+      const arr = Random.pickUniqueNumbersInRange(1,45,6);
+      arr.sort((a, b) => a - b); // 오름차순으로 정렬
+      const lotto = new Lotto(arr);
+      this.lottos.push(lotto);
+      Console.print(arr);
+    }
+  }
 }
 
 export default App;
