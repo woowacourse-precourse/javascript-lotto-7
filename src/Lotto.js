808cfcd9385db27e55d8caed25e855fdc5ecdab2
goodbyebada <goodbyebada@naver.com>
2024-11-04 13:41:16 +0900

    feat(Lotto) : 로또 주요 기능 로직 추가
    - 수익률 계산
    - 각 로또 티켓의 당첨 번호와 일치하는 count 계산
    - 로또 등수 계산
    - 로또 발행 함수

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,83 @@
+import { makeError } from './View/Error.js';
+import { ERROR_MESSAGE } from './View/Error.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.checkDuplicate();
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  makeLottoTickets(ticketCount) {
+    const tickets = [];
+
+    for (let i = 0; i < ticketCount; i++) {
+      const ticket = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      tickets.push(ticket);
+    }
+
+    return tickets;
+  }
+
+  calculateMatchInfo(ticket, bonusNumber) {
+    const matchingNumbers = ticket.filter((elem) =>
+      this.#numbers.includes(elem)
+    );
+
+    if (matchingNumbers.length === 5) {
+      return {
+        hasBonusNumber: matchingNumbers.includes(bonusNumber),
+        mathCount: matchingNumbers.length,
+      };
+    }
+
+    return { hasBonusNumber: null, mathCount: matchingNumbers.length };
+  }
+
+  updateWinnerRankCount(matchingInfo, winnerRank) {
+    switch (matchingInfo.mathCount) {
+      case 6:
+        winnerRank.FIRST++;
+        break;
+      case 5:
+        if (matchingInfo.hasBonusNumber) {
+          winnerRank.SECOND++;
+          break;
+        }
+        winnerRank.THIRD++;
+      case 4:
+        winnerRank.FOURTH++;
+      case 3:
+        winnerRank.FIFTH++;
+      default:
+        break;
+    }
+
+
+    return winnerRank;
+  }
+
+  calculateProfitRate(purchaseMoney, winningMoney) {
+    return ((Number(winningMoney) / Number(purchaseMoney)) * 100).toFixed(1);
+  }
+
+  checkDuplicate() {
+    const set = new Set(this.#numbers);
+
+    if (this.#numbers.length !== set.size) {
+      makeError(ERROR_MESSAGE.WINNING_NUMBER_DUPLICATION);
+    }
+  }
 }
 
 export default Lotto;
