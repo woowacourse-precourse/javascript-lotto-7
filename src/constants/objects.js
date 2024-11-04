7b42ae813f58f06811b880f488fb0d73c016b5b7
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 19:00:04 +0900

    refactor: 접근의 편의성 위한 기존 객체에 출력 객체 추가 후 출력객체 삭제

--- src/constants/objects.js
+++ src/constants/objects.js
@@ -1,14 +1,7 @@
 export const MATCH_COUNTER = {
-    three: {cnt: 0, money: 5000},
-    four: {cnt: 0, money: 50000},
-    five: {cnt: 0, money: 1500000},
-    bonus: {cnt: 0, money: 30000000},
-    six: {cnt: 0, money: 2000000000},
+    THREE_MATCH: {cnt: 0, MONEY: 5000, OUTPUT_CONTENT: "3개 일치 (5,000원) - "},
+    FOUR_MATCH: {cnt: 0, MONEY: 50000, OUTPUT_CONTENT: "4개 일치 (50,000원) - "},
+    FIVE_MATCH: {cnt: 0, MONEY: 1500000, OUTPUT_CONTENT: "5개 일치 (1,500,000원) - "},
+    BONUS_MATCH: {cnt: 0, MONEY: 30000000, OUTPUT_CONTENT: "5개 일치, 보너스 볼 일치 (30,000,000원) - "},
+    SIX_MATCH: {cnt: 0, MONEY: 2000000000, OUTPUT_CONTENT: "6개 일치 (2,000,000,000원) - "},
 }
-
-export const OUTPUT_OBJ = [
-    {content: "3개 일치 (5,000원) - "},
-    {content: "4개 일치 (50,000원) - "},
-    {content: "5개 일치 (1,500,000원) - "},
-    {content: "5개 일치, 보너스 볼 일치 (30,000,000원) - "},
-    {content: "6개 일치 (2,000,000,000원) - "}]
