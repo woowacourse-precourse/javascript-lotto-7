e4df099674677702753fdaf99c9d9e4cd04f3d2b
YSW2 <jrgyber@gmail.com>
2024-11-04 18:09:58 +0900

    feat<ReasultCalculator>: 번호 일치 계산기 기능
    - 로또 번호와 당첨 번호 비교 기능 추가
    - 보너스 번호 일치 여부 기능 추가

---
+++ src/ResultCalculator.js
@@ -0,0 +1,30 @@
+class resultCalculator {
+  #numbers;
+  #bonusNumber;
+
+  constructor(numbers, bonusNumber) {
+    this.#numbers = numbers;
+    this.#bonusNumber = bonusNumber;
+  }
+
+  match(lotto) {
+    const lottoNumbers = lotto.getNumbers();
+    const matchCount = lottoNumbers.filter((num) =>
+      this.#numbers.includes(num)
+    ).length;
+    const matchBonus = lottoNumbers.includes(this.#bonusNumber);
+
+    return {
+      matchCount,
+      matchBonus,
+    };
+  }
+
+  getNumbers() {
+    return [...this.#numbers];
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+}
