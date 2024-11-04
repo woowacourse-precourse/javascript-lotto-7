dde741927e469b4b4c0d4a614ec2b3824b44fe3b
luminox93 <memoji1236@gmail.com>
2024-11-04 21:27:52 +0900

    feat: App 클래스 구현 및 주요 로또 기능 흐름 정의
    - ServiceFactory를 통해 필요한 서비스 인스턴스 생성 (구매, 랜덤 번호, 로또, 출력, 수익률 계산)
    - run 메서드: 로또 구매, 랜덤 번호 생성, 로또 및 보너스 번호 설정, 통계 및 수익률 출력 등의 전체 흐름 정의
    - handlePurchaseAmount: 사용자 입력을 통해 구매 금액 설정
    - generateRandomNumbers: 구매 수량에 따라 랜덤 번호 생성
    - createLottoAndBonus: 로또 번호와 보너스 번호 생성
    - calculateWinningMoney: 당첨 통계 계산 및 상금 배열 생성
    - calculateReturn: 총 상금과 구매 금액을 바탕으로 수익률 계산
    - print 메서드들: 각 단계에서 필요한 출력 로직 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,72 @@
+// App.js
+import ServiceFactory from './factories/ServiceFactory.js';
+import magicNumber from './constants/magic-number.js';
+
 class App {
-  async run() {}
+  constructor() {
+    this.purchaseService = ServiceFactory.createPurchaseService();
+    this.randomNumberService = ServiceFactory.createRandomNumberService();
+    this.lottoService = ServiceFactory.createLottoService();
+    this.printService = ServiceFactory.createPrintService();
+    this.returnCalculator = ServiceFactory.createReturnCalculator();
+  }
+
+  async run() {
+    try {
+      const { purchaseAmount, purchaseCnt } = await this.handlePurchaseAmount();
+      const randomNumObj = this.generateRandomNumbers(purchaseCnt);
+      const { lotto, bonusNumber } = await this.createLottoAndBonus();
+      this.printWinningStatistics();
+      const moneyArr = this.calculateWinningMoney(randomNumObj, bonusNumber);
+      this.printWinningMoney(lotto, moneyArr);
+      const resultNum = this.calculateReturn(purchaseAmount, moneyArr);
+      this.printReturn(resultNum);
+    } catch (error) {
+      this.printService.printError(error.message);
+    }
+  }
+
+  async handlePurchaseAmount() {
+    return await this.purchaseService.makePurchaseAmount();
+  }
+
+  generateRandomNumbers(purchaseCnt) {
+    return this.randomNumberService.generateRandomNumbers(purchaseCnt);
+  }
+
+  async createLottoAndBonus() {
+    const { lotto, lottoNumber } = await this.lottoService.createLotto();
+    const bonusNumber = await this.lottoService.createBonus(lottoNumber);
+    return { lotto, bonusNumber };
+  }
+
+  printWinningStatistics() {
+    this.printService.printWinningStatistics();
+  }
+
+  calculateWinningMoney(randomNumObj, bonusNumber) {
+    const [winningCnt, bonusCnt] = this.lottoService.checkWinning(
+      randomNumObj,
+      bonusNumber,
+    );
+    return this.lottoService.createMoneyArr(winningCnt, bonusCnt);
+  }
+
+  printWinningMoney(lotto, moneyArr) {
+    this.printService.printWinningMoney(lotto, moneyArr);
+  }
+
+  calculateReturn(purchaseAmount, moneyArr) {
+    const sumMoney = this.returnCalculator.calculateSumMoney(
+      moneyArr,
+      magicNumber.WINNING_AMOUNT,
+    );
+    return this.returnCalculator.calculate(sumMoney, purchaseAmount);
+  }
+
+  printReturn(resultNum) {
+    this.printService.printReturn(resultNum);
+  }
 }
 
 export default App;
