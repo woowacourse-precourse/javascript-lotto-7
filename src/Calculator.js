6d3ff1ba645d40e58c6b7b7978d90877b1949348
YeWon Seo <syw1236@naver.com>
2024-11-04 19:49:53 +0900

    feat(Calculator): 수익률 계산 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,28 @@
+import { winningLottoCountPrice, numbers } from "./constants.js";
 export default class Calculator {
-    
+
     static divide(num1, num2) {
         return num1/num2;
     }
+
+    static sum(statisticsCountMap) {
+        const total = Array.from(statisticsCountMap).reduce((total, [key, value]) => {
+            const prizeInfo = winningLottoCountPrice.find(item => item.count === key);
+            if (prizeInfo) {
+                total += parseInt(prizeInfo.price.replace(/,/g, "")) * value;
+            }
+            // if (value > numbers.DEFAULT_COUNT) {
+            //     total += parseInt(winningLottoCountPrice.get(key).replace(/,/g, "") * value);
+            // }
+            return total;
+        }, 0);
+
+        return total;
+    }
+
+    static earningsRate(userMoney, earnMoney) {
+        return ((earnMoney/userMoney) * numbers.HUNDRED).toFixed(numbers.ROUND_NUMBER);
+    }
+
+
 }
