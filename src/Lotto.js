7cf3f1eb8961958e5dc642dda4f04bea68c9d3fa
inaemin <inaemin@gmail.com>
2024-11-04 21:46:35 +0900

    update: Lotto 클래스에 추가 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Validator } from "./Validator";
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    Validator.isValidWinningNumbers(numbers);
   }
 
   // TODO: 추가 기능 구현
+  getLottoNumbers() {
+    return this.#numbers;
+  }
+
+  matchLottoNumbers(winningNumbers, bonus) {
+    let matchCount = winningNumbers.filter((num) =>
+      this.#numbers.includes(num)
+    ).length;
+    let hasBonus = this.#numbers.includes(bonus);
+
+    return { matchCount, hasBonus };
+  }
 }
 
 export default Lotto;
