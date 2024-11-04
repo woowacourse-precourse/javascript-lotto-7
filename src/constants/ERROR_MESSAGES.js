9cea74f790bfa9bd9f543662538bca277a4df39c
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-04 15:22:08 +0900

    feat: 공백 검증 에러 메시지 추가

--- src/constants/ERROR_MESSAGES.js
+++ src/constants/ERROR_MESSAGES.js
@@ -1,5 +1,7 @@
 export const ERROR_MESSAGE = Object.freeze({
   NOT_A_NATURAL_NUMBER: '[ERROR] 해당 값은 자연수여야 합니다.',
   WINNING_AMOUNT_MUST_BE_MULTIPLE_OF_1000:
-    '[ERROR] 유효하지 않은 금액입니다. 당첨 금액은 1,000원 단위로 입력해 주세요.'
+    '[ERROR] 유효하지 않은 금액입니다. 당첨 금액은 1,000원 단위로 입력해 주세요.',
+  WHITESPACE_NOT_ALLOWED:
+    '[ERROR] 공백은 입력할 수 없습니다. 공백을 제거하고 다시 입력해 주세요.'
 });
