1725f2210267c5a5e4cc7737a65199995474fe9d
Daeuning <happyjde3@gmail.com>
2024-11-04 18:54:05 +0900

    refactor(utils/game): 주석 수정

--- src/utils/game.js
+++ src/utils/game.js
@@ -3,8 +3,8 @@ import { GAME_RULES } from '../constants/gameRule.js';
 
 /**
  * 지정된 범위 내에서 고유한 숫자를 생성하는 함수
- * @param {number} min - 생성할 숫자의 최소값
- * @param {number} max - 생성할 숫자의 최대값
+ * @param {number} min - 생성할 숫자의 최소값(포함)
+ * @param {number} max - 생성할 숫자의 최대값(포함)
  * @param {number} count - 생성할 숫자의 개수
  * @returns {number[]} - 고유한 랜덤 숫자 배열
  */
