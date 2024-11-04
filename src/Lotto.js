6fed90891d6e95abfe082b8f8dc9d08c034ff80d
hwiy01 <vdocom02@naver.com>
2024-11-04 20:07:47 +0900

    feat: 로또 번호 중복 시 에러 발생

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,9 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    else if (new Set(numbers).size != numbers.length){
+      throw new Error("[ERROR] 로또 번호는 서로 중복되선 안됩니다.");
+    } 
   }
 
   getNumbers() {
