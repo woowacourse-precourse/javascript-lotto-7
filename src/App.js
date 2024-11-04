ffababf53efb1a360c690605b4cebf4bd4ce75f5
chan8632 <duscks8632@gmail.com>
2024-11-04 16:07:23 +0900

    feat(App):로또 프로그램 초기 로직 구현 - 로또 투자 금액, 당첨번호 및 보너스 번호 입력 기능 추가 - 입력받은 금액에 따라 로또 번호 출력 횟수 설정 - 랜덤 로또 번호 생성 후 오름차순 정렬 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    MissionUtils.Console.print("로또에 얼마 쓸건가요?");
+    this.inputMoney = await MissionUtils.Console.readLineAsync("");
+    this.로또번호몇번출력할건지();
+    MissionUtils.Console.print("당첨번호를 입력해주세요");
+    const 당첨번호 = await MissionUtils.Console.readLineAsync("");
+    MissionUtils.Console.print("보너스 번호를 입력해주세요");
+    const 보너스번호 = await MissionUtils.Console.readLineAsync("");
+  }
+  lottoNumberprint() {
+    const randomNumberList = MissionUtils.Random.pickUniqueNumbersInRange(
+      1,
+      45,
+      6
+    );
+    randomNumberList.sort((a, b) => a - b);
+    MissionUtils.Console.print(randomNumberList);
+  }
+  로또번호몇번출력할건지() {
+    const 로또번호출력횟수 = parseInt(this.inputMoney) / 1000;
+    for (let i = 0; i < 로또번호출력횟수; i++) {
+      this.lottoNumberprint();
+    }
+  }
 }
 
 export default App;
