f1db56891e7982757dfb55f72fb70b92defc6399
Eunhyang Cho <eunhyangcho@Eunhyangui-MacBookAir.local>
2024-11-04 17:29:44 +0900

    ♻️  refector: 당첨번호 개수 상수화

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -18,4 +18,19 @@ describe('로또 클래스 테스트', () => {
       new Lotto([1, 2, 3, 4, 50, 5]);
     }).toThrow('[ERROR]');
   });
+
+  test('올바른 로또 번호 배열이 주어지면 객체가 정상적으로 생성되어야 한다', () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(numbers);
+    expect(lotto.getNumbers()).toEqual(numbers);
+  });
+
+  test('getNumbers 메서드는 로또 번호 배열의 복사본을 반환해야 한다', () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(numbers);
+    const retrievedNumbers = lotto.getNumbers();
+
+    expect(retrievedNumbers).toEqual(numbers);
+    expect(retrievedNumbers).not.toBe(numbers);
+  });
 });
