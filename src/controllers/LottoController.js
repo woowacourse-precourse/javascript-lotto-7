11ee3456cfb425b9b3454fb759d455e6e1480086
jinoo <jinoo0306@naver.com>
2024-11-04 16:24:31 +0900

    feat(2) : 로또 번호를 사용자에게 출력하는 기능 구현

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -1,8 +1,13 @@
 import InputView from "../views/inputView.js";
 import OutputView from "../views/outputView.js";
 import LottoValidator from "../utils/LottoValidator.js";
+import LottoService from "../services/LottoService.js";
 
 class LottoController {
+  constructor() {
+    this.lottoService = new LottoService();
+  }
+
   async start() {
     try {
       const purchaseAmount = await InputView.readPurchaseAmount();
@@ -11,9 +16,18 @@ class LottoController {
       if (LottoValidator.validatePurchaseAmount(validAmount)) {
         const lottoCount = validAmount / 1000;
         OutputView.printLottoCount(lottoCount);
+        this.generateAndPrintLottos(lottoCount);
       }
     } catch (error) {
       OutputView.printErrorMessage(error.message);
+    }
+  }
+
+  generateAndPrintLottos(count) {
+    for (let i = 0; i < count; i++) {
+      const lotto = this.lottoService.generateLotto();
+      // console.log(lotto.getNumbers());
+      OutputView.printLottoNumbers(lotto.getNumbers());
     }
   }
 }
