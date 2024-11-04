d993ae37a057d48cda2adcab2a2d6c5ba0299442
BoRamSon <onbrson1@gmail.com>
2024-11-04 22:00:37 +0900

    feat(OutputView): 당첨통계와 수익률을 출력할 수 있는 메세지

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -9,4 +9,20 @@ export class OutputView {
   OutputLottoNumberArray(lottoNumberArray) {
     lottoNumberArray.map(Array => Console.print(Array))
   }
+
+  OutputPrizeStatisticsMessage() {
+    Console.print(OUTPUT_MESSAGES.PRIZE_STATISTICS);
+  }
+
+  OutputPrizeStatistics(prizeStatisticsArray) {
+    Console.print(`3개 일치 (5,000원) - ${prizeStatisticsArray[0].matchedThree}개`);
+    Console.print(`4개 일치 (50,000원) - ${prizeStatisticsArray[1].matchedFour}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${prizeStatisticsArray[2].matchedFive}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${prizeStatisticsArray[3].matchedFiveAndBonus}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${prizeStatisticsArray[4].matchedSix}개`);
+  }
+
+  OutputPrizeProfitRate(profitRate) {
+    Console.print(`총 수익률은 ${profitRate}%입니다.`);
+  }
 }
