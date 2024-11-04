046ce8b8bff95ce27ea1d8b4b5e1d7c59de5f484
Eunhyang Cho <eunhyangcho@syn-172-100-000-091.res.spectrum.com>
2024-11-04 15:41:24 +0900

    ♻️  refector: LottoGenerator의 로또 셍성 기능 최적화

--- src/model/LottoGenerator.js
+++ src/model/LottoGenerator.js
@@ -2,14 +2,13 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class LottoGenerator {
   static generateSingleLotto() {
-    // 1부터 45 사이의 숫자 중 6개의 고유 숫자를 랜덤으로 선택
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
       (a, b) => a - b,
     );
   }
 
   static generateMultipleLottos(count) {
-    return Array.from({ length: count }, () => this.generateSingleLotto());
+    return Array.from({ length: count }, LottoGenerator.generateSingleLotto);
   }
 }
 
