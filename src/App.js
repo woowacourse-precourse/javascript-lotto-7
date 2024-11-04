11e439a12664846764b7733be4d88abcfd623485
KimPuro <puro@protonmail.ch>
2024-11-04 18:52:31 +0900

    feat(App): 구입금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import {Console} from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+
+  }
+
+  async moneyInput(){
+    const money = await Console.readLineAsync('구입금액을 입력해 주세요.')
+  }
 }
 
 export default App;
