10ee29dac9c5b3f0a05595423b035a52f5ddc4c4
jinoo <jinoo0306@naver.com>
2024-11-04 16:09:45 +0900

    feat(1) : 출력 메세지 상수로 정리

---
+++ src/utils/message.js
@@ -0,0 +1,11 @@
+export const INPUT_MESSAGES = {
+  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.\n",
+};
+
+export const OUTPUT_MESSAGES = {};
+
+export const ERROR_MESSAGE = {
+  ERROR_MESSAGE_PREFIX: "[ERROR]",
+  INVALID_ISNAN: "구입 금액은 숫자여야 합니다.",
+  INVALID_PURCHASE: "구입 금액은 1,000원 단위여야 합니다.",
+};
