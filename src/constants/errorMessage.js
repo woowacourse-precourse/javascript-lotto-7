ad2523bb80e7cd2c6549b5e0485ea2255af7efba
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-04 18:32:06 +0900

    chore: 에러 메시지 추가

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -7,4 +7,7 @@ export const ERROR_MESSAGE = Object.freeze({
   INVALID_NUMBERS_LENGTH: `${ERROR_PREFIX} 로또 번호는 6개여야 합니다.`,
   INVALID_NUMBERS_RANGE: `${ERROR_PREFIX} 로또 번호는 1부터 45 사이여야 합니다.`,
   NOT_NUMBER: `${ERROR_PREFIX} 숫자가 아닌 값이 포함되어 있습니다.`,
+  EMPTY_INPUT: `${ERROR_PREFIX} 값을 입력해주세요.`,
+  DUPLICATED_NUMBER: `${ERROR_PREFIX} 중복된 번호는 입력할 수 없습니다.`,
+  CONTAINED_NUMBER: `${ERROR_PREFIX} 당첨 번호에 포함된 번호는 입력할 수 없습니다.`,
 });
