5dc0dd97885f64b85449c01ac078435be876aedf
Erin <fls4804@gmail.com>
2024-11-04 15:34:04 +0900

    feat:입력 금액이 유효한지 검사

--- src/App.js
+++ src/App.js
@@ -16,6 +16,16 @@ class App {
       const inputtedMoney = await MissionUtils.Console.readLineAsync(
         `구입금액을 입력해주세요./n`
       );
+      if (await this.isValidMoneyInput(inputtedMoney)) return inputtedMoney;
+    }
+  }
+  async isValidMoneyInput(inputtedMoney) {
+    try {
+      this.isValidMoneyInput(inputtedMoney);
+      return true;
+    } catch (e) {
+      MissionUtils.Console.print(e.messsage);
+      return false;
     }
   }
 }
