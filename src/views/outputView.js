69837a1576adb625420efb4fd14d83d2d219063c
ockko <ocko119@gmail.com>
2024-11-04 21:33:06 +0900

    feat: 로또 추첨 결과 문자열 생성 기능 구현

--- src/views/outputView.js
+++ src/views/outputView.js
@@ -1,5 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
-import getRandomNumbers from "../utils/random.js";
+import USER_OUTPUT from "../constants/Logs.js";
 
 const printLottoTickets = (lottoTickets) => {
   const NumberOfPurchases = `${lottoTickets.length}개를 구매했습니다.`;
@@ -7,4 +7,14 @@ const printLottoTickets = (lottoTickets) => {
     .map((ticket) => `[${ticket.join(", ")}]`)
     .join("\n");
   Console.print(`\n${NumberOfPurchases}\n${ticketLines}`);
+};
+
+const createLottoResultString = (lottoResult) => {
+  const outputKeys = ["fifth", "forth", "third", "second", "first"];
+  const outpustLists = outputKeys.map(
+    (key) => `${USER_OUTPUT[key.concat("Prize")]}${lottoResult[key]}개`
+  );
+  const resultString = `\n당첨 통계\n---\n${outputLists.join("\n")}`;
+
+  return resultString;
 };
