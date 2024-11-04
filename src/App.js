64ac11335414c63450cf04bee1a9feee82e96671
Ryu1216 <r12dh16@gmail.com>
2024-11-04 16:54:50 +0900

    feat: Add receiving lotto number and bonus number

--- src/App.js
+++ src/App.js
@@ -25,10 +25,10 @@ class App {
 
     while (!isValid) {
       try {
-        let purchasePrice = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+        purchasePrice = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
         purchasePrice = Number(this.getPositivePrice(purchasePrice));
         lottoQuantity = this.getThousandPrice(purchasePrice) / 1000;
-        
+
         isValid = true;
       } catch (error) {
         MissionUtils.Console.print(error.message);
@@ -39,11 +39,48 @@ class App {
 
     for (let i = 0; i < lottoQuantity; i++) {
       lottoArray.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+      lottoArray[i].sort((a, b) => a - b);
       MissionUtils.Console.print(`[${lottoArray[i]}]`);
     }
+    while (isValid) {
+      try {
+        const lottoNumber = await MissionUtils.Console.readLineAsync("\n당첨번호를 입력해 주세요.\n");
+        const winNumber = lottoNumber.split(",").map(Number);
+        const lotto = new Lotto(winNumber);
+        isValid = false;
+
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
+    while (!isValid) {
+      try {
+        const bonusNumber = Number(await MissionUtils.Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n"));
+        if (!(Number.isInteger(bonusNumber) && bonusNumber >= 1 && bonusNumber <= 45)) {
+          throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 정수여야 합니다.");
+        }
+        if (winNumber.includes(bonusNumber)) {
+          throw new Error("[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.");
+        }
+        isValid = true;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
+
     const lottoNumber = await MissionUtils.Console.readLineAsync("\n당첨번호를 입력해 주세요.\n");
-    const bonusNumber = await MissionUtils.Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
-    
+
+
+
+    const lottoPrize = {
+      1: 2000000000,
+      2: 30000000,
+      3: 1500000,
+      4: 50000,
+      5: 5000,
+    };
+
+
 
   }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -10,11 +10,33 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if(numbers.every(num => isNaN(num))){
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
+    if(!numbers.every(num => Number.isInteger(num) && num >= 1 && num <= 45)){
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 정수여야 합니다.");
+    }
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+    throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
   }
 
-  
-  
-  
+  checkRank(purchasedNumbers, bonusNumber) {
+
+    const matchCount = purchasedNumbers.filter((num) =>
+      this.#numbers.includes(num)
+    ).length;
+
+    const hasBonus = purchasedNumbers.includes(bonusNumber);
+
+    if (matchCount === 6) return 1;
+    if (matchCount === 5 && hasBonus) return 2;
+    if (matchCount === 5) return 3;
+    if (matchCount === 4) return 4;
+    if (matchCount === 3) return 5;
+    return 0;
+  }  
 }
 
 export default Lotto;
