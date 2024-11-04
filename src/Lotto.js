9d0c4d1c0aa877ca4b7ae5a1e3982bab745ce33e
wrryu09 <rryu09@ewhain.net>
2024-11-04 19:02:58 +0900

    feat: Lotto class 유효성 검증 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const isInRange = numbers.every((num) => num >= 1 && num <= 45);
+    if (!isInRange) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    const hasDuplicates = new Set(numbers).size !== numbers.length;
+    if (hasDuplicates) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
   }
 
   getNumbers() {
