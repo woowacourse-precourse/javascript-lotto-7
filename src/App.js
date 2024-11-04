74c46749b5da5fcf079cf21b126886b3329a7283
gwansikk <gwansik.kim@gwansik.dev>
2024-11-04 16:05:08 +0900

    feat: LottoMachine 모델 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import LottoMachine from './LottoMachine.js';
+
 class App {
-  async run() {}
+  async run() {
+    const lottoMachine = new LottoMachine();
+
+    const purchaseAmount = await lottoMachine.readPurchaseAmount();
+    const purchasedLottos = lottoMachine.purchaseLottos(purchaseAmount);
+
+    Console.print(purchaseAmount, purchasedLottos);
+  }
 }
 
 export default App;
---
+++ src/LottoMachine.js
@@ -0,0 +1,76 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import { readUserInput } from './utils/readUserInput.js';
+import { isNumber } from './utils/validators.js';
+import { throwError } from './utils/throwError.js';
+import {
+  ERROR_MESSAGE,
+  INPUT_MESSAGE,
+  LOTTO_LENGTH,
+  LOTTO_NUMBER_END,
+  LOTTO_NUMBER_START,
+  OUTPUT_MESSAGE,
+} from './constants.js';
+
+const DEFAULT_GENERATOR = () =>
+  Random.pickUniqueNumbersInRange(
+    LOTTO_NUMBER_START,
+    LOTTO_NUMBER_END,
+    LOTTO_LENGTH
+  );
+
+export default class LottoMachine {
+  static #LOTTO_PRICE = 1000;
+
+  #generator;
+
+  constructor(generator = DEFAULT_GENERATOR) {
+    this.#generator = generator;
+  }
+
+  async readPurchaseAmount() {
+    const purchaseAmount = await readUserInput(
+      INPUT_MESSAGE.READ_PURCHASE_AMOUNT,
+      [isNumber]
+    );
+    this.#validatePurchaseAmount(purchaseAmount);
+    return Number(purchaseAmount);
+  }
+
+  purchaseLottos(purchaseAmount) {
+    const lottoCount = this.#calculateLottoCount(purchaseAmount);
+    this.#printPurchaseSummary(lottoCount);
+
+    const purchasedLottos = Array.from({ length: lottoCount }, () =>
+      this.#createLotto()
+    );
+    Console.print('');
+    return purchasedLottos;
+  }
+
+  #validatePurchaseAmount(purchaseAmount) {
+    if (purchaseAmount < LottoMachine.#LOTTO_PRICE) {
+      throwError(
+        ERROR_MESSAGE.LOTTO_CHECK_PURCHASE_AMOUNT(LottoMachine.#LOTTO_PRICE)
+      );
+    }
+  }
+
+  #calculateLottoCount(purchaseAmount) {
+    return Math.floor(purchaseAmount / LottoMachine.#LOTTO_PRICE);
+  }
+
+  #createLotto() {
+    const newLotto = new Lotto(this.#generator());
+    this.#printLotto(newLotto);
+    return newLotto;
+  }
+
+  #printLotto(lotto) {
+    Console.print(`[${lotto.getNumbers().join(', ')}]`);
+  }
+
+  #printPurchaseSummary(lottoCount) {
+    Console.print(OUTPUT_MESSAGE.PURCHASE_SUMMARY(lottoCount));
+  }
+}
