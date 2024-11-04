54c8627ba2294838cd7c5c949288896c070be929
mywhzy <cmj2393a@naver.com>
2024-11-04 17:49:12 +0900

    feat: 보너스 넘버 setter 구현

--- src/WinningNumbers.js
+++ src/WinningNumbers.js
@@ -1,4 +1,6 @@
-import { validateWinningNumbers } from './validate.js';
+import { ERROR_MESSAGE } from './constants.js';
+import { throwError } from './utils/throwError.js';
+import { validateWinningNumbers, validateBonusNumber } from './validate.js';
 
 class WinningNumbers {
   #numbers;
@@ -7,6 +9,17 @@ class WinningNumbers {
   constructor(winningNumbers) {
     validateWinningNumbers(winningNumbers);
     this.#numbers = winningNumbers.map(Number);
+    this.#bonusNumber = null;
+  }
+
+  set bonusNumber(bonusNumber) {
+    if (this.#bonusNumber) throwError(ERROR_MESSAGE.immutableFieldError);
+    validateBonusNumber(bonusNumber, this.#numbers);
+    this.#bonusNumber = +bonusNumber;
+  }
+
+  get bonusNumber() {
+    return JSON.parse(JSON.stringify(this.#bonusNumber));
   }
 }
 
