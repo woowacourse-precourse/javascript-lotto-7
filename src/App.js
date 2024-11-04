c715196300ad2354f810f56f35283869c5a7ee48
YeRimmm-Cho <em514@dgu.ac.kr>
2024-11-04 17:09:30 +0900

    Feat(App): 구입 금액만큼 로또 개수 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ class App {
   async run() {
     try {
       const purchaseAmount = await this.getPurchaseAmount();
-      MissionUtils.Console.print(`${purchaseAmount}`);
+      const lottoCount = purchaseAmount / 1000; // 구매 가능한 로또 개수
+
+      MissionUtils.Console.print(`\n${lottoCount}개를 구매했습니다.`);
     } catch (error) {
       MissionUtils.Console.print(error.message);
       await this.run(); // 금액 입력 재시도
@@ -17,7 +19,6 @@ class App {
     );
     const amount = Number(input);
 
-    // 금액이 숫자고 1,000원 단위의 양수인지 확인
     if (isNaN(amount) || amount <= 0 || amount % 1000 !== 0) {
       throw new Error("[ERROR] 금액은 1,000원 단위의 양수로 입력해 주세요.");
     }
