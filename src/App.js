453a7d951fb84c22f035d24f721aebda2b219dcd
Apoliasm <syjsir@gmail.com>
2024-11-04 23:35:01 +0900

    REFACTOR : Divide functions in App.js
    - Return to input phase if throw error

--- src/App.js
+++ src/App.js
@@ -6,31 +6,63 @@ import Slot from "./Slot/Slot.js";
 import { Console } from "@woowacourse/mission-utils";
 import { resultToPrize } from "./Util/Switch.js";
 class App {
-  async run() {
 
+  async inputFirst(){
     //1. 구매 금액 입력
-    const screen = new Screen()
-    const inputMoney = await screen.inputMoney()
-    const slot = new Slot(inputMoney)
-    const drawmLotteries = slot.getLotteryArray()
-    screen.printLotteries(slot.getPurchaseNum(),drawmLotteries.map(element => {return element.getNumbers()}));
+    this.screen = new Screen()
+    this.inputMoney = await this.screen.inputMoney()
+    this.slot = new Slot(this.inputMoney)
+    this.drawmLotteries = this.slot.getLotteryArray()
+    this.screen.printLotteries(this.slot.getPurchaseNum(),this.drawmLotteries.map(element => {return element.getNumbers()}));
+  }
 
+  async inputLottoNums(){
     //2.당첨번호, 보너스 입력
-    const inputWinNum = await screen.inputLotto()
-    const inputBonus = await screen.inputBonus()
-    const lotto = new Lotto(inputWinNum,inputBonus)
+    this.inputWinNum = await this.screen.inputLotto()
+    this.inputBonus = await this.screen.inputBonus()
+    this.lotto = new Lotto(this.inputWinNum,this.inputBonus)
 
+  }
+
+  printResult(){
     //3.당첨 여부 확인 & 결과 등록
-    const report = new Report()
-    drawmLotteries.forEach(element => {
-      const result = element.getResult(lotto.getWinArray(), lotto.getBonus())
+    this.report = new Report()
+    this.drawmLotteries.forEach(element => {
+      const result = element.getResult(this.lotto.getWinArray(), this.lotto.getBonus())
       if (result !== "nothing"){
-        report.result[result] += 1
-        report.prize += resultToPrize(result)
+        this.report.result[result] += 1
+        this.report.prize += resultToPrize(result)
       }
       
     })
-    screen.printResult(report.result,report.getProfit(parseInt(inputMoney)))
+    this.screen.printResult(this.report.result,this.report.getProfit(parseInt(this.inputMoney)))
+
+  }
+
+
+  async run() {
+
+    try {
+      await this.inputFirst()
+    } catch (error) {
+      Console.print(error)
+      await this.inputFirst()
+    }
+
+    try{
+      await this.inputLottoNums()
+    }catch(error){
+      Console.print(error)
+      await this.inputLottoNums()
+    }
+
+    this.printResult()
+    
+
+    
+
+
+    
     
 
 
