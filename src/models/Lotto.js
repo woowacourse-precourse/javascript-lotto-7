ecd099f6e2476986497c0a78843e5d9fe374da44
ockko <ocko119@gmail.com>
2024-11-04 19:37:42 +0900

    feat(Lotto.js): 당첨 번호와 발행 로또 비교 기능 구현

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -18,6 +18,36 @@ class Lotto {
   getNumbers() {
     return [...this.#numbers];
   }
+
+  #matchCount(ticket) {
+    return ticket.filter((number) => this.#numbers.includes(number)).length;
+  }
+
+  #evaluateEachLottoTicket(ticket, bonusNumber, lottoResult) {
+    const updatedResult = { ...lottoResult };
+    const matchingNumber = this.#matchCount(ticket);
+    const bonusMatch = ticket.includes(bonusNumber);
+
+    if (matchingNumber === 6) updatedResult.first += 1;
+    if (matchingNumber === 5 && bonusMatch) updatedResult.second += 1;
+    if (matchingNumber === 5 && !bonusMatch) updatedResult.third += 1;
+    if (matchingNumber === 4) updatedResult.fourth += 1;
+    if (matchingNumber === 3) updatedResult.fifth += 1;
+
+    return updatedResult;
+  }
+
+  evaluateLottoTickets(lottoTickets, bonusNumber, lottoResult) {
+    let evaluatedResult = { ...lottoResult };
+    lottoTickets.forEach((ticket) => {
+      evaluatedResult = this.#evaluateEachLottoTicket(
+        ticket,
+        bonusNumber,
+        evaluatedResult
+      );
+    });
+    return evaluatedResult;
+  }
 }
 
 export default Lotto;
