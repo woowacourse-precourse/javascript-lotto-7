d599c1808d8ac863c358555326fc68028a60052c
thumbthing <ilscc0902@gmail.com>
2024-11-04 14:06:33 +0900

    Style: 주석 삭제

--- src/Class/LottoList.js
+++ src/Class/LottoList.js
@@ -1,20 +1,6 @@
 import { Random } from "@woowacourse/mission-utils";
 
 class LottoList {
-/** TODO :
- *  field : 
- *    purchase : 구입금액을 1000으로 나눈 값
- *    lottoList : 생성된 구매 로또의 배열
- *  constuctor :
- *    purchase 필드 값을 purchase 값을 1000으로 나눈 값으로 초기화
- *    해당 값의 갯수 만큼 로또를 생성
- *    생성된 로또 배열로 lottoList를 초기화
- *  method :
- *    1. 배열을 오름 차순으로 정렬하는 기능
- *    2. 하나의 로또를 생성하는 기능
- *    3. purchase 갯수 만큼 로또를 생성하는 기능
- */
-
   purchase;
   lottoList;
 
