742814928896bc7bcdeac9abd1040bf9c674fb49
yheseul <kangyheseul@naver.com>
2024-11-04 20:47:53 +0900

    refactor: 당첨 개수의 길이, 초기 당첨 개수, 당첨 개수 증가 값 상수화 및 적용

--- src/services/calculateWinningDetails.js
+++ src/services/calculateWinningDetails.js
@@ -1,19 +1,27 @@
+import {
+  INITIAL_WINNING_COUNT,
+  MATCH_INCREMENT,
+  WINNING_COUNT_LENGTH,
+} from "../constants/resultConstants.js";
+
 export const calculateWinningDetails = function (matchingNumber) {
-  const winningCounts = new Array(5).fill(0);
+  const winningCounts = new Array(WINNING_COUNT_LENGTH).fill(
+    INITIAL_WINNING_COUNT
+  );
 
   matchingNumber.forEach((match) => {
     switch (match.length) {
       case 3:
-        winningCounts[0] += 1;
+        winningCounts[0] += MATCH_INCREMENT;
         break;
       case 4:
-        winningCounts[1] += 1;
+        winningCounts[1] += MATCH_INCREMENT;
         break;
       case 5:
-        winningCounts[2] += 1;
+        winningCounts[2] += MATCH_INCREMENT;
         break;
       case 6:
-        winningCounts[4] += 1;
+        winningCounts[4] += MATCH_INCREMENT;
         break;
       default:
         break;
