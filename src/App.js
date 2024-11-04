260a82711b5a97e7fcbc3b6f0b98a6d1b916b08f
sanghyeonLee <hyeon012366@gmail.com>
2024-11-04 12:26:50 +0900

    refactor: 가독성 향상과 오류 발생시 입력 재실행을 위한 기존코드 유틸함수 분리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import {INPUT} from "./constants/message.js";
-import {input, purchaseOutput, winningStatsOutput} from "./ui/view.js";
-import LottoMachine from "./services/LottoMachine.js";
-import Lotto from "./services/Lotto.js";
-import {amountToNumber, stringToNumberArray} from "./utils/methods.js";
+import {purchaseOutput, winningStatsOutput} from "./ui/view.js";
+import {getBonusNumber, getPurchaseAmount, getWinningNumbers} from "./services/utils/inputHandler.js";
 
 class App {
-
     async run() {
-        const purchaseAmount = await input(INPUT.PURCHASE)
-        const purchaseNum = amountToNumber(purchaseAmount) //입력금액 갯수로 변환 한걸 담은 변수
-        const lottoMachine = new LottoMachine(purchaseNum)
-        const purchasedLottos = lottoMachine.lottoRelease()
-        purchaseOutput(purchasedLottos)
-        const winningNum = stringToNumberArray(await input(INPUT.WINNING_STATS))
-        const lotto = new Lotto(winningNum)
-        const bonusNum = await input(INPUT.BONUS_NUM) //보너스 번호는 유효성 검사만 필요하다
-        lotto.recordStats(purchasedLottos, bonusNum)
-        const calculateYield = lotto.calculateYield(purchaseAmount)
-        winningStatsOutput(calculateYield)
+        const {purchaseAmount, lottoMachine} = await getPurchaseAmount(); // 유효할 때까지 반복
+        const purchasedLottos = lottoMachine.lottoRelease();
+        purchaseOutput(purchasedLottos);
+
+        const lotto = await getWinningNumbers();
+
+        const bonusNum = await getBonusNumber(lotto);
+        lotto.recordStats(purchasedLottos, bonusNum);
+
+        const calculateYield = lotto.calculateYield(purchaseAmount);
+        winningStatsOutput(calculateYield);
     }
 }
 
