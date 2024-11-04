14dc7364e0afc440e1c9efdc0be29bc73f78865d
minji2219 <200146@jnu.ac.kr>
2024-11-04 13:33:43 +0900

    test: WinnintTest 클래스 테스트 작성
    로또들 중 당첨 로또의 등수를 출력하는 테스트 작성

--- __tests__/LottosTest.js
+++ __tests__/LottosTest.js
@@ -2,24 +2,22 @@ import Lottos from '../src/Lottos.js';
 
 describe('Lotts 클래스 테스트', () => {
   test('getLottos 테스트', () => {
-    expect(() => {
-      const lottos = new Lottos();
-      lottos.lottoAmount = 2;
-      const lottosArr = lottos.getLottos();
+    const lottos = new Lottos();
+    lottos.lottoAmount = 2;
+    const lottosArr = lottos.getLottos();
 
-      lottosArr.forEach((lotto) => {
-        //6개의 숫자인지 확인
-        expect(lotto).toHaveLength(6);
+    lottosArr.forEach((lotto) => {
+      //6개의 숫자인지 확인
+      expect(lotto).toHaveLength(6);
 
-        //6개의 숫자가 중복 없는지 확인
-        const uniqueNumber = new Set(lotto);
-        expect(uniqueNumber.size).toBe(6);
+      //6개의 숫자가 중복 없는지 확인
+      const uniqueNumber = new Set(lotto);
+      expect(uniqueNumber.size).toBe(6);
 
-        //오름차순으로 정렬되어 있는지 확인
-        for (let i = 0; i < lotto.length; i++) {
-          expect(lotto[i - 1]).toBeLessThanOrEqual(lotto[i]);
-        }
-      });
+      //오름차순으로 정렬되어 있는지 확인
+      for (let i = 1; i < lotto.length; i++) {
+        expect(lotto[i - 1]).toBeLessThanOrEqual(lotto[i]);
+      }
     });
   });
 });
