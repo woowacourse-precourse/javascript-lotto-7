1dbaac849dc2bd1eb1157b8046e7fe47df1e42b7
crolvlee <leecrolv@gmail.com>
2024-11-04 18:31:59 +0900

    feat: 티켓 생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import { Console } from "@woowacourse/mission-utils";
-import { getLottoCount } from "./utils/InputHandler.js";
+import { Console, Random } from "@woowacourse/mission-utils";
+import { getTicketCount } from "./utils/InputHandler.js";
 
 class App {
   async run() {
-    const lottoCount = await getLottoCount();
+    // 구입금액 입력받음
+    const ticketCount = await getTicketCount();
+
+    // 티켓 생성
+    Console.print(`\n${ticketCount}개를 구매했습니다.`);
+
+    const tickets = generateTickets(ticketCount);
+    tickets.forEach(ticket => {
+      Console.print(`[${ticket.join(", ")}]`);
+    });
   }
 }
 
 export default App;
+
+
+function generateTickets(ticketCount) {
+  const tickets = [];
+
+  for (let i = 0; i < ticketCount; i++) {
+    let ticket = Random.pickUniqueNumbersInRange(1, 45, 6);
+    ticket = ticket.sort((a, b) => a - b);
+    tickets.push(ticket);
+  }
+
+  return tickets;
+}
--- src/utils/InputHandler.js
+++ src/utils/InputHandler.js
@@ -1,13 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 import { validateBudget } from "./Validator.js";
 
-export async function getLottoCount() {
+export async function getTicketCount() {
   const budget = await Console.readLineAsync(
     "구입금액을 입력해 주세요.\n"
   );
 
   validateBudget(budget);
 
-  const lottoCount = budget / 1000
-  return lottoCount;
+  const ticketCount = budget / 1000
+  return ticketCount;
 }
