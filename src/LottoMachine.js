1c6338fbf74267e37af6d9a7179adf5d427321b3
mywhzy <cmj2393a@naver.com>
2024-11-04 10:35:28 +0900

    feat: 구입 금액에 따른 로또 발행 수량 구하기 메소드 구현

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -1,8 +1,13 @@
+import { LOTTO_SINGLE_TICKET_PRICE } from './constants.js';
 import { validatePurchaseAmount } from './validate.js';
 
 class LottoMachine {
   constructor(purchaseAmount) {
     validatePurchaseAmount(purchaseAmount);
+  }
+
+  calculateLottoCount(purchaseAmount) {
+    return purchaseAmount / LOTTO_SINGLE_TICKET_PRICE;
   }
 }
 
