e228d3d092787a7907d0bdcb661db497d0de56a2
Jaeho Cha <ischar@naver.com>
2024-11-04 19:26:24 +0900

    refactor(input): update inputHandler to remove exception
    - inputHandler 기능 수정
      - 공백일 때 예외 처리 제거

--- src/handlers/inputHandler.js
+++ src/handlers/inputHandler.js
@@ -5,10 +5,5 @@ export const inputHandler = async (message) => {
   Console.print(message);
 
   const input = await Console.readLineAsync("");
-
-  if (input === "") {
-    throw new Error(ERROR.INPUT_EMPTY);
-  }
-
   return input;
 };
