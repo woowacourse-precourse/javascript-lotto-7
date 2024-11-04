d5326548b8f6dc3bae1f640c69a8448100835062
taeyomi <terry11ty@naver.com>
2024-11-04 17:52:53 +0900

    feat(validaiton): 구입금액이 음수일 경우 유효성검사 추가

--- src/validation.js
+++ src/validation.js
@@ -3,6 +3,8 @@ import ERROR_MESSAGE from "./constants/error.js";
 export const validatePurchasingAmount = (userInputAmounts) => {
   if (userInputAmounts % 1000 !== 0) {
     throw new Error(ERROR_MESSAGE.INPUT_AMOUNT);
+  } else if (userInputAmounts <= 0) {
+    throw new Error(ERROR_MESSAGE.INPUT_AMOUNT);
   }
 };
 
