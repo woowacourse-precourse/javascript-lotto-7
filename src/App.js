7fcd269b0b7142afccb0080abfdeba588d4bbd50
Yerim1234 <kongsun0805@gmail.com>
2024-11-04 22:32:53 +0900

    fix: App.js 파일에서 Console.readLineAsync을 이용해 비동기적인 흐름 개선

--- src/App.js
+++ src/App.js
@@ -9,23 +9,22 @@ class App {
     this.purchaseAmount = 0;
   }
 
-  run() {
-    this.start();
+  async run() {
+    await this.start();
   }
 
-  start() {
+  async start() {
     Console.print("구입금액을 입력해 주세요.");
-    Console.readLine("구입금액: ", (input) => {
-      const purchaseAmount = Number(input);
-      if (this.validatePurchaseAmount(purchaseAmount)) {
-        this.purchaseAmount = purchaseAmount;
-        const lottoCount = purchaseAmount / 1000;
-        this.purchaseLottos(lottoCount);
-      } else {
-        Console.print("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
-        this.start(); // 다시 입력 받기
-      }
-    });
+    const input = await Console.readLineAsync("구입금액: ");
+    const purchaseAmount = Number(input);
+    if (this.validatePurchaseAmount(purchaseAmount)) {
+      this.purchaseAmount = purchaseAmount;
+      const lottoCount = purchaseAmount / 1000;
+      this.purchaseLottos(lottoCount);
+    } else {
+      Console.print("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+      await this.start(); // 다시 입력 받기
+    }
   }
 
   validatePurchaseAmount(amount) {
@@ -42,20 +41,19 @@ class App {
     this.getWinningNumbers(); // 다음 단계로 이동
   }
 
-  getWinningNumbers() {
+  async getWinningNumbers() {
     Console.print("당첨 번호를 입력해 주세요.");
-    Console.readLine("당첨 번호: ", (input) => {
-      const winningNumbers = input.split(",").map(Number);
-      if (this.validateWinningNumbers(winningNumbers)) {
-        this.winningNumbers = winningNumbers;
-        this.getBonusNumber();
-      } else {
-        Console.print(
-          "[ERROR] 당첨 번호는 1부터 45 사이의 중복되지 않는 숫자 6개여야 합니다."
-        );
-        this.getWinningNumbers(); // 잘못된 입력이므로 다시 입력 받기
-      }
-    });
+    const input = await Console.readLineAsync("당첨 번호: ");
+    const winningNumbers = input.split(",").map(Number);
+    if (this.validateWinningNumbers(winningNumbers)) {
+      this.winningNumbers = winningNumbers;
+      this.getBonusNumber();
+    } else {
+      Console.print(
+        "[ERROR] 당첨 번호는 1부터 45 사이의 중복되지 않는 숫자 6개여야 합니다."
+      );
+      await this.getWinningNumbers(); // 잘못된 입력이므로 다시 입력 받기
+    }
   }
 
   validateWinningNumbers(numbers) {
@@ -67,20 +65,19 @@ class App {
     );
   }
 
-  getBonusNumber() {
+  async getBonusNumber() {
     Console.print("보너스 번호를 입력해 주세요.");
-    Console.readLine("보너스 번호: ", (input) => {
-      const bonusNumber = Number(input);
-      if (this.validateBonusNumber(bonusNumber)) {
-        this.bonusNumber = bonusNumber;
-        this.calculateResults(); // 당첨 결과 계산으로 이동
-      } else {
-        Console.print(
-          "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 숫자여야 합니다."
-        );
-        this.getBonusNumber(); // 잘못된 입력이므로 다시 입력 받기
-      }
-    });
+    const input = await Console.readLineAsync("보너스 번호: ");
+    const bonusNumber = Number(input);
+    if (this.validateBonusNumber(bonusNumber)) {
+      this.bonusNumber = bonusNumber;
+      this.calculateResults(); // 당첨 결과 계산으로 이동
+    } else {
+      Console.print(
+        "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 숫자여야 합니다."
+      );
+      await this.getBonusNumber(); // 잘못된 입력이므로 다시 입력 받기
+    }
   }
 
   validateBonusNumber(bonus) {
