bd12694e5c41d252112d6f1bfa4bfc65c4efe1f4
YeWon Seo <syw1236@naver.com>
2024-11-04 11:02:34 +0900

    feat(constants): 화면에 출력할 문자열과 숫자 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const lottoMesaage = {
+    INPUT_MONEY: '구입금액을 입력해 주세요.\n',
+    PRINT_BUYCOUNT: '개를 구매했습니다.\n',
+    INPUT_LOTTONUMBERS: '당첨 번호를 입력해 주세요.\n',
+    INPUT_BONUSNUMBER: '보너스 번호를 입력해 주세요\n',
+    PRINT_STATISTICS: '당첨 통계\n',
+    PRINT_BAR: '-',
+    PRINT_COUNT: '개',
+    PRINT_MATCH: '일치',
+    PRINT_REVENUE: '총 수익률은',
+    OPENING_PARENTHESIS: '(',
+    CLOSING_PARENTHESIS: ')',
+}
+
+export const numbers = {
+    THREE_VALUE: 5000,
+    FOUR_VALUE: 50000,
+    FIVE_VALUE: 1500000,
+    FIVE_BONUS_VALUE: 30000000,
+    SIX_VALUE: 2000000000,
+    HUNDRED: 100,
+    LOTTO_UNITS: 1000,
+}
