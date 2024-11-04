c34c9409136145e86300b2fd51f2a947244fdaef
Jang Eunhye <hh961130@naver.com>
2024-11-04 16:48:04 +0900

    test: Lotto_bonus 예외처리

--- src/Lotto_bonus.js
+++ src/Lotto_bonus.js
@@ -7,7 +7,17 @@ class Lotto_bonus {
     }
   
     validate(inputBonusNumber) {
-        // 예외 처리
+      if (inputBonusNumber.length !== 1) {
+        throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      }
+      
+      if (inputBonusNumber < 1 || 45 < inputBonusNumber) {
+        throw new Error("[ERROR] 로또 번호는 1 - 45 만 가능합니다.");
+      }
+  
+      if (isNaN(inputBonusNumber)) {
+        throw new Error("[ERROR] 로또 번호에 문자가 포함될 수 없습니다.");
+      }
     }
 
   }
