ce6e985a48a642f39a5c52d1a394abe2833ec87c
luminox93 <memoji1236@gmail.com>
2024-11-04 21:25:58 +0900

    test: ObjectFactory 함수별 객체 생성 및 예외 처리 테스트 추가
    - createPurchaseAmount 함수 테스트: 사용자 입력으로 PurchaseAmount 객체 생성 검증
    - createLottoNumber 함수 테스트: 사용자 입력 번호로 Lotto 객체 생성 검증
    - createBonusNumber 함수 테스트:
      - 보너스 번호가 기존 로또 번호와 겹치지 않을 때 BonusNumber 객체 생성 확인
      - 보너스 번호가 기존 로또 번호와 중복될 경우 예외 발생 검증
    - InputView 모킹을 통해 사용자 입력을 시뮬레이션하여 각 객체 생성 로직 테스트

---
+++ __tests__/ObjectFactory.test.js
@@ -0,0 +1,51 @@
+import {
+  createPurchaseAmount,
+  createLottoNumber,
+  createBonusNumber,
+} from '../src/factories/ObjectFactory.js';
+import PurchaseAmount from '../src/lotto/PurchaseAmount.js';
+import Lotto from '../src/Lotto.js';
+import BonusNumber from '../src/lotto/BonusNumber.js';
+import { InputView } from '../src/views/InputView.js';
+import errorConstants from '../src/constants/error-constants.js';
+
+// InputView를 모킹
+jest.mock('../src/views/InputView.js', () => ({
+  InputView: jest.fn(),
+}));
+
+describe('createPurchaseAmount 함수 테스트', () => {
+  test('사용자 입력 금액으로 PurchaseAmount 객체를 생성한다', async () => {
+    InputView.mockResolvedValueOnce('5000');
+    const purchaseAmount = await createPurchaseAmount();
+    expect(purchaseAmount).toBeInstanceOf(PurchaseAmount);
+    expect(purchaseAmount.getPurchaseAmount()).toBe(5000);
+  });
+});
+
+describe('createLottoNumber 함수 테스트', () => {
+  test('사용자 입력 번호로 Lotto 객체를 생성한다', async () => {
+    InputView.mockResolvedValueOnce('1,2,3,4,5,6');
+    const lotto = await createLottoNumber();
+    expect(lotto).toBeInstanceOf(Lotto);
+    expect(lotto.getLottoNumber()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+});
+
+describe('createBonusNumber 함수 테스트', () => {
+  const lottoNumbers = [1, 2, 3, 4, 5, 6];
+
+  test('보너스 번호가 기존 로또 번호와 겹치지 않을 때 BonusNumber 객체를 생성한다', async () => {
+    InputView.mockResolvedValueOnce('7');
+    const bonusNumber = await createBonusNumber(lottoNumbers);
+    expect(bonusNumber).toBeInstanceOf(BonusNumber);
+    expect(bonusNumber.getBonusNumber()).toBe(7); // 수정된 부분
+  });
+
+  test('보너스 번호가 기존 로또 번호와 겹칠 때 에러를 발생시킨다', async () => {
+    InputView.mockResolvedValueOnce('6');
+    await expect(createBonusNumber(lottoNumbers)).rejects.toThrow(
+      errorConstants.DUPLICATED_NUMBER,
+    );
+  });
+});
