ce2c88dccca028600d69b7b838f090716c5368c5
PangKiYeon <pdrkeamy@gmail.com>
2024-11-04 20:04:10 +0900

    feat: lotto calculator 클래스 선언

--- src/App.js
+++ src/App.js
@@ -14,9 +14,21 @@ class App {
         const WINNING_INPUT = await Console.readLineAsync(
             '\n당첨 번호를 입력 주세요.\n'
         );
+        const WINNING_NUMBER = LOTTO_HELPER.makeWinningNumber(WINNING_INPUT);
         const BONUS_NUMBER = await Console.readLineAsync(
             '\n보너스 번호를 입력해 주세요.\n'
         );
+
+        const LOTTO_CALCULATOR = new LottoCalculator(
+            WINNING_NUMBER,
+            BONUS_NUMBER
+        );
+
+        Console.print('\n당첨 통계\n---');
+        const RESULT = LOTTO_CALCULATOR.calculateResult(LOTTO);
+        const SUMMARIZE_RESULT = LOTTO_CALCULATOR.summarizeResult(RESULT);
+        LOTTO_CALCULATOR.printResult(SUMMARIZE_RESULT);
+        LOTTO_CALCULATOR.calculateReturnRate(SUMMARIZE_RESULT, MONEY);
     }
 }
 
