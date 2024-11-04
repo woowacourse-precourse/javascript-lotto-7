386a4dcff9a72f7205e7b1ed373e9cabc3da4c55
chichi <seonrye.yun@gmail.com>
2024-11-04 23:58:12 +0900

    add: 상수 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -2,11 +2,16 @@ export const PROMPT_MESSAGES = Object.freeze({
   INPUT_AMOUNT: '구입금액을 입력해 주세요.\n',
   INPUT_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
   INPUT_BONUS_NUMBERS: '보너스 번호를 입력해 주세요.\n',
+  OUTPUT_LOTTOS: '개를 구매했습니다.',
 });
 
 export const ERROR_MESSAGES = Object.freeze({
-  INVALID_LOTTO_PRICE: '구입 금액은 1,000원 단위로 입력해 주세요.',
-  INVALID_LOTTO_NUMBERS: '당첨 번호를 쉼표 기준으로 구분해 주세요.',
-  EMPTY_LOTTO_NUMBERS: '당첨 번호를 입력해 주세요.',
-  INVALID_LOTTO_COUNT: '당첨 번호를 6개 입력해 주세요.',
+  NON_NUMERIC_AMOUNT: '[ERROR] 구입 금액은 숫자로 입력해 주세요.',
+  EMPTY_LOTTO_PRICE: '[ERROR] 구입 금액이 입력되지 않았습니다.',
+  INVALID_LOTTO_PRICE: '[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.',
+  INVALID_LOTTO_NUMBERS: '[ERROR] 당첨 번호를 숫자로 입력해 주세요.',
+  EMPTY_LOTTO_NUMBERS: '[ERROR] 당첨 번호를 입력해 주세요.',
+  INVALID_LOTTO_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
+  DUPLICATE_LOTTO_NUMBERS:
+    '[ERROR] 로또 번호에 중복된 숫자를 사용하실 수 없습니다.',
 });
