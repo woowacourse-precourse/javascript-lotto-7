1502d6ac728a86718241352f9876d3f71fc7a38d
geunhwa37 <jacika37@naver.com>
2024-11-04 17:17:20 +0900

    feat: Lotto 클래스 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,18 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (!numbers.every(num => num >= 1 && num <= 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  // 로또 번호 반환
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
