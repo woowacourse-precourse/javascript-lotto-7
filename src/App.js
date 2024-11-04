444a2888faef86c0215b1dfac42db152974a6b08
crolvlee <leecrolv@gmail.com>
2024-11-04 22:26:28 +0900

    refactor: 함수 하나 당 하나의 기능을 수행하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,76 +1,7 @@
-import { Console, Random } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 import { getTicketCount, getWinningNumbers, getBonusNumber } from "./utils/InputHandler.js";
-import Lotto from "./Lotto.js";
-
-function generateTickets(ticketCount) {
-  const tickets = [];
-
-  for (let i = 0; i < ticketCount; i++) {
-    let ticket = Random.pickUniqueNumbersInRange(1, 45, 6);
-    ticket = ticket.sort((a, b) => a - b);
-    const lotto = new Lotto(ticket);
-    tickets.push(lotto.getNumbers());
-  }
-
-  return tickets;
-}
-
-function countPrizeResults(tickets, winningNumbers, bonusNumber) {
-  const prizeResults = {
-    '3': 0,
-    '4': 0,
-    '5': 0,
-    '5+bonus': 0,
-    '6': 0
-  }
-
-  tickets.forEach(ticket => {
-    const matchCount = ticket.filter(num => winningNumbers.includes(num)).length;
-    const hasBonus = ticket.includes(bonusNumber);
-
-    if (matchCount == 6) {
-      prizeResults['6']++;
-    } else if (matchCount == 5 && hasBonus) {
-      prizeResults['5+bonus']++;
-    } else if (matchCount == 5) {
-      prizeResults['5']++;
-    } else if (matchCount == 4) {
-      prizeResults['4']++;
-    } else if (matchCount == 3) {
-      prizeResults['3']++;
-    }
-  })
-
-  return prizeResults;
-}
-
-function showStatistics(prizeResults, ticketCount) {
-  const prizeMoney = {
-    '3': 5_000,
-    '4': 50_000,
-    '5': 1_500_000,
-    '5+bonus': 30_000_000,
-    '6': 2_000_000_000
-  }
-
-  let totalMoney = 0;
-  for (const [key, value] of Object.entries(prizeResults)) {
-    totalMoney += value * prizeMoney[key]
-  }
-
-  const totalMoneySpent = ticketCount * 1000
-  const profitRate = Math.round((totalMoney / totalMoneySpent) * 100 * 10) / 10;
-
-
-  Console.print('\n당첨 통계');
-  Console.print('---');
-  Console.print(`3개 일치 (5,000원) - ${prizeResults['3']}개`);
-  Console.print(`4개 일치 (50,000원) - ${prizeResults['4']}개`);
-  Console.print(`5개 일치 (1,500,000원) - ${prizeResults['5']}개`);
-  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${prizeResults['5+bonus']}개`);
-  Console.print(`6개 일치 (2,000,000,000원) - ${prizeResults['6']}개`);
-  Console.print(`총 수익률은 ${profitRate}%입니다.`);
-}
+import { generateTickets } from "./utils/TicketUtils.js";
+import { countPrizeResults, showStatistics} from "./utils/ResultUtils.js"
 
 class App {
   async run() {
---
+++ src/utils/TicketUtils.js
@@ -0,0 +1,15 @@
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "../Lotto.js";
+
+export function generateTickets(ticketCount) {
+  const tickets = [];
+
+  for (let i = 0; i < ticketCount; i++) {
+    let ticket = Random.pickUniqueNumbersInRange(1, 45, 6);
+    ticket = ticket.sort((a, b) => a - b);
+    const lotto = new Lotto(ticket);
+    tickets.push(lotto.getNumbers());
+  }
+
+  return tickets;
+}
