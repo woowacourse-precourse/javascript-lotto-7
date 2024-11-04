a756996907ee675e65ecb44b667b9e54c6239055
kimywann <Kimywan10@gmail.com>
2024-11-04 14:41:31 +0900

    feat(Lotto): 로또 번호 유효성 검사 기능 추가
    - Set을 사용하여 로또 번호에 중복이 없도록 확인
    - 번호가 1부터 45 사이의 숫자인지 확인
    - 모든 로또 번호가 정수인지 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,8 +10,23 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
 
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+
+    }
+
+    numbers.forEach(number => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+
+      if (!Number.isInteger(number)) {
+        throw new Error("[ERROR] 로또 번호는 정수여야 합니다.");
+      }
+    });
+  }
   // TODO: 추가 기능 구현
 }
 
