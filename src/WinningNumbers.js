112d31a3d778b92e32ceadeb189ce1b31accd1d6
wowalswjd <alswjd5491@naver.com>
2024-11-04 15:41:15 +0900

    fix(WinningNumbers): 보너스 번호 저장 및 유효성 검증 메서드 추가

--- src/WinningNumbers.js
+++ src/WinningNumbers.js
@@ -1,7 +1,9 @@
 import Lotto from "./Lotto.js";
+import { WINNING_NUMBERS_ERROR } from "./Message/Message.js";
 
 class WinningNumbers {
   #winningLotto = null; // 당첨 번호 (Lotto)
+  #bonusNumber = null; // 보너스 번호 (number)
 
   setWinningLotto(winningLottoInput) {
     // split 해서 배열로 만들기
@@ -9,8 +11,50 @@ class WinningNumbers {
     this.#winningLotto = new Lotto(winningLottoArr);
   }
 
+  setBonusNumber(bonusNumberInput) {
+    const checkNum = Number(bonusNumberInput);
+    this.#validateBonusNum(checkNum);
+    this.#bonusNumber = checkNum;
+  }
+
   getWinningLotto() {
     return this.#winningLotto.getLotto();
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+
+  #validateBonusNum(num) {
+    this.#checkIsNumber(num);
+    this.#checkisInteger(num);
+    this.#checkIsRange(num);
+
+    this.#checkDuplicateNum(num);
+  }
+
+  #checkIsNumber(num) {
+    if (Number.isNaN(num)) {
+      throw new Error(WINNING_NUMBERS_ERROR.IS_NOT_NUMBER);
+    }
+  }
+
+  #checkisInteger(num) {
+    if (!Number.isInteger(num)) {
+      throw new Error(WINNING_NUMBERS_ERROR.IS_NOT_INTEGER);
+    }
+  }
+
+  #checkIsRange(num) {
+    if (num < 1 || num > 45) {
+      throw new Error(WINNING_NUMBERS_ERROR.IS_NOT_RANGE);
+    }
+  }
+
+  #checkDuplicateNum(num) {
+    if (this.getWinningLotto().includes(num)) {
+      throw new Error(WINNING_NUMBERS_ERROR.IS_DUPLICATE_NUMBER);
+    }
   }
 }
 
