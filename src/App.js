6d7b8a0c2b0f73166db22fbc77d59836b3329364
soyeooooo <alththdus1@naver.com>
2024-11-04 13:32:19 +0900

    feat : 당첨금 계산

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async run() {}
+  async run() {
+    let result = this.getResult(comparedList);
+
+    let rate = this.getRate(result[1], price);
+
+    Console.print("당첨 통계\n---");
+    Console.print(result[0]);
+    Console.print(`총 수익률은 ${rate}%입니다.`);
+  }
+
+  getRate(result, price) {
+    const prizeMoney = {
+      3: 5000,
+      4: 50000,
+      5: 1500000,
+      "5(bonus)": 30000000,
+      6: 2000000000,
+    };
+
+    let totalPrize =
+      result[3] * prizeMoney[3] +
+      result[4] * prizeMoney[4] +
+      result[5] * prizeMoney[5] +
+      result["5(bonus)"] * prizeMoney["5(bonus)"] +
+      result[6] * prizeMoney[6];
+
+    let rate = ((totalPrize / price) * 100).toFixed(1);
+    return rate;
+  }
 }
 
 export default App;
