ca07c01dff348c366070ca71f30515c4ad0a363d
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-11-04 15:14:04 +0900

    test: validation 모듈의 integer 함수 테스트 추가
    - integer 함수가 올바른 정수 문자열에 대해 오류가 발생하지 않는지 확인하는 테스트 추가
    - integer 함수가 숫자가 아닌 문자열 또는 안전 범위를 벗어나는 숫자일 때 오류를 발생시키는지 확인하는 테스트 추가

--- __tests__/validations/validationTest.js
+++ __tests__/validations/validationTest.js
@@ -39,4 +39,27 @@ describe('validation 테스트', () => {
     expect(() => validation.safeInteger(Number.MAX_SAFE_INTEGER)).not.toThrow();
     expect(() => validation.safeInteger(Number.MIN_SAFE_INTEGER)).not.toThrow();
   });
+
+  test('integer가 올바른 정수 문자열에 대해 오류를 발생시키지 않는지 테스트', () => {
+    expect(() => validation.integer('123')).not.toThrow();
+  });
+
+  test('integer가 숫자가 아닌 문자열 또는 안전 범위를 벗어나는 숫자일 때 오류를 발생시키는지 테스트', () => {
+    expect(() => validation.integer('')).toThrow(ERROR_MESSAGES.INVALID_NUMBER);
+    expect(() => validation.integer(' ')).toThrow(
+      ERROR_MESSAGES.INVALID_NUMBER
+    );
+    expect(() => validation.integer('-1')).toThrow(
+      ERROR_MESSAGES.INVALID_NUMBER
+    );
+    expect(() => validation.integer('1.1')).toThrow(
+      ERROR_MESSAGES.INVALID_NUMBER
+    );
+    expect(() => validation.integer('1e2')).toThrow(
+      ERROR_MESSAGES.INVALID_NUMBER
+    );
+    expect(() =>
+      validation.integer(String(Number.MAX_SAFE_INTEGER + 1))
+    ).toThrow(ERROR_MESSAGES.SAFE_INTEGER);
+  });
 });
