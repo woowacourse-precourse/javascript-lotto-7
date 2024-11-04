1a2b45d0e651c417f1112b100767c561130e721d
thumbthing <ilscc0902@gmail.com>
2024-11-04 14:39:32 +0900

    Refactor: 입력값 변환 통합 기능 적용

--- src/process/processWinNumber.js
+++ src/process/processWinNumber.js
@@ -1,19 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 import { getWinNumber } from "../feature/UI/getUserInput.js";
-import { parseToArray, parseToNumberArray } from "../feature/parse/parseArray.js";
 import Lotto from "../Lotto.js";
-import checkDecimal from "../feature/validate/checkDecimal.js";
-import { checkEmptyArguments } from "../feature/validate/checkEmptyInput.js";
+import processParseToArray from "./parse/processParseToArray.js";
 
 async function processWinNumber() {
   try {
     const USER_INPUT = await getWinNumber();
-    checkDecimal(USER_INPUT);
 
-    const PARSED_TO_ARRAY = parseToArray(USER_INPUT);
-    checkEmptyArguments(PARSED_TO_ARRAY);
+    const WIN_NUMBER = processParseToArray(USER_INPUT);
 
-    const WIN_NUMBER = parseToNumberArray(PARSED_TO_ARRAY);
     const LOTTO = new Lotto(WIN_NUMBER).getWinNumber;
     
     return LOTTO;
