54ac514538df267d4b6f67db6f652e977324523c
YeWon Seo <syw1236@naver.com>
2024-11-04 23:43:10 +0900

    refator(ViewOutput): 갯수에 대한 출력문 반환하는 함수 따로 분리하여 생성

--- src/ViewOutput.js
+++ src/ViewOutput.js
@@ -15,12 +15,16 @@ export default class ViweOutput {
 
     static printStatistics(statisticsCountMap) {
         winningLottoCountPrice.forEach((lotto) => {
-            let matchCount = `${lotto.count}${statisticsMesssage.PRINT_MATCH}`;
-            if(lotto.count === number.FIVE_BONUS)
-                matchCount = `${number.FIVE}${statisticsMesssage.PRINT_MATCH}, ${statisticsMesssage.PRINT_BONUS}`;
+            const matchCount = this.getMathCountExpression(lotto);
             this.printText(
                 `${matchCount} ${statisticsMesssage.OPENING_PARENTHESIS}${lotto.price}${statisticsMesssage.MONEY_UNIT}${statisticsMesssage.CLOSING_PARENTHESIS} ${statisticsMesssage.PRINT_BAR} ${statisticsCountMap.get(lotto.count)}${lottoMesaage.PRINT_COUNT}`
             );       
         });
+    }
+
+    static getMathCountExpression(lotto) {
+        if(lotto.count === number.FIVE_BONUS)
+            return `${number.FIVE}${statisticsMesssage.PRINT_MATCH}, ${statisticsMesssage.PRINT_BONUS}`;
+        return `${lotto.count}${statisticsMesssage.PRINT_MATCH}`;
     }
 }
