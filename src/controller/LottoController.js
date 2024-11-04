6b89524d810c342b10681c538573edeef1cd96fa
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-11-04 23:17:17 +0900

    feat: LottoController 로직 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -3,7 +3,23 @@ import OutputView from "../view/OutputView.js";
 import LottoGame from "../LottoGame.js";
 
 class LottoController {
-  
+  async start() {
+    try {
+      const purchaseAmount = await InputView.getPurchaseAmount();
+      const lottoGame = new LottoGame(purchaseAmount);
+      OutputView.printLottoTickets(lottoGame.lottoTickets);
+
+      const winningNumbers = await InputView.getWinningNumbers();
+      const bonusNumber = await InputView.getBonusNumber();
+
+      const ranks = lottoGame.calculateRanks(winningNumbers, bonusNumber);
+      const earningRate = this.calculateEarningRate(ranks, purchaseAmount);
+
+      OutputView.printResults(ranks, earningRate);
+    } catch (error) {
+      OutputView.printError(error.message);
+    }
+  }
 }
 
 export default LottoController;
