9fe19514e9d269856d93f5327dab78a340dcc916
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-04 14:16:43 +0900

    refactor: 당첨 통계 출력 로직의 중복 코드 제거 및 가독성 향상

--- src/OutputValue.js
+++ src/OutputValue.js
@@ -23,22 +23,19 @@ const OutputValue = {
   },
 
   printWinningStatistics(winningStatisticsList) {
+    const winningMessages = [
+      OUTPUT_MESSAGE.WINNING_RESULT_MATCH_SIX_PRIZE,
+      OUTPUT_MESSAGE.WINNING_RESULT_MATCH_FIVE_WITH_BONUS_PRIZE,
+      OUTPUT_MESSAGE.WINNING_RESULT_MATCH_FIVE_PRIZE,
+      OUTPUT_MESSAGE.WINNING_RESULT_MATCH_FOUR_PRIZE,
+      OUTPUT_MESSAGE.WINNING_RESULT_MATCH_THREE_PRIZE,
+    ];
+
     Console.print(OUTPUT_MESSAGE.WINNING_STATISTICS);
-    Console.print(
-      `${OUTPUT_MESSAGE.WINNING_RESULT_MATCH_SIX_PRIZE}${winningStatisticsList[0]}개`
-    );
-    Console.print(
-      `${OUTPUT_MESSAGE.WINNING_RESULT_MATCH_FIVE_WITH_BONUS_PRIZE}${winningStatisticsList[1]}개`
-    );
-    Console.print(
-      `${OUTPUT_MESSAGE.WINNING_RESULT_MATCH_FIVE_PRIZE}${winningStatisticsList[2]}개`
-    );
-    Console.print(
-      `${OUTPUT_MESSAGE.WINNING_RESULT_MATCH_FOUR_PRIZE}${winningStatisticsList[3]}개`
-    );
-    Console.print(
-      `${OUTPUT_MESSAGE.WINNING_RESULT_MATCH_THREE_PRIZE}${winningStatisticsList[4]}개`
-    );
+
+    winningMessages.forEach((message, index) => {
+      Console.print(`${message}${winningStatisticsList[index]}개`);
+    });
   },
 
   printProfitability(profitability) {
