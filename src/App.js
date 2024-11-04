46ca54cf2cc30a1755a9a1afaa42929da560de1c
xxxjinn <tlsguswls319@naver.com>
2024-11-04 15:29:23 +0900

    feat(App.js): 당첨 번호 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 // App.js
 import { Console } from "@woowacourse/mission-utils";
-import { PURCHASE_MESSAGE } from "./constants.js";
+import { PURCHASE_MESSAGE, LOTTO_MESSAGE } from "./constants.js";
 import Purchase from "./Purchase.js";
+import Lotto from "./Lotto.js";
 
 class App {
   async run() {
     const purchase = await this.getValidPurchase();
     purchase.generateAllRandomNumbers();
     this.displayPurchaseResults(purchase);
+
+    const winningLotto = await this.getWinningLottoNumbers();
   }
 
+  /**구입 금액 */
   async getValidPurchase() {
     while (true) {
       try {
-        const cost = await Console.readLineAsync(PURCHASE_MESSAGE.PURCHASE_COST_MESSAGE);
+        const cost = await Console.readLineAsync(PURCHASE_MESSAGE.INPUT_PURCHASE_COST);
         return new Purchase(cost);
       } catch (error) {
         Console.print(`\n${error.message}\n`);
@@ -23,11 +27,24 @@ class App {
 
   displayPurchaseResults(purchase) {
     const purchaseCount = purchase.getPurchaseCount();
-    Console.print(`\n${purchaseCount}${PURCHASE_MESSAGE.PURCHASE_AMOUNT_MESSAGE}`);
+    Console.print(`\n${purchaseCount}${PURCHASE_MESSAGE.PURCHASE_AMOUNT}`);
 
     purchase.getRandomNumbersList().forEach((numbers) => {
       Console.print(numbers);
     });
+  }
+
+  /** 로또 */
+  async getWinningLottoNumbers() {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(LOTTO_MESSAGE.INPUT_LOTTO_NUMBERS);
+        const numbers = input.split(",").map(Number);
+        return new Lotto(numbers);
+      } catch (error) {
+        Console.print(`\n${error.message}\n`);
+      }
+    }
   }
 }
 
