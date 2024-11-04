171e92b6a26f85acdedb484fd4c086078ff45833
standard-chan <starp321@naver.com>
2024-11-04 20:51:25 +0900

    refactor : inputView 문자열 split 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import InputView from "./view/InputView.js";
 class App {
   async run() {
     const input = new InputView();
+    
     const purchaseAmount = await input.readPurchaseAmount();
     const winningNumbers = await input.readWinningNumbers();
     const bonusNumber = await input.readBonusNumber();
     const allLottoNumbers = [...winningNumbers, bonusNumber];
     LottoValidator.validateDuplicateNumbers(allLottoNumbers);
     allLottoNumbers.forEach(LottoValidator.validatorSingleNumber);
+
+
 
     
 
--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,21 +1,32 @@
 import { Console } from "@woowacourse/mission-utils";
-
+import LottoValidator from "../validators/LottoValidator.js";
 
 export default class InputView {
 
   async readPurchaseAmount() {
-    const PurchaseAmount = await Console.readLineAsync("구입금액을 입력해 주세요.");
-    return PurchaseAmount;
+    const purchaseAmountStr = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    const purchaseAmount = Number(purchaseAmountStr);
+    LottoValidator.validatorPurchaseAmount(purchaseAmount);
+    return purchaseAmount;
   }
 
-  async readWinningNumber() {
-    const winningNumber = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
-    return winningNumber;
+  async readWinningNumbers() {
+    const winningNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    LottoValidator.validatorLottoNumbers(winningNumbers);
+    const winningNumbersArr = this.splitNumbers(winningNumbers);
+    return winningNumbersArr;
   }
 
   async readBonusNumber() {
-    const bonusNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
+    let bonusNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    LottoValidator.validatorBonusNumber(bonusNumber);
+    bonusNumber = Number(bonusNumber.trim());
     return bonusNumber;
+  }
+
+  splitNumbers(winningNumbers) {
+    const numbersArray = winningNumbers.split(',').map(number => Number(number.trim()));
+    return numbersArray;
   }
 
 }
