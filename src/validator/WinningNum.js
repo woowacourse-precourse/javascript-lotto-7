60f9f0c3ae024becc034bab64d0d5bf1e7e52ffc
zelkovaria <hyein396@gmail.com>
2024-11-04 14:36:09 +0900

    refactor: 개행 조절

--- src/validator/WinningNum.js
+++ src/validator/WinningNum.js
@@ -1,5 +1,6 @@
 const validateNumberOnly = (winningNumber) => {
   const isNumbers = winningNumber.every((num) => Number.isInteger(num));
+
   if (!isNumbers) {
     throw new Error("[ERROR] 로또 번호는 정수로만 입력되어야 합니다.");
   }
@@ -7,6 +8,7 @@ const validateNumberOnly = (winningNumber) => {
 
 const validateNumberRange = (winningNumber) => {
   const isInRange = winningNumber.every((num) => 1 <= num && num <= 45);
+
   if (!isInRange) {
     throw new Error("[ERROR] 로또 번호는 1~45 사이로 입력되어야 합니다.");
   }
@@ -16,6 +18,7 @@ export const validateWinningNumber = (winningNumber) => {
   if (!Array.isArray(winningNumber)) {
     throw new Error("[ERROR] 잘못된 형식의 당첨 번호가 입력되었습니다.");
   }
+
   validateNumberOnly(winningNumber);
   validateNumberRange(winningNumber);
 };
