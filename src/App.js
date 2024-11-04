ea620ca0b33813cab0b31d61bb1c1a760178134e
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-04 23:16:23 +0900

    feat: 로또 발매기 로직 구현
    App.js 파일에 이하 내용 추가:
    - 로또 발매기 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { lotto } from "../constants/lottoConstants.js";
+import {
+  generateLottos,
+  printLottoResult,
+  printLottos,
+} from "../modules/lottoService.js";
+import {
+  getValidatedPurchaseAmount,
+  getValidatedWinningNumbers,
+} from "../modules/userInput.js";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await getValidatedPurchaseAmount();
+
+    const lottoCount = Math.floor(purchaseAmount / lotto.PRICE);
+
+    const lottos = generateLottos(lottoCount);
+
+    printLottos(lottos);
+
+    const winningNumbers = await getValidatedWinningNumbers();
+
+    const bonusNumber = await getValidatedBonusNumber();
+
+    printLottoResult(lottos, winningNumbers, bonusNumber, purchaseAmount);
+  }
 }
 
 export default App;
