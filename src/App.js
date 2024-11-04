8d831dae592ac41bf92893bcae93f2552570aaac
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-11-04 19:38:15 +0900

    ✨feat: 당첨번호, 보너스 번호 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 import PurchaseLotto from './PurchaseLotto.js';
 import { validatePurchaseMoney } from './validators/validatePurchaseMoney.js';
-import { parsePurchaseMoney } from './utils/Parser.js';
+import {
+  validateLottoNumbers,
+  validateBonusNumber,
+} from './validators/validateLottoNumbers.js';
+import { parseToNumber, parseWinningNumbers } from './utils/Parser.js';
 import { promptUserInput } from './utils/Prompt.js';
 import LottoList from './LottoList.js';
 
@@ -9,13 +13,27 @@ class App {
   async run() {
     const purchaseMoney = await promptUserInput(
       '구입금액을 입력해 주세요.\n',
-      parsePurchaseMoney,
+      parseToNumber,
       validatePurchaseMoney,
     );
 
     const purchaseLotto = new PurchaseLotto(purchaseMoney);
     const lottoList = new LottoList(purchaseLotto.getQuantity());
     lottoList.printTickets();
+
+    const winningNumbers = await promptUserInput(
+      '\n당첨 번호를 입력해 주세요.\n',
+      parseWinningNumbers,
+      validateLottoNumbers,
+    );
+    Console.print(winningNumbers);
+
+    const bonusNumber = await promptUserInput(
+      '\n보너스 번호를 입력해 주세요.\n',
+      parseToNumber,
+      number => validateBonusNumber(number, winningNumbers),
+    );
+    Console.print(bonusNumber);
   }
 }
 
