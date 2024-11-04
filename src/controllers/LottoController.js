123a4abc31296f78c16788a57561d76030d1422b
H0ngJu <hongju4385@gmail.com>
2024-11-04 20:57:49 +0900

    refac: contorller 함수 분리

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -4,12 +4,12 @@ import { generateLottoNumbers } from "../utils/generateLottoNumbers.js";
 import { Console } from "@woowacourse/mission-utils";
 import OutputLotto from "../views/OutputLotto.js";
 import LottoModel from "../models/LottoModel.js";
-import Lotto from "../Lotto.js";
 import { LOTTO_SETTING } from "../constants/Settings.js";
 
 class LottoController {
   #inputLotto;
   #outputLotto;
+  #lottoModel;
 
   constructor() {
     this.#inputLotto = new InputLotto();
@@ -17,43 +17,82 @@ class LottoController {
   }
 
   async start() {
-    try {
-      const purchaseAmount = await this.#inputLotto.getPurchaseAmount();
-      validators.checkMoneyInput(purchaseAmount);
-      validators.checkMinMoneyInput(purchaseAmount);
-      validators.checkDivisible(purchaseAmount);
-      validators.checkLimitMoney(purchaseAmount);
+    const purchaseAmount = await this.handlePurchaseAmount();
+    const lottoModel = new LottoModel(
+      Math.floor(purchaseAmount / LOTTO_SETTING.PRICE_OF_EACH_LOTTO)
+    );
+    this.#lottoModel = lottoModel;
 
-      const lottoModel = new LottoModel(
-        Math.floor(purchaseAmount / LOTTO_SETTING.PRICE_OF_EACH_LOTTO)
-      );
-      const lottoTickets = lottoModel.getLottoTickets();
-      this.#outputLotto.showLottoNumbers(lottoTickets);
+    const lottoTickets = lottoModel.getLottoTickets();
+    this.#outputLotto.showLottoNumbers(lottoTickets);
 
-      const lottoNumbers = await this.#inputLotto.getInputNumbers();
-      validators.checkNumberOfLotto(lottoNumbers);
-      lottoNumbers.forEach((number) => {
-        validators.checkNumber(number);
-        validators.checkRangeOfNumber(number);
-      });
-      validators.checkDuplicateNumber(lottoNumbers);
+    const lottoNumbers = await this.handleLottoNumbers();
+    const bonusNumber = await this.handleBonusNumber(lottoNumbers);
 
-      const bonusNumber = await this.#inputLotto.getBonusNumber();
-      validators.checkNumber(bonusNumber);
-      validators.checkRangeOfNumber(bonusNumber);
-      validators.checkBonusNumber(bonusNumber, lottoNumbers);
+    const matchCounts = lottoModel.calculateMatches(lottoNumbers, bonusNumber);
 
-      const matchCounts = lottoModel.calculateMatches(
-        lottoNumbers,
-        bonusNumber
-      );
+    this.#outputLotto.showMatchStatistics(matchCounts);
+    this.#outputLotto.showRateOfReturn(
+      lottoModel.calc_rate_of_return(matchCounts, purchaseAmount)
+    );
+  }
 
-      this.#outputLotto.showMatchStatistics(matchCounts);
-      this.#outputLotto.showRateOfReturn(
-        lottoModel.calc_rate_of_return(matchCounts, purchaseAmount)
-      );
-    } catch (error) {
-      Console.print(`${error.message}`);
+  async handlePurchaseAmount() {
+    let isValid = false;
+    let purchaseAmount;
+
+    while (!isValid) {
+      try {
+        purchaseAmount = await this.#inputLotto.getPurchaseAmount();
+        validators.checkMoneyInput(purchaseAmount);
+        validators.checkMinMoneyInput(purchaseAmount);
+        validators.checkDivisible(purchaseAmount);
+        validators.checkLimitMoney(purchaseAmount);
+        isValid = true;
+        return purchaseAmount;
+      } catch (error) {
+        Console.print(`${error.message}`);
+        throw error;
+      }
+    }
+  }
+
+  async handleLottoNumbers() {
+    let isValid = false;
+    let lottoNumbers;
+
+    while (!isValid) {
+      try {
+        lottoNumbers = await this.#inputLotto.getInputNumbers();
+        validators.checkNumberOfLotto(lottoNumbers);
+        lottoNumbers.forEach((number) => {
+          validators.checkNumber(number);
+          validators.checkRangeOfNumber(number);
+        });
+        validators.checkDuplicateNumber(lottoNumbers);
+        isValid = true;
+        return lottoNumbers;
+      } catch (error) {
+        Console.print(`${error.message}`);
+      }
+    }
+  }
+
+  async handleBonusNumber(lottoNumbers) {
+    let isValid = false;
+    let bonusNumber;
+
+    while (!isValid) {
+      try {
+        bonusNumber = await this.#inputLotto.getBonusNumber();
+        validators.checkNumber(bonusNumber);
+        validators.checkRangeOfNumber(bonusNumber);
+        validators.checkBonusNumber(bonusNumber, lottoNumbers);
+        isValid = true;
+        return bonusNumber;
+      } catch (error) {
+        Console.print(`${error.message}`);
+      }
     }
   }
 }
