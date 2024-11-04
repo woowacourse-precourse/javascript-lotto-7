b248c3561ec41093fd303200c0076704d599f16f
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-04 15:16:20 +0900

    feat: 생성한 로또 번호에 대한 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE as ERROR } from "./constants/errorMessage.js";
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,22 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR.INVALID_NUMBERS_LENGTH);
+    }
+
+    if (numbers.some((number) => number < 1 || number > 45)) {
+      throw new Error(ERROR.INVALID_NUMBERS_RANGE);
+    }
+
+    if (numbers.some((number) => isNaN(number))) {
+      throw new Error(ERROR.NOT_NUMBER);
     }
   }
 
   // TODO: 추가 기능 구현
+  get getLottoNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
