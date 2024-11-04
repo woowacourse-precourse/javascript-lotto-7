8694cdb4034d0ea6a117b6832869a3dfc123f2c3
hihilong <hyelong20630@gmail.com>
2024-11-04 17:22:35 +0900

    feat:로또시스템

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,44 @@
+import { validateLottoNumbers } from './error.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    validateLottoNumbers(numbers);  
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  getNumbers() {
+    return this.#numbers;
   }
 
-  // TODO: 추가 기능 구현
+  checkWinningNumbers(winningNumbers, bonusNumber) {
+    const matchCount = this.#numbers.filter(num => winningNumbers.includes(num)).length;
+    const bonusMatch = this.#numbers.includes(bonusNumber);
+    return { matchCount, bonusMatch };
+  }
+
+  static calculateResults(tickets, winningNumbers, bonusNumber) {
+    const winnings = { 3: 0, 4: 0, 5: 0, 5.5: 0, 6: 0 };
+
+    tickets.forEach(ticket => {
+      const { matchCount, bonusMatch } = ticket.checkWinningNumbers(winningNumbers, bonusNumber);
+      if (matchCount === 6) winnings[6]++;
+      else if (matchCount === 5 && bonusMatch) winnings[5.5]++;
+      else if (matchCount === 5) winnings[5]++;
+      else if (matchCount === 4) winnings[4]++;
+      else if (matchCount === 3) winnings[3]++;
+    });
+
+    const totalWinnings =
+      winnings[3] * 5000 +
+      winnings[4] * 50000 +
+      winnings[5] * 1500000 +
+      winnings[5.5] * 30000000 +
+      winnings[6] * 2000000000;
+
+    return { winnings, totalWinnings };
+  }
 }
 
 export default Lotto;
