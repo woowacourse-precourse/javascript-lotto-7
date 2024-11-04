ea95ba01feace7025690ee66120713432625483c
Jin-Chanyong <cksdyd324@gmail.com>
2024-11-04 20:41:49 +0900

    refactor: 에러 발생 함수 변경
    throwError.js 파일의 이하 내용 수정:
    - throwError 함수가 하나의 기능만을 하기 위해, 에러 출력 기능 제거

--- utils/throwError.js
+++ utils/throwError.js
@@ -1,6 +1,3 @@
-import { Console } from "@woowacourse/mission-utils";
-
 export default function throwError(message) {
-  Console.print(`[ERROR] ${message}`);
   throw new Error(`[ERROR] ${message}`);
 }
