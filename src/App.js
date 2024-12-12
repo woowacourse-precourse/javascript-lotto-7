4e0b2acdd2b466cd0a972afb046e79a00f0759cb
ahhyun <wndkgus0225@naver.com>
2024-12-12 22:22:55 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import { Console } from '@woowacourse/mission-utils';
-import { getCost, displayLottosCount, displayLottosNumber, userPickedNumbers, userPickedBonusNumber, printResults } from '../src/IO.js';
+import { getCost, displayCount, displayLottosNumber, getNumbers, getBonus, displayResult, printResults } from '../src/IO.js';
 import { generateLottos } from '../src/Lotto.js';
-import { evaluateResults, countMatches } from '../src/result.js';
+import { evaluateResult } from '../src/result.js';
 
-class App {
-  cost;
+class App { // App이라는 클래스 생성
+  cost; // cost라는 변수 선언.
 
-  constructor() {
-    this.cost = 0;
+  constructor(){
+    this.cost = 0;  // 이 변수를 0으로 초기화한다
   }
 
   async run() {
     try {
       const count = await getCost();
-      displayLottosCount(count);
+      displayCount(count);
+      const lottos = generateLottos(count)
+      displayLottosNumber(lottos)
 
-      const lottos = generateLottos(count);
-      displayLottosNumber(lottos);
+      const pickedNumbers = await getNumbers();
+      const pickedBonus = await getBonus(pickedNumbers);
+      displayResult();
 
-      const pickedNumbers = await userPickedNumbers();
-      const pickedBonusNumber = await userPickedBonusNumber(pickedNumbers);
-
-      const rankCount = evaluateResults(lottos, pickedNumbers, pickedBonusNumber);
+      const rankCount = evaluateResult(lottos, pickedNumbers, pickedBonus);
       printResults(rankCount, count * 1000);
+
     } catch (error) {
       Console.print(`[ERROR] 오류가 발생했습니다: ${error.message}`);
-    }
   }
+}
 }
 
 export default App;
+
 
