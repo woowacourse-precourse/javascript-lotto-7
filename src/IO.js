bbef933175f4625b428f90137b2d29990cb09b8e
ahhyun <wndkgus0225@naver.com>
2024-12-12 22:23:37 +0900

    3

--- src/IO.js
+++ src/IO.js
@@ -1,38 +1,38 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from '../src/message.js';
-import { validateCost, validatePickedNumbers, validatePickedBonusNumbers } from '../src/error.js';
-import { generateLottos } from '../src/Lotto.js';
-import { evaluateResults } from '../src/result.js';
+import { validateCost, validatePickedNumbers, validateBouns } from '../src/error.js';
 
-export async function getCost() {
+export async function getCost(){
   const cost = await Console.readLineAsync(MESSAGE.ASK_COST+"\n");
   validateCost(Number(cost));
   const count = cost/1000;
   return count;
 }
 
-export async function displayLottosCount(count){
-  Console.print("\n"+count+MESSAGE.COUNT_INFO);
+export function displayCount(count){
+  Console.print("\n"+count + MESSAGE.SHOW_COUNT);
 }
 
-export function displayLottosNumber(lottos) { // count 대신 lottos를 매개변수로 받습니다.
+export function displayLottosNumber(lottos) {
   lottos.forEach(lotto => {
     Console.print(`[${lotto.getNumbers().join(', ')}]`);
   });
 }
 
-
-export async function userPickedNumbers() {
-  const pickedNumbers = await Console.readLineAsync("\n"+MESSAGE.ASK_USER_PICK_NUMBER+"\n");
+export async function getNumbers() {
+  const pickedNumbers = await Console.readLineAsync("\n"+MESSAGE.ASK_NUMBERS+"\n");
   validatePickedNumbers(pickedNumbers);
   return pickedNumbers.split(',').map(number => Number(number.trim()));
 } 
 
+export async function getBonus(pickedNumbers){
+  const pickedBonus = await Console.readLineAsync("\n"+MESSAGE.ASK_BONUS+"\n")
+  validateBouns(pickedBonus, pickedNumbers)
+  return pickedBonus;
+}
 
-export async function userPickedBonusNumber(pickedNumbers) {
-  const pickedBounsNumber = await Console.readLineAsync("\n"+MESSAGE.ASK_USER_PICK_BONUS_NUMBER+"\n");
-  validatePickedBonusNumbers(pickedBounsNumber, pickedNumbers);
-  return Number(pickedBounsNumber.trim()); 
+export function displayResult(){
+  Console.print("\n"+ MESSAGE.SHOW_RESULT);
 }
 
 
@@ -52,11 +52,10 @@ export function printResults(rankCount, cost) {
 
   const yieldPercentage = ((totalPrize / cost) * 100).toFixed(1);
 
-  Console.print("\n"+MESSAGE.PRIZE_STATISTICS);
-  Console.print(MESSAGE.CORRECT_THREE+`${rankCount[5]}개`);
-  Console.print(MESSAGE.CORRECT_FOUR+`${rankCount[4]}개`);
-  Console.print(MESSAGE.CORRECT_FIVE+`${rankCount[3]}개`);
-  Console.print(MESSAGE.CORRECT_FIVE_WITH_BONUS+`${rankCount[2]}개`);
-  Console.print(MESSAGE.CORRECT_SIX+`${rankCount[1]}개`);
-  Console.print(MESSAGE.TOTAL_YIELD_1+`${yieldPercentage}%`+MESSAGE.TOTAL_YIELD_2);
+  Console.print(MESSAGE.THREE+`${rankCount[5]}개`);
+  Console.print(MESSAGE.FOUR+`${rankCount[4]}개`);
+  Console.print(MESSAGE.FIVE+`${rankCount[3]}개`);
+  Console.print(MESSAGE.FIVE_WITH_BONUS+`${rankCount[2]}개`);
+  Console.print(MESSAGE.SIX+`${rankCount[1]}개`);
+  Console.print(MESSAGE.TOTAL_1+`${yieldPercentage}%`+MESSAGE.TOTAL_2);
 }
