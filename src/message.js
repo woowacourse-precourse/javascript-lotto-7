1fbf1b86bc952e43e4574c0320e687ecb777ae3d
ahhyun <wndkgus0225@naver.com>
2024-12-12 22:23:55 +0900

    5

--- src/message.js
+++ src/message.js
@@ -1,26 +1,24 @@
 export const MESSAGE = {
   ASK_COST: "구입금액을 입력해 주세요.",
-  COUNT_INFO: "개를 구매했습니다.",
-  ASK_USER_PICK_NUMBER: "당첨 번호를 입력해 주세요.",
-  ASK_USER_PICK_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
-  PRIZE_STATISTICS: "당첨 통계\n---",
-  CORRECT_THREE: "3개 일치 (5,000원) - ",
-  CORRECT_FOUR: "4개 일치 (50,000원) - ",
-  CORRECT_FIVE: "5개 일치 (1,500,000원) - ",
-  CORRECT_FIVE_WITH_BONUS: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
-  CORRECT_SIX: "6개 일치 (2,000,000,000원) - ",
-  TOTAL_YIELD_1: "총 수익률은 ",
-  TOTAL_YIELD_2: "입니다."
-}
+  SHOW_COUNT: "개를 구매했습니다.",
+  ASK_NUMBERS: "당첨 번호를 입력해 주세요.",
+  ASK_BONUS: "보너스 번호를 입력해 주세요.",
+  SHOW_RESULT: "당첨통계\n---",
+  THREE: "3개 일치 (5,000원) - ",
+  FOUR: "4개 일치 (50,000원) - ",
+  FIVE: "5개 일치 (1,500,000원) - ",
+  FIVE_WITH_BONUS: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  SIX: "6개 일치 (2,000,000,000원) - ",
+  TOTAL_1: "총 수익률은 ",
+  TOTAL_2: "입니다."
+} 
 
 export const ERROR_MESSAGE = {
   NOT_NUMBER: "[ERROR] 숫자 이외의 입력은 사용할 수 없습니다.",
-  NOT_INTEGER: "[ERROR] 정수 이외의 입력은 사용할 수 없습니다.",
-  NUM_IS_ZERO : "[ERROR] 0은 입력할 수 없습니다.",
-  NEGATIVE_NUMBER: "[ERROR] 음수는 입력할 수 없습니다.",
-  VALIDATE_DIVERSITY_BY_THOUSAND: "[ERROR] 금액은 1000원 단위이어야 합니다.",
-  NOT_SIX_NUMBERS: "[ERROR] 당첨 번호는 6개이어야 합니다.",
-  DUPLICATE_NUMBER: "[ERROR] 당첨 번호는 중복될 수 없습니다,",
-  INVALID_SEPARATOR: "[ERROR] 번호는 쉼표(,)로 구분해야 합니다.",
-  OUT_OF_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+  NUM_IS_ZERO: "[ERROR] 0은 입력할 수 없습니다.",
+  NUM_IS_NEGATIVE: "[ERROR] 음수는 입력할 수 없습니다",
+  CANT_DIVIDE_1000: "[ERROR] 금액은 1000원 단위여야 합니다.",
+  NOT_SIX_NUMBERS: "[ERROR] 6개의 숫자를 입력해야 합니다.",
+  DUPLICATE_NUMBER: "[ERROR] 중복된 숫자는 입력할 수 없습니다.",
+  OUT_OF_RANGE: "[ERROR] 1~45의 범위를 벗어난 숫자는 입력할 수 없습니다."
 }
